53729e188a4855c91433c70272857fe58f8fbffa
Junyi <mytharcher@users.noreply.github.com>
2020-11-16 20:38:56 +0800

    Refactor: change global injection of test for actions package. (#15)
    * Refactor: change global injection to index.ts to simplify all test files.
    
    * Fix: typo.

--- packages/actions/src/__tests__/create.test.ts
+++ packages/actions/src/__tests__/create.test.ts
@@ -1,36 +1,17 @@
-import Koa from 'koa';
-import http from 'http';
-import request from 'supertest';
-import actions from '..';
-import { getConfig } from './index';
-import Database, { Model } from '@nocobase/database';
-import Resourcer from '@nocobase/resourcer';
-import { resolve } from 'path';
+import { initDatabase, agent } from './index';
 
 describe('create', () => {
-  let db: Database;
-  // let resourcer: Resourcer;
-  let app: Koa;
-  beforeAll(async () => {
-    const config = getConfig();
-    app = config.app;
-    db = config.database;
-    db.import({
-      directory: resolve(__dirname, './tables'),
-    });
-    await db.sync({
-      force: true,
-    });
-    // resourcer = config.resourcer;
+  let db;
+  
+  beforeEach(async () => {
+    db = await initDatabase();
   });
-
-  afterAll(async () => {
-    await db.close();
-  });
+  
+  afterAll(() => db.close());
 
   describe('common', () => {
     it('create', async () => {
-      const response = await request(http.createServer(app.callback()))
+      const response = await agent
         .post('/posts')
         .send({
           title: 'title1',
@@ -43,7 +24,7 @@ describe('create', () => {
     it('create', async () => {
       const Post = db.getModel('posts');
       const post = await Post.create();
-      const response = await request(http.createServer(app.callback()))
+      const response = await agent
         .post(`/posts/${post.id}/comments`)
         .send({
           content: 'content1',
