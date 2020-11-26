708a112629d91acda656386e04f2147e51cf35a3
chen <chenlinxh@gmail.com>
2020-11-26 10:12:13 +0800

    add field interface options

---
+++ packages/plugin-collections/src/interfaces/index.ts
@@ -0,0 +1,75 @@
+/**
+ * 考虑到 Interface 的参数模板还不固定，暂时先放这里了，便于后续修改
+ */
+import * as types from './types';
+export * as types from './types';
+
+export const options = [
+  {
+    title: '基本类型',
+    children: [
+      types.string,
+      types.textarea,
+      types.phone,
+      types.email,
+      types.number,
+      types.percent,
+    ],
+  },
+  {
+    title: '多媒体类型',
+    children: [
+      types.wysiwyg,
+      types.attachment,
+    ],
+  },
+  {
+    title: '选择类型',
+    children: [
+      types.boolean,
+      types.select,
+      types.multipleSelect,
+      types.radio,
+      types.checkboxes,
+    ],
+  },
+  {
+    title: '日期和时间',
+    children: [
+      types.date,
+      types.time,
+    ],
+  },
+  {
+    title: '关系类型',
+    children: [
+      types.subTable,
+      types.linkTo,
+    ],
+  },
+  {
+    title: '系统信息',
+    children: [
+      types.createdAt,
+      types.createdBy,
+      types.updatedAt,
+      types.updatedBy,
+    ],
+  },
+  {
+    title: '开发者模式',
+    children: [
+      types.primaryKey,
+      types.sort,
+      types.password,
+    ],
+  }
+].map(({title, children}) => ({
+  label: title,
+  children: children.map(child => ({
+    label: child.title,
+    value: child.options.interface,
+  })),
+}));
+
+export default options;
---
+++ packages/plugin-collections/src/interfaces/types.ts
@@ -0,0 +1,357 @@
+export const string = {
+  title: '单行文本',
+  options: {
+    interface: 'string',
+    type: 'string',
+    component: {
+      type: 'string',
+    },
+  },
+}
+
+export const textarea = {
+  title: '多行文本',
+  options: {
+    interface: 'textarea',
+    type: 'text',
+    component: {
+      type: 'textarea',
+    },
+  }
+}
+
+export const phone = {
+  title: '手机号码',
+  options: {
+    interface: 'phone',
+    type: 'string',
+    format: 'phone',
+    component: {
+      type: 'string',
+    },
+  },
+}
+
+export const email = {
+  title: '邮箱',
+  options: {
+    interface: 'email',
+    type: 'string',
+    format: 'email',
+    component: {
+      type: 'string',
+    },
+  },
+}
+
+export const number = {
+  title: '数字',
+  options: {
+    interface: 'number',
+    type: 'integer',
+    component: {
+      type: 'number',
+      precision: 0,
+    },
+  }
+}
+
+export const percent = {
+  title: '百分比',
+  options: {
+    interface: 'percent',
+    type: 'integer',
+    component: {
+      type: 'number',
+      suffix: '%',
+      precision: 0,
+    },
+  },
+}
+
+export const wysiwyg = {
+  title: '可视化编辑器',
+  options: {
+    interface: 'wysiwyg',
+    type: 'text',
+    component: {
+      type: 'wysiwyg',
+    },
+  },
+}
+
+export const attachment = {
+  title: '附件',
+  options: {
+    interface: 'attachment',
+    type: 'belongsToMany',
+    targetKey: 'attachments',
+    component: {
+      type: 'fileManager',
+    },
+  },
+}
+
+export const select = {
+  title: '下拉选择（单选）',
+  options: {
+    interface: 'select',
+    type: 'belongsTo',
+    fields: [
+      {
+        interface: 'sort',
+        type: 'integer',
+        name: 'title',
+        title: '排序',
+        component: {
+          type: 'sort',
+        },
+      },
+      {
+        interface: 'string',
+        type: 'string',
+        name: 'title',
+        title: '选项',
+        component: {
+          type: 'string',
+        },
+      },
+    ],
+    component: {
+      type: 'select',
+    },
+  },
+};
+
+export const multipleSelect = {
+  title: '下拉选择（多选）',
+  options: {
+    interface: 'multipleSelect',
+    type: 'belongsToMany',
+    fields: [
+      {
+        interface: 'sort',
+        type: 'integer',
+        name: 'title',
+        title: '排序',
+        component: {
+          type: 'sort',
+        },
+      },
+      {
+        interface: 'string',
+        type: 'string',
+        name: 'title',
+        title: '选项',
+        component: {
+          type: 'string',
+        },
+      },
+    ],
+    component: {
+      type: 'select',
+      multiple: true,
+    },
+  },
+};
+
+export const radio = {
+  title: '单选框',
+  options: {
+    interface: 'radio',
+    type: 'belongsTo',
+    fields: [
+      {
+        interface: 'sort',
+        type: 'integer',
+        name: 'title',
+        title: '排序',
+        component: {
+          type: 'sort',
+        },
+      },
+      {
+        interface: 'string',
+        type: 'string',
+        name: 'title',
+        title: '选项',
+        component: {
+          type: 'string',
+        },
+      },
+    ],
+    component: {
+      type: 'radio',
+    },
+  },
+}
+
+export const checkboxes = {
+  title: '多选框',
+  options: {
+    interface: 'checkboxes',
+    type: 'belongsToMany',
+    fields: [
+      {
+        interface: 'sort',
+        type: 'integer',
+        name: 'title',
+        title: '排序',
+        component: {
+          type: 'sort',
+        },
+      },
+      {
+        interface: 'string',
+        type: 'string',
+        name: 'title',
+        title: '选项',
+        component: {
+          type: 'string',
+        },
+      },
+    ],
+    component: {
+      type: 'checkboxes',
+    },
+  },
+}
+
+export const boolean = {
+  title: '是/否',
+  options: {
+    interface: 'boolean',
+    type: 'boolean',
+    component: {
+      type: 'checkbox', // switch
+    },
+  },
+}
+
+export const date = {
+  title: '日期',
+  options: {
+    interface: 'date',
+    type: 'date',
+    component: {
+      type: 'date',
+    },
+  },
+};
+
+export const time = {
+  title: '时间',
+  options: {
+    interface: 'time',
+    type: 'time',
+    component: {
+      type: 'time',
+    },
+  },
+}
+
+export const subTable = {
+  title: '子表格',
+  options: {
+    interface: 'subTable',
+    type: 'hasMany',
+    component: {
+      type: 'subTable',
+    },
+  },
+}
+
+export const linkTo = {
+  title: '关联数据',
+  options: {
+    interface: 'linkTo',
+    type: 'belongsToMany',
+    component: {
+      type: 'drawerSelect',
+    },
+  },
+}
+
+export const createdBy = {
+  title: '创建者',
+  options: {
+    interface: 'createdBy',
+    type: 'belongsTo',
+    component: {
+      type: 'drawerSelect',
+    },
+  },
+}
+
+export const createdAt = {
+  title: '创建时间',
+  options: {
+    interface: 'createdAt',
+    type: 'date',
+    required: true,
+    component: {
+      type: 'date',
+    },
+  },
+};
+
+export const updatedBy = {
+  title: '更新人',
+  options: {
+    interface: 'updatedBy',
+    type: 'belongsTo',
+    component: {
+      type: 'drawerSelect',
+    },
+  },
+}
+
+export const updatedAt = {
+  title: '更新日期',
+  options: {
+    interface: 'updatedAt',
+    type: 'date',
+    required: true,
+    component: {
+      type: 'date',
+    },
+  },
+};
+
+export const primaryKey = {
+  title: '主键',
+  options: {
+    interface: 'primaryKey',
+    name: 'id',
+    type: 'integer',
+    required: true,
+    autoIncrement: true,
+    primaryKey: true,
+    component: {
+      type: 'number',
+    },
+  },
+};
+
+export const sort = {
+  title: '排序',
+  options: {
+    interface: 'sort',
+    type: 'integer',
+    required: true,
+    component: {
+      type: 'sort',
+    },
+  },
+};
+
+export const password = {
+  title: '密码',
+  options: {
+    interface: 'password',
+    type: 'password',
+    component: {
+      type: 'password',
+    },
+  },
+};
