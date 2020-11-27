86d8be40e102a5a98f769d9e77047d5646703fbd
mytharcher <mytharcher@gmail.com>
2020-11-27 15:01:00 +0800

    fix: this declaration

--- packages/plugin-collections/src/server.ts
+++ packages/plugin-collections/src/server.ts
@@ -1,12 +1,11 @@
 import path from 'path';
-import Database, { ModelCtor } from '@nocobase/database';
-import Resourcer from '@nocobase/resourcer';
+import { Application } from '@nocobase/server';
 import CollectionModel from './models/collection';
 import FieldModel from './models/field';
 
-export default async function (options = {}) {
-  const database: Database = this.database;
-  const resourcer: Resourcer = this.resourcer;
+export default async function (this: Application, options = {}) {
+  const database = this.database;
+  const resourcer = this.resourcer;
 
   database.import({
     directory: path.resolve(__dirname, 'collections'),
