42f83eab19bdb773a54a691a6aef9cd10338e9ee
chenos <chenlinxh@gmail.com>
2020-12-05 09:27:24 +0800

    Test/ci (#31)
    * Create node.js.yml
    
    * fix: npm install command
    
    * fix: try to fix github action fail by use same react version
    
    * fix: try to fix github action fail by use same react version
    
    * fix: try to fix github action fail by use same react version
    
    * fix: try to fix github action fail by use same react version
    
    * fix: try to fix github action fail by build dependencies
    
    * fix: try to fix ci fail by types resolution
    
    * fix: try to fix ci fail by types resolution
    
    * fix: try to fix ci fail by types resolution
    
    * fix: try to fix ci fail by types resolution
    
    * fix: try to fix ci fail by types resolution
    
    * fix: try to fix ci fail by set env
    
    * fix: try to fix ci fail by pg version
    
    * fix: try to fix ci fail by fix types
    
    * fix(test): Type '""' is not assignable to type 'IndexType'.
    
    * fix(test): Type '""' is not assignable to type 'IndexType'
    
    Co-authored-by: Junyi <mytharcher@users.noreply.github.com>
    Co-authored-by: mytharcher <mytharcher@gmail.com>

--- packages/database/src/__tests__/tables.test.ts
+++ packages/database/src/__tests__/tables.test.ts
@@ -94,9 +94,10 @@ describe('tables', () => {
         ],
       });
       expect(db.getModel('baz').options.indexes).toStrictEqual(db.getTable('baz').getModelOptions().indexes);
-      expect(db.getTable('baz').getModelOptions().indexes).toStrictEqual([
-        { fields: [ 'col1' ], name: 'baz_col1', type: '', parser: null },
-        { fields: [ 'col2', 'col3' ], name: 'baz_col2_col3', type: '', parser: null }
+      // @ts-ignore
+      expect(db.getTable('baz').getModelOptions().indexes).toMatchObject([
+        { fields: [ 'col1' ], name: 'baz_col1', parser: null },
+        { fields: [ 'col2', 'col3' ], name: 'baz_col2_col3', parser: null }
       ]);
     });
 
@@ -127,9 +128,10 @@ describe('tables', () => {
         ],
       });
       expect(db.getModel('baz2').options.indexes).toStrictEqual(db.getTable('baz2').getModelOptions().indexes);
-      expect(db.getTable('baz2').getModelOptions().indexes).toStrictEqual([
-        { fields: [ 'col1' ], name: 'baz2_col1', type: '', parser: null },
-        { fields: [ 'col2', 'col3' ], name: 'baz2_col2_col3', type: '', parser: null },
+      // @ts-ignore
+      expect(db.getTable('baz2').getModelOptions().indexes).toMatchObject([
+        { fields: [ 'col1' ], name: 'baz2_col1', parser: null },
+        { fields: [ 'col2', 'col3' ], name: 'baz2_col2_col3', parser: null },
       ]);
     });
   });
