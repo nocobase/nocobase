d2d7cd3d01401c3dca611d78b98ffcdf38241fc7
chenos <chenlinxh@gmail.com>
2020-12-09 17:14:42 +0800

    fix: after reinitialization, hooks are lost

--- packages/database/src/table.ts
+++ packages/database/src/table.ts
@@ -220,7 +220,7 @@ export class Table {
       updatedAt: Utils.underscoredIf('updatedAt', underscored),
       indexes: Array.from(this.indexes.values()),
       // freezeTableName: true,
-      hooks: _.get(this.Model.options, 'hooks') || {},
+      hooks: _.get(this.getModel(), 'options.hooks') || {},
       ...this.modelOptions,
     };
   }
