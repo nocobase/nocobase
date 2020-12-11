9d528abb9bc484fb3679ffbb0688e7702bf7e6f8
chenos <chenlinxh@gmail.com>
2020-12-11 15:00:33 +0800

    fix: get pageTitle with first string field

--- packages/plugin-pages/src/actions/getPageInfo.ts
+++ packages/plugin-pages/src/actions/getPageInfo.ts
@@ -1,13 +1,20 @@
 import { ResourceOptions } from '@nocobase/resourcer';
 import { Model, ModelCtor } from '@nocobase/database';
-import { get } from 'lodash';
 
 export default async (ctx, next) => {
   const { resourceName, resourceKey } = ctx.action.params;
   const M = ctx.db.getModel(resourceName) as ModelCtor<Model>;
   const model = await M.findByPk(resourceKey);
+  const Field = ctx.db.getModel('fields') as ModelCtor<Model>;
+  const field = await Field.findOne({
+    where: {
+      collection_name: resourceName,
+      type: 'string',
+    },
+    order: [['sort', 'asc']],
+  });
   ctx.body = {
-    pageTitle: model.title,
+    pageTitle: field ? model.get(field.get('name')) : model.get(M.primaryKeyAttribute),
     ...model.toJSON(),
   };
   await next();
