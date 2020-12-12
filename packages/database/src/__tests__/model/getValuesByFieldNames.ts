5662509f4cd7e7831b3a6462a72beac3104d7e10
Junyi <mytharcher@users.noreply.github.com>
2020-12-12 16:38:08 +0800

    Feature/sort (#38)
    * refactor: change sort strategy from offset to targetId
    
    * fix: remove unnecessary query to optimize performance
    
    * refactor: change sort api to allow object
    
    * refactor: change function member positions
    
    * fix: test case names
    
    * fix: static to instance

--- packages/database/src/__tests__/model/getValuesByFieldNames.ts
+++ packages/database/src/__tests__/model/getValuesByFieldNames.ts
@@ -3,7 +3,7 @@ import Database from '../../database';
 
 
 
-describe('getScopeWhere', () => {
+describe('getValuesByFieldNames', () => {
   let db: Database;
 
   beforeEach(async () => {
@@ -25,14 +25,14 @@ describe('getScopeWhere', () => {
   it('exist column', async () => {
     const Post = db.getModel('posts');
     const post = await Post.create({ status: 'published' });
-    const where = post.getScopeWhere(['status']);
+    const where = post.getScopedValues(['status']);
     expect(where).toEqual({ status: 'published' });
   });
 
   it('non-exist column', async () => {
     const Post = db.getModel('posts');
     const post = await Post.create({});
-    const where = post.getScopeWhere(['whatever']);
+    const where = post.getScopedValues(['whatever']);
     expect(where).toEqual({});
   });
 });
