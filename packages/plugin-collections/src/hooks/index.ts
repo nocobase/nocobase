b918c0f7dfd74ee3313ad0d28da39f20c1509420
chenos <chenlinxh@gmail.com>
2020-12-21 09:34:34 +0800

    fix: add collections after update hook

--- packages/plugin-collections/src/hooks/index.ts
+++ packages/plugin-collections/src/hooks/index.ts
@@ -1,16 +1,18 @@
 import collectionsBeforeValidate from './collections-before-validate';
 import collectionsAfterCreate from './collections-after-create';
+import collectionsAfterUpdate from './collections-after-update';
 
 import fieldsBeforeValidate from './fields-before-validate';
 import fieldsAfterCreate from './fields-after-create';
-import generateName from './generateName';
 import fieldsAfterBulkUpdate from './fields-after-bulk-update';
+
+import generateName from './generateName';
 
 export default {
   collections: {
     beforeValidate: collectionsBeforeValidate,
     afterCreate: collectionsAfterCreate,
-    afterUpdate: collectionsAfterCreate,
+    afterUpdate: collectionsAfterUpdate,
   },
   fields: {
     beforeValidate: fieldsBeforeValidate,
