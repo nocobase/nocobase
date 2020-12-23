b55d8d1ac087ea7771bfa9d2a84397bc5d7573be
chenos <chenlinxh@gmail.com>
2020-12-23 20:29:11 +0800

    fix: improves collections load

--- packages/plugin-collections/src/hooks/fields-after-update.ts
+++ packages/plugin-collections/src/hooks/fields-after-update.ts
@@ -5,11 +5,4 @@ export default async function (model: FieldModel, options: any = {}) {
   if (migrate) {
     await model.migrate(options);
   }
-  // if (model.get('collection_name') && model.get('parent_id')) {
-  //   const parent = await model.getParent({
-  //     ...options,
-  //   });
-  //   const Collection = model.database.getModel('collections');
-  //   await Collection.load({...options, where: {name: parent.get('collection_name')}});
-  // }
 }
