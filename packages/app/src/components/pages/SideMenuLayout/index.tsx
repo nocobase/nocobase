ed8b5504ac6671a8834b92cf7f3a1e7cbc319c7a
chenos <chenlinxh@gmail.com>
2020-12-30 22:36:59 +0800

    feat(app): menu could be collapsed.

--- packages/app/src/components/pages/SideMenuLayout/index.tsx
+++ packages/app/src/components/pages/SideMenuLayout/index.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Layout, Breadcrumb } from 'antd';
 import { Link } from 'umi';
 import './style.less';
 import Menu from '@/components/menu';
+import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
+import { useLocalStorageState } from 'ahooks';
 
 export function SideMenuLayout(props: any) {
-  const { menu = [] } = props.page;
-  console.log(menu);
+  const { menu = [], id } = props.page;
+  // console.log(menu);
+  const [collapsed, setCollapsed] = useLocalStorageState(`nocobase-menu-collapsed-${id}`, false);
   return (
     <Layout style={{height: 'calc(100vh - 48px)'}}>
-      <Layout.Sider className={'nb-sider'} theme={'light'}>
+      <Layout.Sider className={`nb-sider${collapsed ? ' collapsed' : ''}`} theme={'light'}>
         <Menu items={menu} mode={'inline'}/>
+        <div onClick={() => {
+          setCollapsed(!collapsed);
+        }} className={'menu-toggle'}>
+          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            style: { fontSize: 16 },
+          })}
+        </div>
       </Layout.Sider>
       <Layout.Content>
         {props.children}
