2096ee84c4efa4764f0964341d7abe704aff938f
chenos <chenlinxh@gmail.com>
2021-01-07 23:42:31 +0800

    fix: password autocomplete=new-password

--- packages/app/src/components/form.fields/password/index.tsx
+++ packages/app/src/components/form.fields/password/index.tsx
@@ -18,7 +18,7 @@ export const Password = connect({
   return (
     <span className={className}>
       <Input.Password 
-        autoComplete={'off'}
+        autoComplete={'new-password'}
         {...others}
         value={value}
         onChange={(e) => {
