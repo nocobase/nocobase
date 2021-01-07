285cf34c6aff6365a4e1b3f451e116f50726a74b
chenos <chenlinxh@gmail.com>
2021-01-07 12:48:24 +0800

    fix: set to null when text field is empty

--- packages/app/src/components/form.fields/input/index.tsx
+++ packages/app/src/components/form.fields/input/index.tsx
@@ -6,7 +6,15 @@ import { acceptEnum, mapStyledProps, mapTextComponent } from '../shared'
 export const Input = connect<'TextArea'>({
   getProps: mapStyledProps,
   getComponent: mapTextComponent
-})(acceptEnum(AntdInput))
+})(acceptEnum(({onChange, ...restProps}) => (
+  <AntdInput
+    {...restProps}
+    onChange={(e) => {
+      // 文本字段，如果空要 null 处理
+      onChange(e.target.value ? e : null);
+    }}
+  />
+)))
 
 Input.TextArea = connect({
   getProps: mapStyledProps,
