f66d6ae323df3e6f4bfef0be657acbead0ea8e28
mytharcher <mytharcher@gmail.com>
2021-01-18 19:52:35 +0800

    refactor: can api

---
+++ packages/plugin-permissions/src/AccessController.ts
@@ -0,0 +1,301 @@
+import { Op } from 'sequelize';
+
+import { ROLE_TYPE_ANONYMOUS, ROLE_TYPE_ROOT, ROLE_TYPE_USER } from './constants';
+
+
+
+export type CollectionPermissions = {
+  actions: any[];
+  fields: any[];
+  tabs: any[]
+};
+
+export type PermissionParams = true | null | {
+  filter: any,
+  fields: any[]
+};
+
+// ctx.can = new AccessController(ctx).can;
+// ctx.can('collection').permissions()
+// => [{ name: 'list', scope: { id: 1, filter: {} } }]
+// ctx.can('collection').act('list').any()
+// => { filter: {}, fields: [] }
+// ctx.can('collection').act('update').any()
+// ctx.can('collection').act('update').one(resourceKey)
+// ctx.can('collection').act('get').one(resourceKey)
+
+export default class AccessController {
+  static getPermissions(roles) {
+    return roles.reduce((permissions, role) => permissions.concat(role.get('permissions')), []);
+  }
+
+  static getActionPermissions(permissions) {
+    const actionsMap = new Map<string, any>();
+
+    permissions.forEach(permission => {
+      permission.get('actions').forEach(action => {
+        // 如果没有同名 action
+        if (!actionsMap.has(action.name)) {
+          actionsMap.set(action.name, action);
+          return;
+        }
+
+        const existedScope = actionsMap.get(action.name).get('scope');
+        // 如果之前的同名 action 没有 scope 或 filter 为空
+        if (!existedScope || !existedScope.get('filter') || !Object.keys(existedScope.get('filter')).length) {
+          actionsMap.set(action.name, action);
+          return;
+        }
+  
+        const newScope = action.get('scope');
+        // 如果新 action 没有 scope 或 filter 为空
+        if (!newScope || !newScope.get('filter') || !Object.keys(newScope.get('filter')).length) {
+          return;
+        }
+  
+        // 以 or 关心合并两个 scope 中的 filter
+        existedScope.set('filter', { [Op.or]: [existedScope.get('filter'), newScope.get('filter')] });
+      });
+    });
+
+    return Array.from(actionsMap.values());
+  }
+  
+  static getFieldPermissions(permissions) {
+    const fieldsMap = new Map<string, any>();
+    permissions.forEach(permission => {
+      permission.get('fields_permissions').forEach(field => {
+        if (!fieldsMap.has(field.field.name)) {
+          fieldsMap.set(field.field.name, field);
+          return;
+        }
+  
+        const existedActions = fieldsMap.get(field.field.name).get('actions');
+        if (!existedActions || !existedActions.length) {
+          fieldsMap.set(field.field.name, field);
+          return;
+        }
+  
+        const newActions = field.get('actions');
+        if (!newActions || !newActions.length) {
+          return;
+        }
+  
+        const actions = new Set(existedActions);
+        newActions.forEach(item => actions.add(item));
+        fieldsMap.get(field.field.name).set('actions', Array.from(actions));
+      });
+    });
+
+    return Array.from(fieldsMap.values());
+  }
+
+  static getTabPemissions(permissions) {
+    const tabs = new Set();
+    permissions.forEach(permission => {
+      permission.get('tabs_permissions').forEach(tab => tabs.add(tab.id));
+    });
+
+    return Array.from(tabs);
+  }
+
+  context;
+
+  resourceName: string;
+  actionName: string;
+
+  constructor(ctx) {
+    this.context = ctx;
+  }
+
+  can = (resourceName) => {
+    this.resourceName = resourceName;
+    return this;
+  };
+
+  act(name) {
+    this.actionName = name;
+    return this;
+  }
+
+  async permissions(): Promise<CollectionPermissions> {
+    const roles = await this.getRolesWithPermissions();
+    if (roles.some(role => role.type === ROLE_TYPE_ROOT)) {
+      return this.getRootPermissions();
+    }
+
+    const permissions = AccessController.getPermissions(roles);
+    
+    return {
+      actions: AccessController.getActionPermissions(permissions),
+      fields: AccessController.getFieldPermissions(permissions),
+      tabs: AccessController.getTabPemissions(permissions)
+    };
+  }
+
+  async any(): Promise<PermissionParams> {
+    const roles = await this.getRolesWithPermissions();
+    if (roles.some(role => role.type === ROLE_TYPE_ROOT)) {
+      return true;
+    }
+    const permissions = AccessController.getPermissions(roles);
+    const actionPermissions = AccessController.getActionPermissions(permissions);
+
+    if (!actionPermissions.length) {
+      // 如果找不到可用的 action 记录
+      // 则认为没有权限
+      return null;
+    }
+    
+    const filters = actionPermissions
+      .filter(item => Boolean(item.scope) && Object.keys(item.scope.filter).length)
+      .map(item => item.scope.filter);
+
+    const fields = AccessController.getFieldPermissions(permissions);
+
+    return {
+      filter: filters.length
+        ? filters.length > 1 ? { or: filters } : filters[0]
+        : {},
+      fields
+    };
+  }
+  
+  async one(resourceKey): Promise<PermissionParams> {
+    const any = await this.any();
+
+    if (!any || any === true) {
+      return any;
+    }
+
+    const { filter } = any;
+
+    const Collection = this.context.db.getModel(this.resourceName);
+    const existed = await Collection.count({
+      where: {
+        ...filter,
+        [Collection.primaryKeyAttribute]: resourceKey
+      }
+    });
+
+    return existed ? any : null;
+  }
+
+  async getRolesWithPermissions() {
+    const { context, resourceName, actionName = null } = this;
+    if (!resourceName) {
+      throw new Error('resource name must be set first by can(resourceName)');
+    }
+    const Role = context.db.getModel('roles');
+    const permissionInclusion = {
+      association: 'permissions',
+      where: {
+        collection_name: resourceName
+      },
+      required: true,
+      include: [
+        {
+          association: 'actions',
+          where: actionName ? {
+            name: `${resourceName}:${actionName}`
+          } : {},
+          required: true,
+          // 对 hasMany 关系可以进行拆分查询，避免联表过多标识符超过 PG 的 64 字符限制
+          separate: true,
+          include: [
+            {
+              association: 'scope'
+            }
+          ]
+        },
+        {
+          association: 'fields_permissions',
+          include: [
+            {
+              association: 'field'
+            }
+          ],
+          separate: true,
+        },
+        {
+          association: 'tabs_permissions',
+          separate: true,
+        }
+      ],
+    };
+    
+    let userRoles = [];
+    // 获取登入用户的角色及权限
+    const { currentUser } = context.state;
+    if (currentUser) {
+      const rootRoles = await currentUser.getRoles({
+        where: {
+          type: ROLE_TYPE_ROOT
+        }
+      });
+      if (rootRoles.length) {
+        return rootRoles;
+      }
+
+      userRoles = await currentUser.getRoles({
+        where: {
+          type: ROLE_TYPE_USER
+        },
+        include: [
+          permissionInclusion
+        ]
+      });
+    }
+
+    // 获取匿名用户的角色及权限
+    const anonymousRoles = await Role.findAll({
+      where: {
+        type: ROLE_TYPE_ANONYMOUS
+      },
+      include: [
+        permissionInclusion
+      ]
+    });
+
+    return [...anonymousRoles, ...userRoles];
+  }
+
+  async getRootPermissions(): Promise<CollectionPermissions> {
+    const { context, resourceName = null } = this;
+    const Action = context.db.getModel('actions');
+    const actions = await Action.findAll({
+      where: {
+        collection_name: { [Op.or]: [resourceName, null] }
+      }
+    });
+
+    const Field = context.db.getModel('fields');
+    const fields = await Field.findAll({
+      where: {
+        collection_name: resourceName
+      }
+    });
+
+    const Tab = context.db.getModel('tabs');
+    const tabs = await Tab.findAll({
+      where: {
+        collection_name: resourceName
+      },
+      attribute: [Tab.primaryKeyAttribute]
+    });
+
+    const actionsNames = actions.map(action => ({
+      name: `${resourceName}:${actions.name}`
+    }));
+
+    return {
+      actions: actionsNames,
+      fields: fields.map(field => ({
+        field,
+        field_id: field.id,
+        actions: actionsNames.map(({ name }) => name)
+      })),
+      tabs: tabs.map(tab => tab[Tab.primaryKeyAttribute])
+    }
+  }
+}
--- packages/plugin-permissions/src/server.ts
+++ packages/plugin-permissions/src/server.ts
@@ -4,7 +4,7 @@ import { Application } from '@nocobase/server';
 import { Operator } from '@nocobase/database';
 import * as collectionsRolesActions from './actions/collections.roles';
 import * as rolesCollectionsActions from './actions/roles.collections';
-import { ROLE_TYPE_ROOT, ROLE_TYPE_USER, ROLE_TYPE_ANONYMOUS } from './constants';
+import AccessController from './AccessController';
 
 // API
 // const permissions = ctx.app.getPluginInstance('permissions');
@@ -13,16 +13,6 @@ import { ROLE_TYPE_ROOT, ROLE_TYPE_USER, ROLE_TYPE_ANONYMOUS } from './constants
 
 
 export class Permissions {
-  static getActionPermissions(roles) {
-    const permissions = roles.reduce((permissions, role) => permissions.concat(role.get('permissions')), []);
-    return permissions.reduce((actions, permission) => actions.concat(permission.get('actions')), []);
-  }
-  
-  static getFieldPermissions(roles) {
-    const permissions = roles.reduce((permissions, role) => permissions.concat(role.get('permissions')), []);
-    return permissions.reduce((fields, permission) => fields.concat(permission.get('fields_permissions')), []);
-  }
-
   readonly app: Application;
   readonly options: any;
 
@@ -47,33 +37,29 @@ export class Permissions {
     // 针对“自己创建的” scope 添加特殊的操作符以生成查询条件
     if (!Operator.has('$currentUser')) {
       Operator.register('$currentUser', (value, { ctx }) => {
-        const user = this.getCurrentUser(ctx);
+        const user = ctx.state.currentUser;
         return { [Op.eq]: user[user.constructor.primaryKeyAttribute] };
       });
     }
 
-    resourcer.use(this.injection.bind(this));
-    resourcer.use(this.interceptor.bind(this));
+    resourcer.use(this.injection);
+    resourcer.use(this.middleware);
   }
 
-  async injection(ctx, next) {
-    const permissions = this;
-
-    ctx.can = (function(key: string, options = {}) {
-      return permissions.can(key, { ...options, context: this });
-    }).bind(ctx);
+  injection = async (ctx, next) => {
+    ctx.can = new AccessController(ctx).can;
 
     return next();
-  }
+  };
 
-  async interceptor(ctx, next) {
+  middleware = async (ctx, next) => {
     const {
       resourceName,
       actionName
     } = ctx.action.params;
 
-    const result = await this.can(`${resourceName}:${actionName}`, { context: ctx });
-    if (result === false) {
+    const result = await ctx.can(resourceName).act(actionName).any();
+    if (!result) {
       return this.reject(ctx);
     }
 
@@ -81,160 +67,13 @@ export class Permissions {
       return next();
     }
 
-    ctx.action.mergeParams(result);
+    ctx.action.mergeParams({
+      filter: result.filter,
+      fields: result.fields.map(item => item.get('field').get('name'))
+    });
 
     return next();
-  }
-
-  async can(key: string, { context, ...options }) {
-    const matcher = key.match(/^(?:([^\.]+)\.)?([^:]+)(?::([\w]+))$/);
-    if (!matcher) {
-      throw new Error('input key does not match pattern');
-    }
-    const [matched, associatedName, resourceName, actionName] = matcher;
-
-    if (!actionName) {
-      // TODO: 只获取相关数据
-      return {};
-    }
-
-    if (!['list', 'get', 'create', 'update', 'destroy'].includes(actionName)) {
-      // 不是常规 action 的，直接返回 true 以跳过。
-      return true;
-    }
-
-    if (associatedName) {
-      // TODO(feature): 关联数据的处理
-      return true;
-    }
-
-    const roles = await this.getRolesWithPermissions({
-      // ...options,
-      // associatedName,
-      resourceName,
-      actionName,
-      currentUser: this.getCurrentUser(context)
-    });
-
-    if (roles.some(role => role.type === ROLE_TYPE_ROOT)) {
-      // 如果是系统管理员，则不进行其他验证或过滤
-      return true;
-    }
-
-    const actionPermissions = Permissions.getActionPermissions(roles);
-    // 如果找不到可用的 action 记录
-    if (!actionPermissions.length) {
-      // 则认为没有权限
-      return false;
-    }
-    
-    const filters = actionPermissions
-      .filter(item => Boolean(item.scope) && Object.keys(item.scope.filter).length)
-      .map(item => item.scope.filter);
-
-    const fields = new Set();
-    const fieldPermissions = Permissions.getFieldPermissions(roles);
-    fieldPermissions.forEach(item => {
-      const actions = item.get('actions');
-      if (actions && actions.includes(`${resourceName}:${actionName}`)) {
-        fields.add(item.get('field').get('name'));
-      }
-    });
-
-    return {
-      ...(filters.length
-        ? { filter: filters.length > 1 ? { or: filters } : filters[0] }
-        : {}),
-      fields: Array.from(fields)
-    };
-  }
-
-  getCurrentUser(ctx) {
-    // TODO: 获取当前用户应调用当前依赖用户插件的相关方法
-    return ctx.state.currentUser;
-  }
-
-  async getRolesWithPermissions({
-    resourceName,
-    // resourceKey,
-    // associated,
-    // associatedName,
-    // associatedKey,
-    actionName,
-    currentUser
-  }) {
-    // TODO: 还未定义关联数据的权限如何表达
-
-    const Role = this.app.database.getModel('roles');
-    const permissionInclusion = {
-      association: 'permissions',
-      where: {
-        collection_name: resourceName
-      },
-      required: true,
-      include: [
-        {
-          association: 'actions',
-          where: {
-            name: `${resourceName}:${actionName}`
-          },
-          required: true,
-          // 对 hasMany 关系可以进行拆分查询，避免联表过多标识符超过 PG 的 64 字符限制
-          separate: true,
-          include: [
-            {
-              association: 'scope',
-              attribute: ['filter']
-            }
-          ]
-        },
-        {
-          association: 'fields_permissions',
-          include: [
-            {
-              association: 'field',
-              attributes: ['name']
-            }
-          ],
-          separate: true,
-        }
-      ],
-    };
-    
-    let userRoles = [];
-    // 获取登入用户的角色及权限
-    if (currentUser) {
-      const rootRoles = await currentUser.getRoles({
-        where: {
-          type: ROLE_TYPE_ROOT
-        }
-      });
-      if (rootRoles.length) {
-        return rootRoles;
-      }
-  
-      userRoles = await currentUser.getRoles({
-        where: {
-          type: ROLE_TYPE_USER
-        },
-        include: [
-          permissionInclusion
-        ]
-      });
-    }
-  
-    // 获取匿名用户的角色及权限
-    const anonymousRoles = await Role.findAll({
-      where: {
-        type: ROLE_TYPE_ANONYMOUS
-      },
-      include: [
-        permissionInclusion
-      ]
-    });
-  
-    return [...anonymousRoles, ...userRoles];
-  }
+  };
   
   reject(ctx) {
     ctx.throw(404);
