613c4a14adcb071ff3e10873aa106ffd210ecfaa
chenos <chenlinxh@gmail.com>
2021-01-26 22:14:48 +0800

    style: update form description style

--- packages/app/src/components/form.fields/form-description/index.tsx
+++ packages/app/src/components/form.fields/form-description/index.tsx
@@ -11,7 +11,7 @@ export const FormDescription = createVirtualBox(
       <Card size={'small'} headStyle={{padding: 0}} bodyStyle={{
         padding: 0,
       }} className={className} {...props}>
-        <div dangerouslySetInnerHTML={{__html: children && markdown(children)}}></div>
+        {typeof children === 'string' && children && <div dangerouslySetInnerHTML={{__html: markdown(children)}}></div>}
       </Card>
     )
   })`
@@ -19,6 +19,13 @@ export const FormDescription = createVirtualBox(
     &.ant-card {
       border: none;
       box-shadow: none;
+      .ant-card-head {
+        font-size: 16px;
+        background: #fafafa;
+        margin: 0 -24px;
+        padding: 4px 24px !important;
+        border-top: 1px solid #f0f0f0;
+      }
       p:first-child {
         margin-top: 14px;
       }
