2cd55861e2e0243ebc19f2abe8b985d0b4451d2b
chenos <chenlinxh@gmail.com>
2021-03-11 11:13:50 +0800

    fix: icon

--- packages/app/src/components/pages/AdminLoader/menu/index.tsx
+++ packages/app/src/components/pages/AdminLoader/menu/index.tsx
@@ -56,13 +56,13 @@ export default (props: any) => {
       // const subItems = children.filter(child => child.showInMenu);
       if (!hideChildren && children.length) {
         return (
-          <Menu.SubMenu key={`${item.name}`} icon={<Icon type={item.icon}/>} title={<>{item.title}</>}>
+          <Menu.SubMenu key={`${item.name}`} icon={item.icon && <Icon type={item.icon}/>} title={<>{item.title}</>}>
             {renderChildren(children)}
           </Menu.SubMenu>
         )
       }
       return (
-        <Menu.Item icon={<Icon type={item.icon}/>} key={`${item.name}`}>
+        <Menu.Item icon={item.icon && <Icon type={item.icon}/>} key={`${item.name}`}>
           <Link to={item.path}>{item.title}</Link>
         </Menu.Item>
       )
