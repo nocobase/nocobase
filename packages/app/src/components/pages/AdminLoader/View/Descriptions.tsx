ad88d6ff86eae4202c412de732348e9e6e6be99b
chenos <chenlinxh@gmail.com>
2021-03-12 10:55:19 +0800

    详情分组显示

--- packages/app/src/components/pages/AdminLoader/View/Descriptions.tsx
+++ packages/app/src/components/pages/AdminLoader/View/Descriptions.tsx
@@ -14,10 +14,45 @@ import get from 'lodash/get';
 import Drawer from '@/components/pages/AdminLoader/Drawer';
 import Field from '@/components/views/Field';
 import { Form } from './Form';
+import { configResponsive, useResponsive } from 'ahooks';
+import { InfoCircleOutlined } from '@ant-design/icons';
+
+configResponsive({
+  small: 0,
+  middle: 800,
+  large: 1200,
+});
+function toGroups(fields: any[]) {
+  const groups = [];
+  let group = {
+    title: undefined,
+    tooltip: undefined,
+    children: [],
+  };
+  fields.forEach(field => {
+    if (field.interface === 'description') {
+      if (group.children.length) {
+        groups.push(group);
+      }
+      group = {
+        title: field.title,
+        tooltip: field.tooltip,
+        children: [],
+      };
+    } else {
+      group.children.push(field);
+    }
+  });
+  if (group.children.length) {
+    groups.push(group);
+  }
+  return groups;
+}
 
 export function Descriptions(props) {
   const { data: record = {}, schema = {}, onDataChange } = props;
   const { rowKey = 'id', resourceName, fields = [], actions = [], appends = [], associationField = {} } = schema;
+  const responsive = useResponsive();
 
   const resourceKey = props.resourceKey || record[associationField.targetKey||rowKey];
   const associatedKey = props.associatedKey || record[associationField.sourceKey||'id'];
@@ -32,6 +67,16 @@ export function Descriptions(props) {
   if (loading) {
     return <Spin/>;
   }
+  let descriptionsProps: any = {
+    size: 'middle',
+    bordered: true,
+  }
+  if (responsive.small && !responsive.middle && !responsive.large) {
+    descriptionsProps = {
+      layout: 'vertical'
+    }
+  }
+  const groups = toGroups(fields);
   return (
     <div>
       <Actions 
@@ -46,19 +91,23 @@ export function Descriptions(props) {
         actions={actions}
         style={{ marginBottom: 14 }}
       />
-      <AntdDescriptions 
-        // layout={'vertical'}
-        size={'middle'}
-        bordered 
-        column={1}>
-        {fields.map(field => {
-          return (
-            <AntdDescriptions.Item label={field.title||field.name}>
-              <Field data={field} viewType={'descriptions'} schema={field} value={get(data, field.name)}/>
-            </AntdDescriptions.Item>
-          )
-        })}
-      </AntdDescriptions>
+      {groups.map(group => (
+        <AntdDescriptions 
+          // layout={'vertical'}
+          // size={'middle'}
+          // bordered 
+          {...descriptionsProps}
+          title={group.title && <span>{group.title} {group.tooltip && <Tooltip title={group.tooltip}><InfoCircleOutlined /></Tooltip>}</span>}
+          column={1}>
+          {group.children.map((field: any) => {
+            return (
+              <AntdDescriptions.Item label={field.title||field.name}>
+                <Field data={field} viewType={'descriptions'} schema={field} value={get(data, field.name)}/>
+              </AntdDescriptions.Item>
+            )
+          })}
+        </AntdDescriptions>
+      ))}
     </div>
   );
 }
