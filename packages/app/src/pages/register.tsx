e56d9b8573e255650439ad0be25fdcb6e3deadec
chenos <chenlinxh@gmail.com>
2021-03-17 09:53:25 +0800

    fix: document title of the login page

--- packages/app/src/pages/register.tsx
+++ packages/app/src/pages/register.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Link } from 'umi';
 import { Page } from '@/components/pages';
 import ViewFactory from '@/components/views';
-import { Helmet } from 'umi';
+import { Helmet, useModel } from 'umi';
+import get from 'lodash/get';
 
 export default (props: any) => {
+  const { initialState = {}, refresh, setInitialState } = useModel('@@initialState');
+  const siteTitle = get(initialState, 'systemSettings.title');
   return (
     <div>
       <Helmet>
-        <title>{'注册 NocoBase'}</title>
+        <title>{siteTitle ? `注册 - ${siteTitle}` : '注册'}</title>
       </Helmet>
       <ViewFactory 
         {...props}
