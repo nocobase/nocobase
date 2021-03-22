fecfdbf359b2c756f4e425aff15b66093df951f8
chenos <chenlinxh@gmail.com>
2021-03-22 14:12:11 +0800

    fix: move page templates to pages/index

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -1,4 +1,13 @@
-// import PageLoader from '@nocobase/plugin-pages/lib/components/index';
 import PageLoader from '@/components/pages';
+
+export const templates = {
+  TopMenuLayout: require('@/components/pages/TopMenuLayout').default,
+  SideMenuLayout: require('@/components/pages/SideMenuLayout').default,
+  AdminLoader: require('@/components/pages/AdminLoader').default,
+  login: require('@/pages/login').default,
+  register: require('@/pages/register').default,
+  lostpassword: require('@/pages/lostpassword').default,
+  resetpassword: require('@/pages/resetpassword').default,
+};
 
 export default PageLoader;
