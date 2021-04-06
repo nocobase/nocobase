c808c5b6027c3138293e0f705a9ae20e01b078c9
chenos <chenlinxh@gmail.com>
2021-04-06 20:05:56 +0800

    feat: serve dist files for development

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,5 +1,7 @@
 import api from './app';
-import actions from '../../actions/src';
+import actions from '@nocobase/actions';
+import send from 'koa-send';
+import serve from 'koa-static';
 
 api.resourcer.use(async (ctx: actions.Context, next) => {
   const { actionName, resourceField, resourceName, fields = {} } = ctx.action.params;
@@ -28,27 +30,6 @@ api.resourcer.use(async (ctx: actions.Context, next) => {
   }
   await next();
 });
-
-// api.resourcer.use(async (ctx: actions.Context, next) => {
-//   const { resourceField, resourceName, viewName, filter } = ctx.action.params;
-//   // TODO: 需要补充默认视图的情况
-//   let view: any;
-//   if (viewName) {
-//     view = await ctx.db.getModel('views').findOne({
-//       where: {
-//         collection_name: resourceField ? resourceField.options.target : resourceName,
-//         name: viewName,
-//       }
-//     });
-//     const viewFilter = view.get('filter');
-//     if (viewFilter) {
-//       const args = [viewFilter, filter].filter(Boolean);
-//       ctx.action.setParam('filter', {and: args});
-//       console.log(ctx.action.params.filter);
-//     }
-//   }
-//   await next();
-// });
 
 (async () => {
   await api.loadPlugins();
@@ -81,6 +62,17 @@ api.resourcer.use(async (ctx: actions.Context, next) => {
     name: 'users',
   }});
   await api.database.getModel('automations').load();
+
+  api.use(async (ctx, next) => {
+    if (!process.env.APP_DIST || !process.env.USE_STATIC_SERVER || process.env.USE_STATIC_SERVER === 'false') {
+      return next();
+    }
+    await serve(process.env.APP_DIST)(ctx, next);
+    if (ctx.status == 404) {
+      return send(ctx, 'index.html', { root: process.env.APP_DIST });
+    }
+  });
+
   api.listen(process.env.API_PORT, () => {
     console.log(`http://localhost:${process.env.API_PORT}/`);
   });
