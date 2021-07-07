ed4397e112b939eeedafe96ce95db39be1bcfad1
chenos <chenlinxh@gmail.com>
2021-07-07 22:50:40 +0800

    updates...

--- packages/client/src/blocks/table/index.tsx
+++ packages/client/src/blocks/table/index.tsx
@@ -5,49 +5,54 @@ import {
   observer,
   RecursionField,
   useField,
+  useForm,
+  FormProvider,
+  createSchemaField,
 } from '@formily/react';
-import { Button, Pagination, Space, Table as AntdTable } from 'antd';
-import { DesignableBar } from './DesignableBar';
-import { ColumnDesignableBar } from './ColumnDesignableBar';
-import { uid } from '@formily/shared';
-import useRequest from '@ahooksjs/use-request';
+import {
+  Button,
+  Pagination,
+  PaginationProps,
+  Space,
+  Spin,
+  Table as AntdTable,
+} from 'antd';
+import { findIndex } from 'lodash';
 import constate from 'constate';
-import { SchemaRenderer } from '../';
+import useRequest from '@ahooksjs/use-request';
+import { BaseResult } from '@ahooksjs/use-request/lib/types';
+import { uid, clone } from '@formily/shared';
+import { MenuOutlined } from '@ant-design/icons';
 import { useVisibleContext } from '../action';
 
-function useTableColumns(props?: any) {
-  const schema = useFieldSchema();
-  const { dataSource } = props || {};
+interface TableRowProps {
+  index: number;
+  data: any;
+}
 
-  function findColumns(schema: Schema): Schema[] {
+const TableRowContext = createContext<TableRowProps>(null);
+
+function usePaginationProps() {
+  const schema = useFieldSchema();
+
+  function findPagination(schema: Schema): Schema[] {
     return schema.reduceProperties((columns, current) => {
-      if (current['x-component'] === 'Table.Column') {
+      if (current['x-component'] === 'Table.Pagination') {
         return [...columns, current];
       }
-      return [...columns, ...findColumns(current)];
+      return [...columns, ...findPagination(current)];
     }, []);
   }
 
-  return findColumns(schema).map((item) => {
-    const columnProps = item['x-component-props'] || {};
-    return {
-      title: item.title,
-      dataIndex: item.name,
-      ...columnProps,
-      render(value, record, index) {
-        // console.log({ item });
-        // const index = dataSource.indexOf(record);
-        item.mapProperties((s) => {
-          s['title'] = undefined;
-          s['x-read-pretty'] = true;
-          return s;
-        });
-        return (
-          <RecursionField schema={item} name={index} onlyRenderProperties />
-        );
-      },
-    };
-  });
+  const pagination = findPagination(schema).shift();
+
+  if (pagination) {
+    // console.log({ pagination });
+    const props = pagination['x-component-props'] || {};
+    return { defaultCurrent: 1, defaultPageSize: 10, ...props };
+  }
+
+  return false;
 }
 
 function useTableActionBars() {
@@ -79,82 +84,177 @@ function useTableActionBars() {
   return findActionBars(schema);
 }
 
-function useTable() {
-  const field = useField<Formily.Core.Models.ArrayField>();
-  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
-  const response = useRequest<any>(
-    () => {
-      return new Promise((resolve) => {
-        console.log('useRequest');
-        setTimeout(() => {
-          resolve([
-            { key: uid(), field1: uid(), field2: uid() },
-            { key: uid(), field1: uid(), field2: uid() },
-          ]);
-        }, 500);
-      });
-    },
-    {
-      onSuccess(data) {
-        field.setValue(data);
+function useTableColumns(props?: any) {
+  const schema = useFieldSchema();
+  const { dataSource } = props || {};
+
+  function findColumns(schema: Schema): Schema[] {
+    return schema.reduceProperties((columns, current) => {
+      if (current['x-component'] === 'Table.Column') {
+        return [...columns, current];
+      }
+      return [...columns, ...findColumns(current)];
+    }, []);
+  }
+
+  return findColumns(schema).map((item) => {
+    const columnProps = item['x-component-props'] || {};
+    return {
+      title: item.title,
+      dataIndex: item.name,
+      ...columnProps,
+      render(value, record, recordIndex) {
+        const index = dataSource.indexOf(record);
+        return (
+          <TableRowContext.Provider
+            value={{
+              index: index,
+              data: record,
+            }}
+          >
+            <RecursionField schema={item} name={index} onlyRenderProperties />
+          </TableRowContext.Provider>
+        );
       },
-    },
-  );
-  return {
-    selectedRowKeys,
-    setSelectedRowKeys,
-    mutate: response.mutate,
-    response,
-    async create() {
-      response.refresh()
-    },
-    async update() {
-      response.refresh()
-    },
-    async destroy() {
-      response.refresh()
-    },
-  } as any;
+    };
+  });
 }
 
-const [TableContextProvider, useTableContext] = constate(useTable);
+export function useTableRow() {
+  const ctx = useContext(TableRowContext);
+  console.log('useTableRow', ctx.data);
+  return ctx.data;
+}
 
-export function useTableCreateAction() {
-  const { visible, setVisible } = useVisibleContext()
-  const { create } = useTableContext();
-  return {
-    run: async () => {
-      setVisible(false);
-      await create();
-      console.log({ visible })
-    },
+export function useTableIndex() {
+  const { params: { page, pageSize } } = useTableContext();
+  const ctx = useContext(TableRowContext);
+  const field = useField();
+  if (!field.componentProps.isRemoteDataSource) {
+    return ctx.index;
   }
+  return pageSize ? ctx.index + (page - 1) * pageSize : ctx.index;
 }
 
 export function useTableDestroyAction() {
-  // const { setVisible } = useVisibleContext()
-  const { destroy } = useTableContext();
+  const ctx = useContext(TableRowContext);
+  const { field, selectedRowKeys, setSelectedRowKeys, refresh } = useTableContext();
+  const rowKey = field.componentProps.rowKey || 'id';
+
   return {
-    run: async () => {
-      // setVisible(false);
-      await destroy();
-    },
+    async run() {
+      if (ctx && typeof ctx.index !== 'undefined') {
+        field.remove(ctx.index);
+        refresh();
+        return;
+      }
+    
+      if (!selectedRowKeys.length) {
+        return;
+      }
+      while (selectedRowKeys.length) {
+        const key = selectedRowKeys.shift();
+        const index = findIndex(
+          field.value,
+          (item) => item[rowKey] === key,
+        );
+        field.remove(index);
+      }
+      refresh();
+      setSelectedRowKeys([]);
+    }
+  }
+
+}
+
+export function useTableFilterAction() {
+  const { field, refresh, params } = useTableContext();
+  const { setVisible } = useVisibleContext();
+  const form = useForm();
+  return {
+    async run() {
+      setVisible && setVisible(false);
+      refresh();
+    }
   }
 }
 
-const ArrayTable = (props) => {
+export function useTableCreateAction() {
+  const { field, run: exec, params } = useTableContext();
+  const { setVisible } = useVisibleContext();
+  const form = useForm();
+  return {
+    async run() {
+      setVisible && setVisible(false);
+      field.unshift(clone(form.values));
+      form.reset();
+      exec({...params, page: 1});
+    }
+  }
+}
+
+export function useTableUpdateAction() {
+  const { field, refresh, params } = useTableContext();
+  const { setVisible } = useVisibleContext();
+  const form = useForm();
+  return {
+    async run() {
+      setVisible && setVisible(false);
+      refresh();
+    }
+  }
+}
+
+const [TableContextProvider, useTableContext] = constate(() => {
   const field = useField<Formily.Core.Models.ArrayField>();
-  // const dataSource = Array.isArray(field.value) ? field.value.slice() : [];
-  const columns = useTableColumns();
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const pagination = usePaginationProps();
+  const response = useRequest<{
+    list: any[];
+    total: number;
+  }>((params = {}) => {
+    return new Promise((resolve) => {
+      const dataSource = Array.isArray(field.value) ? field.value.slice() : [];
+      if (pagination) {
+        const {
+          page = pagination.defaultCurrent,
+          pageSize = pagination.defaultPageSize,
+        } = params;
+        const startIndex = (page - 1) * pageSize;
+        const endIndex = startIndex + pageSize - 1;
+        resolve({
+          list: dataSource.slice(startIndex, endIndex + 1),
+          total: dataSource.length,
+        });
+      } else {
+        resolve({
+          list: dataSource,
+          total: dataSource.length,
+        });
+      }
+    });
+  });
+  const params = {
+    page: pagination.defaultCurrent,
+    pageSize: pagination.defaultPageSize,
+    ...(response.params[0] || {}),
+  };
+  return { ...response, params, field, selectedRowKeys, setSelectedRowKeys };
+});
+
+export { TableContextProvider, useTableContext };
+
+const TableContainer = observer((props) => {
+  const field = useField<Formily.Core.Models.ArrayField>();
+  const schema = useFieldSchema();
   const actionBars = useTableActionBars();
-  const {
-    selectedRowKeys,
-    setSelectedRowKeys,
-    response = {},
-    create,
-  } = useTableContext();
-  console.log({ response });
-  const { data = [], loading, mutate } = response as any;
+  const { loading, data, refresh, selectedRowKeys, setSelectedRowKeys } =
+    useTableContext();
+  const rowKey = field.componentProps.rowKey || 'id';
+  console.log({ actionBars });
+  const dataSource = Array.isArray(field.value) ? field.value.slice() : []
+  const columns = useTableColumns({ dataSource });
+
   return (
     <div>
       {actionBars.top.map((actionBarSchema) => {
@@ -172,32 +272,30 @@ const ArrayTable = (props) => {
           />
         );
       })}
-      <Button
-        onClick={() => {
-          create();
-          // field.unshift({ key: uid(), field1: uid(), field2: uid() });
-          // const dataSource = Array.isArray(field.value)
-          //   ? field.value.slice()
-          //   : [];
-          // mutate(dataSource);
-          // response.refresh();
-          // console.log({ selectedRowKeys })
-        }}
-      >
-        新增
-      </Button>
       <AntdTable
-        rowKey={'key'}
-        loading={loading}
         pagination={false}
-        columns={columns}
-        dataSource={data}
+        rowKey={rowKey}
+        loading={loading}
         rowSelection={{
           type: 'checkbox',
           selectedRowKeys,
           onChange: (keys) => {
+            console.log(keys);
             setSelectedRowKeys(keys);
           },
+        }}
+        dataSource={data?.list}
+        columns={columns}
+        onRow={(data) => {
+          const index = dataSource.indexOf(data);
+          return {
+            onClick(e) {
+              field.query(`.${schema.name}.${index}.action2`).take((f) => {
+                const setVisible = f.componentProps.setVisible;
+                setVisible && setVisible(true);
+              });
+            },
+          };
         }}
       />
       {actionBars.bottom.map((actionBarSchema) => {
@@ -217,12 +315,12 @@ const ArrayTable = (props) => {
       })}
     </div>
   );
-};
+});
 
 export const Table: any = observer((props) => {
   return (
     <TableContextProvider>
-      <ArrayTable {...props} />
+      <TableContainer />
     </TableContextProvider>
   );
 });
@@ -238,41 +336,29 @@ Table.ActionBar = observer((props) => {
 });
 
 Table.Pagination = observer((props) => {
-  const { response } = useTableContext();
-  return (
+  const { data, params, run } = useTableContext();
+  return data?.total > params?.pageSize && (
     <Pagination
       {...props}
+      defaultCurrent={1}
+      defaultPageSize={5}
+      current={params?.page}
+      pageSize={params?.pageSize}
+      total={data?.total}
       onChange={(page, pageSize) => {
-        response.refresh();
-        // console.log('useRequest', { page, pageSize });
+        run({ page, pageSize });
       }}
     />
   );
 });
 
-Table.View = observer(() => {
-  const schema = useFieldSchema();
-  console.log('Table.View', schema);
-  return (
-    <SchemaRenderer
-      schema={{
-        type: 'object',
-        properties: {
-          [schema.name]: {
-            ...schema.toJSON(),
-            type: 'array',
-            'x-component': 'Table.Field',
-          },
-        },
-      }}
-    />
-  );
+Table.SortHandle = observer((props) => {
+  const field = useField<Formily.Core.Models.Field>();
+  console.log('SortHandle', field.value);
+  return <MenuOutlined />;
 });
 
-Table.Field = observer((props) => {
-  return (
-    <TableContextProvider>
-      <ArrayTable {...props} />
-    </TableContextProvider>
-  );
+Table.Index = observer((props) => {
+  const index = useTableIndex();
+  return <div>#{index + 1}</div>;
 });
