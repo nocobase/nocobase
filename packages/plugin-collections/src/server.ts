91ecd9807159f2402cd49c33ea6be4b845912fd8
chenos <chenlinxh@gmail.com>
2021-07-23 21:28:57 +0800

    bugfix

--- packages/plugin-collections/src/server.ts
+++ packages/plugin-collections/src/server.ts
@@ -3,6 +3,7 @@ import { Application } from '@nocobase/server';
 import { registerModels, Table } from '@nocobase/database';
 import * as models from './models';
 import { createOrUpdate, findAll } from './actions';
+import { create } from './actions/fields';
 
 export default async function (this: Application, options = {}) {
   const database = this.database;
@@ -15,6 +16,7 @@ export default async function (this: Application, options = {}) {
       model.set('name', model.get('key'));
     }
   });
+  this.resourcer.registerActionHandler('collections.fields:create', create);
   this.resourcer.registerActionHandler('collections:findAll', findAll);
   this.resourcer.registerActionHandler('collections:createOrUpdate', createOrUpdate);
 }
