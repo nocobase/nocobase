cfd12a0fcf291865a9f9c8128f47672a400ec16e
chenos <chenlinxh@gmail.com>
2021-08-13 23:33:15 +0800

    feat: siteinfo, more configuration, userinfo...

---
+++ packages/client/src/components/admin-layout/UserInfo.tsx
@@ -0,0 +1,29 @@
+import React, { useContext, useEffect } from 'react';
+import { Button, Dropdown, Menu } from 'antd';
+import { useHistory } from 'react-router-dom';
+import { request } from '../../schemas';
+
+export const UserInfo = () => {
+  const history = useHistory();
+  return (
+    <Dropdown
+      overlay={
+        <Menu>
+          <Menu.Item>个人资料</Menu.Item>
+          <Menu.Divider />
+          <Menu.Item
+            onClick={async () => {
+              await request('/users:logout');
+              localStorage.removeItem('NOCOBASE_TOKEN');
+              history.push('/login');
+            }}
+          >
+            退出登录
+          </Menu.Item>
+        </Menu>
+      }
+    >
+      <Button type={'text'} className={'user-info'}>超级管理员</Button>
+    </Dropdown>
+  );
+};
