f8365c7881feb83c7269b95c8e3256831db8297b
chenos <chenlinxh@gmail.com>
2021-08-18 11:29:57 +0800

    feat: add Action.Group

--- packages/client/src/schemas/action/index.tsx
+++ packages/client/src/schemas/action/index.tsx
@@ -44,7 +44,7 @@ import { VisibleContext } from '../../context';
 import { DndContext, DragOverlay } from '@dnd-kit/core';
 import { createPortal } from 'react-dom';
 import { ActionBar } from './ActionBar';
-import { DragHandle } from '../../components/Sortable';
+import { DragHandle, SortableItem } from '../../components/Sortable';
 import { useDisplayedMapContext } from '../../constate';
 
 export const ButtonComponentContext = createContext(null);
@@ -93,6 +93,71 @@ Action.URL = observer((props: any) => {
     <a target={'_blank'} {...props}>
       {schema.title}
     </a>
+  );
+});
+
+Action.Group = observer((props: any) => {
+  const { type = 'button' } = props;
+  const { root, schema, insertAfter, remove } = useDesignable();
+  const moveToAfter = (path1, path2) => {
+    if (!path1 || !path2) {
+      return;
+    }
+    if (path1.join('.') === path2.join('.')) {
+      return;
+    }
+    const data = findPropertyByPath(root, path1);
+    if (!data) {
+      return;
+    }
+    remove(path1);
+    return insertAfter(data.toJSON(), path2);
+  };
+  const [dragOverlayContent, setDragOverlayContent] = useState('');
+  return (
+    <DndContext
+      onDragStart={(event) => {
+        console.log({ event });
+        setDragOverlayContent(event.active.data?.current?.title || '');
+      }}
+      onDragEnd={async (event) => {
+        const path1 = event.active?.data?.current?.path;
+        const path2 = event.over?.data?.current?.path;
+        const data = moveToAfter(path1, path2);
+        await updateSchema(data);
+      }}
+    >
+      {createPortal(
+        <DragOverlay
+          zIndex={2222}
+          style={{ pointerEvents: 'none', whiteSpace: 'nowrap' }}
+          dropAnimation={{
+            duration: 10,
+            easing: 'cubic-bezier(0.18, 0.67, 0.6, 1.22)',
+          }}
+        >
+          {dragOverlayContent}
+        </DragOverlay>,
+        document.body,
+      )}
+      <div className={cls(`ant-btn-group-${type}`)}>
+        <Space size={16}>
+          {schema.mapProperties((s) => {
+            return (
+              <SortableItem
+                id={s.name}
+                data={{
+                  title: s.title,
+                  path: getSchemaPath(s),
+                }}
+              >
+                <RecursionField name={s.name} schema={s} />
+              </SortableItem>
+            );
+          })}
+        </Space>
+      </div>
+    </DndContext>
   );
 });
 
