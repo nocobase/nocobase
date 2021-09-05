4886a8a3a0afec089e0a45d4e46434a05298bb21
chenos <chenlinxh@gmail.com>
2021-09-05 14:17:45 +0800

    feat: async event emitter

--- packages/server/src/application.ts
+++ packages/server/src/application.ts
@@ -23,6 +23,60 @@ export class Application extends Koa {
     // this.runHook('afterInit');
   }
 
+  async emitAsync(event: string | symbol, ...args: any[]): Promise<boolean> {
+    // @ts-ignore
+    const events = this._events;
+    let callbacks = events[event];
+    if (!callbacks) {
+      return false;
+    }
+    // helper function to reuse as much code as possible
+    const run = (cb) => {
+      switch (args.length) {
+        // fast cases
+        case 0:
+          cb = cb.call(this);
+          break;
+        case 1:
+          cb = cb.call(this, args[0]);
+          break;
+        case 2:
+          cb = cb.call(this, args[0], args[1]);
+          break;
+        case 3:
+          cb = cb.call(this, args[0], args[1], args[2]);
+          break;
+        // slower
+        default:
+          cb = cb.apply(this, args);
+      }
+
+      if (
+        cb && (
+          cb instanceof Promise ||
+          typeof cb.then === 'function'
+        )
+      ) {
+        return cb;
+      }
+
+      return Promise.resolve(true);
+    };
+
+    if (typeof callbacks === 'function') {
+      await run(callbacks);
+    } else if (typeof callbacks === 'object') {
+      callbacks = callbacks.slice().filter(Boolean);
+      await callbacks.reduce((prev, next) => {
+        return prev.then((res) => {
+          return run(next).then((result) => Promise.resolve(res.concat(result)));
+        });
+      }, Promise.resolve([]));
+    }
+
+    return true;
+  }
+
   registerPlugin(key: string | object, plugin?: any) {
     if (typeof key === 'object') {
       Object.keys(key).forEach((k) => {
@@ -46,10 +100,18 @@ export class Application extends Koa {
   }
 
   async loadPlugins() {
+    await this.emitAsync('plugins.beforeLoad');
     const allPlugins = this.plugins.values();
     for (const plugin of allPlugins) {
       plugin.instance = await this.loadPlugin(plugin);
     }
+    await this.emitAsync('plugins.afterLoad');
+  }
+
+  async start(port) {
+    await this.loadPlugins();
+    await this.emitAsync('server.beforeStart');
+    this.listen(port);
   }
 
   protected async loadPlugin({ entry, options = {} }: { entry: string | Function, options: any }) {
