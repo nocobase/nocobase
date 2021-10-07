b5e24fa09136a143d9498931cf2511fb471aa1ae
chenos <chenlinxh@gmail.com>
2021-10-07 16:31:19 +0800

    feat: initialize demo data

---
+++ packages/plugin-client/src/db/part1/04-routes.sql
@@ -0,0 +1,7 @@
+TRUNCATE "routes" RESTART IDENTITY CASCADE;
+INSERT INTO "routes" ("key", "type", "options", "sort", "created_at", "updated_at", "parent_key", "ui_schema_key") VALUES
+('r_94b8nz6evyh',	'redirect',	'{"from":"/","to":"/admin","exact":true}',	1,	'2021-09-02 15:07:57.162+00',	'2021-09-02 15:07:57.162+00',	NULL,	NULL),
+('r_w1sa2lfk44v',	'route',	'{"path":"/admin/:name(.+)?","component":"AdminLayout","title":"后台"}',	2,	'2021-09-02 15:07:57.17+00',	'2021-09-02 15:07:57.185+00',	NULL,	'qqzzjakwkwl'),
+('r_nt33b3m6ptk',	'route',	'{"component":"AuthLayout"}',	3,	'2021-09-02 15:07:57.19+00',	'2021-09-02 15:07:57.19+00',	NULL,	NULL),
+('r_5l9yp15wjk9',	'route',	'{"path":"/login","component":"RouteSchemaRenderer","title":"登录"}',	1,	'2021-09-02 15:07:57.196+00',	'2021-09-02 15:07:57.252+00',	'r_nt33b3m6ptk',	'dtf9j0b8p9u'),
+('r_9gqm4d8wpuw',	'route',	'{"path":"/register","component":"RouteSchemaRenderer","title":"注册"}',	2,	'2021-09-02 15:07:57.256+00',	'2021-09-02 15:07:57.343+00',	'r_nt33b3m6ptk',	'46qlxqam3xk');
--- packages/plugin-client/src/server.ts
+++ packages/plugin-client/src/server.ts
@@ -2,6 +2,24 @@ import path from 'path';
 import send from 'koa-send';
 import serve from 'koa-static';
 import { PluginOptions } from '@nocobase/server';
+import { readFileSync } from 'fs';
+import glob from 'glob';
+
+function getInitSqls() {
+  const part1 = [];
+  const part2 = [];
+  const files1 = glob.sync(path.resolve(__dirname, './db/part1/*.sql'));
+  for (const file of files1) {
+    const sql = readFileSync(file).toString();
+    part1.push(sql);
+  }
+  const files2 = glob.sync(path.resolve(__dirname, './db/part2/*.sql'));
+  for (const file of files2) {
+    const sql = readFileSync(file).toString();
+    part2.push(sql);
+  }
+  return { part1, part2 };
+}
 
 export default {
   name: 'client',
@@ -18,6 +36,32 @@ export default {
       console.log('koa-send', root, ctx.status);
       if (ctx.status == 404) {
         return send(ctx, 'index.html', { root });
+      }
+    });
+    const app = this.app;
+    this.app.on('db.init', async () => {
+      const transaction = await app.db.sequelize.transaction();
+      const sqls = getInitSqls();
+      try {
+        for (const sql of sqls.part1) {
+          await app.db.sequelize.query(sql, { transaction });
+        }
+        await transaction.commit();
+      } catch (error) {
+        await transaction.rollback();
+      }
+      await app.db.getModel('collections').load({
+        skipExisting: true,
+      });
+      await app.db.sync();
+      const transaction2 = await app.db.sequelize.transaction();
+      try {
+        for (const sql of sqls.part2) {
+          await app.db.sequelize.query(sql, { transaction: transaction2 });
+        }
+        await transaction2.commit();
+      } catch (error) {
+        await transaction2.rollback();
       }
     });
   }
