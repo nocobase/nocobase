23f4ec2681ecd642bb9b235a1fbb2da90e549412
chenos <chenlinxh@gmail.com>
2021-10-07 17:24:31 +0800

    feat: improve nocobase app template

--- packages/create-nocobase-app/templates/AppGenerator/.umirc.ts
+++ packages/create-nocobase-app/templates/AppGenerator/.umirc.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 dotenv.config({
-  path: path.resolve(__dirname, '../../.env'),
+  path: path.resolve(__dirname, './.env'),
 });
 
 export default defineConfig({
