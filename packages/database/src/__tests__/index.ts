224f00f29394bf05880206ac634e9658673d62bf
chenos <chenlinxh@gmail.com>
2021-10-14 20:22:42 +0800

    once...

--- packages/database/src/__tests__/index.ts
+++ packages/database/src/__tests__/index.ts
@@ -14,24 +14,25 @@ function getTestKey() {
   return key;
 }
 
-export function getDatabase() {
-  let config: any = {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: Number.parseInt(process.env.DB_PORT, 10),
+let config: any = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: Number.parseInt(process.env.DB_PORT, 10),
+  dialect: process.env.DB_DIALECT as Dialect,
+  logging: process.env.DB_LOG_SQL === 'on' ? console.log : false,
+};
+
+if (process.env.DB_DIALECT === 'sqlite') {
+  config = {
     dialect: process.env.DB_DIALECT as Dialect,
+    storage: path.resolve(__dirname, `./.db/${uid()}.sqlite`),
     logging: process.env.DB_LOG_SQL === 'on' ? console.log : false,
   };
+}
 
-  if (process.env.DB_DIALECT === 'sqlite') {
-    config = {
-      dialect: process.env.DB_DIALECT as Dialect,
-      storage: path.resolve(__dirname, `./.db/${uid()}.sqlite`),
-      logging: process.env.DB_LOG_SQL === 'on' ? console.log : false,
-    };
-  }
+export function getDatabase() {
 
   return new Database({
     ...config,
