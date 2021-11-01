0d70f55f022ddae0b23bcfe52e733df0d2b76d05
chenos <chenlinxh@gmail.com>
2021-11-01 17:25:29 +0800

    fix(create-nocobase-app): process.env.API_URL='/api/'

--- packages/create-nocobase-app/templates/AppGenerator/.umirc.ts
+++ packages/create-nocobase-app/templates/AppGenerator/.umirc.ts
@@ -8,15 +8,13 @@ dotenv.config({
 
 process.env.MFSU_AD = 'none';
 
-const baseUrl = `http://localhost:${process.env.API_PORT || '13001'}/`;
-
 export default defineConfig({
   favicon: '/favicon.png',
   nodeModulesTransform: {
     type: 'none',
   },
   define: {
-    'process.env.API_URL': process.env.API_URL || `${baseUrl}api/`,
+    'process.env.API_URL': process.env.API_URL || `/api/`,
     'process.env.API_PORT': process.env.API_PORT || '13001',
   },
   proxy: {
