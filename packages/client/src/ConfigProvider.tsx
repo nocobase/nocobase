22d0f143624b9725a1f86505545cc073e26f40ba
chenos <chenlinxh@gmail.com>
2021-11-01 14:54:53 +0800

    add AntdConfigProvider

--- packages/client/src/ConfigProvider.tsx
+++ packages/client/src/ConfigProvider.tsx
@@ -1,21 +1,27 @@
 import React, { createContext } from 'react';
-import { I18nextProvider } from 'react-i18next';
-import { i18n } from '.';
-// import { ConfigProvider as AntdConfigProvider } from 'antd';
-// import enUS from 'antd/lib/locale/en_US';
-// import zhCN from 'antd/lib/locale/zh_CN';
+import { I18nextProvider, useTranslation } from 'react-i18next';
+import { i18n } from './i18n';
+import { ConfigProvider as AntdConfigProvider } from 'antd';
+import enUS from 'antd/lib/locale/en_US';
+import zhCN from 'antd/lib/locale/zh_CN';
 import { ClientProvider } from './constate';
-// import moment from 'moment';
-// import 'moment/locale/zh-cn';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
 // moment.locale('zh-cn');
 
-const ConfigContext = createContext<any>(null);
+function AntdProvider(props) {
+  const { i18n } = useTranslation();
+  console.log('i18n.language', i18n.language);
+  return <AntdConfigProvider locale={i18n.language === 'zh-CN' ? zhCN : enUS}>{props.children}</AntdConfigProvider>;
+}
 
 export function ConfigProvider(props: any) {
   const { client } = props;
   return (
     <ClientProvider client={client}>
-      <I18nextProvider i18n={i18n}>{props.children}</I18nextProvider>
+      <I18nextProvider i18n={i18n}>
+        <AntdProvider>{props.children}</AntdProvider>
+      </I18nextProvider>
     </ClientProvider>
   );
 }
