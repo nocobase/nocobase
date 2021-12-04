17799d6c50ad57b43cc0824cc97ab4183cafd200
Chareice <chareice@live.com>
2021-12-04 20:40:23 +0800

    date operators

--- packages/database-next/src/operators/date.ts
+++ packages/database-next/src/operators/date.ts
@@ -1,9 +1,46 @@
 import { Op } from 'sequelize';
+import moment, { MomentInput } from 'moment';
+function stringToDate(value: string): Date {
+  return moment(value).toDate();
+}
+
+function getNextDay(value: MomentInput): Date {
+  return moment(value).add(1, 'd').toDate();
+}
 
 export default {
-  $dateOn: (value) => {
+  $dateOn(value) {
     return {
-      [Op.eq]: value,
+      [Op.and]: [
+        { [Op.gte]: stringToDate(value) },
+        { [Op.lt]: getNextDay(value) },
+      ],
     };
+  },
+
+  $dateNotOn(value) {
+    return {
+      [Op.or]: [
+        { [Op.lt]: stringToDate(value) },
+        { [Op.gte]: getNextDay(value) },
+      ],
+    };
+  },
+
+  $dateBefore(value) {
+    return { [Op.lt]: stringToDate(value) };
+  },
+  $dateNotBefore(value) {
+    return {
+      [Op.gte]: stringToDate(value),
+    };
+  },
+
+  $dateAfter(value) {
+    return { [Op.gte]: getNextDay(value) };
+  },
+
+  $dateNotAfter(value) {
+    return { [Op.lt]: getNextDay(value) };
   },
 };
