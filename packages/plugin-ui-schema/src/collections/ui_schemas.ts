f89c0b48e3e26728faad25de2aa162b2e8b59217
chenos <chenlinxh@gmail.com>
2021-12-13 15:13:41 +0800

    v0.6

---
+++ packages/plugin-ui-schema/src/collections/ui_schemas.ts
@@ -0,0 +1,57 @@
+import { CollectionOptions } from '@nocobase/database';
+
+export default {
+  name: 'ui_schemas',
+  title: '字段配置',
+  model: 'UISchema',
+  sortable: {
+    type: 'sort',
+    name: 'sort',
+    scope: ['parentKey'],
+  },
+  fields: [
+    {
+      type: 'uid',
+      name: 'key',
+      primaryKey: true,
+    },
+    // {
+    //   type: 'sort',
+    //   name: 'sort',
+    //   scope: ['parentKey'],
+    // },
+    {
+      type: 'string',
+      name: 'name',
+    },
+    {
+      type: 'string',
+      name: 'title',
+    },
+    {
+      type: 'string',
+      name: 'type',
+    },
+    {
+      type: 'string',
+      name: 'x-component',
+    },
+    {
+      type: 'json',
+      name: 'options',
+      defaultValue: {},
+    },
+    {
+      type: 'boolean',
+      name: 'async',
+      defaultValue: false,
+    },
+    {
+      type: 'hasMany',
+      name: 'children',
+      target: 'ui_schemas',
+      sourceKey: 'key',
+      foreignKey: 'parentKey',
+    },
+  ],
+} as CollectionOptions;
