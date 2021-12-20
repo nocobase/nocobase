504cc1ef2bfef4d3f35a94d3c6afe433fe281c1b
chenos <chenlinxh@gmail.com>
2021-12-20 23:14:31 +0800

    feat: improve code

--- packages/client/src/components/schema-component/index.tsx
+++ packages/client/src/components/schema-component/index.tsx
@@ -1,37 +1,83 @@
-import React, { createContext, useContext, useMemo } from 'react';
-import { createSchemaField, FormProvider } from '@formily/react';
+import React, { createContext, useContext, useMemo, useState } from 'react';
+import { uid } from '@formily/shared';
 import { createForm } from '@formily/core';
+import {
+  createSchemaField,
+  FormProvider,
+  RecursionField,
+  Schema,
+  SchemaExpressionScopeContext,
+  SchemaOptionsContext,
+} from '@formily/react';
 
-const SchemaComponentContext = createContext<any>({});
+export const SchemaComponentContext = createContext<any>({});
 
 export function SchemaComponentProvider(props) {
   const { components, scope, children } = props;
-  const form = useMemo(() => props.form || createForm(), []);
+  const [, setUid] = useState(uid());
+  const form = props.form || useMemo(() => createForm(), []);
+  const SchemaField = useMemo(
+    () =>
+      createSchemaField({
+        scope,
+        components,
+      }),
+    [],
+  );
   return (
-    <SchemaComponentContext.Provider value={{ components, scope }}>
+    <SchemaComponentContext.Provider
+      value={{
+        SchemaField,
+        components,
+        scope,
+        refresh: () => setUid(uid()),
+      }}
+    >
       <FormProvider form={form}>{children}</FormProvider>
     </SchemaComponentContext.Provider>
   );
 }
 
+export function RecursionComponent(props) {
+  const { components, scope } = useContext(SchemaComponentContext);
+  return (
+    <SchemaOptionsContext.Provider
+      value={{
+        components: { ...props.components, ...components },
+        scope: { ...props.scope, ...scope },
+      }}
+    >
+      <SchemaExpressionScopeContext.Provider value={{ scope }}>
+        <RecursionField {...props} />
+      </SchemaExpressionScopeContext.Provider>
+    </SchemaOptionsContext.Provider>
+  );
+}
+
 function toSchema(schema) {
+  if (Schema.isSchemaInstance(schema)) {
+    return schema;
+  }
   if (schema.name) {
-    return {
+    return new Schema({
       type: 'object',
       properties: {
         [schema.name]: schema,
       },
-    };
+    });
   }
-  return schema;
+  return new Schema(schema);
+}
+
+// TODO
+export function useDesignable() {
+  const { refresh } = useContext(SchemaComponentContext);
+  return { refresh };
 }
 
 export function SchemaComponent(props) {
-  const { schema, ...others } = props;
-  const { components, scope } = useContext(SchemaComponentContext);
-  const SchemaField = createSchemaField({
-    components,
-    scope,
-  });
-  return <SchemaField {...others} schema={toSchema(schema)} />;
+  const { schema: defaultSchema, ...others } = props;
+  const { SchemaField } = useContext(SchemaComponentContext);
+  const schema = useMemo(() => toSchema(defaultSchema), []);
+  return <SchemaField {...others} schema={schema} />;
 }
