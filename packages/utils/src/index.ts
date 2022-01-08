4bb4886ddba85e82373f0ffac9cad1cd3a4b1e58
chenos <chenlinxh@gmail.com>
2022-01-08 18:02:42 +0800

    feat: add uid

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,12 +1,4 @@
-import deepmerge from 'deepmerge';
-
-const overwriteMerge = (destinationArray, sourceArray, options) => sourceArray;
-
-export function merge(obj1: any, obj2: any) {
-  return deepmerge(obj1, obj2, {
-    arrayMerge: overwriteMerge,
-  });
-}
-
+export * from './merge';
 export * from './mixin';
 export * from './mixin/AsyncEmitter';
+export * from './uid';
