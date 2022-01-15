1cc69bea9345ca702482430dfb08293c3a28143d
chenos <chenlinxh@gmail.com>
2022-01-15 13:45:16 +0800

    delete index.tsx

--- packages/client/src/schema-component/antd/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from './checkbox';
-export * from './action';
-export * from './form';
-export * from './form-item';
-export * from './block-item';
-export * from './dnd-context';
-export * from './grid';
