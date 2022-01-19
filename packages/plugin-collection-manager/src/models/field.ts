73f61ee4d6f9e58cd356e84b363bf775058d97ce
chenos <chenlinxh@gmail.com>
2022-01-19 10:02:52 +0800

    Feat/plugin collection manager (#147)
    * refactor: collection manager plugin
    
    * feat(database): magic attribute model
    
    * MagicAttributeModel
    
    * load collections & fields options
    
    * collections filterTargetKey

---
+++ packages/plugin-collection-manager/src/models/field.ts
@@ -0,0 +1,32 @@
+import { SyncOptions } from 'sequelize';
+import Database, { MagicAttributeModel } from '@nocobase/database';
+
+interface LoadOptions {
+  // TODO
+  skipExist?: boolean;
+}
+
+export class FieldModel extends MagicAttributeModel {
+  get db(): Database {
+    return (<any>this.constructor).database;
+  }
+
+  async load(loadOptions?: LoadOptions) {
+    const { skipExist } = loadOptions;
+    const collectionName = this.get('collectionName');
+    if (!this.db.hasCollection(collectionName)) {
+      throw new Error(`${collectionName} collection does not exist.`);
+    }
+    const collection = this.db.getCollection(collectionName);
+    const name = this.get('name');
+    if (skipExist && collection.hasField(name)) {
+      return collection.getField(name);
+    }
+    return collection.setField(name, this.get());
+  }
+
+  async migrate(options?: SyncOptions) {
+    const field = await this.load();
+    await field.sync(options);
+  }
+}
