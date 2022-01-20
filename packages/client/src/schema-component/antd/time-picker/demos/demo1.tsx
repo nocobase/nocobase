b0b79b7d9d7a8b832058dad9e173990df95122d6
SemmyWong <67748948+semmywong@users.noreply.github.com>
2022-01-20 20:33:46 +0800

    migrate TimePicker component into schema components (#164)
    * feat: add TimePicker component into schema components
    
    * improve
    
    * TimePicker.RangePicker
    
    Co-authored-by: chenos <chenlinxh@gmail.com>

---
+++ packages/client/src/schema-component/antd/time-picker/demos/demo1.tsx
@@ -0,0 +1,41 @@
+/**
+ * title: TimePicker
+ */
+import { FormItem } from '@formily/antd';
+import { SchemaComponent, SchemaComponentProvider, TimePicker } from '@nocobase/client';
+import React from 'react';
+
+const schema = {
+  type: 'object',
+  properties: {
+    input: {
+      type: 'string',
+      title: `Editable`,
+      'x-decorator': 'FormItem',
+      'x-component': 'TimePicker',
+      'x-reactions': {
+        target: 'read',
+        fulfill: {
+          state: {
+            value: '{{$self.value}}',
+          },
+        },
+      },
+    },
+    read: {
+      type: 'string',
+      title: `Read pretty`,
+      'x-read-pretty': true,
+      'x-decorator': 'FormItem',
+      'x-component': 'TimePicker',
+    },
+  },
+};
+
+export default () => {
+  return (
+    <SchemaComponentProvider components={{ TimePicker, FormItem }}>
+      <SchemaComponent schema={schema} />
+    </SchemaComponentProvider>
+  );
+};
