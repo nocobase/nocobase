490231d1d22efe072f6b2b68810b89049ff528ee
semmy <semmywong@126.com>
2022-01-22 17:09:08 +0800

    feat: migrate TreeSelect into schema components

---
+++ packages/client/src/schema-component/antd/tree-select/TreeSelect.tsx
@@ -0,0 +1,23 @@
+import { LoadingOutlined } from '@ant-design/icons';
+import { connect, mapProps, mapReadPretty } from '@formily/react';
+import { TreeSelect as AntdTreeSelect } from 'antd';
+import React from 'react';
+import { ReadPretty } from './ReadPretty';
+
+export const TreeSelect = connect(
+  AntdTreeSelect,
+  mapProps(
+    {
+      dataSource: 'treeData',
+    },
+    (props, field) => {
+      return {
+        ...props,
+        suffixIcon: field?.['loading'] || field?.['validating'] ? <LoadingOutlined /> : props.suffixIcon,
+      };
+    },
+  ),
+  mapReadPretty(ReadPretty),
+);
+
+export default TreeSelect;
