609300c1361fe66881ec308f3a12866890dd34be
chenos <chenlinxh@gmail.com>
2022-01-25 00:11:21 +0800

    void table

--- packages/client/src/schema-component/antd/void-table/VoidTable.tsx
+++ packages/client/src/schema-component/antd/void-table/VoidTable.tsx
@@ -1,41 +1,68 @@
-import { createForm } from '@formily/core';
+import { ArrayField, createForm } from '@formily/core';
 import { FieldContext, FormContext, observer, SchemaContext, useField, useFieldSchema } from '@formily/react';
+import { useRequest } from '../../../api-client';
 import { TableProps } from 'antd';
-import React, { useMemo } from 'react';
+import React, { createContext, useMemo } from 'react';
 import { useAttach } from '../../hooks';
 import { ArrayTable } from '../array-table';
 
-type VoidTableType = React.FC<TableProps<any>> & {
+type VoidTableType = React.FC<TableProps<any> & { request?: any }> & {
   Column?: React.FC<any>;
   mixin?: (T: any) => void;
 };
 
-export const VoidTable: VoidTableType = observer((props) => {
+interface ArrayFieldProviderProps {
+  request?: any;
+  initialValue?: any;
+}
+
+const RequestContext = createContext(null);
+
+const ArrayFieldProvider: React.FC<ArrayFieldProviderProps> = (props) => {
+  const { request, initialValue, children } = props;
   const field = useField();
   const fieldSchema = useFieldSchema();
   const form = useMemo(
     () =>
       createForm({
         initialValues: {
-          [fieldSchema.name]: props.dataSource || [],
+          [fieldSchema.name]: initialValue || [],
         },
       }),
     [],
   );
   const f = useAttach(form.createArrayField({ ...field.props, basePath: '' }));
+  const result = useRequest(request, {
+    onSuccess(data) {
+      console.log(data?.data);
+      form.setValues({
+        [fieldSchema.name]: data?.data,
+      });
+    },
+  });
   return (
-    <div>
+    <RequestContext.Provider value={result}>
       <FormContext.Provider value={form}>
         <SchemaContext.Provider value={fieldSchema}>
-          <FieldContext.Provider value={f}>
-            <ArrayTable {...props} />
-          </FieldContext.Provider>
+          <FieldContext.Provider value={f}>{children}</FieldContext.Provider>
         </SchemaContext.Provider>
       </FormContext.Provider>
-    </div>
+    </RequestContext.Provider>
+  );
+};
+
+export const VoidTable: VoidTableType = observer((props) => {
+  return (
+    <ArrayFieldProvider request={props.request} initialValue={props.dataSource}>
+      <RequestContext.Consumer>
+        {(result) => {
+          return <ArrayTable {...props} loading={result.loading} />;
+        }}
+      </RequestContext.Consumer>
+    </ArrayFieldProvider>
   );
 });
 
 VoidTable.mixin = ArrayTable.mixin;
 
-ArrayTable.mixin(VoidTable)
+ArrayTable.mixin(VoidTable);
--- packages/client/src/schema-component/antd/void-table/demos/demo1.tsx
+++ packages/client/src/schema-component/antd/void-table/demos/demo1.tsx
@@ -3,8 +3,18 @@
  */
 import { FormItem } from '@formily/antd';
 import { ISchema } from '@formily/react';
-import { Input, SchemaComponent, SchemaComponentProvider, VoidTable } from '@nocobase/client';
+import {
+  APIClient,
+  APIClientProvider,
+  Input,
+  SchemaComponent,
+  SchemaComponentProvider,
+  VoidTable,
+} from '@nocobase/client';
+import MockAdapter from 'axios-mock-adapter';
 import React from 'react';
+import { uid } from '@formily/shared';
+import _ from 'lodash';
 
 const schema: ISchema = {
   type: 'object',
@@ -18,6 +28,14 @@ const schema: ISchema = {
         rowKey: 'id',
         rowSelection: {
           type: 'checkbox',
+        },
+        request: {
+          resource: 'posts',
+          action: 'list',
+          params: {
+            filter: {},
+            fields: [],
+          },
         },
         dataSource: [
           { id: 1, name: 'Name1' },
@@ -43,10 +61,39 @@ const schema: ISchema = {
   },
 };
 
+const api = new APIClient();
+
+const mock = new MockAdapter(api.axios);
+
+const sleep = (value: number) => new Promise((resolve) => setTimeout(resolve, value));
+
+mock.onGet('/posts:list').reply(async (config) => {
+  // const [{ pageSize }] = config.params;
+  const pageSize = config.params.pageSize || 10;
+  console.log(config.params);
+  await sleep(1000);
+  return [
+    200,
+    {
+      data: _.range(pageSize).map((v) => {
+        return {
+          id: v,
+          name: `tttt${uid()}`,
+        };
+      }),
+      meta: {
+        count: 100,
+      },
+    },
+  ];
+});
+
 export default () => {
   return (
-    <SchemaComponentProvider components={{ Input, VoidTable, FormItem }}>
-      <SchemaComponent schema={schema} />
-    </SchemaComponentProvider>
+    <APIClientProvider apiClient={api}>
+      <SchemaComponentProvider components={{ Input, VoidTable, FormItem }}>
+        <SchemaComponent schema={schema} />
+      </SchemaComponentProvider>
+    </APIClientProvider>
   );
 };
