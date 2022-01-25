28e432b5b30185e760b0984501dc2131072a2a22
mytharcher <mytharcher@gmail.com>
2022-01-26 02:27:23 +0800

    feat(plugin-workflow): execution life cycle with branch and join

--- packages/plugin-workflow/src/constants.ts
+++ packages/plugin-workflow/src/constants.ts
@@ -1,11 +1,20 @@
 export const EXECUTION_STATUS = {
   STARTED: 0,
   RESOLVED: 1,
-  REJECTED: -1
+  REJECTED: -1,
+  CANCELLED: -2
 };
 
 export const JOB_STATUS = {
   PENDING: 0,
   RESOLVED: 1,
-  REJECTED: -1
+  REJECTED: -1,
+  CANCELLED: -2
+};
+
+export const LINK_TYPE = {
+  DEFAULT: null,
+  ON_TRUE: 1,
+  ON_FALSE: 0,
+  NO_LIMIT: -1
 };
