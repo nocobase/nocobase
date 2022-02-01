2a576b27cc5c4f9d8a0e3c8f7411823212fe7505
mytharcher <mytharcher@gmail.com>
2022-02-01 13:57:23 +0800

    fix(root): fix dependencies in packages to avoid building errors

--- packages/plugin-action-logs/src/server.ts
+++ packages/plugin-action-logs/src/server.ts
@@ -1,9 +1,9 @@
 import path from 'path';
-import { IPlugin } from '@nocobase/server';
+import { Plugin } from '@nocobase/server';
 import { afterCreate, afterUpdate, afterDestroy } from './hooks';
 
-export default {
-  name: 'action-logs',
+export default class extends Plugin {
+  name: 'action-logs';
   async load() {
     const database = this.app.db;
     await database.import({
@@ -12,5 +12,5 @@ export default {
     database.on('afterCreate', afterCreate);
     database.on('afterUpdate', afterUpdate);
     database.on('afterDestroy', afterDestroy);
-  },
-} as IPlugin;
+  }
+};
