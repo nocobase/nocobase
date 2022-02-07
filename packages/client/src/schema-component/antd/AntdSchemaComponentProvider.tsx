4e777c942d2872d09267155f974a3ae0da7c55cc
semmy <semmywong@126.com>
2022-02-07 11:23:43 +0800

    refactor: rename sort-item

--- packages/client/src/schema-component/antd/AntdSchemaComponentProvider.tsx
+++ packages/client/src/schema-component/antd/AntdSchemaComponentProvider.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as components from '.';
-import { SchemaComponentOptions } from '../components/SchemaComponentOptions';
+import { SchemaComponentOptions } from '../core/SchemaComponentOptions';
 
 export const AntdSchemaComponentProvider = (props) => {
   const { children } = props;
