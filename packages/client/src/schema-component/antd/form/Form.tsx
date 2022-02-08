75bf8b59515d40d32784252bd0bf55c8c9d2ed31
chenos <chenlinxh@gmail.com>
2022-02-08 13:43:40 +0800

    fix(client): improve form schema component

--- packages/client/src/schema-component/antd/form/Form.tsx
+++ packages/client/src/schema-component/antd/form/Form.tsx
@@ -1,56 +1,22 @@
 import { FormLayout } from '@formily/antd';
-import { createForm, FormPath } from '@formily/core';
-import {
-  FieldContext,
-  FormContext,
-  observer,
-  SchemaContext,
-  SchemaOptionsContext,
-  useField,
-  useFieldSchema
-} from '@formily/react';
-import { toJS } from '@formily/reactive';
+import { createForm } from '@formily/core';
+import { FieldContext, FormContext, observer, useField, useFieldSchema } from '@formily/react';
 import { Spin } from 'antd';
-import React, { useContext, useMemo } from 'react';
-import { SchemaComponent, useAttach } from '../..';
+import React, { useMemo } from 'react';
+import { useAttach } from '../..';
 import { useRequest } from '../../../api-client';
 
-type ComposedForm = React.FC<any> & {
-  __NOCOBASE_FORM?: boolean;
-};
-
-const useFormDecorator = () => {
-  const field = useField();
-  const options = useContext(SchemaOptionsContext);
-  const decorator = field.decoratorType ? FormPath.getIn(options?.components, field.decoratorType) : null;
-  return decorator?.__NOCOBASE_FORM ? decorator : null;
-};
-
-const FormDecorator: React.FC<any> = (props) => {
-  const { form } = props;
-  const options = useContext(SchemaOptionsContext);
-  const field = useField();
-  const fieldSchema = useFieldSchema();
-  const f = useAttach(form.createVoidField({ ...field.props, basePath: '' }));
-  const finalComponent = FormPath.getIn(options?.components, field.componentType) ?? field.componentType;
-  return (
-    <FormContext.Provider value={form}>
-      <SchemaContext.Provider value={fieldSchema}>
-        <FieldContext.Provider value={f}>
-          {React.createElement(finalComponent, toJS(field.componentProps))}
-        </FieldContext.Provider>
-      </SchemaContext.Provider>
-    </FormContext.Provider>
-  );
-};
-
 const FormComponent: React.FC<any> = (props) => {
-  const { form } = props;
-  const fieldSchema = useFieldSchema();
+  const { form, children, ...others } = props;
+  const field = useField();
+  // TODO: component 里 useField 会与当前 field 存在偏差
+  const f = useAttach(form.createVoidField({ ...field.props, basePath: '' }));
   return (
     <FieldContext.Provider value={undefined}>
       <FormContext.Provider value={form}>
-        <SchemaComponent schema={fieldSchema} />
+        <FormLayout layout={'vertical'} {...others}>
+          <FieldContext.Provider value={f}>{children}</FieldContext.Provider>
+        </FormLayout>
       </FormContext.Provider>
     </FieldContext.Provider>
   );
@@ -72,9 +38,8 @@ const useDefaultValues = (props: any = {}, opts: any = {}) => {
   return useRequest(useRequestProps(props), opts);
 };
 
-export const Form: ComposedForm = observer((props) => {
+export const Form: React.FC<any> = observer((props) => {
   const { request, initialValue, useValues = useDefaultValues, ...others } = props;
-  const decorator = useFormDecorator();
   const fieldSchema = useFieldSchema();
   const form = useMemo(() => createForm(), []);
   const { loading } = useValues(props, {
@@ -86,11 +51,7 @@ export const Form: ComposedForm = observer((props) => {
   });
   return (
     <Spin spinning={loading}>
-      <FormLayout layout={'vertical'} {...others}>
-        {decorator ? <FormDecorator form={form} /> : <FormComponent form={form} />}
-      </FormLayout>
+      <FormComponent form={form} {...others} />
     </Spin>
   );
 });
-
-Form.__NOCOBASE_FORM = true;
