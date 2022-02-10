335e009f4f01f1ec17245a4b25e7f1bfabd908d4
Chareice <chareice@live.com>
2022-02-10 20:28:27 +0800

    feat: removeSchema Hook with params

--- packages/plugin-ui-schema-storage/src/server-hooks/hooks/index.ts
+++ packages/plugin-ui-schema-storage/src/server-hooks/hooks/index.ts
@@ -1,3 +1,9 @@
-const hooks = [require('./removeEmptyParents').default, require('./remove').default];
+import { hookFactory } from './factory';
+import { removeSchema } from './removeSchema';
+
+const hooks = [
+  hookFactory('onCollectionDestroy', 'removeSchema', removeSchema),
+  hookFactory('onCollectionFieldDestroy', 'removeSchema', removeSchema),
+];
 
 export { hooks };
--- packages/plugin-ui-schema-storage/src/server-hooks/hooks/remove.ts
+++
@@ -1,9 +0,0 @@
-import { hookFactory } from './factory';
-import { UiSchemaRepository } from '@nocobase/plugin-ui-schema-storage';
-
-async function remove({ schemaInstance, options, db }) {
-  const uiSchemaRepository: UiSchemaRepository = db.getRepository('ui_schemas');
-  await uiSchemaRepository.remove(schemaInstance.get('uid'));
-}
-
-export default hookFactory('onCollectionDestroy', 'remove', remove);
