51ca12cc87486ae40905dbadba851b38d0f2a4fb
chenos <chenlinxh@gmail.com>
2022-02-12 12:38:57 +0800

    feat(plugin-china-region): improve code

--- packages/plugin-china-region/src/server.ts
+++ packages/plugin-china-region/src/server.ts
@@ -1,67 +1,70 @@
-import path from 'path';
-import { provinces, cities, areas, streets, villages } from 'china-division';
 import { Plugin } from '@nocobase/server';
+import { areas, cities, provinces } from 'china-division';
+import { resolve } from 'path';
 
-async function importData(model) {
-  const timer = Date.now();
+export class ChinaRegionPlugin extends Plugin {
 
-  await model.bulkCreate(
-    provinces.map((item) => ({
-      code: item.code,
-      name: item.name,
-      level: 1,
-    })),
-  );
-
-  await model.bulkCreate(
-    cities.map((item) => ({
-      code: item.code,
-      name: item.name,
-      level: 2,
-      parent_code: item.provinceCode,
-    })),
-  );
-
-  await model.bulkCreate(
-    areas.map((item) => ({
-      code: item.code,
-      name: item.name,
-      level: 3,
-      parent_code: item.cityCode,
-    })),
-  );
-
-  // // 乡级数据 2856 条
-  // await model.bulkCreate(streets.map(item => ({
-  //   code: item.code,
-  //   name: item.name,
-  //   level: 4,
-  //   parent_code: item.areaCode
-  // })));
-
-  // // 村级数据 658001 条
-  // await model.bulkCreate(villages.map(item => ({
-  //   code: item.code,
-  //   name: item.name,
-  //   level: 5,
-  //   parent_code: item.streetCode
-  // })));
-
-  const count = await model.count();
-  console.log(`${count} rows of region data imported in ${(Date.now() - timer) / 1000}s`);
-}
-
-export default class PluginChinaRegion extends Plugin {
   async beforeLoad() {
     this.app.on('installing', async () => {
-      const ChinaRegion = this.db.getCollection('china_regions').model;
-      await importData(ChinaRegion);
+      await this.importData();
     });
   }
 
   async load() {
     await this.db.import({
-      directory: path.resolve(__dirname, 'collections'),
+      directory: resolve(__dirname, 'collections'),
     });
   }
+
+  async importData() {
+    const timer = Date.now();
+    const ChinaRegion = this.db.getModel('chinaRegions');
+
+    await ChinaRegion.bulkCreate(
+      provinces.map((item) => ({
+        code: item.code,
+        name: item.name,
+        level: 1,
+      })),
+    );
+
+    await ChinaRegion.bulkCreate(
+      cities.map((item) => ({
+        code: item.code,
+        name: item.name,
+        level: 2,
+        parentCode: item.provinceCode,
+      })),
+    );
+
+    await ChinaRegion.bulkCreate(
+      areas.map((item) => ({
+        code: item.code,
+        name: item.name,
+        level: 3,
+        parentCode: item.cityCode,
+      })),
+    );
+
+    // // 乡级数据 2856 条
+    // await ChinaRegion.bulkCreate(streets.map(item => ({
+    //   code: item.code,
+    //   name: item.name,
+    //   level: 4,
+    //   parentCode: item.areaCode
+    // })));
+
+    // // 村级数据 658001 条
+    // await ChinaRegion.bulkCreate(villages.map(item => ({
+    //   code: item.code,
+    //   name: item.name,
+    //   level: 5,
+    //   parentCode: item.streetCode
+    // })));
+
+    const count = await ChinaRegion.count();
+    console.log(`${count} rows of region data imported in ${(Date.now() - timer) / 1000}s`);
+  }
 }
+
+export default ChinaRegionPlugin;
