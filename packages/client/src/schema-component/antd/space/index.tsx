658af8f255996a263c96a271928433d12093a5e8
chenos <chenlinxh@gmail.com>
2022-02-16 22:45:49 +0800

    feat(client): add `Space` schema component

---
+++ packages/client/src/schema-component/antd/space/index.tsx
@@ -0,0 +1,18 @@
+import { useFormLayout } from '@formily/antd';
+import { Divider, Space as AntdSpace, SpaceProps } from 'antd';
+import React from 'react';
+
+export const Space: React.FC<SpaceProps> = (props) => {
+  let { split } = props;
+  if (split === '|') {
+    split = <Divider type="vertical" />;
+  }
+  const layout = useFormLayout();
+  return React.createElement(AntdSpace, {
+    size: props.size ?? layout?.spaceGap,
+    ...props,
+    split,
+  });
+};
+
+export default Space;
