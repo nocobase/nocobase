98076d628d86a6799260e0168abac94957876ee2
chenos <chenlinxh@gmail.com>
2022-02-22 23:41:19 +0800

    feat: improve collection manager module

--- packages/client/src/collection-manager/Configuration/ConfigurationTable.tsx
+++ packages/client/src/collection-manager/Configuration/ConfigurationTable.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
-import { SchemaComponent } from '../../schema-component';
+import { action } from '@formily/reactive';
+import { uid } from '@formily/shared';
+import React, { useEffect } from 'react';
+import { useRequest } from '../../api-client';
+import { SchemaComponent, useActionContext } from '../../schema-component';
+import { useCollectionManager } from '../hooks/useCollectionManager';
 import { collectionSchema } from './schemas/collections';
 
+const useAsyncDataSource = (service: any) => (field: any) => {
+  field.loading = true;
+  service(field).then(
+    action.bound((data: any) => {
+      field.dataSource = data;
+      field.loading = false;
+    }),
+  );
+};
+
+const useCollectionValues = (options) => {
+  const { visible } = useActionContext();
+  const result = useRequest(
+    () =>
+      Promise.resolve({
+        data: {
+          name: `t_${uid()}`,
+        },
+      }),
+    {
+      ...options,
+      manual: true,
+    },
+  );
+
+  useEffect(() => {
+    if (visible) {
+      result.run();
+    }
+  }, [visible]);
+
+  return result;
+}
+
 export const ConfigurationTable = () => {
+  const { collections = [] } = useCollectionManager();
+  const loadCollections = async (field: any) => {
+    return collections?.map((item: any) => ({
+      label: item.title,
+      value: item.name,
+    }));
+  };
   return (
     <div>
-      <SchemaComponent schema={collectionSchema} />
+      <SchemaComponent schema={collectionSchema} scope={{ useCollectionValues, useAsyncDataSource, loadCollections }}/>
     </div>
   );
 };
--- packages/client/src/collection-manager/interfaces/linkTo.ts
+++ packages/client/src/collection-manager/interfaces/linkTo.ts
@@ -48,7 +48,7 @@ export const linkTo: IField = {
       type: 'string',
       title: '{{t("Related collection")}}',
       required: true,
-      // 'x-reactions': ['{{useAsyncDataSource(loadCollections)}}'],
+      'x-reactions': ['{{useAsyncDataSource(loadCollections)}}'],
       'x-decorator': 'FormItem',
       'x-component': 'Select',
     },
