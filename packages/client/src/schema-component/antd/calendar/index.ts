c2b1ed192636edebbcbe6803eeed0da4c590c43e
ChengLei Shao <chareice@live.com>
2022-03-02 15:13:06 +0800

    feat: string operators (#215)
    * feat: string operators
    
    * fix: remove filter
    
    Co-authored-by: chenos <chenlinxh@gmail.com>

--- packages/client/src/schema-component/antd/calendar/index.ts
+++ packages/client/src/schema-component/antd/calendar/index.ts
@@ -1,2 +1,3 @@
 export * from './Calendar';
 export * from './context';
+
---
+++ packages/database/src/__tests__/operator/string-operator.test.ts
@@ -0,0 +1,42 @@
+import Database, { Collection, mockDatabase } from '@nocobase/database';
+
+describe('string operator', () => {
+  let db: Database;
+
+  let User: Collection;
+
+  afterEach(async () => {
+    await db.close();
+  });
+
+  beforeEach(async () => {
+    db = mockDatabase({});
+
+    await db.clean({ drop: true });
+
+    User = db.collection({
+      name: 'users',
+      fields: [{ type: 'string', name: 'name' }],
+    });
+
+    await db.sync({
+      force: true,
+    });
+  });
+
+  it('should query with include operator', async () => {
+    const u1 = await db.getRepository('users').create({
+      values: {
+        name: 'names of u1',
+      },
+    });
+
+    const u1Res = await db.getRepository('users').findOne({
+      filter: {
+        'name.$includes': 'u1',
+      },
+    });
+
+    expect(u1Res.get('id')).toEqual(u1.get('id'));
+  });
+});
