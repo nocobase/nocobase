328b1e028285e820ab73fe828fbdced367704810
ChengLei Shao <chareice@live.com>
2022-03-13 19:34:24 +0800

    feat: ne operator (#233)

---
+++ packages/database/src/__tests__/operator/ne.test.ts
@@ -0,0 +1,33 @@
+import { mockDatabase } from '../index';
+import Database from '../../database';
+
+describe('ne operator', () => {
+  let db: Database;
+  let Test;
+  beforeEach(async () => {
+    db = mockDatabase({});
+
+    Test = db.collection({
+      name: 'tests',
+      fields: [{ type: 'string', name: 'name' }],
+    });
+
+    await db.sync();
+  });
+
+  afterEach(async () => {
+    await db.close();
+  });
+
+  it('should ne with null', async () => {
+    await db.getRepository('tests').create({});
+
+    const results = await db.getRepository('tests').count({
+      filter: {
+        'name.$ne': '123',
+      },
+    });
+
+    expect(results).toEqual(1);
+  });
+});
