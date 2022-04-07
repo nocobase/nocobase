268ec83d07ef9a68e78e36df73c2d704e5e1608c
Chareice <chareice@live.com>
2022-04-07 23:33:56 +0800

    fix: load packages from process.cwd

--- packages/cli/.bin/nocobase.ts
+++ packages/cli/.bin/nocobase.ts
@@ -1,17 +1,32 @@
 #! /usr/bin/env ts-node-script
 
 import path from 'path';
-import { Application } from '@nocobase/server';
+import { Application, PluginManager } from '@nocobase/server';
 import { readConfig } from '@nocobase/server';
 
 const { Command } = require('commander');
 
 const loadApplication = async () => {
-  const configurationDir = path.join(process.cwd(), 'packages/server/src/config');
-  const config = await readConfig(configurationDir);
-  const app = new Application(config);
-  await app.load();
-  return app;
+  PluginManager.resolvePlugin = (name) => {
+    let pluginClass;
+    try {
+      pluginClass = require(path.join(`${process.cwd()}`, `packages/${name}/server`));
+    } catch (e) {
+      pluginClass = require(name);
+    }
+    return pluginClass.default;
+  };
+
+  try {
+    const configurationDir = path.join(process.cwd(), 'packages/server/src/config');
+    const config = await readConfig(configurationDir);
+    const app = new Application(config);
+    await app.load();
+    return app;
+  } catch (e) {
+    console.log({ e });
+    return null;
+  }
 };
 
 (async () => {
@@ -34,16 +49,20 @@ const loadApplication = async () => {
     };
 
   program.command('hello').action(runSubCommand('hello'));
-  program.command('console').action(runSubCommand('console'));
+
   program
     .command('develop')
     .alias('dev')
     .description('Start your NocoBase application with auto reload')
     .action(runSubCommand('dev'));
 
+  program.command('create-plugin').description('create new plugin');
+
   if (application) {
     const appCommand = program.command('app').description('Application Command');
     application.cli.commands.forEach((cmd) => appCommand.addCommand(cmd));
+
+    program.command('console').action(runSubCommand('console'));
   }
 
   await program.parseAsync(process.argv);
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,4 +1,5 @@
 export * from './application';
+export { PluginManager } from './plugin-manager';
 export * as middlewares from './middlewares';
 export * from './plugin';
 export { Application as default } from './application';
