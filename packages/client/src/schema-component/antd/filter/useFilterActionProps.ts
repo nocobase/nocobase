20680d3bc708cc78c9799bf3f3b3f9caca02a48c
chenos <chenlinxh@gmail.com>
2022-04-07 17:34:29 +0800

    feat: improve filter component

--- packages/client/src/schema-component/antd/filter/useFilterActionProps.ts
+++ packages/client/src/schema-component/antd/filter/useFilterActionProps.ts
@@ -1,10 +1,19 @@
+import { Field } from '@formily/core';
+import { useField, useFieldSchema } from '@formily/react';
+import flat from 'flat';
+import { useTranslation } from 'react-i18next';
 import { useBlockRequestContext } from '../../../block-provider';
 import { useCollection, useCollectionManager } from '../../../collection-manager';
 
 export const useFilterOptions = (collectionName: string) => {
+  const fieldSchema = useFieldSchema();
+  const fieldNames = fieldSchema?.['x-component-props']?.fieldNames || [];
   const { getCollectionFields, getInterface } = useCollectionManager();
   const fields = getCollectionFields(collectionName);
   const field2option = (field, nochildren) => {
+    if (fieldNames.length && !nochildren && !fieldNames.includes(field.name)) {
+      return;
+    }
     if (!field.interface) {
       return;
     }
@@ -46,15 +55,45 @@ export const useFilterOptions = (collectionName: string) => {
   return getOptions(fields);
 };
 
+const isEmpty = (obj) => {
+  return obj && Object.keys(obj).length === 0 && Object.getPrototypeOf(obj) === Object.prototype;
+};
+
+const removeNullCondition = (filter) => {
+  const items = flat(filter);
+  console.log('filter', items);
+  const values = {};
+  for (const key in items) {
+    const value = items[key];
+    if (value !== null && !isEmpty(value)) {
+      values[key] = value;
+    }
+  }
+  return flat.unflatten(values);
+};
+
 export const useFilterActionProps = () => {
   const { name } = useCollection();
   const options = useFilterOptions(name);
   const { service } = useBlockRequestContext();
+  const field = useField<Field>();
+  const { t } = useTranslation();
   return {
     options,
-    onSubmit({ filter }) {
-      console.log('filter', filter)
+    onSubmit(values) {
+      const filter = removeNullCondition(values?.filter);
       service.run({ ...service.params?.[0], filter });
+      const items = filter?.$and || filter?.$or;
+      if (items?.length) {
+        field.title = t('{{count}} filter items', { count: items?.length || 0 });
+      } else {
+        field.title = t('Filter');
+      }
+    },
+    onReset(values) {
+      const filter = removeNullCondition(values?.filter);
+      service.run({ ...service.params?.[0], filter });
+      field.title = t('Filter');
     },
   };
 };
