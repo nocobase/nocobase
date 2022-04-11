8f52296afd7b9483ff0cfc963b01d5fdd8b8ba83
Junyi <mytharcher@users.noreply.github.com>
2022-04-11 22:06:41 +0800

    feat(plugin-workflow): support context variables from model trigger (#284)

--- packages/client/src/workflow/triggers/model.tsx
+++ packages/client/src/workflow/triggers/model.tsx
@@ -1,9 +1,15 @@
+import React from 'react';
 import { action } from '@formily/reactive';
 import { t } from 'i18next';
+import { Select } from 'antd';
+
 import { useCollectionManager } from '../../collection-manager';
+import { useFlowContext } from '../WorkflowCanvas';
+import { BaseTypeSet } from '../calculators';
 
 export default {
   title: '数据表事件',
+  type: 'model',
   fieldset: {
     collection: {
       type: 'string',
@@ -32,5 +38,22 @@ export default {
         })(collections);
       }
     }
+  },
+  getter({ type, options, onChange }) {
+    const { collections = [] } = useCollectionManager();
+    const { workflow } = useFlowContext();
+    const collection = collections.find(item => item.name === workflow.config.collection) ?? { fields: [] };
+
+    return (
+      <Select value={options.path} placeholder="选择字段" onChange={path => {
+        onChange({ type, options: { ...options, path: `data.${path}` } });
+      }}>
+        {collection.fields
+          .filter(field => BaseTypeSet.has(field.uiSchema.type))
+          .map(field => (
+          <Select.Option key={field.name} value={field.name}>{t(field.uiSchema.title)}</Select.Option>
+        ))}
+      </Select>
+    );
   }
 };
