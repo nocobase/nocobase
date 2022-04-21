be235786d0d1ddf4ef10c84e65ef308ca6b8e2f3
chenos <chenlinxh@gmail.com>
2022-04-21 11:56:53 +0800

    fix: jump to the previous url after logging in

--- packages/core/client/src/user/CurrentUserProvider.tsx
+++ packages/core/client/src/user/CurrentUserProvider.tsx
@@ -1,6 +1,6 @@
 import { Spin } from 'antd';
 import React, { createContext, useContext } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, useLocation } from 'react-router-dom';
 import { useRequest } from '../api-client';
 
 export const CurrentUserContext = createContext(null);
@@ -10,14 +10,17 @@ export const useCurrentUserContext = () => {
 }
 
 export const CurrentUserProvider = (props) => {
+  const location = useLocation();
   const result = useRequest({
     url: 'users:check',
   });
   if (result.loading) {
     return <Spin />;
   }
+  const { pathname, search } = location;
+  let redirect = `?redirect=${pathname}${search}`;
   if (!result?.data?.data?.id) {
-    return <Redirect to={'/signin'} />;
+    return <Redirect to={`/signin${redirect}`} />;
   }
   return <CurrentUserContext.Provider value={result}>{props.children}</CurrentUserContext.Provider>;
 };
