2d264ebf7de664febe2c987a523d44120bbf42c0
chenos <chenlinxh@gmail.com>
2022-04-22 11:13:06 +0800

    fix: allow collection provider to be null

--- packages/core/client/src/collection-manager/CollectionProvider.tsx
+++ packages/core/client/src/collection-manager/CollectionProvider.tsx
@@ -3,11 +3,11 @@ import { CollectionContext } from './context';
 import { useCollectionManager } from './hooks';
 import { CollectionOptions } from './types';
 
-export const CollectionProvider: React.FC<{ name?: string; collection?: CollectionOptions }> = (props) => {
-  const { name, collection, children } = props;
+export const CollectionProvider: React.FC<{ allowNull?: boolean; name?: string; collection?: CollectionOptions }> = (props) => {
+  const { allowNull, name, collection, children } = props;
   const { getCollection } = useCollectionManager();
   const value = getCollection(collection || name);
-  if (!value) {
+  if (!value && !allowNull) {
     return null;
   }
   return <CollectionContext.Provider value={value}>{children}</CollectionContext.Provider>;
