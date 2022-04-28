158e6af6f6a282b838fcd411bf14fe010ba92306
chenos <chenlinxh@gmail.com>
2022-04-28 15:22:13 +0800

    fix: tips for app.install

--- packages/core/server/src/commands/install.ts
+++ packages/core/server/src/commands/install.ts
@@ -1,5 +1,23 @@
+import chalk from 'chalk';
+
 export default async ({ app, cliArgs }) => {
   const [opts] = cliArgs;
+
+  if (!opts?.clean || !opts?.force) {
+    const tables = await app.db.sequelize.getQueryInterface().showAllTables();
+    if (tables.includes('collections')) {
+      console.log();
+      console.log();
+      console.log(chalk.yellow('NocoBase is already installed. To reinstall, please execute:'));
+      console.log();
+      let command = 'yarn nocobase install -f';
+      console.log(chalk.yellow(command));
+      console.log();
+      console.log();
+      return;
+    }
+  }
+
   await app.install({
     cliArgs,
     clean: opts.clean,
