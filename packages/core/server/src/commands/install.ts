6d9bf9888b95f0cc7ff3e864b904f314d0641f10
chenos <chenlinxh@gmail.com>
2022-04-28 20:22:26 +0800

    feat: silent option

--- packages/core/server/src/commands/install.ts
+++ packages/core/server/src/commands/install.ts
@@ -2,19 +2,26 @@ import chalk from 'chalk';
 
 export default async ({ app, cliArgs }) => {
   const [opts] = cliArgs;
+
   if (!opts?.clean && !opts?.force) {
     const tables = await app.db.sequelize.getQueryInterface().showAllTables();
     if (tables.includes('collections')) {
-      console.log();
-      console.log();
-      console.log(chalk.yellow('NocoBase is already installed. To reinstall, please execute:'));
-      console.log();
-      let command = 'yarn nocobase install -f';
-      console.log(chalk.yellow(command));
-      console.log();
-      console.log();
+      if (!opts.silent) {
+        console.log();
+        console.log();
+        console.log(chalk.yellow('NocoBase is already installed. To reinstall, please execute:'));
+        console.log();
+        let command = 'yarn nocobase install -f';
+        console.log(chalk.yellow(command));
+        console.log();
+        console.log();
+      }
       return;
     }
+  }
+
+  if (!opts.silent) {
+    console.log(`NocoBase installing`);
   }
 
   await app.install({
@@ -24,7 +31,12 @@ export default async ({ app, cliArgs }) => {
       force: opts.force,
     },
   });
+
   await app.stop({
     cliArgs,
   });
+
+  if (!opts.silent) {
+    console.log(`NocoBase installed`);
+  }
 };
