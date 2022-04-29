2c0d3fcc5ad1bce2cbc47e82e76277918c66c565
chenos <chenlinxh@gmail.com>
2022-04-29 12:00:24 +0800

    feat: uuid field

---
+++ packages/core/database/src/fields/uuid-field.ts
@@ -0,0 +1,21 @@
+import { DataTypes } from 'sequelize';
+import { BaseColumnFieldOptions, Field, FieldContext } from './field';
+
+export class UuidField extends Field {
+  constructor(options?: any, context?: FieldContext) {
+    super(
+      {
+        defaultValue: DataTypes.UUIDV4,
+        ...options,
+      },
+      context,
+    );
+  }
+  get dataType() {
+    return DataTypes.UUID;
+  }
+}
+
+export interface UUIDFieldOptions extends BaseColumnFieldOptions {
+  type: 'uuid';
+}
