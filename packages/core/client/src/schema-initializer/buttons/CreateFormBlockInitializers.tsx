e7b7710d0c93902d75a6cc844e707c0366407cba
chenos <chenlinxh@gmail.com>
2022-04-30 23:28:48 +0800

    fix: props for CreateFormBlockInitializers

--- packages/core/client/src/schema-initializer/buttons/CreateFormBlockInitializers.tsx
+++ packages/core/client/src/schema-initializer/buttons/CreateFormBlockInitializers.tsx
@@ -5,11 +5,14 @@ import { gridRowColWrap } from '../utils';
 
 export const CreateFormBlockInitializers = (props: any) => {
   const { t } = useTranslation();
+  const { insertPosition, component } = props;
   return (
     <SchemaInitializer.Button
       wrap={gridRowColWrap}
-      title={t('Add block')}
+      title={component ? null : t('Add block')}
       icon={'PlusOutlined'}
+      insertPosition={insertPosition}
+      component={component}
       items={[
         {
           type: 'itemGroup',
