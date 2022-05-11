9c25af44c363c8469203d268917a5769edba6ec3
ChengLei Shao <chareice@live.com>
2022-05-11 15:01:35 +0000

    fix: not in operator with null value record

---
+++ packages/core/database/src/__tests__/operator/notIn.test.ts
@@ -0,0 +1,33 @@
+import { mockDatabase } from '../index';
+import Database from '../../database';
+
+describe('ne operator', () => {
+  let db: Database;
+  let Test;
+  beforeEach(async () => {
+    db = mockDatabase({});
+
+    Test = db.collection({
+      name: 'tests',
+      fields: [{ type: 'string', name: 'name' }],
+    });
+
+    await db.sync();
+  });
+
+  afterEach(async () => {
+    await db.close();
+  });
+
+  it('should notIn with null', async () => {
+    await db.getRepository('tests').create({});
+
+    const results = await db.getRepository('tests').count({
+      filter: {
+        'name.$notIn': ['123'],
+      },
+    });
+
+    expect(results).toEqual(1);
+  });
+});
