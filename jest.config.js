6272a204cbe850204ea8e3923d0fc6b348f1c3ee
mytharcher <mytharcher@gmail.com>
2022-05-14 11:48:36 +0800

    test: clean db before start test to avoid tons of tables in test db

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,40 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
 const { compilerOptions } = require('./tsconfig.jest.json');
+const Sequelize = require('sequelize');
+const { getConfigByEnv } = require('@nocobase/database');
 
-module.exports = {
-  collectCoverage: false,
-  verbose: true,
-  testEnvironment: 'jsdom',
-  preset: 'ts-jest',
-  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
-  setupFilesAfterEnv: [require.resolve('jest-dom/extend-expect'), './jest.setup.ts'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/',
-  }),
-  globals: {
-    'ts-jest': {
-      babelConfig: false,
-      tsconfig: './tsconfig.jest.json',
-      diagnostics: false,
+module.exports = async () => {
+  const db = new Sequelize(getConfigByEnv());
+  await db.getQueryInterface().dropAllTables();
+  await db.close();
+
+  return {
+    rootDir: process.cwd(),
+    collectCoverage: false,
+    verbose: true,
+    testEnvironment: 'jsdom',
+    preset: 'ts-jest',
+    testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+    setupFilesAfterEnv: [require.resolve('jest-dom/extend-expect'), './jest.setup.ts'],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+      prefix: '<rootDir>/',
+    }),
+    globals: {
+      'ts-jest': {
+        babelConfig: false,
+        tsconfig: './tsconfig.jest.json',
+        diagnostics: false,
+      },
     },
-  },
-  modulePathIgnorePatterns: ['/esm/', '/lib/'],
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '/__tests__/',
-    '/esm/',
-    '/lib/',
-    'package.json',
-    '/demo/',
-    'package-lock.json',
-  ],
+    modulePathIgnorePatterns: ['/esm/', '/lib/'],
+    coveragePathIgnorePatterns: [
+      '/node_modules/',
+      '/__tests__/',
+      '/esm/',
+      '/lib/',
+      'package.json',
+      '/demo/',
+      'package-lock.json',
+    ],
+  };
 };
