24c853563c4b205a1086593b9cc7273c6f772e48
Semmy <semmywong@126.com>
2022-05-17 23:38:17 +0800

    feat: add form custom

--- packages/core/client/src/schema-initializer/buttons/ReadPrettyFormActionInitializers.tsx
+++ packages/core/client/src/schema-initializer/buttons/ReadPrettyFormActionInitializers.tsx
@@ -33,5 +33,84 @@ export const ReadPrettyFormActionInitializers = {
         },
       ],
     },
+
+    {
+      type: 'divider',
+    },
+    {
+      type: 'subMenu',
+      title: '{{t("Customize")}}',
+      children: [
+        {
+          type: 'item',
+          title: '{{t("Popup window")}}',
+          component: 'CustomizeActionInitializer',
+          schema: {
+            type: 'void',
+            title: '{{ t("Popup window") }}',
+            'x-action': 'view',
+            'x-designer': 'Action.Designer',
+            'x-component': 'Action',
+            'x-component-props': {
+              openMode: 'drawer',
+            },
+            properties: {
+              drawer: {
+                type: 'void',
+                title: '{{ t("Popup window") }}',
+                'x-component': 'Action.Container',
+                'x-component-props': {
+                  className: 'nb-action-popup',
+                },
+                properties: {
+                  tabs: {
+                    type: 'void',
+                    'x-component': 'Tabs',
+                    'x-component-props': {},
+                    'x-initializer': 'TabPaneInitializers',
+                    properties: {
+                      tab1: {
+                        type: 'void',
+                        title: '{{t("Details")}}',
+                        'x-component': 'Tabs.TabPane',
+                        'x-designer': 'Tabs.Designer',
+                        'x-component-props': {},
+                        properties: {
+                          grid: {
+                            type: 'void',
+                            'x-component': 'Grid',
+                            'x-initializer': 'RecordBlockInitializers',
+                            properties: {},
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        {
+          type: 'item',
+          title: '{{t("Update record")}}',
+          component: 'CustomizeActionInitializer',
+          schema: {
+            title: '{{ t("Update record") }}',
+            'x-component': 'Action',
+            'x-designer': 'Action.Designer',
+            'x-action-settings': {
+              assignedValues: {},
+              onSuccess: {
+                successMessage: '{{t("Submitted successfully")}}',
+              },
+            },
+            'x-component-props': {
+              useProps: '{{ useCustomizeUpdateActionProps }}',
+            },
+          },
+        },
+      ],
+    },
   ],
 };
