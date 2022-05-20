30a50103eee8c5d7cc9ba58a30779d9f862ee218
chenos <chenlinxh@gmail.com>
2022-05-20 13:35:56 +0800

    fix(client): pagination dropdown menu is blocked #398

--- packages/core/client/src/route-switch/antd/admin-layout/index.tsx
+++ packages/core/client/src/route-switch/antd/admin-layout/index.tsx
@@ -163,11 +163,16 @@ const InternalAdminLayout = (props: any) => {
             min-height: calc(100vh - 46px);
             position: relative;
             padding-bottom: 70px;
+            > div {
+              position: relative;
+              z-index: 1;
+            }
             .ant-layout-footer {
               position: absolute;
               bottom: 0;
               text-align: center;
               width: 100%;
+              z-index: 0;
             }
           `}
         >
