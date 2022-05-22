e031e2e6c1abe30fd2a00d53ece64a9187fabfd1
chenos <chenlinxh@gmail.com>
2022-05-22 21:38:15 +0800

    fix(cli): nocobase test command --db-clean option is invalid

--- packages/core/cli/src/commands/test.js
+++ packages/core/cli/src/commands/test.js
@@ -14,7 +14,15 @@ module.exports = (cli) => {
       nodeCheck();
       if (options.dbClean) {
         promptForTs();
-        await runAppCommand('db:clean', '-y');
+        await runAppCommand('db:clean', ['-y']);
+      }
+      let index = process.argv.indexOf('-c');
+      if (index > 0) {
+        process.argv.splice(index, 1);
+      }
+      index = process.argv.indexOf('--db-clean');
+      if (index > 0) {
+        process.argv.splice(index, 1);
       }
       process.argv.splice(2, 1, '-i');
       require('jest-cli/bin/jest');
