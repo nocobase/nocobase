96b734e62368fc3a8e6a30a660cb8e360cb84b35
chenos <chenlinxh@gmail.com>
2022-05-28 08:26:32 +0800

    feat: relationship fields

---
+++ packages/core/client/src/collection-manager/interfaces/m2o.tsx
@@ -0,0 +1,207 @@
+import { ISchema } from '@formily/react';
+import { uid } from '@formily/shared';
+import { cloneDeep } from 'lodash';
+import React from 'react';
+import { ForeignKey, TargetKey } from './components';
+import { recordPickerSelector, recordPickerViewer } from './properties';
+import { IField } from './types';
+
+export const m2o: IField = {
+  name: 'm2o',
+  type: 'object',
+  group: 'relation',
+  order: 5,
+  title: '{{t("Many to one")}}',
+  isAssociation: true,
+  default: {
+    type: 'belongsTo',
+    // name,
+    uiSchema: {
+      // title,
+      'x-component': 'RecordPicker',
+      'x-component-props': {
+        // mode: 'tags',
+        multiple: false,
+        fieldNames: {
+          label: 'id',
+          value: 'id',
+        },
+      },
+    },
+    reverseField: {
+      interface: 'o2m',
+      type: 'hasMany',
+      // name,
+      uiSchema: {
+        // title,
+        'x-component': 'RecordPicker',
+        'x-component-props': {
+          // mode: 'tags',
+          multiple: true,
+          fieldNames: {
+            label: 'id',
+            value: 'id',
+          },
+        },
+      },
+    },
+  },
+  schemaInitialize(schema: ISchema, { readPretty }) {
+    if (readPretty) {
+      schema['properties'] = {
+        viewer: cloneDeep(recordPickerViewer),
+      };
+    } else {
+      schema['properties'] = {
+        selector: cloneDeep(recordPickerSelector),
+      };
+    }
+  },
+  initialize: (values: any) => {
+    if (values.type === 'belongsToMany') {
+      if (!values.through) {
+        values.through = `t_${uid()}`;
+      }
+      if (!values.foreignKey) {
+        values.foreignKey = `f_${uid()}`;
+      }
+      if (!values.otherKey) {
+        values.otherKey = `f_${uid()}`;
+      }
+      if (!values.sourceKey) {
+        values.sourceKey = 'id';
+      }
+      if (!values.targetKey) {
+        values.targetKey = 'id';
+      }
+    }
+  },
+  properties: {
+    'uiSchema.title': {
+      type: 'string',
+      title: '{{t("Field display name")}}',
+      required: true,
+      'x-decorator': 'FormItem',
+      'x-component': 'Input',
+    },
+    name: {
+      type: 'string',
+      title: '{{t("Field name")}}',
+      required: true,
+      'x-disabled': '{{ !createOnly }}',
+      'x-decorator': 'FormItem',
+      'x-component': 'Input',
+      description:
+        "{{t('Randomly generated and can be modified. Support letters, numbers and underscores, must start with an letter.')}}",
+    },
+    type: {
+      type: 'string',
+      title: '{{t("Relationship type")}}',
+      required: true,
+      'x-disabled': true,
+      'x-decorator': 'FormItem',
+      'x-component': 'Select',
+      enum: [
+        { label: 'One to one', value: 'hasOne' },
+        { label: 'One to many', value: 'hasMany' },
+        { label: 'Many to one', value: 'belongsTo' },
+        { label: 'Many to many', value: 'belongsToMany' },
+      ],
+    },
+    grid: {
+      type: 'void',
+      'x-component': 'Grid',
+      properties: {
+        row1: {
+          type: 'void',
+          'x-component': 'Grid.Row',
+          properties: {
+            col11: {
+              type: 'void',
+              'x-component': 'Grid.Col',
+              properties: {
+                source: {
+                  type: 'string',
+                  title: '{{t("Source collection")}}',
+                  'x-reactions': ['{{useAsyncDataSource(loadCollections)}}'],
+                  'x-decorator': 'FormItem',
+                  'x-component': 'Select',
+                  'x-disabled': true,
+                },
+              },
+            },
+            col12: {
+              type: 'void',
+              'x-component': 'Grid.Col',
+              properties: {
+                target: {
+                  type: 'string',
+                  title: '{{t("Target collection")}}',
+                  required: true,
+                  'x-reactions': ['{{useAsyncDataSource(loadCollections)}}'],
+                  'x-decorator': 'FormItem',
+                  'x-component': 'Select',
+                  'x-disabled': '{{ !createOnly }}',
+                },
+              },
+            },
+          },
+        },
+        row2: {
+          type: 'void',
+          'x-component': 'Grid.Row',
+          properties: {
+            col21: {
+              type: 'void',
+              'x-component': 'Grid.Col',
+              properties: {
+                foreignKey: {
+                  type: 'string',
+                  title: <ForeignKey />,
+                  'x-reactions': ['{{useAsyncDataSource(loadCollections)}}'],
+                  'x-decorator': 'FormItem',
+                  'x-component': 'Select',
+                  'x-disabled': '{{ !createOnly }}',
+                },
+              },
+            },
+            col22: {
+              type: 'void',
+              'x-component': 'Grid.Col',
+              properties: {
+                targetKey: {
+                  type: 'string',
+                  title: <TargetKey/>,
+                  default: 'id',
+                  enum: [{ label: 'ID', value: 'id' }],
+                  'x-reactions': ['{{useAsyncDataSource(loadCollections)}}'],
+                  'x-decorator': 'FormItem',
+                  'x-component': 'Select',
+                  'x-disabled': '{{ !createOnly }}',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  filterable: {
+    nested: true,
+    children: [
+      // {
+      //   name: 'id',
+      //   title: '{{t("Exists")}}',
+      //   operators: [
+      //     { label: '{{t("exists")}}', value: '$exists', noValue: true },
+      //     { label: '{{t("not exists")}}', value: '$notExists', noValue: true },
+      //   ],
+      //   schema: {
+      //     title: '{{t("Exists")}}',
+      //     type: 'string',
+      //     'x-component': 'Input',
+      //   },
+      // },
+    ],
+  },
+};
