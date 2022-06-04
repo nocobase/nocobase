527360c13240132b569be38a5a18ecb9ab021dec
Semmy <semmywong@126.com>
2022-06-05 02:14:36 +0800

    feat: add client export

---
+++ packages/plugins/export/src/client/ExportActionInitializer.tsx
@@ -0,0 +1,65 @@
+import { Schema, useFieldSchema } from '@formily/react';
+import { merge } from '@formily/shared';
+import { SchemaInitializer, useDesignable } from '@nocobase/client';
+import React from 'react';
+
+const findSchema = (schema: Schema, key: string, action: string) => {
+  return schema.reduceProperties((buf, s) => {
+    if (s[key] === action) {
+      return s;
+    }
+    const c = findSchema(s, key, action);
+    if (c) {
+      return c;
+    }
+    return buf;
+  });
+};
+const removeSchema = (schema, cb) => {
+  return cb(schema);
+};
+export const useCurrentSchema = (action: string, key: string, find = findSchema, rm = removeSchema) => {
+  const fieldSchema = useFieldSchema();
+  const { remove } = useDesignable();
+  const schema = find(fieldSchema, key, action);
+  return {
+    schema,
+    exists: !!schema,
+    remove() {
+      schema && rm(schema, remove);
+    },
+  };
+};
+
+export const ExportActionInitializer = (props) => {
+  const schema = {
+    type: 'void',
+    title: '{{ t("Export") }}',
+    'x-action': 'export',
+    'x-action-settings': {
+      export: {},
+    },
+    'x-designer': 'ExportDesigner',
+    'x-component': 'Action',
+    'x-component-props': {
+      icon: 'PlusOutlined',
+      type: 'primary',
+    },
+  };
+  const { item, insert } = props;
+  const { exists, remove } = useCurrentSchema('export', 'x-action', item.find, item.remove);
+  return (
+    <SchemaInitializer.SwitchItem
+      checked={exists}
+      title={item.title}
+      onClick={() => {
+        if (exists) {
+          return remove();
+        }
+        const s = merge(schema || {}, item.schema || {});
+        item?.schemaInitialize?.(s);
+        insert(s);
+      }}
+    />
+  );
+};
---
+++ packages/plugins/export/src/client/ExportProvider.tsx
@@ -0,0 +1,19 @@
+import { SchemaInitializerContext } from '@nocobase/client';
+import { useContext } from 'react';
+
+export const ExportProvider = (props: any) => {
+  const initializes = useContext(SchemaInitializerContext);
+  initializes.TableActionInitializers.items[0].children.push({
+    type: 'item',
+    title: "{{t('Export')}}",
+    component: 'ExportActionInitializer',
+    schema: {
+      'x-align': 'right',
+      'x-decorator': 'ACLActionProvider',
+      'x-acl-action-props': {
+        skipScopeCheck: true,
+      },
+    },
+  });
+  return props.children;
+};
