75b170352ade5935402ee34c3a909bc7b097098a
chenos <chenlinxh@gmail.com>
2022-06-07 21:34:17 +0800

    feat: chart blocks (#484)
    * feat: g2plot charts
    
    * feat: improve code
    
    * fix: update yarn.lock

---
+++ packages/core/client/src/schema-initializer/buttons/chart-config/bar.ts
@@ -0,0 +1,15 @@
+export const bar = {
+  data: [
+    { year: '1951 年', value: 38 },
+    { year: '1952 年', value: 52 },
+    { year: '1956 年', value: 61 },
+    { year: '1957 年', value: 145 },
+    { year: '1958 年', value: 48 },
+  ],
+  xField: 'value',
+  yField: 'year',
+  seriesField: 'year',
+  legend: {
+    position: 'top-left',
+  },
+};
