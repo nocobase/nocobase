78071b4cd85378707d2a468c85d751b3ccc59753
Junyi <mytharcher@users.noreply.github.com>
2022-06-08 11:22:08 +0800

    refactor(database): hook proxy (#402)
    * refactor(database): make db hooks clearly managed by proxy and support named events
    
    * refactor(database): roll back most event logic
    
    * refactor(database): rollback module name
    
    * fix(database): fix test file path
    
    * test: exec order
    
    * fix(database): rollback buildSequelizeHook method
    
    Co-authored-by: chenos <chenlinxh@gmail.com>

---
+++ packages/core/database/src/__tests__/model-hook.test.ts
@@ -0,0 +1,54 @@
+import { Database } from '..';
+import { mockDatabase } from '.';
+
+describe('model hook', () => {
+  let db: Database;
+
+  beforeEach(async () => {
+    db = mockDatabase();
+  });
+
+  afterEach(async () => {
+    await db.close();
+  });
+
+  describe('match', () => {
+    test('sequelize db hooks', async () => {
+      const matcher = db.modelHook.match('beforeDefine');
+      expect(matcher).toEqual('beforeDefine');
+    });
+
+    test('sequelize global model hooks', async () => {
+      const matcher = db.modelHook.match('beforeCreate');
+      expect(matcher).toEqual('beforeCreate');
+    });
+
+    test('sequelize model hooks without existing collection', async () => {
+      const matcher = db.modelHook.match('posts.beforeCreate');
+      expect(matcher).toEqual('beforeCreate');
+    });
+
+    test('sequelize model hooks with existing collection', async () => {
+      db.collection({
+        name: 'posts',
+        fields: []
+      });
+      const matcher = db.modelHook.match('posts.beforeCreate');
+      expect(matcher).toEqual('beforeCreate');
+    });
+
+    test('customized global hooks', async () => {
+      const matcher = db.modelHook.match('beforeDefineCollection');
+      expect(matcher).toBeNull();
+    });
+
+    test('customized model hooks', async () => {
+      db.collection({
+        name: 'posts',
+        fields: []
+      });
+      const matcher = db.modelHook.match('posts.beforeCreateWithAssociations');
+      expect(matcher).toBeNull();
+    });
+  });
+});
