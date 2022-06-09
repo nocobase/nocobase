95f61b610c02a26479dd0e420f650430f1c856e4
Junyi <mytharcher@users.noreply.github.com>
2022-06-09 16:40:10 +0800

    refactor(plugin-workflow): add revision column to execution (#491)

--- packages/core/client/src/workflow/ExecutionResourceProvider.tsx
+++ packages/core/client/src/workflow/ExecutionResourceProvider.tsx
@@ -11,7 +11,7 @@ export const ExecutionResourceProvider = ({ request, ...others }) => {
         ...request?.params,
         filter: {
           ...(request?.params?.filter),
-          workflowId: workflow.id
+          key: workflow.key
         }
       }
     },
