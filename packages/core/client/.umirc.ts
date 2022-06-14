5c0235567a6c654ad0274c6b1ecae6a2cd721f1a
chenos <chenlinxh@gmail.com>
2022-06-14 10:56:29 +0800

    feat: client doc

---
+++ packages/core/client/.umirc.ts
@@ -0,0 +1,24 @@
+import { resolveNocobasePackagesAlias } from '@nocobase/devtools/umiConfig';
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'NocoBase',
+  // outputPath: `./docs/dist/${lang}`,
+  mode: 'site',
+  resolve: {
+    includes: ['./'],
+  },
+  // locales: [[lang, lang]],
+  hash: true,
+  logo: 'https://www.nocobase.com/images/logo.png',
+  navs: [
+    null,
+    {
+      title: 'GitHub',
+      path: 'https://github.com/nocobase/nocobase',
+    },
+  ],
+  chainWebpack(config) {
+    resolveNocobasePackagesAlias(config);
+  },
+});
