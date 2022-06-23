eaf4cbbf61a21abb8e20d8a23a2975eae445af7c
chenos <chenlinxh@gmail.com>
2022-06-23 23:37:55 +0800

    fix: missing transaction (#531)

---
+++ packages/core/database/src/__tests__/hooks/afterCreateWithAssociations.test.ts
@@ -0,0 +1,33 @@
+import { mockDatabase } from '../';
+import { Database } from '../../database';
+
+describe('afterCreateWithAssociations', () => {
+  let db: Database;
+
+  beforeEach(async () => {
+    db = mockDatabase();
+  });
+
+  afterEach(async () => {
+    await db.close();
+  });
+
+  test('case 1', async () => {
+    db.collection({
+      name: 'test',
+    });
+    await db.sync();
+    const repo = db.getRepository('test');
+    db.on('test.afterCreateWithAssociations', async (model, { transaction }) => {
+      throw new Error('test error');
+    });
+    try {
+      await repo.create({
+        values: {},
+      });
+    } catch (error) {
+    }
+    const count = await repo.count();
+    expect(count).toBe(0);
+  });
+});
