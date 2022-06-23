9589ac234051e866f598a202dd16ea8b028f228b
金昶 <jinchang@baicl.com>
2022-06-23 16:10:24 +0800

    fix: remove pattern without form item (#528)

--- packages/core/client/src/schema-component/antd/form-item/FormItem.tsx
+++ packages/core/client/src/schema-component/antd/form-item/FormItem.tsx
@@ -187,7 +187,7 @@ FormItem.Designer = () => {
           }}
         />
       )}
-      {!form?.readPretty && collectionField?.interface !== 'o2m' && (
+      {form && !form?.readPretty && collectionField?.interface !== 'o2m' && (
         <SchemaSettings.SelectItem
           key="pattern"
           title={t('Pattern')}
