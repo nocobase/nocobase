ad03554fac27b1c323bd64778a7fcceac49c10c2
mytharcher <mytharcher@gmail.com>
2022-07-07 11:01:38 +0800

    fix(plugin-workflow): fix select value

--- packages/plugins/workflow/src/client/nodes/query.tsx
+++ packages/plugins/workflow/src/client/nodes/query.tsx
@@ -54,8 +54,8 @@ export default {
       <CollectionFieldSelect
         collection={config.collection}
         value={value}
-        onChange={(value) => {
-          onChange({ type, options: { ...options, path: value?.join('.') } });
+        onChange={(path) => {
+          onChange({ type, options: { ...options, path } });
         }}
       />
     );
