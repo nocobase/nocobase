143c9ebc4f14bfba9f1c469e5fb695258861c82c
qing.deng <dengqing0821@gmail.com>
2022-07-12 19:28:53 +0800

    fix: remove console

--- packages/core/client/src/schema-component/common/dnd-context/index.tsx
+++ packages/core/client/src/schema-component/common/dnd-context/index.tsx
@@ -50,7 +50,6 @@ const useDragEnd = (props?: any) => {
     }
 
     if (insertAdjacent) {
-      console.log('Drag End', insertAdjacent, activeSchema, overSchema)
       dn.insertAdjacent(insertAdjacent, activeSchema, {
         wrap: wrapSchema,
         breakRemoveOn,
