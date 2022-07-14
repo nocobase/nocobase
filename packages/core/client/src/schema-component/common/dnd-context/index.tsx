ff96cad7cca2bf42101695cc575741c1afa5bdbd
qing.deng <dengqing0821@gmail.com>
2022-07-14 12:48:59 +0800

    Revert "fix: dragging an element to the left, right, or bottom would cause the element to disappear"
    This reverts commit 26c55e22578c34e2947af200c481d312e201ca5a.

--- packages/core/client/src/schema-component/common/dnd-context/index.tsx
+++ packages/core/client/src/schema-component/common/dnd-context/index.tsx
@@ -24,15 +24,10 @@ const useDragEnd = (props?: any) => {
       return;
     }
 
-    const allowComponents = ['Grid.Col', 'Grid.Row', 'Grid']
-    let currentSchema = activeSchema
-    do {
-      if (currentSchema === overSchema) {
-        props?.onDragEnd?.(event);
-        return;
-      }
-      currentSchema = currentSchema.parent
-    } while(currentSchema && allowComponents.includes(currentSchema['x-component']))
+    if (activeSchema === overSchema) {
+      props?.onDragEnd?.(event);
+      return;
+    }
 
     const dn = createDesignable({
       t,
@@ -50,7 +45,6 @@ const useDragEnd = (props?: any) => {
     }
 
     if (insertAdjacent) {
-      console.log('Drag End', insertAdjacent, activeSchema, overSchema)
       dn.insertAdjacent(insertAdjacent, activeSchema, {
         wrap: wrapSchema,
         breakRemoveOn,
