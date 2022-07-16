34ac67d13f4e7934a8fcc6a41dd16c09b5154758
chenos <chenlinxh@gmail.com>
2022-07-16 15:49:34 +0800

    fix(client): build error

--- packages/core/client/src/schema-initializer/buttons/FormItemInitializers.tsx
+++ packages/core/client/src/schema-initializer/buttons/FormItemInitializers.tsx
@@ -1,51 +1,60 @@
+import { union } from 'lodash';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { SchemaInitializer } from '../SchemaInitializer';
 import { gridRowColWrap, useAssociatedFormItemInitializerFields, useFormItemInitializerFields } from '../utils';
-import { union } from 'lodash';
 
 // 表单里配置字段
 export const FormItemInitializers = (props: any) => {
   const { t } = useTranslation();
   const { insertPosition, component } = props;
-  const associationFields = useAssociatedFormItemInitializerFields({readPretty: true, block: 'Form'});
+  const associationFields = useAssociatedFormItemInitializerFields({ readPretty: true, block: 'Form' });
 
   return (
     <SchemaInitializer.Button
       wrap={gridRowColWrap}
       icon={'SettingOutlined'}
-      items={union([
-        {
-          type: 'itemGroup',
-          title: t('Display fields'),
-          children: useFormItemInitializerFields(),
-        }],
-        associationFields.length > 0 ? [{
-          type: 'divider',
-        }, {
-          type: 'itemGroup',
-          title: t('Display association fields'),
-          children: associationFields,
-        }] : [],
-        [{
-          type: 'divider',
-        },
-        {
-          type: 'item',
-          title: t('Add text'),
-          component: 'BlockInitializer',
-          schema: {
-            type: 'void',
-            'x-editable': false,
-            'x-decorator': 'FormItem',
-            'x-designer': 'Markdown.Void.Designer',
-            'x-component': 'Markdown.Void',
-            'x-component-props': {
-              content: t('This is a demo text, **supports Markdown syntax**.'),
+      items={union<any>(
+        [
+          {
+            type: 'itemGroup',
+            title: t('Display fields'),
+            children: useFormItemInitializerFields(),
+          },
+        ],
+        associationFields.length > 0
+          ? [
+              {
+                type: 'divider',
+              },
+              {
+                type: 'itemGroup',
+                title: t('Display association fields'),
+                children: associationFields,
+              },
+            ]
+          : [],
+        [
+          {
+            type: 'divider',
+          },
+          {
+            type: 'item',
+            title: t('Add text'),
+            component: 'BlockInitializer',
+            schema: {
+              type: 'void',
+              'x-editable': false,
+              'x-decorator': 'FormItem',
+              'x-designer': 'Markdown.Void.Designer',
+              'x-component': 'Markdown.Void',
+              'x-component-props': {
+                content: t('This is a demo text, **supports Markdown syntax**.'),
+              },
             },
           },
-        },
-      ])}
+        ],
+      )}
       insertPosition={insertPosition}
       component={component}
       title={component ? null : t('Configure fields')}
