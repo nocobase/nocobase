1042c3acb0ee9987e62a6c061dc556727f5ac999
chenos <chenlinxh@gmail.com>
2022-07-25 23:24:18 +0800

    fix(plugin-error-handler): no error message

--- packages/plugins/error-handler/src/server.ts
+++ packages/plugins/error-handler/src/server.ts
@@ -27,7 +27,7 @@ export class PluginErrorHandler extends Plugin {
       const model = instance.constructor;
       const collection = this.db.modelCollection.get(model);
       const field = collection.getField(path);
-      const fieldOptions = Schema.compile(field.options, { t: tFunc });
+      const fieldOptions = Schema.compile(field?.options, { t: tFunc });
       const title = lodash.get(fieldOptions, 'uiSchema.title', path);
       return title;
     };
