70f1ffac0498d6adb7cdbbdeca10b933647f9373
mytharcher <mytharcher@gmail.com>
2022-07-27 16:54:04 +0800

    feat(plugin-workflow): add assignees config for prompt instruction

---
+++ packages/plugins/workflow/src/server/extensions/assignees/collections/users_jobs.ts
@@ -0,0 +1,42 @@
+import { CollectionOptions } from '@nocobase/database';
+
+export default {
+  name: 'users_jobs',
+  fields: [
+    {
+      type: 'integer',
+      name: 'id',
+      primaryKey: true,
+      autoIncrement: true
+    },
+    {
+      type: 'belongsTo',
+      name: 'job'
+    },
+    {
+      type: 'belongsTo',
+      name: 'user'
+    },
+    {
+      type: 'belongsTo',
+      name: 'execution'
+    },
+    {
+      type: 'belongsTo',
+      name: 'node',
+      target: 'flow_nodes'
+    },
+    {
+      type: 'belongsTo',
+      name: 'workflow'
+    },
+    {
+      type: 'integer',
+      name: 'status'
+    },
+    {
+      type: 'jsonb',
+      name: 'result'
+    }
+  ]
+} as CollectionOptions;
