f5ee35ef05457d093341978b239b9e21ce879c79
Junyi <mytharcher@users.noreply.github.com>
2022-07-27 18:23:16 +0800

    feat(plugin-workflow): add assignees config for prompt instruction (#690)

---
+++ packages/plugins/workflow/src/server/actions/jobs.ts
@@ -0,0 +1,28 @@
+import { Context } from '@nocobase/actions';
+import { JOB_STATUS } from '../constants';
+
+export async function submit(context: Context, next) {
+  const { values } = context.action.params;
+
+  const { body: instance } = context;
+
+  // NOTE: validate status
+  if (instance.status !== JOB_STATUS.PENDING) {
+    return context.throw(400);
+  }
+
+  // NOTE: validate assignee
+  instance.set({
+    status: values.status,
+    result: values.result
+  });
+
+  context.status = 202;
+
+  await next();
+
+  const plugin = context.app.pm.get('@nocobase/plugin-workflow');
+  const processor = plugin.createProcessor(instance.execution);
+  // NOTE: resume the process and no `await` for quick returning
+  processor.resume(instance);
+}
---
+++ packages/plugins/workflow/src/server/extensions/index.ts
@@ -0,0 +1,5 @@
+import assignees from './assignees';
+
+export default [
+  assignees
+];
