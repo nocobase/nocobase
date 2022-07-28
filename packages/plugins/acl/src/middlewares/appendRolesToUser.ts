30a86dfa802b9fd5544d6baadd86887bff89e320
chenos <chenlinxh@gmail.com>
2022-07-28 20:21:31 +0800

    fix: append roles to current user (#695)

--- packages/plugins/acl/src/middlewares/appendRolesToUser.ts
+++
@@ -1,5 +0,0 @@
-export async function appendRolesToUser(ctx, next) {
-  ctx.state.currentUserAppends = ctx.state.currentUserAppends || [];
-  ctx.state.currentUserAppends.push('roles');
-  await next();
-}
