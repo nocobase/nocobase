377934b16423713e619a1a27312f2f5a186dc7d0
SemmyWong <67748948+semmywong@users.noreply.github.com>
2022-07-28 09:26:50 +0800

    fix: fix accuracy of percent (#685)
    * fix: fix accuracy of percent
    
    * fix: fix accuracy of percent
    
    * fix: test unit modify
    
    * fix: export number
    
    Co-authored-by: chenos <chenlinxh@gmail.com>

--- packages/core/utils/src/server.ts
+++ packages/core/utils/src/server.ts
@@ -1,6 +1,7 @@
 export * from './merge';
 export * from './mixin';
 export * from './mixin/AsyncEmitter';
+export * from './number';
 export * from './registry';
 export * from './requireModule';
 export * from './uid';
