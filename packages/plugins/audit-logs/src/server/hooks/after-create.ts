d47a6b7ca4e77f53e7db4c2127ae3b084dfa16a8
songyx <duunn@163.com>
2022-08-03 20:04:40 +0800

    add audit-logs with associations loged.

--- packages/plugins/audit-logs/src/server/hooks/after-create.ts
+++ packages/plugins/audit-logs/src/server/hooks/after-create.ts
@@ -27,6 +27,58 @@ export function afterCreate(app: Application) {
           }
         });
       }
+
+      //add code for the associations.
+      //it will find whether the option's values include associations.
+      //and then push it into the changes array.
+      for (const v in options.values) {
+        //find all  associations,find all changed associations.
+        // 1.find the associations fileds.
+        const fd = collection.findField((f) => {
+          if (f.name == v && (['hasOne', 'belongsTo', 'hasMany', 'belongsToMany'].includes(<string>f.type))) {
+            return true;
+          }
+        });
+
+
+        //handle the associations.
+        if (fd) {
+          console.log(v);
+          console.log(typeof options.values[v]);
+          let fvalue: string = '';
+
+          let bvalue: string = '';
+
+          //judge whether the object is a list or a object.
+          if (options.values[v] instanceof Array) {//nn
+
+            // 3.read the after values.which is submit from the front pages.
+            fvalue = ''.concat(options.values[v]);
+
+          } else {//11 or 1n
+            fvalue += options.values[v].id
+
+          }
+
+          // 4.judge whether the before != after.
+          // 5.push the != datas into the changes array.then into the database.
+          if (!fd.options.hidden) {
+            fd.options.uiSchema['x-component'] = 'Input';
+
+            if (fd.options.uiSchema.title.indexOf('[relation]') == -1) {
+              fd.options.uiSchema.title = fd.options.uiSchema.title + ' [relation]';//display tilte r = relationship,a =attribute.
+            }
+
+            changes.push({
+              field: fd.options,
+              after: fvalue,
+              before: bvalue,
+            });
+          }
+
+        }
+      }
+
       await AuditLog.repository.create({
         values: {
           type: LOG_TYPE_CREATE,
@@ -39,6 +91,6 @@ export function afterCreate(app: Application) {
         transaction,
         hooks: false,
       });
-    } catch (error) {}
+    } catch (error) { }
   };
 }
--- packages/plugins/audit-logs/src/server/hooks/after-destroy.ts
+++ packages/plugins/audit-logs/src/server/hooks/after-destroy.ts
@@ -24,6 +24,62 @@ export function afterDestroy(app: Application) {
           });
         }
       });
+
+      //add code for the associations.
+      //it will find all the associations.
+      // list the values 
+      //push to changes.
+      const as = collection.model.associations;
+      for (const v of Object.keys(as)) {
+        if(['createdBy', 'updatedBy'].includes(v)){
+          continue;
+        }
+        //find all  associations,find all changed associations.
+        // 1.find the associations fileds.
+        const fd = collection.findField((f) => {
+          if (f.name == v && (['hasOne', 'belongsTo', 'hasMany', 'belongsToMany'].includes(<string>f.type))) {
+            return true;
+          }
+        });
+
+        //handle the associations.
+        if (fd) {
+          let fvalue: string = '';
+
+          // 2.read the before values.using the model.get[object] method ,read from database.
+          const datas = await model['get' + v[0].toUpperCase() + v.substring(1)]();
+          // const datas = await model.get(v);
+          let bvalue: string = '';
+
+          //judge whether the object is a list or a object.
+          if (datas instanceof Array) {//nn
+
+            // 3.read the after values.which is submit from the front pages.
+            for (let t of datas) {
+              bvalue += ',' + t.dataValues.id;
+            }
+            bvalue = bvalue.substring(1);
+
+          } else {//11 or 1n
+            bvalue += datas.dataValues.id;
+          }
+
+          fd.options.uiSchema['x-component'] = 'Input';
+
+          if (fd.options.uiSchema.title.indexOf('[relation]') == -1) {
+            fd.options.uiSchema.title = fd.options.uiSchema.title + ' [relation]';//display tilte r = relationship,a =attribute.
+          }
+
+          changes.push({
+            field: fd.options,
+            after: '',
+            before: bvalue,
+          });
+
+        }
+
+      }
+
       await AuditLog.repository.create({
         values: {
           type: LOG_TYPE_DESTROY,
