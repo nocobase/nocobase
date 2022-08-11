c90d0f98ecabcea8cd8725bb112c2889facfcfa1
Semmy <semmywong@126.com>
2022-08-11 15:44:00 +0800

    refactor: upload excel file done

--- packages/plugins/import/src/client/useImportAction.ts
+++ packages/plugins/import/src/client/useImportAction.ts
@@ -30,32 +30,39 @@ export const useImportAction = () => {
             { value: false, label: t('No') },
           ];
         }
-        es.defaultTitle = uiSchema?.title;
+        es.defaultTitle = compile(uiSchema?.title);
         if (fieldInterface === 'chinaRegion') {
           es.dataIndex.push('name');
         }
       });
-      const { data } = await resource.export(
-        {
-          title: compile(title),
-          columns: JSON.stringify(compile(importSettings)),
-          appends: service.params[0]?.appends?.join(),
-          filter: JSON.stringify(service.params[0]?.filter),
-        },
-        {
-          method: 'get',
-          responseType: 'blob',
-        },
-      );
-      let blob = new Blob([data], { type: 'application/x-xls' });
-      const a = document.createElement('a');
-      const blobUrl = window.URL.createObjectURL(blob);
-      a.download = `${compile(title)}.xlsx`;
-      a.href = blobUrl;
-      document.body.appendChild(a);
-      a.click();
-      URL.revokeObjectURL(blobUrl);
-      document.body.removeChild(a);
+      // 创建一个 input 节点
+      var input = document.createElement('input');
+      // 设置 type 为 file 类型
+      input.setAttribute('type', 'file');
+      // 设置为多选选中文件 | 单选情况下不要设置
+      // input.setAttribute("multiple", 'muttiple')
+      // 设置 accept 为 .png,.jpg 类型 | 表示为弹出框选择文件的时候只能选中 png 和 jpg后缀的文件
+      input.setAttribute('accept', '.xls,.xlsx');
+      // 触发文件变更按钮操作
+      input.onchange = async function () {
+        // 这里的this 指的是 当前input事件中的this, 可以通过拿到 files
+        // 不管选一个文件还是两个文件，最终得到的结果都是 数组结构
+        // 这里我们只要单文件，所以拿到 索引为0的文件即可
+        var file = input.files[0];
+        // 创建一个formData对象 | 这个是浏览器自带的
+        let formData = new FormData();
+        // 将得到的file 资源放到到formData中
+        formData.append('file', file);
+        formData.append('columns', JSON.stringify(importSettings));
+        // 可以附带其他参数 | 如 id
+        // formData.append('id', 1);
+        // 这里的uploadImage 为axios封装的 export 出来的一个方法
+        await apiClient.axios.post('import:importXlsx', formData, {});
+        //   .catch((err) => {});
+      };
+
+      // 执行 click 事件，弹出文件选择框
+      input.click();
     },
   };
 };
--- packages/plugins/import/src/server/actions/import-xlsx.ts
+++ packages/plugins/import/src/server/actions/import-xlsx.ts
@@ -1,43 +1,22 @@
 import { Context, Next } from '@nocobase/actions';
 import { Repository } from '@nocobase/database';
 import xlsx from 'node-xlsx';
-import render from '../renders';
-import { columns2Appends } from '../utils';
 
 export async function importXlsx(ctx: Context, next: Next) {
-  let { title, columns, filter, fields, except } = ctx.action.params;
+  let { fields, except } = ctx.action.params;
+  let { columns } = ctx.request.body;
+  const { ['file']: file, storage } = ctx;
   const { resourceName, resourceOf } = ctx.action;
   if (typeof columns === 'string') {
     columns = JSON.parse(columns);
   }
-  const appends = columns2Appends(columns, ctx);
   columns = columns?.filter((col) => col?.dataIndex?.length > 0);
+  const list = xlsx.parse(file.buffer);
   const repository = ctx.db.getRepository<any>(resourceName, resourceOf) as Repository;
-  const collection = repository.collection;
-  const data = await repository.find({
-    filter,
-    fields,
-    appends,
-    except,
-    context: ctx,
-  });
-  const collectionFields = columns.map((col) => collection.fields.get(col.dataIndex[0]));
-  const { rows, ranges } = await render({ columns, fields: collectionFields, data }, ctx);
-  ctx.body = xlsx.build([
-    {
-      name: title,
-      data: rows,
-      options: {
-        '!merges': ranges,
-      },
-    },
-  ]);
 
-  ctx.set({
-    'Content-Type': 'application/octet-stream',
-    // to avoid "invalid character" error in header (RFC)
-    'Content-Disposition': `attachment; filename=${encodeURI(title)}.xlsx`,
-  });
+  ctx.body = {
+    result: 'ok',
+  };
 
   await next();
 }
