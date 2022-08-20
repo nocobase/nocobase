74bbeb6cb410d6ac88dd8ff01bba51db059a1ebb
chenos <chenlinxh@gmail.com>
2022-08-20 23:24:02 +0800

    fix: mysql column in where clause is ambiguous (#756)
    * fix: mysql column in where clause is ambiguous
    
    * feat: add test case
    
    * fix: sqlite error

---
+++ packages/core/database/src/__tests__/fields/array.test.ts
@@ -0,0 +1,66 @@
+import { mockDatabase } from '../';
+import { Database } from '../../database';
+
+describe('array field', () => {
+  let db: Database;
+
+  beforeEach(async () => {
+    db = mockDatabase();
+  });
+
+  afterEach(async () => {
+    await db.close();
+  });
+
+  it('should not be ambiguous', async () => {
+    db.collection({
+      name: 'a',
+      fields: [
+        {
+          type: 'array',
+          name: 'arr',
+        },
+        {
+          type: 'belongsToMany',
+          name: 'b',
+          target: 'b',
+        },
+      ],
+    });
+    db.collection({
+      name: 'b',
+      fields: [
+        {
+          type: 'array',
+          name: 'arr',
+        },
+      ],
+    });
+    await db.sync();
+    const repository = db.getRepository('a');
+    await repository.find({
+      filter: {
+        'arr.$match': ['aa'],
+      },
+      appends: ['b'],
+    });
+    await repository.find({
+      filter: {
+        'arr.$notMatch': ['aa'],
+      },
+      appends: ['b'],
+    });
+    await repository.find({
+      filter: {
+        'arr.$anyOf': ['aa'],
+      },
+      appends: ['b'],
+    });
+    await repository.find({
+      filter: {
+        'arr.$noneOf': ['aa'],
+      },
+      appends: ['b'],
+    });
+  });
+});
