6291650d1064cdb46346e25c053196c95afd9d95
mytharcher <mytharcher@gmail.com>
2022-08-22 15:20:44 +0800

    fix(plugin-users): fix update profile 500 (#766)

--- packages/plugins/users/src/__tests__/actions.test.ts
+++ packages/plugins/users/src/__tests__/actions.test.ts
@@ -1,30 +1,41 @@
 import Database from '@nocobase/database';
-import PluginACL from '@nocobase/plugin-acl';
 import { mockServer, MockServer } from '@nocobase/test';
-import supertest from 'supertest';
 import PluginUsers from '../server';
 import { userPluginConfig } from './utils';
 
 describe('actions', () => {
-  let api: MockServer;
+  let app: MockServer;
   let db: Database;
+  let adminUser;
   let agent;
+  let adminAgent;
   let pluginUser;
 
   beforeEach(async () => {
-    api = mockServer();
-    await api.cleanDb();
+    app = mockServer();
+    await app.cleanDb();
     process.env.INIT_ROOT_EMAIL = 'test@nocobase.com';
     process.env.INIT_ROOT_PASSWORD = '123456';
     process.env.INIT_ROOT_NICKNAME = 'Test';
-    api.plugin(PluginUsers, userPluginConfig);
-    api.plugin(PluginACL);
+    app.plugin(PluginUsers, userPluginConfig);
 
-    await api.loadAndInstall();
-    db = api.db;
+    await app.loadAndInstall();
+    db = app.db;
 
-    agent = supertest.agent(api.callback());
-    pluginUser = api.getPlugin('@nocobase/plugin-users');
+    pluginUser = app.getPlugin('@nocobase/plugin-users');
+    adminUser = await db.getRepository('users').findOne({
+      filter: {
+        email: process.env.INIT_ROOT_EMAIL
+      }
+    });
+
+    agent = app.agent();
+    adminAgent = app.agent().auth(
+      pluginUser.jwtService.sign({
+        userId: adminUser.get('id'),
+      }),
+      { type: 'bearer' },
+    );
   });
 
   afterEach(async () => {
@@ -34,7 +45,7 @@ describe('actions', () => {
   it('should login user with password', async () => {
     const { INIT_ROOT_EMAIL, INIT_ROOT_PASSWORD } = process.env;
 
-    let response = await api.agent().resource('users').check();
+    let response = await agent.resource('users').check();
     expect(response.body.data.id).toBeUndefined();
 
     response = await agent.post('/users:signin').send({
@@ -50,5 +61,23 @@ describe('actions', () => {
 
     response = await agent.get('/users:check').set({ Authorization: 'Bearer ' + token });
     expect(response.body.data.id).toBeDefined();
+  });
+
+  it('update profile', async () => {
+    const res1 = await agent.resource('users').updateProfile({
+      filterByTk: adminUser.id,
+      values: {
+        nickname: 'a'
+      }
+    });
+    expect(res1.status).toBe(401);
+
+    const res2 = await adminAgent.resource('users').updateProfile({
+      filterByTk: adminUser.id,
+      values: {
+        nickname: 'a'
+      }
+    });
+    expect(res2.status).toBe(200);
   });
 });
