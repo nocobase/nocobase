d24a880a47c3025e03f4d5b9d1f2a3e95d645ab3
mytharcher <mytharcher@gmail.com>
2022-08-27 17:17:49 +0800

    test(feat/database): test field options

--- packages/core/client/src/collection-manager/interfaces/serialString.tsx
+++ packages/core/client/src/collection-manager/interfaces/serialString.tsx
@@ -1,34 +1,65 @@
 import React, { useContext } from 'react';
 import { Button, Select } from 'antd';
-import { SchemaOptionsContext, useForm } from '@formily/react';
-import { ArrayTable, FormDrawer, FormLayout, Submit } from '@formily/antd';
+import { SchemaOptionsContext, useForm, useFormEffects } from '@formily/react';
+import { ArrayTable, FormButtonGroup, FormDrawer, FormLayout, Submit } from '@formily/antd';
 import { useTranslation } from 'react-i18next';
 
 import { IField } from './types';
 import { defaultProps, operators, unique } from './properties';
-import { Cron, SchemaComponent, SchemaComponentOptions, useCompile } from '../../schema-component';
+import { Cron, SchemaComponent, SchemaComponentOptions, useActionContext, useCompile } from '../../schema-component';
+import { css } from '@emotion/css';
+import { onFieldValueChange } from '@formily/core';
+
+function renderRuleOptions(options = {}) {
+  const compile = useCompile();
+  return (
+    <div className={css`
+      display: flex;
+      gap: 1em;
+      flex-wrap: wrap;
+    `}>
+      {Object.keys(options).map(key => {
+        const Component = this.optionRenders[key];
+        const { title } = this.fieldset[key]
+        return Component
+          ? (
+            <dl className={css`
+              margin: 0;
+              padding: 0;
+            `}>
+              <dt>
+                {compile(title)}
+              </dt>
+              <dd>
+                <Component key={key} value={options[key]} />
+              </dd>
+            </dl>
+          )
+          : null;
+      })}
+    </div>
+  );
+}
 
 const RuleTypes = {
   string: {
-    title: '{{t("Fixed string")}}',
-    render(options = { value: '' }) {
-      return options.value;
+    title: '{{t("Fixed text")}}',
+    optionRenders: {
+      value(options = { value: '' }) {
+        return <code>{options.value}</code>;
+      }
+    },
+    fieldset: {
+      value: {
+        type: 'string',
+        title: '{{t("Text content")}}',
+        'x-decorator': 'FormItem',
+        'x-component': 'Input'
+      }
     }
   },
   integer: {
     title: '{{t("Autoincrement")}}',
-    render(options = {}) {
-      return (
-        <>
-          {Object.keys(options).map(key => {
-            const Component = this.optionRenders[key];
-            return Component
-              ? <Component key={key} value={options[key]} />
-              : null;
-          })}
-        </>
-      );
-    },
     optionRenders: {
       digits({ value }) {
         const { t } = useTranslation();
@@ -47,7 +78,6 @@ const RuleTypes = {
         );
       },
       cycle({ value }) {
-        console.log('----------', value);
         return (
           <SchemaComponent
             schema={{
@@ -123,6 +153,14 @@ const RuleTypes = {
         default: null
       }
     }
+  },
+  date: {
+    title: '{{t("Date")}}',
+    optionRenders: {
+      format(options = { value: 'YYYYMMDD' }) {
+        return <code>{options.value}</code>;
+      }
+    }
   }
 };
 
@@ -147,7 +185,30 @@ function RuleConfig({ value, onChange }) {
     : null;
 }
 
-function FormButton() {
+function RuleTypeSelect(props) {
+  const compile = useCompile();
+  const index = ArrayTable.useIndex();
+  useFormEffects(() => {
+    onFieldValueChange(`patterns.${index}.type`, (field) => {
+      setValuesIn(`patterns.${index}.options`, null);
+    });
+  });
+
+  return (
+    <Select
+      {...props}
+      className={css`
+        min-width: 6em;
+      `}
+    >
+      {Object.keys(RuleTypes).map(key => (
+        <Select.Option key={key} value={key}>{compile(RuleTypes[key].title)}</Select.Option>
+      ))}
+    </Select>
+  );
+}
+
+function RuleConfigForm() {
   const { t } = useTranslation();
   const compile = useCompile();
   const schemaOptions = useContext(SchemaOptionsContext);
@@ -172,13 +233,17 @@ function FormButton() {
                 />
               </SchemaComponentOptions>
               <FormDrawer.Footer>
-                <Submit
-                  onSubmit={(values) => {
-                    return values;
-                  }}
-                >
-                  {t('Submit')}
-                </Submit>
+                <FormButtonGroup className={css`
+                  justify-content: flex-end;
+                `}>
+                  <Submit
+                    onSubmit={(values) => {
+                      return values;
+                    }}
+                  >
+                    {t('Submit')}
+                  </Submit>
+                </FormButtonGroup>
               </FormDrawer.Footer>
             </FormLayout>
           )
@@ -249,12 +314,26 @@ export const serialString: IField = {
                 type: 'string',
                 required: true,
                 'x-decorator': 'FormItem',
-                'x-component': 'Select',
+                // 'x-component': 'Select',
+                'x-component': RuleTypeSelect,
                 enum: [
                   { label: '{{t("Autoincrement")}}', value: 'integer' },
-                  { label: '{{t("Fixed string")}}', value: 'string' },
+                  { label: '{{t("Fixed text")}}', value: 'string' },
                   { label: '{{t("Date")}}', value: 'date' },
-                ]
+                ],
+                // 'x-reactions': {
+                //   target: '.options',
+                //   effects: ['onFieldInputValueChange'],
+                //   fulfill: {
+                //     state: {
+                //       value: '{{{}}}'
+                //     }
+                //   }
+                // }
+                // ['x-reactions'](field) {
+                //   console.log('------', field);
+                //   field.form.setValuesIn(`patterns.${field.path.segments[1]}`, { type: field.value, options: {} });
+                // }
               },
             },
           },
@@ -263,13 +342,22 @@ export const serialString: IField = {
             'x-component': 'ArrayTable.Column',
             'x-component-props': { title: '{{t("Rule content")}}' },
             properties: {
-              description: {
-                type: 'void',
+              options: {
+                type: 'object',
                 ['x-component'](props) {
                   const row = ArrayTable.useRecord();
                   const ruleType = RuleTypes[row.type];
-                  return ruleType?.render?.(row.options) ?? null;
+                  return renderRuleOptions.call(ruleType, row.options) ?? null;
                 },
+                'x-reactions': {
+                  dependencies: ['.type'],
+                  when: '{{$deps[0]}}',
+                  fulfill: {
+                    state: {
+                      value: '{{{}}}',
+                    },
+                  },
+                }
               }
             }
           },
@@ -285,58 +373,57 @@ export const serialString: IField = {
               config: {
                 type: 'void',
                 title: '{{t("Configure")}}',
-                'x-component': FormButton
+                'x-component': RuleConfigForm
               },
-              configure: {
-                type: 'void',
-                title: '{{t("Configure")}}',
-                'x-component': 'Action.Link',
-                properties: {
-                  drawer: {
-                    type: 'void',
-                    'x-component': 'Action.Drawer',
-                    'x-decorator': 'Form',
-                    'x-decorator-props': {
-                      useValues: useRowOptions
-                    },
-                    title: '{{t("Configure")}}',
-                    properties: {
-                      options: {
-                        type: 'void',
-                        'x-component': RuleConfig
-                      },
-                      actions: {
-                        type: 'void',
-                        'x-component': 'Action.Drawer.Footer',
-                        properties: {
-                          cancel: {
-                            title: '{{t("Cancel")}}',
-                            'x-component': 'Action',
-                            'x-component-props': {
-                              // useAction: '{{ cm.useCancelAction }}',
-                            },
-                          },
-                          submit: {
-                            title: '{{t("Submit")}}',
-                            'x-component': 'Action',
-                            'x-component-props': {
-                              type: 'primary',
-                              // async useAction() {
-                              //   const form = useForm();
-                              //   const ctx = useActionContext();
-                              //   await form.submit();
-                              //   console.log(form);
-                              //   ctx.setVisible(false);
-
-                              // }
-                            }
-                          }
-                        }
-                      }
-                    }
-                  },
-                },
-              },
+              // configure: {
+              //   type: 'void',
+              //   title: '{{t("Configure")}}',
+              //   'x-component': 'Action.Link',
+              //   properties: {
+              //     drawer: {
+              //       type: 'void',
+              //       'x-component': 'Action.Drawer',
+              //       'x-decorator': 'Form',
+              //       'x-decorator-props': {
+              //         useValues: useRowOptions
+              //       },
+              //       title: '{{t("Configure")}}',
+              //       properties: {
+              //         options: {
+              //           type: 'void',
+              //           'x-component': RuleConfig
+              //         },
+              //         actions: {
+              //           type: 'void',
+              //           'x-component': 'Action.Drawer.Footer',
+              //           properties: {
+              //             cancel: {
+              //               title: '{{t("Cancel")}}',
+              //               'x-component': 'Action',
+              //               'x-component-props': {
+              //                 // useAction: '{{ cm.useCancelAction }}',
+              //               },
+              //             },
+              //             submit: {
+              //               title: '{{t("Submit")}}',
+              //               'x-component': 'Action',
+              //               'x-component-props': {
+              //                 type: 'primary',
+              //                 async useAction() {
+              //                   const form = useForm();
+              //                   const ctx = useActionContext();
+              //                   await form.submit();
+              //                   console.log(form);
+              //                   ctx.setVisible(false);
+              //                 }
+              //               }
+              //             }
+              //           }
+              //         }
+              //       }
+              //     },
+              //   },
+              // },
               remove: {
                 type: 'void',
                 'x-component': 'ArrayTable.Remove',
@@ -361,3 +448,11 @@ export const serialString: IField = {
     operators: operators.string,
   }
 };
+function clearFormGraph(arg0: string) {
+  throw new Error('Function not implemented.');
+}
+
+function setValuesIn(arg0: string, arg1: null) {
+  throw new Error('Function not implemented.');
+}
+
