52cf72cb85a23b3589dfeb59bf8c8eabf1a6ca00
Junyi <mytharcher@users.noreply.github.com>
2022-09-13 23:27:19 +0800

    refactor(resourcer): combine middleware class (#825)
    * refactor(resourcer): combine middleware class
    
    * refactor(resourcer): move new middleware to plugin instance

--- packages/plugins/users/src/middlewares/parseToken.ts
+++ packages/plugins/users/src/middlewares/parseToken.ts
@@ -1,26 +1,21 @@
 import { Context, Next } from '@nocobase/actions';
-import { MiddlewareManager } from '@nocobase/resourcer';
-import UsersPlugin from '../server';
 
-export function parseToken(options?: { plugin: UsersPlugin }) {
-  const middleware = new MiddlewareManager();
-  middleware.use(async function (ctx: Context, next: Next) {
-    const user = await findUserByToken(ctx, options.plugin);
-    if (user) {
-      ctx.state.currentUser = user;
-    }
-    return next();
-  });
-  return middleware;
-}
+export async function parseToken(ctx: Context, next: Next) {
+  const user = await findUserByToken(ctx);
+  if (user) {
+    ctx.state.currentUser = user;
+  }
+  return next();
+};
 
-async function findUserByToken(ctx: Context, plugin: UsersPlugin) {
+async function findUserByToken(ctx: Context) {
   const token = ctx.getBearerToken();
   if (!token) {
     return null;
   }
+  const { jwtService } = ctx.app.getPlugin('@nocobase/plugin-users');
   try {
-    const { userId } = await plugin.jwtService.decode(token);
+    const { userId } = await jwtService.decode(token);
     const collection = ctx.db.getCollection('users');
     ctx.state.currentUserAppends = ctx.state.currentUserAppends || [];
     for (const [, field] of collection.fields) {
