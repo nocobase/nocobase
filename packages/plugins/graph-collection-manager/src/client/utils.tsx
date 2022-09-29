e98e09c7a7b2c0ebb28ce0ce6333ba0f4457110d
唐小爱 <tangxiaoai@192.168.0.103>
2022-09-29 16:24:24 +0800

    feat: 完善图形化管理数据表

--- packages/plugins/graph-collection-manager/src/client/utils.tsx
+++ packages/plugins/graph-collection-manager/src/client/utils.tsx
@@ -5,14 +5,12 @@ const shape = {
 };
 
 export const formatData = (data) => {
-    
+  console.log(data);
   const edgeData = [];
   const targetTablekeys = [];
   const tableData = data.map((item, index) => {
     const ports = item.fields.map((field) => {
-      if (['linkTo', 'belongsTo', 'hasMany', 'belongsToMany'].includes(field.type)) {
-        edgeData.push(field);
-      }
+      edgeData.push(field);
       return {
         id: field.key,
         group: 'list',
@@ -38,11 +36,10 @@ export const formatData = (data) => {
   });
   const edges = formatEdgeData(edgeData, targetTablekeys, tableData);
   console.log([...tableData, ...edges]);
-  return {nodes:tableData, edges};
+  return { nodes: tableData, edges };
 };
 
 const formatEdgeData = (data, targetTables, tableData) => {
-    console.log(data)
   const edges = [];
   for (let i = 0; i < data.length; i++) {
     if (
@@ -51,104 +48,219 @@ const formatEdgeData = (data, targetTables, tableData) => {
     ) {
       const targetTable = tableData.find((v) => v.label === data[i].target);
       const sourceTable = tableData.find((v) => v.label === data[i].collectionName);
-      edges.push({
-        id: uid(),
-        shape: shape.EDGE,
-        source: {
-          cell: sourceTable.id,
-          port: data[i].key,
-        },
-        target: {
-          cell: targetTable.id,
-          port: targetTable.ports.find((v) => v.attrs.portNameLabel.text === data[i].targetKey).id,
-        },
-        attrs: {
-          line: {
-            stroke: '#A2B1C3',
-            strokeWidth: 1,
-            textAnchor: "middle",
-            textVerticalAnchor: "middle"
-          },
-        },
-        connector: 'rounded',
-        zIndex: 0,
-        labels: [
-            {
-              markup: [
-                {
-                  tagName: 'ellipse',
-                  selector: 'labelBody',
-                },
-                {
-                  tagName: 'text',
-                  selector: 'labelText',
-                },
-              ],
-              attrs: {
-                labelText: {
-                  text: getRelationship(data[i].interface)[0],
-                  fill: '#ffa940',
-                  textAnchor: 'middle',
-                  textVerticalAnchor: 'middle',
-                },
-                labelBody: {
-                  ref: 'labelText',
-                  refWidth: '100%',
-                  refHeight: '100%',
-                  stroke: '#ffa940',
-                  fill: '#fff',
-                  strokeWidth: 1,
-                  rx: 15,
-                  ry: 15,
-                },
-              },
-              position: {
-                distance: 0.3,
-                args: {
-                  keepGradient: true,
-                  ensureLegibility: true,
-                },
+      if (data[i].interface === 'm2m') {
+        const throughTable = tableData.find((v) => v.label === data[i].through);
+        throughTable &&
+          edges.push({
+            id: uid(),
+            shape: shape.EDGE,
+            source: {
+              cell: sourceTable.id,
+              port: sourceTable.ports.find((v) => v.attrs.portNameLabel.text === data[i].sourceKey).id,
+            },
+            target: {
+              cell: throughTable.id,
+              port: throughTable.ports.find((v) => v.attrs.portNameLabel.text === data[i].foreignKey).id,
+            },
+            attrs: {
+              line: {
+                stroke: '#A2B1C3',
+                strokeWidth: 1,
+                textAnchor: 'middle',
+                textVerticalAnchor: 'middle',
               },
             },
-            {
-              markup: [
-                {
-                  tagName: 'ellipse',
-                  selector: 'labelBody',
+            connector: 'rounded',
+            router: 'metro',
+            labels: [
+              {
+                markup: [
+                  {
+                    tagName: 'ellipse',
+                    selector: 'labelBody',
+                  },
+                  {
+                    tagName: 'text',
+                    selector: 'labelText',
+                  },
+                ],
+                attrs: {
+                  labelText: {
+                    text: '1',
+                    fill: '#ffa940',
+                    textAnchor: 'middle',
+                    textVerticalAnchor: 'middle',
+                  },
+                  labelBody: {
+                    ref: 'labelText',
+                    refWidth: '100%',
+                    refHeight: '100%',
+                    stroke: '#ffa940',
+                    fill: '#fff',
+                    strokeWidth: 1,
+                    rx: 15,
+                    ry: 15,
+                  },
                 },
-                {
-                  tagName: 'text',
-                  selector: 'labelText',
-                },
-              ],
-              attrs: {
-                labelText: {
-                  text: getRelationship(data[i].interface)[1],
-                  fill: '#ffa940',
-                  textAnchor: 'middle',
-                  textVerticalAnchor: 'middle',
-                },
-                labelBody: {
-                  ref: 'labelText',
-                  refWidth: '100%',
-                  refHeight: '100%',
-                  stroke: '#ffa940',
-                  fill: '#fff',
-                  rx: 15,
-                  ry: 15,
-                  strokeWidth: 1,
-                },
-              },
-              position: {
-                distance: 0.7,
-                args: {
+                position: {
+                  distance: 0.3,
+                  args: {
                     keepGradient: true,
                     ensureLegibility: true,
                   },
+                },
+              },
+              {
+                markup: [
+                  {
+                    tagName: 'ellipse',
+                    selector: 'labelBody',
+                  },
+                  {
+                    tagName: 'text',
+                    selector: 'labelText',
+                  },
+                ],
+                attrs: {
+                  labelText: {
+                    text: 'N',
+                    fill: '#ffa940',
+                    textAnchor: 'middle',
+                    textVerticalAnchor: 'middle',
+                  },
+                  labelBody: {
+                    ref: 'labelText',
+                    refWidth: '100%',
+                    refHeight: '100%',
+                    stroke: '#ffa940',
+                    fill: '#fff',
+                    rx: 15,
+                    ry: 15,
+                    strokeWidth: 1,
+                  },
+                },
+                position: {
+                  distance: 0.7,
+                  args: {
+                    keepGradient: true,
+                    ensureLegibility: true,
+                  },
+                },
+              },
+            ],
+          });
+      } else {
+        const legalEdge = tableData
+          .find((v) => v.label == data[i].collectionName)
+          .ports.find((v) => v.attrs.portNameLabel.text === data[i].foreignKey);
+        legalEdge &&
+          edges.push({
+            id: uid(),
+            shape: shape.EDGE,
+            source: {
+              cell: sourceTable.id,
+              port: legalEdge.id,
+            },
+            target: {
+              cell: targetTable.id,
+              port: targetTable.ports.find((v) => v.attrs.portNameLabel.text === data[i].targetKey).id,
+            },
+            attrs: {
+              line: {
+                stroke: '#A2B1C3',
+                strokeWidth: 1,
+                textAnchor: 'middle',
+                textVerticalAnchor: 'middle',
               },
             },
-          ],
-      });
+            connector: 'rounded',
+            router:
+              sourceTable.id === targetTable.id
+                ? {
+                    name: 'oneSide',
+                    args: {
+                      side: 'right',
+                    },
+                  }
+                : 'metro',
+            labels: [
+              {
+                markup: [
+                  {
+                    tagName: 'ellipse',
+                    selector: 'labelBody',
+                  },
+                  {
+                    tagName: 'text',
+                    selector: 'labelText',
+                  },
+                ],
+                attrs: {
+                  labelText: {
+                    text: getRelationship(data[i].interface)[0],
+                    fill: '#ffa940',
+                    textAnchor: 'middle',
+                    textVerticalAnchor: 'middle',
+                  },
+                  labelBody: {
+                    ref: 'labelText',
+                    refWidth: '100%',
+                    refHeight: '100%',
+                    stroke: '#ffa940',
+                    fill: '#fff',
+                    strokeWidth: 1,
+                    rx: 15,
+                    ry: 15,
+                  },
+                },
+                position: {
+                  distance: 0.3,
+                  args: {
+                    keepGradient: true,
+                    ensureLegibility: true,
+                  },
+                },
+              },
+              {
+                markup: [
+                  {
+                    tagName: 'ellipse',
+                    selector: 'labelBody',
+                  },
+                  {
+                    tagName: 'text',
+                    selector: 'labelText',
+                  },
+                ],
+                attrs: {
+                  labelText: {
+                    text: getRelationship(data[i].interface)[1],
+                    fill: '#ffa940',
+                    textAnchor: 'middle',
+                    textVerticalAnchor: 'middle',
+                  },
+                  labelBody: {
+                    ref: 'labelText',
+                    refWidth: '100%',
+                    refHeight: '100%',
+                    stroke: '#ffa940',
+                    fill: '#fff',
+                    rx: 15,
+                    ry: 15,
+                    strokeWidth: 1,
+                  },
+                },
+                position: {
+                  distance: 0.7,
+                  args: {
+                    keepGradient: true,
+                    ensureLegibility: true,
+                  },
+                },
+              },
+            ],
+          });
+      }
     }
   }
   return edges;
@@ -158,14 +270,14 @@ const getRelationship = (relatioship) => {
   switch (relatioship) {
     case 'm2m':
     case 'linkTo':
-      return ['N','N'];
+      return ['N', 'N'];
     case 'o2m':
-      return ['1','N'];
+      return ['1', 'N'];
     case 'm2o':
-      return ['N','1'];
+      return ['N', '1'];
     case 'obo':
     case 'oho':
-      return['1','1'];
+      return ['1', '1'];
     default:
       return [];
   }
