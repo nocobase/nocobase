6a164642b54ff2539b3d10fc3a0b22e9297e2588
Junyi <mytharcher@users.noreply.github.com>
2022-09-30 17:36:10 +0800

    docs: add hooks dev doc (#868)
    * docs: add hooks dev doc
    
    * docs: change name from hooks to events

---
+++ packages/samples/shop-events/src/server/collections/deliveries.ts
@@ -0,0 +1,22 @@
+export default {
+  name: 'deliveries',
+  fields: [
+    {
+      type: 'belongsTo',
+      name: 'order'
+    },
+    {
+      type: 'string',
+      name: 'provider'
+    },
+    {
+      type: 'string',
+      name: 'trackingNumber'
+    },
+    {
+      type: 'integer',
+      name: 'status',
+      defaultValue: 0
+    }
+  ]
+};
