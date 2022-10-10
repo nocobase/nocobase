9da187faab97f4cb04eacdcbe203b31248420487
lyf-coder <58352715+lyf-coder@users.noreply.github.com>
2022-10-10 15:37:47 +0800

    feat(core/cache): support cache (#876)
    * feat(core/cache): support cache
    
    * build(create-nocobase-app): remove --cache-store-package cli option
    
    * perf(core/cache): modify default cache config and remove unnecessary logic code
    
    (cherry picked from commit 6e6086de7a908f38fe516e5340cb2154229c3843)
    
    # Conflicts:
    #	packages/core/server/src/application.ts
    #	packages/core/server/src/helper.ts

--- packages/app/server/src/config/index.ts
+++ packages/app/server/src/config/index.ts
@@ -1,9 +1,11 @@
 import database from './database';
 import plugins from './plugins';
 import resourcer from './resourcer';
+import cache from './cache';
 
 export default {
   database,
   resourcer,
   plugins,
+  cache,
 };
