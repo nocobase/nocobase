fd5e137a862e5461b76e2cc599879a2ce9057e29
lyf-coder <lyf-coder@foxmail.com>
2022-10-23 12:01:02 +0800

    build(create-nocobase-app): remove create-nocobase cli's cache-store-package option

--- packages/core/create-nocobase-app/src/cli.js
+++ packages/core/create-nocobase-app/src/cli.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 const { resolve } = require('path');
-const { Command,Option } = require('commander');
+const { Command } = require('commander');
 const { AppGenerator } = require('./generator');
 const { concat } = require('./util');
 const packageJson = require('../package.json');
@@ -12,22 +12,6 @@ cli
   .option('--quickstart', 'quickstart app creation')
   .option('-a, --all-db-dialect', 'install all database dialect dependencies')
   .option('-d, --db-dialect <dbDialect>', 'database dialect, current support sqlite/mysql/postgres', 'sqlite')
-  .addOption(
-    new Option('-c, --cache-store-package <cacheStorePackage>', 'cache store package').choices([
-      'all',
-      'cache-manager-redis',
-      'cache-manager-redis-store ',
-      'cache-manager-ioredis',
-      'cache-manager-mongodb',
-      'cache-manager-mongoose',
-      'cache-manager-fs-binary',
-      'cache-manager-fs-hash',
-      'cache-manager-hazelcast',
-      'cache-manager-memcached-store',
-      'cache-manager-couchbase',
-    ]),
-  )
-  // -d, --drink <size>     drink cup size (choices: "small", "medium", "large")
   .option('-e, --env <env>', 'environment variables write into .env file', concat, [])
   .description('create a new application')
   .action(async (name, options) => {
