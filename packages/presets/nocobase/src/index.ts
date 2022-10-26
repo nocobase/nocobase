403619f142148c2a943cbc6e1dc765ce60ee0efc
chenos <chenlinxh@gmail.com>
2022-10-27 00:07:58 +0800

    feat: add built-in plugins before upgrade

--- packages/presets/nocobase/src/index.ts
+++ packages/presets/nocobase/src/index.ts
@@ -1,33 +1,43 @@
 import { Plugin } from '@nocobase/server';
 
 export class PresetNocoBase<O = any> extends Plugin {
+  async addBuiltInPlugins() {
+    const plugins = [
+      'error-handler',
+      'collection-manager',
+      'ui-schema-storage',
+      'ui-routes-storage',
+      'file-manager',
+      'system-settings',
+      'verification',
+      'users',
+      'acl',
+      'china-region',
+      'workflow',
+      'client',
+      'export',
+      'audit-logs',
+    ];
+    await this.app.pm.add(plugins, {
+      enabled: true,
+      builtIn: true,
+      installed: true,
+    });
+    await this.app.reload();
+  }
 
   initialize() {
-    this.app.on('beforeInstall', async (app, options) => {
-      const plugins = [
-        'error-handler',
-        'collection-manager',
-        'ui-schema-storage',
-        'ui-routes-storage',
-        'file-manager',
-        'system-settings',
-        'verification',
-        'users',
-        'acl',
-        'china-region',
-        'workflow',
-        'client',
-        'export',
-        'audit-logs',
-      ];
-      await this.app.pm.add(plugins, {
-        enabled: true,
-        builtIn: true,
-        installed: true,
-      });
-      await this.app.reload();
+    this.app.on('beforeUpgrade', async () => {
+      const result = await this.app.version.satisfies('<0.8.0-alpha.1');
+      if (result) {
+        await this.addBuiltInPlugins();
+      }
+    });
+    this.app.on('beforeInstall', async () => {
+      await this.addBuiltInPlugins();
     });
   }
+  beforeLoad() {}
 }
 
 export default PresetNocoBase;
