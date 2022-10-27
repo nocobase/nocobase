fa180354a9af7c5468db15bb8e7d787e472a3c98
chenos <chenlinxh@gmail.com>
2022-10-27 15:53:59 +0800

    fix: error

--- packages/plugins/graph-collection-manager/src/server/index.ts
+++ packages/plugins/graph-collection-manager/src/server/index.ts
@@ -1,22 +1,12 @@
-import { InstallOptions, Plugin } from '@nocobase/server';
+import { Plugin } from '@nocobase/server';
 import path from 'path';
+
 export class GraphCollectionManagerPlugin extends Plugin {
-  getName(): string {
-    return this.getPackageName(__dirname);
-  }
-
-  beforeLoad() {
-  
-  }
-
   async load() {
-   await this.db.import({
-    directory: path.resolve(__dirname,'collections')
-   });  
-   this.app.acl.allow('graphPositions', '*');
-  }
-
-  async install(options: InstallOptions) {
+    await this.db.import({
+      directory: path.resolve(__dirname, 'collections'),
+    });
+    this.app.acl.allow('graphPositions', '*');
   }
 }
 
