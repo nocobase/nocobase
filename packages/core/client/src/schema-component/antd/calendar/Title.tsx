01893817351676da39237c995fb8909c308fbf7b
Dunqing <dengqing0821@gmail.com>
2022-10-29 14:14:21 +0800

    feat: support show lunar day in week and day (#977)
    Co-authored-by: qing <qing@imideMacBook-Air.local>

--- packages/core/client/src/schema-component/antd/calendar/Title.tsx
+++ packages/core/client/src/schema-component/antd/calendar/Title.tsx
@@ -1,14 +1,26 @@
 import { observer } from '@formily/react';
-import React, { useContext } from 'react';
+import moment from 'moment';
+import React, { useContext, useMemo } from 'react';
+import solarLunar from 'solarlunar-es';
 import { useDesignable } from '../../hooks';
 import { CalendarToolbarContext } from './context';
+import { getLunarDay } from './utils';
 
 export const Title = observer(() => {
   const { DesignableBar } = useDesignable();
-  const { label } = useContext(CalendarToolbarContext);
+  const { date, view, label, showLunar } = useContext(CalendarToolbarContext);
+
+  const lunarElement = useMemo(() => {
+    if (!showLunar || view !== 'day') {
+      return;
+    }
+    return <span>{getLunarDay(date)}</span>;
+  }, [view, date, showLunar]);
+
   return (
     <div className="ant-btn-group" style={{ fontSize: '1.75em', fontWeight: 300 }}>
-      {label}
+      <span>{label}</span>
+      <span style={{ marginLeft: '4px' }}>{lunarElement}</span>
       <DesignableBar />
     </div>
   );
