bd7b64a45641a7486e911e28157e86e1566fe6bb
chareice <chareice@live.com>
2022-10-30 09:36:57 +0800

    fix: test

--- packages/core/database/src/features/ReferencesMap.ts
+++ packages/core/database/src/features/ReferencesMap.ts
@@ -1,3 +1,4 @@
+
 export interface Reference {
   sourceCollectionName: string;
   sourceField: string;
@@ -10,9 +11,20 @@ class ReferencesMap {
   protected map: Map<string, Reference[]> = new Map();
 
   addReference(reference: Reference) {
+    const existReference = this.existReference(reference);
+
+    if (existReference) {
+      if (reference.onDelete && existReference.onDelete !== reference.onDelete) {
+        throw new Error(`On Delete Conflict, exist reference ${existReference.onDelete}, new reference ${reference.onDelete}`)
+      }
+
+      return;
+    }
+
     if (!reference.onDelete) {
       reference.onDelete = 'SET NULL';
     }
+
 
     this.map.set(reference.targetCollectionName, [...(this.map.get(reference.targetCollectionName) || []), reference]);
   }
@@ -21,9 +33,23 @@ class ReferencesMap {
     return this.map.get(collectionName);
   }
 
-  removeReference(reference: Reference) {
+  existReference(reference: Reference) {
     const references = this.map.get(reference.targetCollectionName);
 
+    if (!references) {
+      return null
+    }
+
+    const keys = Object.keys(reference).filter(k => k !== 'onDelete');
+
+    return references.find((ref) => keys.every((key) => ref[key] === reference[key]));
+  }
+
+  removeReference(reference: Reference) {
+    const references = this.map.get(reference.targetCollectionName);
+    if (!references) {
+      return;
+    }
     const keys = Object.keys(reference);
 
     this.map.set(
