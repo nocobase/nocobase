497ca4f72caa17518fa6f9e43fec1b720bbec385
chenos <chenlinxh@gmail.com>
2022-11-04 00:32:25 +0800

    feat: more console log

--- packages/presets/nocobase/src/index.ts
+++ packages/presets/nocobase/src/index.ts
@@ -35,10 +35,13 @@ export class PresetNocoBase extends Plugin {
       if (options?.method !== 'upgrade') {
         return;
       }
+      const version = await this.app.version.get();
+      console.log(`The version number before upgrade is ${version}`);
       const result = await this.app.version.satisfies('<0.8.0-alpha.1');
       if (result) {
         const r = await this.db.collectionExistsInDb('applicationPlugins');
         if (r) {
+          console.log(`Clear the installed application plugins`);
           await this.db.getRepository('applicationPlugins').destroy({ truncate: true });
           await this.app.reload();
         }
@@ -47,10 +50,12 @@ export class PresetNocoBase extends Plugin {
     this.app.on('beforeUpgrade', async () => {
       const result = await this.app.version.satisfies('<0.8.0-alpha.1');
       if (result) {
+        console.log(`Initialize all built-in plugins`);
         await this.addBuiltInPlugins();
       }
     });
     this.app.on('beforeInstall', async () => {
+      console.log(`Initialize all built-in plugins`);
       await this.addBuiltInPlugins();
     });
   }
