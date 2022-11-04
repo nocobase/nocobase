5a69a20b826296c1c26d17a88683d2982de9edb8
chareice <chareice@live.com>
2022-11-04 11:42:07 +0800

    fix: middleware await

--- packages/core/logging/src/middlewares/koa-logger.ts
+++ packages/core/logging/src/middlewares/koa-logger.ts
@@ -3,7 +3,7 @@ import { Logger } from 'winston';
 
 function KoaLogger(options: { logger: Logger }) {
   const { logger } = options;
-  return (ctx, next) => {
+  return async (ctx, next) => {
     ctx.log = logger;
     ctx.reqId = ctx.req.id = randomUUID();
 
@@ -11,19 +11,18 @@ function KoaLogger(options: { logger: Logger }) {
       reqId: ctx.reqId,
     });
 
-    next()
-      .then(() => {
-        ctx.log.info(`--> ${ctx.method} ${ctx.url}`, {
-          reqId: ctx.reqId,
-          status: ctx.status,
-        });
-      })
-      .catch((err) => {
-        ctx.log.error(err, {
-          reqId: ctx.reqId,
-        });
-        throw err;
+    try {
+      await next();
+    } catch (error) {
+      ctx.log.error(error, {
+        reqId: ctx.reqId,
       });
+    }
+
+    ctx.log.info(`--> ${ctx.method} ${ctx.url}`, {
+      reqId: ctx.reqId,
+      status: ctx.status,
+    });
   };
 }
 
