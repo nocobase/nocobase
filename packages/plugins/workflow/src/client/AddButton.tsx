8a8478cf4c60daff40ba80e8320f0d8717c50653
Junyi <mytharcher@users.noreply.github.com>
2022-11-10 13:33:52 +0800

    refactor(plugin-workflow): adjust some api (#1067)

--- packages/plugins/workflow/src/client/AddButton.tsx
+++ packages/plugins/workflow/src/client/AddButton.tsx
@@ -49,6 +49,7 @@ export function AddButton({ upstream, branchIndex = null }: AddButtonProps) {
   const groups = [
     { value: 'control', name: '{{t("Control")}}' },
     { value: 'collection', name: '{{t("Collection operations")}}' },
+    { value: 'extended', name: '{{t("Extended")}}' },
   ];
   const instructionList = (Array.from(instructions.getValues()) as Instruction[]);
 
@@ -58,21 +59,24 @@ export function AddButton({ upstream, branchIndex = null }: AddButtonProps) {
         trigger={['click']}
         overlay={
           <Menu onClick={ev => onCreate(ev)}>
-            {groups.map(group => (
-              <Menu.ItemGroup key={group.value} title={compile(group.name)}>
-                {instructionList.filter(item => item.group === group.value).map(item => item.options
-                ? (
-                  <Menu.SubMenu key={item.type} title={compile(item.title)}>
-                    {item.options.map(option => (
-                      <Menu.Item key={option.key}>{compile(option.label)}</Menu.Item>
-                    ))}
-                  </Menu.SubMenu>
-                )
-                : (
-                  <Menu.Item key={item.type}>{compile(item.title)}</Menu.Item>
-                ))}
-              </Menu.ItemGroup>
-            ))}
+            {groups.map(group => {
+              const groupInstructions = instructionList.filter(item => item.group === group.value);
+              return groupInstructions.length ? (
+                <Menu.ItemGroup key={group.value} title={compile(group.name)}>
+                  {groupInstructions.map(item => item.options
+                  ? (
+                    <Menu.SubMenu key={item.type} title={compile(item.title)}>
+                      {item.options.map(option => (
+                        <Menu.Item key={option.key}>{compile(option.label)}</Menu.Item>
+                      ))}
+                    </Menu.SubMenu>
+                  )
+                  : (
+                    <Menu.Item key={item.type}>{compile(item.title)}</Menu.Item>
+                  ))}
+                </Menu.ItemGroup>
+              ) : null;
+            })}
           </Menu>
         }
         disabled={workflow.executed}
--- packages/plugins/workflow/src/client/index.tsx
+++ packages/plugins/workflow/src/client/index.tsx
@@ -1,4 +1,5 @@
 export { triggers } from './triggers';
 export * from './nodes';
 export { calculators } from './calculators';
+export * from './FlowContext';
 export { WorkflowProvider as default } from './WorkflowProvider';
