df3a5cfcf57544dcb827b492615e25abb7cef7c0
chenos <chenlinxh@gmail.com>
2022-11-11 22:37:37 +0800

    feat: improve code

--- packages/core/logging/src/middlewares/koa-logger.ts
+++ packages/core/logging/src/middlewares/koa-logger.ts
@@ -1,47 +1,47 @@
 import { randomUUID } from 'crypto';
+import Koa from 'koa';
 import { Logger } from 'winston';
-import lodash from 'lodash';
 
 function KoaLogger(options: { logger: Logger }) {
   const { logger } = options;
-  return async (ctx, next) => {
-    ctx.reqId = ctx.req.id = randomUUID();
-    const ctxLogger = logger.child({ reqId: ctx.reqId });
+  return async (ctx: Koa.Context, next) => {
+    const startTime = Date.now();
+    const info = {
+      level: 'info',
+      status: 200,
+      message: `END: ${ctx.method} ${ctx.url}`,
+    };
 
-    ctx.logger = ctx.log = ctxLogger;
+    ctx.reqId = ctx.req['id'] = randomUUID();
+    ctx.logger = ctx.log = logger.child({ reqId: ctx.reqId });
 
-    ctx.logger.info(`<-- ${ctx.method} ${ctx.url}`, {
-      headers: lodash.omit(ctx.headers, [
-        'authorization',
-        'cookie',
-        'x-xsrf-token',
-        'accept',
-        'accept-encoding',
-        'user-agent',
-      ]),
-      body: ctx.request.body,
-    });
+    ctx.logger.info(`BEGIN: ${ctx.method} ${ctx.url}`);
+
+    let error;
 
     try {
       await next();
-    } catch (error) {
-      ctx.logger.error(error);
+    } catch (e) {
+      error = e;
+    } finally {
+      info['status'] = ctx.status;
+      if (Math.floor(ctx.status / 100) == 5) {
+        info.level = 'error';
+        info['errors'] = ctx.body?.['errors'] || ctx.body;
+      } else if (Math.floor(ctx.status / 100) == 4) {
+        info.level = 'warn';
+        info['errors'] = ctx.body?.['errors'] || ctx.body;
+      }
+      info['duration'] = Date.now() - startTime;
+      if (ctx.action) {
+        info['action'] = ctx.action?.toJSON?.();
+      }
+      ctx.logger.log(info);
+    }
 
+    if (error) {
       throw error;
     }
-
-    const statusCode = ctx.status;
-    let responseLevel = 'info';
-
-    if (Math.floor(statusCode / 100) == 5) {
-      responseLevel = 'error';
-    }
-
-    ctx.logger.log({
-      level: responseLevel,
-      message: `--> ${ctx.method} ${ctx.url}`,
-      status: statusCode,
-    });
   };
 }
 
