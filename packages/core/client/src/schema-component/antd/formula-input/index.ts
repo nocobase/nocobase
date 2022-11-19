32220dcc2661f58541ad23a72af1ede5e247efc7
chenos <chenlinxh@gmail.com>
2022-11-19 18:15:42 +0800

    Revert "refactor: formula plugin (#1082)"
    This reverts commit 0469b8c44d4c89c403bd8bde68053c769159952e.

---
+++ packages/core/client/src/schema-component/antd/formula-input/index.ts
@@ -0,0 +1,6 @@
+import { Compute } from './Compute';
+import { FormulaInput } from './FormulaInput';
+
+export const Formula: any = () => null;
+Formula.Expression = FormulaInput;
+Formula.Result = Compute;
