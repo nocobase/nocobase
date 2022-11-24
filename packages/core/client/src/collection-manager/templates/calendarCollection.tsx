033ba8a88711cd29f9051f4e1d3835764bad47c9
katherinehhh <katherine_15995@163.com>
2022-11-24 15:47:22 +0800

    feat: code optimization

--- packages/core/client/src/collection-manager/templates/calendarCollection.tsx
+++ packages/core/client/src/collection-manager/templates/calendarCollection.tsx
@@ -1,7 +1,7 @@
 import { defaultProps, defaultSystemFields, defaultCollectionOptions } from './properties';
-import { IField } from './types';
+import { ICollectionTemplate } from './types';
 
-export const calendarCollection: IField = {
+export const calendarCollection: ICollectionTemplate = {
   name: 'calendarCollection',
   type: 'object',
   title: '{{t("Calendar collection")}}',
