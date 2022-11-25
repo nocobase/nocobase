3d4d5ae025f6d05ef0422ebb8b174f830f01b6a4
katherinehhh <katherine_15995@163.com>
2022-11-25 11:13:22 +0800

    feat: getCurrentOptions fix

--- packages/core/client/src/schema-component/antd/select/shared.ts
+++ packages/core/client/src/schema-component/antd/select/shared.ts
@@ -7,7 +7,21 @@ export const defaultFieldNames = {
   options: 'children',
 };
 
+
+
 export const getCurrentOptions = (values, dataSource, fieldNames) => {
+  function flatData(data) {
+    let newArr = [];
+    for (let i = 0; i < data.length; i++) {
+      const children = data[i][fieldNames.options];
+      if (Array.isArray(children)) {
+        newArr.push(...flatData(children))
+      }
+      newArr.push({ ...data[i] });
+    }
+    return newArr;
+  }
+  const result = flatData(dataSource);
   values = castArray(values)
     .filter((item) => item != null)
     .map((val) => (typeof val === 'object' ? val[fieldNames.value] : val));
@@ -17,13 +31,7 @@ export const getCurrentOptions = (values, dataSource, fieldNames) => {
       const option = options.find((v) => v[fieldNames.value] === value) || { value: value, label: value };
       current.push(option);
     }
-    for (const option of options) {
-      const children = option[fieldNames.options];
-      if (Array.isArray(children)) {
-        current.push(...findOptions(children));
-      }
-    }
     return current;
   };
-  return findOptions(dataSource);
+  return findOptions(result);
 };
