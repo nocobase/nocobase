e488785112ed95088744333c1f2aa4c8485ed727
chareice <chareice@live.com>
2022-11-28 15:40:13 +0800

    fix: role resource scope onDelete

--- packages/plugins/acl/src/collections/rolesResourcesActions.ts
+++ packages/plugins/acl/src/collections/rolesResourcesActions.ts
@@ -23,7 +23,7 @@ export default {
       type: 'belongsTo',
       name: 'scope',
       target: 'rolesResourcesScopes',
-      onDelete: 'CASCADE',
+      onDelete: 'RESTRICT',
     },
   ],
 } as CollectionOptions;
