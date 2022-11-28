f24358de24638e3cd67e412f93d26a641679a122
katherinehhh <katherine_15995@163.com>
2022-11-28 12:55:04 +0800

    feat: collection template local

--- packages/core/client/src/collection-manager/templates/listCollection.tsx
+++ packages/core/client/src/collection-manager/templates/listCollection.tsx
@@ -3,7 +3,7 @@ import { ICollectionTemplate } from './types';
 
 export const listCollection: ICollectionTemplate = {
   name: 'listCollection',
-  title: '{{t("List collection")}}',
+  title: '{{t("General collection")}}',
   order: 1,
   color: 'blue',
   default: {
