4085ed0db762bac50f1a4cd9fc381b98de7bd05d
katherinehhh <katherine_15995@163.com>
2022-11-29 11:31:30 +0800

    Feat/collection templates (#1124)
    * feat: add collection templates
    
    * feat:  collection templates
    
    * feat:  collection templates
    
    * feat:  collection templates
    
    * feat:  collection templates
    
    * feat: collection templates
    
    * feat: calendar collection template
    
    * feat: no id does not support sorting
    
    * feat: collection template edit
    
    * feat: collectiom templates code improve
    
    * feat: collection template supports configurable field interface
    
    * feat: collection template supports configurable field interface
    
    * feat: establish relation field when there is no id
    
    * feat: collection templates
    
    * feat: collection templates locale
    
    * feat: calendar collection
    
    * feat: calendar collection default fields
    
    * feat: code optimization
    
    * feat: code optimization
    
    * feat: code optimization
    
    * feat: collectin template fix
    
    * feat: add sample-custom-collection-template
    
    * feat: code optimization
    
    * feat: code optimization
    
    * feat: code optimization
    
    * feat: code optimization
    
    * feat: import code
    
    * feat: collection template local
    
    * feat: code opimization
    
    * feat: code opimization
    
    * feat: code opimization
    
    * feat: code opimization
    
    * feat: code opimization
    
    * feat: collection template local
    
    * feat: collection template local
    
    * fix(collection-manager): exclude reverse parameters when overriding
    
    * feat: update docs
    
    * feat: disabled
    
    * feat: improve code
    
    * feat: update submodule
    
    * feat: submodules: true
    
    * fix: token
    
    * fix: with ssh-key
    
    * fix: update dockerfile
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: --no-verify-access
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: missing env
    
    * fix: env
    
    Co-authored-by: chenos <chenlinxh@gmail.com>
    Co-authored-by: chareice <chareice@live.com>

--- packages/core/client/src/collection-manager/CollectionManagerProvider.tsx
+++ packages/core/client/src/collection-manager/CollectionManagerProvider.tsx
@@ -1,20 +1,24 @@
 import { Spin } from 'antd';
 import React, { useContext, useState } from 'react';
+import { keyBy } from 'lodash';
 import { useAPIClient, useRequest } from '../api-client';
 import { CollectionManagerSchemaComponentProvider } from './CollectionManagerSchemaComponentProvider';
 import { CollectionManagerContext } from './context';
 import * as defaultInterfaces from './interfaces';
 import { CollectionManagerOptions } from './types';
+import { templateOptions } from '../collection-manager/Configuration/templates';
 
 export const CollectionManagerProvider: React.FC<CollectionManagerOptions> = (props) => {
-  const { service, interfaces, collections = [], refreshCM } = props;
+  const { service, interfaces, collections = [], refreshCM, templates } = props;
+  const defaultTemplates = keyBy(templateOptions(), (item) => item.name);
   const ctx = useContext(CollectionManagerContext);
   return (
     <CollectionManagerContext.Provider
       value={{
         ...ctx,
         service,
-        interfaces: { ...defaultInterfaces, ...interfaces, ...ctx.interfaces },
+        interfaces: { ...defaultInterfaces, ...interfaces },
+        templates: { ...defaultTemplates, ...templates },
         collections,
         refreshCM,
       }}
--- packages/core/client/src/collection-manager/Configuration/index.tsx
+++ packages/core/client/src/collection-manager/Configuration/index.tsx
@@ -1,15 +1,17 @@
 import { registerValidateFormats } from '@formily/core';
-import { exp } from 'mathjs';
 
 export * from './AddFieldAction';
 export * from './ConfigurationTable';
 export * from './EditFieldAction';
 export * from './interfaces';
 export * from './components';
+export * from './templates';
 export * from './CollectionFieldsTable';
-export * from './schemas/collections'
-export * from './OverridingCollectionField'
-export * from './ViewInheritedField'
+export * from './schemas/collections';
+export * from './OverridingCollectionField';
+export * from './ViewInheritedField';
+export * from './AddCollectionAction';
+export * from './EditCollectionAction';
 
 registerValidateFormats({
   uid: /^[A-Za-z0-9][A-Za-z0-9_-]*$/,
