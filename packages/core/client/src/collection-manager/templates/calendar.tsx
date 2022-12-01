747f5b2c546ada293ca251f4270ac5f83a472d1b
lyf-coder <lyf-coder@foxmail.com>
2022-12-01 10:59:03 +0800

    feat(client/collection-manager): calendar collection support set titleField

--- packages/core/client/src/collection-manager/templates/calendar.tsx
+++ packages/core/client/src/collection-manager/templates/calendar.tsx
@@ -33,5 +33,5 @@ export const calendar: ICollectionTemplate = {
   availableFieldInterfaces: {
     include: [],
   },
-  configurableProperties: getConfigurableProperties('title', 'name', 'inherits'),
+  configurableProperties: getConfigurableProperties('title', 'name','titleField', 'inherits'),
 };
