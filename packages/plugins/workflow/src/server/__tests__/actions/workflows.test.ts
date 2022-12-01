8673a1fa58626998d6235f266d26d579829cf703
mytharcher <mytharcher@gmail.com>
2022-12-01 21:49:24 +0800

    fix(plugin-workflow): fix test case

--- packages/plugins/workflow/src/server/__tests__/actions/workflows.test.ts
+++ packages/plugins/workflow/src/server/__tests__/actions/workflows.test.ts
@@ -125,8 +125,15 @@ describe('workflow > actions > workflows', () => {
         }
       });
 
+      const p1 = await PostRepo.create({ values: { title: 't1' } });
+
+      await sleep(500);
+
       const { body, status } = await agent.resource(`workflows`).revision({
-        filterByTk: w1.id
+        filterByTk: w1.id,
+        filter: {
+          key: w1.key
+        }
       });
 
       expect(status).toBe(200);
@@ -135,10 +142,8 @@ describe('workflow > actions > workflows', () => {
       expect(w2.key).toBe(w1.key);
       expect(w2.current).toBeFalsy();
       expect(w2.enabled).toBe(false);
-
-      const p1 = await PostRepo.create({ values: { title: 't1' } });
-
-      await sleep(500);
+      expect(w2.executed).toBe(0);
+      expect(w2.allExecuted).toBe(1);
 
       await WorkflowModel.update({
         enabled: true
@@ -149,18 +154,21 @@ describe('workflow > actions > workflows', () => {
         individualHooks: true
       });
 
+      const p2 = await PostRepo.create({ values: { title: 't2' } });
+
+      await sleep(500);
+
       const [w1next, w2next] = await WorkflowModel.findAll({
         order: [['id', 'ASC']]
       });
 
       expect(w1next.enabled).toBe(false);
       expect(w1next.current).toBe(null);
+      expect(w1next.allExecuted).toBe(2);
       expect(w2next.enabled).toBe(true);
       expect(w2next.current).toBe(true);
-
-      const p2 = await PostRepo.create({ values: { title: 't2' } });
-
-      await sleep(500);
+      expect(w2next.executed).toBe(1);
+      expect(w2next.allExecuted).toBe(2);
 
       const [e1] = await w1next.getExecutions();
       const [e2] = await w2next.getExecutions();
@@ -206,7 +214,10 @@ describe('workflow > actions > workflows', () => {
       await n1.setDownstream(n2);
 
       const { body } = await agent.resource(`workflows`).revision({
-        filterByTk: w1.id
+        filterByTk: w1.id,
+        filter: {
+          key: w1.key
+        }
       });
       const w2 = await WorkflowModel.findByPk(body.data.id, {
         include: [
@@ -282,10 +293,71 @@ describe('workflow > actions > workflows', () => {
       });
 
       const { status } = await agent.resource(`workflows`).revision({
-        filterByTk: w1.id
+        filterByTk: w1.id,
+        filter: {
+          key: w1.key
+        }
       });
 
       expect(status).toBe(400);
+    });
+
+    it('duplicate workflow', async () => {
+      const w1 = await WorkflowModel.create({
+        enabled: true,
+        type: 'collection',
+        config: {
+          mode: 1,
+          collection: 'posts'
+        }
+      });
+
+      const p1 = await PostRepo.create({ values: { title: 't1' } });
+
+      await sleep(500);
+
+      const { body, status } = await agent.resource(`workflows`).revision({
+        filterByTk: w1.id,
+      });
+
+      expect(status).toBe(200);
+      const { data: w2 } = body;
+      expect(w2.config).toMatchObject(w1.config);
+      expect(w2.key).not.toBe(w1.key);
+      expect(w2.current).toBeTruthy();
+      expect(w2.enabled).toBe(false);
+      expect(w2.allExecuted).toBe(0);
+
+      await WorkflowModel.update({
+        enabled: true
+      }, {
+        where: {
+          id: w2.id
+        },
+        individualHooks: true
+      });
+
+      const p2 = await PostRepo.create({ values: { title: 't2' } });
+
+      await sleep(500);
+
+      const [w1next, w2next] = await WorkflowModel.findAll({
+        order: [['id', 'ASC']]
+      });
+
+      expect(w1next.enabled).toBe(true);
+      expect(w1next.current).toBe(true);
+      expect(w1next.executed).toBe(2);
+      expect(w1next.allExecuted).toBe(2);
+      expect(w2next.enabled).toBe(true);
+      expect(w2next.executed).toBe(1);
+      expect(w2next.allExecuted).toBe(1);
+
+      const [e1] = await w1next.getExecutions();
+      const [e2] = await w2next.getExecutions();
+      expect(e1.key).not.toBe(e2.key);
+      expect(e1.workflowId).toBe(w1.id);
+      expect(e2.workflowId).toBe(w2.id);
     });
   });
 });
