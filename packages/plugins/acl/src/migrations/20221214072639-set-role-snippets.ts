f57d4d161f931156e4e008c23dd9f994f482a9da
Chareice <chareice@live.com>
2022-12-14 11:21:11 +0800

    feat: set role snippets migration

---
+++ packages/plugins/acl/src/migrations/20221214072639-set-role-snippets.ts
@@ -0,0 +1,16 @@
+import { Migration } from '@nocobase/server';
+
+export default class SetRoleSnippetsMigration extends Migration {
+  async up() {
+    await this.app.db.getRepository('roles').update({
+      filter: {
+        allowConfigure: true,
+      },
+      values: {
+        snippets: ['ui-editor.*', 'plugin-manager.*', 'settings-center.*'],
+      },
+    });
+  }
+
+  async down() {}
+}
