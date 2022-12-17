e995e4f9362f8cca3e8c0c408701c77de37fa143
chenos <chenlinxh@gmail.com>
2022-12-18 00:00:40 +0800

    feat: page tabs

---
+++ packages/core/client/src/schema-component/antd/page/PageTabDesigner.tsx
@@ -0,0 +1,158 @@
+import { MenuOutlined } from '@ant-design/icons';
+import { ISchema, useField, useFieldSchema } from '@formily/react';
+import { Modal, Space } from 'antd';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDesignable } from '../..';
+import { SchemaSettings } from '../../../schema-settings';
+
+export const PageDesigner = ({ title }) => {
+  const { dn } = useDesignable();
+  const { t } = useTranslation();
+  const field = useField();
+  const fieldSchema = useFieldSchema();
+  const disablePageHeader = fieldSchema['x-component-props']?.disablePageHeader;
+  return (
+    <div className={'general-schema-designer'}>
+      <div className={'general-schema-designer-icons'}>
+        <Space size={2} align={'center'}>
+          <SchemaSettings title={<MenuOutlined style={{ cursor: 'pointer', fontSize: 12 }} />}>
+            <SchemaSettings.SwitchItem
+              title={t('Enable page header')}
+              checked={!fieldSchema['x-component-props']?.disablePageHeader}
+              onChange={(v) => {
+                fieldSchema['x-component-props'] = fieldSchema['x-component-props'] || {};
+                fieldSchema['x-component-props']['disablePageHeader'] = !v;
+                dn.emit('patch', {
+                  schema: {
+                    ['x-uid']: fieldSchema['x-uid'],
+                    ['x-component-props']: fieldSchema['x-component-props'],
+                  },
+                });
+                dn.refresh();
+              }}
+            />
+            {!disablePageHeader && <SchemaSettings.Divider />}
+            {!disablePageHeader && (
+              <SchemaSettings.ModalItem
+                hide
+                title={t('Edit page title')}
+                schema={
+                  {
+                    type: 'object',
+                    title: t('Edit page title'),
+                    properties: {
+                      title: {
+                        title: t('Title'),
+                        required: true,
+                        'x-decorator': 'FormItem',
+                        'x-component': 'Input',
+                        'x-component-props': {},
+                      },
+                    },
+                  } as ISchema
+                }
+                initialValues={{ title }}
+                onSubmit={({ title }) => {
+                  field.title = title;
+                  fieldSchema['title'] = title;
+                  dn.emit('patch', {
+                    schema: {
+                      ['x-uid']: fieldSchema['x-uid'],
+                      title,
+                    },
+                  });
+                }}
+              />
+            )}
+            {!disablePageHeader && (
+              <SchemaSettings.SwitchItem
+                title={t('Enable page tabs')}
+                checked={fieldSchema['x-component-props']?.enablePageTabs}
+                onChange={(v) => {
+                  fieldSchema['x-component-props'] = fieldSchema['x-component-props'] || {};
+                  fieldSchema['x-component-props']['enablePageTabs'] = v;
+                  dn.emit('patch', {
+                    schema: {
+                      ['x-uid']: fieldSchema['x-uid'],
+                      ['x-component-props']: fieldSchema['x-component-props'],
+                    },
+                  });
+                  dn.refresh();
+                }}
+              />
+            )}
+          </SchemaSettings>
+        </Space>
+      </div>
+    </div>
+  );
+};
+
+export const PageTabDesigner = ({ schema }) => {
+  const { dn } = useDesignable();
+  const { t } = useTranslation();
+  return (
+    <div className={'general-schema-designer'}>
+      <div className={'general-schema-designer-icons'}>
+        <Space size={2} align={'center'}>
+          <SchemaSettings title={<MenuOutlined style={{ cursor: 'pointer', fontSize: 12 }} />}>
+            <SchemaSettings.ModalItem
+              title={t('Edit')}
+              schema={
+                {
+                  type: 'object',
+                  title: t('Edit tab'),
+                  properties: {
+                    title: {
+                      title: t('Tab name'),
+                      required: true,
+                      'x-decorator': 'FormItem',
+                      'x-component': 'Input',
+                      'x-component-props': {},
+                    },
+                    icon: {
+                      title: t('Icon'),
+                      'x-decorator': 'FormItem',
+                      'x-component': 'IconPicker',
+                      'x-component-props': {},
+                    },
+                  },
+                } as ISchema
+              }
+              initialValues={{ title: schema.title, icon: schema['x-icon'] }}
+              onSubmit={({ title, icon }) => {
+                schema.title = title;
+                schema['x-icon'] = icon;
+                dn.emit('patch', {
+                  schema: {
+                    ['x-uid']: schema['x-uid'],
+                    title,
+                    'x-icon': icon,
+                  },
+                });
+                dn.refresh();
+              }}
+            />
+            <SchemaSettings.Divider />
+            <SchemaSettings.Item
+              eventKey="remove"
+              onClick={() => {
+                Modal.confirm({
+                  title: t('Delete block'),
+                  content: t('Are you sure you want to delete it?'),
+                  ...confirm,
+                  onOk() {
+                    dn.remove(schema);
+                  },
+                });
+              }}
+            >
+              {t('Delete')}
+            </SchemaSettings.Item>
+          </SchemaSettings>
+        </Space>
+      </div>
+    </div>
+  );
+};
