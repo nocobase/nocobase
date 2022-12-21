dcc247a806b1e82a9560a563d4aea6b737929bb8
katherinehhh <katherine_15995@163.com>
2022-12-21 17:13:03 +0800

    feat: setting center fix

--- packages/core/client/src/acl/Configuration/PermisionProvider.tsx
+++ packages/core/client/src/acl/Configuration/PermisionProvider.tsx
@@ -43,7 +43,7 @@ export const PermissionProvider = (props) => {
                 values: [path],
               });
             }
-            setCurrentRecord({ ...currentRecord, [path]: value });
+            setCurrentRecord({ ...currentRecord, ...form.values, [path]: value });
           } else {
             await api.resource('roles').update({
               filterByTk: record.name,
