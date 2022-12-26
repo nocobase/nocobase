8f4e4958273788736a8d0031909c5307a2c23830
chenos <chenlinxh@gmail.com>
2022-12-26 15:53:27 +0800

    chore: init plugin

--- packages/plugins/data-analyse/src/client/index.tsx
+++ packages/plugins/data-analyse/src/client/index.tsx
@@ -1,5 +1,66 @@
-import React from 'react';
+import { PieChartOutlined } from '@ant-design/icons';
+import {
+  SchemaComponentOptions,
+  SchemaInitializer,
+  SchemaInitializerContext, SettingsCenterProvider
+} from '@nocobase/client';
+import { Card } from 'antd';
+import React, { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const DataAnalyseInitializer = (props) => {
+  const { insert } = props;
+  const { t } = useTranslation();
+  return (
+    <SchemaInitializer.Item
+      {...props}
+      icon={<PieChartOutlined />}
+      onClick={() => {
+        insert({
+          type: 'void',
+          //TODO 添加设计器 'x-designer': 'G2Plot.Designer',
+          'x-component': 'CardItem',
+          properties: {
+            content: {
+              type: 'void',
+              'x-component': 'div',
+              'x-content': 'Hello World',
+            },
+          },
+        });
+      }}
+      title={t('Hello Data Analyse')}
+    />
+  );
+};
 
 export default React.memo((props) => {
-  return <>{props.children}</>;
+  const items = useContext(SchemaInitializerContext);
+  const children = items.BlockInitializers.items[1].children;
+  children.push({
+    key: 'dataAnalyse',
+    type: 'item',
+    title: '{{t("DataAnalyse")}}',
+    component: 'DataAnalyseInitializer',
+  });
+  return (
+    <SettingsCenterProvider
+      settings={{
+        'data-analyse': {
+          title: 'data-analyse',
+          icon: 'ApiOutlined',
+          tabs: {
+            tab1: {
+              title: 'data analyse tab',
+              component: () => <Card bordered={false}>Hello Settings</Card>,
+            },
+          },
+        },
+      }}
+    >
+      <SchemaComponentOptions components={{ DataAnalyseInitializer }}>
+        <SchemaInitializerContext.Provider value={items}>{props.children}</SchemaInitializerContext.Provider>
+      </SchemaComponentOptions>
+    </SettingsCenterProvider>
+  );
 });
