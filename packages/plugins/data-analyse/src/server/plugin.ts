1bc94f63fbff6787ad148e3f720d2a9c69a8a692
Frozen FIsh <976499226@qq.com>
2022-12-28 15:44:20 +0800

    chore: init data analyse plugin

---
+++ packages/plugins/data-analyse/src/server/plugin.ts
@@ -0,0 +1,19 @@
+import { InstallOptions, Plugin } from '@nocobase/server';
+
+export class DataAnalysePlugin extends Plugin {
+  afterAdd() {}
+
+  beforeLoad() {}
+
+  async load() {}
+
+  async install(options?: InstallOptions) {}
+
+  async afterEnable() {}
+
+  async afterDisable() {}
+
+  async remove() {}
+}
+
+export default DataAnalysePlugin;
