6810f45803b077d9041c58f1d837788e730bf4c3
Frozen FIsh <976499226@qq.com>
2022-12-28 18:02:15 +0800

    chore: export schema initializers

--- packages/core/client/src/schema-initializer/index.ts
+++ packages/core/client/src/schema-initializer/index.ts
@@ -1,5 +1,4 @@
 export * from './SchemaInitializer';
 export * from './SchemaInitializerProvider';
 export * from './types';
-export * from './items';
-export { gridRowColWrap, useRecordCollectionDataSourceItems } from './utils';
+export * from './items'
