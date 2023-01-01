a8900b637d3ceb684d143ada40c2c35db0f4b1da
Frozen FIsh <976499226@qq.com>
2023-01-01 16:31:33 +0800

    chore: refactor

--- packages/plugins/charts/src/client/ChartBlockInitializer.tsx
+++ packages/plugins/charts/src/client/ChartBlockInitializer.tsx
@@ -6,6 +6,7 @@ import {useTranslation} from 'react-i18next';
 import {SchemaComponent, SchemaComponentOptions, useAPIClient, useCollectionManager} from "@nocobase/client";
 import {DataBlockInitializer} from '@nocobase/client'
 import {useSaveRoleResourceAction} from "@nocobase/client/src/acl/Configuration/schemas/useSaveRoleResourceAction";
+import PieSchemaTemplate from "./PieSchemaTemplate";
 
 export const ChartBlockInitializer = (props) => {
   const {insert} = props;
@@ -19,6 +20,9 @@ export const ChartBlockInitializer = (props) => {
       componentType={'Kanban'}
       icon={<FormOutlined/>}
       onCreateBlockSchema={async ({item}) => {
+        const collectionFields = getCollectionFields(item.name);
+        const stringFields = collectionFields
+        console.log(item)
         const values = await FormDialog(t('Create chart block'), () => {
           return (
             <SchemaComponentOptions scope={options.scope} components={{...options.components}}>
@@ -30,138 +34,15 @@ export const ChartBlockInitializer = (props) => {
                         title: t('Chart type'),
                         required: true,
                         'x-component': 'Select',
-                        'x-component-props': {
-                          objectValue: true,
-                          fieldNames: {chartType: 'chartType'},
-                        },
                         'x-decorator': 'FormItem',
-                        enum: [{chartType: 'Pie', value: 'Pie'},{chartType: 'Column', value: 'Column'},{chartType:'Statistic',value:'Statistic'}],
+                        enum: [
+                          {label: 'Pie', value: 'Pie'},
+                          {label: 'Column', value: 'Column'},
+                          {label: 'Statistic', value: 'Statistic'}
+                        ],
                       },
-                      tabs1: {
-                        type: 'void',
-                        'x-component': 'Tabs',
-                        'x-component-props': {
-                          style: {
-                            marginTop: -15,
-                          },
-                        },
-                        properties: {
-                          dataset: {
-                            type: 'object',
-                            title: 'Dataset options',
-                            'x-component': 'Tabs.TabPane',
-                            'x-component-props': {
-                              tab: 'Dataset options',
-                            },
-                            properties: {
-                              type: {
-                                title: t('Type'),
-                                required: true,
-                                'x-component': 'Select',
-                                'x-decorator': 'FormItem',
-                                default: 'builtIn',
-                                enum: [
-                                  {label: 'Built-in', value: 'builtIn'},
-                                  {label: 'SQL', value: 'sql'},
-                                  {label: 'API', value: 'api'},
-                                ],
-                              },
-                              sql: {
-                                title: t('SQL'),
-                                'x-component': 'Input.TextArea',
-                                'x-decorator': 'FormItem',
-                                'x-reactions': {
-                                  dependencies: ['dataset.type'],
-                                  fulfill: {
-                                    state: {
-                                      visible: '{{$deps[0] === "sql"}}',
-                                    },
-                                  },
-                                },
-                              },
-                              api: {
-                                title: t('API'),
-                                'x-component': 'Input',
-                                'x-decorator': 'FormItem',
-                                'x-reactions': {
-                                  dependencies: ['dataset.type'],
-                                  fulfill: {
-                                    state: {
-                                      visible: '{{$deps[0] === "api"}}',
-                                    },
-                                  },
-                                },
-                              },
-                              aggregateFunction: {
-                                title: t('Aggregate function'),
-                                required: true,
-                                'x-component': 'Radio.Group',
-                                'x-decorator': 'FormItem',
-                                enum: [
-                                  {label: 'SUM', value: 'SUM'},
-                                  {label: 'COUNT', value: 'COUNT'},
-                                  {label: 'AVG', value: 'AVG'},
-                                ],
-                                'x-reactions': {
-                                  dependencies: ['dataset.type'],
-                                  fulfill: {
-                                    state: {
-                                      visible: '{{$deps[0] === "builtIn"}}',
-                                    },
-                                  },
-                                },
-                              },
-                              computedField: {
-                                title: t('Computed field'),
-                                required: true,
-                                'x-component': 'Select',
-                                'x-component-props': {
-                                  objectValue: true,
-                                  fieldNames: {label: 'label', value: 'value'},
-                                },
-                                'x-decorator': 'FormItem',
-                                enum: [{label: 'Statistic', value: 'statistic'}],
-                                'x-reactions': {
-                                  dependencies: ['dataset.type'],
-                                  fulfill: {
-                                    state: {
-                                      visible: '{{$deps[0] === "builtIn"}}',
-                                    },
-                                  },
-                                },
-                              },
-                              filter: {
-                                title: t('Filter'),
-                                'x-component': 'Filter',
-                                'x-decorator': 'FormItem',
-                                'x-reactions': {
-                                  dependencies: ['dataset.type'],
-                                  fulfill: {
-                                    state: {
-                                      visible: '{{$deps[0] === "builtIn"}}',
-                                    },
-                                  },
-                                },
-                              },
-                            },
-                          },
-                          chart: {
-                            type: 'object',
-                            title: 'Chart options',
-                            'x-component': 'Tabs.TabPane',
-                            'x-component-props': {
-                              tab: 'Chart options',
-                            },
-                            properties: {
-                              title: {
-                                title: t('Title'),
-                                'x-component': 'Input',
-                                'x-decorator': 'FormItem',
-                              },
-                            },
-                          },
-                        },
-                      },
+                      //   template
+                      ...PieSchemaTemplate()
                     },
                   }}
                 />
