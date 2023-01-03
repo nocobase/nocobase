7080e93d86e1f06d6c6f4e57d392bb07822ee81b
anuoua <anuoua@gmail.com>
2023-01-03 20:45:17 +0800

    feat: snapshot snapshot association field fix

--- packages/plugins/snapshot-field/src/client/SnapshotHistoryCollectionProvider.tsx
+++ packages/plugins/snapshot-field/src/client/SnapshotHistoryCollectionProvider.tsx
@@ -4,16 +4,29 @@ import React, { useContext } from 'react';
 export const SnapshotHistoryCollectionProvider: React.FC<{ collectionName: string }> = (props) => {
   const { collectionName } = props;
   const { collections: allCollections, ...rest } = useContext(CollectionManagerContext);
-  const historyCollection = useHistoryCollectionsByNames([collectionName])?.[0];
-  const inheritCollections = useHistoryCollectionsByNames(historyCollection.inherits ?? []);
 
-  const finallyHistoryCollecionts = [historyCollection, ...inheritCollections].filter((i) => i);
+  // 目标表
+  const snapshotTargetCollection = useHistoryCollectionsByNames([collectionName])?.[0];
+  // 目标如果是继承表则获取继承表
+  const inheritCollections = useHistoryCollectionsByNames(snapshotTargetCollection?.inherits ?? []);
+  // 目标表内关联字段的表
+  const associationFieldTargetCollections = useHistoryCollectionsByNames(
+    snapshotTargetCollection?.fields.filter((i) => i.interface !== 'snapshot').map((i) => i.target) ?? [],
+  );
 
+  // 替换表的集合
+  const finallyHistoryCollecionts = [
+    snapshotTargetCollection,
+    ...associationFieldTargetCollections,
+    ...inheritCollections,
+  ].filter((i) => i);
+
+  // 过滤出不需要替换的表
   const filterdAllCollection = allCollections.filter(
     (c) => !finallyHistoryCollecionts.map((i) => i.name).includes(c.name),
   );
 
-  // 用历史 collection 替换当前的 collection
+  // 最终替换后的表
   const overridedCollections = [...filterdAllCollection, ...finallyHistoryCollecionts];
 
   return (
