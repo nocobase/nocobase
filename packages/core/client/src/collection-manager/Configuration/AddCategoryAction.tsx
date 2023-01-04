83084a985ec84241012474a3c941397d10cbdb04
katherinehhh <katherine_15995@163.com>
2023-01-04 21:37:13 +0800

    feat: collection category

--- packages/core/client/src/collection-manager/Configuration/AddCategoryAction.tsx
+++ packages/core/client/src/collection-manager/Configuration/AddCategoryAction.tsx
@@ -1,122 +1,22 @@
-import { DownOutlined, PlusOutlined } from '@ant-design/icons';
-import { ISchema, useForm } from '@formily/react';
-import { uid } from '@formily/shared';
+import { PlusOutlined } from '@ant-design/icons';
+import { useForm } from '@formily/react';
 import { Button } from 'antd';
 import { cloneDeep } from 'lodash';
 import React, { useState, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useAPIClient } from '../../api-client';
 import { RecordProvider, useRecord } from '../../record-provider';
-import { ActionContext, SchemaComponent, useActionContext, useCompile } from '../../schema-component';
+import { ActionContext, SchemaComponent, useActionContext } from '../../schema-component';
 import { useCancelAction } from '../action-hooks';
-import { useCollectionManager } from '../hooks';
-import { useResourceActionContext, useResourceContext } from '../ResourceActionProvider';
 import * as components from './components';
 import { collectionCategorySchema } from './schemas/collections';
 import { CollectionCategroriesContext } from '../context';
 
-const useDefaultCollectionFields = (values) => {
-  let defaults = values.fields ? [...values.fields] : [];
-  const { autoGenId = true, createdAt = true, createdBy = true, updatedAt = true, updatedBy = true } = values;
-  if (autoGenId) {
-    const pk = values.fields.find((f) => f.primaryKey);
-    if (!pk) {
-      defaults.push({
-        name: 'id',
-        type: 'bigInt',
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false,
-        uiSchema: { type: 'number', title: '{{t("ID")}}', 'x-component': 'InputNumber', 'x-read-pretty': true },
-        interface: 'id',
-      });
-    }
-  }
-  if (createdAt) {
-    defaults.push({
-      name: 'createdAt',
-      interface: 'createdAt',
-      type: 'date',
-      field: 'createdAt',
-      uiSchema: {
-        type: 'datetime',
-        title: '{{t("Created at")}}',
-        'x-component': 'DatePicker',
-        'x-component-props': {},
-        'x-read-pretty': true,
-      },
-    });
-  }
-  if (createdBy) {
-    defaults.push({
-      name: 'createdBy',
-      interface: 'createdBy',
-      type: 'belongsTo',
-      target: 'users',
-      foreignKey: 'createdById',
-      uiSchema: {
-        type: 'object',
-        title: '{{t("Created by")}}',
-        'x-component': 'RecordPicker',
-        'x-component-props': {
-          fieldNames: {
-            value: 'id',
-            label: 'nickname',
-          },
-        },
-        'x-read-pretty': true,
-      },
-    });
-  }
-  if (updatedAt) {
-    defaults.push({
-      type: 'date',
-      field: 'updatedAt',
-      name: 'updatedAt',
-      interface: 'updatedAt',
-      uiSchema: {
-        type: 'string',
-        title: '{{t("Last updated at")}}',
-        'x-component': 'DatePicker',
-        'x-component-props': {},
-        'x-read-pretty': true,
-      },
-    });
-  }
-  if (updatedBy) {
-    defaults.push({
-      type: 'belongsTo',
-      target: 'users',
-      foreignKey: 'updatedById',
-      name: 'updatedBy',
-      interface: 'updatedBy',
-      uiSchema: {
-        type: 'object',
-        title: '{{t("Last updated by")}}',
-        'x-component': 'RecordPicker',
-        'x-component-props': {
-          fieldNames: {
-            value: 'id',
-            label: 'nickname',
-          },
-        },
-        'x-read-pretty': true,
-      },
-    });
-  }
-  // 其他
-  return defaults;
-};
-
 const useCreateCategry = () => {
   const form = useForm();
-  const { refreshCM } = useCollectionManager();
   const ctx = useActionContext();
-  const { refresh } = useResourceActionContext();
   const CategroriesCtx = useContext(CollectionCategroriesContext);
-  console.log(CategroriesCtx)
   const api = useAPIClient();
-
   return {
     async run() {
       await form.submit();
@@ -128,7 +28,6 @@ const useCreateCategry = () => {
       });
       ctx.setVisible(false);
       await form.reset();
-      // refresh();
       await CategroriesCtx.refresh();
     },
   };
@@ -140,7 +39,7 @@ export const AddCategory = (props) => {
 };
 
 export const AddCategoryAction = (props) => {
-  const { scope, getContainer, item: record, children, trigger, align } = props;
+  const { scope, getContainer, item: record, children } = props;
   const [visible, setVisible] = useState(false);
   const { t } = useTranslation();
   return (
