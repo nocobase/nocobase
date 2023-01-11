3ea7e5b8d8bd1f1b7f9d3b3cfc8ad47cdc9d4c41
chenos <chenlinxh@gmail.com>
2023-01-11 11:58:47 +0800

    feat: change license (#1350)

---
+++ packages/plugins/graph-collection-manager/src/client/components/EditFieldAction.tsx
@@ -0,0 +1,29 @@
+import { EditOutlined } from '@ant-design/icons';
+import { EditFieldAction as EditCollectionFieldAction } from '@nocobase/client';
+import React from 'react';
+import { useCancelAction, useUpdateFieldAction } from '../action-hooks';
+
+const useUpdateCollectionField = (record) => {
+  const collectionName = record.collectionName;
+  const { run } = useUpdateFieldAction({collectionName, name:record.name,key:record.key});
+  return {
+    async run() {
+      await run();
+    },
+  };
+};
+
+export const EditFieldAction = ({ item: record }) => {
+  return (
+    <EditCollectionFieldAction
+      item={record}
+      scope={{
+        useCancelAction,
+        useUpdateCollectionField: () => useUpdateCollectionField(record),
+      }}
+      getContainer={() => document.getElementById('graph_container')}
+    >
+      <EditOutlined className="btn-edit" />
+    </EditCollectionFieldAction>
+  );
+};
