f9cc0fee8b4a816366e447cb81d58bba58ec4594
Junyi <mytharcher@users.noreply.github.com>
2023-01-12 18:22:06 +0800

    refactor(plugin-workflow): refactor request instruction (#1356)

--- packages/plugins/workflow/src/server/instructions/request.ts
+++ packages/plugins/workflow/src/server/instructions/request.ts
@@ -1,12 +1,13 @@
-import { JOB_STATUS } from '../constants';
 import { render } from 'ejs';
 import axios, { AxiosRequestConfig } from 'axios';
 import _ from 'lodash';
+
 import { Instruction } from './index';
+import { JOB_STATUS } from '../constants';
 
 export interface Header {
   name: string;
-  value: any;
+  value: string;
 }
 
 export type RequestConfig = Pick<AxiosRequestConfig, 'url' | 'method' | 'data' | 'timeout'> & {
@@ -14,24 +15,15 @@ export type RequestConfig = Pick<AxiosRequestConfig, 'url' | 'method' | 'data' |
   ignoreFail: boolean;
 };
 
-async function renderData(templateStr: string, vars: any): Promise<string> {
-  if (_.isEmpty(templateStr)) {
-    return '';
-  }
-  return render(templateStr, vars, { async: true });
-}
-
-async function triggerResume(plugin, job, status, result) {
-  job.set('status', status);
-  job.set('result', result);
-  return plugin.resume(job);
-}
-
 export default class implements Instruction {
-  async run(node, input, processor) {
+  constructor(public plugin) {}
+
+  request = async (node, job, processor) => {
     const templateVars = {
       node: processor.jobsMapByNodeId,
       ctx: processor.execution.context,
+      $jobsMapByNodeId: processor.jobsMapByNodeId,
+      $context: processor.execution.context,
     };
     const requestConfig = node.config as RequestConfig;
 
@@ -39,48 +31,54 @@ export default class implements Instruction {
     const headers = {
       'Content-Type': 'application/json',
     };
-    const { headers: headerArr = [], ignoreFail = false, method = 'POST', timeout = 5000 } = requestConfig;
+    const { headers: headerArr = [], method = 'POST', timeout = 5000 } = requestConfig;
     headerArr.forEach((header) => (headers[header.name] = header.value));
 
     let url, data;
     try {
-      url = await renderData(requestConfig.url, templateVars);
-    } catch (e) {
-      console.warn(e);
-      throw new Error(`ejs can't render url, please check url format！${(e as Error)?.message}`);
-    }
-    try {
-      data = await renderData(requestConfig.data, templateVars);
-    } catch (e) {
-      console.warn(e);
-      throw new Error(`ejs can't render request data, please check request data format！${(e as Error)?.message}`);
+      url = await render(requestConfig.url!.trim(), templateVars, { async: true });
+      data = requestConfig.data ? await render(requestConfig.data.trim(), templateVars, { async: true }) : undefined;
+
+    } catch (error2) {
+      // console.error(error2);
+      job.set({
+        status: JOB_STATUS.REJECTED,
+        result: error2.message
+      });
     }
 
+    try {
+      const response = await axios.request({
+        url,
+        method,
+        headers,
+        data,
+        timeout,
+      });
+      job.set({
+        status: JOB_STATUS.RESOLVED,
+        result: response.data
+      });
+    } catch (error1) {
+      // console.error('axios error?', error1);
+      job.set({
+        status: JOB_STATUS.REJECTED,
+        result: error1.isAxiosError ? error1.toJSON() : error1.message
+      });
+    }
+
+    return this.plugin.resume(job);
+  };
+
+  async run(node, input, processor) {
     const job = await processor.saveJob({
       status: JOB_STATUS.PENDING,
       nodeId: node.id,
     });
 
-    const plugin = processor.options.plugin;
-    axios
-      .request({
-        method,
-        timeout,
-        headers,
-        url,
-        data,
-      })
-      .then((resp) => {
-        if (resp.status >= 200 && resp.status < 300) {
-          triggerResume(plugin, job, JOB_STATUS.RESOLVED, resp.data);
-        } else {
-          triggerResume(plugin, job, JOB_STATUS.REJECTED, resp);
-        }
-      })
-      .catch((e) => {
-        console.warn(e);
-        triggerResume(plugin, job, JOB_STATUS.REJECTED, (e as Error)?.message);
-      });
+    setTimeout(() => {
+      this.request(node, job, processor);
+    });
 
     return job;
   }
