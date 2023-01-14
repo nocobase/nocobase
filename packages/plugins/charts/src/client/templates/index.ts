369d831e53fee9e6154634cbc5e4f6a9cb219844
Frozen FIsh <976499226@qq.com>
2023-01-14 13:26:09 +0800

    chore: update templates

--- packages/plugins/charts/src/client/templates/index.ts
+++ packages/plugins/charts/src/client/templates/index.ts
@@ -1,7 +1,7 @@
-import { lineTemplate } from "./Line";
 import { pieTemplate } from "./PieTemplate";
+import { barTemplate } from "./BarTemplate";
 
 export const templates = new Map();
 
 templates.set('Pie', pieTemplate);
-templates.set('Line', lineTemplate);
+templates.set('Bar', barTemplate);
