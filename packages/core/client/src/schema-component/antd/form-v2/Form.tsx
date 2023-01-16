c05c1a97252aa149a43d0001be2f27a2391fe6bc
chenos <chenlinxh@gmail.com>
2023-01-16 21:30:52 +0800

    fix: form-v2 disabled

--- packages/core/client/src/schema-component/antd/form-v2/Form.tsx
+++ packages/core/client/src/schema-component/antd/form-v2/Form.tsx
@@ -2,9 +2,8 @@ import { FormLayout } from '@formily/antd';
 import { createForm, Field, onFormInputChange } from '@formily/core';
 import { FieldContext, FormContext, observer, RecursionField, useField, useFieldSchema } from '@formily/react';
 import { uid } from '@formily/shared';
-import { Spin } from 'antd';
-import DisabledContext, { DisabledContextProvider } from 'antd/es/config-provider/DisabledContext';
-import React, { useEffect, useMemo, useContext } from 'react';
+import { ConfigProvider, Spin } from 'antd';
+import React, { useEffect, useMemo } from 'react';
 import { useActionContext } from '..';
 import { useAttach, useComponent } from '../..';
 import { useProps } from '../../hooks/useProps';
@@ -33,28 +32,25 @@ const FormComponent: React.FC<FormProps> = (props) => {
 const Def = (props: any) => props.children;
 
 const FormDecorator: React.FC<FormProps> = (props) => {
-  const contextDisabled = useContext(DisabledContext);
-  const { form, children, disabled = contextDisabled, ...others } = props;
+  const { form, children, disabled, ...others } = props;
   const field = useField();
   const fieldSchema = useFieldSchema();
   // TODO: component 里 useField 会与当前 field 存在偏差
   const f = useAttach(form.createVoidField({ ...field.props, basePath: '' }));
   const Component = useComponent(fieldSchema['x-component'], Def);
   return (
-    <DisabledContextProvider disabled={disabled}>
-      <FieldContext.Provider value={undefined}>
-        <FormContext.Provider value={form}>
-          <FormLayout layout={'vertical'} {...others}>
-            <FieldContext.Provider value={f}>
-              <Component {...field.componentProps}>
-                <RecursionField basePath={f.address} schema={fieldSchema} onlyRenderProperties />
-              </Component>
-            </FieldContext.Provider>
-            {/* <FieldContext.Provider value={f}>{children}</FieldContext.Provider> */}
-          </FormLayout>
-        </FormContext.Provider>
-      </FieldContext.Provider>
-    </DisabledContextProvider>
+    <FieldContext.Provider value={undefined}>
+      <FormContext.Provider value={form}>
+        <FormLayout layout={'vertical'} {...others}>
+          <FieldContext.Provider value={f}>
+            <Component {...field.componentProps}>
+              <RecursionField basePath={f.address} schema={fieldSchema} onlyRenderProperties />
+            </Component>
+          </FieldContext.Provider>
+          {/* <FieldContext.Provider value={f}>{children}</FieldContext.Provider> */}
+        </FormLayout>
+      </FormContext.Provider>
+    </FieldContext.Provider>
   );
 };
 
@@ -69,6 +65,7 @@ const WithForm = (props) => {
         setFormValueChanged?.(true);
       });
     });
+    form.disabled = props.disabled;
     return () => {
       form.removeEffects(id);
     };
@@ -82,6 +79,7 @@ const WithoutForm = (props) => {
   const form = useMemo(
     () =>
       createForm({
+        disabled: props.disabled,
         effects() {
           onFormInputChange((form) => {
             setFormValueChanged?.(true);
@@ -99,12 +97,19 @@ const WithoutForm = (props) => {
 
 export const Form: React.FC<FormProps> & { Designer?: any; ReadPrettyDesigner?: any } = observer((props) => {
   const field = useField<Field>();
-  const { form, ...others } = useProps(props);
+  const { form, disabled, ...others } = useProps(props);
+  const formDisabled = disabled || field.disabled;
   return (
-    <form>
-      <Spin spinning={field.loading || false}>
-        {form ? <WithForm form={form} {...others} /> : <WithoutForm {...others} />}
-      </Spin>
-    </form>
+    <ConfigProvider componentDisabled={formDisabled}>
+      <form>
+        <Spin spinning={field.loading || false}>
+          {form ? (
+            <WithForm form={form} {...others} disabled={formDisabled} />
+          ) : (
+            <WithoutForm {...others} disabled={formDisabled} />
+          )}
+        </Spin>
+      </form>
+    </ConfigProvider>
   );
 });
