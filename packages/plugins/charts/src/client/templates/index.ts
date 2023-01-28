88c5c383148e518ff79089552e50cbbdc1eab85f
Frozen FIsh <976499226@qq.com>
2023-01-28 18:46:05 +0800

    feat: add line chart template

--- packages/plugins/charts/src/client/templates/index.ts
+++ packages/plugins/charts/src/client/templates/index.ts
@@ -1,9 +1,11 @@
 import { pieTemplate } from './PieTemplate';
 import { barTemplate } from './BarTemplate';
 import { columnTemplate } from './ColumnTemplate';
+import { lineTemplate } from './LineTemplate';
 
 export const templates = new Map();
 
 templates.set('Pie', pieTemplate);
 templates.set('Bar', barTemplate);
 templates.set('Column', columnTemplate);
+templates.set('Line', lineTemplate);
