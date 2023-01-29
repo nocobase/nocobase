83a0bfb832828e7ba3a138dfbda282d3abd33c80
Frozen FIsh <976499226@qq.com>
2023-01-29 12:49:05 +0800

    fix: export missing utils

--- packages/core/client/src/schema-initializer/index.ts
+++ packages/core/client/src/schema-initializer/index.ts
@@ -2,4 +2,4 @@ export * from './SchemaInitializer';
 export * from './SchemaInitializerProvider';
 export * from './types';
 export * from './items';
-export { gridRowColWrap, useRecordCollectionDataSourceItems } from './utils';
+export * from './utils';
