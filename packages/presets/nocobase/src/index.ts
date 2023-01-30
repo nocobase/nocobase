5f609cec88e48e78024912af556ba28dc886a26b
chenos <chenlinxh@gmail.com>
2023-01-30 10:31:01 +0800

    fix: upgrade error

--- packages/presets/nocobase/src/index.ts
+++ packages/presets/nocobase/src/index.ts
@@ -55,15 +55,15 @@ export class PresetNocoBase extends Plugin {
       }
       const version = await this.app.version.get();
       console.log(`The version number before upgrade is ${version}`);
-      const result = await this.app.version.satisfies('<0.8.0-alpha.1');
-      if (result) {
-        const r = await this.db.collectionExistsInDb('applicationPlugins');
-        if (r) {
-          console.log(`Clear the installed application plugins`);
-          await this.db.getRepository('applicationPlugins').destroy({ truncate: true });
-          await this.app.reload({ method: options.method });
-        }
-      }
+      // const result = await this.app.version.satisfies('<0.8.0-alpha.1');
+      // if (result) {
+      //   const r = await this.db.collectionExistsInDb('applicationPlugins');
+      //   if (r) {
+      //     console.log(`Clear the installed application plugins`);
+      //     await this.db.getRepository('applicationPlugins').destroy({ truncate: true });
+      //     await this.app.reload({ method: options.method });
+      //   }
+      // }
     });
     this.app.on('beforeUpgrade', async (options) => {
       const result = await this.app.version.satisfies('<0.8.0-alpha.1');
