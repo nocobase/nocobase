f126c5185a53996e3da1c6e22a583ad77e742b46
mytharcher <mytharcher@gmail.com>
2023-02-04 10:34:00 +0800

    fix(plugin-workflow): fix functions init

--- packages/plugins/workflow/src/server/functions/index.ts
+++ packages/plugins/workflow/src/server/functions/index.ts
@@ -2,7 +2,7 @@ import Plugin from "..";
 
 
 
-export default function({ functions }: Plugin, more: { [key: string]: Function }) {
+export default function({ functions }: Plugin, more: { [key: string]: Function } = {}) {
   functions.register('now', () => new Date());
 
   for (const [name, fn] of Object.entries(more)) {
