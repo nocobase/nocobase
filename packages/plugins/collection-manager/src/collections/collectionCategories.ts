bc695616262c5a5476e0d104921afed170846ea7
chenos <chenlinxh@gmail.com>
2023-02-09 20:52:19 +0800

    fix: collectionCategory

--- packages/plugins/collection-manager/src/collections/collectionCategories.ts
+++ packages/plugins/collection-manager/src/collections/collectionCategories.ts
@@ -25,6 +25,7 @@ export default {
       target: 'collections',
       foreignKey: 'collectionCategoryId',
       targetKey: 'name',
+      through: 'collectionCategory',
     },
   ],
 } as CollectionOptions;
