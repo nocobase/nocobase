4449325489a3aa9c919cbf8df78e57f446b1e6c4
Chareice <chareice@live.com>
2023-02-09 13:01:25 +0800

    fix: pg schema in sync runner

--- packages/plugins/collection-manager/src/__tests__/inherits/inherited-collection.test.ts
+++ packages/plugins/collection-manager/src/__tests__/inherits/inherited-collection.test.ts
@@ -12,6 +12,56 @@ pgOnly()('Inherited Collection', () => {
   let fieldsRepository: Repository;
 
   beforeEach(async () => {
+    app = await createApp({
+      database: {
+        schema: 'testSchema',
+      },
+    });
+
+    db = app.db;
+
+    collectionRepository = db.getCollection('collections').repository;
+    fieldsRepository = db.getCollection('fields').repository;
+  });
+
+  afterEach(async () => {
+    await app.destroy();
+  });
+
+  it('should create inherited collection in difference schema', async () => {
+    await collectionRepository.create({
+      values: {
+        name: 'b',
+        fields: [
+          {
+            name: 'name',
+            type: 'string',
+          },
+        ],
+      },
+      context: {},
+    });
+
+    await collectionRepository.create({
+      values: {
+        name: 'a',
+        inherits: ['b'],
+        fields: [{ type: 'string', name: 'bField' }],
+      },
+      context: {},
+    });
+  });
+});
+
+pgOnly()('Inherited Collection', () => {
+  let db: Database;
+  let app: Application;
+
+  let collectionRepository: Repository;
+
+  let fieldsRepository: Repository;
+
+  beforeEach(async () => {
     app = await createApp();
 
     db = app.db;
