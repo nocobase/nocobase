3e5f11d67e25f45faec910870d44cc8fefcd76dc
sudongyuer <976499226@qq.com>
2023-02-14 23:19:22 +0800

    chore: init plugin

--- packages/plugins/dataset/src/server/plugin.ts
+++ packages/plugins/dataset/src/server/plugin.ts
@@ -1,19 +1,50 @@
 import { InstallOptions, Plugin } from '@nocobase/server';
 
 export class DatasetPlugin extends Plugin {
-  afterAdd() {}
+  afterAdd() {
+  }
 
-  beforeLoad() {}
+  beforeLoad() {
+  }
 
-  async load() {}
+  async load() {
+    // create dataset collection
+    this.db.collection({
+      'name': 'datasets',
+      fields: [
+        {
+          name: 'data_set_id',
+          type: 'string',
+        },
+        {
+          name: 'data_set_type',
+          type: 'string',
+        },
+        {
+          name: 'data_set_value',
+          type: 'text',
+        },
+      ],
+    });
+    // sync database
+    await this.db.sync();
+    this.app.acl.allow('products', '*');
+    this.app.acl.allow('categories', '*');
+    this.app.acl.allow('users', '*');
+    this.app.acl.allow('datasets', '*');
+  }
 
-  async install(options?: InstallOptions) {}
+  async install(options?: InstallOptions) {
+  }
 
-  async afterEnable() {}
+  async afterEnable() {
+  }
 
-  async afterDisable() {}
+  async afterDisable() {
+  }
 
-  async remove() {}
+  async remove() {
+  }
 }
 
 export default DatasetPlugin;
