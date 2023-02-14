7a2f72bac617453403a970a764ce500e2177d036
sudongyuer <976499226@qq.com>
2023-02-14 23:18:35 +0800

    chore: update utils

--- packages/plugins/dataset/src/client/utils.ts
+++ packages/plugins/dataset/src/client/utils.ts
@@ -1,21 +1,39 @@
-const validateArray = `{{(value)=> {
-        try {
-          value = JSON.parse(value)
-        }catch (e){
-          return 'Please input array'
-        }
-        if(Array.isArray(value)) {
-          return true;
-        }
-          return 'Please input array'
-      }}}`;
+import JSON5 from 'json5';
+import { uid } from '@formily/shared';
 
-const parseNonStandardJsonStringToJsonObj = (str: string) => {
-
-  return '';
+const validateArray = (value) => {
+  try {
+    value = JSON5.parse(value);
+  } catch (e) {
+    return 'Please input validate dataset';
+  }
+  if (Array.isArray(value)) {
+    if (value.every((item) => {
+      return typeof item === 'object' && Object.keys(item).length > 1;
+    }))
+      return true;
+  }
+  return 'Please input validate dataset';
 };
+
+const parseDataSetString=(str)=>{
+  const dataSetDataArray = JSON5.parse(str);
+  if (Array.isArray(dataSetDataArray)) {
+    if (dataSetDataArray.every((item) => {
+      return typeof item === 'object' && Object.keys(item).length > 1;
+    }))
+      dataSetDataArray.map((item) => {
+        if (!item?.id) {
+          item.id = uid();
+        }
+        return item;
+      });
+  }
+  return dataSetDataArray
+}
+
 
 export {
   validateArray,
-  parseNonStandardJsonStringToJsonObj,
+  parseDataSetString
 };
