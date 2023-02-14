eac8d071f686d2f7b54ff31c43bae12ca8aa2c3d
sudongyuer <976499226@qq.com>
2023-02-14 23:18:35 +0800

    chore: update utils

--- packages/plugins/dataset/src/client/DataSetPreviewTable.tsx
+++ packages/plugins/dataset/src/client/DataSetPreviewTable.tsx
@@ -2,55 +2,47 @@ import { ISchema } from '@formily/react';
 import { Input, SchemaComponent, SchemaComponentProvider, Table } from '@nocobase/client';
 import React from 'react';
 
-const a= [{ id: 1, name: 'Name1' },
-  { id: 2, name: 'Name2' },
-  { id: 3, name: 'Name3' },
-]
-const schema: ISchema = {
-  type: 'object',
-  properties: {
-    input: {
-      type: 'array',
-      title: `编辑模式`,
-      'x-component': 'Table.Void',
-      'x-component-props': {
-        rowKey: 'id',
-        rowSelection: {
-          type: 'checkbox',
-        },
-        // dataSource: [
-        //   { id: 1, name: 'Name1' },
-        //   { id: 2, name: 'Name2' },
-        //   { id: 3, name: 'Name3' },
-        // ],
-        dataSource: '{{dataSet}}',
-      },
-      properties: {
-        column1: {
-          type: 'void',
-          title: 'Name',
-          'x-component': 'Table.Column',
-          properties: {
-            name: {
-              type: 'string',
-              'x-component': 'Input',
-              'x-read-pretty': true,
-            },
-          },
-        },
-      },
-    },
-  },
-};
-
 interface DataSetPreviewProps {
-  dataSet: null | [];
+  dataSet: null | object[];
 }
 
 export default ({ dataSet }: DataSetPreviewProps) => {
-  console.log(dataSet, '===========================');
+  const columns = {};
+  if (dataSet) {
+    const dataKeys = Object.keys(dataSet[0]);
+    for (const dataKey of dataKeys) {
+      columns[dataKey] = {
+        type: 'void',
+        title: dataKey,
+        'x-component': 'Table.Column',
+        properties: {
+          [dataKey]: {
+            type: 'string',
+            'x-component': 'Input',
+            'x-read-pretty': true,
+          },
+        },
+      };
+    }
+  }
+  const schema: ISchema = {
+    type: 'object',
+    properties: {
+      input: {
+        type: 'array',
+        title: `编辑模式`,
+        'x-component': 'Table.Void',
+        'x-component-props': {
+          rowKey: 'id',
+          dataSource: '{{dataSet}}',
+          scroll: { y: 300 },
+        },
+        properties: columns,
+      },
+    },
+  };
   return (
-    <SchemaComponentProvider scope={{dataSet}} components={{ Table, Input }}>
+    <SchemaComponentProvider scope={{ dataSet }} components={{ Table, Input }}>
       <SchemaComponent schema={schema} />
     </SchemaComponentProvider>
   );
