4c4aa75924e71135ecadaca2bb4382f24a77d6d6
Chareice <chareice@live.com>
2023-02-14 20:04:49 +0800

    feat: collection add schema table name method

--- packages/core/database/src/database.ts
+++ packages/core/database/src/database.ts
@@ -15,7 +15,7 @@ import {
   Sequelize,
   SyncOptions,
   Transactionable,
-  Utils
+  Utils,
 } from 'sequelize';
 import { SequelizeStorage, Umzug } from 'umzug';
 import { Collection, CollectionOptions, RepositoryType } from './collection';
@@ -58,7 +58,7 @@ import {
   SyncListener,
   UpdateListener,
   UpdateWithAssociationsListener,
-  ValidateListener
+  ValidateListener,
 } from './types';
 import { snakeCase } from './utils';
 
@@ -323,6 +323,10 @@ export class Database extends EventEmitter implements AsyncEmitter {
             return index;
           });
         }
+      }
+
+      if (this.options.schema && !options.schema) {
+        options.schema = this.options.schema;
       }
     });
   }
