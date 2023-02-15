5688c3b17cecd6ff0a8426c9a18ef7daa1b92f81
katherinehhh <katherine_15995@163.com>
2023-02-15 22:36:40 +0800

    fix(linakge rules): conditionAnalyse

--- packages/core/client/src/schema-component/common/utils/uitls.tsx
+++ packages/core/client/src/schema-component/common/utils/uitls.tsx
@@ -1,13 +1,41 @@
-import { every, some } from 'lodash';
+import { every, some, get } from 'lodash';
+import flat from 'flat';
 import jsonLogic from '../../common/utils/logic';
+
+function getDeepestProperty(obj) {
+  let deepest = {
+    operator: null,
+    value: null,
+  };
+  let deepestLevel = 0;
+  function traverse(obj, level) {
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        if (typeof obj[key] === 'object') {
+          traverse(obj[key], level + 1);
+        } else {
+          if (level > deepestLevel) {
+            deepestLevel = level;
+            deepest.operator = key;
+            deepest.value = obj[key];
+          }
+        }
+      }
+    }
+  }
+  traverse(obj, 1);
+  return deepest;
+}
+
 export const conditionAnalyse = (rules, values) => {
   const type = Object.keys(rules)[0] || '$and';
   const conditions = rules[type];
   const results = conditions.map((c) => {
-    const operator = jsonLogic.get_operator(jsonLogic.get_values(c));
-    const targetField = jsonLogic.get_operator(c);
-    const value = jsonLogic.get_values(c)[operator];
-    const result = jsonLogic.apply({ [operator]: [values?.[targetField], value] });
+    const jsonlogic = getDeepestProperty(c);
+    const operator = jsonlogic.operator;
+    const value = jsonlogic.value;
+    const targetField = Object.keys(flat(c))[0].replace(`.${operator}`, '');
+    const result = jsonLogic.apply({ [operator]: [flat(values)?.[targetField], value] });
     return result;
   });
   if (type === '$and') {
