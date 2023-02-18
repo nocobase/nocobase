328f2fda1e0fb225f55353c82e18170eb4c8d3f2
sudongyuer <976499226@qq.com>
2023-02-18 17:54:07 +0800

    chore: update template

--- packages/plugins/charts/src/client/templates/LineTemplate.tsx
+++ packages/plugins/charts/src/client/templates/LineTemplate.tsx
@@ -1,4 +1,4 @@
-import { i18n } from '@nocobase/client';
+import JSON5 from 'json5';
 
 const validateJSON = {
   validator: `{{(value, rule)=> {
@@ -6,7 +6,7 @@ const validateJSON = {
       return '';
     }
     try {
-      const val = JSON.parse(value);
+      const val = JSON5.parse(value);
       if(!isNaN(val)) {
         return false;
       }
@@ -20,184 +20,61 @@ const validateJSON = {
 };
 
 const chartConfig = {
-  padding: 'auto',
-  xField: 'type',
-  yField: 'value',
+  yField: '{{metric}}',
+  xField: '{{dimension}}',
+  seriesField: '{{category}}',
   xAxis: {
-    // type: 'timeCat',
-    tickCount: 5,
+    type: 'time',
+  },
+  yAxis: {
+    label: {
+      formatter: '{{(v) => `${v}`.replace(/\d{1,3}(?=(\d{3})+$)/g, (s) => `${s},`)}}',
+    },
   },
 };
-
 export const lineTemplate = {
-  title: 'Line',
+  title: 'Line(最少一个维度，最少一个度量)',
   type: 'Line',
   renderComponent: 'G2Plot',
-  defaultChartOptions: {
-    padding: 'auto',
-    xField: 'type',
-    yField: 'value',
-    xAxis: {
-      // type: 'timeCat',
-      tickCount: 5,
-    },
-  },
+  defaultChartOptions: chartConfig,
   configurableProperties: {
-    type: 'void',
-    'x-component': 'Tabs',
-    'x-component-props': {
-      style: {
-        marginTop: -15,
-      },
-    },
+    type: 'object',
     properties: {
-      dataset: {
-        type: 'object',
-        title: 'Dataset options',
-        'x-component': 'Tabs.TabPane',
-        'x-component-props': {
-          tab: 'Dataset options',
-        },
-        properties: {
-          type: {
-            title: '{{t(\'Type\')}}',
-            required: true,
-            'x-component': 'Select',
-            'x-decorator': 'FormItem',
-            default: 'builtIn',
-            enum: [
-              { label: 'Built-in', value: 'builtIn' },
-              { label: 'SQL', value: 'sql' },
-              { label: 'API', value: 'api' },
-            ],
-          },
-          sql: {
-            title: '{{t(\'SQL\')}}',
-            'x-component': 'Input.TextArea',
-            'x-decorator': 'FormItem',
-            'x-reactions': {
-              dependencies: ['dataset.type'],
-              fulfill: {
-                state: {
-                  visible: '{{$deps[0] === "sql"}}',
-                },
-              },
-            },
-          },
-          api: {
-            title: '{{t(\'API\')}}',
-            'x-component': 'Input',
-            'x-decorator': 'FormItem',
-            'x-reactions': {
-              dependencies: ['dataset.type'],
-              fulfill: {
-                state: {
-                  visible: '{{$deps[0] === "api"}}',
-                },
-              },
-            },
-          },
-          aggregateFunction: {
-            title: '{{t(\'Aggregate function\')}}',
-            required: true,
-            'x-component': 'Radio.Group',
-            'x-decorator': 'FormItem',
-            enum: [
-              { label: 'SUM', value: 'SUM' },
-              { label: 'COUNT', value: 'COUNT' },
-              { label: 'AVG', value: 'AVG' },
-            ],
-            'x-reactions': {
-              dependencies: ['dataset.type'],
-              fulfill: {
-                state: {
-                  visible: '{{$deps[0] === "builtIn"}}',
-                },
-              },
-            },
-          },
-          computedField: {
-            title: '{{t(\'Computed field\')}}',
-            required: true,
-            'x-component': 'Select',
-            'x-decorator': 'FormItem',
-            enum: '{{computedFields}}',
-            'x-reactions': {
-              dependencies: ['dataset.type', 'dataset.aggregateFunction'],
-              fulfill: {
-                state: {
-                  visible: '{{$deps[0] === "builtIn" && $deps[1] !== "COUNT"}}',
-                },
-              },
-            },
-          },
-          groupByField: {
-            title: '{{t(\'GroupBy field\')}}',
-            required: true,
-            'x-component': 'Select',
-            'x-decorator': 'FormItem',
-            enum: '{{groupByFields}}',
-            'x-reactions': {
-              dependencies: ['dataset.type'],
-              fulfill: {
-                state: {
-                  visible: '{{$deps[0] === "builtIn"}}',
-                },
-              },
-            },
-          },
-          /*filter: {
-            title: "{{t('Filter')}}",
-            'x-component': 'Filter',
-            'x-decorator': 'FormItem',
-            'x-component-props': {},
-            'x-reactions': {
-              dependencies: ['dataset.type'],
-              fulfill: {
-                state: {
-                  visible: '{{$deps[0] === "builtIn"}}',
-                },
-              },
-            },
-          },*/
-        },
+      dimension: {
+        required: true,
+        type: 'string',
+        title: '{{t("Sector label/dimensional")}}',
+        'x-decorator': 'FormItem',
+        'x-component': 'Select',
+        enum: '{{dataSource}}',
       },
-      chartOptions: {
-        type: 'object',
-        title: 'Chart options',
-        'x-component': 'Tabs.TabPane',
-        'x-component-props': {
-          tab: 'Chart options',
-        },
-        properties: {
-          title: {
-            title: '{{t(\'Title\')}}',
-            'x-component': 'Input',
-            'x-decorator': 'FormItem',
-          },
-        },
+      metric: {
+        required: true,
+        type: 'string',
+        title: '{{t("Sector Angle/Metric")}}',
+        'x-decorator': 'FormItem',
+        'x-component': 'Select',
+        enum: '{{dataSource}}',
       },
-      chartConfig: {
-        type: 'object',
-        title: 'Chart Config',
-        'x-component': 'Tabs.TabPane',
+      category: {
+        required: true,
+        type: 'string',
+        title: '{{t("Color legend/dimensional")}}',
+        'x-decorator': 'FormItem',
+        'x-component': 'Select',
+        enum: '{{dataSource}}',
+      },
+      advanceConfig: {
+        required: true,
+        title: '{{t("AdvanceConfig")}}',
+        type: 'string',
+        default: JSON5.stringify(chartConfig, null, 2),
+        'x-decorator': 'FormItem',
+        'x-component': 'Input.TextArea',
         'x-component-props': {
-          tab: 'Chart Config',
+          autoSize: { minRows: 8, maxRows: 16 },
         },
-        properties: {
-          config: {
-            required: true,
-            title: '{{t("Config")}}',
-            type: 'string',
-            default: JSON.stringify(chartConfig, null, 2),
-            'x-decorator': 'FormItem',
-            'x-component': 'Input.TextArea',
-            'x-component-props': {
-              autoSize: { minRows: 8, maxRows: 16 },
-            },
-            'x-validator': validateJSON,
-          },
-        },
+        'x-validator': validateJSON,
       },
     },
   },
--- packages/plugins/charts/src/client/templates/PieTemplate.tsx
+++ packages/plugins/charts/src/client/templates/PieTemplate.tsx
@@ -1,10 +1,12 @@
+import JSON5 from 'json5';
+
 const validateJSON = {
   validator: `{{(value, rule)=> {
     if (!value) {
       return '';
     }
     try {
-      const val = JSON.parse(value);
+      const val = JSON5.parse(value);
       if(!isNaN(val)) {
         return false;
       }
@@ -18,23 +20,23 @@ const validateJSON = {
 };
 
 const chartConfig = {
-    appendPadding: 10,
-    angleField: '{{metric}}',
-    colorField: '{{dimension}}',
-    radius: 0.9,
-    label: {
-      type: 'inner',
-      offset: '-30%',
-      content: '{{({percent}) => `${(percent * 100).toFixed(0)}%`}}',
-      style: {
-        fontSize: 14,
-        textAlign: 'center',
-      },
+  appendPadding: 10,
+  angleField: '{{metric}}',
+  colorField: '{{dimension}}',
+  radius: 0.9,
+  label: {
+    type: 'inner',
+    offset: '-30%',
+    content: '{{({percent}) => `${(percent * 100).toFixed(0)}%`}}',
+    style: {
+      fontSize: 14,
+      textAlign: 'center',
     },
-    interactions: [{ type: 'element-active' }],
-  };
+  },
+  interactions: [{ type: 'element-active' }],
+};
 export const pieTemplate = {
-  title: 'Pie(一个维度，最少一个指标,最多10个指标)',
+  title: 'Pie(一个维度，一个度量)',
   type: 'Pie',
   renderComponent: 'G2Plot',
   defaultChartOptions: chartConfig,
@@ -61,7 +63,7 @@ export const pieTemplate = {
         required: true,
         title: '{{t("AdvanceConfig")}}',
         type: 'string',
-        default: JSON.stringify(chartConfig, null, 2),
+        default: JSON5.stringify(chartConfig, null, 2),
         'x-decorator': 'FormItem',
         'x-component': 'Input.TextArea',
         'x-component-props': {
@@ -70,204 +72,5 @@ export const pieTemplate = {
         'x-validator': validateJSON,
       },
     },
-    /*properties: {
-      dataset: {
-        type: 'object',
-        title: 'Dataset options',
-        'x-component': 'Tabs.TabPane',
-        'x-component-props': {
-          tab: 'Dataset options',
-        },
-        properties: {
-          type: {
-            title: '{{t(\'Type\')}}',
-            required: true,
-            'x-component': 'Select',
-            'x-decorator': 'FormItem',
-            default: 'builtIn',
-            enum: [
-              { label: 'Built-in', value: 'builtIn' },
-              { label: 'SQL', value: 'sql' },
-              { label: 'API', value: 'api' },
-            ],
-          },
-          sql: {
-            title: '{{t(\'SQL\')}}',
-            'x-component': 'Input.TextArea',
-            'x-decorator': 'FormItem',
-            'x-reactions': {
-              dependencies: ['dataset.type'],
-              fulfill: {
-                state: {
-                  visible: '{{$deps[0] === "sql"}}',
-                },
-              },
-            },
-          },
-          api: {
-            title: '{{t(\'API\')}}',
-            'x-component': 'Input',
-            'x-decorator': 'FormItem',
-            'x-reactions': {
-              dependencies: ['dataset.type'],
-              fulfill: {
-                state: {
-                  visible: '{{$deps[0] === "api"}}',
-                },
-              },
-            },
-          },
-          aggregateFunction: {
-            title: '{{t(\'Aggregate function\')}}',
-            required: true,
-            'x-component': 'Radio.Group',
-            'x-decorator': 'FormItem',
-            enum: [
-              { label: 'SUM', value: 'SUM' },
-              { label: 'COUNT', value: 'COUNT' },
-              { label: 'AVG', value: 'AVG' },
-            ],
-            'x-reactions': {
-              dependencies: ['dataset.type'],
-              fulfill: {
-                state: {
-                  visible: '{{$deps[0] === "builtIn"}}',
-                },
-              },
-            },
-          },
-          computedField: {
-            title: '{{t(\'Computed field\')}}',
-            required: true,
-            'x-component': 'Select',
-            'x-decorator': 'FormItem',
-            enum: '{{computedFields}}',
-            'x-reactions': {
-              dependencies: ['dataset.type', 'dataset.aggregateFunction'],
-              fulfill: {
-                state: {
-                  visible: '{{$deps[0] === "builtIn" && $deps[1] !== "COUNT"}}',
-                },
-              },
-            },
-          },
-          groupByField: {
-            title: '{{t(\'GroupBy field\')}}',
-            required: true,
-            'x-component': 'Select',
-            'x-decorator': 'FormItem',
-            enum: '{{groupByFields}}',
-            'x-reactions': {
-              dependencies: ['dataset.type'],
-              fulfill: {
-                state: {
-                  visible: '{{$deps[0] === "builtIn"}}',
-                },
-              },
-            },
-          },
-          /!*filter: {
-            title: "{{t('Filter')}}",
-            'x-component': 'Filter',
-            'x-decorator': 'FormItem',
-            'x-component-props': {},
-            'x-reactions': {
-              dependencies: ['dataset.type'],
-              fulfill: {
-                state: {
-                  visible: '{{$deps[0] === "builtIn"}}',
-                },
-              },
-            },
-          },*!/
-        },
-      },
-      chartOptions: {
-        type: 'object',
-        // required: true,
-        title: 'Chart options',
-        'x-component': 'Tabs.TabPane',
-        'x-component-props': {
-          tab: 'Chart options',
-        },
-        properties: {
-          title: {
-            title: '{{t(\'Title\')}}',
-            'x-component': 'Input',
-            'x-decorator': 'FormItem',
-          },
-          /!*angleField: {
-            title: '{{t(\'angleField\')}}',
-            required: true,
-            'x-component': 'Select',
-            'x-decorator': 'FormItem',
-            'x-reactions': (field) => {
-              const computedField = field.query('dataset.computedField')?.value();
-              const groupByField = field.query('dataset.groupByField')?.value();
-              if (groupByField || computedField) {
-                field.dataSource = [
-                  {
-                    label: 'type',
-                    value: 'type',
-                  },
-                  {
-                    label: 'value',
-                    value: 'value',
-                  },
-                ];
-              } else {
-                field.dataSource = [];
-              }
-            },
-          },*!/
-          // colorField: {
-          //   title: '{{t(\'colorField\')}}',
-          //   required: true,
-          //   'x-component': 'Select',
-          //   'x-decorator': 'FormItem',
-          //   'x-reactions': (field) => {
-          //     const computedField = field.query('dataset.computedField')?.value();
-          //     const groupByField = field.query('dataset.groupByField')?.value();
-          //     if (groupByField || computedField) {
-          //       field.dataSource = [
-          //         {
-          //           label: 'type',
-          //           value: 'type',
-          //         },
-          //         {
-          //           label: 'value',
-          //           value: 'value',
-          //         },
-          //       ];
-          //     } else {
-          //       field.dataSource = [];
-          //     }
-          //   },
-          // },
-        },
-      },
-      chartConfig: {
-        type: 'object',
-        title: 'Chart Config',
-        'x-component': 'Tabs.TabPane',
-        'x-component-props': {
-          tab: 'Chart Config',
-        },
-        properties: {
-          config: {
-            required: true,
-            title: '{{t("Config")}}',
-            type: 'string',
-            default: JSON.stringify(chartConfig, null, 2),
-            'x-decorator': 'FormItem',
-            'x-component': 'Input.TextArea',
-            'x-component-props': {
-              autoSize: { minRows: 8, maxRows: 16 },
-            },
-            'x-validator': validateJSON,
-          },
-        },
-      },
-    },*/
   },
 };
