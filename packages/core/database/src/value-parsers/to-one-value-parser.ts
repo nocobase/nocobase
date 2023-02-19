6eb40e4582d9567e3a6d98ae8246673d24264861
chenos <chenlinxh@gmail.com>
2023-02-19 17:35:23 +0800

    feat: support for importing attachments (#1466)

--- packages/core/database/src/value-parsers/to-one-value-parser.ts
+++ packages/core/database/src/value-parsers/to-one-value-parser.ts
@@ -3,27 +3,18 @@ import { BaseValueParser } from './base-value-parser';
 
 export class ToOneValueParser extends BaseValueParser {
   async setValue(value: any) {
-    const fieldNames = this.getFileNames();
     const dataIndex = this.ctx?.column?.dataIndex || [];
     if (Array.isArray(dataIndex) && dataIndex.length < 2) {
+      this.errors.push(`data index invalid`);
       return;
     }
-    const field = this.ctx.column.dataIndex[1];
+    const key = this.ctx.column.dataIndex[1];
     const repository = this.field.database.getRepository(this.field.target) as Repository;
-    const instance = await repository.findOne({ filter: { [field]: value.trim() } });
+    const instance = await repository.findOne({ filter: { [key]: this.trim(value) } });
     if (instance) {
-      this.value = instance.get(fieldNames.value);
+      this.value = instance.get(this.field.targetKey || 'id');
     } else {
       this.errors.push(`"${value}" does not exist`);
     }
-  }
-
-  getFileNames() {
-    const fieldNames = this.field.options?.uiSchema?.['x-component-props']?.['fieldNames'] || {};
-    return { label: 'id', value: 'id', ...fieldNames };
-  }
-
-  isInterface(name) {
-    return this.field.options.interface === name;
   }
 }
