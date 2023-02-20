d11c14fb290f65225b9949d4bedf0b538c038048
mytharcher <mytharcher@gmail.com>
2023-02-20 23:00:35 +0800

    fix(plugin-formula): revert legacy component to fix build

--- packages/core/client/src/index.tsx
+++ packages/core/client/src/index.tsx
@@ -12,6 +12,7 @@ export * from './board';
 export * from './china-region';
 export * from './collection-manager';
 export * from './document-title';
+export * from './formula';
 export * from './i18n';
 export * from './icon';
 export * from './plugin-manager';
