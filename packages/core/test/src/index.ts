4b0f6510c781d10299827f5a0e66aea2d0b38e91
ChengLei Shao <chareice@live.com>
2023-02-23 20:14:50 +0800

    feat: prepare database method (#1492)

--- packages/core/test/src/index.ts
+++ packages/core/test/src/index.ts
@@ -3,3 +3,8 @@ export * from './mockServer';
 
 const pgOnly: () => jest.Describe = () => (process.env.DB_DIALECT == 'postgres' ? describe : describe.skip);
 export { pgOnly };
+
+export function randomStr() {
+  // create random string
+  return Math.random().toString(36).substring(2);
+}
