ab48260c05935a744fafd73d7c8dd2b3ca0edc79
chenos <chenlinxh@gmail.com>
2023-03-03 17:27:19 +0800

    feat: quick add query

--- packages/plugins/charts/src/client/ChartQueryBlockInitializer.tsx
+++ packages/plugins/charts/src/client/ChartQueryBlockInitializer.tsx
@@ -1,21 +1,9 @@
 import { TableOutlined } from '@ant-design/icons';
+import { FormDialog } from '@formily/antd';
 import { SchemaInitializer, SchemaInitializerButtonContext, useAPIClient } from '@nocobase/client';
 import React, { useContext, useEffect, useState } from 'react';
 import { useChartQueryMetadataContext } from './ChartQueryMetadataProvider';
 
-// {
-//   "id": 5,
-//   "title": "Demo1",
-//   "type": "json",
-//   "fields": [
-//   {
-//     "name": "Date"
-//   },
-//   {
-//     "name": "scales"
-//   }
-// ]
-// }
 export interface ChartQueryMetadata {
   id: number;
   title: string;
@@ -36,16 +24,57 @@ export const ChartQueryBlockInitializer = (props) => {
   const [items, setItems] = useState(defaultItems);
   const apiClient = useAPIClient();
   const ctx = useChartQueryMetadataContext();
+  const onAddQuery = async (info) => {
+    await FormDialog('Add query', () => {
+      return <div>{info.key}</div>;
+    }).open({
+      initialValues: {
+        type: info.key,
+      },
+    });
+    // TODO 获取插入的 query data
+    const item = ctx.data[0] ?? {};
+    onCreateBlockSchema({ item });
+    setVisible(false);
+    console.log(info);
+  };
   useEffect(() => {
     const chartQueryMetadata = ctx.data;
     if (chartQueryMetadata && Array.isArray(chartQueryMetadata)) {
-      setItems([
-        {
-          type: 'itemGroup',
-          title: 'select a data source',
-          children: chartQueryMetadata,
-        },
-      ]);
+      setItems(
+        [
+          // {
+          //   type: 'itemGroup',
+          //   title: '{{t("Select chart query")}}',
+          //   children:
+          // },
+          {
+            type: 'subMenu',
+            title: 'Add chart query',
+            // component: AddChartQuery,
+            children: [
+              {
+                key: 'sql',
+                type: 'item',
+                title: 'SQL',
+                onClick: onAddQuery,
+              },
+              {
+                key: 'json',
+                type: 'item',
+                title: 'JSON',
+                onClick: onAddQuery,
+              },
+            ],
+          },
+          chartQueryMetadata.length
+            ? {
+                type: 'divider',
+              }
+            : null,
+          ...chartQueryMetadata,
+        ].filter(Boolean),
+      );
     }
   }, []);
   return (
