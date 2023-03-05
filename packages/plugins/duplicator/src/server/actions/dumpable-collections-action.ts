0fb6e94954365d520a00973a641c103b9425b63f
chareice <chareice@live.com>
2023-03-05 19:00:44 +0800

    chore: dumpable collections api

--- packages/plugins/duplicator/src/server/actions/dumpable-collections-action.ts
+++ packages/plugins/duplicator/src/server/actions/dumpable-collections-action.ts
@@ -1,9 +1,11 @@
 import { Dumper } from '../dumper';
+import { getApp } from './get-app';
 
 export default async function dumpableCollections(ctx, next) {
   ctx.withoutDataWrapping = true;
 
-  const dumper = new Dumper(ctx.app);
+  const app = await getApp(ctx, ctx.request.query.app);
+  const dumper = new Dumper(app);
 
   ctx.body = await dumper.dumpableCollections();
 
