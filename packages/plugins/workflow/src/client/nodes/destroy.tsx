6955cc2488ac7979ceafe6341384907ccef2dd5f
mytharcher <mytharcher@gmail.com>
2023-03-08 01:13:15 +0800

    fix(plugin-workflow): fix import

--- packages/plugins/workflow/src/client/nodes/destroy.tsx
+++ packages/plugins/workflow/src/client/nodes/destroy.tsx
@@ -3,6 +3,7 @@ import { useCollectionDataSource } from '@nocobase/client';
 import { FilterDynamicComponent } from '../components/FilterDynamicComponent';
 import { collection, filter } from '../schemas/collection';
 import { isValidFilter } from '../utils';
+import { NAMESPACE } from '../locale';
 
 export default {
   title: '{{t("Delete record")}}',
