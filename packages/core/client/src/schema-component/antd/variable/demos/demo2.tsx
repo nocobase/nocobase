f3b210685f9e19d0b28a5ad3b9fa82067b297c21
mytharcher <mytharcher@gmail.com>
2023-03-10 17:06:24 +0800

    docs(client): add variable docs

---
+++ packages/core/client/src/schema-component/antd/variable/demos/demo2.tsx
@@ -0,0 +1,42 @@
+/**
+ * title: Variable.Input
+ */
+ import { FormItem } from '@formily/antd';
+ import { SchemaComponent, SchemaComponentProvider, Variable } from '@nocobase/client';
+ import React from 'react';
+
+ const scope = [
+   { label: 'v1', value: 'v1' },
+   { label: 'nested', value: 'nested', children: [{ label: 'v2', value: 'v2'}] },
+ ];
+
+ const schema = {
+   type: 'object',
+   properties: {
+     input: {
+       type: 'string',
+       title: `表达式模式`,
+       'x-decorator': 'FormItem',
+       'x-component': 'Variable.TextArea',
+       'x-component-props': {
+         scope,
+       },
+       // 'x-reactions': {
+       //   target: 'read',
+       //   fulfill: {
+       //     state: {
+       //       value: '{{$self.value}}',
+       //     },
+       //   },
+       // },
+     },
+   },
+ };
+
+ export default () => {
+   return (
+     <SchemaComponentProvider components={{ Variable, FormItem }}>
+       <SchemaComponent schema={schema} />
+     </SchemaComponentProvider>
+   );
+ };
