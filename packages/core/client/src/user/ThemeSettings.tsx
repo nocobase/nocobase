4e7245314b6e70db0b6476471f92f6769d697f0c
chenos <chenlinxh@gmail.com>
2023-03-11 23:20:24 +0800

    fix: styling

--- packages/core/client/src/user/ThemeSettings.tsx
+++ packages/core/client/src/user/ThemeSettings.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/css';
 import { Menu, Select } from 'antd';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -6,20 +7,28 @@ export const ThemeSettings = () => {
   const { t } = useTranslation();
   return (
     <Menu.Item key="theme" eventKey={'theme'}>
-      {t('Theme')}{' '}
-      <Select
-        style={{ minWidth: 100 }}
-        bordered={false}
-        defaultValue={localStorage.getItem('NOCOBASE_THEME') || 'default'}
-        options={[
-          { label: t('Default theme'), value: 'default' },
-          { label: t('Compact theme'), value: 'compact' },
-        ]}
-        onChange={async (value) => {
-          localStorage.setItem('NOCOBASE_THEME', value);
-          window.location.reload();
-        }}
-      />
+      <div
+        className={css`
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        `}
+      >
+        {t('Theme')}{' '}
+        <Select
+          style={{ minWidth: 100 }}
+          bordered={false}
+          defaultValue={localStorage.getItem('NOCOBASE_THEME') || 'default'}
+          options={[
+            { label: t('Default theme'), value: 'default' },
+            { label: t('Compact theme'), value: 'compact' },
+          ]}
+          onChange={async (value) => {
+            localStorage.setItem('NOCOBASE_THEME', value);
+            window.location.reload();
+          }}
+        />
+      </div>
     </Menu.Item>
   );
 };
