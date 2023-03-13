d27cbeb1bd9e671a3d7fbbf76d4becb5baef0445
katherinehhh <katherine_15995@163.com>
2023-03-13 19:03:53 +0800

    feat: suport add child

--- packages/core/client/src/schema-component/antd/table-v2/utils.ts
+++ packages/core/client/src/schema-component/antd/table-v2/utils.ts
@@ -10,7 +10,7 @@ export const isColumnComponent = (schema: Schema) => {
 
 export function extractIndex(str) {
   const numbers = [];
-  str.split('.').forEach(function (element) {
+  str?.split('.').forEach(function (element) {
     if (!isNaN(element)) {
       numbers.push(String(Number(element) + 1));
     }
