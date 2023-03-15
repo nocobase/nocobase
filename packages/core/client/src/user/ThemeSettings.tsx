4e7a7f3b15e9ab2c0b9cdf01d8563ff0ceb35c5a
chenos <chenlinxh@gmail.com>
2023-03-15 20:21:13 +0800

    feat: improve

--- packages/core/client/src/user/ThemeSettings.tsx
+++ packages/core/client/src/user/ThemeSettings.tsx
@@ -2,9 +2,14 @@ import { css } from '@emotion/css';
 import { Menu, Select } from 'antd';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useAPIClient } from '../api-client';
+import { useCurrentUserContext } from './CurrentUserProvider';
 
 export const ThemeSettings = () => {
   const { t } = useTranslation();
+  const ctx = useCurrentUserContext();
+  console.log('ctx.data.data.id', ctx.data.data.id);
+  const api = useAPIClient();
   return (
     <Menu.Item key="theme" eventKey={'theme'}>
       <div
@@ -18,12 +23,21 @@ export const ThemeSettings = () => {
         <Select
           style={{ minWidth: 100 }}
           bordered={false}
-          defaultValue={localStorage.getItem('NOCOBASE_THEME') || 'default'}
+          defaultValue={localStorage.getItem('NOCOBASE_THEME')}
           options={[
             { label: t('Default theme'), value: 'default' },
             { label: t('Compact theme'), value: 'compact' },
           ]}
           onChange={async (value) => {
+            await api.resource('users').update({
+              filterByTk: ctx.data.data.id,
+              values: {
+                systemSettings: {
+                  ...ctx.data.data.systemSettings,
+                  theme: value,
+                },
+              },
+            });
             localStorage.setItem('NOCOBASE_THEME', value);
             window.location.reload();
           }}
