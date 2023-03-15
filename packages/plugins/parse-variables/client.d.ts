3b13cbe811ed898ed98a96f9484338cdc43b99d9
Rairn <958414905@qq.com>
2023-03-15 22:46:52 +0800

    refactor: remove code of plugin

--- packages/plugins/parse-variables/client.d.ts
+++
@@ -1,4 +0,0 @@
-// @ts-nocheck
-export * from './lib/client';
-export { default } from './lib/client';
-
--- packages/plugins/parse-variables/src/client/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default React.memo((props) => {
-  return <>{props.children}</>;
-});
