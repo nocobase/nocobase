7b92bc11aebb23bffc5caa6ad7f062ece82ab0a4
Rairn <958414905@qq.com>
2023-03-17 19:20:12 +0800

    feat(markdown): support Mermaid and better style

--- packages/core/client/src/schema-component/antd/markdown/util.ts
+++ packages/core/client/src/schema-component/antd/markdown/util.ts
@@ -1,10 +1,23 @@
-import { marked } from 'marked';
+import MarkdownIt from 'markdown-it';
+import markdownItHighlightjs from 'markdown-it-highlightjs';
+import mermaidPlugin from './markdown-it-plugins/mermaidPlugin';
+
+import './highlight-theme/default.less';
+import './highlight-theme/table.less';
+
+const md = new MarkdownIt({
+  html: true,
+  linkify: true,
+  typographer: true,
+});
+md.use(markdownItHighlightjs);
+md.use(mermaidPlugin);
 
 export function markdown(text) {
   if (!text) {
     return '';
   }
-  return marked.parse(text);
+  return md.render(text);
 }
 export function convertToText(markdownText: string) {
   const content = markdown(markdownText);
