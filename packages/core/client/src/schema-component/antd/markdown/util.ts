b604f0bde22e6eda4f3171b8efe2f4640ad893a3
被雨水过滤的空气-Rairn <958414905@qq.com>
2023-03-19 08:18:50 +0800

    feat(markdown): support mermaid and better style (#1583)
    * feat(markdown): support Mermaid and better style
    
    * fix: img width
    
    * fix: build problem
    
    * feat: dynamic import
    
    * feat: loading
    
    ---------
    
    Co-authored-by: chenos <chenlinxh@gmail.com>

--- packages/core/client/src/schema-component/antd/markdown/util.ts
+++ packages/core/client/src/schema-component/antd/markdown/util.ts
@@ -1,13 +1,27 @@
-import { marked } from 'marked';
+import { useEffect, useState } from 'react';
+import './highlight-theme/default.less';
+import './highlight-theme/table.less';
 
-export function markdown(text) {
-  if (!text) {
-    return '';
-  }
-  return marked.parse(text);
+export async function parseMarkdown(text: string) {
+  const m = await import('./md');
+  return m.default.render(text);
 }
+
+export function useParseMarkdown(text: string) {
+  const [html, setHtml] = useState<any>('');
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    parseMarkdown(text).then((r) => {
+      setHtml(r);
+      setLoading(false);
+    });
+  }, [text]);
+  return { html, loading };
+}
+
 export function convertToText(markdownText: string) {
-  const content = markdown(markdownText);
+  const content = markdownText;
   let temp = document.createElement('div');
   temp.innerHTML = content;
   const text = temp.innerText;
