f54c7f58a05185472b2e598c502c56b5d9c9f6de
Rairn <958414905@qq.com>
2023-03-21 14:51:48 +0800

    refactor: extract components

---
+++ packages/plugins/duplicator/src/client/DuplicatorDump.tsx
@@ -0,0 +1,93 @@
+import { useAPIClient, useRequest } from '@nocobase/client';
+import { Button, Card, Table } from 'antd';
+import { saveAs } from 'file-saver';
+import React from 'react';
+
+export const DuplicatorDump = () => {
+  const api = useAPIClient();
+  const optionsWithDisabled = [
+    { label: 'migrations', value: 'migrations' },
+    { label: 'applicationPlugins', value: 'applicationPlugins' },
+    { label: 'applicationVersion', value: 'applicationVersion', disabled: false },
+  ];
+  const { data, loading } = useRequest({
+    resource: 'duplicator',
+    action: 'getDict',
+  });
+
+  const groups = {};
+
+  for (const collection of data || []) {
+    const moduleName = (collection.namespace || '').split('.').shift();
+    if (!moduleName) {
+      console.log(collection);
+      continue;
+    }
+    groups[moduleName] = groups[moduleName] || [];
+    groups[moduleName].push(collection);
+  }
+
+  const columns = [
+    {
+      title: '表名',
+      dataIndex: 'title',
+      render: (v, record) => <span>{v || record.name}</span>,
+    },
+    {
+      title: '标识',
+      dataIndex: 'name',
+    },
+    {
+      title: 'namespace',
+      dataIndex: 'namespace',
+    },
+  ];
+
+  const requiredData = (data || []).filter((item) => {
+    const required = item.duplicator === 'required' || item?.duplicator?.dumpable === 'required';
+    return required;
+  });
+  return (
+    <Card bordered={false}>
+      <div>
+        <h3>Required</h3>
+        <Table
+          loading={loading}
+          size={'middle'}
+          rowKey={'name'}
+          pagination={false}
+          rowSelection={{
+            type: 'checkbox',
+            selectedRowKeys: requiredData.map((item) => item.name),
+            getCheckboxProps(item) {
+              const required = item.duplicator === 'required' || item?.duplicator?.dumpable === 'required';
+              return {
+                disabled: required,
+              };
+            },
+          }}
+          columns={columns}
+          dataSource={requiredData}
+          // scroll={{ y: '55vh' }}
+        />
+        <Button
+          style={{ marginTop: 24 }}
+          type={'primary'}
+          onClick={async () => {
+            const response = await api.request({
+              url: 'duplicator:dump',
+              method: 'post',
+              responseType: 'blob',
+            });
+            const match = /filename="(.+)"/.exec(response?.headers?.['content-disposition'] || '');
+            const filename = match ? match[1] : 'duplicator.nbdump';
+            let blob = new Blob([response.data]);
+            saveAs(blob, filename);
+          }}
+        >
+          Dump
+        </Button>
+      </div>
+    </Card>
+  );
+};
---
+++ packages/plugins/duplicator/src/client/DuplicatorRestore.tsx
@@ -0,0 +1,10 @@
+import { Card } from 'antd';
+import React from 'react';
+
+export const DuplicatorRestore = () => {
+  return (
+    <Card bordered={false}>
+      <div>Upload</div>
+    </Card>
+  );
+};
