d28c717e9bff7f81f1224d781f73af9f83f2e8c2
KieSun <zx597813039@gmail.com>
2023-03-21 20:37:36 +0800

    feat: add run util

---
+++ packages/core/build/src/utiils.ts
@@ -0,0 +1,13 @@
+import execa from 'execa'
+
+export const run = (command, argv, options = {}) => {
+  return execa(command, argv, {
+    shell: true,
+    stdio: 'inherit',
+    ...options,
+    env: {
+      ...process.env,
+      ...options.env,
+    },
+  });
+};
