dca9e9eec65e758a2a13524c9221ef8bd9e37499
Chareice <chareice@live.com>
2023-03-21 11:38:08 +0800

    test: create view collection

---
+++ packages/core/database/src/__tests__/view/create-view.test.ts
@@ -0,0 +1,95 @@
+import { Database, mockDatabase } from '@nocobase/database';
+
+describe('create view', () => {
+  let db: Database;
+
+  beforeEach(async () => {
+    db = mockDatabase({
+      tablePrefix: '',
+    });
+    await db.clean({ drop: true });
+  });
+
+  afterEach(async () => {
+    await db.close();
+  });
+
+  it('should create view collection', async () => {
+    const UserCollection = db.collection({
+      name: 'users',
+      fields: [
+        {
+          type: 'string',
+          name: 'name',
+        },
+        {
+          type: 'hasOne',
+          name: 'profile',
+        },
+      ],
+    });
+
+    const ProfileCollection = db.collection({
+      name: 'profiles',
+      fields: [
+        {
+          type: 'integer',
+          name: 'age',
+        },
+        {
+          type: 'belongsTo',
+          name: 'user',
+          foreignKey: 'user_id',
+        },
+      ],
+    });
+
+    await db.sync();
+
+    await UserCollection.repository.create({
+      values: {
+        name: 'foo',
+        profile: {
+          age: 18,
+        },
+      },
+    });
+    const schema = UserCollection.collectionSchema();
+    const viewName = 'users_with_profiles';
+
+    const appendSchema = db.inDialect('postgres') ? `"${schema}".` : '';
+
+    const viewSql = `CREATE OR REPLACE VIEW ${appendSchema}${viewName} AS SELECT users.name, profiles.age FROM ${appendSchema}${UserCollection.model.tableName} as users LEFT JOIN ${appendSchema}${ProfileCollection.model.tableName} as profiles ON users.id = profiles.user_id;`;
+
+    await db.sequelize.query(viewSql);
+
+    const UserWithProfileView = db.collection({
+      name: 'users_with_profiles',
+      autoGenId: false,
+      timestamps: false,
+
+      fields: [
+        {
+          type: 'string',
+          name: 'name',
+        },
+        {
+          type: 'integer',
+          name: 'age',
+        },
+      ],
+      viewName,
+    });
+
+    expect(UserWithProfileView).toBeDefined();
+
+    const fooData = await UserWithProfileView.repository.findOne({
+      filter: {
+        name: 'foo',
+      },
+    });
+
+    expect(fooData.get('name')).toBe('foo');
+    expect(fooData.get('age')).toBe(18);
+  });
+});
