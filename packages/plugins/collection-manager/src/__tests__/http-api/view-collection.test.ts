5a6e982161de099537e4fee0b33204ebe0d5459c
Chareice <chareice@live.com>
2023-03-21 16:15:05 +0800

    test: view resourcer

--- packages/plugins/collection-manager/src/__tests__/http-api/view-collection.test.ts
+++ packages/plugins/collection-manager/src/__tests__/http-api/view-collection.test.ts
@@ -1,5 +1,6 @@
 import { MockServer } from '@nocobase/test';
 import { createApp } from '../index';
+import { uid } from '@nocobase/utils';
 
 describe('view collection', () => {
   let app: MockServer;
@@ -14,5 +15,13 @@ describe('view collection', () => {
     await app.destroy();
   });
 
-  it('should list views', async () => {});
+  it('should list views', async () => {
+    const viewName = `view_${uid(6)}`;
+    const viewSQL = `CREATE OR REPLACE VIEW ${viewName} AS select 1+1 as result`;
+    await app.db.sequelize.query(viewSQL);
+
+    const response = await agent.resource('views').list();
+    expect(response.status).toBe(200);
+    expect(response.body.data.find((item) => item.viewname === viewName)).toBeTruthy();
+  });
 });
