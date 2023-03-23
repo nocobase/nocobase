7b4a98452d9b2d74815e6458c32aedb63ba4b2c9
katherinehhh <katherine_15995@163.com>
2023-03-23 14:10:02 +0800

    chore: sync form database view

--- packages/core/client/src/collection-manager/templates/components/PreviewFields.tsx
+++ packages/core/client/src/collection-manager/templates/components/PreviewFields.tsx
@@ -11,7 +11,15 @@ export const PreviewFields = (props) => {
   const [dataSource, setDataSource] = useState([]);
   const [previewColumns, setPreviewColumns] = useState([]);
   const [previewData, setPreviewData] = useState([]);
-   const field:any=useField();
+  const field: any = useField();
+
+  const handleFieldChange = (record, index) => {
+    dataSource.splice(index, 1, record);
+    const pColumns = formatPreviewColumns(dataSource);
+    setPreviewColumns(pColumns);
+    setDataSource(dataSource);
+    field.value = dataSource;
+  };
   const columns = [
     {
       title: t('Field name'),
@@ -30,24 +38,42 @@ export const PreviewFields = (props) => {
       title: t('Field source'),
       dataIndex: 'source',
       key: 'source',
-      render: (text) => {
-        return <Select defaultValue={text} style={{ width: '100%' }} />;
+      render: (text,record, index) => {
+        return (
+          <Select
+            defaultValue={text}
+            style={{ width: '100%' }}
+            onChange={(value) => handleFieldChange({ ...record, source: value }, index)}
+          />
+        );
       },
     },
     {
       title: t('Field interface'),
       dataIndex: 'interface',
       key: 'interface',
-      render: (text, record) => {
-        return record.source ? text : <Select defaultValue={text} style={{ width: '100%' }} />;
+      render: (text, record, index) => {
+        return record.source ? (
+          text
+        ) : (
+          <Select
+            defaultValue={text}
+            style={{ width: '100%' }}
+            onChange={(value) => handleFieldChange({ ...record, interface: value }, index)}
+          />
+        );
       },
     },
     {
       title: t('Field display name'),
       dataIndex: 'title',
       key: 'title',
-      render: (text, record) => {
-        return record.source ? text : <Input defaultValue={text} />;
+      render: (text, record, index) => {
+        return record.source ? (
+          text
+        ) : (
+          <Input defaultValue={text} onChange={(e) => handleFieldChange({ ...record, title: e.target.value }, index)} />
+        );
       },
     },
   ];
@@ -60,28 +86,40 @@ export const PreviewFields = (props) => {
         })
         .then(({ data }) => {
           if (data?.data) {
-            field.value=data.data
+            field.value = data.data;
             setDataSource(data?.data);
-            const pColumns = data?.data?.map((item) => {
-              const target = item.source || item.title || item.name;
-              return {
-                title: target,
-                dataIndex: target,
-                key: target,
-              };
-            });
+            const pColumns = formatPreviewColumns(data?.data);
             setPreviewColumns(pColumns);
           }
         });
     }
   }, [name]);
+
+  const formatPreviewColumns = (data) => {
+    return data?.map((item) => {
+      const target = item.source || item.title || item.name;
+      return {
+        title: target,
+        dataIndex: target,
+        key: target,
+      };
+    });
+  };
   return (
     dataSource.length > 0 && (
       <>
         <h4>Fields:</h4>
-        <Table bordered columns={columns} dataSource={dataSource} scroll={{ y: 300 }} pagination={false} rowClassName="editable-row" />
+        <Table
+          bordered
+          columns={columns}
+          dataSource={dataSource}
+          scroll={{ y: 300 }}
+          pagination={false}
+          rowClassName="editable-row"
+          key={name}
+        />
         <h4>Preview:</h4>
-        <Table bordered columns={previewColumns} />
+        <Table bordered columns={previewColumns} scroll={{ x: 300 }} />
       </>
     )
   );
