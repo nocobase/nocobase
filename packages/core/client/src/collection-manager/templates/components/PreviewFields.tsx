abcaae1f9e7efb3c209084200f393a0de580159c
katherinehhh <katherine_15995@163.com>
2023-03-23 16:14:58 +0800

    chore: sync form database view

--- packages/core/client/src/collection-manager/templates/components/PreviewFields.tsx
+++ packages/core/client/src/collection-manager/templates/components/PreviewFields.tsx
@@ -1,24 +1,69 @@
 import React, { useEffect, useState } from 'react';
 import { Table, Tag, Select, Input } from 'antd';
+import { Cascader } from '@formily/antd';
 import { useField } from '@formily/react';
 import { useTranslation } from 'react-i18next';
 import { useAPIClient } from '../../../api-client';
+import { useCollectionManager } from '../../hooks/useCollectionManager';
+import { useCompile } from '../../../';
 
 export const PreviewFields = (props) => {
-  const { name } = props;
+  const { name, source } = props;
   const api = useAPIClient();
   const { t } = useTranslation();
   const [dataSource, setDataSource] = useState([]);
   const [previewColumns, setPreviewColumns] = useState([]);
   const [previewData, setPreviewData] = useState([]);
+  const [sourceFields, setSourceFields] = useState([]);
   const field: any = useField();
+  const { getCollection } = useCollectionManager();
+  const compile = useCompile();
+
+  useEffect(() => {
+    const data = [];
+    source.forEach((item) => {
+      const collection = getCollection(item);
+      const children = collection.fields?.map((v) => {
+        return { value: v.name, label: v.uiSchema.title };
+      });
+      data.push({
+        value: item,
+        label: collection.title,
+        children,
+      });
+    });
+    setSourceFields(data);
+  }, [source]);
+
+  useEffect(() => {
+    if (name) {
+      api
+        .resource(`dbViews/${name}/fields`)
+        .list({
+          paginate: false,
+        })
+        .then(({ data }) => {
+          if (data?.data) {
+            field.value = data.data;
+            setDataSource(data?.data);
+            const pColumns = formatPreviewColumns(data?.data);
+            setPreviewColumns(pColumns);
+          }
+        });
+    }
+  }, [name]);
 
   const handleFieldChange = (record, index) => {
     dataSource.splice(index, 1, record);
     const pColumns = formatPreviewColumns(dataSource);
     setPreviewColumns(pColumns);
     setDataSource(dataSource);
-    field.value = dataSource;
+    field.value = dataSource.map((v) => {
+      return {
+        ...v,
+        source: v.source?.join('.'),
+      };
+    });
   };
   const columns = [
     {
@@ -38,12 +83,17 @@ export const PreviewFields = (props) => {
       title: t('Field source'),
       dataIndex: 'source',
       key: 'source',
-      render: (text,record, index) => {
+      render: (text, record, index) => {
         return (
-          <Select
-            defaultValue={text}
+          <Cascader
+            defaultValue={text?.split('.')}
+            allowClear
             style={{ width: '100%' }}
-            onChange={(value) => handleFieldChange({ ...record, source: value }, index)}
+            options={compile(sourceFields)}
+            onChange={(value, selectedOptions) => {
+              handleFieldChange({ ...record, source: value }, index);
+            }}
+            placeholder={t('Select field source')}
           />
         );
       },
@@ -52,14 +102,15 @@ export const PreviewFields = (props) => {
       title: t('Field interface'),
       dataIndex: 'interface',
       key: 'interface',
-      render: (text, record, index) => {
-        return record.source ? (
+      render: (text, _, index) => {
+        const item = dataSource[index];
+        return item.source ? (
           text
         ) : (
           <Select
             defaultValue={text}
             style={{ width: '100%' }}
-            onChange={(value) => handleFieldChange({ ...record, interface: value }, index)}
+            onChange={(value) => handleFieldChange({ ...item, interface: value }, index)}
           />
         );
       },
@@ -68,38 +119,23 @@ export const PreviewFields = (props) => {
       title: t('Field display name'),
       dataIndex: 'title',
       key: 'title',
-      render: (text, record, index) => {
-        return record.source ? (
+      render: (text, _, index) => {
+        const item = dataSource[index];
+        return item.source ? (
           text
         ) : (
-          <Input defaultValue={text} onChange={(e) => handleFieldChange({ ...record, title: e.target.value }, index)} />
+          <Input defaultValue={text} onChange={(e) => handleFieldChange({ ...item, title: e.target.value }, index)} />
         );
       },
     },
   ];
-  useEffect(() => {
-    if (name) {
-      api
-        .resource(`dbViews/${name}/fields`)
-        .list({
-          paginate: false,
-        })
-        .then(({ data }) => {
-          if (data?.data) {
-            field.value = data.data;
-            setDataSource(data?.data);
-            const pColumns = formatPreviewColumns(data?.data);
-            setPreviewColumns(pColumns);
-          }
-        });
-    }
-  }, [name]);
-
   const formatPreviewColumns = (data) => {
     return data?.map((item) => {
-      const target = item.source || item.title || item.name;
+      const sourceField = getCollection(item?.source?.[0])?.fields.find((v) => v.name === item?.source?.[1])?.uiSchema
+        .title;
+      const target = sourceField || item.title || item.name;
       return {
-        title: target,
+        title: compile(target),
         dataIndex: target,
         key: target,
       };
