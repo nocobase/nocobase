6d9e0efa4ecbf6a09be92572ea7cc9e83b4f0611
Rairn <958414905@qq.com>
2023-03-25 16:17:24 +0800

    fix: improve test

--- packages/core/client/src/schema-component/antd/date-picker/__tests__/util.test.ts
+++ packages/core/client/src/schema-component/antd/date-picker/__tests__/util.test.ts
@@ -52,7 +52,7 @@ describe('moment2str', () => {
   test('showTime is true, gmt is false', () => {
     const m = moment('2023-06-21 10:10:00');
     const str = moment2str(m, { showTime: true, gmt: false });
-    expect(str).toBe('2023-06-21T02:10:00.000Z');
+    expect(str).toBe(m.toISOString());
   });
 
   test('gmt is true', () => {
@@ -61,11 +61,10 @@ describe('moment2str', () => {
     expect(str).toBe('2023-06-21T00:00:00.000Z');
   });
 
-  // 北京时区
   test('gmt is false', () => {
     const m = moment('2023-06-21 10:10:00');
     const str = moment2str(m, { gmt: false });
-    expect(str).toBe('2023-06-20T16:00:00.000Z');
+    expect(str).toBe(moment('2023-06-21 00:00:00').toISOString());
   });
 
   test('gmt date only', () => {
@@ -83,7 +82,7 @@ describe('moment2str', () => {
   test('picker is year, gmt is false', () => {
     const m = moment('2023-06-21 10:10:00');
     const str = moment2str(m, { picker: 'year', gmt: false });
-    expect(str).toBe('2022-12-31T16:00:00.000Z');
+    expect(str).toBe(moment('2023-01-01 00:00:00').toISOString());
   });
 
   test('picker is year, gmt is true', () => {
@@ -101,19 +100,19 @@ describe('moment2str', () => {
   test('picker is quarter, gmt is false', () => {
     const m = moment('2023-06-21 10:10:00');
     const str = moment2str(m, { picker: 'quarter', gmt: false });
-    expect(str).toBe('2023-03-31T16:00:00.000Z');
-  })
+    expect(str).toBe(moment('2023-04-01 00:00:00').toISOString());
+  });
 
   test('picker is quarter, gmt is true', () => {
     const m = moment('2023-06-21 10:10:00');
     const str = moment2str(m, { picker: 'quarter', gmt: true });
     expect(str).toBe('2023-04-01T00:00:00.000Z');
-  })
+  });
 
   test('picker is month, gmt is false', () => {
     const m = moment('2023-06-21 10:10:00');
     const str = moment2str(m, { picker: 'month', gmt: false });
-    expect(str).toBe('2023-05-31T16:00:00.000Z');
+    expect(str).toBe(moment('2023-06-01 00:00:00').toISOString());
   });
 
   test('picker is month, gmt is true', () => {
@@ -131,14 +130,14 @@ describe('moment2str', () => {
   test('picker is week, gmt is false', () => {
     const m = moment('2023-06-21 10:10:00');
     const str = moment2str(m, { picker: 'week', gmt: false });
-    expect(str).toBe('2023-06-18T16:00:00.000Z');
-  })
+    expect(str).toBe(moment('2023-06-19 00:00:00').toISOString());
+  });
 
   test('picker is week, gmt is true', () => {
     const m = moment('2023-06-21 10:10:00');
     const str = moment2str(m, { picker: 'week', gmt: true });
     expect(str).toBe('2023-06-19T00:00:00.000Z');
-  })
+  });
 
   test('value is null', async () => {
     const m = moment2str(null);
