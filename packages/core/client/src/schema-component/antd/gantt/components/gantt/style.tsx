69c1d47ea832ea4ae132cd3ea90ded6decf023ac
katherinehhh <katherine_15995@163.com>
2023-03-27 09:51:28 +0800

    style: style improve

--- packages/core/client/src/schema-component/antd/gantt/components/gantt/style.tsx
+++ packages/core/client/src/schema-component/antd/gantt/components/gantt/style.tsx
@@ -7,7 +7,7 @@ export const ganttVerticalContainer=css `
   margin: 0;
   padding: 0;
   width:100%;
-  border-left:2px solid #c5c4c4
+  border-left:2px solid #f4f2f2
 `
 
 export const horizontalContainer=css `
