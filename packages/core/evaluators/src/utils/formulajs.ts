edd9bc9922b6ce880756193785cf0cde72be0b64
katherinehhh <katherine_15995@163.com>
2023-03-30 18:19:58 +0800

    fix: formulajs tyy catch

--- packages/core/evaluators/src/utils/formulajs.ts
+++ packages/core/evaluators/src/utils/formulajs.ts
@@ -1,20 +1,18 @@
 import * as functions from '@formulajs/formulajs';
 
-const fnNames = Object.keys(functions).filter((key) => key !== 'default');
-const fns = fnNames.map((key) => functions[key]);
 
-export default function (expression: string, scope = {}) {
-  try {
-    const fn = new Function(...fnNames, ...Object.keys(scope), `return ${expression}`);
-    const result = fn(...fns, ...Object.values(scope));
-    if (typeof result === 'number') {
-      if (Number.isNaN(result) || !Number.isFinite(result)) {
-        return null;
-      }
-      return functions.ROUND(result, 9);
+
+const fnNames = Object.keys(functions).filter(key => key !== 'default');
+const fns = fnNames.map(key => functions[key]);
+
+export default function(expression: string, scope = {}) {
+  const fn = new Function(...fnNames, ...Object.keys(scope), `return ${expression}`);
+  const result = fn(...fns, ...Object.values(scope));
+  if (typeof result === 'number') {
+    if (Number.isNaN(result) || !Number.isFinite(result)) {
+      return null;
     }
-    return result;
-  } catch (error) {
-    console.error(expression);
+    return functions.ROUND(result, 9);
   }
+  return result;
 }
