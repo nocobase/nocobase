eccdfdb482ba2cd79a1ff694a857ff59ff1bfb6d
ChengLei Shao <chareice@live.com>
2023-04-04 11:34:20 +0800

    chore: add transaction in set field action (#1647)

--- packages/plugins/collection-manager/src/resourcers/collections.ts
+++ packages/plugins/collection-manager/src/resourcers/collections.ts
@@ -4,28 +4,40 @@ export default {
   async ['collections:setFields'](ctx, next) {
     const { filterByTk, values } = ctx.action.params;
 
-    // WARN: 删掉 key 才能保存
-    const fields = values.fields?.map(f => {
-      delete f.key;
-      return f;
-    });
+    const transaction = await ctx.app.db.sequelize.transaction();
 
-    const db = ctx.app.db as Database;
+    try {
+      const fields = values.fields?.map((f) => {
+        delete f.key;
+        return f;
+      });
 
-    const collection = await db.getRepository('collections').findOne({
-      filter: {
-        name: filterByTk,
-      },
-    });
+      const db = ctx.app.db as Database;
 
-    await db.getRepository('collections').update({
-      filterByTk,
-      values: {
-        fields,
-      },
-    });
+      const collection = await db.getRepository('collections').findOne({
+        filter: {
+          name: filterByTk,
+        },
+        transaction,
+      });
 
-    await collection.loadFields();
+      await db.getRepository('collections').update({
+        filterByTk,
+        values: {
+          fields,
+        },
+        transaction,
+      });
+
+      await collection.loadFields({
+        transaction,
+      });
+
+      await transaction.commit();
+    } catch (e) {
+      await transaction.rollback();
+      throw e;
+    }
 
     await next();
   },
