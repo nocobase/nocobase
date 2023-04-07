11bc5329031bef7f9bc692bf5239f5b9858e122e
Rairn <958414905@qq.com>
2023-04-07 10:34:17 +0800

    feat: auto close on uploading over

--- packages/core/client/src/schema-component/antd/upload/Upload.tsx
+++ packages/core/client/src/schema-component/antd/upload/Upload.tsx
@@ -8,6 +8,7 @@ import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
+import { useActionContext } from '../action';
 import { ReadPretty } from './ReadPretty';
 import { isImage, toArr, toFileList, toItem, toValue, useUploadProps } from './shared';
 import './style.less';
@@ -217,6 +218,7 @@ Upload.DraggerV2 = connect(
     const { title = defaultTitle, subTitle = defaultSubTitle, useProps } = props;
     const extraProps: Record<string, any> = useProps?.() || {};
     const [loading, setLoading] = useState(false);
+    const { setVisible } = useActionContext();
 
     const handleChange = (fileList = []) => {
       const { onChange } = extraProps;
@@ -226,6 +228,7 @@ Upload.DraggerV2 = connect(
         setLoading(true);
       } else {
         setLoading(false);
+        setVisible(false);
       }
     };
 
