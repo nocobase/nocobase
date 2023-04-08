efa69a25bf32ec5fbcf1c9221a9342887f36907f
mytharcher <mytharcher@gmail.com>
2023-04-08 14:21:11 +0800

    fix(client): fix variable component read pretty mode

--- packages/core/client/src/schema-component/antd/variable/TextArea.tsx
+++ packages/core/client/src/schema-component/antd/variable/TextArea.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect, useRef, useMemo } from 'react';
-import { Input, Cascader, Button } from 'antd';
+import { Input, Cascader, Button, Tag } from 'antd';
 import { useForm } from '@formily/react';
 import { cx, css } from '@emotion/css';
 import { useTranslation } from 'react-i18next';
 import * as sanitizeHTML from 'sanitize-html';
 
-import { useCompile } from '../..';
+import { EllipsisWithTooltip, useCompile } from '../..';
+import { useRecord } from '../../../record-provider';
 
 type RangeIndexes = [number, number, number, number];
 
@@ -380,61 +381,97 @@ export function TextArea(props) {
         contentEditable={!disabled}
         dangerouslySetInnerHTML={{ __html: html }}
       />
-      <Button className={cx('x-button', css`
-        position: relative;
-      `)}>
-        <span
-          className={css`
-            font-style: italic;
-            font-family: "New York", "Times New Roman", Times, serif;
-          `}
-        >x</span>
-        <Cascader
-          placeholder={t('Select a variable')}
-          value={[]}
-          options={options}
-          onChange={(keyPaths = [], selectedOptions = []) => {
-            setSelectedVar(keyPaths as string[]);
-            if (!keyPaths.length) {
-              return;
-            }
-            const option = selectedOptions[selectedOptions.length - 1];
-            if (!option?.children?.length) {
-              onInsert(keyPaths);
-            }
-          }}
-          changeOnSelect
-          onClick={(e: any) => {
-            if (e.detail !== 2) {
-              return;
-            }
-            for (let n = e.target; n && n !== e.currentTarget; n = n.parentNode) {
-              if (Array.from(n.classList ?? []).includes('ant-cascader-menu-item')) {
-                onInsert(selectedVar);
-              }
-            }
-          }}
-          dropdownClassName={css`
-            .ant-cascader-menu{
-              margin-bottom: 0;
-            }
-          `}
-          dropdownRender={(menu) => (
-            <>
-              {menu}
-              <div
-                className={css`
-                  padding: .5em;
-                  border-top: 1px solid rgba(0, 0, 0, .06);
-                  color: rgba(0, 0, 0, .45);
-                `}
-              >
-                {t('Double click to choose entire object')}
-              </div>
-            </>
-          )}
-        />
-      </Button>
+      {!disabled
+        ? (
+          <Button className={cx('x-button', css`
+            position: relative;
+          `)}>
+            <span
+              className={css`
+                font-style: italic;
+                font-family: "New York", "Times New Roman", Times, serif;
+              `}
+            >x</span>
+            <Cascader
+              placeholder={t('Select a variable')}
+              value={[]}
+              options={options}
+              onChange={(keyPaths = [], selectedOptions = []) => {
+                setSelectedVar(keyPaths as string[]);
+                if (!keyPaths.length) {
+                  return;
+                }
+                const option = selectedOptions[selectedOptions.length - 1];
+                if (!option?.children?.length) {
+                  onInsert(keyPaths);
+                }
+              }}
+              changeOnSelect
+              onClick={(e: any) => {
+                if (e.detail !== 2) {
+                  return;
+                }
+                for (let n = e.target; n && n !== e.currentTarget; n = n.parentNode) {
+                  if (Array.from(n.classList ?? []).includes('ant-cascader-menu-item')) {
+                    onInsert(selectedVar);
+                  }
+                }
+              }}
+              dropdownClassName={css`
+                .ant-cascader-menu{
+                  margin-bottom: 0;
+                }
+              `}
+              dropdownRender={(menu) => (
+                <>
+                  {menu}
+                  <div
+                    className={css`
+                      padding: .5em;
+                      border-top: 1px solid rgba(0, 0, 0, .06);
+                      color: rgba(0, 0, 0, .45);
+                    `}
+                  >
+                    {t('Double click to choose entire object')}
+                  </div>
+                </>
+              )}
+            />
+          </Button>
+        )
+        : null
+      }
     </Input.Group>
+  );
+}
+
+TextArea.ReadPretty = (props) => {
+  const { value, multiline = true, scope } = props;
+  const compile = useCompile();
+  const options = compile((typeof scope === 'function' ? scope() : scope) ?? []);
+  const keyLabelMap = useMemo(() => createOptionsValueLabelMap(options), [scope]);
+  const html = renderHTML(value ?? '', keyLabelMap);
+
+  const content = (
+    <span
+      dangerouslySetInnerHTML={{ __html: html }}
+      className={css`
+        overflow: auto;
+
+        .ant-tag {
+          display: inline;
+          line-height: 19px;
+          margin: 0 .25em;
+          padding: 2px 7px;
+          border-radius: 10px;
+        }
+      `}
+    />
+  );
+
+  return (
+    <EllipsisWithTooltip ellipsis popoverContent={content}>
+      {content}
+    </EllipsisWithTooltip>
   );
 }
