2bce840dedf6a48deeed7adc199c833d49a0fc07
katherinehhh <katherine_15995@163.com>
2023-04-10 11:31:50 +0800

    test: linkage form field display test

--- packages/core/client/src/schema-component/antd/form-v2/__tests__/linkageDisplay.test.tsx
+++ packages/core/client/src/schema-component/antd/form-v2/__tests__/linkageDisplay.test.tsx
@@ -1,19 +1,14 @@
 import '@testing-library/jest-dom';
 import React, { useMemo } from 'react';
 import { FormItem } from '@formily/antd';
+import { useForm } from '@formily/react';
 import { createForm } from '@formily/core';
 import { render, fireEvent } from '@testing-library/react';
-import {
-  SchemaComponent,
-  SchemaComponentProvider,
-  Grid,
-  Input,
-  FormBlockProvider,
-} from '@nocobase/client';
+import { SchemaComponent, SchemaComponentProvider, Grid, Input, FormBlockProvider } from '@nocobase/client';
 import { Form } from '../Form';
 
 describe('form field displaylinkage', () => {
-  it('updates name field disaply when single field value is changed', () => {
+  it(' name field hidden when single field value is apple', () => {
     const ComponentA = () => {
       const useFormBlockProps = () => {
         const form = useMemo(() => createForm(), []);
@@ -116,5 +111,104 @@ describe('form field displaylinkage', () => {
       // 元素不存在
     }
     expect(f2).toBeUndefined();
+  });
+
+  it(' name field required when single field value is apple', async () => {
+    const ComponentA = () => {
+      const useFormBlockProps = () => {
+        const form = useMemo(() => createForm(), []);
+        return {
+          form: form,
+        };
+      };
+      const schema: any = {
+        type: 'void',
+        'x-acl-action-props': { skipScopeCheck: true },
+        'x-acl-action': 'tags:create',
+        'x-decorator': 'FormBlockProvider',
+        'x-decorator-props': { resource: 'tags', collection: 'tags' },
+        'x-component': 'CardItem',
+        properties: {
+          ukypn3nb6bc: {
+            type: 'void',
+            'x-component': 'Form',
+            'x-component-props': {
+              useProps: useFormBlockProps,
+            },
+            properties: {
+              grid: {
+                type: 'void',
+                'x-component': 'Grid',
+                'x-linkage-rules': [
+                  {
+                    condition: { $and: [{ single: { $eq: 'apple' } }] },
+                    actions: [
+                      {
+                        targetFields: ['name'],
+                        operator: 'required',
+                      },
+                    ],
+                  },
+                ],
+                properties: {
+                  '18p3c2nodkw': {
+                    type: 'void',
+                    'x-component': 'Grid.Row',
+                    properties: {
+                      '3p36izpvy9q': {
+                        type: 'void',
+                        'x-component': 'Grid.Col',
+                        properties: {
+                          single: {
+                            type: 'string',
+                            title: 'single',
+                            'x-component': Input,
+                            'x-decorator': 'FormItem',
+                            'x-component-props': {
+                              placeholder: 'single',
+                            },
+                          },
+                        },
+                      },
+                    },
+                  },
+                  p4372tmusz2: {
+                    type: 'void',
+                    'x-component': 'Grid.Row',
+                    properties: {
+                      xuiwn30smwl: {
+                        type: 'void',
+                        'x-component': 'Grid.Col',
+                        properties: {
+                          name: {
+                            type: 'string',
+                            title: 'name',
+                            'x-component': Input,
+                            'x-decorator': 'FormItem',
+                            'x-component-props': {
+                              placeholder: 'name',
+                            },
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      };
+      return (
+        <SchemaComponentProvider scope={{ useFormBlockProps }} components={{ Form, Grid, FormItem, FormBlockProvider }}>
+          <SchemaComponent schema={schema} />
+        </SchemaComponentProvider>
+      );
+    };
+    const { getAllByPlaceholderText, getByText } = render(<ComponentA />);
+    const f1 = getAllByPlaceholderText('single')[0];
+    fireEvent.input(f1, { target: { value: 'apple' } });
+    const requiredLabel = getByText('*');
+    expect(requiredLabel).toBeInTheDocument();
   });
 });
