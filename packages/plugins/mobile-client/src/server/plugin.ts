2ab4e26e9769140c6058a5f05f2913f478960d38
Dunqing <dengqing0821@gmail.com>
2023-04-13 14:57:03 +0800

    feat: init mobile client

---
+++ packages/plugins/mobile-client/src/server/plugin.ts
@@ -0,0 +1,19 @@
+import { InstallOptions, Plugin } from '@nocobase/server';
+
+export class MobileClientPlugin extends Plugin {
+  afterAdd() {}
+
+  beforeLoad() {}
+
+  async load() {}
+
+  async install(options?: InstallOptions) {}
+
+  async afterEnable() {}
+
+  async afterDisable() {}
+
+  async remove() {}
+}
+
+export default MobileClientPlugin;
