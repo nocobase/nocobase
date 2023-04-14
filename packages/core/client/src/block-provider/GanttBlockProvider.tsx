27f248b9cf545ed284662b73807a4be7f6ca1528
katherinehhh <shunai.tang@hand-china.com>
2023-04-14 22:08:01 +0800

    fix(gantt): update permission check  in gantt block (#1701)
    * fix: view collection can  update in gantt
    
    * fix: gantt update without acl check
    
    * style: style improve
    
    * style: style improve
    
    * style: style improve
    
    * style: style improve

--- packages/core/client/src/block-provider/GanttBlockProvider.tsx
+++ packages/core/client/src/block-provider/GanttBlockProvider.tsx
@@ -2,6 +2,8 @@ import { useField } from '@formily/react';
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { BlockProvider, useBlockRequestContext } from './BlockProvider';
 import { TableBlockProvider } from './TableBlockProvider';
+import { useACLRoleContext } from '../acl/ACLProvider';
+import { useCollection } from '../collection-manager/hooks';
 
 export const GanttBlockContext = createContext<any>({});
 
@@ -11,9 +13,11 @@ const formatData = (
   tasks: any[] = [],
   projectId: any = undefined,
   hideChildren: boolean = false,
+  checkPermassion?: Function,
 ) => {
   data.forEach((item: any) => {
-    const percent=item[fieldNames.progress] * 100;
+    const disable = checkPermassion(item);
+    const percent = item[fieldNames.progress] * 100;
     if (item.children && item.children.length) {
       tasks.push({
         start: new Date(item[fieldNames.start]),
@@ -21,12 +25,13 @@ const formatData = (
         name: item[fieldNames.title],
         id: item.id + '',
         type: 'project',
-        progress: percent>100?100:percent || 0,
+        progress: percent > 100 ? 100 : percent || 0,
         hideChildren: hideChildren,
         project: projectId,
         color: item.color,
+        isDisabled: disable,
       });
-      formatData(item.children, fieldNames, tasks, item.id + '', hideChildren);
+      formatData(item.children, fieldNames, tasks, item.id + '', hideChildren, checkPermassion);
     } else {
       tasks.push({
         start: item[fieldNames.start] ? new Date(item[fieldNames.start]) : undefined,
@@ -34,9 +39,10 @@ const formatData = (
         name: item[fieldNames.title],
         id: item.id + '',
         type: fieldNames.end ? 'task' : 'milestone',
-        progress: percent>100?100:percent || 0,
+        progress: percent > 100 ? 100 : percent || 0,
         project: projectId,
         color: item.color,
+        isDisabled: disable,
       });
     }
   });
@@ -66,8 +72,8 @@ const InternalGanttBlockProvider = (props) => {
 
 export const GanttBlockProvider = (props) => {
   return (
-    <BlockProvider {...props} params={{ tree:true, paginate: false, sort: props.fieldNames.start }}>
-      <TableBlockProvider {...props} params={{ tree:true, paginate: false, sort: props.fieldNames.start }}>
+    <BlockProvider {...props} params={{ tree: true, paginate: false, sort: props.fieldNames.start }}>
+      <TableBlockProvider {...props} params={{ tree: true, paginate: false, sort: props.fieldNames.start }}>
         <InternalGanttBlockProvider {...props} />
       </TableBlockProvider>
     </BlockProvider>
@@ -81,6 +87,17 @@ export const useGanttBlockContext = () => {
 export const useGanttBlockProps = () => {
   const ctx = useGanttBlockContext();
   const [tasks, setTasks] = useState<any>([]);
+  const { getPrimaryKey, name, template } = useCollection();
+  const { parseAction } = useACLRoleContext();
+  const primaryKey = getPrimaryKey();
+  const checkPermassion = (record) => {
+    const actionPath = `${name}:update`;
+    const schema = {};
+    const recordPkValue = record?.[primaryKey];
+    const params = parseAction(actionPath, { schema, recordPkValue });
+    return template === 'view' || !params;
+  };
+
   const onExpanderClick = (task: any) => {
     const data = ctx.field.data;
     const tasksData = data.map((t: any) => (t.id === task.id ? task : t));
@@ -88,13 +105,13 @@ export const useGanttBlockProps = () => {
     ctx.field.data = tasksData;
   };
   const expandAndCollapseAll = (flag) => {
-    const data = formatData(ctx.service.data?.data, ctx.fieldNames, [], undefined, flag);
+    const data = formatData(ctx.service.data?.data, ctx.fieldNames, [], undefined, flag, checkPermassion);
     setTasks(data);
     ctx.field.data = data;
   };
   useEffect(() => {
     if (!ctx?.service?.loading) {
-      const data = formatData(ctx.service.data?.data, ctx.fieldNames);
+      const data = formatData(ctx.service.data?.data, ctx.fieldNames, [], undefined, false, checkPermassion);
       setTasks(data);
       ctx.field.data = data;
     }
