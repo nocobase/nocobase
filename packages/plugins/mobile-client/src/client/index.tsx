0ff4939ab6ad91f22f8c8e71f5161483f691dd41
Dunqing <dengqing0821@gmail.com>
2023-04-14 15:17:08 +0800

    feat: ready to develop

--- packages/plugins/mobile-client/src/client/index.tsx
+++ packages/plugins/mobile-client/src/client/index.tsx
@@ -1,5 +1,33 @@
+import { SchemaComponentOptions, SettingsCenterProvider } from '@nocobase/client';
 import React from 'react';
+import { useTranslation } from './locale';
+import { Configuration } from './configuration';
+
+const ComingSoon = () => <div>Coming Soon</div>;
 
 export default React.memo((props) => {
-  return <>{props.children}</>;
+  const { t } = useTranslation();
+
+  return (
+    <SettingsCenterProvider
+      settings={{
+        ['mobile-client']: {
+          title: t('Mobile Client-side'),
+          icon: 'PhoneOutlined',
+          tabs: {
+            'interface-configuration': {
+              title: t('Interface Configuration'),
+              component: Configuration,
+            },
+            'app-configuration': {
+              title: t('App Configuration'),
+              component: ComingSoon,
+            },
+          },
+        },
+      }}
+    >
+      <SchemaComponentOptions components={{}}>{props.children}</SchemaComponentOptions>
+    </SettingsCenterProvider>
+  );
 });
