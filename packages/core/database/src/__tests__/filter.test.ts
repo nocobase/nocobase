1a2695a98d4e9ecde3450bc07ee7d46b62550b29
chareice <chareice@live.com>
2023-04-15 21:51:51 +0800

    test: hasMany filter test

--- packages/core/database/src/__tests__/filter.test.ts
+++ packages/core/database/src/__tests__/filter.test.ts
@@ -14,6 +14,47 @@ describe('filter', () => {
     await db.close();
   });
 
+  it('should filter by hasMany association field', async () => {
+    const DeptCollection = db.collection({
+      name: 'depts',
+      fields: [
+        { type: 'string', name: 'name' },
+        { type: 'belongsTo', name: 'org', target: 'orgs' },
+      ],
+    });
+
+    const OrgCollection = db.collection({
+      name: 'orgs',
+      fields: [
+        { type: 'string', name: 'name' },
+        { type: 'hasMany', name: 'depts', target: 'depts' },
+      ],
+    });
+
+    await db.sync();
+
+    await OrgCollection.repository.create({
+      values: [
+        {
+          name: 'org1',
+          depts: [{ name: 'dept1' }, { name: 'dept2' }],
+        },
+        {
+          name: 'org2',
+          depts: [{ name: 'dept3' }, { name: 'dept4' }],
+        },
+      ],
+    });
+
+    const dept1 = await DeptCollection.repository.findOne({});
+
+    const orgs = await OrgCollection.repository.find({
+      filter: { $and: [{ depts: { id: { $eq: dept1.get('id') } } }] },
+    });
+
+    expect(orgs.length).toBe(1);
+  });
+
   it('should filter by association field', async () => {
     const UserCollection = db.collection({
       name: 'users',
