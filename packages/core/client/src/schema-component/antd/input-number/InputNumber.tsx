fd957cc854045465423ce237ff2cf8ab99efd68c
siuyoon <siuyoon@Mac-mini.local>
2023-04-16 18:04:49 +0800

    fix: fix the failure of Percent and inputNumber steppers

--- packages/core/client/src/schema-component/antd/input-number/InputNumber.tsx
+++ packages/core/client/src/schema-component/antd/input-number/InputNumber.tsx
@@ -4,12 +4,12 @@ import React from 'react';
 import { ReadPretty } from './ReadPretty';
 
 export const InputNumber = connect((props) => {
-  const { onChange, ...others } = props;
+  const { onChange, step, ...others } = props;
   const field = useField<any>();
   const handleChange = (v) => {
     onChange(parseFloat(v));
   };
-  return <AntdNumber onChange={handleChange} {...others}  />;
+  return <AntdNumber step={Number(step) || 1} onChange={handleChange} {...others}  />;
 }, mapReadPretty(ReadPretty));
 
 export default InputNumber;
--- packages/core/client/src/schema-component/antd/percent/Percent.tsx
+++ packages/core/client/src/schema-component/antd/percent/Percent.tsx
@@ -6,12 +6,13 @@ import * as math from 'mathjs';
 
 export const Percent = connect(
   (props) => {
-    const { value, onChange } = props;
+    const { value, step, onChange } = props;
 
     return (
       <InputNumber 
         {...props}
         addonAfter="%" 
+        step={Number(step) || 1}
         value={value ? math.round(value * 100, 9) : null}
         onChange={(v: any) => {
           if (onChange) {
