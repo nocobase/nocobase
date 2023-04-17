018839c772d242bebcf01a4163c322e74f917606
Dunqing <dengqing0821@gmail.com>
2023-04-17 18:39:52 +0800

    feat: improve map configuration

--- packages/plugins/map/src/client/hooks/useMapConfiguration.ts
+++ packages/plugins/map/src/client/hooks/useMapConfiguration.ts
@@ -1,15 +1,31 @@
-import { useRequest } from "@nocobase/client";
+import { useRequest } from '@nocobase/client';
 
 export const MapConfigurationResourceKey = 'map-configuration';
+export const getSSKey = (type) => {
+  return `NOCOBASE_PLUGIN_MAP_CONFIGURATION_${type}`;
+};
 
 export const useMapConfiguration = (type: string) => {
-  return useRequest({
-    resource: MapConfigurationResourceKey,
-    action: 'get',
-    params: {
-      type,
+  // cache
+  const config = sessionStorage.getItem(getSSKey(type));
+  if (config) {
+    return JSON.parse(config);
+  }
+  return useRequest(
+    {
+      resource: MapConfigurationResourceKey,
+      action: 'get',
+      params: {
+        type,
+      },
     },
-  }).data?.data;
-}
-
-
+    {
+      onSuccess(data) {
+        sessionStorage.setItem(getSSKey(type), JSON.stringify(data?.data));
+      },
+      refreshOnWindowFocus: false,
+      refreshDeps: [],
+      manual: config ? true : false,
+    },
+  ).data?.data;
+};
