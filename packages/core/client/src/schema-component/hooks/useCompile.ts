e2cf7eb91a22562d17161ebe1334ea87f82b99b8
Rairn <958414905@qq.com>
2023-04-19 11:55:24 +0800

    fix: fix error

--- packages/core/client/src/schema-component/hooks/useCompile.ts
+++ packages/core/client/src/schema-component/hooks/useCompile.ts
@@ -1,16 +1,13 @@
 import { Schema, SchemaExpressionScopeContext, SchemaOptionsContext } from '@formily/react';
-import { useCallback, useContext } from 'react';
+import { useContext } from 'react';
 
 export const useCompile = () => {
   const options = useContext(SchemaOptionsContext);
   const scope = useContext(SchemaExpressionScopeContext);
-  return useCallback(
-    (source: any, ext?: any) => {
-      if (!source) {
-        return source;
-      }
-      return Schema.compile(source, { ...options.scope, ...scope, ...ext });
-    },
-    [options, scope],
-  );
+  return (source: any, ext?: any) => {
+    if (!source) {
+      return source;
+    }
+    return Schema.compile(source, { ...options.scope, ...scope, ...ext });
+  };
 };
