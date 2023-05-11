67de0c5280b82670bb693d865296a88cf2904442
katherinehhh <katherine_15995@163.com>
2023-05-11 17:05:51 +0800

    fix: sub-form collection context

--- packages/core/client/src/schema-component/antd/association-field/InternalNester.tsx
+++ packages/core/client/src/schema-component/antd/association-field/InternalNester.tsx
@@ -2,19 +2,22 @@ import { FormLayout } from '@formily/antd';
 import { RecursionField, useField, useFieldSchema } from '@formily/react';
 import React, { useEffect } from 'react';
 import { CollectionProvider } from '../../../collection-manager';
-import { useAssociationFieldContext, useInsertSchema } from './hooks';
+import { useInsertSchema } from './hooks';
+import { useCollection, useCollectionManager } from '../../../';
 import schema from './schema';
 
 export const InternalNester = () => {
   const field = useField();
   const fieldSchema = useFieldSchema();
   const insertNester = useInsertSchema('Nester');
-  const { options } = useAssociationFieldContext();
+  const { getField } = useCollection();
+  const { getCollectionJoinField } = useCollectionManager();
+  const collectionField = getField(fieldSchema['name']) || getCollectionJoinField(fieldSchema['x-collection-field']);
   useEffect(() => {
     insertNester(schema.Nester);
   }, []);
   return (
-    <CollectionProvider name={options.target}>
+    <CollectionProvider name={collectionField.target}>
       <FormLayout layout={'vertical'}>
         <RecursionField
           onlyRenderProperties
