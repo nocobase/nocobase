c0f0a66ae7f3c44b6d80ec14fb35231aadb827f7
katherinehhh <katherine_15995@163.com>
2023-05-11 17:13:24 +0800

    refactor: code improve

--- packages/core/client/src/schema-component/antd/association-field/InternalNester.tsx
+++ packages/core/client/src/schema-component/antd/association-field/InternalNester.tsx
@@ -3,16 +3,15 @@ import { RecursionField, useField, useFieldSchema } from '@formily/react';
 import React, { useEffect } from 'react';
 import { CollectionProvider } from '../../../collection-manager';
 import { useInsertSchema } from './hooks';
-import { useCollection, useCollectionManager } from '../../../';
+import { useCollectionManager } from '../../../';
 import schema from './schema';
 
 export const InternalNester = () => {
   const field = useField();
   const fieldSchema = useFieldSchema();
   const insertNester = useInsertSchema('Nester');
-  const { getField } = useCollection();
   const { getCollectionJoinField } = useCollectionManager();
-  const collectionField = getField(fieldSchema['name']) || getCollectionJoinField(fieldSchema['x-collection-field']);
+  const collectionField = getCollectionJoinField(fieldSchema['x-collection-field']);
   useEffect(() => {
     insertNester(schema.Nester);
   }, []);
