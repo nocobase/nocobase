067a33f385ef8536a100963f658aafa70f010d71
ChengLei Shao <chareice@live.com>
2023-05-24 16:20:39 +0800

    test: append acl resource params

--- packages/core/acl/src/acl-resource.ts
+++ packages/core/acl/src/acl-resource.ts
@@ -1,5 +1,6 @@
 import { ACLRole, RoleActionParams } from './acl-role';
 import { ACL, ListenerContext } from './acl';
+import lodash from 'lodash';
 
 export type ResourceActions = { [key: string]: RoleActionParams };
 
@@ -35,7 +36,8 @@ export class ACLResource {
   }
 
   getAction(name: string) {
-    return this.actions.get(name) || this.actions.get(this.acl.resolveActionAlias(name));
+    const result = this.actions.get(name) || this.actions.get(this.acl.resolveActionAlias(name));
+    return lodash.cloneDeep(result);
   }
 
   setAction(name: string, params: RoleActionParams) {
