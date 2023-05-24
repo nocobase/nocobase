2f19c3915b18671178f7aaf393a0224f135b243f
ChengLei Shao <chareice@live.com>
2023-05-24 14:49:24 +0800

    fix: audit log changes

--- packages/plugins/audit-logs/src/server/__tests__/hook.test.ts
+++ packages/plugins/audit-logs/src/server/__tests__/hook.test.ts
@@ -8,6 +8,8 @@ describe('hook', () => {
 
   beforeEach(async () => {
     api = mockServer();
+    await api.db.clean({ drop: true });
+
     api.plugin(logPlugin, { name: 'audit-logs' });
     await api.load();
     db = api.db;
