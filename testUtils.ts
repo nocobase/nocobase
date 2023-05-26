764268a122c8af3fb9d1829df32dd8229dacc2b8
Rairn <958414905@qq.com>
2023-05-26 22:14:06 +0800

    feat: add test utils

--- testUtils.ts
+++ testUtils.ts
@@ -1,7 +1,29 @@
+import { act, cleanup, render } from '@testing-library/react';
+import { afterEach } from 'vitest';
+
+afterEach(() => {
+  cleanup();
+});
+
+function customRender(ui: React.ReactElement, options = {}) {
+  return render(ui, {
+    // wrap provider(s) here if needed
+    wrapper: ({ children }) => children,
+    ...options,
+  });
+}
+
+export * from '@testing-library/react';
+export { default as userEvent } from '@testing-library/user-event';
+// override render export
+export { customRender as render };
+
 const globalTimeout = global.setTimeout;
 
 export const sleep = async (timeout = 0) => {
-  await new Promise((resolve) => {
-    globalTimeout(resolve, timeout);
+  await act(async () => {
+    await new Promise((resolve) => {
+      globalTimeout(resolve, timeout);
+    });
   });
 };
