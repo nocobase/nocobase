644b6eccb38db7edc39c43a9ab6a6d6c76b45045
katherinehhh <shunai.tang@hand-china.com>
2023-05-26 11:39:16 +0800

    refactor: association select display loading when data is loading (#1925)
    * refactor: association select loading
    
    * refactor: association select loading
    
    * refactor: loading

--- packages/core/client/src/schema-component/antd/remote-select/RemoteSelect.tsx
+++ packages/core/client/src/schema-component/antd/remote-select/RemoteSelect.tsx
@@ -192,7 +192,6 @@ const InternalRemoteSelect = connect(
       run();
       firstRun.current = true;
     };
-
     return (
       <Select
         dropdownMatchSelectWidth={false}
@@ -205,7 +204,7 @@ const InternalRemoteSelect = connect(
         objectValue={objectValue}
         value={value}
         {...others}
-        loading={loading}
+        loading={data! ? loading : true}
         options={mapOptionsToTags(options)}
       />
     );
