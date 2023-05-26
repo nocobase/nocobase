621e46f756b3803c6077a0f14ce31345bd07ec5e
Dunqing <dengqing0821@gmail.com>
2023-05-26 14:56:08 +0800

    fix(grid-card, list): display title only work on current block

--- packages/core/client/src/schema-items/GeneralSchemaItems.tsx
+++ packages/core/client/src/schema-items/GeneralSchemaItems.tsx
@@ -55,7 +55,7 @@ export const GeneralSchemaItems: React.FC<{
         />
       )}
       <SchemaSettings.SwitchItem
-        checked={field.decoratorProps.showTitle ?? true}
+        checked={fieldSchema['x-decorator-props'] ?? true}
         title={t('Display title')}
         onChange={(checked) => {
           fieldSchema['x-decorator-props'] = fieldSchema['x-decorator-props'] || {};
