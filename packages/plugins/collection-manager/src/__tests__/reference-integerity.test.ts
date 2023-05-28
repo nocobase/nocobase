9ba398c83fc35161d8047a3ea95551d4837c9441
ChengLei Shao <chareice@live.com>
2023-05-28 18:28:26 +0800

    test: reference map with multiple belongs to

--- packages/plugins/collection-manager/src/__tests__/reference-integerity.test.ts
+++ packages/plugins/collection-manager/src/__tests__/reference-integerity.test.ts
@@ -19,6 +19,70 @@ describe('reference integrity check', () => {
     await app.destroy();
   });
 
+  it('should cascade delete on belongs to relation with multiple belongs to field', async () => {
+    const Group = db.collection({
+      name: 'groups',
+      fields: [{ type: 'string', name: 'name' }],
+    });
+
+    const User = db.collection({
+      name: 'users',
+      fields: [
+        { type: 'string', name: 'name' },
+        {
+          type: 'belongsTo',
+          name: 'group2',
+          target: 'groups',
+          onDelete: 'SET NULL',
+          foreignKey: 'group2_id',
+          targetKey: 'id',
+        },
+        {
+          type: 'belongsTo',
+          name: 'group',
+          target: 'groups',
+          onDelete: 'CASCADE',
+          foreignKey: 'group_id',
+          targetKey: 'id',
+        },
+      ],
+    });
+
+    await db.sync();
+
+    const g1 = await Group.repository.create({
+      values: {
+        name: 'group1',
+      },
+    });
+
+    const g2 = await Group.repository.create({
+      values: {
+        name: 'group2',
+      },
+    });
+
+    const u1 = await User.repository.create({
+      values: {
+        name: 'user1',
+        group_id: g1.id,
+        group2_id: g2.id,
+      },
+    });
+
+    expect(await User.repository.count()).toEqual(1);
+
+    await g2.destroy();
+
+    await u1.reload();
+
+    expect(u1.group2_id).toBeNull();
+
+    await g1.destroy();
+
+    expect(await User.repository.count()).toEqual(0);
+  });
+
   it('should cascade delete on belongs to many relation on collection', async () => {
     const users = db.collection({
       name: 'users',
