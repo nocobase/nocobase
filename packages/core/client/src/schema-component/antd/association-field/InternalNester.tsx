8c17fafd3e09f364f07148d53050ddc4c1089bff
katherinehhh <katherine_15995@163.com>
2023-05-29 22:20:06 +0800

    style: sub nester style improve

--- packages/core/client/src/schema-component/antd/association-field/InternalNester.tsx
+++ packages/core/client/src/schema-component/antd/association-field/InternalNester.tsx
@@ -1,6 +1,7 @@
 import { FormLayout } from '@formily/antd';
 import { RecursionField, useField, useFieldSchema } from '@formily/react';
 import React, { useEffect } from 'react';
+import { css, cx } from '@emotion/css';
 import { CollectionProvider } from '../../../collection-manager';
 import { useInsertSchema } from './hooks';
 import { useAssociationFieldContext } from './hooks';
@@ -18,14 +19,30 @@ export const InternalNester = () => {
   return (
     <CollectionProvider name={collectionField.target}>
       <FormLayout layout={'vertical'}>
-        <RecursionField
-          onlyRenderProperties
-          basePath={field.address}
-          schema={fieldSchema}
-          filterProperties={(s) => {
-            return s['x-component'] === 'AssociationField.Nester';
-          }}
-        />
+        <div
+          className={cx(
+            css`
+              & .ant-formily-item-layout-vertical {
+                margin-bottom: 10px;
+              }
+              .ant-card-body {
+                padding: 15px 20px ;
+              }
+              .ant-divider-horizontal{
+                margin:10px 0
+              }
+            `,
+          )}
+        >
+          <RecursionField
+            onlyRenderProperties
+            basePath={field.address}
+            schema={fieldSchema}
+            filterProperties={(s) => {
+              return s['x-component'] === 'AssociationField.Nester';
+            }}
+          />
+        </div>
       </FormLayout>
     </CollectionProvider>
   );
