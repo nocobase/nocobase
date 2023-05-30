78345992ea15ea3149df435a94c37c72f51b8bc5
Dunqing <dengqing0821@gmail.com>
2023-05-30 03:09:16 +0000

    feat: improve jsbridge apis

--- packages/plugins/mobile-client/src/client/core/bridge/native-call.ts
+++ packages/plugins/mobile-client/src/client/core/bridge/native-call.ts
@@ -1,15 +1,13 @@
-declare interface Window {
-  onBackPressed: (cb: (v: boolean) => boolean) => void;
-}
+import { invoke } from './injects';
+
 /**
  * App 右滑返回
  * @param cb 回调函数，返回 true 表示 web 自己消费，false表示 app 消费
  */
-window.onBackPressed = (cb) => {
+window.onBackPressed = () => {
   if (history.length === 1) {
-    cb(false);
+    invoke({ action: 'moveTaskToBack' });
   } else {
     history.back();
-    cb(true);
   }
 };
