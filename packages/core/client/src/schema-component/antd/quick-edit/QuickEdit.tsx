9a47e6723c8fca81db2f38aa4f0e40394a8287bd
katherinehhh <katherine_15995@163.com>
2023-05-31 10:07:41 +0800

    style: quickEdit style improve

--- packages/core/client/src/schema-component/antd/quick-edit/QuickEdit.tsx
+++ packages/core/client/src/schema-component/antd/quick-edit/QuickEdit.tsx
@@ -1,11 +1,10 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { EditOutlined } from '@ant-design/icons';
 import { Popover } from 'antd';
 import { useField, observer, useFieldSchema, RecursionField } from '@formily/react';
 import { FormItem } from '@formily/antd';
 import CollectionField from '../../../collection-manager/CollectionField';
 import { useCollectionManager } from '../../../collection-manager';
-import { createForm } from '@formily/core';
 import { FormProvider } from '../../core';
 
 export const QuickEdit = observer((props) => {
@@ -13,8 +12,6 @@ export const QuickEdit = observer((props) => {
   const { getCollectionJoinField } = useCollectionManager();
   const fieldSchema = useFieldSchema();
   const collectionField = getCollectionJoinField(fieldSchema['x-collection-field']);
-  const form = useMemo(() => createForm(), []);
-  console.log(fieldSchema);
   const schema: any = {
     name: fieldSchema.name,
     'x-collection-field': fieldSchema['x-collection-field'],
@@ -22,12 +19,12 @@ export const QuickEdit = observer((props) => {
     default: field.value,
     'x-component-props': {
       onChange: (e) => {
-        console.log(e);
         const data = e.target?.value || e;
         if (['hasMany', 'belongsToMany'].includes(collectionField.type)) {
           const result = field.value || [];
           result.push(data);
           field.value = result;
+          field.onInput(field.value);
         } else {
           field.value = data;
         }
@@ -35,9 +32,11 @@ export const QuickEdit = observer((props) => {
     },
   };
   const content = (
-    <FormProvider>
-      <RecursionField schema={schema} name={fieldSchema.name} />
-    </FormProvider>
+    <div style={{ width: '100%', height: '100%', minWidth: 300 }}>
+      <FormProvider>
+        <RecursionField schema={schema} name={fieldSchema.name} />
+      </FormProvider>
+    </div>
   );
   return (
     <Popover content={content} trigger="click">
