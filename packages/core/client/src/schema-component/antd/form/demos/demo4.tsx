07d3fb2674f23f994f25b9a30bb1f0e81bc2d97a
Rairn <958414905@qq.com>
2023-06-03 08:42:45 +0800

    test(Form): add test

--- packages/core/client/src/schema-component/antd/form/demos/demo4.tsx
+++ packages/core/client/src/schema-component/antd/form/demos/demo4.tsx
@@ -3,47 +3,45 @@ import { ISchema, observer, useForm } from '@formily/react';
 import { Action, Form, SchemaComponent, SchemaComponentProvider, useCloseAction } from '@nocobase/client';
 import React from 'react';
 
-export default observer(() => {
-  const schema: ISchema = {
-    type: 'object',
-    properties: {
-      a1: {
-        type: 'void',
-        'x-component': 'Action',
-        'x-component-props': {
-          type: 'primary',
-        },
-        title: 'Open',
-        properties: {
-          d1: {
-            type: 'void',
-            'x-component': 'Action.Drawer',
-            'x-decorator': 'Form',
-            'x-decorator-props': {
-              initialValue: {
-                field1: 'aaa',
-              },
+const schema: ISchema = {
+  type: 'object',
+  properties: {
+    a1: {
+      type: 'void',
+      'x-component': 'Action',
+      'x-component-props': {
+        type: 'primary',
+      },
+      title: 'Open',
+      properties: {
+        d1: {
+          type: 'void',
+          'x-component': 'Action.Drawer',
+          'x-decorator': 'Form',
+          'x-decorator-props': {
+            initialValue: {
+              field1: 'aaa',
             },
-            title: 'Drawer Title',
-            properties: {
-              field1: {
-                title: 'T1',
-                'x-component': 'Input',
-                'x-decorator': 'FormItem',
-              },
-              out: {
-                'x-component': 'Output',
-              },
-              f1: {
-                type: 'void',
-                'x-component': 'Action.Drawer.Footer',
-                properties: {
-                  a1: {
-                    'x-component': 'Action',
-                    title: 'Close',
-                    'x-component-props': {
-                      useAction: '{{ useCloseAction }}',
-                    },
+          },
+          title: 'Drawer Title',
+          properties: {
+            field1: {
+              title: 'T1',
+              'x-component': 'Input',
+              'x-decorator': 'FormItem',
+            },
+            out: {
+              'x-component': 'Output',
+            },
+            f1: {
+              type: 'void',
+              'x-component': 'Action.Drawer.Footer',
+              properties: {
+                a1: {
+                  'x-component': 'Action',
+                  title: 'Close',
+                  'x-component-props': {
+                    useAction: '{{ useCloseAction }}',
                   },
                 },
               },
@@ -52,8 +50,10 @@ export default observer(() => {
         },
       },
     },
-  };
+  },
+};
 
+export default observer(() => {
   const Output = observer(
     () => {
       const form = useForm();
