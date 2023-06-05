b3f75f640c3dfdbe76a5fbcd517955b8886c7700
Junyi <mytharcher@users.noreply.github.com>
2023-06-05 15:52:43 +0700

    feat(plugin-workflow): manual forms (#1748)
    * feat(plugin-workflow): add create record form for manual node
    
    * feat(plugin-workflow): add update form for manual node
    
    * fix(plugin-workflow): fix antd version and menu
    
    * refactor(plugin-workflow): change collection config modal to initializer sub-menu
    
    * test(plugin-workflow): add test case
    
    * fix(plugin-workflow): fix todo components based on forms
    
    * fix(plugin-workflow): fix cycling trigger when create or update
    
    * fix(plugin-workflow): fix transaction in manual processor
    
    * refactor(plugin-workflow): optimize todo list data loading
    
    * fix(plugin-workflow): fix uncommitted manual action effects
    
    * fix(plugin-workflow): fix save multiple forms and only submit one
    
    * chore(plugin-workflow): fix lint
    
    * fix(plugin-workflow): fix lint error
    
    * refactor(plugin-workflow): abstract workflow scoped FormBlockProvider
    
    * fix(plugin-workflow): adjust designable api to use current schema in refresh
    
    * fix(plugin-workflow): fix schema config and support block template
    
    * fix(plugin-workflow): fix lint and build error
    
    * fix(plugin-workflow): adjust components and scope to inner schema component
    
    * fix(plugin-workflow): fix ref template in todo drawer
    
    * fix(plugin-workflow): fix todo form undefined
    
    * fix(plugin-workflow): fix manual form schema
    
    * fix(plugin-workflow): fix manual createdBy/updatedBy user
    
    * fix(plugin-workflow): disable save to template on create form
    
    * fix(plugin-workflow): fix manual form variables
    
    * fix(plugin-workflow): fix FormBlockProvider for default field value
    
    * fix(plugin-workflow): fix manual node variables

---
+++ packages/plugins/workflow/src/client/nodes/manual/forms/create.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { useFieldSchema } from '@formily/react';
+
+import { GeneralSchemaDesigner, SchemaSettings, useCollection, useCollectionManager } from '@nocobase/client';
+
+import { NAMESPACE } from '../../../locale';
+import { findSchema } from '../utils';
+import { ManualFormType } from '../SchemaConfig';
+import { FormBlockInitializer } from '../FormBlockInitializer';
+
+function CreateFormDesigner() {
+  const { name, title } = useCollection();
+
+  return (
+    <GeneralSchemaDesigner title={title || name}>
+      <SchemaSettings.BlockTitleItem />
+      <SchemaSettings.LinkageRules collectionName={name} />
+      <SchemaSettings.DataTemplates collectionName={name} />
+      <SchemaSettings.Divider />
+      <SchemaSettings.Remove
+        removeParentsIfNoChildren
+        breakRemoveOn={{
+          'x-component': 'Grid',
+        }}
+      />
+    </GeneralSchemaDesigner>
+  );
+}
+
+export default {
+  title: `{{t("Create record form", { ns: "${NAMESPACE}" })}}`,
+  config: {
+    useInitializer() {
+      const { collections } = useCollectionManager();
+      return {
+        key: 'createRecordForm',
+        type: 'subMenu',
+        title: `{{t("Create record form", { ns: "${NAMESPACE}" })}}`,
+        children: collections
+          .filter((item) => !item.hidden)
+          .map((item) => ({
+            key: `createForm-${item.name}`,
+            type: 'item',
+            title: item.title,
+            schema: {
+              collection: item.name,
+              title: `{{t("Create record", { ns: "${NAMESPACE}" })}}`,
+              formType: 'create',
+              'x-designer': 'CreateFormDesigner',
+            },
+            component: FormBlockInitializer,
+          })),
+      };
+    },
+    initializers: {
+      // AddCustomFormField
+    },
+    components: {
+      CreateFormDesigner,
+    },
+    parseFormOptions(root) {
+      const forms = {};
+      const formBlocks: any[] = findSchema(
+        root,
+        (item) => item['x-decorator'] === 'FormBlockProvider' && item['x-decorator-props'].formType === 'create',
+      );
+      formBlocks.forEach((formBlock) => {
+        const [formKey] = Object.keys(formBlock.properties);
+        const formSchema = formBlock.properties[formKey];
+        forms[formKey] = {
+          type: 'create',
+          title: formBlock['x-component-props']?.title || formKey,
+          actions: findSchema(formSchema.properties.actions, (item) => item['x-component'] === 'Action').map(
+            (item) => item['x-decorator-props'].value,
+          ),
+          collection: formBlock['x-decorator-props'].collection,
+        };
+      });
+      return forms;
+    },
+  },
+  block: {
+    scope: {
+      // useFormBlockProps
+    },
+    components: {},
+  },
+} as ManualFormType;
