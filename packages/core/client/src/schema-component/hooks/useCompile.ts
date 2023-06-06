fc7613f1d553d90c1ecb7fe1a99e5187ba7691b6
dream2023 <1098626505@qq.com>
2023-06-06 15:45:57 +0800

    feat: 优化代码

--- packages/core/client/src/schema-component/hooks/useCompile.ts
+++ packages/core/client/src/schema-component/hooks/useCompile.ts
@@ -8,22 +8,46 @@ export const useCompile = () => {
   const options = useContext(SchemaOptionsContext);
   const scope = useContext(SchemaExpressionScopeContext);
   return (source: any, ext?: any) => {
+    // if (!source) return source;
+
+    // if (typeof source === 'string') {
+    //   // 多语言
+    //   if (source.startsWith('{{')) {
+    //     i18nMap[source] = i18nMap[source] || Schema.compile(source, { ...options.scope, ...scope, ...ext });
+    //     return i18nMap[source];
+    //   } else {
+    //     // 普通字符串
+    //     return source;
+    //   }
+    // }
+
+    // // 其他的都走 Schema.compile
+    // return Schema.compile(source, { ...options.scope, ...scope, ...ext });
+
     // source is i18n, for example: {{ t('Add new') }}
     if (typeof source === 'string' && source.startsWith('{{')) {
       i18nMap[source] = i18nMap[source] || Schema.compile(source, { ...options.scope, ...scope, ...ext });
       return i18nMap[source];
     }
-    // source is ReactNode
-    if (source && typeof source === 'object') {
+
+    // source is Component Object, for example: { 'x-component': "Cascader", type: "array", title: "所属地区(行政区划)" }
+    if (source && typeof source === 'object' && source['x-component']) {
       try {
         componentMap[JSON.stringify(source)] =
           componentMap[JSON.stringify(source)] || Schema.compile(source, { ...options.scope, ...scope, ...ext });
         return componentMap[JSON.stringify(source)];
       } catch (e) {
-        console.log('useCompile error', e);
+        console.log('useCompile error', source, e);
         return Schema.compile(source, { ...options.scope, ...scope, ...ext });
       }
     }
+
+    // source is Array, for example: [{ 'title': "{{ ('Admin')}}", name: 'admin' }, { 'title': "{{ ('Root')}}", name: 'root' }]
+    if (Array.isArray(source)) {
+      return Schema.compile(source, { ...options.scope, ...scope, ...ext });
+    }
+
+    // source is: plain object、string、number、boolean、undefined、null
     return source;
   };
 };
