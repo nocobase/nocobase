f762dd39704a0c72fd0369101d98c5f7063f4833
chenos <chenlinxh@gmail.com>
2023-06-07 23:45:28 +0800

    fix: translation

--- packages/plugins/auth/src/client/locale/zh-CN.ts
+++ packages/plugins/auth/src/client/locale/zh-CN.ts
@@ -1,7 +1,7 @@
 const locale = {
   'Auth Type': '认证类型',
   Authenticators: '认证器',
-  Authentication: '认证',
+  Authentication: '用户认证',
   'Sign in via email': '邮箱登录',
   'Not allowed to sign up': '禁止注册',
   'Allow to sign up': '允许注册',
