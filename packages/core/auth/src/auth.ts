24601aa66f54a8fa5e5183449eb9b46b6e117efe
YANG QIA <2013xile@gmail.com>
2023-06-07 23:46:42 +0800

    feat(auth): support custom authentication (#2007)
    * feat(auth): init auth package & collection
    
    * feat(auth): register
    
    * feat(auth): use authenticator
    
    * feat(auth): mapRoles
    
    * feat(auth): refactor
    
    * feat(auth): base auth class
    
    * feat(auth): add plugin
    
    * chore(auth): test
    
    * chore(auth): add test cases
    
    * feat(auth): authenticators pane
    
    * chore(auth): custom hook useAuthTypes
    
    * feat(auth): authenticator pane
    
    * chore(auth): store options schema using context
    
    * feat(auth): signInPage provider
    
    * feat(auth): signUpPage provider
    
    * chore(auth): solve build errors
    
    * chore(auth): add dependency
    
    * chore(auth): remove dependency cycles
    
    * chore(auth): add plugin-auth to preset
    
    * chore(auth): fix test
    
    * feat(auth): authenticator enable status
    
    * fix(test): fix test using new authentication
    
    * feat(auth): migration, set up basic auth
    
    * chore(auth): can set options ui by component
    
    * fix(test): workflow manunal.test
    
    * fix(test): typo
    
    * feat(auth): support multi-language
    
    * chore(auth): imporve code
    
    * chore(auth): hide button if no configuration
    
    * chore(auth): readme
    
    * chore(auth): remove allowSignup prop
    
    * chore(auth): move configure pane to edit form
    
    * fix(auth): jwt options bug
    
    * feat(auth): init sms-auth
    
    * chore(auth): at least authenticator required
    
    * chore(auth): add test
    
    * feat(auth): support sms auth
    
    * fix(auth): fix test
    
    * chore(auth): move findOrCreateUser to AuthModel
    
    * chore(auth): history compatible processing
    
    * feat(auth): support SAML auth
    
    * chore(auth): saml auth list
    
    * chore(saml-auth): improve ui
    
    * Merge branch 'main' into feat/authentication
    
    * chore(auth): improve code
    
    * fix(saml-auth): fix bug
    
    * fix(saml-auth): fix saml options
    
    * chore(saml-auth): compatible processing && ut
    
    * fix(auth): signin page bug
    
    * chore(auth): saml compatible processing
    
    * feat(auth): oidc-auth
    
    * fix(oidc-auth): bug
    
    * fix(oidc-auth): bug
    
    * fix(auth): fix test
    
    * chore(auth): filter enabled authenticator
    
    * chore(oidc): add field map
    
    * chore(auth): update readme
    
    * docs(auth): create sms-auth readme
    
    * feat(auth): allow signup config
    
    * test(auth): fix test
    
    * feat(auth): allow saml and oidc use http
    
    * chore(oidc-auth): extends timeout
    
    * docs(auth): update readme
    
    * feat(auth): support sort
    
    * docs(saml): update readme
    
    * feat(auth): support sort all authenticator
    
    * Merge branch 'main' into feat/authentication
    
    * Merge branch 'main' into feat/authentication
    
    * feat: improve code
    
    * docs(auth): add doc
    
    * Merge branch 'main' into feat/authentication
    
    * chore: update yarn.lock
    
    * feat: improve code
    
    * chore(acl): write role to acl if it exists in database and not found … (#2001)
    
    * chore(acl): write role to acl if it exists in database and not found in acl
    
    * fix: test
    
    * fix: eager load with nested association (#2002)
    
    * chore: upgrade vitest
    
    * chore: edit
    
    * refactor: auth class
    
    * fix: set options
    
    * chore(acl): write role to acl if it exists in database and not found … (#2001)
    
    * chore(acl): write role to acl if it exists in database and not found in acl
    
    * fix: test
    
    * fix: eager load with nested association (#2002)
    
    * chore: upgrade vitest
    
    * chore: add migrations
    
    * test: fix api-client test
    
    * chore: add sms-auth
    
    * feat: avoid no permission after auth type disabled
    
    * fix: translation
    
    ---------
    
    Co-authored-by: chenos <chenlinxh@gmail.com>

---
+++ packages/core/auth/src/auth.ts
@@ -0,0 +1,44 @@
+import { Context } from '@nocobase/actions';
+import { Model } from '@nocobase/database';
+
+export type AuthConfig = {
+  authenticator: Model;
+  options: {
+    [key: string]: any;
+  };
+  ctx: Context;
+};
+
+export type AuthExtend<T extends Auth> = new (config: AuthConfig) => T;
+
+interface IAuth {
+  user: Model;
+  // Check the authenticaiton status and return the current user.
+  check(): Promise<Model>;
+  signIn(): Promise<any>;
+  signUp(): Promise<any>;
+  signOut(): Promise<any>;
+}
+
+export abstract class Auth implements IAuth {
+  abstract user: Model;
+  protected authenticator: Model;
+  protected options: {
+    [key: string]: any;
+  };
+  protected ctx: Context;
+
+  constructor(config: AuthConfig) {
+    const { authenticator, options, ctx } = config;
+    this.authenticator = authenticator;
+    this.options = options;
+    this.ctx = ctx;
+  }
+
+  // The abstract methods are required to be implemented by all authentications.
+  abstract check();
+  // The following methods are mainly designed for user authentications.
+  async signIn(): Promise<any> {}
+  async signUp(): Promise<any> {}
+  async signOut(): Promise<any> {}
+}
