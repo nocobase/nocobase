b211032f57b393e628cc364eda74b2a47523a352
Dunqing <dengqing0821@gmail.com>
2023-06-09 12:17:47 +0800

    fix: re-insert repeat routes

--- packages/plugins/mobile-client/src/server/plugin.ts
+++ packages/plugins/mobile-client/src/server/plugin.ts
@@ -1,56 +1,19 @@
 import { InstallOptions, Plugin } from '@nocobase/server';
+import { routes } from './routes';
 
 export class MobileClientPlugin extends Plugin {
   afterAdd() {}
 
-  async load() {
+  async load() {}
+
+  async install() {
     const repository = this.app.db.getRepository('uiRoutes');
-    const routes = [
-      {
-        type: 'route',
-        path: '/mobile/:name(.+)?',
-        component: 'MApplication',
-        uiSchema: {
-          type: 'void',
-          'x-component': 'MContainer',
-          'x-designer': 'MContainer.Designer',
-          'x-component-props': {},
-          properties: {
-            page: {
-              type: 'void',
-              'x-component': 'MPage',
-              'x-designer': 'MPage.Designer',
-              'x-component-props': {},
-              properties: {
-                grid: {
-                  type: 'void',
-                  'x-component': 'Grid',
-                  'x-initializer': 'MBlockInitializers',
-                  'x-component-props': {
-                    showDivider: false,
-                  },
-                },
-              },
-            },
-          },
-        },
-        routes: [
-          {
-            type: 'route',
-            path: '',
-            component: 'RouteSchemaComponent',
-          },
-        ],
-      },
-    ];
     for (const values of routes) {
       await repository.create({
         values,
       });
     }
   }
-
-  async install(options?: InstallOptions) {}
 
   async afterEnable() {}
 
