64463d2cd8e12873bcada0c538fa48953515ba72
katherinehhh <katherine_15995@163.com>
2023-06-09 15:47:15 +0800

    refactor: code improve

--- packages/core/client/src/schema-component/antd/association-field/InternalSubTable.tsx
+++ packages/core/client/src/schema-component/antd/association-field/InternalSubTable.tsx
@@ -1,5 +1,5 @@
 import { FormLayout } from '@formily/antd';
-import { RecursionField, useField, useFieldSchema } from '@formily/react';
+import { RecursionField, useField, useFieldSchema, observer } from '@formily/react';
 import React, { useEffect } from 'react';
 import { CollectionProvider } from '../../../collection-manager';
 import { useAssociationFieldContext, useInsertSchema } from './hooks';
@@ -7,34 +7,37 @@ import schema from './schema';
 import { SchemaComponentOptions } from '../../../schema-component';
 import Select from '../select/Select';
 
-export const InternalSubTable = () => {
-  const field: any = useField();
-  const fieldSchema = useFieldSchema();
-  const insert = useInsertSchema('SubTable');
-  const { options } = useAssociationFieldContext();
-  useEffect(() => {
-    insert(schema.SubTable);
-    field.required = fieldSchema['required'];
-  }, []);
-  return (
-    <CollectionProvider name={options.target}>
-      <FormLayout layout={'vertical'}>
-        <SchemaComponentOptions
-          components={{
-            'Radio.Group': Select,
-            'Checkbox.Group': (props) => <Select multiple={true} mode="multiple" {...props} />,
-          }}
-        >
-          <RecursionField
-            onlyRenderProperties
-            basePath={field.address}
-            schema={fieldSchema}
-            filterProperties={(s) => {
-              return s['x-component'] === 'AssociationField.SubTable';
+export const InternalSubTable = observer(
+  () => {
+    const field: any = useField();
+    const fieldSchema = useFieldSchema();
+    const insert = useInsertSchema('SubTable');
+    const { options } = useAssociationFieldContext();
+    useEffect(() => {
+      insert(schema.SubTable);
+      field.required = fieldSchema['required'];
+    }, []);
+    return (
+      <CollectionProvider name={options.target}>
+        <FormLayout layout={'vertical'}>
+          <SchemaComponentOptions
+            components={{
+              'Radio.Group': Select,
+              'Checkbox.Group': (props) => <Select multiple={true} mode="multiple" {...props} />,
             }}
-          />
-        </SchemaComponentOptions>
-      </FormLayout>
-    </CollectionProvider>
-  );
-};
+          >
+            <RecursionField
+              onlyRenderProperties
+              basePath={field.address}
+              schema={fieldSchema}
+              filterProperties={(s) => {
+                return s['x-component'] === 'AssociationField.SubTable';
+              }}
+            />
+          </SchemaComponentOptions>
+        </FormLayout>
+      </CollectionProvider>
+    );
+  },
+  { displayName: 'InternalSubTable' },
+);
--- packages/core/client/src/schema-component/antd/association-field/SubTable.tsx
+++ packages/core/client/src/schema-component/antd/association-field/SubTable.tsx
@@ -11,87 +11,93 @@ import { useTranslation } from 'react-i18next';
 import { Table } from '../table-v2/Table';
 import { useAssociationFieldContext } from './hooks';
 
-export const SubTable: any = observer((props: any) => {
-  const { field } = useAssociationFieldContext<ArrayField>();
-  const { t } = useTranslation();
-  const move = (fromIndex: number, toIndex: number) => {
-    if (toIndex === undefined) return;
-    if (!isArr(field.value)) return;
-    if (fromIndex === toIndex) return;
-    return action(() => {
-      const fromItem = field.value[fromIndex];
-      field.value.splice(fromIndex, 1);
-      field.value.splice(toIndex, 0, fromItem);
-      exchangeArrayState(field, {
-        fromIndex,
-        toIndex,
+export const SubTable: any = observer(
+  (props: any) => {
+    const { field } = useAssociationFieldContext<ArrayField>();
+    const { t } = useTranslation();
+    const move = (fromIndex: number, toIndex: number) => {
+      if (toIndex === undefined) return;
+      if (!isArr(field.value)) return;
+      if (fromIndex === toIndex) return;
+      return action(() => {
+        const fromItem = field.value[fromIndex];
+        field.value.splice(fromIndex, 1);
+        field.value.splice(toIndex, 0, fromItem);
+        exchangeArrayState(field, {
+          fromIndex,
+          toIndex,
+        });
+        return field.onInput(field.value);
       });
-      return field.onInput(field.value);
-    });
-  };
-  field.move = move;
-  return (
-    <div
-      className={css`
-        .ant-formily-item-error-help {
-          display: none;
-        }
-        .ant-table-cell .ant-formily-item-error-help {
-          display: block;
-          position: absolute;
-          font-size: 12px;
-          top: 100%;
-          background: #fff;
-          width: 100%;
-          margin-top: 3px;
-          padding: 3px;
-          z-index: 1;
-          border-radius: 3px;
-          box-shadow: 0 0 10px #eee;
-          animation: none;
-          transform: translateY(0);
-          opacity: 1;
-        }
-      `}
-    >
-      <FormItem labelStyle={{ display: 'none' }} wrapperStyle={{ marginBottom: -25 }}>
-        <Table
-          className={css`
-            .ant-select-selector {
-              min-height: 31px;
-            }
-            .ant-formily-item {
-              margin-bottom: 0px;
-            }
-            .ant-formily-editable {
-              vertical-align: sub;
-            }
-            .ant-table table {
-              margin-bottom: 20px;
-            }
-          `}
-          size={'small'}
-          field={field}
-          showIndex
-          dragSort={field.editable}
-          showDel={field.editable}
-          pagination={false}
-          rowSelection={{ type: 'none', hideSelectAll: true }}
-        />
-      </FormItem>
-      {field.editable && (
-        <Button
-          type={'dashed'}
-          block
-          onClick={() => {
-            field.value = field.value || [];
-            field.value.push({});
-            field.onInput(field.value);
-          }}
-        >
-          {t('Add new')}
-        </Button>
-      )}
-    </div>
-  );
-});
+    };
+    field.move = move;
+    return (
+      <div
+        className={css`
+          .ant-formily-item-error-help {
+            display: none;
+          }
+          .ant-description-textarea {
+            line-height: 34px;
+          }
+          .ant-table-cell .ant-formily-item-error-help {
+            display: block;
+            position: absolute;
+            font-size: 12px;
+            top: 100%;
+            background: #fff;
+            width: 100%;
+            margin-top: 3px;
+            padding: 3px;
+            z-index: 1;
+            border-radius: 3px;
+            box-shadow: 0 0 10px #eee;
+            animation: none;
+            transform: translateY(0);
+            opacity: 1;
+          }
+        `}
+      >
+        <FormItem labelStyle={{ display: 'none' }} wrapperStyle={{ marginBottom: -25 }}>
+          <Table
+            className={css`
+              .ant-select-selector {
+                min-height: 31px;
+              }
+              .ant-formily-item {
+                margin-bottom: 0px;
+              }
+              .ant-formily-editable {
+                vertical-align: sub;
+              }
+              .ant-table table {
+                margin-bottom: 20px;
+              }
+            `}
+            size={'small'}
+            field={field}
+            showIndex
+            dragSort={field.editable}
+            showDel={field.editable}
+            pagination={false}
+            rowSelection={{ type: 'none', hideSelectAll: true }}
+          />
+        </FormItem>
+        {field.editable && (
+          <Button
+            type={'dashed'}
+            block
+            onClick={() => {
+              field.value = field.value || [];
+              field.value.push({});
+              field.onInput(field.value);
+            }}
+          >
+            {t('Add new')}
+          </Button>
+        )}
+      </div>
+    );
+  },
+  { displayName: 'SubTable' },
+);
