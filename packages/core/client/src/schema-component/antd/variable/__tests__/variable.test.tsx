5a7c94123f5d7b055110ceacb97bd57047053617
mytharcher <mytharcher@gmail.com>
2023-06-11 13:58:19 +0800

    test(client): remove unused test case

--- packages/core/client/src/schema-component/antd/variable/__tests__/variable.test.tsx
+++ packages/core/client/src/schema-component/antd/variable/__tests__/variable.test.tsx
@@ -61,7 +61,6 @@ describe('Variable', () => {
     const variableSelector = document.querySelector('.ant-select-selector') as HTMLElement;
     expect(input).toBeInTheDocument();
     expect(variableSelector).toBeInTheDocument();
-    expect(screen.getByText('Prettify')).toBeInTheDocument();
 
     // https://testing-library.com/docs/user-event/keyboard/
     await userEvent.type(input, '{{ "a": "');
@@ -71,13 +70,5 @@ describe('Variable', () => {
 
     await userEvent.type(input, '" }');
     expect(input.value).toMatchInlineSnapshot('"{ \\"a\\": \\"{{v1}}\\" }"');
-
-    // 格式化一下
-    await userEvent.click(screen.getByText('Prettify'));
-    expect(input.value).toMatchInlineSnapshot(`
-      "{
-        \\"a\\": \\"{{v1}}\\"
-      }"
-    `);
   });
 });
