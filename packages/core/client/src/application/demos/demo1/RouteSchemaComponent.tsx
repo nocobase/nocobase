a58d7bd6b9555ca15e01c46e0850e57ba90fa23f
dream2023 <1098626505@qq.com>
2023-06-20 11:27:12 +0800

    feat: delete application-v1 and router-switch

--- packages/core/client/src/application/demos/demo1/RouteSchemaComponent.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { SchemaComponent, useRoute } from '@nocobase/client';
-
-export const RouteSchemaComponent = () => {
-  const route = useRoute();
-  return <SchemaComponent schema={route.schema} />;
-};
