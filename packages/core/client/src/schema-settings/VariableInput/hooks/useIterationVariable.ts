cb52b80cf0d6eff948969551c2eb53744596966c
katherinehhh <shunai.tang@hand-china.com>
2023-06-22 20:19:34 +0800

    feat: data scope support variables for association fields (#2049)
    * feat: association field support data scope
    
    * refactor: association data scope
    
    * refactor: association data scope local
    
    * refactor: association data scope
    
    * refactor: association data scope  code improve
    
    * refactor: code improve
    
    * fix: useFormVariable
    
    * fix: useFormVariable
    
    * chore: useFormVariable
    
    * chore: useFormVariable
    
    * chore: useFormVariable
    
    * chore: useFormVariable
    
    * refactor: locale improve
    
    * refactor: locale improve
    
    ---------
    
    Co-authored-by: chenos <chenlinxh@gmail.com>

---
+++ packages/core/client/src/schema-settings/VariableInput/hooks/useIterationVariable.ts
@@ -0,0 +1,94 @@
+import { useMemo } from 'react';
+import { useCollectionManager } from '../../../collection-manager';
+import { useCompile, useGetFilterOptions } from '../../../schema-component';
+
+interface GetOptionsParams {
+  schema: any;
+  operator?: string;
+  maxDepth: number;
+  count?: number;
+  getFilterOptions: (collectionName: string) => any[];
+}
+
+const getChildren = (options: any[], { schema, operator, maxDepth, count = 1, getFilterOptions }: GetOptionsParams) => {
+  if (count > maxDepth) {
+    return [];
+  }
+
+  const result = options.map((option) => {
+    if ((option.type !== 'belongsTo' && option.type !== 'hasOne') || !option.target) {
+      return {
+        key: option.name,
+        value: option.name,
+        label: option.title,
+        // TODO: 现在是通过组件的名称来过滤能够被选择的选项，这样的坏处是不够精确，后续可以优化
+        // disabled: schema?.['x-component'] !== option.schema?.['x-component'],
+        disabled: false,
+      };
+    }
+
+    const children =
+      getChildren(getFilterOptions(option.target), {
+        schema,
+        operator,
+        maxDepth,
+        count: count + 1,
+        getFilterOptions,
+      }) || [];
+
+    return {
+      key: option.name,
+      value: option.name,
+      label: option.title,
+      children,
+      disabled: children.every((child) => child.disabled),
+    };
+  });
+
+  return result;
+};
+
+export const useIterationVariable = ({
+  blockForm,
+  collectionField,
+  operator,
+  schema,
+  level,
+  rootCollection,
+}: {
+  blockForm?: any;
+  collectionField: any;
+  operator?: any;
+  schema: any;
+  level?: number;
+  rootCollection?: string;
+}) => {
+  const compile = useCompile();
+  const getFilterOptions = useGetFilterOptions();
+  const fields = getFilterOptions(collectionField?.collectionName);
+  const children = useMemo(() => {
+    const allowFields = fields.filter((field) => {
+      return Object.keys(blockForm.fields).some((name) => name.includes(field.name));
+    });
+    return (
+      getChildren(allowFields, {
+        schema,
+        operator,
+        maxDepth: level || 3,
+        getFilterOptions,
+      }) || []
+    );
+  }, [operator, schema, blockForm]);
+
+  return useMemo(() => {
+    return rootCollection !== collectionField?.collectionName && children.length > 0
+      ? compile({
+          label: `{{t("Current object")}}`,
+          value: '$iteration',
+          key: '$iteration',
+          disabled: children.every((option) => option.disabled),
+          children: children,
+        })
+      : null;
+  }, [children]);
+};
