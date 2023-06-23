68f9194d76411d840bd87005edc4ad5a2af5820a
Rairn <958414905@qq.com>
2023-06-23 13:26:32 +0800

    refactor: remove useless code

--- packages/core/client/src/schema-component/antd/__builtins__/hooks/index.ts
+++ packages/core/client/src/schema-component/antd/__builtins__/hooks/index.ts
@@ -1,4 +1,4 @@
-export * from './useClickAway';
 export * from './useConfig';
 export * from './usePrefixCls';
 export * from './useToken';
+
--- packages/core/client/src/schema-component/antd/__builtins__/index.ts
+++ packages/core/client/src/schema-component/antd/__builtins__/index.ts
@@ -1,7 +1,3 @@
-export * from './dayjs';
 export * from './hooks';
-export * from './loading';
-export * from './pickDataProps';
-export * from './portal';
-export * from './sort';
 export * from './style';
+
