78f09895794fcdd34f8c52cd8a2d51f0d26fa7c5
katherinehhh <katherine_15995@163.com>
2023-06-24 16:24:14 +0800

    refactor:  performance optimization of association field data scope

--- packages/core/client/src/schema-settings/VariableInput/hooks/useVariableOptions.ts
+++ packages/core/client/src/schema-settings/VariableInput/hooks/useVariableOptions.ts
@@ -1,16 +1,21 @@
 import { useMemo } from 'react';
 import { useValues } from '../../../schema-component/antd/filter/useValues';
 import { useDateVariable } from './useDateVariable';
-import { useUserVariable } from './useUserVariable';
 import { useFormVariable } from './useFormVariable';
 import { useIterationVariable } from './useIterationVariable';
+import { useUserVariable } from './useUserVariable';
 
 export const useVariableOptions = ({ form, collectionField, rootCollection }) => {
   const { operator, schema } = useValues();
   const userVariable = useUserVariable({ maxDepth: 3, schema });
   const dateVariable = useDateVariable({ operator, schema });
   const formVariabele = useFormVariable({ blockForm: form, rootCollection, schema });
-  const iterationVariabele = useIterationVariable({ blockForm: form, collectionField, schema, rootCollection });
+  const iterationVariabele = useIterationVariable({
+    blockForm: form,
+    currentCollection: collectionField.collectionName,
+    rootCollection,
+    schema,
+  });
 
   const result = useMemo(
     () => [userVariable, dateVariable, formVariabele, iterationVariabele].filter(Boolean),
