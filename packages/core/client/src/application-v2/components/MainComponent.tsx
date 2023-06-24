f2a991909c5b604d456bdbd63b1478780a7d98ae
dream2023 <1098626505@qq.com>
2023-06-24 16:44:52 +0800

    fix: remove deps

--- packages/core/client/src/application-v2/components/MainComponent.tsx
+++ packages/core/client/src/application-v2/components/MainComponent.tsx
@@ -1,16 +1,11 @@
 import React, { useMemo } from 'react';
-import { SchemaComponentOptions } from '../../schema-component/core/SchemaComponentOptions';
 import { useApp } from '../hooks';
 
 export const MainComponent = React.memo(() => {
   const app = useApp();
   const Router = useMemo(() => app.router.createRouter(), [app]);
   const Providers = useMemo(() => app.renderProviders(), [app]);
-  return (
-    <SchemaComponentOptions components={app.components} scope={app.scopes}>
-      <Router BaseLayout={Providers} />
-    </SchemaComponentOptions>
-  );
+  return <Router BaseLayout={Providers} />;
 });
 
 MainComponent.displayName = 'MainComponent';
