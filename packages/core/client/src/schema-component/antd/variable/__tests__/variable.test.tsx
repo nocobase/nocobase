25533e037779bf7f83821ec54a0027663ecb1951
Rairn <958414905@qq.com>
2023-06-24 13:31:11 +0800

    test: fix error

--- packages/core/client/src/schema-component/antd/variable/__tests__/variable.test.tsx
+++ packages/core/client/src/schema-component/antd/variable/__tests__/variable.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, sleep, userEvent, waitFor } from 'testUtils';
+import { render, screen, userEvent, waitFor } from 'testUtils';
 import App1 from '../demos/demo1';
 import App2 from '../demos/demo2';
 import App3 from '../demos/demo3';
@@ -12,47 +12,45 @@ describe('Variable', () => {
 
     await userEvent.click(screen.getByText('x'));
     await userEvent.click(screen.getByText('v1'));
-    await sleep(100);
-    expect(screen.getByText('v1', { selector: '.ant-tag' })).toMatchInlineSnapshot(`
-      <span
-        class="ant-tag ant-tag-blue css-dev-only-do-not-override-1wazalj"
-        contenteditable="false"
-      >
-        v1
-      </span>
-    `);
+    await waitFor(() => {
+      expect(screen.getByText('v1', { selector: '.ant-tag' })).toMatchInlineSnapshot(`
+        <span
+          class="ant-tag ant-tag-blue css-dev-only-do-not-override-1wazalj"
+          contenteditable="false"
+        >
+          v1
+        </span>
+      `);
+    });
   });
 
   it('Variable.TextArea', async () => {
     render(<App2 />);
 
-    await waitFor(async () => {
-      const input = document.querySelector('.ant-input') as HTMLElement;
-      const variableSelector = document.querySelector('.ant-select-selector') as HTMLElement;
-      expect(input).toBeInTheDocument();
-      expect(variableSelector).toBeInTheDocument();
+    const input = document.querySelector('.ant-input') as HTMLElement;
+    const variableSelector = document.querySelector('.ant-select-selector') as HTMLElement;
+    expect(input).toBeInTheDocument();
+    expect(variableSelector).toBeInTheDocument();
 
-      await userEvent.type(input, '1+');
-      await sleep(100);
-
-      await userEvent.click(variableSelector);
-      await userEvent.click(screen.getByText('v1'));
-      await sleep(100);
+    await userEvent.type(input, '1+');
+    await userEvent.click(variableSelector);
+    await userEvent.click(screen.getByText('v1'));
+    await waitFor(() => {
       expect(input).toMatchInlineSnapshot(`
-      <div
-        class="css-dev-only-do-not-override-1wazalj ant-input css-1p5yrh5"
-        contenteditable="true"
-      >
-        1+
-        <span
-          class="ant-tag ant-tag-blue"
-          contenteditable="false"
-          data-variable="v1"
+        <div
+          class="css-dev-only-do-not-override-1wazalj ant-input css-1p5yrh5"
+          contenteditable="true"
         >
-          v1
-        </span>
-      </div>
-    `);
+          1+
+          <span
+            class="ant-tag ant-tag-blue"
+            contenteditable="false"
+            data-variable="v1"
+          >
+            v1
+          </span>
+        </div>
+      `);
     });
   });
 
@@ -68,9 +66,9 @@ describe('Variable', () => {
     await userEvent.type(input, '{{ "a": "');
     await userEvent.click(variableSelector);
     await userEvent.click(screen.getByText('v1'));
-    await sleep(100);
-
     await userEvent.type(input, '" }');
-    // expect(input.value).toMatchInlineSnapshot('"{ \\"a\\": \\"{{v1}}\\" }"');
+    await waitFor(() => {
+      expect(input.value).toMatchInlineSnapshot('"{ \\"a\\": \\"{{v1}}\\" }"');
+    });
   });
 });
