b7597235995d8ea021aa65fb2a4e53852fae67aa
Bierxiensi <40264423+Bierxiensi@users.noreply.github.com>
2023-06-26 22:04:04 +0800

    Update config.ts
    修改跳转链接地址

--- docs/config.ts
+++ docs/config.ts
@@ -252,7 +252,7 @@ const sidebar = {
     {
       title: '@nocobase/acl',
       type: 'subMenu',
-      children: ['/api/acl/acl', '/api/acl/acl-role', '/api/acl/acl-resource'],
+      children: ['/api/acl', '/api/acl/acl-role', '/api/acl/acl-resource'],
     },
     {
       title: '@nocobase/client',
