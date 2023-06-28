36b6fe46d4f56b21618629c5f5614f87f5a6bc1f
Dunqing <dengqing0821@gmail.com>
2023-06-28 10:17:00 +0800

    chore: update X-Role

--- packages/core/auth/src/base/auth.ts
+++ packages/core/auth/src/base/auth.ts
@@ -44,7 +44,7 @@ export class BaseAuth extends Auth {
       const { userId, roleName } = await this.jwt.decode(token);
 
       if (roleName) {
-        this.ctx.headers['X-Role'] = roleName;
+        this.ctx.set('X-Role', roleName);
       }
 
       return await this.userRepository.findOne({
