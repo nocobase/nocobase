079434863e08fa446c6f89e8bb77dc93a46317ad
katherinehhh <katherine_15995@163.com>
2023-06-29 15:12:42 +0800

    fix: the first selection of the form template shows incomplete data

--- packages/core/client/src/schema-component/antd/form-v2/Templates.tsx
+++ packages/core/client/src/schema-component/antd/form-v2/Templates.tsx
@@ -96,6 +96,7 @@ export const Templates = ({ style = {}, form }) => {
             forEach(data, (value, key) => {
               if (value) {
                 form.values[key] = value;
+                form.setInitialValuesIn(key, value);
               }
             });
           }
