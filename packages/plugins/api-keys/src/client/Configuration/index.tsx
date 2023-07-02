74b0ae70a0f5ce5936517db0a91dee2570d107f3
Dunqing <dengqing0821@gmail.com>
2023-07-02 10:36:36 +0800

    fix(plugin-api-keys): use currentRoles instead of get roles from roles:list (#2163)

--- packages/plugins/api-keys/src/client/Configuration/index.tsx
+++ packages/plugins/api-keys/src/client/Configuration/index.tsx
@@ -1,13 +1,17 @@
 import { RecursionField } from '@formily/react';
-import { CollectionManagerProvider } from '@nocobase/client';
+import { CollectionManagerProvider, SchemaComponentOptions, useCurrentRoles } from '@nocobase/client';
 import React from 'react';
 import { apiKeysCollection } from '../../collections';
 import { configurationSchema } from './schema';
 
 export const Configuration = () => {
+  const currentRoles = useCurrentRoles();
+
   return (
     <CollectionManagerProvider collections={[apiKeysCollection]}>
-      <RecursionField schema={configurationSchema} />
+      <SchemaComponentOptions scope={{ currentRoles }}>
+        <RecursionField schema={configurationSchema} />
+      </SchemaComponentOptions>
     </CollectionManagerProvider>
   );
 };
