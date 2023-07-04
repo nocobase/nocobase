7c3e99c362b6f90d194dd94e3e889844820acc56
katherinehhh <katherine_15995@163.com>
2023-07-04 15:56:00 +0800

    refactor: code improve

--- packages/core/client/src/schema-component/antd/filter/useValues.ts
+++ packages/core/client/src/schema-component/antd/filter/useValues.ts
@@ -71,7 +71,7 @@ export const useValues = () => {
       const s1 = cloneDeep(option?.schema);
       const s2 = cloneDeep(operator?.schema);
       field.data.schema = merge(s1, s2);
-      field.data.value = operator.noValue ? operator.default || true : null;
+      field.data.value = operator.noValue ? operator.default || true : undefined;
       data2value();
     },
     setValue(value) {
