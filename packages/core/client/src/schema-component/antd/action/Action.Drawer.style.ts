8b5ccbdaaab0266fb0ea6682d96d3c7581b78fca
Rairn <958414905@qq.com>
2023-07-05 23:06:35 +0800

    refactor: fix style of Drawer

---
+++ packages/core/client/src/schema-component/antd/action/Action.Drawer.style.ts
@@ -0,0 +1,38 @@
+import { genStyleHook } from '../__builtins__';
+
+export const useStyles = genStyleHook('nb-action-drawer', (token) => {
+  const { componentCls } = token;
+
+  return {
+    [componentCls]: {
+      overflow: 'hidden',
+      '.ant-drawer-body': {
+        overflow: 'hidden !important',
+      },
+      '&.reset': {
+        '&.nb-action-popup': {
+          '.ant-drawer-header': { display: 'none' },
+          '.ant-drawer-body': { paddingTop: token.paddingSM },
+        },
+        '&.nb-record-picker-selector': {
+          '.nb-block-item': {
+            marginBottom: token.marginLG,
+            '.general-schema-designer': {
+              top: -token.sizeXS,
+              bottom: -token.sizeXS,
+              left: -token.sizeXS,
+              right: -token.sizeXS,
+            },
+          },
+        },
+      },
+
+      '.footer': {
+        display: 'flex',
+        justifyContent: 'flex-end',
+        width: '100%',
+        '.ant-btn': { marginRight: token.marginXS },
+      },
+    },
+  };
+});
