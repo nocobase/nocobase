285434a5f941e7ebb48d685f043038f85fdab82a
mytharcher <mytharcher@gmail.com>
2023-07-07 22:16:40 +0800

    fix(plugin-workflow): fix loop variable

--- packages/plugins/workflow/src/client/nodes/loop.tsx
+++ packages/plugins/workflow/src/client/nodes/loop.tsx
@@ -9,18 +9,20 @@ import { addButtonClass, branchBlockClass, branchClass, nodeSubtreeClass } from 
 import { nodesOptions, triggerOptions, useWorkflowVariableOptions, VariableOption } from '../variable';
 
 function findOption(options: VariableOption[], paths: string[]) {
-  let current = options;
+  let opts = options;
+  let option = null;
   for (let i = 0; i < paths.length; i++) {
     const path = paths[i];
-    const option = current.find((item) => item.value === path);
-    if (!option) {
-      return null;
+    const current = opts.find((item) => item.value === path);
+    if (!current) {
+      break;
     }
-    if (option.children) {
-      current = option.children;
+    option = current;
+    if (current.children) {
+      opts = current.children;
     }
   }
-  return current;
+  return option;
 }
 
 export default {
@@ -124,7 +126,7 @@ export default {
     // find target data model by path described in `config.target`
     // 1. get options from $context/$jobsMapByNodeId
     // 2. route to sub-options and use as loop target options
-    const targetOption: VariableOption = { key: 'item', value: 'item', label: lang('Loop target') };
+    let targetOption: VariableOption = { key: 'item', value: 'item', label: lang('Loop target') };
 
     if (typeof target === 'string' && target.startsWith('{{') && target.endsWith('}}')) {
       const paths = target
@@ -133,17 +135,19 @@ export default {
         .map((path) => path.trim());
 
       const targetOptions = [nodesOptions, triggerOptions].map((item: any) => {
-        const opts = typeof item.useOptions === 'function' ? item.useOptions(options).filter(Boolean) : null;
+        const opts = item.useOptions(options).filter(Boolean);
         return {
           label: compile(item.title),
           value: item.value,
           key: item.value,
-          children: compile(opts),
+          children: opts,
           disabled: opts && !opts.length,
         };
       });
 
-      targetOption.children = findOption(targetOptions, paths);
+      const found = findOption(targetOptions, paths);
+
+      targetOption = Object.assign({ ...targetOption }, found, targetOption);
     }
 
     return [
