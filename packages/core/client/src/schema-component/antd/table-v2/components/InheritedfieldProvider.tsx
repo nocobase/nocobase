93e07a93306443e871b1ec4fc27b34af2e02f0f5
katherinehhh <katherine_15995@163.com>
2023-07-10 15:49:22 +0800

    refactor: code improve

--- packages/core/client/src/schema-component/antd/table-v2/components/InheritedfieldProvider.tsx
+++ packages/core/client/src/schema-component/antd/table-v2/components/InheritedfieldProvider.tsx
@@ -1,33 +1,33 @@
-import { RecursionField } from '@formily/react';
+import { observer, RecursionField } from '@formily/react';
 import React from 'react';
-import { useCollectionManager } from '../../../../collection-manager';
 import { useRecord } from '../../../../record-provider';
-export const InheritedFieldProvider = (props) => {
-  const { schema, basePath } = props;
-  const record = useRecord();
-  const { getCollectionJoinField } = useCollectionManager();
-  const fieldSchema = schema.reduceProperties((buf, s) => {
-    if (s['x-component'] === 'CollectionField') {
-      return s;
-    }
-    return buf;
-  }, null);
-  if (fieldSchema && record?.__collection) {
-    const fieldName = `${record.__collection}.${fieldSchema.name}`;
-    const collectionField = getCollectionJoinField(fieldName);
-    const newSchema = {
-      ...schema.toJSON(),
-      properties: {
-        [fieldSchema.name]: {
-          ...fieldSchema.toJSON(),
-          'x-collection-field': fieldName,
-          'x-component-props': {
-            ...fieldSchema['x-component-props'],
+export const InheritedFieldProvider = observer(
+  (props) => {
+    const { schema, basePath } = props;
+    const record = useRecord();
+    const fieldSchema = schema.reduceProperties((buf, s) => {
+      if (s['x-component'] === 'CollectionField') {
+        return s;
+      }
+      return buf;
+    }, null);
+    if (fieldSchema && record?.__collection) {
+      const fieldName = `${record.__collection}.${fieldSchema.name}`;
+      const newSchema = {
+        ...schema.toJSON(),
+        properties: {
+          [fieldSchema.name]: {
+            ...fieldSchema.toJSON(),
+            'x-collection-field': fieldName,
+            'x-component-props': {
+              ...fieldSchema['x-component-props'],
+            },
           },
         },
-      },
-    };
-    return <RecursionField basePath={basePath} schema={newSchema} onlyRenderProperties />;
-  }
-  return props.children;
-};
+      };
+      return <RecursionField basePath={basePath} schema={newSchema} onlyRenderProperties />;
+    }
+    return props.children;
+  },
+  { displayName: 'InheritedFieldProvider' },
+);
