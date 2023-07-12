714deae58c4d18602c2a4b4c4dce1278cb8edd35
Rain <958414905@qq.com>
2023-07-12 11:35:04 +0800

    fix: fix gap between blocks (T-896)

--- packages/core/client/src/schema-component/antd/card-item/style.ts
+++ packages/core/client/src/schema-component/antd/card-item/style.ts
@@ -5,19 +5,6 @@ const useStyles = genStyleHook('nb-card-item', (token) => {
 
   return {
     [componentCls]: {
-      height: '100%',
-      '.ant-card': {
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100%',
-        '.ant-card-body': {
-          height: '1px',
-          flex: 1,
-          display: 'flex',
-          flexDirection: 'column',
-          overflow: 'hidden',
-        },
-      },
       '.card': {
         marginBottom: token.marginLG,
       },
