0b9b2e603c8c9e17b94450445ef8ecdf359b7272
katherinehhh <katherine_15995@163.com>
2023-07-14 09:22:28 +0800

    refactor: code improve

--- packages/plugins/workflow/src/client/nodes/manual/WorkflowTodoBlockInitializer.tsx
+++ packages/plugins/workflow/src/client/nodes/manual/WorkflowTodoBlockInitializer.tsx
@@ -1,6 +1,7 @@
-import { TableOutlined } from '@ant-design/icons';
-import { SchemaInitializer } from '@nocobase/client';
 import React, { FC } from 'react';
+import { TableOutlined } from '@ant-design/icons';
+
+import { SchemaInitializer, useCollectionManager } from '@nocobase/client';
 
 export const WorkflowTodoBlockInitializer: FC<any> = ({ insert, ...rest }) => {
   return (
@@ -10,19 +11,18 @@ export const WorkflowTodoBlockInitializer: FC<any> = ({ insert, ...rest }) => {
       onClick={() => {
         insert({
           type: 'void',
-          'x-component': 'WorkflowTodo',
-          // // 'x-decorator': 'WorkflowTodo.Decorator',
-          // // 'x-decorator-props': {},
-          // 'x-component': 'CardItem',
-          // 'x-designer': 'TableBlockDesigner',
-          // properties: {
-          //   todos: {
-          //     type: 'void',
-          //     'x-component': 'WorkflowTodo',
-          //   },
-          // },
+          'x-decorator': 'WorkflowTodo.Decorator',
+          'x-decorator-props': {},
+          'x-component': 'CardItem',
+          'x-designer': 'TableBlockDesigner',
+          properties: {
+            todos: {
+              type: 'void',
+              'x-component': 'WorkflowTodo',
+            },
+          },
         });
       }}
     />
   );
-};
+}
