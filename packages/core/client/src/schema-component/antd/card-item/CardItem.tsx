39eed25d6f8319c97881f9bbc9219c7fa373cd77
xilesun <2013xile@gmail.com>
2023-07-14 11:41:40 +0800

    fix: card title bug

--- packages/core/client/src/schema-component/antd/card-item/CardItem.tsx
+++ packages/core/client/src/schema-component/antd/card-item/CardItem.tsx
@@ -1,7 +1,6 @@
 import { useFieldSchema } from '@formily/react';
 import { Card } from 'antd';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { useSchemaTemplate } from '../../../schema-templates';
 import { BlockItem } from '../block-item';
 
@@ -10,11 +9,10 @@ export const CardItem: React.FC = (props) => {
   const template = useSchemaTemplate();
   const fieldSchema = useFieldSchema();
   const templateKey = fieldSchema['x-template-key'];
-  const title = fieldSchema['x-component-props']?.title || '';
 
   return templateKey && !template ? null : (
     <BlockItem className={'noco-card-item'}>
-      <Card style={{ marginBottom: 'var(--nb-spacing)' }} bordered={false} {...restProps} title={title}>
+      <Card style={{ marginBottom: 'var(--nb-spacing)' }} bordered={false} {...restProps}>
         {props.children}
       </Card>
     </BlockItem>
