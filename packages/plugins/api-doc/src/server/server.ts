c9fd4ed9415683e6cfdcb75d8ea689888de70d1e
Dunqing <dengqing0821@gmail.com>
2023-07-19 11:02:23 +0800

    refactor: implement `SwaggerManager`

--- packages/plugins/api-doc/src/server/server.ts
+++ packages/plugins/api-doc/src/server/server.ts
@@ -1,213 +1,17 @@
 import { Plugin } from '@nocobase/server';
 import send from 'koa-send';
-import { merge } from 'lodash';
 import { resolve } from 'path';
-import baseSwagger from './base-swagger';
-import { getSwaggerDocument } from './load';
-
-const SchemaTypeMapping = {
-  uid: 'string',
-  json: 'object',
-  jsonb: 'object',
-  text: 'string',
-  bigInt: 'integer',
-  sort: 'integer',
-  content: 'string',
-  set: 'array',
-};
-
-const createDefaultActionSwagger = ({ collection }) => {
-  const responses = {
-    default: {
-      content: {
-        'application/json': {
-          schema: {
-            $ref: `#/components/schemas/${collection.name}`,
-          },
-        },
-      },
-    },
-  };
-  const requestBody = {
-    content: {
-      'application/json': {
-        schema: {
-          $ref: `#/components/schemas/${collection.name}`,
-        },
-      },
-    },
-  };
-  return {
-    list: {
-      method: 'get',
-      responses,
-    },
-    create: {
-      method: 'post',
-      requestBody,
-    },
-    get: {
-      method: 'get',
-      responses,
-    },
-    update: {
-      method: 'put',
-      requestBody,
-      responses,
-    },
-    destroy: {
-      method: 'delete',
-      responses,
-    },
-    add: {
-      method: 'post',
-      requestBody,
-      responses,
-    },
-    set: {
-      method: 'post',
-      requestBody,
-    },
-    remove: {
-      method: 'delete',
-      responses,
-    },
-    toggle: {
-      method: 'post',
-      requestBody,
-      responses,
-    },
-    move: {
-      method: 'post',
-      requestBody,
-    },
-  };
-};
+import { SwaggerManager } from './swagger';
 
 const API_DOC_PATH = '/api/_documentation';
 
 export default class APIDoc extends Plugin {
+  swagger: SwaggerManager;
+  constructor(app, options) {
+    super(app, options);
+    this.swagger = new SwaggerManager(this);
+  }
   beforeLoad() {}
-
-  generateModels = () => {
-    const models = {};
-    this.app.i18n;
-    this.db.collections.forEach((collection) => {
-      const properties = {};
-      const model = {
-        type: 'object',
-        properties,
-      };
-
-      collection.forEachField((field) => {
-        const property: Record<string, any> = {};
-        if (field.type === 'array') {
-          property['items'] = {
-            type: 'object',
-          };
-        } else if (field.type === 'hasMany' || field.type === 'belongsToMany') {
-          property['type'] = 'array';
-          property['items'] = {
-            $ref: `#/components/schemas/${field.target}`,
-          };
-        } else if (field.type === 'belongsTo' || field.type === 'hasOne') {
-          property['type'] = 'object';
-          property['$ref'] = `#/components/schemas/${field.target}`;
-        } else {
-          property.type = SchemaTypeMapping[field.type] || field.type;
-        }
-        properties[field.name] = property;
-      });
-      models[collection.name] = model;
-    });
-    return models;
-  };
-
-  generatePaths = () => {
-    const options = this.app.resourcer.options;
-    const accessors = {
-      // 常规 actions
-      list: 'list',
-      create: 'create',
-      get: 'get',
-      update: 'update',
-      delete: 'destroy',
-      // associate 操作
-      add: 'add',
-      set: 'set',
-      remove: 'remove',
-      toggle: 'toggle',
-      move: 'move',
-      ...(options.accessors || {}),
-    };
-
-    const single = {
-      '/{resourceName}': [accessors.list, accessors.create, accessors.delete],
-      '/{resourceName}/{resourceIndex}': [accessors.get, accessors.update, accessors.delete],
-      '/{associatedName}/{associatedIndex}/{resourceName}': [
-        accessors.list,
-        accessors.create,
-        accessors.delete,
-        accessors.toggle,
-        accessors.add,
-        accessors.remove,
-      ],
-      '/{associatedName}/{associatedIndex}/{resourceName}/{resourceIndex}': [
-        accessors.get,
-        accessors.update,
-        accessors.delete,
-        accessors.remove,
-        accessors.toggle,
-        accessors.set,
-        accessors.move,
-      ],
-    };
-
-    const paths = {};
-    this.db.collections.forEach((collection) => {
-      const { name } = collection;
-      if (!this.app.resourcer.isDefined(name)) {
-        return;
-      }
-
-      const { actions } = this.app.resourcer.getResource(name);
-      const collectionMethods = {};
-      Object.entries(single).forEach(([path, actionNames]) => {
-        actionNames.forEach((actionName) => {
-          const actionMethods = createDefaultActionSwagger({
-            collection,
-          });
-          const { method = 'post', ...swaggerArgs } = actionMethods[actionName];
-          const parameters = [];
-          ['associatedName', 'associatedIndex', 'resourceIndex'].forEach((name) => {
-            if (path.includes(`{${name}}`)) {
-              parameters.push({
-                name,
-                required: true,
-                schema: {
-                  type: 'string',
-                },
-              });
-            }
-          });
-
-          if (actions.has(actionName)) {
-            collectionMethods[path.replace('{resourceName}', `${name}:${actionName}`)] = {
-              [method]: {
-                tags: [name],
-                description: `${name}:${actionName}`,
-                parameters,
-                ...swaggerArgs,
-              },
-            };
-          }
-        });
-      });
-      Object.assign(paths, collectionMethods);
-    });
-    return paths;
-  };
-
   async load() {
     this.app.use(async (ctx, next) => {
       console.log(ctx.path);
@@ -221,20 +25,8 @@ export default class APIDoc extends Plugin {
       }
 
       if (ctx.path.startsWith('/api/swagger.json')) {
-        const swagger = await getSwaggerDocument(this.db);
         ctx.withoutDataWrapping = true;
-        const automaticGenerateSwagger = {
-          paths: this.generatePaths(),
-          components: {
-            schemas: this.generateModels(),
-          },
-          servers: [
-            {
-              url: (this.app.resourcer.options.prefix || '/').replace(/^[^/]/, '/$1'),
-            },
-          ],
-        };
-        ctx.body = merge(baseSwagger, automaticGenerateSwagger, swagger);
+        ctx.body = this.swagger.getSwagger();
       }
       await next();
     });
---
+++ packages/plugins/api-doc/src/server/swagger/index.ts
@@ -0,0 +1,141 @@
+import { merge } from '@nocobase/utils';
+import baseSwagger from '../base-swagger';
+import { getSwaggerDocument } from '../load';
+import ApiDocPlugin from '../server';
+import { SchemaTypeMapping } from './constants';
+import { createDefaultActionSwagger, getInterfaceCollection } from './helpers';
+export class SwaggerManager {
+  private swagger: Record<string, any> = baseSwagger;
+  private plugin: ApiDocPlugin;
+  private get app() {
+    return this.plugin.app;
+  }
+  private get db() {
+    return this.plugin.db;
+  }
+
+  private get baseSwagger() {
+    return Object.assign(baseSwagger, {
+      servers: [
+        {
+          url: (this.app.resourcer.options.prefix || '/').replace(/^[^/]/, '/$1'),
+        },
+      ],
+    });
+  }
+
+  constructor(plugin: ApiDocPlugin) {
+    this.plugin = plugin;
+    this.listenAppEvent();
+  }
+
+  private listenAppEvent() {
+    const eventCallback = () => {
+      this.generateSwagger();
+    };
+    this.app.on('afterLoad', eventCallback);
+    this.app.on('afterReload', eventCallback);
+    this.app.on('afterEnablePlugin', eventCallback);
+    this.app.on('afterDisablePlugin', eventCallback);
+  }
+
+  private generateSchemas() {
+    const schemas = {};
+    this.app.i18n;
+    this.db.collections.forEach((collection) => {
+      const properties = {};
+      const model = {
+        type: 'object',
+        properties,
+      };
+
+      collection.forEachField((field) => {
+        const property: Record<string, any> = {};
+        if (field.type === 'array') {
+          property['items'] = {
+            type: 'object',
+          };
+        } else if (field.type === 'hasMany' || field.type === 'belongsToMany') {
+          property['type'] = 'array';
+          property['items'] = {
+            $ref: `#/components/schemas/${field.target}`,
+          };
+        } else if (field.type === 'belongsTo' || field.type === 'hasOne') {
+          property['type'] = 'object';
+          property['$ref'] = `#/components/schemas/${field.target}`;
+        } else {
+          property.type = SchemaTypeMapping[field.type] || field.type;
+        }
+        properties[field.name] = property;
+      });
+      schemas[collection.name] = model;
+    });
+    return schemas;
+  }
+
+  private generateCollectionBuiltInInterface() {
+    const paths = {};
+    const IC = getInterfaceCollection(this.app.resourcer.options);
+    this.db.collections.forEach((collection) => {
+      const { name } = collection;
+      if (!this.app.resourcer.isDefined(name)) {
+        return;
+      }
+
+      const { actions } = this.app.resourcer.getResource(name);
+      const collectionMethods = {};
+      Object.entries(IC).forEach(([path, actionNames]) => {
+        actionNames.forEach((actionName) => {
+          const actionMethods = createDefaultActionSwagger({
+            collection,
+          });
+          const { method = 'post', ...swaggerArgs } = actionMethods[actionName];
+          const parameters = [];
+          ['associatedName', 'associatedIndex', 'resourceIndex'].forEach((name) => {
+            if (path.includes(`{${name}}`)) {
+              parameters.push({
+                name,
+                required: true,
+                schema: {
+                  type: 'string',
+                },
+              });
+            }
+          });
+
+          if (actions.has(actionName)) {
+            collectionMethods[path.replace('{resourceName}', `${name}:${actionName}`)] = {
+              [method]: {
+                tags: [name],
+                description: `${name}:${actionName}`,
+                parameters,
+                ...swaggerArgs,
+              },
+            };
+          }
+        });
+      });
+      Object.assign(paths, collectionMethods);
+    });
+    return paths;
+  }
+
+  private loadSwaggers() {
+    return getSwaggerDocument(this.db);
+  }
+
+  async generateSwagger() {
+    const newSchemas = {
+      paths: await this.generateCollectionBuiltInInterface(),
+      components: {
+        schemas: await this.generateSchemas(),
+      },
+    };
+    this.swagger = merge(merge(this.baseSwagger, newSchemas), await this.loadSwaggers());
+    return this.swagger;
+  }
+
+  getSwagger() {
+    return this.swagger;
+  }
+}
