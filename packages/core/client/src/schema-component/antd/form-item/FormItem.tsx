137e3eb171411343ecceca22b5b4fce514eadcc2
katherinehhh <shunai.tang@hand-china.com>
2023-07-25 13:51:50 +0800

    refactor: date field UI supports configuration formatting (#2241)
    * refactor: support format in datetime field
    
    * refactor: support format config  in datetime field
    
    * fix: datetime field support dateformat config
    
    * fix: datetime field support dateformat config
    
    * refactor: table column support date format config
    
    * refactor: table column support date format config
    
    * refactor: table column support date format config
    
    * refactor: code improve
    
    * refactor: code improve
    
    * fix: merge bug
    
    * fix: merge bug
    
    * style: style improve
    
    * Update index.tsx
    
    * refactor: code improve
    
    * refactor: code improve
    
    * refactor: code improve
    
    * refactor: code improve
    
    * refactor: code improve
    
    * refactor: code improve
    
    * refactor: code improve
    
    * refactor: locale improve
    
    * refactor: locale improve
    
    ---------
    
    Co-authored-by: chenos <chenlinxh@gmail.com>

--- packages/core/client/src/schema-component/antd/form-item/FormItem.tsx
+++ packages/core/client/src/schema-component/antd/form-item/FormItem.tsx
@@ -1,7 +1,7 @@
 import { css, cx } from '@emotion/css';
-import { ArrayCollapse, ArrayItems, FormLayout, FormItem as Item } from '@formily/antd-v5';
+import { ArrayCollapse, ArrayItems, FormItem as Item, FormLayout } from '@formily/antd-v5';
 import { Field } from '@formily/core';
-import { ISchema, Schema, observer, useField, useFieldSchema } from '@formily/react';
+import { ISchema, observer, Schema, useField, useFieldSchema } from '@formily/react';
 import { dayjs } from '@nocobase/utils/client';
 import { Select } from 'antd';
 import _ from 'lodash';
@@ -20,9 +20,9 @@ import {
 } from '../../../collection-manager';
 import { isTitleField } from '../../../collection-manager/Configuration/CollectionFields';
 import { GeneralSchemaItems } from '../../../schema-items/GeneralSchemaItems';
-import { GeneralSchemaDesigner, SchemaSettings, isPatternDisabled, isShowDefaultValue } from '../../../schema-settings';
-import { VariableInput } from '../../../schema-settings/VariableInput/VariableInput';
+import { GeneralSchemaDesigner, isPatternDisabled, isShowDefaultValue, SchemaSettings } from '../../../schema-settings';
 import { useIsShowMultipleSwitch } from '../../../schema-settings/hooks/useIsShowMultipleSwitch';
+import { VariableInput } from '../../../schema-settings/VariableInput/VariableInput';
 import { isVariable, parseVariables, useVariablesCtx } from '../../common/utils/uitls';
 import { SchemaComponent } from '../../core';
 import { useCompile, useDesignable, useFieldModeOptions } from '../../hooks';
@@ -175,6 +175,8 @@ FormItem.Designer = function Designer() {
   const isPickerMode = fieldSchema['x-component-props']?.mode === 'Picker';
   const showFieldMode = isAssociationField && fieldModeOptions && !isTableField;
   const showModeSelect = showFieldMode && isPickerMode;
+  const isDateField = ['datetime', 'createdAt', 'updatedAt'].includes(collectionField.interface);
+
   return (
     <GeneralSchemaDesigner>
       <GeneralSchemaItems />
@@ -840,6 +842,7 @@ FormItem.Designer = function Designer() {
           }}
         />
       )}
+      {isDateField && <SchemaSettings.DataFormat fieldSchema={fieldSchema} />}
       {collectionField && <SchemaSettings.Divider />}
       <SchemaSettings.Remove
         key="remove"
