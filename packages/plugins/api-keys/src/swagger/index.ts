d6459d22726b35470110df4cd3f43d1721ce74d5
Dunqing <dengqing0821@gmail.com>
2023-07-25 18:32:20 +0800

    fix: key

--- packages/plugins/api-keys/src/swagger/index.ts
+++ packages/plugins/api-keys/src/swagger/index.ts
@@ -93,7 +93,7 @@ export default {
             type: 'string',
             example: 'key-name',
           },
-          roles: {
+          role: {
             type: 'object',
             // $ref: '#/components/schemas/roles'
           },
