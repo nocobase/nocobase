acdbd60605aee1fc540545fd8e6a7bb6a394bd6e
Rain <958414905@qq.com>
2023-07-27 10:15:58 +0800

    refactor: remove useless code

--- packages/core/client/src/collection-manager/interfaces/types.ts
+++ packages/core/client/src/collection-manager/interfaces/types.ts
@@ -10,8 +10,6 @@ export interface IField extends ISchema {
   default?: IDefault;
   operators?: any[];
   filterable?: any;
-  /** 不支持使用变量的值进行设置 */
-  invariable?: boolean;
   // NOTE: set to `true` means field could be used as a title field
   titleUsable?: boolean;
   [key: string]: any;
