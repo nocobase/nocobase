929eb3e5b2e33f761ccbcb8c58d742667a1e7ec9
katherinehhh <katherine_15995@163.com>
2023-08-01 14:41:51 +0800

    refactor: code improve

--- packages/core/client/src/schema-component/antd/association-field/InternaPopoverNester.tsx
+++ packages/core/client/src/schema-component/antd/association-field/InternaPopoverNester.tsx
@@ -2,27 +2,23 @@ import { Popover } from 'antd';
 import { EditOutlined } from '@ant-design/icons';
 import { useField, useFieldSchema } from '@formily/react';
 import React from 'react';
+import { useDesignable } from '../../../';
 import { ReadPrettyInternalViewer } from './InternalViewer';
 import { InternalNester } from './InternalNester';
 
 function isEmptyObject(obj) {
-  // 判断是否是对象
   if (typeof obj !== 'object' || obj === null) {
     return false;
   }
-
-  // 如果是数组
   if (Array.isArray(obj)) {
-    // 判断数组长度是否为1，并且第一个元素是否是空对象
     return obj.length === 1 && typeof obj[0] === 'object' && Object.keys(obj[0]).length === 0;
   }
-
-  // 如果是普通对象
   return Object.keys(obj).length === 0;
 }
 
 export const InternaPopoverNester = (props) => {
   const field: any = useField();
+  const { dn } = useDesignable();
   const content = (
     <div style={{ minWidth: '400px', maxHeight: '440px', overflow: 'auto' }}>
       <InternalNester {...props} />
@@ -34,8 +30,9 @@ export const InternaPopoverNester = (props) => {
       trigger="click"
       placement="topLeft"
       onOpenChange={(open) => {
-        console.log(open);
-        console.log(field.value);
+        if (!open) {
+          dn.refresh();
+        }
       }}
     >
       <span style={{ cursor: 'pointer' }}>
