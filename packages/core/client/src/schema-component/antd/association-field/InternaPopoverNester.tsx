7fca39fcb3c83e00fc2dea8bd8d013aaf9ef5ac4
katherinehhh <katherine_15995@163.com>
2023-08-01 16:58:29 +0800

    refactor: code improve

--- packages/core/client/src/schema-component/antd/association-field/InternaPopoverNester.tsx
+++ packages/core/client/src/schema-component/antd/association-field/InternaPopoverNester.tsx
@@ -1,7 +1,7 @@
 import { Popover } from 'antd';
 import { EditOutlined } from '@ant-design/icons';
-import { useField, useFieldSchema } from '@formily/react';
-import React, { useEffect, useState } from 'react';
+import { useField, observer } from '@formily/react';
+import React, { useMemo, useState } from 'react';
 import { ReadPrettyInternalViewer } from './InternalViewer';
 import { InternalNester } from './InternalNester';
 
@@ -15,29 +15,36 @@ function isEmptyObject(obj) {
   return Object.keys(obj).length === 0;
 }
 
-export const InternaPopoverNester = (props) => {
-  const field: any = useField();
-  const content = (
-    <div style={{ minWidth: '400px', maxHeight: '440px', overflow: 'auto' }}>
-      <InternalNester {...props} />
-    </div>
-  );
-  const [value, setValue] = useState(field.value);
-  return (
-    <Popover
-      content={content}
-      trigger="click"
-      placement="topLeft"
-      onOpenChange={(open) => {
-        if (!open) {
-          const data = Array.isArray(field.value) ? [...field.value] : { ...field.value };
-          setValue(data);
-        }
-      }}
-    >
-      <span style={{ cursor: 'pointer', display: 'inline-block', width: '100%' }}>
-        {!isEmptyObject(value) ? <ReadPrettyInternalViewer {...props} enableLink={true} /> : <EditOutlined />}
-      </span>
-    </Popover>
-  );
-};
+export const InternaPopoverNester = observer(
+  (props) => {
+    const field: any = useField();
+    const content = (
+      <div style={{ minWidth: '400px', maxHeight: '440px', overflow: 'auto' }}>
+        <InternalNester {...props} />
+      </div>
+    );
+    const [value, setValue] = useState(field.value);
+    const titleProps = {
+      ...props,
+      enableLink: true,
+    };
+    return (
+      <Popover
+        content={content}
+        trigger="click"
+        placement="topLeft"
+        onOpenChange={(open) => {
+          if (!open) {
+            const data = Array.isArray(field.value) ? [...field.value] : { ...field.value };
+            setValue(data);
+          }
+        }}
+      >
+        <span style={{ cursor: 'pointer', display: 'inline-block', width: '100%' }}>
+          {!isEmptyObject(value) ? <ReadPrettyInternalViewer {...titleProps} /> : <EditOutlined />}
+        </span>
+      </Popover>
+    );
+  },
+  { displayName: 'InternaPopoverNester' },
+);
