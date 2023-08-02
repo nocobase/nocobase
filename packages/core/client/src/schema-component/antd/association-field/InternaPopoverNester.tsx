6ed0947de3213e5fa8d4ff82988a29bc9834e566
katherinehhh <katherine_15995@163.com>
2023-08-02 14:28:13 +0800

    refactor: code improve

--- packages/core/client/src/schema-component/antd/association-field/InternaPopoverNester.tsx
+++ packages/core/client/src/schema-component/antd/association-field/InternaPopoverNester.tsx
@@ -1,25 +1,15 @@
 import { Popover } from 'antd';
 import { EditOutlined } from '@ant-design/icons';
 import { useField, observer } from '@formily/react';
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { ReadPrettyInternalViewer } from './InternalViewer';
 import { InternalNester } from './InternalNester';
-
-function isEmptyObject(obj) {
-  if (typeof obj !== 'object' || obj === null) {
-    return false;
-  }
-  if (Array.isArray(obj)) {
-    return obj.length === 1 && typeof obj[0] === 'object' && Object.keys(obj[0]).length === 0;
-  }
-  return Object.keys(obj).length === 0;
-}
 
 export const InternaPopoverNester = observer(
   (props) => {
     const field: any = useField();
     const content = (
-      <div style={{ minWidth: '400px', maxHeight: '440px', overflow: 'auto' }}>
+      <div style={{ minWidth: '600px', maxHeight: '440px', overflow: 'auto' }}>
         <InternalNester {...props} />
       </div>
     );
@@ -30,6 +20,7 @@ export const InternaPopoverNester = observer(
     };
     return (
       <Popover
+        overlayStyle={{ padding: '0px' }}
         content={content}
         trigger="click"
         placement="topLeft"
@@ -41,7 +32,10 @@ export const InternaPopoverNester = observer(
         }}
       >
         <span style={{ cursor: 'pointer', display: 'inline-block', minWidth: '400px' }}>
-          {!isEmptyObject(value) ? <ReadPrettyInternalViewer {...titleProps} /> : <EditOutlined />}
+          <div style={{ display: 'inline-flex' }}>
+            <ReadPrettyInternalViewer {...titleProps} />
+          </div>
+          <EditOutlined style={{ display: 'inline-flex', marginLeft: '5px' }} />
         </span>
       </Popover>
     );
