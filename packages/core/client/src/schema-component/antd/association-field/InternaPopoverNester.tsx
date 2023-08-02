5bfcced57a27c39dee6be7e5e50e46a277e9ea3c
katherinehhh <katherine_15995@163.com>
2023-08-02 15:19:06 +0800

    refactor: code improve

--- packages/core/client/src/schema-component/antd/association-field/InternaPopoverNester.tsx
+++ packages/core/client/src/schema-component/antd/association-field/InternaPopoverNester.tsx
@@ -1,13 +1,17 @@
 import { Popover } from 'antd';
 import { css } from '@emotion/css';
 import { EditOutlined } from '@ant-design/icons';
-import { useField, observer } from '@formily/react';
+import { observer } from '@formily/react';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { ReadPrettyInternalViewer } from './InternalViewer';
 import { InternalNester } from './InternalNester';
+import { useAssociationFieldContext } from './hooks';
 
 export const InternaPopoverNester = observer(
   (props) => {
+    const { options } = useAssociationFieldContext();
+    const { t } = useTranslation();
     const content = (
       <div
         style={{ minWidth: '600px', maxHeight: '440px', overflow: 'auto' }}
@@ -27,8 +31,15 @@ export const InternaPopoverNester = observer(
       ...props,
       enableLink: true,
     };
+
     return (
-      <Popover overlayStyle={{ padding: '0px' }} content={content} trigger="click" placement="topLeft">
+      <Popover
+        overlayStyle={{ padding: '0px' }}
+        content={content}
+        trigger="click"
+        placement="topLeft"
+        title={t(options?.uiSchema?.rawTitle)}
+      >
         <span style={{ cursor: 'pointer', display: 'inline-block', minWidth: '400px' }}>
           <div
             className={css`
