1b69bd6b817e08a16144e8265560c98ba38afef6
katherinehhh <katherine_15995@163.com>
2023-08-02 14:34:11 +0800

    refactor: code improve

--- packages/core/client/src/schema-component/antd/association-field/InternaPopoverNester.tsx
+++ packages/core/client/src/schema-component/antd/association-field/InternaPopoverNester.tsx
@@ -1,38 +1,40 @@
 import { Popover } from 'antd';
+import { css } from '@emotion/css';
 import { EditOutlined } from '@ant-design/icons';
 import { useField, observer } from '@formily/react';
-import React, { useState } from 'react';
+import React from 'react';
 import { ReadPrettyInternalViewer } from './InternalViewer';
 import { InternalNester } from './InternalNester';
 
 export const InternaPopoverNester = observer(
   (props) => {
-    const field: any = useField();
     const content = (
-      <div style={{ minWidth: '600px', maxHeight: '440px', overflow: 'auto' }}>
+      <div
+        style={{ minWidth: '600px', maxHeight: '440px', overflow: 'auto' }}
+        className={css`
+          min-width: 600px;
+          max-height: 440px;
+          overflow: auto;
+          .ant-card {
+            border: 0px;
+          }
+        `}
+      >
         <InternalNester {...props} />
       </div>
     );
-    const [value, setValue] = useState(field.value);
     const titleProps = {
       ...props,
       enableLink: true,
     };
     return (
-      <Popover
-        overlayStyle={{ padding: '0px' }}
-        content={content}
-        trigger="click"
-        placement="topLeft"
-        onOpenChange={(open) => {
-          if (!open) {
-            const data = Array.isArray(field.value) ? [...field.value] : { ...field.value };
-            setValue(data);
-          }
-        }}
-      >
+      <Popover overlayStyle={{ padding: '0px' }} content={content} trigger="click" placement="topLeft">
         <span style={{ cursor: 'pointer', display: 'inline-block', minWidth: '400px' }}>
-          <div style={{ display: 'inline-flex' }}>
+          <div
+            className={css`
+              display: inline-flex;
+            `}
+          >
             <ReadPrettyInternalViewer {...titleProps} />
           </div>
           <EditOutlined style={{ display: 'inline-flex', marginLeft: '5px' }} />
