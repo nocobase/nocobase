de37be9591c0a3352687bf7e7ac85464590a1f57
katherinehhh <katherine_15995@163.com>
2023-08-07 22:54:17 +0800

    fix: merge bug

--- packages/core/database/src/fields/sort-field.ts
+++ packages/core/database/src/fields/sort-field.ts
@@ -10,105 +10,40 @@ export class SortField extends Field {
     return DataTypes.BIGINT;
   }
 
-  isAssociatedScopeKey() {
-    return this.options.scopeKey && this.options.scopeKey.includes('.');
-  }
-
-  scopeKeyAsField = () => {
-    const scopeKey = this.options.scopeKey;
-
-    if (!scopeKey) return null;
-
-    const model = this.collection.model;
-    if (!this.isAssociatedScopeKey()) return model.rawAttributes[scopeKey].field;
-
-    // get last two parts of scopeKey
-    const parts = scopeKey.split('.');
-    return parts[parts.length - 2] + '.' + parts[parts.length - 1];
-  };
-
   setSortValue = async (instance, options) => {
-    if (options.withAssociations) return;
-
     const { name, scopeKey } = this.options;
+    const { model } = this.context.collection;
 
-    if (instance.get(name) && !options.reset) return;
-
-    const { transaction } = options;
-
-    if (
-      !options.reset &&
-      isNumber(instance.get(name)) &&
-      instance._previousDataValues[scopeKey] == instance[scopeKey]
-    ) {
+    if (isNumber(instance.get(name)) && instance._previousDataValues[scopeKey] == instance[scopeKey]) {
       return;
     }
 
-    const filter = {};
-    const primaryKey = instance.constructor.primaryKeyAttribute;
-
-    filter[`${primaryKey}.$ne`] = instance.get(primaryKey);
+    const where = {};
 
     if (scopeKey) {
-      try {
-        filter[scopeKey] = this.isAssociatedScopeKey()
-          ? await instance.lazyLoadGet(scopeKey, { transaction })
-          : instance.get(scopeKey);
-
-        if (filter[scopeKey] === undefined) {
-          filter[scopeKey] = null;
-        }
-      } catch (e) {
-        if (e.message.includes('not found')) {
-          filter[scopeKey] = null;
-        } else {
-          throw e;
-        }
+      const value = instance.get(scopeKey);
+      if (value !== undefined && value !== null) {
+        where[scopeKey] = value;
       }
     }
 
     await sortFieldMutex.runExclusive(async () => {
-      const max = await this.context.collection.repository.max({
-        field: name,
-        filter,
-        transaction,
-      });
-
+      const max = await model.max<number, any>(name, { ...options, where });
       const newValue = (max || 0) + 1;
-      await instance.update({ [name]: newValue }, { transaction, hooks: false, silent: true });
+      instance.set(name, newValue);
     });
   };
 
   onScopeChange = async (instance, options) => {
     const { scopeKey } = this.options;
-
     if (scopeKey && !instance.isNewRecord && instance._previousDataValues[scopeKey] != instance[scopeKey]) {
-      await this.setSortValue(instance, {
-        ...options,
-        reset: true,
-      });
+      await this.setSortValue(instance, options);
     }
   };
 
-  afterChangeScope = async (instance, options) => {
-    await this.setSortValue(instance, {
-      ...options,
-      reset: true,
-    });
-  };
-
   initRecordsSortValue = async ({ transaction }) => {
-    const db = this.collection.db;
-
-    const sequelizeQueryInterface = db.sequelize.getQueryInterface();
-    const q = sequelizeQueryInterface.quoteIdentifier.bind(sequelizeQueryInterface);
-    const qs = sequelizeQueryInterface.quoteIdentifiers.bind(sequelizeQueryInterface);
-
-    const queryInterface = db.queryInterface;
-
     const orderField = (() => {
       const model = this.collection.model;
-
       if (model.primaryKeyAttribute) {
         return model.primaryKeyAttribute;
       }
@@ -119,80 +54,69 @@ export class SortField extends Field {
       throw new Error(`can not find order key for collection ${this.collection.name}`);
     })();
 
-    const needInit = async (
-      scopeKey,
-    ): Promise<
-      Array<{
-        need_init: boolean;
-        scope_key?: string;
-      }>
-    > => {
-      const SQL = `
-        select count(*) =
-               count(
-                 CASE
-                   WHEN ${q(this.columnName())} IS NULL THEN 1
-                   ELSE NULL
-                   END
-                 ) AND count(*) > 0 as need_init
-          ${scopeKey ? `, ${qs(this.scopeKeyAsField())}  as scope_key` : ''}
-        from ${this.collection.quotedTableName()} ${
-        this.isAssociatedScopeKey() ? queryInterface.createJoinSQL(this.collection, scopeKey) : ''
-      } ${scopeKey ? `group by ${qs(this.scopeKeyAsField())}` : ''}
-      `;
+    const needInit = async (scopeKey = null, scopeValue = null) => {
+      const filter = {};
+      if (scopeKey && scopeValue) {
+        filter[scopeKey] = scopeValue;
+      }
 
-      // @ts-ignore
-      return await db.sequelize.query(SQL, {
-        type: 'SELECT',
+      const totalCount = await this.collection.repository.count({
+        filter,
         transaction,
       });
+
+      const emptyCount = await this.collection.repository.count({
+        filter: {
+          [this.name]: null,
+          ...filter,
+        },
+        transaction,
+      });
+
+      return emptyCount === totalCount && emptyCount > 0;
     };
 
     const doInit = async (scopeKey = null, scopeValue = null) => {
-      const quotedOrderField = `${q(orderField)}`;
-      const orderFieldWithTableName = `${this.collection.quotedTableName()}.${quotedOrderField}`;
+      const queryInterface = this.collection.db.sequelize.getQueryInterface();
+
+      const quotedOrderField = queryInterface.quoteIdentifier(orderField);
 
       const sql = `
         WITH ordered_table AS (
-        SELECT ${orderFieldWithTableName} as ${quotedOrderField}, ROW_NUMBER() OVER (${
-        scopeKey ? `PARTITION BY ${qs(this.scopeKeyAsField())}` : ''
-      }
-        ORDER BY ${orderFieldWithTableName}) AS new_sequence_number
-        FROM ${this.collection.quotedTableName()}
-        ${this.isAssociatedScopeKey() ? queryInterface.createJoinSQL(this.collection, scopeKey) : ''}
-        ${(() => {
-          if (scopeKey && scopeValue) {
-            const hasNull = scopeValue.includes(null);
+          SELECT *, ROW_NUMBER() OVER (${
+            scopeKey ? `PARTITION BY ${queryInterface.quoteIdentifier(scopeKey)}` : ''
+          } ORDER BY ${quotedOrderField}) AS new_sequence_number
+          FROM ${this.collection.quotedTableName()}
+          ${(() => {
+            if (scopeKey && scopeValue) {
+              const hasNull = scopeValue.includes(null);
 
-            const notNullValues = scopeValue.filter((v) => v !== null);
-
-            const conditions = [];
-            if (notNullValues.length) {
-              conditions.push(`${qs(this.scopeKeyAsField())} IN (${notNullValues.map((v) => `'${v}'`).join(',')})`);
-            }
-            if (hasNull) {
-              conditions.push(`${qs(scopeKey)} IS NULL`);
+              return `WHERE ${queryInterface.quoteIdentifier(scopeKey)} IN (${scopeValue
+                .filter((v) => v !== null)
+                .map((v) => `'${v}'`)
+                .join(',')}) ${hasNull ? `OR ${queryInterface.quoteIdentifier(scopeKey)} IS NULL` : ''} `;
             }
 
-            return `WHERE ${conditions.join(' OR ')}`;
-          }
+            return '';
+          })()}
 
-          return '';
-        })()})
-
-          ${
-            this.collection.db.inDialect('mysql')
-              ? `
-                UPDATE ${this.collection.quotedTableName()}, ordered_table
-                SET ${this.collection.quotedTableName()}.${this.name} = ordered_table.new_sequence_number
-                WHERE ${orderFieldWithTableName} = ${q('ordered_table')}.${quotedOrderField}
-              `
-              : `
-                UPDATE ${this.collection.quotedTableName()}
-                SET ${q(this.name)} = ordered_table.new_sequence_number FROM ordered_table
-                WHERE ${orderFieldWithTableName} = ${q('ordered_table')}.${quotedOrderField};
-              `
-          }
+        )
+        ${
+          this.collection.db.inDialect('mysql')
+            ? `
+             UPDATE ${this.collection.quotedTableName()}, ordered_table
+             SET ${this.collection.quotedTableName()}.${this.name} = ordered_table.new_sequence_number
+             WHERE ${this.collection.quotedTableName()}.${quotedOrderField} = ordered_table.${quotedOrderField}
+            `
+            : `
+          UPDATE ${this.collection.quotedTableName()}
+        SET ${queryInterface.quoteIdentifier(this.name)} = ordered_table.new_sequence_number
+        FROM ordered_table
+        WHERE ${this.collection.quotedTableName()}.${quotedOrderField} = ${queryInterface.quoteIdentifier(
+                'ordered_table',
+              )}.${quotedOrderField};
+        `
+        }
 
       `;
 
@@ -202,32 +126,41 @@ export class SortField extends Field {
     };
 
     const scopeKey = this.options.scopeKey;
-
-    let needInitResults = await needInit(scopeKey);
-
     if (scopeKey) {
-      needInitResults = needInitResults.filter((r) => r['need_init']);
-      if (needInitResults.length > 0) {
-        await doInit(
-          scopeKey,
-          needInitResults.map((r) => r['scope_key']),
-        );
+      const groups = await this.collection.repository.find({
+        attributes: [scopeKey],
+        group: [scopeKey],
+        raw: true,
+        transaction,
+      });
+
+      const needInitGroups = [];
+      for (const group of groups) {
+        if (await needInit(scopeKey, group[scopeKey])) {
+          needInitGroups.push(group[scopeKey]);
+        }
       }
-    } else if (needInitResults[0]['need_init']) {
+
+      if (needInitGroups.length > 0) {
+        await doInit(scopeKey, needInitGroups);
+      }
+    } else if (await needInit()) {
       await doInit();
     }
   };
 
   bind() {
     super.bind();
+    this.on('afterSync', this.initRecordsSortValue);
+    this.on('beforeUpdate', this.onScopeChange);
+    this.on('beforeCreate', this.setSortValue);
+  }
 
-    this.setListeners({
-      afterSync: this.initRecordsSortValue,
-      afterUpdate: this.onScopeChange,
-      afterChangeScope: this.afterChangeScope,
-      afterCreateWithAssociations: this.setSortValue,
-      afterCreate: this.setSortValue,
-    });
+  unbind() {
+    super.unbind();
+    this.off('beforeUpdate', this.onScopeChange);
+    this.off('beforeCreate', this.setSortValue);
+    this.off('afterSync', this.initRecordsSortValue);
   }
 }
 
