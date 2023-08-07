b1cca421474d6e21fb6b11f8b5daefc7a507acc8
katherinehhh <katherine_15995@163.com>
2023-08-07 17:45:04 +0800

    refactor:  checkbox do not assign initial value when fields have value

--- packages/core/client/src/collection-manager/CollectionField.tsx
+++ packages/core/client/src/collection-manager/CollectionField.tsx
@@ -18,7 +18,8 @@ const InternalField: React.FC = (props: Props) => {
   const { component } = props;
   const field = useField<Field>();
   const fieldSchema = useFieldSchema();
-  const { uiSchema, defaultValue } = useCollectionField();
+  const collectionField = useCollectionField();
+  const { uiSchema, defaultValue } = collectionField;
   const Component = useComponent(component || uiSchema?.['x-component'] || 'Input');
   const compile = useCompile();
   const setFieldProps = (key, value) => {
@@ -47,7 +48,13 @@ const InternalField: React.FC = (props: Props) => {
     setFieldProps('description', uiSchema.description);
     if (ctx?.form) {
       const defaultVal = fieldSchema.default || defaultValue;
-      defaultVal !== null && defaultVal !== undefined && !field.value && setFieldProps('initialValue', defaultVal);
+      if (defaultVal !== null && defaultVal !== undefined) {
+        if (collectionField.interface === 'checkbox') {
+          !field.value && setFieldProps('initialValue', defaultVal);
+        } else {
+          setFieldProps('initialValue', defaultVal);
+        }
+      }
     }
 
     if (!field.validator && (uiSchema['x-validator'] || fieldSchema['x-validator'])) {
