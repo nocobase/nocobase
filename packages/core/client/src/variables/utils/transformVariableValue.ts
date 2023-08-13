9053477ce757d12e0287e93d3a5e54134f427666
Rain <958414905@qq.com>
2023-08-13 18:15:42 +0800

    fix: fix Select (T-1337)

--- packages/core/client/src/variables/utils/transformVariableValue.ts
+++ packages/core/client/src/variables/utils/transformVariableValue.ts
@@ -20,6 +20,7 @@ interface Deps {
 export const transformVariableValue = (value: any, deps: Deps) => {
   const { targetCollectionFiled } = deps;
 
+  // 关系字段的值应该是一个对象，如果不是一个对象就在开发环境抛出一个错误
   if (process.env.NODE_ENV !== 'production') {
     if (['belongsTo', 'hasOne', 'hasMany', 'belongsToMany'].includes(targetCollectionFiled.type)) {
       if (Array.isArray(value) && value.length && !_.isObject(value[0])) {
@@ -47,6 +48,7 @@ export const transformVariableValue = (value: any, deps: Deps) => {
     return value;
   }
 
+  // 日期字段的值是一个字符串，但是日期字符串有严格的格式要求，如果把数组中的日期拼接起来会报错
   if (targetCollectionFiled.uiSchema['x-component'] === 'DatePicker') {
     if (Array.isArray(value)) {
       return value[0];
@@ -54,6 +56,7 @@ export const transformVariableValue = (value: any, deps: Deps) => {
     return value;
   }
 
+  // id 字段是一个 number ，但是又不能相加，所以也应该取第一个值
   if (targetCollectionFiled.name === 'id') {
     if (Array.isArray(value)) {
       return value[0];
@@ -61,6 +64,28 @@ export const transformVariableValue = (value: any, deps: Deps) => {
     return value;
   }
 
+  // 下拉多选
+  if (targetCollectionFiled.interface === 'multipleSelect') {
+    const options = _.isArray(targetCollectionFiled.uiSchema.enum) ? targetCollectionFiled.uiSchema.enum : [];
+    console.log('options_1', options);
+    if (Array.isArray(value)) {
+      return value.filter((item) => options.some((enumItem: { value: any }) => enumItem.value === item));
+    }
+    return [value].filter((item) => options.some((enumItem: { value: any }) => enumItem.value === item));
+  }
+
+  // 下拉单选
+  if (targetCollectionFiled.interface === 'select') {
+    const options = _.isArray(targetCollectionFiled.uiSchema.enum) ? targetCollectionFiled.uiSchema.enum : [];
+    console.log('options_2', options);
+    if (Array.isArray(value)) {
+      return value.find((item) => options.some((enumItem: { value: any }) => enumItem.value === item));
+    }
+    return options.some((item: { value: any }) => item.value === value) ? value : undefined;
+  }
+
+  // 字符串应该拼接。
+  // 数字应该相加，比如一个学校有 10 个班级，每个班级有 10 个同学，那么选择所有班级中的人数也就是整个学校的人数就是 100。
   if (Array.isArray(value)) {
     if (_.isString(value[0])) {
       return value.join(',');
