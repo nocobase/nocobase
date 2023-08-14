564ace50eb078a3a14d6862af70c16d1fa06a451
Rain <958414905@qq.com>
2023-08-14 15:04:49 +0800

    refactor: remove useless code

--- packages/core/client/src/schema-component/antd/association-field/util.ts
+++ packages/core/client/src/schema-component/antd/association-field/util.ts
@@ -3,7 +3,6 @@ import { isArr } from '@formily/shared';
 import { getDefaultFormat, str2moment } from '@nocobase/utils/client';
 import { Tag } from 'antd';
 import dayjs from 'dayjs';
-import { get, isFunction } from 'lodash';
 import React from 'react';
 import { CollectionFieldOptions, useCollectionManager } from '../../../collection-manager';
 
@@ -86,45 +85,4 @@ export function flatData(data) {
 
 export function isShowFilePicker(labelUiSchema) {
   return labelUiSchema?.['x-component'] === 'Preview';
-}
-
-export const toValue = (value, placeholder) => {
-  if (value === null || value === undefined) {
-    return placeholder;
-  }
-  return value;
-};
-
-export const parseVariables = (str: string, ctx) => {
-  if (str) {
-    const result = get(ctx, str);
-    return isFunction(result) ? result() : result;
-  } else {
-    return str;
-  }
-};
-export function extractFilterField(str) {
-  const match = str.match(/^\$form\.([^.[\]]+)/);
-  if (match) {
-    return match[1];
-  }
-  return null;
-}
-
-export function extractValuesByPattern(obj, pattern) {
-  const regexPattern = new RegExp(pattern.replace(/\*/g, '\\d+'));
-  const result = [];
-
-  for (const key in obj) {
-    if (regexPattern.test(key)) {
-      const value = obj[key];
-      result.push(value);
-    }
-  }
-
-  return result;
-}
-export function generatePattern(str, fieldName) {
-  const result = str.replace(`$form.${fieldName}.`, `${fieldName}.*.`);
-  return result;
 }
