e5c830961497bf8b6b714f0d4b058a38e42913c2
ChengLei Shao <chareice@live.com>
2023-08-22 17:25:34 +0800

    chore: single association doc

--- packages/plugins/api-doc/src/server/swagger/collections/paths/associations/index.ts
+++ packages/plugins/api-doc/src/server/swagger/collections/paths/associations/index.ts
@@ -12,10 +12,6 @@ import singleAssociation from './single-association';
 export default (collection: Collection) => {
   return associationFields(collection)
     .map((field) => {
-      console.log({
-        fieldName: field.name,
-        fieldType: field.type,
-      });
       if (field.type === 'belongsToMany' || field.type === 'hasMany') {
         return multipleAssociation(collection, field as BelongsToManyField | HasManyField);
       }
@@ -23,7 +19,6 @@ export default (collection: Collection) => {
       return singleAssociation(collection, field as BelongsToField | HasOneField);
     })
     .reduce((obj, item) => {
-      console.log({ item });
       return {
         ...obj,
         ...item,
@@ -32,5 +27,7 @@ export default (collection: Collection) => {
 };
 
 export function associationFields(collection: Collection): Array<RelationField> {
-  return Array.from(collection.fields.values()).filter((field) => field instanceof RelationField);
+  return Array.from(collection.fields.values())
+    .filter((field) => field instanceof RelationField)
+    .filter((field) => field.name !== 'createdBy' && field.name !== 'updatedBy');
 }
