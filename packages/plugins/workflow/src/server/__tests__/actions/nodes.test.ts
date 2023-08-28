1a82e92ae396a16a76b9292eaa7b37409870b827
Junyi <mytharcher@users.noreply.github.com>
2023-08-28 21:33:53 +0700

    fix(plugin-workflow): fix destroy node bug (#2553)

---
+++ packages/plugins/workflow/src/server/__tests__/actions/nodes.test.ts
@@ -0,0 +1,84 @@
+import { MockServer } from '@nocobase/test';
+import Database from '@nocobase/database';
+import { getApp, sleep } from '..';
+
+describe('workflow > actions > workflows', () => {
+  let app: MockServer;
+  let agent;
+  let db: Database;
+  let PostModel;
+  let PostRepo;
+  let WorkflowModel;
+
+  beforeEach(async () => {
+    app = await getApp();
+    agent = app.agent();
+    db = app.db;
+    WorkflowModel = db.getCollection('workflows').model;
+    PostModel = db.getCollection('posts').model;
+    PostRepo = db.getCollection('posts').repository;
+  });
+
+  afterEach(() => app.destroy());
+
+  describe('destroy', () => {
+    it('node in executed workflow could not be destroyed', async () => {
+      const workflow = await WorkflowModel.create({
+        enabled: true,
+        type: 'collection',
+        config: {
+          mode: 1,
+          collection: 'posts',
+        },
+      });
+
+      const n1 = await workflow.createNode({
+        type: 'echo',
+      });
+
+      await PostRepo.create({});
+
+      await sleep(500);
+
+      const { status } = await agent.resource('flow_nodes').destroy({
+        filterByTk: n1.id,
+      });
+
+      expect(status).toBe(400);
+    });
+
+    it('cascading destroy all nodes in sub-branches', async () => {
+      const workflow = await WorkflowModel.create({
+        enabled: true,
+        type: 'collection',
+        config: {
+          mode: 1,
+          collection: 'posts',
+        },
+      });
+
+      const n1 = await workflow.createNode({
+        type: 'echo',
+      });
+
+      const n2 = await workflow.createNode({
+        type: 'echo',
+        branchIndex: 0,
+        upstreamId: n1.id,
+      });
+
+      const n3 = await workflow.createNode({
+        type: 'echo',
+        branchIndex: 0,
+        upstreamId: n2.id,
+      });
+
+      await agent.resource('flow_nodes').destroy({
+        filterByTk: n1.id,
+      });
+
+      const nodes = await workflow.getNodes();
+      expect(nodes.length).toBe(0);
+    });
+  });
+});
