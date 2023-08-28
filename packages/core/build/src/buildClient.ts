6aa2b8de78ae63459a64566d3ee08adc04c39408
jack zhang <1098626505@qq.com>
2023-08-28 14:57:13 +0800

    fix: local error

--- packages/core/build/src/buildClient.ts
+++ packages/core/build/src/buildClient.ts
@@ -1,22 +1,26 @@
 import path from 'path';
-import { build } from 'vite';
+import fg from 'fast-glob';
+import { build as viteBuild } from 'vite';
+import { build as tsupBuild } from 'tsup';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import react from '@vitejs/plugin-react';
 import { PkgLog } from './utils';
+import { globExcludeFiles } from './constant';
 
-export function buildClient(cwd: string, sourcemap: boolean = false, log: PkgLog) {
+export async function buildClient(cwd: string, sourcemap: boolean = false, log: PkgLog) {
   log('build client');
 
-  return Promise.all([viteBuild(cwd, sourcemap, 'cjs'), viteBuild(cwd, sourcemap, 'es')]);
+  await Promise.all([buildLib(cwd, sourcemap, 'cjs'), buildLib(cwd, sourcemap, 'es')]);
+  await buildLocale(cwd);
 }
 
-export function viteBuild(cwd: string, sourcemap: boolean, format: 'cjs' | 'es') {
+export function buildLib(cwd: string, sourcemap: boolean, format: 'cjs' | 'es') {
   const outDir = path.resolve(cwd, format === 'cjs' ? 'lib' : 'es');
   const entry = path.join(cwd, 'src/index.ts').replaceAll(/\\/g, '/');
   const cwdWin = cwd.replaceAll(/\\/g, '/');
   const cwdUnix = cwd.replaceAll(/\//g, '\\');
 
-  return build({
+  return viteBuild({
     mode: 'production',
     define: {
       'process.env.NODE_ENV': JSON.stringify('production'),
@@ -45,5 +49,23 @@ export function viteBuild(cwd: string, sourcemap: boolean, format: 'cjs' | 'es')
       },
     },
     plugins: [react(), libInjectCss()],
+  });
+}
+
+export function buildLocale(cwd: string) {
+  const entry = fg.globSync(['src/locale/**', ...globExcludeFiles], { cwd, absolute: true });
+  const outDir = path.resolve(cwd, 'lib', 'locale');
+  return tsupBuild({
+    entry,
+    splitting: false,
+    clean: false,
+    bundle: false,
+    silent: true,
+    treeshake: false,
+    target: 'node16',
+    keepNames: true,
+    outDir,
+    format: 'cjs',
+    skipNodeModulesBundle: true,
   });
 }
