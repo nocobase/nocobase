eece43510456cbe9bc73fabe36ce49081c947aef
katherinehhh <katherine_15995@163.com>
2023-08-28 19:17:58 +0800

    refactor: attachment field support size config

--- packages/core/client/src/schema-component/antd/upload/style.ts
+++ packages/core/client/src/schema-component/antd/upload/style.ts
@@ -20,7 +20,21 @@ export const useStyles = genStyleHook('upload', (token) => {
             padding: '1px !important',
           },
       },
-
+      '&.nb-upload-large': {
+        [`${componentCls}-list-picture-card-container${componentCls}-list-picture-card-container`]: {
+          margin: '0 3px 3px 0 !important',
+          height: '160px !important',
+          width: '160px !important',
+          marginBlock: '0 !important',
+        },
+        [`${componentCls}-list-picture-card ${componentCls}-list-item-name${componentCls}-list-item-name`]: {
+          display: 'none !important',
+        },
+        [`${componentCls}-list-picture ${componentCls}-list-item, ${componentCls}-list-picture-card ${componentCls}-list-item`]:
+          {
+            padding: '1px !important',
+          },
+      },
       '&.nb-upload': {
         [`${componentCls}-list-item${componentCls}-list-item-list-type-picture-card`]: {
           padding: '3px !important',
