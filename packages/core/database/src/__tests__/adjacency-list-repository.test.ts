a2ad3d5c0e32f33d47c77956d1a81ea2cd925749
ChengLei Shao <chareice@live.com>
2023-08-30 19:45:50 +0800

    fix: load with appends

--- packages/core/database/src/__tests__/adjacency-list-repository.test.ts
+++ packages/core/database/src/__tests__/adjacency-list-repository.test.ts
@@ -15,6 +15,75 @@ describe('adjacency list repository', () => {
     await db.close();
   });
 
+  it('should append relation parent recursively', async () => {
+    const Category = db.collection({
+      name: 'categories',
+      tree: 'adjacency-list',
+      fields: [
+        { type: 'string', name: 'name' },
+        {
+          type: 'belongsTo',
+          name: 'parent',
+          treeParent: true,
+        },
+        {
+          type: 'hasMany',
+          name: 'children',
+          treeChildren: true,
+        },
+      ],
+    });
+
+    const Post = db.collection({
+      name: 'posts',
+      fields: [
+        { type: 'string', name: 'title' },
+        { type: 'belongsToMany', name: 'categories', target: 'categories' },
+      ],
+    });
+
+    await db.sync();
+
+    await Category.repository.create({
+      values: [
+        {
+          name: 'c1',
+          children: [
+            {
+              name: 'c1-1',
+              children: [
+                {
+                  name: 'c1-1-1',
+                },
+              ],
+            },
+            {
+              name: 'c12',
+            },
+          ],
+        },
+      ],
+    });
+
+    const c111 = await Category.repository.findOne({ where: { name: 'c1-1-1' } });
+
+    const p1 = await Post.repository.create({
+      values: [
+        {
+          title: 'p1',
+          categories: [{ id: c111.id }],
+        },
+      ],
+    });
+
+    const p1WithCategory = await Post.repository.findOne({
+      appends: ['categories', 'categories.parent(recursively=true)'],
+    });
+
+    expect(p1WithCategory.categories[0].parent.name).toBe('c1-1');
+    expect(p1WithCategory.categories[0].parent.parent.name).toBe('c1');
+  });
+
   it('should append parent recursively', async () => {
     const Tree = db.collection({
       name: 'categories',
