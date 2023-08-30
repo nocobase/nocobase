938cfff35ff27d3fca1e52356d923874a721001f
Dunqing <dengqing0821@gmail.com>
2023-08-30 14:51:14 +0800

    fix: i18n, Close T-1684

--- packages/plugins/custom-request/src/locale/zh-CN.ts
+++ packages/plugins/custom-request/src/locale/zh-CN.ts
@@ -1,6 +1,7 @@
 export default {
   'Access Control': '权限控制',
   'Custom Request': '自定义请求',
+  'Request settings': '请求设置',
   'Roles': '角色',
   'If not set, all roles can see this action': '如果不设置，所有角色都可以看到这个自定义请求',
   "Title": "标题",
