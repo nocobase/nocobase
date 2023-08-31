9d6bbe29d2e38141d78227ecb24b898c3d4ebd6d
ChengLei Shao <chareice@live.com>
2023-08-31 10:57:28 +0800

    chore: test

--- packages/core/database/src/__tests__/adjacency-list-repository.test.ts
+++ packages/core/database/src/__tests__/adjacency-list-repository.test.ts
@@ -15,7 +15,76 @@ describe('adjacency list repository', () => {
     await db.close();
   });
 
-  it('should append relation parent recursively', async () => {
+  it('should append relation parent recursively with belongs to assoc', async () => {
+    const Category = db.collection({
+      name: 'categories',
+      tree: 'adjacency-list',
+      fields: [
+        { type: 'string', name: 'name' },
+        {
+          type: 'belongsTo',
+          name: 'parent',
+          treeParent: true,
+        },
+        {
+          type: 'hasMany',
+          name: 'children',
+          treeChildren: true,
+        },
+      ],
+    });
+
+    const Post = db.collection({
+      name: 'posts',
+      fields: [
+        { type: 'string', name: 'title' },
+        { type: 'belongsTo', name: 'category', target: 'categories' },
+      ],
+    });
+
+    await db.sync();
+
+    await Category.repository.create({
+      values: [
+        {
+          name: 'c1',
+          children: [
+            {
+              name: 'c1-1',
+              children: [
+                {
+                  name: 'c1-1-1',
+                },
+              ],
+            },
+            {
+              name: 'c12',
+            },
+          ],
+        },
+      ],
+    });
+
+    const c111 = await Category.repository.findOne({ where: { name: 'c1-1-1' } });
+
+    const p1 = await Post.repository.create({
+      values: [
+        {
+          title: 'p1',
+          category: { id: c111.id },
+        },
+      ],
+    });
+
+    const p1WithCategory = await Post.repository.findOne({
+      appends: ['category', 'category.parent(recursively=true)'],
+    });
+
+    expect(p1WithCategory.category.parent.name).toBe('c1-1');
+    expect(p1WithCategory.category.parent.parent.name).toBe('c1');
+  });
+
+  it('should append relation parent recursively with belongs to many', async () => {
     const Category = db.collection({
       name: 'categories',
       tree: 'adjacency-list',
@@ -39,6 +108,7 @@ describe('adjacency list repository', () => {
       fields: [
         { type: 'string', name: 'title' },
         { type: 'belongsToMany', name: 'categories', target: 'categories' },
+        { type: 'belongsToMany', name: 'categories2', target: 'categories' },
       ],
     });
 
@@ -72,16 +142,25 @@ describe('adjacency list repository', () => {
         {
           title: 'p1',
           categories: [{ id: c111.id }],
+          categories2: [{ id: c111.id }],
         },
       ],
     });
 
     const p1WithCategory = await Post.repository.findOne({
-      appends: ['categories', 'categories.parent(recursively=true)'],
+      appends: [
+        'categories',
+        'categories.parent(recursively=true)',
+        'categories2',
+        'categories2.parent(recursively=true)',
+      ],
     });
 
     expect(p1WithCategory.categories[0].parent.name).toBe('c1-1');
     expect(p1WithCategory.categories[0].parent.parent.name).toBe('c1');
+
+    expect(p1WithCategory.categories2[0].parent.name).toBe('c1-1');
+    expect(p1WithCategory.categories2[0].parent.parent.name).toBe('c1');
   });
 
   it('should append parent recursively', async () => {
