eb4e94e87d146cbb9ffb2e125b83209a80ae47be
ChengLei Shao <chareice@live.com>
2023-09-01 16:11:27 +0800

    feat(gateway): response cli result when run nocobase command (#2563)
    * chore(gateway): refresh message in websocket
    
    * chore(gateway): throw error when cli error
    
    * chore(gateway): await ipc server response
    
    * chore: notification message
    
    * fix: build
    
    * chore: notification type
    
    * feat: notification
    
    ---------
    
    Co-authored-by: chenos <chenlinxh@gmail.com>

--- packages/core/client/src/global-theme/AntdAppProvider.tsx
+++ packages/core/client/src/global-theme/AntdAppProvider.tsx
@@ -1,13 +1,16 @@
 import { App } from 'antd';
 import React, { memo, useEffect } from 'react';
 import { useAPIClient } from '../api-client';
+import { useApp } from '../application';
 
 const AppInner = memo(({ children }: { children: React.ReactNode }) => {
+  const app = useApp();
   const { notification } = App.useApp();
   const apiClient = useAPIClient();
 
   useEffect(() => {
     apiClient.notification = notification;
+    app.notification = notification;
   }, [notification]);
 
   return <>{children}</>;
---
+++ packages/core/server/src/errors/plugin-command-error.ts
@@ -0,0 +1,1 @@
+export class PluginCommandError extends Error {}
