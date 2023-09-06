252e88e375549bc64268f0c803fe962312c7b9ee
被雨水过滤的空气-Rain <958414905@qq.com>
2023-09-06 11:11:35 +0800

    fix: fix collapsed panels not working when clicked (#2600)
    * refactor: rename
    
    * fix: fix collapsed panels not working when clicked

--- packages/plugins/charts/src/client/select/CustomSelect.tsx
+++ packages/plugins/charts/src/client/select/CustomSelect.tsx
@@ -1,7 +1,7 @@
 import { LoadingOutlined } from '@ant-design/icons';
 import { connect, mapProps, mapReadPretty } from '@formily/react';
 import { isValid } from '@formily/shared';
-import { Icon, Popover, css } from '@nocobase/client';
+import { Icon, PopoverWithStopPropagation, css } from '@nocobase/client';
 import type { SelectProps } from 'antd';
 import { Select as AntdSelect } from 'antd';
 import React from 'react';
@@ -36,7 +36,7 @@ const InternalSelect = connect(
         <OptGroup label={lang('Basic charts')}>
           {group1.map((option) => (
             <Option key={option.key} value={option.key} label={lang(option.title)}>
-              <Popover
+              <PopoverWithStopPropagation
                 placement={'right'}
                 zIndex={99999999999}
                 content={() => (
@@ -60,14 +60,14 @@ const InternalSelect = connect(
                     {lang(option.title)}
                   </span>
                 </div>
-              </Popover>
+              </PopoverWithStopPropagation>
             </Option>
           ))}
         </OptGroup>
         <OptGroup label={lang('More charts')}>
           {group2.map((option) => (
             <Option key={option.key} value={option.key} label={lang(option.title)}>
-              <Popover
+              <PopoverWithStopPropagation
                 placement={'right'}
                 zIndex={99999999999}
                 content={() => (
@@ -91,7 +91,7 @@ const InternalSelect = connect(
                     {lang(option.title)}
                   </span>
                 </div>
-              </Popover>
+              </PopoverWithStopPropagation>
             </Option>
           ))}
         </OptGroup>
