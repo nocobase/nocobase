adf11bf6243632ac9a9ff9919a0d8b7bb66d1530
chenos <chenlinxh@gmail.com>
2023-09-11 15:50:27 +0800

    fix: error: SQLITE_ERROR: no such table: authenticators

--- packages/plugins/client/src/server/migrations/20230620203216-admin-ui-schema-uid.ts
+++ packages/plugins/client/src/server/migrations/20230620203216-admin-ui-schema-uid.ts
@@ -3,6 +3,12 @@ import { Migration } from '@nocobase/server';
 
 export default class extends Migration {
   async up() {
+    await this.db.getCollection('systemSettings').sync({
+      force: false,
+      alter: {
+        drop: false,
+      },
+    });
     const systemSettings = this.db.getRepository('systemSettings');
     let instance: Model = await systemSettings.findOne();
     if (instance?.options?.adminSchemaUid) {
@@ -13,7 +19,6 @@ export default class extends Migration {
     if (!uiRoutes) {
       return;
     }
-    await this.db.getCollection('systemSettings').sync();
     for (const route of routes) {
       if (route.uiSchemaUid && route?.options?.component === 'AdminLayout') {
         const options = instance.options || {};
