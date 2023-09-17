7a7dd7905054ac495b58c0b6f15d9783feed5aa7
Rain <958414905@qq.com>
2023-09-17 08:15:04 +0800

    chore: fix build error

--- packages/core/client/src/collection-manager/types.ts
+++ packages/core/client/src/collection-manager/types.ts
@@ -1,7 +1,52 @@
 import { ISchema } from '@formily/react';
 import { ReactNode } from 'react';
 
+type dumpable = 'required' | 'optional' | 'skip';
+type CollectionSortable = string | boolean | { name?: string; scopeKey?: string };
+
 export interface CollectionOptions {
+  name: string;
+  title?: string;
+  namespace?: string;
+  /**
+   * Used for @nocobase/plugin-duplicator
+   * @see packages/core/database/src/collection-group-manager.tss
+   *
+   * @prop {'required' | 'optional' | 'skip'} dumpable - Determine whether the collection is dumped
+   * @prop {string[] | string} [with] - Collections dumped with this collection
+   * @prop {any} [delayRestore] - A function to execute after all collections are restored
+   */
+  duplicator?:
+    | dumpable
+    | {
+        dumpable: dumpable;
+        with?: string[] | string;
+        delayRestore?: any;
+      };
+
+  tableName?: string;
+  inherits?: string[] | string;
+  viewName?: string;
+  writableView?: boolean;
+
+  filterTargetKey?: string;
+  fields?: FieldOptions[];
+  model?: any;
+  repository?: any;
+  sortable?: CollectionSortable;
+  /**
+   * @default true
+   */
+  autoGenId?: boolean;
+  /**
+   * @default 'options'
+   */
+  magicAttribute?: string;
+
+  tree?: string;
+
+  template?: string;
+
   [key: string]: any;
 }
 
@@ -15,7 +60,7 @@ export interface CollectionManagerOptions {
   updateCollection?: (collection: any) => void;
 }
 
-export type FieldOptions = CollectionOptions['fields'][0];
+export type FieldOptions = any;
 
 export interface ICollectionProviderProps {
   name?: string;
