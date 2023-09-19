cef62f65e6a36d14ce5ed92a7cfbcc6ce70fde0f
Dunqing <dengqing0821@gmail.com>
2023-09-19 18:50:22 +0800

    feat: support google map

--- packages/plugins/@nocobase/plugin-map/src/client/components/GoogleMaps/Block.tsx
+++ packages/plugins/@nocobase/plugin-map/src/client/components/GoogleMaps/Block.tsx
@@ -29,8 +29,16 @@ const labelClass = css`
   border: 1px solid #0000f5;
 `;
 
+const pointClass = css`
+  margin-top: -64px;
+  padding: 2px 4px;
+  background: #fff;
+  border: 1px solid #0000f5;
+`;
+
 export const GoogleMapsBlock = (props) => {
-  const { collectionField, fieldNames, dataSource, fixedBlock, zoom, setSelectedRecordKeys } = useProps(props);
+  const { collectionField, fieldNames, dataSource, fixedBlock, zoom, setSelectedRecordKeys, lineSort } =
+    useProps(props);
   const { getPrimaryKey } = useCollection();
   const primaryKey = getPrimaryKey();
   const { marker: markerName = 'id' } = fieldNames;
@@ -151,7 +159,7 @@ export const GoogleMapsBlock = (props) => {
         : fieldNames?.field;
     const cf = getCollectionJoinField([name, ...fieldPaths].flat().join('.'));
 
-    const overlays: google.maps.Polygon[] = dataSource
+    const overlays: google.maps.MVCObject[] = dataSource
       .map((item) => {
         const data = getSource(item, fieldNames?.field, cf?.interface);
         if (!data?.length) return [];
@@ -177,7 +185,6 @@ export const GoogleMapsBlock = (props) => {
       .filter(Boolean);
 
     overlaysRef.current = overlays;
-    mapRef.current?.setFitView(overlays);
 
     const events = overlays.map((o: google.maps.MVCObject) => {
       const onClick = (event) => {
@@ -217,9 +224,53 @@ export const GoogleMapsBlock = (props) => {
       return () => o.unbindAll();
     });
 
+    if (collectionField.type === 'point' && lineSort?.length && overlays?.length > 1) {
+      const positions = overlays.map((o: google.maps.Marker) => o.getPosition());
+
+      (overlays[0] as google.maps.Marker).setZIndex(138);
+      (overlays[overlays.length - 1] as google.maps.Marker).setZIndex(138);
+
+      const createText = (start = true) => {
+        if (!mapRef.current?.map) return;
+        return new google.maps.Marker({
+          label: {
+            // direction: 'top',
+            // offset: [0, 0],
+            className: pointClass,
+            fontFamily: 'inherit',
+            fontSize: '13px',
+            color: '#333',
+            text: start ? t('Start point') : t('End point'),
+          },
+          icon: getIcon(defaultImage),
+          position: start ? positions[0] : positions[positions.length - 1],
+          map: mapRef.current.map,
+        });
+      };
+
+      overlays.push(
+        ...[
+          mapRef.current.setOverlay(
+            'lineString',
+            positions.map((p) => [p.lng(), p.lat()]),
+            {
+              strokeColor: '#4e9bff',
+              fillColor: '#4e9bff',
+              strokeWeight: 2,
+              cursor: 'pointer',
+            },
+          ),
+          createText(),
+          createText(false),
+        ].filter(Boolean),
+      );
+    }
+
+    mapRef.current?.setFitView(overlays);
+
     return () => {
       overlays.forEach((ov) => {
-        ov.setMap(null);
+        (ov as google.maps.Polygon).setMap(null);
         ov.unbindAll();
       });
       events.forEach((e) => e());
