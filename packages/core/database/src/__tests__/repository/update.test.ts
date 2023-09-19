942f6621816a360146bfedfae22dccacbc9beef8
ChengLei Shao <chareice@live.com>
2023-09-19 11:45:30 +0800

    chore: test

--- packages/core/database/src/__tests__/repository/update.test.ts
+++ packages/core/database/src/__tests__/repository/update.test.ts
@@ -2,21 +2,29 @@ import { Collection, Database, mockDatabase } from '@nocobase/database';
 
 describe('update', () => {
   let db: Database;
-  let User: Collection;
+  let Post: Collection;
   let Tag: Collection;
+  let PostTag: Collection;
+
   afterEach(async () => {
     await db.close();
   });
 
   beforeEach(async () => {
     db = mockDatabase();
-    User = db.collection({
+
+    PostTag = db.collection({
+      name: 'post_tag',
+    });
+
+    Post = db.collection({
       name: 'posts',
       fields: [
         { type: 'string', name: 'title' },
         {
           type: 'belongsToMany',
           name: 'tags',
+          through: 'post_tag',
         },
       ],
     });
@@ -31,11 +39,55 @@ describe('update', () => {
         {
           type: 'belongsToMany',
           name: 'posts',
+          through: 'post_tag',
         },
       ],
     });
 
     await db.sync();
+  });
+
+  it('should throw error when update data conflicted', async () => {
+    const t1 = await Tag.repository.create({
+      values: {
+        name: 't1',
+      },
+    });
+
+    const t2 = await Tag.repository.create({
+      values: {
+        name: 't2',
+      },
+    });
+
+    const post1 = await Post.repository.create({
+      values: {
+        title: 'p1',
+        tags: [t1.get('id')],
+      },
+    });
+
+    let error;
+
+    try {
+      await Post.repository.update({
+        filterByTk: post1.get('id'),
+        values: {
+          post_tag: [
+            {
+              tag: {
+                id: t1.get('id'),
+              },
+            },
+          ],
+          tags: [t2.get('id')],
+        },
+      });
+    } catch (err) {
+      error = err;
+    }
+
+    expect(error).toBeDefined();
   });
 
   it('should update tags to null', async () => {
