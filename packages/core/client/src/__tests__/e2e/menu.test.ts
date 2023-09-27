f2acd5496699a0426c3d09b7e1ba1f0c5abe132d
Rain <958414905@qq.com>
2023-09-27 19:50:06 +0800

    fix(vscode): load env when using vscode plugin

--- packages/core/client/src/__tests__/e2e/menu.test.ts
+++ packages/core/client/src/__tests__/e2e/menu.test.ts
@@ -1,7 +1,7 @@
 import { enableToConfig, expect, test } from '@nocobase/test/client';
 
 test.describe('menu', () => {
-  test('create new page', async ({ page }) => {
+  test('create new page, then delete', async ({ page }) => {
     await page.goto('/');
     await enableToConfig(page);
 
@@ -13,5 +13,11 @@ test.describe('menu', () => {
     await page.getByText('new page').click();
 
     await expect(page.getByTestId('add-block-button-in-page')).toBeVisible();
+
+    // 删除页面，避免影响其他测试
+    await page.getByRole('menu').getByText('new page').hover();
+    await page.getByTestId('designer-schema-settings').hover();
+    await page.getByRole('menuitem', { name: 'Delete' }).click();
+    await page.getByRole('button', { name: 'OK' }).click();
   });
 });
