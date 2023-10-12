59605824f258848f73e74aa674c9a332880bcef6
katherinehhh <katherine_15995@163.com>
2023-10-12 15:55:08 +0800

    test: menu insert page before

--- packages/core/client/src/__tests__/e2e/menu.test.ts
+++ packages/core/client/src/__tests__/e2e/menu.test.ts
@@ -40,21 +40,36 @@ test.describe('menu', () => {
       name: 'page1',
     }).goto();
     await mockPage({ name: 'page2' }).goto();
+    await enableToConfig(page);
 
-    await page.getByRole('menu').getByText('page2').hover();
+    await page.getByRole('menu').getByText('page1').click();
+    await page.getByRole('menu').getByText('page1').hover();
     await page.getByTestId('designer-schema-settings').hover();
     await page.getByRole('menuitem', { name: 'Move to' }).click();
     await page.getByLabel('Search').click();
-    await page.getByTitle('page2').getByText('page1').click();
-    await page.getByLabel('Before').check();
+    await page.getByTitle('page2').getByText('page2').click();
+    await page.getByRole('button', { name: 'OK' }).click();
+    const page1 = await page.getByRole('menu').getByText('page1').boundingBox();
+    const page2 = await page.getByRole('menu').getByText('page2').boundingBox();
+    expect(page2.x).toBeLessThan(page1.x);
+  });
+
+  test('insert page before', async ({ page, mockPage }) => {
+    await mockPage({
+      name: 'page3',
+    }).goto();
+    await enableToConfig(page);
+    await page.getByRole('menu').getByText('page3').click();
+    await page.getByRole('menu').getByText('page3').hover();
+    await page.getByTestId('designer-schema-settings').hover();
+    await page.getByRole('menuitem', { name: 'Insert before' }).click();
+    await page.getByRole('menuitem', { name: 'Page' }).locator('span').click();
+    await page.locator('.ant-input-affix-wrapper').click();
+    await page.getByRole('textbox').fill('page4');
     await page.getByRole('button', { name: 'OK' }).click();
 
-    // await page.getByRole('textbox').click();
-    // await page.getByRole('textbox').fill('page title1');
-    // await page.getByRole('button', { name: 'OK' }).click();
-    // await page.getByRole('menu').getByText('page title1').click();
-
-    // await page.getByText('page title1').click();
-    // await expect(page.getByTitle('page title1')).toBeVisible();
+    const page3 = await page.getByRole('menu').getByText('page3').boundingBox();
+    const page4 = await page.getByRole('menu').getByText('page4').boundingBox();
+    expect(page4.x).toBeLessThan(page3.x);
   });
 });
