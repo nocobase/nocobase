7a1aad2bff66d4536cb31a4bb4a2b10d5d604295
katherinehhh <katherine_15995@163.com>
2023-10-12 17:01:21 +0800

    test: menu test

--- packages/core/client/src/__tests__/e2e/menu.test.ts
+++ packages/core/client/src/__tests__/e2e/menu.test.ts
@@ -6,12 +6,11 @@ test.describe('menu', () => {
     await enableToConfig(page);
 
     await page.getByTestId('add-menu-item-button-in-header').hover();
-    await page.getByRole('menuitem', { name: 'Page' }).click();
+    await page.getByRole('menuitem', { name: 'Page', exact: true }).click();
     await page.getByRole('textbox').click();
     await page.getByRole('textbox').fill('new page');
     await page.getByRole('button', { name: 'OK' }).click();
-    await page.getByText('new page').click();
-
+    await page.getByRole('menu').getByText('new page').click();
     await expect(page.getByTestId('add-block-button-in-page')).toBeVisible();
 
     // 删除页面，避免影响其他测试
