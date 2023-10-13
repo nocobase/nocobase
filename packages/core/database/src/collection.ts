ec772a99933460ba6e4857e98e3de323398eed0f
Dunqing <dengqing0821@gmail.com>
2023-10-13 02:16:50 -0500

    feat(plugin-custom-request): support better custom-request (#2536)
    * feat(plugin-custom-request): support better custom-request
    
    * feat: re implement custom request
    
    * feat: complete configuration
    
    * feat: support request in server
    
    * feat: improve usage
    
    * feat: support request
    
    * feat: remove customRequests when action is deleted
    
    * feat: avoid override
    
    * fix: the data is not latest
    
    * feat: should autoSize in url
    
    * feat: support title and acl
    
    * fix: i18n
    
    * feat: support display title
    
    * feat: i18n supports
    
    * refactor: acl check
    
    * feat: better method
    
    * fix: style
    
    * fix: remove unused code
    
    * fix: acl
    
    * docs: update
    
    * fix: upgrade
    
    * feat: hidden skip validator option and display should display data
    
    * feat: support root access all custom-request
    
    * chore: update deps
    
    * fix: limit height of modal body, Close T-1682
    
    * fix: parse incorrect, Close T-1686
    
    * fix: i18n, Close T-1684
    
    * fix: linkage rules incorrect, Close T-1719
    
    * fix: missing pass appends, Close T-1720
    
    * fix: should update initialValues when it changed
    
    * feat: update better message
    
    * fix: permission incorrect
    
    * test: setup
    
    * refactor: fix locale
    
    * chore: update deps
    
    * feat: move to @nocobase
    
    * fix: dir
    
    * fix: ui.*
    
    * feat: add listByCurrentRole action, add decorator instead of acl
    
    * fix: remove title field
    
    * feat: compatible pervious version
    
    * feat: support passing x-* headers to custom-request
    
    * feat: support list all keys
    
    * fix: support baseURL, close T-2090
    
    * fix: remove unused code
    
    * fix: sqlite, Close T-2092
    
    * fix: close T-2089
    
    * feat: alert error when request settings doesn't configure, Close T-2101
    
    * feat: improve message
    
    * chore: fix eslint error
    
    * fix: fix version of plugin-custom-request
    
    * fix: build error
    
    ---------
    
    Co-authored-by: chenos <chenlinxh@gmail.com>
    Co-authored-by: Rain <958414905@qq.com>

--- packages/core/database/src/collection.ts
+++ packages/core/database/src/collection.ts
@@ -108,7 +108,7 @@ export interface CollectionContext {
 
 export class Collection<
   TModelAttributes extends {} = any,
-  TCreationAttributes extends {} = TModelAttributes
+  TCreationAttributes extends {} = TModelAttributes,
 > extends EventEmitter {
   options: CollectionOptions;
   context: CollectionContext;
---
+++ packages/plugins/@nocobase/plugin-custom-request/src/client/hooks/useCustomizeRequestActionProps.ts
@@ -0,0 +1,112 @@
+import { useField, useFieldSchema, useForm } from '@formily/react';
+import {
+  TableFieldResource,
+  getFormValues,
+  useAPIClient,
+  useActionContext,
+  useBlockRequestContext,
+  useCollection,
+  useCompile,
+  useFilterByTk,
+  useFormActiveFields,
+  useRecord,
+} from '@nocobase/client';
+import { App } from 'antd';
+import { isURL } from '@nocobase/utils/client';
+import { useNavigate } from 'react-router-dom';
+import { useGetCustomRequest } from './useGetCustomRequest';
+import { useTranslation } from '../locale';
+
+export const useCustomizeRequestActionProps = () => {
+  const apiClient = useAPIClient();
+  const navigate = useNavigate();
+  const filterByTk = useFilterByTk();
+  const actionSchema = useFieldSchema();
+  const compile = useCompile();
+  const form = useForm();
+  const { fields, getField, getPrimaryKey } = useCollection();
+  const { field, resource, __parent, service } = useBlockRequestContext();
+  const { getActiveFieldsName } = useFormActiveFields() || {};
+  const record = useRecord();
+  const fieldSchema = useFieldSchema();
+  const { data, runAsync } = useGetCustomRequest();
+  const actionField = useField();
+  const { setVisible } = useActionContext();
+  const { modal, message } = App.useApp();
+  const { t } = useTranslation();
+
+  return {
+    async onClick() {
+      const { skipValidator, onSuccess } = actionSchema?.['x-action-settings'] ?? {};
+      const options = data ? data?.data?.options : (await runAsync())?.data?.options;
+      if (!options?.['url']) {
+        return message.error(t('Please configure the request settings first'));
+      }
+      const xAction = actionSchema?.['x-action'];
+      if (skipValidator !== true && xAction === 'customize:form:request') {
+        await form.submit();
+      }
+
+      const requestConfig = {};
+      const methods = ['POST', 'PUT', 'PATCH'];
+      if (xAction === 'customize:form:request' && methods.includes(options['method'])) {
+        const fieldNames = fields.map((field) => field.name);
+        const values = getFormValues({
+          filterByTk,
+          field,
+          form,
+          fieldNames,
+          getField,
+          resource,
+          actionFields: getActiveFieldsName?.('form') || [],
+        });
+        requestConfig['data'] = values;
+      }
+
+      actionField.data ??= {};
+      actionField.data.loading = true;
+      try {
+        await apiClient.request({
+          url: `/customRequests:send/${fieldSchema['x-uid']}`,
+          method: 'POST',
+          data: {
+            requestConfig,
+            currentRecord: {
+              id: record[getPrimaryKey()],
+              appends: service.params[0].appends,
+            },
+          },
+        });
+        actionField.data.loading = false;
+        if (!(resource instanceof TableFieldResource)) {
+          __parent?.service?.refresh?.();
+        }
+        service?.refresh?.();
+        if (xAction === 'customize:form:request') {
+          setVisible?.(false);
+        }
+        if (!onSuccess?.successMessage) {
+          return;
+        }
+        if (onSuccess?.manualClose) {
+          modal.success({
+            title: compile(onSuccess?.successMessage),
+            onOk: async () => {
+              if (onSuccess?.redirecting && onSuccess?.redirectTo) {
+                if (isURL(onSuccess.redirectTo)) {
+                  window.location.href = onSuccess.redirectTo;
+                } else {
+                  navigate(onSuccess.redirectTo);
+                }
+              }
+            },
+          });
+        } else {
+          return message.success(compile(onSuccess?.successMessage));
+        }
+      } finally {
+        actionField.data.loading = false;
+      }
+    },
+  };
+};
