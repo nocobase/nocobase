4b702a51fbd772603012bc0a00849a04f75cc685
katherinehhh <katherine_15995@163.com>
2023-10-14 21:55:32 +0800

    test: page title

--- packages/core/client/src/__tests__/e2e/page.test.ts
+++ packages/core/client/src/__tests__/e2e/page.test.ts
@@ -38,24 +38,38 @@ test.describe('page header', () => {
 
 test.describe('page title', () => {
   test('disable & not disable page title', async ({ page, mockPage }) => {
-    await mockPage().goto();
-    await page.getByTestId('add-menu-item-button-in-header').hover();
-    await page.getByRole('menuitem', { name: 'Link' }).click();
-    await page.getByTestId('title-item').getByRole('textbox').fill('link');
-    await page.getByTestId('href-item').getByRole('textbox').click();
-    await page.getByTestId('href-item').getByRole('textbox').fill('https://www.baidu.com/');
-    await page.getByRole('button', { name: 'OK' }).click();
-    const page2Promise = page.waitForEvent('popup');
-    await page.getByText('link', { exact: true }).click();
-    const page2 = await page2Promise;
-
-    await expect(page2.getByRole('button', { name: '百度一下' })).toBeVisible();
-
-    // 删除页面，避免影响其他测试
-    await page.getByRole('menu').getByText('link').hover();
-    await page.getByTestId('designer-schema-settings').hover();
-    await page.getByRole('menuitem', { name: 'Delete' }).click();
-    await page.getByRole('button', { name: 'OK' }).click();
+    await mockPage({ name: 'page title' }).goto();
+    //默认显示
+    await expect(page.getByTitle('page title')).toBeVisible();
+    await page
+      .locator('div')
+      .filter({ hasText: /^page title$/ })
+      .nth(3)
+      .click();
+    await page.getByTestId('page-designer-button').locator('path').hover();
+    await expect(page.getByRole('menuitem', { name: 'Display page title' }).getByRole('switch')).toBeChecked();
+    //不显示
+    await page.getByRole('menuitem', { name: 'Display page title' }).getByRole('switch').click();
+    await expect(page.locator('.ant-page-header')).toBeVisible();
+    await expect(page.getByTitle('page title')).not.toBeVisible();
+    await expect(
+      page
+        .locator('div')
+        .filter({ hasText: /^page title$/ })
+        .nth(3),
+    ).not.toBeVisible();
+    await expect(page.getByRole('menuitem', { name: 'Display page title' }).getByRole('switch')).not.toBeChecked();
+    //开启
+    await page.locator('.ant-page-header').click();
+    await page.getByTestId('page-designer-button').locator('path').hover();
+    await page.getByRole('menuitem', { name: 'Display page title' }).getByRole('switch').click();
+    await expect(page.getByTitle('page title')).toBeVisible();
+    await expect(
+      page
+        .locator('div')
+        .filter({ hasText: /^page title$/ })
+        .nth(3),
+    ).toBeVisible();
   });
   test('edit page title', async ({ page, mockPage }) => {
     await mockPage().goto();
