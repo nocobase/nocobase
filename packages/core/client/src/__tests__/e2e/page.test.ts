5d6bb76c5787a6f41acc0ff05a8d0f4e9cfe543e
katherinehhh <katherine_15995@163.com>
2023-10-14 22:07:11 +0800

    test: edit page title

--- packages/core/client/src/__tests__/e2e/page.test.ts
+++ packages/core/client/src/__tests__/e2e/page.test.ts
@@ -72,24 +72,22 @@ test.describe('page title', () => {
     ).toBeVisible();
   });
   test('edit page title', async ({ page, mockPage }) => {
-    await mockPage().goto();
-    await page.getByTestId('add-menu-item-button-in-header').hover();
-    await page.getByRole('menuitem', { name: 'Link' }).click();
-    await page.getByTestId('title-item').getByRole('textbox').fill('link');
-    await page.getByTestId('href-item').getByRole('textbox').click();
-    await page.getByTestId('href-item').getByRole('textbox').fill('https://www.baidu.com/');
-    await page.getByRole('button', { name: 'OK' }).click();
-    const page2Promise = page.waitForEvent('popup');
-    await page.getByText('link', { exact: true }).click();
-    const page2 = await page2Promise;
+    await mockPage({ name: 'page title1' }).goto();
 
-    await expect(page2.getByRole('button', { name: '百度一下' })).toBeVisible();
-
-    // 删除页面，避免影响其他测试
-    await page.getByRole('menu').getByText('link').hover();
-    await page.getByTestId('designer-schema-settings').hover();
-    await page.getByRole('menuitem', { name: 'Delete' }).click();
+    await expect(page.getByTitle('page title1')).toBeVisible();
+    await page
+      .locator('div')
+      .filter({ hasText: /^page title1$/ })
+      .nth(3)
+      .click();
+    await page.getByTestId('page-designer-button').locator('path').hover();
+    await page.getByText('Edit page title').click();
+    await page.getByRole('textbox').click();
+    await page.getByRole('textbox').fill('page title2');
     await page.getByRole('button', { name: 'OK' }).click();
+    await page.getByText('page title2').click();
+    await expect(page.getByText('page title2')).toBeVisible();
+    await expect(page.locator('div').filter({ hasText: /^page title1$/ })).toBeVisible();
   });
 });
 
