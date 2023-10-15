9fbdf8f5b3dba13c5e553ef9b01e0214abd2ec0a
katherinehhh <katherine_15995@163.com>
2023-10-15 19:29:52 +0800

    test: page test

--- packages/core/client/src/__tests__/e2e/page.test.ts
+++ packages/core/client/src/__tests__/e2e/page.test.ts
@@ -93,26 +93,33 @@ test.describe('page title', () => {
 
 test.describe('page tabs', () => {
   test('enable & disabled page tab', async ({ page, mockPage }) => {
+    await mockPage({ name: 'page tab' }).goto();
+    await page
+      .locator('div')
+      .filter({ hasText: /^page tab$/ })
+      .nth(3)
+      .click();
+    await page.getByTestId('page-designer-button').locator('path').hover();
+    //默认不启用
+    await expect(page.getByRole('menuitem', { name: 'Enable page tabs' }).getByRole('switch')).not.toBeChecked();
+    //启用标签
+    await page.getByText('Enable page tabs').click();
+    await expect(page.getByRole('tab').locator('div').filter({ hasText: 'Unnamed' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'plus Add tab' })).toBeVisible();
     await mockPage().goto();
-    await page.getByTestId('add-menu-item-button-in-header').hover();
-    await page.getByRole('menuitem', { name: 'Link' }).click();
-    await page.getByTestId('title-item').getByRole('textbox').fill('link');
-    await page.getByTestId('href-item').getByRole('textbox').click();
-    await page.getByTestId('href-item').getByRole('textbox').fill('https://www.baidu.com/');
-    await page.getByRole('button', { name: 'OK' }).click();
-    const page2Promise = page.waitForEvent('popup');
-    await page.getByText('link', { exact: true }).click();
-    const page2 = await page2Promise;
+    await page.getByRole('menu').locator('li').filter({ hasText: 'page tab' }).click();
 
-    await expect(page2.getByRole('button', { name: '百度一下' })).toBeVisible();
-
-    // 删除页面，避免影响其他测试
-    await page.getByRole('menu').getByText('link').hover();
-    await page.getByTestId('designer-schema-settings').hover();
-    await page.getByRole('menuitem', { name: 'Delete' }).click();
-    await page.getByRole('button', { name: 'OK' }).click();
+    //修改tab名称
+    await page.getByRole('tab').locator('div').filter({ hasText: 'Unnamed' }).click();
+    await page.getByRole('button', { name: 'drag' }).hover();
+    await page.getByRole('tab').getByLabel('menu').locator('svg').hover();
+    // await page.getByText('Edit', { exact: true }).click();
+    // await page.locator('.ant-input-affix-wrapper').click();
+    // await page.getByRole('textbox').fill('page tab 1');
+    // await page.getByRole('button', { name: 'OK' }).click();
+    // await expect(page.getByRole('tab').locator('div').filter({ hasText: 'page tab 1' })).toBeVisible();
   });
-  test('move page tab', async ({ page, mockPage }) => {
+  test.skip('move page tab', async ({ page, mockPage }) => {
     await mockPage().goto();
     await page.getByTestId('add-menu-item-button-in-header').hover();
     await page.getByRole('menuitem', { name: 'Link' }).click();
