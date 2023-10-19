0609738eab8ec33acb47567c54b18fdfcd22ec8e
Chareice <chareice@live.com>
2023-10-19 14:29:55 +0800

    feat: sync false option

--- packages/plugins/@nocobase/plugin-collection-manager/src/server/__tests__/sync.test.ts
+++ packages/plugins/@nocobase/plugin-collection-manager/src/server/__tests__/sync.test.ts
@@ -15,6 +15,30 @@ describe('sync collection', () => {
     await app.destroy();
   });
 
+  it('should not sync collection that set sync false', async () => {
+    const c1 = db.collection({
+      name: 'c1',
+      sync: false,
+      fields: [{ type: 'string', name: 'f1' }],
+    });
+
+    await db.sync({
+      force: true,
+    });
+
+    const queryInterface = db.sequelize.getQueryInterface();
+    let err;
+
+    try {
+      await queryInterface.describeTable(c1.model.tableName);
+    } catch (e) {
+      err = e;
+    }
+
+    expect(err).toBeTruthy();
+    expect(err.message.includes('No description found')).toBeTruthy();
+  });
+
   it('should not remove column when async with drop false', async () => {
     const getTableInfo = async (tableName: string) => {
       const queryInterface = db.sequelize.getQueryInterface();
