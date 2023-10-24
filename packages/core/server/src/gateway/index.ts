b6e66bbae92bd72e4ae6f4903635a55a1f12998c
Chareice <chareice@live.com>
2023-10-24 22:48:40 +0800

    chore: start app if app is initialized status

--- packages/core/server/src/gateway/index.ts
+++ packages/core/server/src/gateway/index.ts
@@ -13,7 +13,7 @@ import { parse } from 'url';
 import xpipe from 'xpipe';
 import { AppSupervisor } from '../app-supervisor';
 import { ApplicationOptions } from '../application';
-import { PLUGIN_STATICS_PATH, getPackageDirByExposeUrl, getPackageNameByExposeUrl } from '../plugin-manager';
+import { getPackageDirByExposeUrl, getPackageNameByExposeUrl, PLUGIN_STATICS_PATH } from '../plugin-manager';
 import { applyErrorWithArgs, getErrorWithCode } from './errors';
 import { IPCSocketClient } from './ipc-socket-client';
 import { IPCSocketServer } from './ipc-socket-server';
@@ -198,6 +198,11 @@ export class Gateway extends EventEmitter {
     if (appStatus === 'initializing') {
       this.responseErrorWithCode('APP_INITIALIZING', res, { appName: handleApp });
       return;
+    }
+
+    if (appStatus === 'initialized') {
+      const appInstance = await AppSupervisor.getInstance().getApp(handleApp);
+      await appInstance.runAsCLI(['start'], { from: 'user' });
     }
 
     const app = await AppSupervisor.getInstance().getApp(handleApp);
