afa7057b93b1285e3acdd8c0e78633ebcbc0f064
Rain <958414905@qq.com>
2023-11-01 16:07:03 +0800

    chore: add aria-label for SchemaInitializerButton

--- packages/core/client/src/application/schema-initializer/components/SchemaInitializerButton.tsx
+++ packages/core/client/src/application/schema-initializer/components/SchemaInitializerButton.tsx
@@ -1,17 +1,18 @@
 import { Button } from 'antd';
 import React, { FC } from 'react';
-import { useCompile } from '../../../schema-component';
-import { SchemaInitializerOptions } from '../types';
 import { Icon } from '../../../icon';
+import { useCompile } from '../../../schema-component';
+import { useGetAriaLabelOfSchemaInitializer } from '../../../schema-initializer/hooks/useGetAriaLabelOfSchemaInitializer';
+import { SchemaInitializerOptions } from '../types';
 
 export const SchemaInitializerButton: FC<SchemaInitializerOptions> = (props) => {
   const { style, options, ...others } = props;
   const compile = useCompile();
+  const { getAriaLabel } = useGetAriaLabelOfSchemaInitializer();
 
   return (
     <Button
       type={'dashed'}
-      data-testid={props['data-testid']}
       style={{
         borderColor: 'var(--colorSettings)',
         color: 'var(--colorSettings)',
@@ -19,6 +20,7 @@ export const SchemaInitializerButton: FC<SchemaInitializerOptions> = (props) => 
       }}
       icon={typeof options.icon === 'string' ? <Icon type={options.icon as string} /> : options.icon}
       {...others}
+      aria-label={getAriaLabel()}
     >
       {compile(options.title)}
     </Button>
