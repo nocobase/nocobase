403689fd1edd73dd3d3a2f749182856fe869e7df
Rain <958414905@qq.com>
2023-11-05 12:24:08 +0800

    perf: avoid page lag or stuttering

--- packages/core/client/src/schema-settings/SchemaSettings.tsx
+++ packages/core/client/src/schema-settings/SchemaSettings.tsx
@@ -72,6 +72,7 @@ import {
   updateFilterTargets,
   useFormActiveFields,
 } from '../block-provider/hooks';
+import { useCollectionFilterOptionsV2 } from '../collection-manager/action-hooks';
 import {
   FilterBlockType,
   getSupportFieldsByAssociation,
@@ -1585,6 +1586,7 @@ SchemaSettings.DefaultValue = function DefaultValueConfigure(props: { fieldSchem
   const { t } = useTranslation();
   const actionCtx = useActionContext();
   let targetField;
+
   const { getField } = useCollection();
   const { getCollectionJoinField, getCollectionFields, getAllCollectionsInheritChain } = useCollectionManager();
   const variables = useVariables();
@@ -1592,17 +1594,21 @@ SchemaSettings.DefaultValue = function DefaultValueConfigure(props: { fieldSchem
   const collection = useCollection();
   const record = useRecord();
   const { form } = useFormBlockContext();
-  const currentFormFields = useCollectionFilterOptions(collection);
+  const { getFields } = useCollectionFilterOptionsV2(collection);
   const { isInSubForm, isInSubTable } = useFlag() || {};
 
   const { name } = collection;
-  const collectionField = getField(fieldSchema['name']) || getCollectionJoinField(fieldSchema['x-collection-field']);
+  const collectionField = useMemo(
+    () => getField(fieldSchema['name']) || getCollectionJoinField(fieldSchema['x-collection-field']),
+    [fieldSchema, getCollectionJoinField, getField],
+  );
   const fieldSchemaWithoutRequired = _.omit(fieldSchema, 'required');
   if (collectionField?.target) {
     targetField = getCollectionJoinField(
       `${collectionField.target}.${fieldSchema['x-component-props']?.fieldNames?.label || 'id'}`,
     );
   }
+
   const parentFieldSchema = collectionField?.interface === 'm2o' && findParentFieldSchema(fieldSchema);
   const parentCollectionField = parentFieldSchema && getCollectionJoinField(parentFieldSchema?.['x-collection-field']);
   const tableCtx = useTableBlockContext();
@@ -1619,123 +1625,147 @@ SchemaSettings.DefaultValue = function DefaultValueConfigure(props: { fieldSchem
       // fix https://nocobase.height.app/T-1355
       // 工作流人工节点的 `自定义表单` 区块，与其它表单区块不同，根据它的数据表名称，获取到的字段列表为空，所以需要在这里特殊处理一下
       if (!fields?.length && currentForm) {
-        currentForm.children = formatVariableScop(currentFormFields);
+        currentForm.children = formatVariableScop(getFields());
       }
 
       return scope;
     },
-    [currentFormFields, name],
+    [getFields, name],
+  );
+
+  const DefaultValueComponent: any = useMemo(() => {
+    return {
+      ArrayCollapse,
+      FormLayout,
+      VariableInput: (props) => {
+        return (
+          <FlagProvider isInSubForm={isInSubForm} isInSubTable={isInSubTable} isInSetDefaultValueDialog>
+            <VariableInput {...props} />
+          </FlagProvider>
+        );
+      },
+    };
+  }, [isInSubForm, isInSubTable]);
+
+  const schema = useMemo(() => {
+    return {
+      type: 'object',
+      title: t('Set default value'),
+      properties: {
+        default: {
+          'x-decorator': 'FormItem',
+          'x-component': 'VariableInput',
+          'x-component-props': {
+            ...(fieldSchema?.['x-component-props'] || {}),
+            collectionField,
+            contextCollectionName: isAllowContextVariable && tableCtx.collection,
+            schema: collectionField?.uiSchema,
+            targetFieldSchema: fieldSchema,
+            className: defaultInputStyle,
+            form,
+            record,
+            returnScope,
+            shouldChange: getShouldChange({
+              collectionField,
+              variables,
+              localVariables,
+              getAllCollectionsInheritChain,
+            }),
+            renderSchemaComponent: function Com(props) {
+              const s = _.cloneDeep(fieldSchemaWithoutRequired) || ({} as Schema);
+              s.title = '';
+              s.name = 'default';
+              s['x-read-pretty'] = false;
+              s['x-disabled'] = false;
+
+              const defaultValue = getFieldDefaultValue(s, collectionField);
+
+              if (collectionField.target && s['x-component-props']) {
+                s['x-component-props'].mode = 'Select';
+              }
+
+              if (collectionField?.uiSchema.type) {
+                s.type = collectionField.uiSchema.type;
+              }
+
+              if (collectionField?.uiSchema['x-component'] === 'Checkbox') {
+                s['x-component-props'].defaultChecked = defaultValue;
+
+                // 在这里如果不设置 type 为 void，会导致设置的默认值不生效
+                // 但是我不知道为什么必须要设置为 void ？
+                s.type = 'void';
+              }
+
+              const schema = {
+                ...(s || {}),
+                'x-decorator': 'FormItem',
+                'x-component-props': {
+                  ...s['x-component-props'],
+                  collectionName: collectionField?.collectionName,
+                  targetField,
+                  onChange: props.onChange,
+                  defaultValue: isVariable(defaultValue) ? '' : defaultValue,
+                  style: {
+                    width: '100%',
+                    verticalAlign: 'top',
+                    minWidth: '200px',
+                  },
+                },
+                default: isVariable(defaultValue) ? '' : defaultValue,
+              } as ISchema;
+
+              return (
+                <FormProvider>
+                  <SchemaComponent schema={schema} />
+                </FormProvider>
+              );
+            },
+          },
+          title: t('Default value'),
+          default: getFieldDefaultValue(fieldSchema, collectionField),
+        },
+      },
+    } as ISchema;
+  }, [
+    collectionField,
+    fieldSchema,
+    fieldSchemaWithoutRequired,
+    form,
+    getAllCollectionsInheritChain,
+    isAllowContextVariable,
+    localVariables,
+    record,
+    returnScope,
+    t,
+    tableCtx.collection,
+    targetField,
+    variables,
+  ]);
+  const handleSubmit: (values: any) => void = useCallback(
+    (v) => {
+      const schema: ISchema = {
+        ['x-uid']: fieldSchema['x-uid'],
+      };
+      fieldSchema.default = v.default;
+      if (!v.default && v.default !== 0) {
+        field.value = null;
+      }
+      schema.default = v.default;
+      dn.emit('patch', {
+        schema,
+        currentSchema,
+      });
+    },
+    [currentSchema, dn, field, fieldSchema],
   );
 
   return (
     <SchemaSettings.ModalItem
       title={t('Set default value')}
-      components={{
-        ArrayCollapse,
-        FormLayout,
-        VariableInput: (props) => {
-          return (
-            <FlagProvider isInSubForm={isInSubForm} isInSubTable={isInSubTable} isInSetDefaultValueDialog>
-              <VariableInput {...props} />
-            </FlagProvider>
-          );
-        },
-      }}
+      components={DefaultValueComponent}
       width={800}
-      schema={
-        {
-          type: 'object',
-          title: t('Set default value'),
-          properties: {
-            default: {
-              'x-decorator': 'FormItem',
-              'x-component': 'VariableInput',
-              'x-component-props': {
-                ...(fieldSchema?.['x-component-props'] || {}),
-                collectionField,
-                contextCollectionName: isAllowContextVariable && tableCtx.collection,
-                schema: collectionField?.uiSchema,
-                targetFieldSchema: fieldSchema,
-                className: defaultInputStyle,
-                form,
-                record,
-                returnScope,
-                shouldChange: getShouldChange({
-                  collectionField,
-                  variables,
-                  localVariables,
-                  getAllCollectionsInheritChain,
-                }),
-                renderSchemaComponent: function Com(props) {
-                  const s = _.cloneDeep(fieldSchemaWithoutRequired) || ({} as Schema);
-                  s.title = '';
-                  s.name = 'default';
-                  s['x-read-pretty'] = false;
-                  s['x-disabled'] = false;
-
-                  const defaultValue = getFieldDefaultValue(s, collectionField);
-
-                  if (collectionField.target && s['x-component-props']) {
-                    s['x-component-props'].mode = 'Select';
-                  }
-
-                  if (collectionField?.uiSchema.type) {
-                    s.type = collectionField.uiSchema.type;
-                  }
-
-                  if (collectionField?.uiSchema['x-component'] === 'Checkbox') {
-                    s['x-component-props'].defaultChecked = defaultValue;
-
-                    // 在这里如果不设置 type 为 void，会导致设置的默认值不生效
-                    // 但是我不知道为什么必须要设置为 void ？
-                    s.type = 'void';
-                  }
-
-                  const schema = {
-                    ...(s || {}),
-                    'x-decorator': 'FormItem',
-                    'x-component-props': {
-                      ...s['x-component-props'],
-                      collectionName: collectionField?.collectionName,
-                      targetField,
-                      onChange: props.onChange,
-                      defaultValue: isVariable(defaultValue) ? '' : defaultValue,
-                      style: {
-                        width: '100%',
-                        verticalAlign: 'top',
-                        minWidth: '200px',
-                      },
-                    },
-                    default: isVariable(defaultValue) ? '' : defaultValue,
-                  } as ISchema;
-
-                  return (
-                    <FormProvider>
-                      <SchemaComponent schema={schema} />
-                    </FormProvider>
-                  );
-                },
-              },
-              title: t('Default value'),
-              default: getFieldDefaultValue(fieldSchema, collectionField),
-            },
-          },
-        } as ISchema
-      }
-      onSubmit={(v) => {
-        const schema: ISchema = {
-          ['x-uid']: fieldSchema['x-uid'],
-        };
-        fieldSchema.default = v.default;
-        if (!v.default && v.default !== 0) {
-          field.value = null;
-        }
-        schema.default = v.default;
-        dn.emit('patch', {
-          schema,
-          currentSchema,
-        });
-      }}
+      schema={schema}
+      onSubmit={handleSubmit}
     />
   );
 };
