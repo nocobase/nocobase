2e36af795882c3487ff97d1b1dbb624ff47a3b80
chenos <chenlinxh@gmail.com>
2023-11-05 15:53:26 +0800

    fix: demos

--- packages/core/client/.dumirc.ts
+++ packages/core/client/.dumirc.ts
@@ -47,7 +47,7 @@ export default defineConfig({
                 },
                 {
                   title: 'SettingsCenter',
-                  link: '/apis/api-client',
+                  link: '#',
                 },
               ],
             },
