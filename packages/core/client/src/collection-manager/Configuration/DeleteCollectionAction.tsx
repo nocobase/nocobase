a6df1a9c72c8691c3834627ac07e70b538bda2c2
katherinehhh <katherine_15995@163.com>
2023-11-06 10:47:45 +0800

    revert: code improve

--- packages/core/client/src/collection-manager/Configuration/DeleteCollectionAction.tsx
+++ packages/core/client/src/collection-manager/Configuration/DeleteCollectionAction.tsx
@@ -1,18 +1,78 @@
 import React, { useState } from 'react';
 import { css } from '@emotion/css';
 import { useTranslation } from 'react-i18next';
-import { Button } from 'antd';
+import { Button, message } from 'antd';
+import { useForm } from '@formily/react';
 import { DeleteOutlined, ExclamationCircleFilled } from '@ant-design/icons';
 import { RecordProvider, useRecord } from '../../record-provider';
 import { ActionContextProvider, SchemaComponent } from '../../schema-component';
 import * as components from './components';
-import { useCancelAction, useBulkDestroyActionAndRefreshCM, useDestroyActionAndRefreshCM } from '../action-hooks';
+import { useCollectionManager, useResourceActionContext, useResourceContext, useActionContext } from '../../';
+import { useCancelAction } from '../action-hooks';
 
 export const DeleteCollection = (props) => {
   const record = useRecord();
   return <DeleteCollectionAction item={record} {...props} />;
 };
 
+export const useDestroyActionAndRefreshCM = () => {
+  const { run } = useDestroyAction();
+  const { refreshCM } = useCollectionManager();
+  return {
+    async run() {
+      await run();
+      await refreshCM();
+    },
+  };
+};
+export const useBulkDestroyActionAndRefreshCM = () => {
+  const { run } = useBulkDestroyAction();
+  const { refreshCM } = useCollectionManager();
+  return {
+    async run() {
+      await run();
+      await refreshCM();
+    },
+  };
+};
+export const useDestroyAction = () => {
+  const { refresh } = useResourceActionContext();
+  const { resource, targetKey } = useResourceContext();
+  const { [targetKey]: filterByTk } = useRecord();
+  const ctx = useActionContext();
+  const form = useForm();
+  const { cascade } = form?.values || {};
+  return {
+    async run() {
+      await resource.destroy({ filterByTk, cascade });
+      ctx?.setVisible?.(false);
+      refresh();
+    },
+  };
+};
+
+export const useBulkDestroyAction = () => {
+  const { state, setState, refresh } = useResourceActionContext();
+  const { resource } = useResourceContext();
+  const ctx = useActionContext();
+  const { t } = useTranslation();
+  const form = useForm();
+  const { cascade } = form?.values || {};
+  return {
+    async run() {
+      if (!state?.selectedRowKeys?.length) {
+        return message.error(t('Please select the records you want to delete'));
+      }
+      await resource.destroy({
+        filterByTk: state?.selectedRowKeys || [],
+        cascade,
+      });
+      ctx?.setVisible?.(false);
+      setState?.({ selectedRowKeys: [] });
+      refresh();
+    },
+  };
+};
 export const DeleteCollectionAction = (props) => {
   const { scope, getContainer, item: record, children, isBulk, useAction, ...otherProps } = props;
   const { t } = useTranslation();
@@ -35,6 +95,7 @@ export const DeleteCollectionAction = (props) => {
       </span>
     );
   };
+
   return (
     <RecordProvider record={record}>
       <ActionContextProvider value={{ visible, setVisible }}>
