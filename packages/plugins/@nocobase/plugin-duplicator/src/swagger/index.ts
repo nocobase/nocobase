482c0bd6bb5e682e3d84b7be083684f9e5ba8568
Chareice <chareice@live.com>
2023-11-08 18:47:04 +0800

    chore: api doc

--- packages/plugins/@nocobase/plugin-duplicator/src/swagger/index.ts
+++ packages/plugins/@nocobase/plugin-duplicator/src/swagger/index.ts
@@ -306,6 +306,68 @@ export default {
         },
       },
     },
+    '/backupFiles:dumpableCollections': {
+      get: {
+        summary: 'Get dumpable collections',
+        responses: {
+          '200': {
+            description: 'A list of dumpable collections',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    meta: {
+                      type: 'array',
+                      items: {
+                        type: 'object',
+                        properties: {
+                          name: {
+                            type: 'string',
+                          },
+                          title: {
+                            type: 'string',
+                          },
+                        },
+                      },
+                    },
+                    config: {
+                      type: 'array',
+                      items: {
+                        type: 'object',
+                        properties: {
+                          name: {
+                            type: 'string',
+                          },
+                          title: {
+                            type: 'string',
+                          },
+                        },
+                      },
+                    },
+                    business: {
+                      type: 'array',
+                      items: {
+                        type: 'object',
+                        properties: {
+                          name: {
+                            type: 'string',
+                          },
+                          title: {
+                            type: 'string',
+                          },
+                        },
+                      },
+                    },
+                  },
+                  required: ['meta', 'config', 'business'],
+                },
+              },
+            },
+          },
+        },
+      },
+    },
   },
 
   components: {
