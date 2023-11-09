1470c20a3b102b4ad0ba5ffbf5b96b33a8883117
chenos <chenlinxh@gmail.com>
2023-11-09 09:49:26 +0800

    fix: formula

--- packages/plugins/@nocobase/plugin-mock-collections/src/server/field-interfaces/formula.ts
+++ packages/plugins/@nocobase/plugin-mock-collections/src/server/field-interfaces/formula.ts
@@ -1,10 +1,16 @@
 export const formula = () => ({
-  type: 'string',
-  // title,
-  'x-component': 'Formula.Result',
-  'x-component-props': {
-    stringMode: true,
-    step: '1',
+  type: 'formula',
+  dataType: 'double',
+  engine: 'math.js',
+  // name,
+  uiSchema: {
+    type: 'string',
+    // title,
+    'x-component': 'Formula.Result',
+    'x-component-props': {
+      stringMode: true,
+      step: '1',
+    },
+    'x-read-pretty': true,
   },
-  'x-read-pretty': true,
 });
