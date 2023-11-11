8bc935d9b3bd9c385cd11c911a61aedc35230f7e
katherinehhh <katherine_15995@163.com>
2023-11-11 22:07:15 +0800

    test: client test

--- packages/plugins/@nocobase/plugin-kanban/src/client/demos/demo1.tsx
+++ packages/plugins/@nocobase/plugin-kanban/src/client/demos/demo1.tsx
@@ -7,11 +7,19 @@ import {
   CurrentUserProvider,
   SchemaComponent,
   SchemaComponentProvider,
+  APIClient,
 } from '@nocobase/client';
 import React from 'react';
-import { mockAPIClient } from '../../../../testUtils';
 import collections from './collections';
 import data from './data';
+import MockAdapter from 'axios-mock-adapter';
+
+export const mockAPIClient = () => {
+  const apiClient = new APIClient();
+  const mockRequest = new MockAdapter(apiClient.axios);
+
+  return { apiClient, mockRequest };
+};
 
 const { apiClient, mockRequest } = mockAPIClient();
 const sleep = (value: number) => new Promise((resolve) => setTimeout(resolve, value));
