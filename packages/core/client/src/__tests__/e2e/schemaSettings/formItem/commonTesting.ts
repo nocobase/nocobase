dd4a572fdc2776c558ed43ce0c591c19f2d8f04d
Rain <958414905@qq.com>
2023-11-20 22:40:11 +0800

    test: add tests for email field in creating block

--- packages/core/client/src/__tests__/e2e/schemaSettings/formItem/commonTesting.ts
+++ packages/core/client/src/__tests__/e2e/schemaSettings/formItem/commonTesting.ts
@@ -1,4 +1,4 @@
-import { Page, expect } from '@nocobase/test/client';
+import { Page, expect, test } from '@nocobase/test/client';
 
 export async function testEditFieldTitle(page: Page) {
   await page.getByRole('menuitem', { name: 'Edit field title' }).click();
@@ -10,6 +10,7 @@ export async function testEditFieldTitle(page: Page) {
 }
 
 export async function testDisplayTitle(page: Page, title: string) {
+  // 默认情况下是开启状态
   await expect(page.getByRole('menuitem', { name: 'Display title' }).getByRole('switch')).toBeChecked();
   await page.getByRole('menuitem', { name: 'Display title' }).click();
   await expect(page.getByRole('menuitem', { name: 'Display title' }).getByRole('switch')).not.toBeChecked();
@@ -29,3 +30,39 @@ export async function testRequired(page: Page) {
   await page.getByRole('menuitem', { name: 'Required' }).click();
   await expect(page.getByRole('menuitem', { name: 'Required' }).getByRole('switch')).toBeChecked();
 }
+
+export async function clickDeleteAndOk(page: Page) {
+  await page.getByRole('menuitem', { name: 'Delete' }).click();
+  await page.getByRole('button', { name: 'OK', exact: true }).click();
+}
+
+export const commonTesting = (showMenu, fieldTitle: string) => {
+  test('edit field title', async ({ page, mockPage }) => {
+    await showMenu(page, mockPage);
+    await testEditFieldTitle(page);
+  });
+
+  test('display title', async ({ page, mockPage }) => {
+    await showMenu(page, mockPage);
+    await testDisplayTitle(page, fieldTitle);
+  });
+
+  test('edit description', async ({ page, mockPage }) => {
+    await showMenu(page, mockPage);
+    await testEditDescription(page);
+  });
+
+  // TODO: BUG，现在没有该选项
+  test.skip('edit tooltip', async ({ page, mockPage }) => {});
+
+  test('required', async ({ page, mockPage }) => {
+    await showMenu(page, mockPage);
+    await testRequired(page);
+  });
+
+  test('delete', async ({ page, mockPage }) => {
+    await showMenu(page, mockPage);
+    await clickDeleteAndOk(page);
+    await expect(page.getByText(fieldTitle)).not.toBeVisible();
+  });
+};
--- packages/core/client/src/__tests__/e2e/schemaSettings/formItem/creatingForm/basic.test.ts
+++ packages/core/client/src/__tests__/e2e/schemaSettings/formItem/creatingForm/basic.test.ts
@@ -1,5 +1,5 @@
 import { Page, expect, oneTableBlockWithAddNewAndViewAndEditAndBasicFields, test } from '@nocobase/test/client';
-import { testDisplayTitle, testEditDescription, testEditFieldTitle, testRequired } from '../commonTesting';
+import { commonTesting } from '../commonTesting';
 
 test.describe('color', () => {
   const showMenu = async (page: Page, mockPage) => {
@@ -10,29 +10,7 @@ test.describe('color', () => {
     await page.getByLabel('designer-schema-settings-CollectionField-FormItem.Designer-general-general.color').hover();
   };
 
-  test('edit field title', async ({ page, mockPage }) => {
-    await showMenu(page, mockPage);
-    await testEditFieldTitle(page);
-  });
-
-  test('display title', async ({ page, mockPage }) => {
-    await showMenu(page, mockPage);
-    // 默认情况下是开启状态
-    await testDisplayTitle(page, 'color');
-  });
-
-  test('edit description', async ({ page, mockPage }) => {
-    await showMenu(page, mockPage);
-    await testEditDescription(page);
-  });
-
-  // TODO: BUG，现在没有该选项
-  test.skip('edit tooltip', async ({ page, mockPage }) => {});
-
-  test('required', async ({ page, mockPage }) => {
-    await showMenu(page, mockPage);
-    await testRequired(page);
-  });
+  commonTesting(showMenu, 'color');
 
   test('pattern', async ({ page, mockPage }) => {
     const nocoPage = await mockPage(oneTableBlockWithAddNewAndViewAndEditAndBasicFields).waitForInit();
@@ -57,14 +35,209 @@ test.describe('color', () => {
     await page.getByRole('option', { name: 'Easy-reading' }).click();
     await expect(page.getByLabel('color-picker-read-pretty')).toBeVisible();
   });
+
+  test('set default value', async ({ page, mockPage }) => {
+    await showMenu(page, mockPage);
+    // 简单测试下是否有选项，值的话不太好测
+    await expect(page.getByRole('menuitem', { name: 'Set default value' })).toBeVisible();
+  });
 });
-test.describe('email', () => {});
-test.describe('icon', () => {});
-test.describe('single line text', () => {});
-test.describe('integer', () => {});
-test.describe('number', () => {});
-test.describe('password', () => {});
-test.describe('percent', () => {});
-test.describe('phone', () => {});
-test.describe('long text', () => {});
-test.describe('URL', () => {});
+
+test.describe('email', () => {
+  const gotoPage = async (mockPage) => {
+    const nocoPage = await mockPage(oneTableBlockWithAddNewAndViewAndEditAndBasicFields).waitForInit();
+    await nocoPage.goto();
+  };
+
+  const openDialog = async (page: Page) => {
+    await page.getByRole('button', { name: 'Add new' }).click();
+  };
+
+  const showMenu = async (page: Page) => {
+    await page.getByLabel('block-item-CollectionField-general-form-general.email-email').hover();
+    await page.getByLabel('designer-schema-settings-CollectionField-FormItem.Designer-general-general.email').hover();
+  };
+
+  const openDialogAndShowMenu = async (page: Page, mockPage) => {
+    await gotoPage(mockPage);
+    await openDialog(page);
+    await showMenu(page);
+  };
+
+  commonTesting(openDialogAndShowMenu, 'email');
+
+  test('set default value', async ({ page, mockPage }) => {
+    await openDialogAndShowMenu(page, mockPage);
+
+    // 设置一个常量作为默认值
+    await page.getByRole('menuitem', { name: 'Set default value' }).click();
+    await page.getByLabel('block-item-VariableInput-').getByRole('textbox').click();
+    await page.getByLabel('block-item-VariableInput-').getByRole('textbox').fill('test@nocobase.com');
+    await page.getByRole('button', { name: 'OK', exact: true }).click();
+
+    // 关闭弹窗，然后再次打开后，应该显示刚才设置的默认值
+    await page.getByLabel('drawer-Action.Container-general-Add record-mask').click();
+    await openDialog(page);
+    await expect(
+      page.getByLabel('block-item-CollectionField-general-form-general.email-email').getByRole('textbox'),
+    ).toHaveValue('test@nocobase.com');
+
+    // 设置一个变量作为默认值
+    await showMenu(page);
+    await page.getByRole('menuitem', { name: 'Set default value' }).click();
+    await page.getByLabel('variable-button').click();
+    await page.getByRole('menuitemcheckbox', { name: 'Current user' }).click();
+    await page.getByRole('menuitemcheckbox', { name: 'Email' }).click();
+    await page.getByRole('button', { name: 'OK' }).click();
+
+    // 关闭弹窗，然后再次打开后，应该显示当前用户的邮箱：admin@nocobase.com
+    await page.getByLabel('drawer-Action.Container-general-Add record-mask').click();
+    await openDialog(page);
+    await expect(
+      page.getByLabel('block-item-CollectionField-general-form-general.email-email').getByRole('textbox'),
+    ).toHaveValue('admin@nocobase.com');
+  });
+
+  test('pattern', async ({ page, mockPage }) => {
+    await gotoPage(mockPage);
+    await openDialog(page);
+
+    // 填充上一个值，方便后面断言
+    await page.getByLabel('block-item-CollectionField-general-form-general.email-email').getByRole('textbox').click();
+    await page
+      .getByLabel('block-item-CollectionField-general-form-general.email-email')
+      .getByRole('textbox')
+      .fill('admin@nocobase.com');
+
+    // 默认情况下，显示的是 Editable
+    await showMenu(page);
+    await expect(page.getByText('PatternEditable')).toBeVisible();
+
+    // 更改为 Readonly
+    await page.getByRole('menuitem', { name: 'Pattern' }).click();
+    await page.getByRole('option', { name: 'Readonly' }).click();
+    // 输入框应该被禁用
+    await expect(
+      page.getByLabel('block-item-CollectionField-general-form-general.email-email').getByRole('textbox'),
+    ).toBeDisabled();
+
+    // 更改为 Easy-reading
+    await showMenu(page);
+    await page.getByRole('menuitem', { name: 'Pattern' }).click();
+    await page.getByRole('option', { name: 'Easy-reading' }).click();
+    await expect(
+      page.getByLabel('block-item-CollectionField-general-form-general.email-email').getByRole('textbox'),
+    ).not.toBeVisible();
+    await expect(page.getByLabel('block-item-CollectionField-general-form-general.email-email')).toHaveText(
+      'email:admin@nocobase.com',
+    );
+  });
+});
+
+test.describe('icon', () => {
+  const showMenu = async (page: Page, mockPage) => {
+    const nocoPage = await mockPage(oneTableBlockWithAddNewAndViewAndEditAndBasicFields).waitForInit();
+    await nocoPage.goto();
+    await page.getByRole('button', { name: 'Add new' }).click();
+    await page.getByLabel('block-item-CollectionField-general-form-general.icon-icon').hover();
+    await page.getByLabel('designer-schema-settings-CollectionField-FormItem.Designer-general-general.email').hover();
+  };
+
+  commonTesting(showMenu, 'icon');
+});
+
+test.describe('single line text', () => {
+  const showMenu = async (page: Page, mockPage) => {
+    const nocoPage = await mockPage(oneTableBlockWithAddNewAndViewAndEditAndBasicFields).waitForInit();
+    await nocoPage.goto();
+    await page.getByRole('button', { name: 'Add new' }).click();
+    await page.getByLabel('block-item-CollectionField-general-form-general.singleLineText-singleLineText').hover();
+    await page.getByLabel('designer-schema-settings-CollectionField-FormItem.Designer-general-general.email').hover();
+  };
+
+  commonTesting(showMenu, 'singleLineText');
+});
+
+test.describe('integer', () => {
+  const showMenu = async (page: Page, mockPage) => {
+    const nocoPage = await mockPage(oneTableBlockWithAddNewAndViewAndEditAndBasicFields).waitForInit();
+    await nocoPage.goto();
+    await page.getByRole('button', { name: 'Add new' }).click();
+    await page.getByLabel('block-item-CollectionField-general-form-general.integer-integer').hover();
+    await page.getByLabel('designer-schema-settings-CollectionField-FormItem.Designer-general-general.email').hover();
+  };
+
+  commonTesting(showMenu, 'integer');
+});
+
+test.describe('number', () => {
+  const showMenu = async (page: Page, mockPage) => {
+    const nocoPage = await mockPage(oneTableBlockWithAddNewAndViewAndEditAndBasicFields).waitForInit();
+    await nocoPage.goto();
+    await page.getByRole('button', { name: 'Add new' }).click();
+    await page.getByLabel('block-item-CollectionField-general-form-general.number-number').hover();
+    await page.getByLabel('designer-schema-settings-CollectionField-FormItem.Designer-general-general.email').hover();
+  };
+
+  commonTesting(showMenu, 'number');
+});
+
+test.describe('password', () => {
+  const showMenu = async (page: Page, mockPage) => {
+    const nocoPage = await mockPage(oneTableBlockWithAddNewAndViewAndEditAndBasicFields).waitForInit();
+    await nocoPage.goto();
+    await page.getByRole('button', { name: 'Add new' }).click();
+    await page.getByLabel('block-item-CollectionField-general-form-general.password-password').hover();
+    await page.getByLabel('designer-schema-settings-CollectionField-FormItem.Designer-general-general.email').hover();
+  };
+
+  commonTesting(showMenu, 'password');
+});
+
+test.describe('percent', () => {
+  const showMenu = async (page: Page, mockPage) => {
+    const nocoPage = await mockPage(oneTableBlockWithAddNewAndViewAndEditAndBasicFields).waitForInit();
+    await nocoPage.goto();
+    await page.getByRole('button', { name: 'Add new' }).click();
+    await page.getByLabel('block-item-CollectionField-general-form-general.percent-percent').hover();
+    await page.getByLabel('designer-schema-settings-CollectionField-FormItem.Designer-general-general.email').hover();
+  };
+
+  commonTesting(showMenu, 'percent');
+});
+
+test.describe('phone', () => {
+  const showMenu = async (page: Page, mockPage) => {
+    const nocoPage = await mockPage(oneTableBlockWithAddNewAndViewAndEditAndBasicFields).waitForInit();
+    await nocoPage.goto();
+    await page.getByRole('button', { name: 'Add new' }).click();
+    await page.getByLabel('block-item-CollectionField-general-form-general.phone-phone').hover();
+    await page.getByLabel('designer-schema-settings-CollectionField-FormItem.Designer-general-general.email').hover();
+  };
+
+  commonTesting(showMenu, 'phone');
+});
+
+test.describe('long text', () => {
+  const showMenu = async (page: Page, mockPage) => {
+    const nocoPage = await mockPage(oneTableBlockWithAddNewAndViewAndEditAndBasicFields).waitForInit();
+    await nocoPage.goto();
+    await page.getByRole('button', { name: 'Add new' }).click();
+    await page.getByLabel('block-item-CollectionField-general-form-general.longText-longText').hover();
+    await page.getByLabel('designer-schema-settings-CollectionField-FormItem.Designer-general-general.email').hover();
+  };
+
+  commonTesting(showMenu, 'longText');
+});
+
+test.describe('URL', () => {
+  const showMenu = async (page: Page, mockPage) => {
+    const nocoPage = await mockPage(oneTableBlockWithAddNewAndViewAndEditAndBasicFields).waitForInit();
+    await nocoPage.goto();
+    await page.getByRole('button', { name: 'Add new' }).click();
+    await page.getByLabel('block-item-CollectionField-general-form-general.url-url').hover();
+    await page.getByLabel('designer-schema-settings-CollectionField-FormItem.Designer-general-general.email').hover();
+  };
+
+  commonTesting(showMenu, 'url');
+});
