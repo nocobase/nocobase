3f469731770b7e1224ebbb63277ac86adc51b97d
Rain <958414905@qq.com>
2023-11-29 12:05:28 +0800

    fix: fix init state

--- packages/core/client/src/schema-component/antd/form-v2/utils.tsx
+++ packages/core/client/src/schema-component/antd/form-v2/utils.tsx
@@ -40,10 +40,10 @@ export const collectFieldStateOfLinkageRules = ({
   variables,
   localVariables,
 }: Props) => {
-  const requiredResult = field?.linkageProperty?.required || [field?.initProperty?.required || false];
-  const displayResult = field?.linkageProperty?.display || [field?.initProperty?.display];
-  const patternResult = field?.linkageProperty?.pattern || [field?.initProperty?.pattern];
-  const valueResult = field?.linkageProperty?.value || [field?.initProperty?.value];
+  const requiredResult = field?.linkageProperty?.required || [field?.required || false];
+  const displayResult = field?.linkageProperty?.display || [field?.display];
+  const patternResult = field?.linkageProperty?.pattern || [field?.pattern];
+  const valueResult = field?.linkageProperty?.value || [field?.value];
   const { evaluate } = evaluators.get('formula.js');
 
   switch (operator) {
