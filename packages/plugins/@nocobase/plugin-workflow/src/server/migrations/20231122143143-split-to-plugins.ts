58ddaac4250d4ad97e20f6d325995ad769571fab
mytharcher <mytharcher@gmail.com>
2023-11-30 23:31:46 +0800

    fix(plugin-workflow): fix migration version matching

--- packages/plugins/@nocobase/plugin-workflow/src/server/migrations/20231122143143-split-to-plugins.ts
+++ packages/plugins/@nocobase/plugin-workflow/src/server/migrations/20231122143143-split-to-plugins.ts
@@ -2,7 +2,7 @@ import { Migration } from '@nocobase/server';
 
 export default class extends Migration {
   async up() {
-    const match = await this.app.version.satisfies('<0.16.0-alpha.4');
+    const match = await this.app.version.satisfies('<0.16.0-alpha.5');
     if (!match) {
       return;
     }
