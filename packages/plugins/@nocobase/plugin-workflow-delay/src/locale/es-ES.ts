1c5809b334e5d3f92952cd3a9952907c40ae79e8
mytharcher <mytharcher@gmail.com>
2023-11-30 11:11:50 +0800

    refactor(plugin-workflow): move locale to plugins

---
+++ packages/plugins/@nocobase/plugin-workflow-delay/src/locale/es-ES.ts
@@ -0,0 +1,8 @@
+export default {
+  "Delay": "Retraso",
+  "Duration": "Duración",
+  "End Status": "Estado Final",
+  "Select status": "Seleccionar estado",
+  "Succeed and continue": "Éxito y continuar",
+  "Fail and exit": "Falla y sale",
+};
