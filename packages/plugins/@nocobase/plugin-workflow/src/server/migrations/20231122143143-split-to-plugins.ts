21a5ddcca400537c73802d1f3d3b70de067acd9a
mytharcher <mytharcher@gmail.com>
2023-12-02 15:41:43 +0800

    fix(plugin-workflow): fix migration and packages

--- packages/plugins/@nocobase/plugin-workflow/src/server/migrations/20231122143143-split-to-plugins.ts
+++ packages/plugins/@nocobase/plugin-workflow/src/server/migrations/20231122143143-split-to-plugins.ts
@@ -8,77 +8,109 @@ export default class extends Migration {
     }
     const { db } = this.context;
 
-    const PluginRepo = db.getRepository('applicationPlugins');
+    const PluginModel = db.getModel('applicationPlugins');
     const NodeRepo = db.getRepository('flow_nodes');
     await db.sequelize.transaction(async (transaction) => {
-      await PluginRepo.createMany({
-        records: [
-          {
-            name: '@nocobase/plugin-workflow-dynamic-calculation',
-            packageName: '@nocobase/plugin-workflow-dynamic-calculation',
-            version: '0.16.0-alpha.3',
-            enabled: true,
-            installed: true,
-          },
-          {
-            name: '@nocobase/plugin-workflow-manual',
-            packageName: '@nocobase/plugin-workflow-manual',
-            version: '0.16.0-alpha.3',
-            enabled: true,
-            installed: true,
-          },
-          {
-            name: '@nocobase/plugin-workflow-loop',
-            packageName: '@nocobase/plugin-workflow-loop',
-            version: '0.16.0-alpha.3',
-            enabled: true,
-            installed: true,
-          },
-          {
-            name: '@nocobase/plugin-workflow-parallel',
-            packageName: '@nocobase/plugin-workflow-parallel',
-            version: '0.16.0-alpha.3',
-            enabled: true,
-            installed: true,
-          },
-          {
-            name: '@nocobase/plugin-workflow-delay',
-            packageName: '@nocobase/plugin-workflow-delay',
-            version: '0.16.0-alpha.3',
-            enabled: true,
-            installed: true,
-          },
-          {
-            name: '@nocobase/plugin-workflow-aggregate',
-            packageName: '@nocobase/plugin-workflow-aggregate',
-            version: '0.16.0-alpha.3',
-            enabled: true,
-            installed: true,
-          },
-          {
-            name: '@nocobase/plugin-workflow-sql',
-            packageName: '@nocobase/plugin-workflow-sql',
-            version: '0.16.0-alpha.3',
-            enabled: true,
-            installed: true,
-          },
-          {
-            name: '@nocobase/plugin-workflow-request',
-            packageName: '@nocobase/plugin-workflow-request',
-            version: '0.16.0-alpha.3',
-            enabled: true,
-            installed: true,
-          },
-          {
-            name: '@nocobase/plugin-workflow-form-trigger',
-            packageName: '@nocobase/plugin-workflow-form-trigger',
-            version: '0.16.0-alpha.3',
-            enabled: true,
-            installed: true,
-          },
-        ],
-        transaction,
-      });
+      await PluginModel.upsert(
+        {
+          name: '@nocobase/plugin-workflow-dynamic-calculation',
+          packageName: '@nocobase/plugin-workflow-dynamic-calculation',
+          version: '0.16.0-alpha.5',
+          enabled: true,
+          installed: true,
+          builtin: true,
+        },
+        { transaction },
+      );
+      await PluginModel.upsert(
+        {
+          name: '@nocobase/plugin-workflow-manual',
+          packageName: '@nocobase/plugin-workflow-manual',
+          version: '0.16.0-alpha.5',
+          enabled: true,
+          installed: true,
+          builtin: true,
+        },
+        { transaction },
+      );
+      await PluginModel.upsert(
+        {
+          name: '@nocobase/plugin-workflow-loop',
+          packageName: '@nocobase/plugin-workflow-loop',
+          version: '0.16.0-alpha.5',
+          enabled: true,
+          installed: true,
+          builtin: true,
+        },
+        { transaction },
+      );
+      await PluginModel.upsert(
+        {
+          name: '@nocobase/plugin-workflow-parallel',
+          packageName: '@nocobase/plugin-workflow-parallel',
+          version: '0.16.0-alpha.5',
+          enabled: true,
+          installed: true,
+          builtin: true,
+        },
+        { transaction },
+      );
+      await PluginModel.upsert(
+        {
+          name: '@nocobase/plugin-workflow-delay',
+          packageName: '@nocobase/plugin-workflow-delay',
+          version: '0.16.0-alpha.5',
+          enabled: true,
+          installed: true,
+          builtin: true,
+        },
+        { transaction },
+      );
+      await PluginModel.upsert(
+        {
+          name: '@nocobase/plugin-workflow-aggregate',
+          packageName: '@nocobase/plugin-workflow-aggregate',
+          version: '0.16.0-alpha.5',
+          enabled: true,
+          installed: true,
+          builtin: true,
+        },
+        { transaction },
+      );
+      await PluginModel.upsert(
+        {
+          name: '@nocobase/plugin-workflow-sql',
+          packageName: '@nocobase/plugin-workflow-sql',
+          version: '0.16.0-alpha.5',
+          enabled: true,
+          installed: true,
+          builtin: true,
+        },
+        { transaction },
+      );
+      await PluginModel.upsert(
+        {
+          name: '@nocobase/plugin-workflow-request',
+          packageName: '@nocobase/plugin-workflow-request',
+          version: '0.16.0-alpha.5',
+          enabled: true,
+          installed: true,
+          builtin: true,
+        },
+        { transaction },
+      );
+      await PluginModel.upsert(
+        {
+          name: '@nocobase/plugin-workflow-form-trigger',
+          packageName: '@nocobase/plugin-workflow-form-trigger',
+          version: '0.16.0-alpha.5',
+          enabled: true,
+          installed: true,
+          builtin: true,
+        },
+        { transaction },
+      );
+
       const nodes = await NodeRepo.find({
         transaction,
       });
