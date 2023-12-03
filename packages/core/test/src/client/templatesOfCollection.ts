3d3a06053be74212810b97cd1aa3b9e0b4bea8f1
Zeke Zhang <958414905@qq.com>
2023-12-03 16:31:38 +0800

    test: add tests for lazy loading of variables

--- packages/core/test/src/client/templatesOfCollection.ts
+++ packages/core/test/src/client/templatesOfCollection.ts
@@ -610,3 +610,43 @@ export const tree: CollectionSetting[] = [
     template: 'tree',
   },
 ];
+
+export const generalWithMultiLevelRelationshipFields: CollectionSetting[] = [
+  {
+    name: 'general',
+    title: 'General',
+    fields: [
+      {
+        name: 'm2oField0',
+        interface: 'm2o',
+        target: 'm2oField1',
+      },
+      {
+        name: 'singleLineText',
+        interface: 'input',
+      },
+    ],
+  },
+  {
+    name: 'm2oField1',
+    title: 'M2o field 1',
+    fields: [
+      {
+        name: 'm2oField1',
+        interface: 'm2o',
+        target: 'm2oField2',
+      },
+    ],
+  },
+  {
+    name: 'm2oField2',
+    title: 'M2o field 2',
+    fields: [
+      {
+        name: 'm2oField2',
+        interface: 'm2o',
+        target: 'm2oField3',
+      },
+    ],
+  },
+];
