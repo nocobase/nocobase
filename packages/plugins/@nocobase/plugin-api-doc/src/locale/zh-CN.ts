ff097936dded933301e3359188c8e2c5c8f94777
YANG QIA <2013xile@gmail.com>
2023-12-06 14:30:17 +0800

    fix(lm): reserve i18n namespaces for plugins to avoid conflicts (#3121)
    * fix: fix T-2063
    
    * fix: fix T-2589
    
    * chore: remove translation of page header
    
    * chore: add version check for migration
    
    * chore: switch locale files to JSON format
    
    * fix: bug
    
    * fix(auth): fix translation
    
    * fix: add App provider
    
    ---------
    
    Co-authored-by: chenos <chenlinxh@gmail.com>

--- packages/plugins/@nocobase/plugin-api-doc/src/locale/zh-CN.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  'API documentation': 'API 文档',
-  'Documentation': '文档',
-  'Select a definition': '选择端点',
-};
--- packages/plugins/@nocobase/plugin-error-handler/src/locale/fr_FR.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  'unique violation': '{{field}} doit être unique',
-  'notNull violation': 'Violation de contrainte notNull',
-  'Validation error': 'Erreur de validation de {{field}}',
-  'notNull Violation': '{{field}} ne peut pas être null',
-};
