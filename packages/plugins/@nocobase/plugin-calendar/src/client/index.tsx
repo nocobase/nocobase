316e161c227be90deb6205dc309ee7f54adb7b29
Dunqing <dengqing0821@gmail.com>
2023-12-06 18:55:49 +0800

    fix: missing CalendarV2

--- packages/plugins/@nocobase/plugin-calendar/src/client/index.tsx
+++ packages/plugins/@nocobase/plugin-calendar/src/client/index.tsx
@@ -1,25 +1,25 @@
 import { Plugin, SchemaInitializerContext, registerTemplate } from '@nocobase/client';
 import { CalendarBlockProvider, useCalendarBlockProps } from './schema-initializer/CalendarBlockProvider';
-import React, { useContext, useEffect } from 'react';
 import { CalendarActionInitializers, CalendarFormActionInitializers } from './schema-initializer/initializers';
 import { CalendarBlockInitializer, RecordAssociationCalendarBlockInitializer } from './schema-initializer/items';
 import { generateNTemplate } from '../locale';
 import { calendar } from './collection-templates/calendar';
+import { CalendarV2 } from './calendar';
 
 export class CalendarPlugin extends Plugin {
   async load() {
-    this.app.addComponents({
-      CalendarBlockProvider,
-      CalendarBlockInitializer,
-      RecordAssociationCalendarBlockInitializer,
-    });
-
     const blockInitializers = this.app.schemaInitializerManager.get('BlockInitializers');
     blockInitializers?.add('dataBlocks.calendar', {
       title: generateNTemplate('Calendar'),
       Component: 'CalendarBlockInitializer',
     });
 
+    this.app.addComponents({
+      CalendarBlockProvider,
+      CalendarBlockInitializer,
+      RecordAssociationCalendarBlockInitializer,
+      CalendarV2,
+    });
     this.app.addScopes({ useCalendarBlockProps });
     this.app.schemaInitializerManager.add(CalendarActionInitializers);
     this.app.schemaInitializerManager.add(CalendarFormActionInitializers);
