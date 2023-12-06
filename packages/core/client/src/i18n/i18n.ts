795704b6a133efbbb39ba5555c77d141a233fef2
chenos <chenlinxh@gmail.com>
2023-12-06 12:18:56 +0800

    fix: default action schema settings

--- packages/core/client/src/i18n/i18n.ts
+++ packages/core/client/src/i18n/i18n.ts
@@ -1,6 +1,13 @@
-import i18next from 'i18next';
+import i18next, { TFuncKey, TOptions } from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import locale from '../locale';
+
+export function tval(text: TFuncKey | TFuncKey[], options?: TOptions) {
+  if (options) {
+    return `{{t(${JSON.stringify(text)}, ${JSON.stringify(options)})}}`;
+  }
+  return `{{t(${JSON.stringify(text)})}}`;
+}
 
 export const i18n = i18next.createInstance();
 
