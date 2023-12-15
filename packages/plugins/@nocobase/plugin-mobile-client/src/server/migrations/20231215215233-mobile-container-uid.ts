ea77340893727674d13242e6c5c7115cfea25182
chenos <chenlinxh@gmail.com>
2023-12-15 22:48:24 +0800

    feat(ui-schema): nocobase-admin-menu & nocobase-mobile-container

---
+++ packages/plugins/@nocobase/plugin-mobile-client/src/server/migrations/20231215215233-mobile-container-uid.ts
@@ -0,0 +1,27 @@
+import { Model } from '@nocobase/database';
+import { Migration } from '@nocobase/server';
+
+export default class extends Migration {
+  async up() {
+    const result = await this.app.version.satisfies('<0.17.0-alpha.8');
+    if (!result) {
+      return;
+    }
+    const systemSettings = this.db.getRepository('systemSettings');
+    const instance: Model = await systemSettings.findOne();
+    if (!instance?.options?.mobileSchemaUid) {
+      return;
+    }
+    const UiSchemas = this.db.getModel('uiSchemas');
+    await UiSchemas.update(
+      {
+        'x-uid': 'nocobase-mobile-container',
+      },
+      {
+        where: {
+          'x-uid': instance?.options?.mobileSchemaUid,
+        },
+      },
+    );
+  }
+}
