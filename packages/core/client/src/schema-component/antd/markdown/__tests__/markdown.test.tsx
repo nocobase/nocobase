23925efa8a99a1cdeb944d4bdfd90b22c20baaf0
Zeke Zhang <958414905@qq.com>
2023-12-15 22:22:00 +0800

    fix: fix unit tests

--- packages/core/client/src/schema-component/antd/markdown/__tests__/markdown.test.tsx
+++ packages/core/client/src/schema-component/antd/markdown/__tests__/markdown.test.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { act, fireEvent, render } from 'testUtils';
-import { GlobalThemeProvider } from '../../../../global-theme';
 import App1 from '../demos/demo1';
 import App2 from '../demos/demo2';
 
 describe('Markdown', () => {
   it('should display the value of user input', () => {
-    const { container } = render(<App1 />, {
-      wrapper: GlobalThemeProvider,
-    });
-    const textarea = container.querySelector('.ant-input') as HTMLTextAreaElement;
+    const { container } = render(<App1 />);
+    const textarea = container.querySelector<HTMLTextAreaElement>('.ant-input');
     act(() => {
       fireEvent.change(textarea, { target: { value: '## Hello World' } });
     });
@@ -19,10 +16,8 @@ describe('Markdown', () => {
 
 describe('Markdown.Void', () => {
   it('should display the value of user input', async () => {
-    const { container } = render(<App2 />, {
-      wrapper: GlobalThemeProvider,
-    });
-    const button = container.querySelector('.ant-btn') as HTMLButtonElement;
+    const { container } = render(<App2 />);
+    const button = container.querySelector('.ant-btn');
 
     expect(button).not.toBeNull();
     expect(container.querySelector('.ant-input')).toBeNull();
