7a5094f2ecb6b28b084ccda92a664a1c77c87fff
Chareice <chareice@live.com>
2023-12-19 11:26:50 +0800

    chore: remove field from db

--- packages/core/database/src/collection-factory.ts
+++ packages/core/database/src/collection-factory.ts
@@ -4,7 +4,8 @@ import { Model } from './model';
 
 type CollectionTypeOptions = {
   condition: (options: CollectionOptions) => boolean;
-  onSync?: (model: typeof Model, options: any) => Promise<void>;
+  onSync?: (model: Model, options: any) => Promise<void>;
+  onDestroyField?: (model: Model, options: any) => Promise<void>;
 };
 
 export class CollectionFactory {
