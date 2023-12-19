5f8b98784ad3064f130917e0457e65163e42ccf5
katherinehhh <katherine_15995@163.com>
2023-12-19 10:48:36 +0800

    test: acl test

--- packages/plugins/@nocobase/plugin-acl/src/client/__e2e__/configure-acl.test.ts
+++ packages/plugins/@nocobase/plugin-acl/src/client/__e2e__/configure-acl.test.ts
@@ -1,31 +1,94 @@
 import { expect, test } from '@nocobase/test/client';
+import { oneEmptyTableBlockWithActions } from './utils';
 
-test.describe('allows to configure interface', () => {
-  test('view ', async ({ page, mockPage }) => {
-    await mockPage().goto();
+test.afterEach(async ({ page }) => {
+  await page.evaluate(() => {
+    window.localStorage.setItem('NOCOBASE_ROLE', 'root');
   });
 });
-
-test.describe('allows to install ,install,disabled plugins', () => {
-  test('view ', async ({ page, mockPage }) => {
-    await mockPage().goto();
+test('allows to configure interface', async ({ page, mockPage, mockRole }) => {
+  await mockPage().goto();
+  //新建角色并切换到新角色
+  const roleData = await mockRole({
+    snippets: ['ui.*'],
+    default: true,
+    allowNewMenu: true,
   });
+  await page.evaluate((roleData) => {
+    window.localStorage.setItem('NOCOBASE_ROLE', roleData.name);
+  }, roleData);
+  await page.reload();
+  await page.getByTestId('user-center-button').hover();
+  await expect(await page.getByTestId('ui-editor-button')).toBeVisible();
 });
 
-test.describe('allows to confgiure plugins', () => {
-  test('view ', async ({ page, mockPage }) => {
-    await mockPage().goto();
+test('allows to install ,install,disabled plugins ', async ({ page, mockPage, mockRole }) => {
+  await mockPage().goto();
+  //新建角色并切换到新角色
+  const roleData = await mockRole({
+    snippets: ['pm'],
+    default: true,
+    allowNewMenu: true,
   });
+  await page.evaluate((roleData) => {
+    window.localStorage.setItem('NOCOBASE_ROLE', roleData.name);
+  }, roleData);
+  await page.reload();
+  await page.getByTestId('user-center-button').hover();
+  await expect(await page.getByTestId('plugin-manager-button')).toBeVisible();
 });
 
-test.describe('allows to clear cache,reboot application', () => {
-  test('view ', async ({ page, mockPage }) => {
-    await mockPage().goto();
+test('allows to confgiure plugins ', async ({ page, mockPage, mockRole }) => {
+  await mockPage().goto();
+  //新建角色并切换到新角色
+  const roleData = await mockRole({
+    snippets: ['pm.*'],
+    strategy: {
+      actions: ['update', 'destroy', 'view'],
+    },
+    default: true,
+    allowNewMenu: true,
   });
+  await page.evaluate((roleData) => {
+    window.localStorage.setItem('NOCOBASE_ROLE', roleData.name);
+  }, roleData);
+  await page.reload();
+  await page.getByTestId('user-center-button').hover();
+  await page.getByTestId('plugin-settings-button').click();
+  await page.getByLabel('acl').click();
+  await page.getByLabel(`action-Action.Link-Configure-roles-${roleData.name}`).click();
+  await expect(page.getByRole('tab').getByText('Plugin settings permissions')).toBeVisible();
 });
 
-test.describe('new menu items allow to be asscessed by default', () => {
-  test('view ', async ({ page, mockPage }) => {
-    await mockPage().goto();
+test('allows to clear cache,reboot application ', async ({ page, mockPage, mockRole }) => {
+  await mockPage().goto();
+  //新建角色并切换到新角色
+  const roleData = await mockRole({
+    snippets: ['app'],
+    default: true,
+    allowNewMenu: true,
   });
+  await page.evaluate((roleData) => {
+    window.localStorage.setItem('NOCOBASE_ROLE', roleData.name);
+  }, roleData);
+  await page.reload();
+  await page.getByTestId('user-center-button').hover();
+  await expect(await page.getByRole('menuitem', { name: 'Clear cache' })).toBeVisible();
+  await expect(await page.getByRole('menuitem', { name: 'Restart application' })).toBeVisible();
+});
+
+test('new menu items allow to be asscessed by default ', async ({ page, mockPage, mockRole }) => {
+  await mockPage().goto();
+  //新建角色并切换到新角色
+  const roleData = await mockRole({
+    snippets: ['ui.*'],
+    default: true,
+    allowNewMenu: true,
+  });
+  await page.evaluate((roleData) => {
+    window.localStorage.setItem('NOCOBASE_ROLE', roleData.name);
+  }, roleData);
+  await page.reload();
+  await mockPage({ ...oneEmptyTableBlockWithActions, name: 'new page' }).goto();
+  await expect(page.getByLabel('new page')).toBeVisible();
 });
