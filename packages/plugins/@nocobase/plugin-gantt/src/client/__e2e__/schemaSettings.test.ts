6c8e7318b3c18595897decb2167d0808533a0c4a
katherinehhh <katherine_15995@163.com>
2023-12-19 12:00:47 +0800

    test: kanban test

--- packages/plugins/@nocobase/plugin-gantt/src/client/__e2e__/schemaSettings.test.ts
+++ packages/plugins/@nocobase/plugin-gantt/src/client/__e2e__/schemaSettings.test.ts
@@ -26,7 +26,7 @@ test.describe('configure params in gantt block', () => {
     await page.getByRole('spinbutton').fill('1');
     const [request] = await Promise.all([
       page.waitForRequest((request) => request.url().includes('api/general:list')),
-      page.getByRole('button', { name: 'OK' }).click(),
+      page.getByRole('button', { name: 'OK', exact: true }).click(),
     ]);
     const requestUrl = request.url();
     const queryParams = new URLSearchParams(new URL(requestUrl).search);
