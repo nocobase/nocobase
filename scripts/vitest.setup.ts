d94fdca8b0881431ba66310d2fd67393af1e44ab
chenos <chenlinxh@gmail.com>
2023-12-20 09:59:34 +0800

    fix: types error

--- scripts/vitest.setup.ts
+++ scripts/vitest.setup.ts
@@ -2,7 +2,22 @@ import { configure } from '@testing-library/react';
 import dotenv from 'dotenv';
 import { vi } from 'vitest';
 import 'vitest-dom/extend-expect';
-import 'vitest/globals';
+declare global {
+  const suite: (typeof import('vitest'))['suite'];
+  const test: (typeof import('vitest'))['test'];
+  const describe: (typeof import('vitest'))['describe'];
+  const it: (typeof import('vitest'))['it'];
+  const expectTypeOf: (typeof import('vitest'))['expectTypeOf'];
+  const assertType: (typeof import('vitest'))['assertType'];
+  const expect: (typeof import('vitest'))['expect'];
+  const assert: (typeof import('vitest'))['assert'];
+  const vitest: (typeof import('vitest'))['vitest'];
+  const vi: (typeof import('vitest'))['vitest'];
+  const beforeAll: (typeof import('vitest'))['beforeAll'];
+  const afterAll: (typeof import('vitest'))['afterAll'];
+  const beforeEach: (typeof import('vitest'))['beforeEach'];
+  const afterEach: (typeof import('vitest'))['afterEach'];
+}
 
 /**
  * 解决 TypeError: URL.createObjectURL is not a function
