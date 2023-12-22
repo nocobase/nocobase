be6f124a455383f37091e73371c576b1e738c610
katherinehhh <katherine_15995@163.com>
2023-12-22 09:48:08 +0800

    test: acl test

--- packages/core/test/src/e2e/e2eUtils.ts
+++ packages/core/test/src/e2e/e2eUtils.ts
@@ -430,7 +430,7 @@ const _test = base.extend<ExtendUtils>({
     await use(mockRole);
   },
   updateRole: async ({ page }, use) => {
-    const mockRole = async (roleSetting: AclRoleSetting) => {
+    const updateRole = async (roleSetting: AclRoleSetting) => {
       return updateRole(roleSetting);
     };
 
