4b580e4f0899d0635767b7b33ccfba4b0d6c4346
Junyi <mytharcher@users.noreply.github.com>
2023-12-24 17:14:38 +0800

    feat(plugin-workflow): add space control to RadioWithTooltip (#3252)

--- packages/plugins/@nocobase/plugin-workflow/src/client/components/RadioWithTooltip.tsx
+++ packages/plugins/@nocobase/plugin-workflow/src/client/components/RadioWithTooltip.tsx
@@ -1,6 +1,6 @@
 import { QuestionCircleOutlined } from '@ant-design/icons';
 import { css, useCompile } from '@nocobase/client';
-import { Radio, Tooltip } from 'antd';
+import { Radio, Space, Tooltip } from 'antd';
 import React from 'react';
 
 export interface RadioWithTooltipOption {
@@ -10,29 +10,31 @@ export interface RadioWithTooltipOption {
 }
 
 export function RadioWithTooltip(props) {
-  const { options = [], ...other } = props;
+  const { options = [], direction, ...other } = props;
   const compile = useCompile();
 
   return (
     <Radio.Group {...other}>
-      {options.map((option) => (
-        <Radio key={option.value} value={option.value}>
-          <span
-            className={css`
-              & + .anticon {
-                margin-left: 0.25em;
-              }
-            `}
-          >
-            {compile(option.label)}
-          </span>
-          {option.tooltip && (
-            <Tooltip title={compile(option.tooltip)}>
-              <QuestionCircleOutlined style={{ color: '#666' }} />
-            </Tooltip>
-          )}
-        </Radio>
-      ))}
+      <Space direction={direction}>
+        {options.map((option) => (
+          <Radio key={option.value} value={option.value}>
+            <span
+              className={css`
+                & + .anticon {
+                  margin-left: 0.25em;
+                }
+              `}
+            >
+              {compile(option.label)}
+            </span>
+            {option.tooltip && (
+              <Tooltip title={compile(option.tooltip)}>
+                <QuestionCircleOutlined style={{ color: '#666' }} />
+              </Tooltip>
+            )}
+          </Radio>
+        ))}
+      </Space>
     </Radio.Group>
   );
 }
