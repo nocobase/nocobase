7c674cc1c2dfb0782ed1fc9d406a6e62282dfece
katherinehhh <katherine_15995@163.com>
2023-12-27 11:07:35 +0800

    feat: support databse connection

--- packages/core/client/src/acl/Configuration/index.tsx
+++ packages/core/client/src/acl/Configuration/index.tsx
@@ -5,3 +5,4 @@ export { RoleTable } from './RoleTable';
 export { StrategyActions } from './StrategyActions';
 export { SettingsCenterConfigure, SettingCenterProvider } from './ConfigureCenter';
 export * from './PermisionProvider';
+export * from './RolesResourcesActions';
