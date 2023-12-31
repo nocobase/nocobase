3c3896b18a692641fef16a2ccbd66222a7a62c7d
Zeke Zhang <958414905@qq.com>
2024-01-01 01:36:31 +0800

    chore: test

--- packages/core/test/src/e2e/defineConfig.ts
+++ packages/core/test/src/e2e/defineConfig.ts
@@ -2,10 +2,10 @@ import { devices, defineConfig as playwrightDefineConfig, type PlaywrightTestCon
 
 export const defineConfig = (config?: PlaywrightTestConfig) => {
   return playwrightDefineConfig({
-    timeout: process.env.CI ? 3 * 60 * 1000 : 30 * 1000,
+    timeout: process.env.CI ? 200 * 60 * 1000 : 30 * 1000,
 
     expect: {
-      timeout: process.env.CI ? 30 * 1000 : 5000,
+      timeout: process.env.CI ? 200 * 60 * 1000 : 5000,
     },
 
     // Look for test files in the "tests" directory, relative to this configuration file.
