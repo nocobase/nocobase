5f5d9351ddf7c6763b940c6577b219cc114ca85e
Zeke Zhang <958414905@qq.com>
2024-01-01 23:54:54 +0800

    chore: change concurrency to 4 from 2

--- packages/core/cli/src/commands/e2e.js
+++ packages/core/cli/src/commands/e2e.js
@@ -205,7 +205,7 @@ module.exports = (cli) => {
   e2e
     .command('p-test')
     .option('--stop-on-error')
-    .option('--concurrency [concurrency]', '', 2)
+    .option('--concurrency [concurrency]', '', 4)
     .action(async (opts) => {
       await pTest(opts);
     });
