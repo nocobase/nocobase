2cf80917fdc6cba8cf45a12fcb14765ac4f4e938
Zeke Zhang <958414905@qq.com>
2024-01-04 06:55:10 +0800

    chore: update config

--- packages/core/test/src/e2e/defineConfig.ts
+++ packages/core/test/src/e2e/defineConfig.ts
@@ -2,10 +2,10 @@ import { devices, defineConfig as playwrightDefineConfig, type PlaywrightTestCon
 
 export const defineConfig = (config?: PlaywrightTestConfig) => {
   return playwrightDefineConfig({
-    timeout: process.env.CI ? 2 * 60 * 1000 : 30 * 1000,
+    timeout: process.env.CI ? 5 * 60 * 1000 : 30 * 1000,
 
     expect: {
-      timeout: process.env.CI ? 20 * 1000 : 5000,
+      timeout: process.env.CI ? 1 * 60 * 1000 : 5000,
     },
 
     // Look for test files in the "tests" directory, relative to this configuration file.
@@ -15,7 +15,7 @@ export const defineConfig = (config?: PlaywrightTestConfig) => {
     testMatch: /(.*\/e2e\/|.*\/__e2e__\/).+\.test\.[tj]sx*$/,
 
     // Run all tests in parallel.
-    fullyParallel: true,
+    fullyParallel: false,
 
     // Fail the build on CI if you accidentally left test.only in the source code.
     forbidOnly: !!process.env.CI,
