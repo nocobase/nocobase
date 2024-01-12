c4257f1e2374f547b70ef835c4f58da63b1974b9
dream2023 <1098626505@qq.com>
2024-01-12 10:17:38 +0800

    fix: build error

--- packages/core/client/src/application/collection/CollectionFieldInterface.tsx
+++ packages/core/client/src/application/collection/CollectionFieldInterface.tsx
@@ -42,12 +42,47 @@ export interface CollectionFieldInterfaceV2Options extends IField {
 }
 
 export class CollectionFieldInterfaceV2 implements CollectionFieldInterfaceV2Options {
-  name: string;
-  group: string;
   app: Application;
   collectionManager: CollectionManagerV2;
   constructor(app: Application) {
     this.app = app;
     this.collectionManager = app.collectionManager;
   }
+  name: string;
+  group: string;
+  title?: string;
+  description?: string;
+  order?: number;
+  default?: {
+    type: string;
+    uiSchema?: ISchema;
+    [key: string]: any;
+  };
+  sortable?: boolean;
+  availableTypes?: string[];
+  hasDefaultValue?: boolean;
+  isAssociation?: boolean;
+  validateSchema(fieldSchema: ISchema): Record<string, ISchema> {
+    return null as any;
+  }
+  usePathOptions?: (field: CollectionFieldOptionsV2) => any;
+  operators?: any[];
+  /**
+   * - 如果该值为空，则在 Filter 组件中该字段会被过滤掉
+   * - 如果该值为空，则不会在变量列表中看到该字段
+   */
+  filterable?: {
+    /**
+     * 字段所支持的操作符，会在 Filter 组件中显示，比如设置 `数据范围` 的时候可以看见
+     */
+    operators?: any[];
+    /**
+     * 为当前字段添加子选项，这个子选项会在 Filter 组件中显示，比如设置 `数据范围` 的时候可以看见
+     */
+    children?: any[];
+    [key: string]: any;
+  };
+  // NOTE: set to `true` means field could be used as a title field
+  titleUsable?: boolean;
+  schemaInitialize(schema: ISchema, data: any): void {}
 }
