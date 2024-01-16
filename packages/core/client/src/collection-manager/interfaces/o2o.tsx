c2c65f15a167c2200dae128029e4cfa80cd95194
katherinehhh <katherine_15995@163.com>
2024-01-16 15:57:43 +0800

    refactor: o2o sourceKey should be string

--- packages/core/client/src/collection-manager/interfaces/o2o.tsx
+++ packages/core/client/src/collection-manager/interfaces/o2o.tsx
@@ -120,7 +120,7 @@ export const o2o: IField = {
               'x-component': 'Grid.Col',
               properties: {
                 sourceKey: {
-                  type: 'void',
+                  type: 'string',
                   title: '{{t("Source key")}}',
                   'x-decorator': 'FormItem',
                   'x-component': 'SourceKey',
@@ -298,7 +298,7 @@ export const oho: IField = {
               'x-component': 'Grid.Col',
               properties: {
                 sourceKey: {
-                  type: 'void',
+                  type: 'string',
                   title: '{{t("Source key")}}',
                   'x-decorator': 'FormItem',
                   'x-component': 'SourceKey',
