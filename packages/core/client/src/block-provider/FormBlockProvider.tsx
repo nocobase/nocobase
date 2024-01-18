f81bb1fa33b13ffd2298c4a9fadc7f37b29036bf
katherinehhh <shunai.tang@hand-china.com>
2024-01-18 09:46:27 +0800

    refactor: parent inherited collection support enable child collection in add new action (#3398)
    * refactor: parent inherited collectionsholud enable child collection in add new
    
    * fix: useisDetailBlock
    
    * fix: useIsDetailBlock
    
    * fix: useIsDetailBlock
    
    * fix: useIsDetailBlock

--- packages/core/client/src/block-provider/FormBlockProvider.tsx
+++ packages/core/client/src/block-provider/FormBlockProvider.tsx
@@ -86,7 +86,8 @@ export const useFormBlockType = () => {
 
 export const useIsDetailBlock = () => {
   const ctx = useFormBlockContext();
-  return ctx.type !== 'create';
+  const { fieldSchema } = useActionContext();
+  return ctx.type !== 'create' && fieldSchema?.['x-acl-action'] !== 'create' && fieldSchema?.['x-action'] !== 'create';
 };
 
 export const FormBlockProvider = (props) => {
