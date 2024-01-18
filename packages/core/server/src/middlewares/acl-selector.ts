d98a42e34f942c0475a485fa5f14faac2a659514
Chareice <chareice@live.com>
2024-01-18 13:48:28 +0800

    chore: acl

--- packages/core/server/src/middlewares/acl-selector.ts
+++ packages/core/server/src/middlewares/acl-selector.ts
@@ -1,9 +1,13 @@
 export function AclSelectorMiddleware() {
   return async (ctx, next) => {
     const connectionName = ctx.get('x-connection');
-    const aclInstance = connectionName ? ctx.app.acls.get(connectionName) : ctx.app.acl;
+    if (connectionName) {
+      await next();
+    } else {
+      const aclInstance = connectionName ? ctx.app.acls.get(connectionName) : ctx.app.acl;
 
-    const middleware = aclInstance.middleware();
-    await middleware(ctx, next);
+      const middleware = aclInstance.middleware();
+      await middleware(ctx, next);
+    }
   };
 }
