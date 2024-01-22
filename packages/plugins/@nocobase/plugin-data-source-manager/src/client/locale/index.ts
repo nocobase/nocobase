c0807c17f363f883c0ad7716f37c83468eb3df53
katherinehhh <katherine_15995@163.com>
2024-01-22 22:06:41 +0800

    refactor: local improve

--- packages/plugins/@nocobase/plugin-data-source-manager/src/client/locale/index.ts
+++ packages/plugins/@nocobase/plugin-data-source-manager/src/client/locale/index.ts
@@ -1,6 +1,6 @@
 import { i18n } from '@nocobase/client';
 
-export const NAMESPACE = 'database-connections';
+export const NAMESPACE = 'data-source-manager';
 
 export function lang(key: string, options = {}) {
   return i18n.t(key, { ...options, ns: NAMESPACE });
