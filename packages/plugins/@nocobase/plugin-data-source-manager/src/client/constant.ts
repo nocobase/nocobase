a71f46dc4f9f80b74c0190f78e5676cb5b351d54
katherinehhh <katherine_15995@163.com>
2024-01-22 22:20:26 +0800

    refactor: code improve

--- packages/plugins/@nocobase/plugin-data-source-manager/src/client/constant.ts
+++ packages/plugins/@nocobase/plugin-data-source-manager/src/client/constant.ts
@@ -1,5 +1,2 @@
-export const getConnectionPath = (name: string | number) => `/admin/settings/data-source-manager/${name}`;
 export const getConnectionCollectionPath = (name: string | number) =>
   `/admin/settings/data-source-manager/${name}/collections`;
-export const getConnectionPermissionPath = (name: string | number) =>
-  `/admin/settings/data-source-manager/${name}/permissions`;
