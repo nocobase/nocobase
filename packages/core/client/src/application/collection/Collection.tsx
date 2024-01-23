b3ced4c7e1a8d36ee1304dac2d78c2372bbeb42f
dream2023 <1098626505@qq.com>
2024-01-23 11:09:29 +0800

    fix: block resource

--- packages/core/client/src/application/collection/Collection.tsx
+++ packages/core/client/src/application/collection/Collection.tsx
@@ -93,6 +93,10 @@ export class CollectionV2 {
     return this.options?.fields || [];
   }
 
+  get sourceKey() {
+    return this.options.sourceKey;
+  }
+
   get name() {
     return this.options.name;
   }
