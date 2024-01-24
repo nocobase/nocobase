5d070fea64500404e53b19c6e9837c7b0b3a1cb7
Chareice <chareice@live.com>
2024-01-24 20:07:55 +0800

    chore: test

--- packages/plugins/@nocobase/plugin-data-source-manager/src/server/__tests__/data-sources.test.ts
+++ packages/plugins/@nocobase/plugin-data-source-manager/src/server/__tests__/data-sources.test.ts
@@ -1,4 +1,5 @@
 import { createMockServer, MockServer } from '@nocobase/test';
+import { DataSource } from '@nocobase/data-source-manager';
 
 describe('data source', async () => {
   let app: MockServer;
@@ -13,5 +14,33 @@ describe('data source', async () => {
     await app.destroy();
   });
 
-  it('should create data source', async () => {});
+  it('should create data source', async () => {
+    const loadFn = vi.fn();
+
+    class MockDataSource extends DataSource {
+      async load(): Promise<void> {
+        loadFn();
+      }
+
+      createCollectionManager(options?: any): any {
+        return undefined;
+      }
+    }
+
+    app.dataSourceManager.factory.register('mock', MockDataSource);
+
+    await app.db.getRepository('dataSources').create({
+      values: {
+        key: 'mockInstance1',
+        type: 'mock',
+        displayName: 'Mock',
+        options: {},
+      },
+    });
+
+    expect(loadFn).toBeCalledTimes(1);
+
+    const mockDataSource = app.dataSourceManager.dataSources.get('mockInstance1');
+    expect(mockDataSource).toBeInstanceOf(MockDataSource);
+  });
 });
