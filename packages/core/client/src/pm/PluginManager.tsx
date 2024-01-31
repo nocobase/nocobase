31243bb9b55f17f3c30eac5bdeb92c74c2ba216c
chenos <chenlinxh@gmail.com>
2024-01-31 11:19:53 +0800

    fix: styles

--- packages/core/client/src/pm/PluginManager.tsx
+++ packages/core/client/src/pm/PluginManager.tsx
@@ -1,7 +1,7 @@
 export * from './PluginManagerLink';
 import { PageHeader } from '@ant-design/pro-layout';
 import { useDebounce } from 'ahooks';
-import { Button, Divider, Input, Result, Space, Spin, Tabs, Row, Col, List } from 'antd';
+import { Button, Col, Divider, Input, List, Result, Row, Space, Spin, Tabs } from 'antd';
 import _ from 'lodash';
 import React, { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -147,68 +147,68 @@ const LocalPlugins = () => {
           // if (isRefresh) refresh();
         }}
       />
-      <Row style={{ width: '100%' }}>
-        <Col span={2}>
-          <h4 style={{ margin: '8px 16px 15px 16px', color: '#0073' }}>分类</h4>
-          <List
-            size="small"
-            dataSource={keyWordsfilterList}
-            split={false}
-            renderItem={(item) => (
-              <List.Item
-                style={{ padding: '3px 15px' }}
-                onClick={() => (item.key !== keyword ? setKeyword(item.key) : setKeyword(null))}
-              >
-                <a style={{ fontWeight: keyword === item.key ? 'bold' : 'normal' }}>
-                  {item.key?.charAt?.(0).toUpperCase() + item.key?.slice?.(1)}
+
+      <div style={{ width: '100%' }}>
+        <div
+          style={{ marginBottom: theme.marginLG }}
+          className={css`
+            justify-content: space-between;
+            display: flex;
+            align-items: center;
+          `}
+        >
+          <div style={{ marginLeft: 200 }}>
+            <Space size={theme.marginXXS} split={<Divider type="vertical" />}>
+              {filterList.map((item, index) => (
+                <a
+                  role="button"
+                  aria-label={item.type}
+                  onClick={() => setFilterIndex(index)}
+                  key={item.type}
+                  style={{ fontWeight: filterIndex === index ? 'bold' : 'normal' }}
+                >
+                  {t(item.type)}
+                  {filterIndex === index ? `(${pluginList?.length})` : null}
                 </a>
-              </List.Item>
-            )}
-          />
-        </Col>
-        <Col span={22}>
-          <div style={{ width: '100%' }}>
-            <div
-              style={{ marginBottom: theme.marginLG }}
-              className={css`
-                justify-content: space-between;
-                display: flex;
-                align-items: center;
-              `}
-            >
-              <div>
-                <Space size={theme.marginXXS} split={<Divider type="vertical" />}>
-                  {filterList.map((item, index) => (
-                    <a
-                      role="button"
-                      aria-label={item.type}
-                      onClick={() => setFilterIndex(index)}
-                      key={item.type}
-                      style={{ fontWeight: filterIndex === index ? 'bold' : 'normal' }}
-                    >
-                      {t(item.type)}
-                      {filterIndex === index ? `(${pluginList?.length})` : null}
-                    </a>
-                  ))}
-                  <Input
-                    allowClear
-                    placeholder={t('Search plugin')}
-                    onChange={(e) => handleSearch(e.currentTarget.value)}
-                  />
-                </Space>
-              </div>
-              <div>
-                <Space>
-                  <Button onClick={() => setShowAddForm(true)} type="primary">
-                    {t('Add new')}
-                  </Button>
-                </Space>
-              </div>
-            </div>
+              ))}
+              <Input
+                allowClear
+                placeholder={t('Search plugin')}
+                onChange={(e) => handleSearch(e.currentTarget.value)}
+              />
+            </Space>
+          </div>
+          <div>
+            <Space>
+              <Button onClick={() => setShowAddForm(true)} type="primary">
+                {t('Add new')}
+              </Button>
+            </Space>
+          </div>
+        </div>
+        <Row style={{ width: '100%' }}>
+          <Col flex="200px">
+            <List
+              size="small"
+              dataSource={keyWordsfilterList}
+              split={false}
+              renderItem={(item) => (
+                <List.Item
+                  style={{ padding: '3px 0' }}
+                  onClick={() => (item.key !== keyword ? setKeyword(item.key) : setKeyword(null))}
+                >
+                  <a style={{ fontWeight: keyword === item.key ? 'bold' : 'normal' }}>
+                    {item.key?.charAt?.(0).toUpperCase() + item.key?.slice?.(1)}
+                  </a>
+                </List.Item>
+              )}
+            />
+          </Col>
+          <Col flex="auto">
             <div
               className={css`
                 --grid-gutter: ${theme.margin}px;
-                --extensions-card-width: 320px;
+                --extensions-card-width: calc(20% - var(--grid-gutter) + var(--grid-gutter) / 5);
                 display: grid;
                 grid-column-gap: var(--grid-gutter);
                 grid-row-gap: var(--grid-gutter);
@@ -221,9 +221,9 @@ const LocalPlugins = () => {
                 <PluginCard key={item.name} data={item} />
               ))}
             </div>
-          </div>
-        </Col>
-      </Row>
+          </Col>
+        </Row>
+      </div>
     </>
   );
 };
