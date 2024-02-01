befde8a5eac87ff9aec539f5cd2504152088786b
dream2023 <1098626505@qq.com>
2024-02-01 20:05:25 +0800

    fix: unit test

--- packages/core/client/src/application/__tests__/Application.test.tsx
+++ packages/core/client/src/application/__tests__/Application.test.tsx
@@ -242,8 +242,7 @@ describe('Application', () => {
       expect(screen.getByText('AboutComponent')).toBeInTheDocument();
     });
 
-    // TODO: 会一直 loading，暂时不知道怎么解决，先跳过
-    it.skip('mount', async () => {
+    it('mount', async () => {
       const Hello = () => <div>Hello</div>;
       const app = new Application({
         router,
