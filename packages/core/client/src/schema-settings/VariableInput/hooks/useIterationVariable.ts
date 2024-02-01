61c2517bf5f2addf2373f02e393d5521f6bc95c7
Zeke Zhang <958414905@qq.com>
2024-02-01 09:05:20 +0800

    refactor: ensure compatibility with older versions

--- packages/core/client/src/schema-settings/VariableInput/hooks/useIterationVariable.ts
+++ packages/core/client/src/schema-settings/VariableInput/hooks/useIterationVariable.ts
@@ -5,6 +5,55 @@ import { useSubFormValue } from '../../../schema-component/antd/association-fiel
 import { useBaseVariable } from './useBaseVariable';
 
 /**
+ * @deprecated
+ * 该 hook 已废弃，请使用 `useCurrentObjectVariable` 代替
+ *
+ * 变量：`当前对象`
+ * @param param0
+ * @returns
+ */
+export const useIterationVariable = ({
+  currentCollection,
+  collectionField,
+  schema,
+  noDisabled,
+  targetFieldSchema,
+}: {
+  currentCollection: string;
+  collectionField: CollectionFieldOptions;
+  schema?: any;
+  noDisabled?: boolean;
+  /** 消费变量值的字段 */
+  targetFieldSchema?: Schema;
+}) => {
+  // const { getActiveFieldsName } = useFormActiveFields() || {};
+  const { t } = useTranslation();
+  const result = useBaseVariable({
+    collectionField,
+    uiSchema: schema,
+    targetFieldSchema,
+    maxDepth: 4,
+    name: '$iteration',
+    title: t('Current object'),
+    collectionName: currentCollection,
+    noDisabled,
+    returnFields: (fields, option) => {
+      // fix https://nocobase.height.app/T-2277
+      return fields;
+      // const activeFieldsName = getActiveFieldsName?.('nester') || [];
+
+      // return option.depth === 0
+      //   ? fields.filter((field) => {
+      //       return activeFieldsName?.includes(field.name);
+      //     })
+      //   : fields;
+    },
+  });
+
+  return result;
+};
+
+/**
  * 变量：`当前对象`
  * @param param0
  * @returns
