8809fab813a0912c5e8e78599aee8d15398f7f1c
chenos <chenlinxh@gmail.com>
2024-02-05 19:45:27 +0800

    fix: password cannot be empty

--- packages/core/database/src/__tests__/fields/password-field.test.ts
+++ packages/core/database/src/__tests__/fields/password-field.test.ts
@@ -19,13 +19,17 @@ describe('password field', () => {
       fields: [{ type: 'password', name: 'password' }],
     });
     await db.sync();
-    const user = await User.model.create<any>({
+    let user = await User.model.create<any>({
       password: '123456',
     });
     const pwd = User.getField<PasswordField>('password');
     expect(await pwd.verify('123456', user.password)).toBeTruthy();
     user.set('password', '654321');
     await user.save();
+    expect(await pwd.verify('654321', user.password)).toBeTruthy();
+    user.set('password', null);
+    await user.save();
+    user = await User.model.findOne();
     expect(await pwd.verify('654321', user.password)).toBeTruthy();
   });
 });
