386804f277fe7fbccb6eb838e3b4784b9b710c40
dream2023 <1098626505@qq.com>
2024-02-06 10:41:13 +0800

    fix: bug

--- packages/core/client/src/collection-manager/index.tsx
+++ packages/core/client/src/collection-manager/index.tsx
@@ -22,7 +22,7 @@ export * from './ResourceActionProvider';
 export { getConfigurableProperties } from './templates/properties';
 export * from './templates/types';
 export * from './types';
-export { CollectionHistoryProvider } from './CollectionHistoryProvider';
+export * from './CollectionHistoryProvider';
 export * from './interfaces/properties';
 export * from './collectionPlugin';
 export * from './mixins/InheritanceCollectionMixin';
