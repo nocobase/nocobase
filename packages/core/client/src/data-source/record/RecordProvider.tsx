f02f8dc84568b3aa91185a4d111837aafa430355
Zeke Zhang <958414905@qq.com>
2024-02-06 20:33:57 +0800

    chore: avoid error

--- packages/core/client/src/data-source/record/RecordProvider.tsx
+++ packages/core/client/src/data-source/record/RecordProvider.tsx
@@ -68,15 +68,15 @@ export function useRecordV2<DataType = {}, ParentDataType = {}>(
 
 export function useRecordDataV2<DataType = any>(showErrorWhenNotExists = true): DataType {
   const record = useRecordV2<DataType>(showErrorWhenNotExists);
-  return record.data;
+  return record?.data;
 }
 
 export function useParentRecordV2<ParentDataType>(showErrorWhenNotExists = true): RecordV2<ParentDataType> {
   const record = useRecordV2<any, ParentDataType>(showErrorWhenNotExists);
-  return record.parentRecord;
+  return record?.parentRecord;
 }
 
 export function useParentRecordDataV2<ParentDataType>(showErrorWhenNotExists = true): ParentDataType {
   const record = useRecordV2<any, ParentDataType>(showErrorWhenNotExists);
-  return record.parentRecord?.data;
+  return record?.parentRecord?.data;
 }
