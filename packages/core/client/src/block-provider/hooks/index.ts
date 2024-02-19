9cb9e8aa5276f92f240311330026c8a1f503c080
Zeke Zhang <958414905@qq.com>
2024-02-19 10:42:24 +0800

    fix(useAssociationFilterBlockProps): should add x-data-source in headers (T-3062)

--- packages/core/client/src/block-provider/hooks/index.ts
+++ packages/core/client/src/block-provider/hooks/index.ts
@@ -1089,6 +1089,8 @@ export const useAssociationFilterBlockProps = () => {
   const { getDataBlocks } = useFilterBlock();
   const collectionFieldName = collectionField?.name;
   const field = useField();
+  const { props: blockProps } = useBlockRequestContext();
+  const headers = useDataSourceHeaders(blockProps?.dataSource);
 
   let list, handleSearchInput, params, run, data, valueKey, labelKey, filterKey;
 
@@ -1100,6 +1102,7 @@ export const useAssociationFilterBlockProps = () => {
     data: { [key: string]: any }[];
   }>(
     {
+      headers,
       resource: collectionField?.target,
       action: 'list',
       params: {
