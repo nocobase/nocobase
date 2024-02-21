77cd58d73d452ebc31828ffe98b8f1f062be6f1d
katherinehhh <katherine_15995@163.com>
2024-02-21 11:24:17 +0800

    refactor: sort field interface

--- packages/core/client/src/collection-manager/interfaces/sort.ts
+++ packages/core/client/src/collection-manager/interfaces/sort.ts
@@ -1,6 +1,6 @@
 import { defaultProps, operators, primaryKey, unique, autoIncrement } from './properties';
 import { CollectionFieldInterface } from '../../data-source/collection-field-interface/CollectionFieldInterface';
-
+import { i18n } from '../../i18n';
 export class SortFieldInterface extends CollectionFieldInterface {
   name = 'sort';
   type = 'object';
@@ -31,5 +31,73 @@ export class SortFieldInterface extends CollectionFieldInterface {
   };
   filterable = {
     operators: operators.number,
+  };
+  validateSchema = (fieldSchema) => {
+    return {
+      maximum: {
+        type: 'number',
+        title: '{{ t("Maximum") }}',
+        'x-decorator': 'FormItem',
+        'x-component': 'InputNumber',
+        'x-component-props': {
+          precision: 0,
+        },
+        'x-reactions': `{{(field) => {
+          const targetValue = field.query('.minimum').value();
+          field.selfErrors =
+            !!targetValue && !!field.value && targetValue > field.value ? '${i18n.t(
+              'Maximum must greater than minimum',
+            )}' : ''
+        }}}`,
+      },
+      minimum: {
+        type: 'number',
+        title: '{{ t("Minimum") }}',
+        'x-decorator': 'FormItem',
+        'x-component': 'InputNumber',
+        'x-component-props': {
+          precision: 0,
+        },
+        'x-reactions': {
+          dependencies: ['.maximum'],
+          fulfill: {
+            state: {
+              selfErrors: `{{!!$deps[0] && !!$self.value && $deps[0] < $self.value ? '${i18n.t(
+                'Minimum must less than maximum',
+              )}' : ''}}`,
+            },
+          },
+        },
+      },
+      format: {
+        type: 'string',
+        title: '{{ t("Format") }}',
+        'x-decorator': 'FormItem',
+        'x-component': 'Select',
+        'x-component-props': {
+          allowClear: true,
+        },
+        enum: [
+          {
+            label: '{{ t("Odd") }}',
+            value: 'odd',
+          },
+          {
+            label: '{{ t("Even") }}',
+            value: 'even',
+          },
+        ],
+      },
+      pattern: {
+        type: 'string',
+        title: '{{ t("Regular expression") }}',
+        'x-decorator': 'FormItem',
+        'x-component': 'Input',
+        'x-component-props': {
+          prefix: '/',
+          suffix: '/',
+        },
+      },
+    };
   };
 }
