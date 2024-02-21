e04f4f1b7eba7a25d433cffd801176a32a6e44e2
katherinehhh <katherine_15995@163.com>
2024-02-21 11:17:31 +0800

    refactor: edit field

--- packages/core/client/src/collection-manager/Configuration/EditFieldAction.tsx
+++ packages/core/client/src/collection-manager/Configuration/EditFieldAction.tsx
@@ -185,7 +185,7 @@ export const EditFieldAction = (props) => {
               defaultValues.autoCreateReverseField = false;
               defaultValues.reverseField = interfaceConf?.default?.reverseField;
               set(defaultValues.reverseField, 'name', `f_${uid()}`);
-              set(defaultValues.reverseField, 'uiSchema.title', record.__parent.title);
+              set(defaultValues.reverseField, 'uiSchema.title', record.__parent?.title);
             }
             const schema = getSchema(
               {
