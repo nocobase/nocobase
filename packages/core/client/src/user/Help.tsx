3fa3f5211238761e12f99c7d559e7a99216e528a
Zeke Zhang <958414905@qq.com>
2024-02-22 09:25:55 +0800

    chore: change License link

--- packages/core/client/src/user/Help.tsx
+++ packages/core/client/src/user/Help.tsx
@@ -49,7 +49,7 @@ const SettingsMenu: React.FC<{
     {
       key: 'license',
       label: (
-        <a href="https://github.com/nocobase/nocobase?tab=readme-ov-file#license" target="__blank">
+        <a href="https://github.com/nocobase/nocobase/blob/main/LICENSE-AGPL" target="__blank">
           {t('License')}
         </a>
       ),
