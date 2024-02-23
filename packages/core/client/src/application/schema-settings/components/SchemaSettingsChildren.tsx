735231e44e2909d0bd0a7427de66e4af3e4fff03
Zeke Zhang <958414905@qq.com>
2024-02-23 18:37:48 +0800

    test: fix e2e

--- packages/core/client/src/application/schema-settings/components/SchemaSettingsChildren.tsx
+++ packages/core/client/src/application/schema-settings/components/SchemaSettingsChildren.tsx
@@ -18,6 +18,7 @@ import {
 import { SchemaSettingItemContext } from '../context';
 import { SchemaSettingsItemType } from '../types';
 import { uid } from '@formily/shared';
+import { useFieldComponentName } from '../../../common/useFieldComponentName';
 
 export interface SchemaSettingsChildrenProps {
   children: SchemaSettingsItemType[];
@@ -41,6 +42,8 @@ export const SchemaSettingsChildren: FC<SchemaSettingsChildrenProps> = (props) =
   const { children } = props;
   const { visible } = useSchemaSettings();
   const firstVisible = useRef<boolean>(false);
+  const fieldComponentName = useFieldComponentName();
+
   useEffect(() => {
     if (visible) {
       firstVisible.current = true;
@@ -54,10 +57,12 @@ export const SchemaSettingsChildren: FC<SchemaSettingsChildrenProps> = (props) =
       {children
         .sort((a, b) => (a.sort || 0) - (b.sort || 0))
         .map((item) => {
-          // 当动态切换 SchemaSettings 列表时，切换前和切换后的 item.name 可能相同，导致 key 相同，从而导致 React 认为其前后是同一个组件；
-          // 因为 SchemaSettingsChild 的某些 hooks 是通过 props 传入的，两次渲染之间 props 可能发生变化，就可能报 hooks 调用
-          // 顺序的错误。所以这里使用一个随机值作为 key，保证每次渲染都是新的组件。
-          return <SchemaSettingsChild key={uid()} {...item} />;
+          // 当动态切换 SchemaSettings 列表时（比如切换 field component 时，列表会动态变化），切换前和切换后的 item.name 可能相同，
+          // 此时如果使用 item.name 作为 key，会导致 React 认为其前后是同一个组件；因为 SchemaSettingsChild 的某些 hooks 是通过 props 传入的，
+          // 两次渲染之间 props 可能发生变化，就可能报 hooks 调用顺序的错误。所以这里使用 fieldComponentName 和 item.name 拼成
+          // 一个不会重复的 key，保证每次渲染都是新的组件。
+          const key = `${fieldComponentName ? fieldComponentName + '-' : ''}${item.name}`;
+          return <SchemaSettingsChild key={key} {...item} name={key} />;
         })}
     </>
   );
