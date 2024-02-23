165d73d475dbfa0975a18c620fd040189823d183
ChengLei Shao <chareice@live.com>
2024-02-23 15:12:57 +0800

    fix: lazy loading belongs to association (#3559)
    * chore: lazy loading belongs to association
    
    * chore: test
    
    * chore: console.log

--- packages/plugins/@nocobase/plugin-collection-manager/src/server/__tests__/fields/belongsTo.test.ts
+++ packages/plugins/@nocobase/plugin-collection-manager/src/server/__tests__/fields/belongsTo.test.ts
@@ -1,6 +1,7 @@
 import Database, { Collection as DBCollection } from '@nocobase/database';
 import Application from '@nocobase/server';
 import { createApp } from '..';
+import { CollectionRepository } from '../../index';
 
 describe('belongsTo', () => {
   let db: Database;
@@ -13,23 +14,55 @@ describe('belongsTo', () => {
     db = app.db;
     Collection = db.getCollection('collections');
     Field = db.getCollection('fields');
-    await Collection.repository.create({
-      values: {
-        name: 'tests',
-      },
-    });
-    await Collection.repository.create({
-      values: {
-        name: 'foos',
-      },
-    });
   });
 
   afterEach(async () => {
     await app.destroy();
   });
 
-  it('a', () => {
-    expect(true).toBe(true);
+  it('should load belongsTo field', async () => {
+    await Collection.repository.create({
+      values: {
+        name: 'orders',
+        fields: [
+          {
+            type: 'integer',
+            name: 'amount',
+          },
+          {
+            type: 'belongsTo',
+            name: 'users',
+            targetKey: 'uid',
+            foreignKey: 'userId',
+          },
+        ],
+      },
+    });
+
+    await Collection.repository.create({
+      values: {
+        name: 'users',
+        fields: [
+          {
+            type: 'string',
+            name: 'name',
+          },
+          {
+            type: 'string',
+            name: 'uid',
+          },
+        ],
+      },
+    });
+
+    let error;
+
+    try {
+      await db.getRepository<CollectionRepository>('collections').load();
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).toBeUndefined();
   });
 });
