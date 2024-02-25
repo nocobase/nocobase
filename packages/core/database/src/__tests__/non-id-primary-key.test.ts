f49c8b0ce95edcd13c0b8ebf5da780c1612575ba
Chareice <chareice@live.com>
2024-02-25 10:33:47 +0800

    chore: created at & updated at field

--- packages/core/database/src/__tests__/non-id-primary-key.test.ts
+++ packages/core/database/src/__tests__/non-id-primary-key.test.ts
@@ -21,12 +21,63 @@ describe('non-id primary key', () => {
     });
     await db.sync();
 
-    console.log('start sync');
     User.setField('field_auto_incr', { type: 'integer', primaryKey: true, autoIncrement: true });
 
     await db.sync();
 
-    console.log('sync finished');
     await User.repository.create({});
+  });
+
+  it('should add createdAt && updatedAt field', async () => {
+    const User = db.collection({
+      name: 'users',
+      autoGenId: false,
+      timestamps: false,
+      fields: [
+        {
+          type: 'string',
+          name: 'name',
+        },
+      ],
+    });
+
+    await db.sync();
+
+    User.setField('createdAt', {
+      uiSchema: {
+        'x-component-props': { dateFormat: 'YYYY-MM-DD', showTime: true, timeFormat: 'HH:mm:ss' },
+        type: 'datetime',
+        title: '{{t("Created at")}}',
+        'x-component': 'DatePicker',
+        'x-read-pretty': true,
+      },
+      name: 'xxxx',
+      type: 'date',
+      field: 'createdAt',
+      interface: 'createdAt',
+    });
+
+    User.setField('updatedAt', {
+      uiSchema: {
+        'x-component-props': { dateFormat: 'YYYY-MM-DD', showTime: true, timeFormat: 'HH:mm:ss' },
+        type: 'datetime',
+        title: '{{t("Updated at")}}',
+        'x-component': 'DatePicker',
+        'x-read-pretty': true,
+      },
+      name: 'updatedAttt',
+      type: 'date',
+      field: 'updatedAt',
+      interface: 'updatedAt',
+    });
+
+    await db.sync();
+
+    const user = await User.repository.create({
+      values: { name: 'test' },
+    });
+
+    expect(user.get('xxxx')).toBeTruthy();
+    expect(user.get('updatedAttt')).toBeTruthy();
   });
 });
