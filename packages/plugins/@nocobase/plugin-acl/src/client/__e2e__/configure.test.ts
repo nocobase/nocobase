2393088f6a5b349358559b5b85aeb0deb967c14e
katherinehhh <katherine_15995@163.com>
2024-03-01 09:49:33 +0800

    test: acl test

--- packages/plugins/@nocobase/plugin-acl/src/client/__e2e__/configure.test.ts
+++ packages/plugins/@nocobase/plugin-acl/src/client/__e2e__/configure.test.ts
@@ -63,8 +63,13 @@ test('allows to confgiure plugins ', async ({ page, mockPage, mockRole, updateRo
   }, roleData);
   await page.reload();
   await page.getByTestId('plugin-settings-button').click();
-  await page.getByRole('link', { name: 'Access control' }).click();
-  await page.getByLabel(`action-Action.Link-Configure-roles-${roleData.name}`).click();
+  await page.getByRole('link', { name: 'Users & Permissions' }).click();
+  await page.getByText('Roles & Permissions').click();
+  await page
+    .getByRole('menuitem', { name: `${roleData.name}` })
+    .locator('span')
+    .nth(1)
+    .click();
   await expect(page.getByRole('tab').getByText('Plugin settings permissions')).toBeVisible();
   await updateRole({
     name: roleData.name,
@@ -128,10 +133,10 @@ test('plugin settings permissions', async ({ page, mockPage, mockRole, updateRol
   }, roleData);
   await page.reload();
   await page.getByTestId('plugin-settings-button').hover();
-  await expect(page.getByRole('link', { name: 'Access control' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Users & Permissions' })).toBeVisible();
   await expect(page.getByLabel('auth')).not.toBeVisible();
   await expect(page.getByLabel('collection-manager')).not.toBeVisible();
-  await page.getByRole('link', { name: 'Access control' }).click();
+  await page.getByRole('link', { name: 'Users & Permissions' }).click();
   await expect(page.getByRole('menuitem', { name: 'login Authentication' })).not.toBeVisible();
   await expect(page.getByRole('menuitem', { name: 'database Collection manager' })).not.toBeVisible();
   await updateRole({
@@ -140,8 +145,8 @@ test('plugin settings permissions', async ({ page, mockPage, mockRole, updateRol
   });
   await page.reload();
   await page.getByTestId('plugin-settings-button').hover();
-  await expect(page.getByRole('link', { name: 'Access control' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Users & Permissions' })).toBeVisible();
   await expect(page.getByRole('link', { name: 'Authentication' })).toBeVisible();
-  await page.getByRole('link', { name: 'Access control' }).click();
+  await page.getByRole('link', { name: 'Users & Permissions' }).click();
   await expect(page.getByRole('menuitem', { name: 'login Authentication' })).toBeVisible();
 });
