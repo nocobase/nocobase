68ad0cde6aaf80837dcc2f855e75715f77928b2e
dream2023 <1098626505@qq.com>
2024-03-05 17:55:08 +0800

    fix: observer add displayName

--- packages/plugins/@nocobase/plugin-data-visualization/src/client/filter/FilterItemInitializers.tsx
+++ packages/plugins/@nocobase/plugin-data-visualization/src/client/filter/FilterItemInitializers.tsx
@@ -29,11 +29,14 @@ import { lang, useChartsTranslation } from '../locale';
 import { getPropsSchemaByComponent } from './utils';
 const { Paragraph, Text } = Typography;
 
-const FieldComponentProps: React.FC = observer((props) => {
-  const form = useForm();
-  const schema = getPropsSchemaByComponent(form.values.component);
-  return schema ? <SchemaComponent schema={schema} {...props} /> : null;
-});
+const FieldComponentProps: React.FC = observer(
+  (props) => {
+    const form = useForm();
+    const schema = getPropsSchemaByComponent(form.values.component);
+    return schema ? <SchemaComponent schema={schema} {...props} /> : null;
+  },
+  { displayName: 'FieldComponentProps' },
+);
 
 const ErrorFallback = ({ error }) => {
   return (
