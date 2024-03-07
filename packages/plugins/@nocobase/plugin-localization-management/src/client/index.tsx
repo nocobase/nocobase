a2c66aa5de01d36a7f94923fe1753e79cc9b3d2b
xilesun <2013xile@gmail.com>
2024-03-07 20:06:01 +0800

    chore: update menu name of plugin-localization

--- packages/plugins/@nocobase/plugin-localization-management/src/client/index.tsx
+++ packages/plugins/@nocobase/plugin-localization-management/src/client/index.tsx
@@ -5,7 +5,7 @@ import { NAMESPACE } from './locale';
 export class LocalizationManagementPlugin extends Plugin {
   async load() {
     this.app.pluginSettingsManager.add(NAMESPACE, {
-      title: `{{t("Localization management", { ns: "${NAMESPACE}" })}}`,
+      title: `{{t("Localization", { ns: "${NAMESPACE}" })}}`,
       icon: 'GlobalOutlined',
       Component: Localization,
       aclSnippet: 'pm.localization-management.localization',
