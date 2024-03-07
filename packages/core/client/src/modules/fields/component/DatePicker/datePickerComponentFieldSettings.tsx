30b4ed77a1ea877a75b65b69c6ce897a231660f7
katherinehhh <shunai.tang@hand-china.com>
2024-03-07 10:48:49 +0800

    fix: dateTime format configured in table is invalid (#3630)
    * fix: datetime format configured in the table is invalid
    
    * test: date display format
    
    * refactor: default timeformat
    
    * test: datetime display format

--- packages/core/client/src/modules/fields/component/DatePicker/datePickerComponentFieldSettings.tsx
+++ packages/core/client/src/modules/fields/component/DatePicker/datePickerComponentFieldSettings.tsx
@@ -1,6 +1,7 @@
 import { useFieldSchema } from '@formily/react';
 import { SchemaSettings } from '../../../../application/schema-settings/SchemaSettings';
 import { SchemaSettingsDateFormat } from '../../../../schema-settings/SchemaSettingsDateFormat';
+import { useColumnSchema } from '../../../../schema-component/antd/table-v2/Table.Column.Decorator';
 
 export const datePickerComponentFieldSettings = new SchemaSettings({
   name: 'fieldSettings:component:DatePicker',
@@ -9,7 +10,9 @@ export const datePickerComponentFieldSettings = new SchemaSettings({
       name: 'dateDisplayFormat',
       Component: SchemaSettingsDateFormat as any,
       useComponentProps() {
-        const fieldSchema = useFieldSchema();
+        const schema = useFieldSchema();
+        const { fieldSchema: tableColumnSchema } = useColumnSchema();
+        const fieldSchema = tableColumnSchema || schema;
         return {
           fieldSchema,
         };
