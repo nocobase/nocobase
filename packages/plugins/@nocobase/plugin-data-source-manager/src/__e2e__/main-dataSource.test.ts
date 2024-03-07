2f6e1155b0528e8282680f0a36003577a018d16c
katherinehhh <katherine_15995@163.com>
2024-03-07 17:58:46 +0800

    test: data source test

--- packages/plugins/@nocobase/plugin-data-source-manager/src/__e2e__/main-dataSource.test.ts
+++ packages/plugins/@nocobase/plugin-data-source-manager/src/__e2e__/main-dataSource.test.ts
@@ -1,9 +1,62 @@
 import { expect, test } from '@nocobase/test/e2e';
 import { uid } from '@nocobase/utils';
 
-test.describe('Initializing main data source by default', () => {
-  test('basic', async ({ page }) => {
-    await page.goto('/');
-    // await page.goto('admin/settings/data-source-manager');
+test('initializing main data source by default', async ({ page }) => {
+  await page.goto('/');
+  await page.getByTestId('plugin-settings-button').click();
+  await page.getByRole('link', { name: 'Data sources' }).click();
+  await expect(page.getByText('main', { exact: true })).toBeVisible();
+});
+
+test.describe('preset fields', () => {
+  test('create all preset fields by default', async ({ page }) => {
+    await page.goto('/admin/settings/data-source-manager/list');
+    await page.getByRole('button', { name: 'Configure' }).click();
+    await page.getByRole('button', { name: 'plus Create collection down' }).click();
+    await page.getByRole('menuitem', { name: 'General collection' }).locator('span').click();
+    await page.getByLabel('block-item-Input-collections-Collection display name').getByRole('textbox').click();
+    await page.getByLabel('block-item-Input-collections-Collection display name').getByRole('textbox').fill(uid());
+
+    //添加关系字段,断言提交的data是否符合预期
+    const [request] = await Promise.all([
+      page.waitForRequest((request) => request.url().includes('api/collections:create')),
+      page.getByLabel('action-Action-Submit').click(),
+    ]);
+    const postData = request.postDataJSON();
+    //默认提交的数据符合预期
+    expect(postData).toMatchObject({
+      autoGenId: true,
+      createdAt: true,
+      createdBy: true,
+      updatedAt: true,
+      updatedBy: true,
+      fields: expect.arrayContaining([
+        expect.objectContaining({
+          name: 'id',
+          type: 'bigInt',
+          // 其他属性
+        }),
+        expect.objectContaining({
+          name: 'createdAt',
+          type: 'date',
+          // 其他属性
+        }),
+        expect.objectContaining({
+          name: 'createdBy',
+          type: 'belongsTo',
+          // 其他属性
+        }),
+        expect.objectContaining({
+          name: 'updatedBy',
+          type: 'belongsTo',
+          // 其他属性
+        }),
+        expect.objectContaining({
+          name: 'updatedAt',
+          type: 'date',
+          // 其他属性
+        }),
+      ]),
+    });
   });
 });
