6a37bb50c95ce74cd81476e877a06ed23f759bd2
katherinehhh <katherine_15995@163.com>
2024-03-10 19:52:14 +0800

    test: main data source test

---
+++ packages/plugins/@nocobase/plugin-data-source-manager/src/__e2e__/externalDataSource.test.ts
@@ -0,0 +1,14 @@
+import { expect, test } from '@nocobase/test/e2e';
+import { uid } from '@nocobase/utils';
+
+test('add data source', async ({ page }) => {
+  await page.goto('/');
+  await page.getByTestId('plugin-settings-button').click();
+  await page.getByRole('link', { name: 'Data sources' }).click();
+  await expect(page.getByText('main', { exact: true })).toBeVisible();
+});
+
+test.describe('configure Data Source', () => {
+  test('external data source collection configure', async ({ page }) => {});
+  test('Synchronize(Refresh) external data source collection', async ({ page }) => {});
+});
