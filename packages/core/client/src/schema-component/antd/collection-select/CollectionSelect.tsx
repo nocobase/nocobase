a0f1c73f6facbfffb3db2deb83ef9959460685c2
mytharcher <mytharcher@gmail.com>
2024-03-12 14:08:49 +0000

    refactor(client): add component to support data source select

--- packages/core/client/src/schema-component/antd/collection-select/CollectionSelect.tsx
+++ packages/core/client/src/schema-component/antd/collection-select/CollectionSelect.tsx
@@ -1,18 +1,20 @@
 import { connect, mapReadPretty, observer, useField } from '@formily/react';
-import { Select, SelectProps, Tag } from 'antd';
-import React, { useContext } from 'react';
+import { Cascader, Select, SelectProps, Tag } from 'antd';
+import React, { useCallback, useContext, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelfAndChildrenCollections } from '../../../collection-manager/action-hooks';
 import { useCollection_deprecated, useCollectionManager_deprecated } from '../../../collection-manager/hooks';
 import { useCompile } from '../../hooks';
 import { FilterContext } from '../filter/context';
+import { useDataSourceManager } from '../../../data-source';
+import { parseCollectionName } from '../../../collection-manager';
 
 export type CollectionSelectProps = SelectProps<any, any> & {
   filter?: (item: any, index: number, array: any[]) => boolean;
   isTableOid?: boolean;
 };
 
-function useOptions({ filter, isTableOid }: CollectionSelectProps) {
+function useCollectionOptions({ filter, isTableOid }: CollectionSelectProps) {
   const compile = useCompile();
   const field: any = useField();
   const ctx: any = useContext(FilterContext);
@@ -29,20 +31,30 @@ function useOptions({ filter, isTableOid }: CollectionSelectProps) {
     typeof filter === 'function'
       ? ((inheritCollections || currentCollections) as any[]).filter(filter)
       : inheritCollections || currentCollections;
-  return filtered
-    .filter((item) => !item.hidden)
-    .map((item) => ({
-      label: compile(item.title || item.label),
-      value: item.name || item.value,
-      color: item.category?.color,
-    }));
+  return useMemo(
+    () =>
+      filtered
+        .filter((item) => !item.hidden)
+        .map((item) => ({
+          label: compile(item.title || item.label),
+          value: item.name || item.value,
+          color: item.category?.color,
+        })),
+    [filtered],
+  );
 }
 
 export const CollectionSelect = connect(
   (props: CollectionSelectProps) => {
     const { filter, ...others } = props;
-    const options = useOptions(props);
+    const options = useCollectionOptions(props);
     const { t } = useTranslation();
+    const optionFilter = useCallback(
+      (input, option) =>
+        (option?.label.toLowerCase() ?? '').includes(input.toLocaleLowerCase()) ||
+        (option?.value.toString().toLowerCase() ?? '').includes(input.toLocaleLowerCase()),
+      [],
+    );
     return (
       <Select
         // @ts-ignore
@@ -52,10 +64,7 @@ export const CollectionSelect = connect(
         popupMatchSelectWidth={false}
         {...others}
         showSearch
-        filterOption={(input, option) =>
-          (option?.label.toLowerCase() ?? '').includes(input.toLocaleLowerCase()) ||
-          (option?.value.toString().toLowerCase() ?? '').includes(input.toLocaleLowerCase())
-        }
+        filterOption={optionFilter}
         options={options}
       />
     );
@@ -65,7 +74,7 @@ export const CollectionSelect = connect(
       (props: CollectionSelectProps) => {
         const { mode } = props;
         const compile = useCompile();
-        const options = useOptions(props).filter((option) => {
+        const options = useCollectionOptions(props).filter((option) => {
           if (mode === 'multiple') {
             return (props.value ?? []).includes(option.value);
           }
@@ -86,3 +95,88 @@ export const CollectionSelect = connect(
     ),
   ),
 );
+
+export type DataSourceSelectProps = SelectProps<any, any> & {
+  filter?: (item: any, index: number, array: any[]) => boolean;
+};
+
+function useDataSourceOptions({ filter }: DataSourceSelectProps) {
+  const compile = useCompile();
+  const dataSourceManager = useDataSourceManager();
+  const dataSources = dataSourceManager.getDataSources();
+  return useMemo(
+    () => [
+      {
+        label: compile('Main'),
+        value: 'main',
+      },
+      ...(typeof filter === 'function' ? dataSources.filter(filter) : dataSources).map((item) => ({
+        label: item.displayName,
+        value: item.key,
+      })),
+    ],
+    [dataSources, filter],
+  );
+}
+
+export const DataSourceSelect = connect((props: DataSourceSelectProps) => {
+  const { filter, ...others } = props;
+  const options = useDataSourceOptions(props);
+  const { t } = useTranslation();
+  const optionFilter = useCallback(
+    (input, option) =>
+      (option?.label.toLowerCase() ?? '').includes(input.toLocaleLowerCase()) ||
+      (option?.value.toString().toLowerCase() ?? '').includes(input.toLocaleLowerCase()),
+    [],
+  );
+  return (
+    <Select
+      // @ts-ignore
+      role="button"
+      data-testid="select-datasource"
+      placeholder={t('Select data source')}
+      popupMatchSelectWidth={false}
+      {...others}
+      showSearch
+      filterOption={optionFilter}
+      options={options}
+    />
+  );
+});
+
+export const DataSourceCollectionCascader = connect((props) => {
+  const dataSourceManager = useDataSourceManager();
+  const compile = useCompile();
+  const { value, onChange, dataSourceFilter, collectionFilter, ...others } = props;
+  const [dataSourceName, collectionName] = parseCollectionName(value);
+  const path = [dataSourceName, collectionName].filter(Boolean);
+  const dataSources = dataSourceManager.getDataSources();
+
+  const options = useMemo(() => {
+    return (dataSourceFilter ? dataSources.filter(dataSourceFilter) : dataSources).map((dataSource) => {
+      return {
+        label: compile(dataSource.displayName),
+        value: dataSource.key,
+        children: dataSource.collectionManager.collectionInstancesArr
+          .filter(collectionFilter ?? ((collection) => !collection.hidden))
+          .map((collection) => {
+            return {
+              label: compile(collection.title),
+              value: collection.name,
+            };
+          }),
+      };
+    });
+  }, [dataSources, dataSourceFilter, collectionFilter]);
+
+  const handleChange = useCallback(
+    (value) => {
+      if (!value) {
+        return onChange(value);
+      }
+      onChange(value[0] === 'main' ? value[1] : value.join(':'));
+    },
+    [onChange],
+  );
+  return <Cascader showSearch {...others} options={options} value={path} onChange={handleChange} />;
+});
