1df2d82528f2aa42ab8accccf2423e85364929ff
katherinehhh <katherine_15995@163.com>
2024-03-14 11:31:11 +0800

    test: connect pg  data source

--- packages/plugins/@nocobase/plugin-data-source-manager/src/client/__e2e__/externalDataSource.test.ts
+++ packages/plugins/@nocobase/plugin-data-source-manager/src/client/__e2e__/externalDataSource.test.ts
@@ -3,16 +3,31 @@ import { uid } from '@nocobase/utils';
 
 test.describe('add external data source', () => {
   test('pg external data source', async ({ page }) => {
+    await page.goto('/');
     await page.getByTestId('plugin-settings-button').click();
     await page.getByRole('link', { name: 'Data sources' }).click();
+    await page.getByRole('button', { name: 'plus Add new down' }).click();
     await page.getByRole('menuitem', { name: 'PostgreSQL' }).click();
     await page.getByLabel('Data source name').click();
     await page.getByLabel('Data source name').getByRole('textbox').fill('pg');
     await page.getByLabel('Data source display name').getByRole('textbox').fill('pg');
-    await page.getByLabel('Database').getByRole('textbox').fill(`${process.env.DATABASE}_external_test`);
+    await page.getByLabel('Port').getByRole('textbox').fill(`${process.env.DB_PORT}`);
+    await page.getByLabel('Database').getByRole('textbox').fill(`${process.env.DB_DATABASE}_external_test`);
     await page.getByLabel('Username').getByRole('textbox').fill(`${process.env.USER}`);
-    await page.locator('input[type="password"]').fill(`${process.env.PASSWORD}`);
+    await page.locator('input[type="password"]').fill(`${process.env.DB_PASSWORD}`);
+    //测试连接
+    const [request] = await Promise.all([
+      page.waitForRequest((request) => request.url().includes('api/dataSources:testConnection')),
+      page.getByLabel('action-Action-Test Connection').click(),
+    ]);
+    const response = await (await request.response()).json();
+    expect(response).toMatchObject({
+      data: {
+        success: true,
+      },
+    });
     await page.getByLabel('action-Action-Submit-').click();
+    await page.waitForTimeout(1000);
   });
 });
 
