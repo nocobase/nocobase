86d4344edd83424ee76e335ea0b6d18587492546
Zeke Zhang <958414905@qq.com>
2024-03-15 22:56:35 +0800

    fix(variables): fix varaibles for table selector (#3725)
    * fix(variables): fix varaibles for table selector
    
    * test: add e2e
    
    * chore: fix build
    
    * fix: should not display current record in create form
    
    * fix: fix known bugs
    
    * test: fix e2e

--- packages/core/client/src/schema-settings/VariableInput/hooks/useIterationVariable.ts
+++ packages/core/client/src/schema-settings/VariableInput/hooks/useIterationVariable.ts
@@ -5,6 +5,7 @@ import { CollectionFieldOptions } from '../../../data-source/collection/Collecti
 import { useFlag } from '../../../flag-provider';
 import { useSubFormValue } from '../../../schema-component/antd/association-field/hooks';
 import { useBaseVariable } from './useBaseVariable';
+import { useCollection } from '../../../data-source';
 
 /**
  * @deprecated
@@ -61,13 +62,11 @@ export const useIterationVariable = ({
  * @returns
  */
 export const useCurrentObjectVariable = ({
-  currentCollection,
   collectionField,
   schema,
   noDisabled,
   targetFieldSchema,
 }: {
-  currentCollection?: string;
   collectionField?: CollectionFieldOptions;
   schema?: any;
   noDisabled?: boolean;
@@ -75,7 +74,8 @@ export const useCurrentObjectVariable = ({
   targetFieldSchema?: Schema;
 } = {}) => {
   // const { getActiveFieldsName } = useFormActiveFields() || {};
-  const { formValue: currentObjectCtx } = useSubFormValue();
+  const collection = useCollection();
+  const { formValue: currentObjectCtx, collection: collectionOfCurrentObject } = useSubFormValue();
   const { isInSubForm, isInSubTable } = useFlag() || {};
   const { t } = useTranslation();
   const currentObjectSettings = useBaseVariable({
@@ -85,7 +85,7 @@ export const useCurrentObjectVariable = ({
     maxDepth: 4,
     name: '$iteration',
     title: t('Current object'),
-    collectionName: currentCollection,
+    collectionName: collectionOfCurrentObject?.name || collection?.name,
     noDisabled,
     returnFields: (fields, option) => {
       // fix https://nocobase.height.app/T-2277
--- packages/core/client/src/schema-settings/VariableInput/hooks/useRecordVariable.ts
+++ packages/core/client/src/schema-settings/VariableInput/hooks/useRecordVariable.ts
@@ -2,10 +2,8 @@ import { Schema } from '@formily/json-schema';
 import _ from 'lodash';
 import { useTranslation } from 'react-i18next';
 import { CollectionFieldOptions_deprecated } from '../../../collection-manager';
-import { useCollection } from '../../../data-source/collection/CollectionProvider';
-import { useCollectionRecord } from '../../../data-source/collection-record/CollectionRecordProvider';
-import { useFlag } from '../../../flag-provider/hooks/useFlag';
 import { useBaseVariable } from './useBaseVariable';
+import { useFormBlockContext } from '../../../block-provider/FormBlockProvider';
 
 interface Props {
   collectionField?: CollectionFieldOptions_deprecated;
@@ -47,16 +45,13 @@ export const useRecordVariable = (props: Props) => {
  */
 export const useCurrentRecordVariable = (props: Props = {}) => {
   const { t } = useTranslation();
-  const { isInSubForm, isInSubTable } = useFlag() || {};
-  const record = useCollectionRecord();
-  const recordData = isInSubForm || isInSubTable ? record?.parentRecord?.data : record?.data;
-  const { name: collectionName } = useCollection() || {};
+  const { formRecord, collectionName } = useFormBlockContext();
   const currentRecordSettings = useBaseVariable({
     collectionField: props.collectionField,
     uiSchema: props.schema,
     name: '$nRecord',
     title: t('Current record'),
-    collectionName: props.collectionName || collectionName,
+    collectionName: collectionName,
     noDisabled: props.noDisabled,
     targetFieldSchema: props.targetFieldSchema,
   });
@@ -65,9 +60,9 @@ export const useCurrentRecordVariable = (props: Props = {}) => {
     /** 变量配置 */
     currentRecordSettings,
     /** 变量值 */
-    currentRecordCtx: recordData,
+    currentRecordCtx: formRecord?.data,
     /** 用于判断是否需要显示配置项 */
-    shouldDisplayCurrentRecord: !_.isEmpty(recordData),
+    shouldDisplayCurrentRecord: !formRecord?.isNew && !_.isEmpty(formRecord?.data),
     /** 当前记录对应的 collection name */
     collectionName,
   };
