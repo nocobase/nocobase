c191f149f96def2672fc50d4d042ca976c863ebc
chenos <chenlinxh@gmail.com>
2024-03-16 22:44:57 +0800

    fix: yarn dev error

--- packages/core/app/client/src/pages/index.tsx
+++ packages/core/app/client/src/pages/index.tsx
@@ -5,16 +5,16 @@ import devDynamicImport from '../.plugins/index';
 export const app = new Application({
   apiClient: {
     // @ts-ignore
-    baseURL: window['__nocobase_api_base_url__'] || '/api/',
+    baseURL: window['__nocobase_api_base_url__'] || process.env.API_BASE_URL || '/api/',
   },
   // @ts-ignore
-  publicPath: window['__nocobase_public_path__'] || '/',
+  publicPath: window['__nocobase_public_path__'] || process.env.APP_PUBLIC_PATH || '/',
   plugins: [NocoBaseClientPresetPlugin],
   ws: {
     // @ts-ignore
-    url: window['__nocobase_ws_url__'] || '',
+    url: window['__nocobase_ws_url__'] || process.env.WEBSOCKET_URL || '',
     // @ts-ignore
-    basename: window['__nocobase_ws_path__'] || '/ws',
+    basename: window['__nocobase_ws_path__'] || process.env.WS_PATH || '/ws',
   },
   loadRemotePlugins: true,
   devDynamicImport,
