8c82f1efd61ca35008cb3112ddc9e62e90b87e5d
mytharcher <mytharcher@gmail.com>
2024-03-20 08:54:16 +0000

    fix(plugin-workflow-action-trigger): fix user fields in context

--- packages/plugins/@nocobase/plugin-workflow-action-trigger/src/server/ActionTrigger.ts
+++ packages/plugins/@nocobase/plugin-workflow-action-trigger/src/server/ActionTrigger.ts
@@ -57,8 +57,9 @@ export default class extends Trigger {
     const { triggerWorkflows = '', values } = context.action.params;
 
     const { currentUser, currentRole } = context.state;
+    const { model: UserModel } = this.workflow.db.getCollection('users');
     const userInfo = {
-      user: toJSON(currentUser),
+      user: UserModel.build(currentUser).desensitize(),
       roleName: currentRole,
     };
 
