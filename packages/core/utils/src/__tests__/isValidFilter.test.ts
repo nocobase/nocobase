8470f835b6eecf91afd3e25f6955e34db89e474c
Chareice <chareice@live.com>
2024-03-21 15:29:21 +0800

    chore: test

--- packages/core/utils/src/__tests__/isValidFilter.test.ts
+++ packages/core/utils/src/__tests__/isValidFilter.test.ts
@@ -2,7 +2,7 @@ import { isValidFilter } from '..';
 
 describe('isValidFilter', () => {
   it('should return false', () => {
-    expect(isValidFilter()).toBe(false);
+    expect(isValidFilter(undefined)).toBe(false);
     expect(isValidFilter({})).toBe(false);
     expect(isValidFilter({ $and: [] })).toBe(false);
     expect(isValidFilter({ $or: [] })).toBe(false);
@@ -10,6 +10,7 @@ describe('isValidFilter', () => {
     expect(isValidFilter({ $or: [{}] })).toBe(false);
     expect(isValidFilter({ $and: [{ $or: [] }] })).toBe(false);
     expect(isValidFilter({ $or: [{ $and: [] }] })).toBe(false);
+    expect(isValidFilter({ $and: [{}], $or: [{ $and: [], $or: [] }] })).toBe(false);
   });
 
   it('should return true', () => {
