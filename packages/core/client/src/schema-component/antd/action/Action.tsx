535b0c92a07ca7ede3447a5b63344e58ac167d9c
dream2023 <1098626505@qq.com>
2024-03-21 10:14:52 +0800

    fix: remove Action performance imporve

--- packages/core/client/src/schema-component/antd/action/Action.tsx
+++ packages/core/client/src/schema-component/antd/action/Action.tsx
@@ -176,9 +176,9 @@ export const Action: ComposedAction = observer(
 
     const buttonElement = renderButton();
 
-    if (!btnHover) {
-      return buttonElement;
-    }
+    // if (!btnHover) {
+    //   return buttonElement;
+    // }
 
     const result = (
       <ActionContextProvider
