62e17cc8a631d6fb45e3eb7b431e32400013c7ba
Zeke Zhang <958414905@qq.com>
2024-03-21 12:42:53 +0800

    test: add unit tests

---
+++ packages/core/client/src/modules/blocks/data-blocks/details-multi/__tests__/createDetailsBlockWithPagingUISchema.test.ts
@@ -0,0 +1,81 @@
+import { createDetailsBlockWithPagingUISchema } from '../createDetailsBlockWithPagingUISchema';
+
+vi.mock('@formily/shared', () => ({
+  uid: vi.fn().mockReturnValue('mocked-uid'),
+}));
+
+describe('createDetailsBlockWithPagingUISchema', () => {
+  it('should throw an error if collectionName, dataSource, and rowKey are not provided', () => {
+    expect(() => {
+      createDetailsBlockWithPagingUISchema({
+        collectionName: '',
+        dataSource: '',
+        rowKey: '',
+      });
+    }).toThrow('collectionName, dataSource and rowKey are required');
+  });
+
+  it('should return the correct schema', () => {
+    const schema = createDetailsBlockWithPagingUISchema({
+      collectionName: 'users',
+      dataSource: 'users',
+      rowKey: 'id',
+    });
+
+    expect(schema).toEqual({
+      type: 'void',
+      'x-acl-action': 'users:view',
+      'x-decorator': 'DetailsBlockProvider',
+      'x-use-decorator-props': 'useDetailsBlockWithPagingDecoratorProps',
+      'x-decorator-props': {
+        dataSource: 'users',
+        collection: 'users',
+        association: undefined,
+        readPretty: true,
+        action: 'list',
+        params: {
+          pageSize: 1,
+        },
+      },
+      'x-toolbar': 'BlockSchemaToolbar',
+      'x-settings': 'blockSettings:multiDataDetails',
+      'x-component': 'CardItem',
+      properties: {
+        'mocked-uid': {
+          type: 'void',
+          'x-component': 'Details',
+          'x-read-pretty': true,
+          'x-component-props': {
+            useProps: '{{ useDetailsBlockWithPagingProps }}',
+          },
+          properties: {
+            'mocked-uid': {
+              type: 'void',
+              'x-initializer': 'detailsWithPaging:configureActions',
+              'x-component': 'ActionBar',
+              'x-component-props': {
+                style: {
+                  marginBottom: 24,
+                },
+              },
+              properties: {},
+            },
+            grid: {
+              type: 'void',
+              'x-component': 'Grid',
+              'x-initializer': 'details:configureFields',
+              properties: {},
+            },
+            pagination: {
+              type: 'void',
+              'x-component': 'Pagination',
+              'x-component-props': {
+                useProps: '{{ useDetailsPaginationProps }}',
+              },
+            },
+          },
+        },
+      },
+    });
+  });
+});
---
+++ packages/core/client/src/modules/blocks/data-blocks/details-single/__tests__/createDetailsBlockWithoutPagingUISchema.test.ts
@@ -0,0 +1,82 @@
+import { createDetailsBlockWithoutPagingUISchema } from '../createDetailsBlockWIthoutPagingUISchema';
+
+// Mock uid to always return a fixed value
+vi.mock('@formily/shared', () => ({
+  uid: () => 'fixed-uid',
+}));
+
+describe('createDetailsBlockWithoutPagingUISchema', () => {
+  it('should throw an error when collectionName is missing', () => {
+    const options = { dataSource: 'someDataSource' };
+
+    // @ts-ignore
+    expect(() => createDetailsBlockWithoutPagingUISchema(options)).toThrowError(
+      'collectionName and dataSource are required',
+    );
+  });
+
+  it('should throw an error when dataSource is missing', () => {
+    const options = { collectionName: 'someCollectionName' };
+
+    // @ts-ignore
+    expect(() => createDetailsBlockWithoutPagingUISchema(options)).toThrowError(
+      'collectionName and dataSource are required',
+    );
+  });
+
+  it('should create a valid schema with mandatory fields', () => {
+    const options = {
+      collectionName: 'users',
+      dataSource: 'usersDataSource',
+    };
+
+    const result = createDetailsBlockWithoutPagingUISchema(options);
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "properties": {
+          "fixed-uid": {
+            "properties": {
+              "fixed-uid": {
+                "properties": {},
+                "type": "void",
+                "x-component": "ActionBar",
+                "x-component-props": {
+                  "style": {
+                    "marginBottom": 24,
+                  },
+                },
+                "x-initializer": "details:configureActions",
+              },
+              "grid": {
+                "properties": {},
+                "type": "void",
+                "x-component": "Grid",
+                "x-initializer": "details:configureFields",
+              },
+            },
+            "type": "void",
+            "x-component": "Details",
+            "x-component-props": {
+              "useProps": "{{ useDetailsBlockWithoutPagingProps }}",
+            },
+            "x-read-pretty": true,
+          },
+        },
+        "type": "void",
+        "x-acl-action": "users:get",
+        "x-component": "CardItem",
+        "x-decorator": "DetailsBlockProvider",
+        "x-decorator-props": {
+          "action": "list",
+          "association": undefined,
+          "collection": "users",
+          "dataSource": "usersDataSource",
+          "readPretty": true,
+        },
+        "x-settings": "blockSettings:singleDataDetails",
+        "x-toolbar": "BlockSchemaToolbar",
+        "x-use-decorator-props": "useDetailsBlockWithoutPagingDecoratorProps",
+      }
+    `);
+  });
+});
