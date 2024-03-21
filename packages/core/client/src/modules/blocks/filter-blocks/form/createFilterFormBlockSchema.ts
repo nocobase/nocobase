b6508008761ae15a70960df2a373464c9c9da69a
Zeke Zhang <958414905@qq.com>
2024-03-21 20:15:31 +0800

    refactor: migrate to modules

---
+++ packages/core/client/src/modules/blocks/filter-blocks/form/createFilterFormBlockSchema.ts
@@ -0,0 +1,60 @@
+import { ISchema } from '@formily/react';
+import { uid } from '@formily/shared';
+
+export const createFilterFormBlockSchema = (options: {
+  collectionName: string;
+  dataSource: string;
+  templateSchema?: ISchema;
+}) => {
+  const { collectionName, dataSource, templateSchema } = options;
+
+  if (!collectionName || !dataSource) {
+    throw new Error('collectionName and dataSource are required');
+  }
+
+  const schema: ISchema = {
+    type: 'void',
+    'x-decorator': 'FilterFormBlockProvider',
+    'x-decorator-props': {
+      dataSource,
+      collection: collectionName,
+    },
+    'x-toolbar': 'BlockSchemaToolbar',
+    'x-settings': 'blockSettings:filterForm',
+    'x-component': 'CardItem',
+    // 保存当前筛选区块所能过滤的数据区块
+    'x-filter-targets': [],
+    // 用于存储用户设置的每个字段的运算符，目前仅筛选表单区块支持自定义
+    'x-filter-operators': {},
+    properties: {
+      [uid()]: {
+        type: 'void',
+        'x-component': 'FormV2',
+        'x-component-props': {
+          useProps: '{{ useFormBlockProps }}',
+        },
+        properties: {
+          grid: templateSchema || {
+            type: 'void',
+            'x-component': 'Grid',
+            'x-initializer': 'filterForm:configureFields',
+            properties: {},
+          },
+          [uid()]: {
+            type: 'void',
+            'x-initializer': 'filterForm:configureActions',
+            'x-component': 'ActionBar',
+            'x-component-props': {
+              layout: 'one-column',
+              style: {
+                float: 'right',
+              },
+            },
+            properties: {},
+          },
+        },
+      },
+    },
+  };
+  return schema;
+};
