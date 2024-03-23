8514c626f1af51ac11d70e28415955be3c138c35
Zeke Zhang <958414905@qq.com>
2024-03-23 17:22:12 +0800

    chore: fix unit test

--- packages/core/client/src/modules/blocks/data-blocks/list/__tests__/createListBlockSchema.test.ts
+++ packages/core/client/src/modules/blocks/data-blocks/list/__tests__/createListBlockSchema.test.ts
@@ -22,80 +22,76 @@ describe('createListBlockSchema', () => {
 
     const schema = createListBlockSchema(options);
 
-    expect(schema).toEqual({
-      type: 'void',
-      'x-acl-action': 'user:view',
-      'x-decorator': 'List.Decorator',
-      'x-use-decorator-props': 'useListBlockDecoratorProps',
-      'x-decorator-props': {
-        collection: 'users',
-        dataSource: 'users',
-        association: 'user',
-        readPretty: true,
-        action: 'list',
-        params: {
-          pageSize: 10,
-        },
-        runWhenParamsChanged: true,
-        rowKey: 'id',
-      },
-      'x-component': 'CardItem',
-      'x-toolbar': 'BlockSchemaToolbar',
-      'x-settings': 'blockSettings:list',
-      properties: {
-        actionBar: {
-          type: 'void',
-          'x-initializer': 'list:configureActions',
-          'x-component': 'ActionBar',
-          'x-component-props': {
-            style: {
-              marginBottom: 'var(--nb-spacing)',
-            },
-          },
-          properties: {},
-        },
-        list: {
-          type: 'array',
-          'x-component': 'List',
-          'x-component-props': {
-            props: '{{ useListBlockProps }}',
-          },
-          properties: {
-            item: {
-              type: 'object',
-              'x-component': 'List.Item',
-              'x-read-pretty': true,
-              'x-component-props': {
-                useProps: '{{ useListItemProps }}',
-              },
-              properties: {
-                grid: {
-                  type: 'object',
-                  properties: {
-                    name: {
-                      type: 'string',
-                    },
-                    age: {
-                      type: 'number',
-                    },
-                  },
-                },
-                actionBar: {
-                  type: 'void',
-                  'x-align': 'left',
-                  'x-initializer': 'list:configureItemActions',
-                  'x-component': 'ActionBar',
-                  'x-component-props': {
-                    useProps: '{{ useListActionBarProps }}',
-                    layout: 'one-column',
-                  },
-                  properties: {},
-                },
+    expect(schema).toMatchInlineSnapshot(`
+      {
+        "properties": {
+          "actionBar": {
+            "type": "void",
+            "x-component": "ActionBar",
+            "x-component-props": {
+              "style": {
+                "marginBottom": "var(--nb-spacing)",
               },
             },
+            "x-initializer": "list:configureActions",
+          },
+          "list": {
+            "properties": {
+              "item": {
+                "properties": {
+                  "actionBar": {
+                    "type": "void",
+                    "x-align": "left",
+                    "x-component": "ActionBar",
+                    "x-component-props": {
+                      "layout": "one-column",
+                    },
+                    "x-initializer": "list:configureItemActions",
+                    "x-use-component-props": "useListActionBarProps",
+                  },
+                  "grid": {
+                    "properties": {
+                      "age": {
+                        "type": "number",
+                      },
+                      "name": {
+                        "type": "string",
+                      },
+                    },
+                    "type": "object",
+                  },
+                },
+                "type": "object",
+                "x-component": "List.Item",
+                "x-read-pretty": true,
+                "x-use-component-props": "useListItemProps",
+              },
+            },
+            "type": "array",
+            "x-component": "List",
+            "x-use-component-props": "useListBlockProps",
           },
         },
-      },
-    });
+        "type": "void",
+        "x-acl-action": "user:view",
+        "x-component": "CardItem",
+        "x-decorator": "List.Decorator",
+        "x-decorator-props": {
+          "action": "list",
+          "association": "user",
+          "collection": "users",
+          "dataSource": "users",
+          "params": {
+            "pageSize": 10,
+          },
+          "readPretty": true,
+          "rowKey": "id",
+          "runWhenParamsChanged": true,
+        },
+        "x-settings": "blockSettings:list",
+        "x-toolbar": "BlockSchemaToolbar",
+        "x-use-decorator-props": "useListBlockDecoratorProps",
+      }
+    `);
   });
 });
