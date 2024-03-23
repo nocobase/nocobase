de1646e9d2785bf9cd90424b4d5ba545a09c6582
mytharcher <mytharcher@gmail.com>
2024-03-23 03:24:04 +0000

    fix(plugin-workflow): fix client cycling import

--- packages/plugins/@nocobase/plugin-workflow/src/client/FlowContext.tsx
+++ packages/plugins/@nocobase/plugin-workflow/src/client/FlowContext.tsx
@@ -1,38 +1,7 @@
-import { SchemaComponentOptions, usePlugin } from '@nocobase/client';
 import React, { useContext } from 'react';
-
-import PluginWorkflowClient from '.';
 
 export const FlowContext = React.createContext<any>({});
 
 export function useFlowContext() {
   return useContext(FlowContext);
-}
-
-export function ExecutionContextProvider({ children, workflow, execution, nodes }) {
-  const workflowPlugin = usePlugin(PluginWorkflowClient);
-  const triggerComponents = workflowPlugin.triggers.get(workflow.type).components;
-  const nodeComponents = nodes.reduce(
-    (components, { type }) => Object.assign(components, workflowPlugin.instructions.get(type).components),
-    {},
-  );
-
-  return (
-    <FlowContext.Provider
-      value={{
-        workflow,
-        nodes,
-        execution,
-      }}
-    >
-      <SchemaComponentOptions
-        components={{
-          ...triggerComponents,
-          ...nodeComponents,
-        }}
-      >
-        {children}
-      </SchemaComponentOptions>
-    </FlowContext.Provider>
-  );
 }
--- packages/plugins/@nocobase/plugin-workflow/src/client/index.tsx
+++ packages/plugins/@nocobase/plugin-workflow/src/client/index.tsx
@@ -1,15 +1,3 @@
-export * from './Branch';
-export * from './FlowContext';
-export * from './constants';
-export * from './nodes';
-export { Trigger, useTrigger } from './triggers';
-export * from './variable';
-export * from './components';
-export * from './utils';
-export * from './hooks';
-export { default as useStyles } from './style';
-export * from './variable';
-
 import React from 'react';
 
 import { Plugin } from '@nocobase/client';
@@ -113,3 +101,16 @@ export default class PluginWorkflowClient extends Plugin {
     });
   }
 }
+
+export * from './Branch';
+export * from './FlowContext';
+export * from './constants';
+export * from './nodes';
+export { Trigger, useTrigger } from './triggers';
+export * from './variable';
+export * from './components';
+export * from './utils';
+export * from './hooks';
+export { default as useStyles } from './style';
+export * from './variable';
+export * from './ExecutionContextProvider';
