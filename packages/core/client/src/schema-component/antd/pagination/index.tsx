6694ffe27a35aec13415a2202bb4199680d5349b
Zeke Zhang <958414905@qq.com>
2024-03-24 11:21:41 +0800

    chore: fix Pagination

--- packages/core/client/src/schema-component/antd/pagination/index.tsx
+++ packages/core/client/src/schema-component/antd/pagination/index.tsx
@@ -5,24 +5,26 @@ import { useProps } from '../../hooks/useProps';
 import { withDynamicSchemaProps } from '../../../application/hoc/withDynamicSchemaProps';
 
 export const Pagination = withDynamicSchemaProps(
-  observer((props: any) => {
-    // 新版 UISchema（1.0 之后）中已经废弃了 useProps，这里之所以继续保留是为了兼容旧版的 UISchema
-    const { hidden, ...others } = useProps(props);
+  observer(
+    (props: any) => {
+      // 新版 UISchema（1.0 之后）中已经废弃了 useProps，这里之所以继续保留是为了兼容旧版的 UISchema
+      const { hidden, ...others } = useProps(props);
 
-    if (hidden) {
-      return null;
-    }
-    const onKeypress: KeyboardEventHandler<HTMLDivElement> = (e) => {
-      if (e.key === 'Enter') {
-        e.preventDefault();
+      if (hidden) {
+        return null;
       }
-    };
+      const onKeypress: KeyboardEventHandler<HTMLDivElement> = (e) => {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+        }
+      };
 
-    return (
-      <div onKeyPress={onKeypress}>
-        <AntdPagination {...others} />
-      </div>
-    );
-  }),
-  { displayName: 'Pagination' },
+      return (
+        <div onKeyPress={onKeypress}>
+          <AntdPagination {...others} />
+        </div>
+      );
+    },
+    { displayName: 'Pagination' },
+  ),
 );
