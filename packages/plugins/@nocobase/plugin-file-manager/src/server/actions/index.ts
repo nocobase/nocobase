e410dece222f1e64cbf8b9b60769022de1977b3f
ChengLei Shao <chareice@live.com>
2024-03-24 19:08:53 +0800

    fix: create attachments middleware (#3794)

--- packages/plugins/@nocobase/plugin-file-manager/src/server/actions/index.ts
+++ packages/plugins/@nocobase/plugin-file-manager/src/server/actions/index.ts
@@ -2,7 +2,7 @@ import actions from '@nocobase/actions';
 import { createMiddleware, destroyMiddleware } from './attachments';
 
 export default function ({ app }) {
-  app.resourcer.use(createMiddleware);
+  app.resourcer.use(createMiddleware, { tag: 'createMiddleware', after: 'auth' });
   app.resourcer.registerActionHandler('upload', actions.create);
 
   app.resourcer.use(destroyMiddleware);
