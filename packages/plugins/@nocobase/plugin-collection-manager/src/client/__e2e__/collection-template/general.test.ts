b55635100759d6d8dbdff26479003a6e623d18e7
katherinehhh <katherine_15995@163.com>
2024-03-27 21:43:14 +0800

    test: main data source e2e test

--- packages/plugins/@nocobase/plugin-collection-manager/src/client/__e2e__/collection-template/general.test.ts
+++ packages/plugins/@nocobase/plugin-collection-manager/src/client/__e2e__/collection-template/general.test.ts
@@ -99,54 +99,54 @@ test.describe('create collection', () => {
 
 // //预设字段
 test.describe('create collection with preset fields', () => {
-  // test('all preset fields by default', async ({ page }) => {
-  //   await page.goto('/admin/settings/data-source-manager/list');
-  //   await page.getByRole('button', { name: 'Configure' }).first().click();
-  //   await page.getByRole('button', { name: 'plus Create collection down' }).click();
-  //   await page.getByRole('menuitem', { name: 'General collection' }).locator('span').click();
-  //   await page.getByLabel('block-item-Input-collections-Collection display name').getByRole('textbox').fill(uid());
-  //   //断言提交的data是否符合预期
-  //   const [request] = await Promise.all([
-  //     page.waitForRequest((request) => request.url().includes('api/collections:create')),
-  //     page.getByLabel('action-Action-Submit').click(),
-  //   ]);
-  //   const postData = request.postDataJSON();
-  //   //默认提交的数据符合预期
-  //   expect(postData).toMatchObject({
-  //     autoGenId: true,
-  //     createdAt: true,
-  //     createdBy: true,
-  //     updatedAt: true,
-  //     updatedBy: true,
-  //     fields: expect.arrayContaining([
-  //       expect.objectContaining({
-  //         name: 'id',
-  //         type: 'bigInt',
-  //         // 其他属性
-  //       }),
-  //       expect.objectContaining({
-  //         name: 'createdAt',
-  //         type: 'date',
-  //         // 其他属性
-  //       }),
-  //       expect.objectContaining({
-  //         name: 'createdBy',
-  //         type: 'belongsTo',
-  //         // 其他属性
-  //       }),
-  //       expect.objectContaining({
-  //         name: 'updatedBy',
-  //         type: 'belongsTo',
-  //         // 其他属性
-  //       }),
-  //       expect.objectContaining({
-  //         name: 'updatedAt',
-  //         type: 'date',
-  //         // 其他属性
-  //       }),
-  //     ]),
-  //   });
-  // });
+  test('all preset fields by default', async ({ page }) => {
+    await page.goto('/admin/settings/data-source-manager/list');
+    await page.getByRole('button', { name: 'Configure' }).first().click();
+    await page.getByRole('button', { name: 'plus Create collection down' }).click();
+    await page.getByRole('menuitem', { name: 'General collection' }).locator('span').click();
+    await page.getByLabel('block-item-Input-collections-Collection display name').getByRole('textbox').fill(uid());
+    //断言提交的data是否符合预期
+    const [request] = await Promise.all([
+      page.waitForRequest((request) => request.url().includes('api/collections:create')),
+      page.getByLabel('action-Action-Submit').click(),
+    ]);
+    const postData = request.postDataJSON();
+    //默认提交的数据符合预期
+    expect(postData).toMatchObject({
+      autoGenId: true,
+      createdAt: true,
+      createdBy: true,
+      updatedAt: true,
+      updatedBy: true,
+      fields: expect.arrayContaining([
+        expect.objectContaining({
+          name: 'id',
+          type: 'bigInt',
+          // 其他属性
+        }),
+        expect.objectContaining({
+          name: 'createdAt',
+          type: 'date',
+          // 其他属性
+        }),
+        expect.objectContaining({
+          name: 'createdBy',
+          type: 'belongsTo',
+          // 其他属性
+        }),
+        expect.objectContaining({
+          name: 'updatedBy',
+          type: 'belongsTo',
+          // 其他属性
+        }),
+        expect.objectContaining({
+          name: 'updatedAt',
+          type: 'date',
+          // 其他属性
+        }),
+      ]),
+    });
+  });
 
   test('id preset field', async ({ page }) => {
     await page.goto('/admin/settings/data-source-manager/list');
