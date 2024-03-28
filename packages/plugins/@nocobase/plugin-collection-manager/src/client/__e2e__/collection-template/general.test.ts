dda4bdb6c6d7fec827d92d7c36f96406b7ab2e0a
katherinehhh <katherine_15995@163.com>
2024-03-28 11:42:25 +0800

    test: test

--- packages/plugins/@nocobase/plugin-collection-manager/src/client/__e2e__/collection-template/general.test.ts
+++ packages/plugins/@nocobase/plugin-collection-manager/src/client/__e2e__/collection-template/general.test.ts
@@ -154,8 +154,8 @@ test.describe('create collection with preset fields', () => {
     await page.getByRole('button', { name: 'plus Create collection down' }).click();
     await page.getByRole('menuitem', { name: 'General collection' }).locator('span').click();
     await page.getByLabel('block-item-Input-collections-Collection display name').getByRole('textbox').fill(uid());
-    await page.getByLabel('block-item-collections-Preset').getByLabel('Select all').hover();
-    await page.getByLabel('block-item-collections-Preset').getByLabel('Select all').click();
+    await page.locator('.ant-drawer-content-wrapper .ant-table-container .ant-table-selection-column').first().hover();
+    await page.locator('.ant-drawer-content-wrapper .ant-table-container .ant-table-selection-column').first().click();
     await page.getByRole('row', { name: 'ID Integer Primary key' }).locator('.ant-checkbox-input').check();
     //断言提交的data是否符合预期
     const [request] = await Promise.all([
@@ -179,149 +179,154 @@ test.describe('create collection with preset fields', () => {
       ]),
     });
   });
-  // test('createdAt preset field', async ({ page }) => {
-  //   await page.goto('/admin/settings/data-source-manager/list');
-  //   await page.getByRole('button', { name: 'Configure' }).first().click();
-  //   await page.getByRole('button', { name: 'plus Create collection down' }).click();
-  //   await page.getByRole('menuitem', { name: 'General collection' }).locator('span').click();
-  //   await page.getByLabel('block-item-Input-collections-Collection display name').getByRole('textbox').fill(uid());
-  //   await page.getByLabel('block-item-collections-Preset').getByLabel('Select all').uncheck();
-  //   await page.getByRole('row', { name: 'Created at' }).locator('.ant-checkbox-input').check();
-  //   //断言提交的data是否符合预期
-  //   const [request] = await Promise.all([
-  //     page.waitForRequest((request) => request.url().includes('api/collections:create')),
-  //     page.getByLabel('action-Action-Submit').click(),
-  //   ]);
-  //   const postData = request.postDataJSON();
-  //   //提交的数据符合预期
-  //   expect(postData).toMatchObject({
-  //     autoGenId: false,
-  //     createdAt: true,
-  //     createdBy: false,
-  //     updatedAt: false,
-  //     updatedBy: false,
-  //     fields: expect.arrayContaining([
-  //       expect.objectContaining({
-  //         name: 'createdAt',
-  //         type: 'date',
-  //         // 其他属性
-  //       }),
-  //     ]),
-  //   });
-  // });
-  // test('createdBy preset field', async ({ page }) => {
-  //   await page.goto('/admin/settings/data-source-manager/list');
-  //   await page.getByRole('button', { name: 'Configure' }).first().click();
-  //   await page.getByRole('button', { name: 'plus Create collection down' }).click();
-  //   await page.getByRole('menuitem', { name: 'General collection' }).locator('span').click();
-  //   await page.getByLabel('block-item-Input-collections-Collection display name').getByRole('textbox').fill(uid());
-  //   await page.getByLabel('block-item-collections-Preset').getByLabel('Select all').uncheck();
-  //   await page.getByRole('row', { name: 'created By' }).locator('.ant-checkbox-input').check();
-  //   //断言提交的data是否符合预期
-  //   const [request] = await Promise.all([
-  //     page.waitForRequest((request) => request.url().includes('api/collections:create')),
-  //     page.getByLabel('action-Action-Submit').click(),
-  //   ]);
-  //   const postData = request.postDataJSON();
-  //   //提交的数据符合预期
-  //   expect(postData).toMatchObject({
-  //     autoGenId: false,
-  //     createdAt: false,
-  //     createdBy: true,
-  //     updatedAt: false,
-  //     updatedBy: false,
-  //     fields: expect.arrayContaining([
-  //       expect.objectContaining({
-  //         name: 'createdBy',
-  //         type: 'belongsTo',
-  //         // 其他属性
-  //       }),
-  //     ]),
-  //   });
-  // });
-  // test('updatedBy preset field', async ({ page }) => {
-  //   await page.goto('/admin/settings/data-source-manager/list');
-  //   await page.getByRole('button', { name: 'Configure' }).first().click();
-  //   await page.getByRole('button', { name: 'plus Create collection down' }).click();
-  //   await page.getByRole('menuitem', { name: 'General collection' }).locator('span').click();
-  //   await page.getByLabel('block-item-Input-collections-Collection display name').getByRole('textbox').fill(uid());
-  //   await page.getByLabel('block-item-collections-Preset').getByLabel('Select all').uncheck();
-  //   await page.getByRole('row', { name: 'Last updated by' }).locator('.ant-checkbox-input').check();
-  //   //添加关系字段,断言提交的data是否符合预期
-  //   const [request] = await Promise.all([
-  //     page.waitForRequest((request) => request.url().includes('api/collections:create')),
-  //     page.getByLabel('action-Action-Submit').click(),
-  //   ]);
-  //   const postData = request.postDataJSON();
-  //   //提交的数据符合预期
-  //   expect(postData).toMatchObject({
-  //     autoGenId: false,
-  //     createdAt: false,
-  //     createdBy: false,
-  //     updatedAt: false,
-  //     updatedBy: true,
-  //     fields: expect.arrayContaining([
-  //       expect.objectContaining({
-  //         name: 'updatedBy',
-  //         type: 'belongsTo',
-  //         // 其他属性
-  //       }),
-  //     ]),
-  //   });
-  // });
-  // test('updatedAt preset field', async ({ page }) => {
-  //   await page.goto('/admin/settings/data-source-manager/list');
-  //   await page.getByRole('button', { name: 'Configure' }).first().click();
-  //   await page.getByRole('button', { name: 'plus Create collection down' }).click();
-  //   await page.getByRole('menuitem', { name: 'General collection' }).locator('span').click();
-  //   await page.getByLabel('block-item-Input-collections-Collection display name').getByRole('textbox').fill(uid());
-  //   await page.getByLabel('block-item-collections-Preset').getByLabel('Select all').uncheck();
-  //   await page.getByRole('row', { name: 'Last updated at' }).locator('.ant-checkbox-input').check();
-  //   //添加关系字段,断言提交的data是否符合预期
-  //   const [request] = await Promise.all([
-  //     page.waitForRequest((request) => request.url().includes('api/collections:create')),
-  //     page.getByLabel('action-Action-Submit').click(),
-  //   ]);
-  //   const postData = request.postDataJSON();
-  //   //提交的数据符合预期
-  //   expect(postData).toMatchObject({
-  //     autoGenId: false,
-  //     createdAt: false,
-  //     createdBy: false,
-  //     updatedAt: true,
-  //     updatedBy: false,
-  //     fields: expect.arrayContaining([
-  //       expect.objectContaining({
-  //         name: 'updatedAt',
-  //         type: 'date',
-  //         // 其他属性
-  //       }),
-  //     ]),
-  //   });
-  // });
-  // test('unselect preset fields', async ({ page }) => {
-  //   await page.goto('/admin/settings/data-source-manager/list');
-  //   await page.getByRole('button', { name: 'Configure' }).first().click();
-  //   await page.getByRole('button', { name: 'plus Create collection down' }).click();
-  //   await page.getByRole('menuitem', { name: 'General collection' }).locator('span').click();
-  //   await page.getByLabel('block-item-Input-collections-Collection display name').getByRole('textbox').fill(uid());
-  //   await page.getByLabel('block-item-collections-Preset').getByLabel('Select all').uncheck();
-  //   //断言提交的data是否符合预期
-  //   const [request] = await Promise.all([
-  //     page.waitForRequest((request) => request.url().includes('api/collections:create')),
-  //     page.getByLabel('action-Action-Submit').click(),
-  //   ]);
-  //   const postData = request.postDataJSON();
-  //   //提交的数据符合预期
-  //   expect(postData).toMatchObject({
-  //     autoGenId: false,
-  //     createdAt: false,
-  //     createdBy: false,
-  //     updatedAt: false,
-  //     updatedBy: false,
-  //     fields: [],
-  //   });
-  // });
+  test('createdAt preset field', async ({ page }) => {
+    await page.goto('/admin/settings/data-source-manager/list');
+    await page.getByRole('button', { name: 'Configure' }).first().click();
+    await page.getByRole('button', { name: 'plus Create collection down' }).click();
+    await page.getByRole('menuitem', { name: 'General collection' }).locator('span').click();
+    await page.getByLabel('block-item-Input-collections-Collection display name').getByRole('textbox').fill(uid());
+    await page.locator('.ant-drawer-content-wrapper .ant-table-container .ant-table-selection-column').first().hover();
+    await page.locator('.ant-drawer-content-wrapper .ant-table-container .ant-table-selection-column').first().click();
+    await page.getByRole('row', { name: 'Created at' }).locator('.ant-checkbox-input').check();
+    //断言提交的data是否符合预期
+    const [request] = await Promise.all([
+      page.waitForRequest((request) => request.url().includes('api/collections:create')),
+      page.getByLabel('action-Action-Submit').click(),
+    ]);
+    const postData = request.postDataJSON();
+    //提交的数据符合预期
+    expect(postData).toMatchObject({
+      autoGenId: false,
+      createdAt: true,
+      createdBy: false,
+      updatedAt: false,
+      updatedBy: false,
+      fields: expect.arrayContaining([
+        expect.objectContaining({
+          name: 'createdAt',
+          type: 'date',
+          // 其他属性
+        }),
+      ]),
+    });
+  });
+  test('createdBy preset field', async ({ page }) => {
+    await page.goto('/admin/settings/data-source-manager/list');
+    await page.getByRole('button', { name: 'Configure' }).first().click();
+    await page.getByRole('button', { name: 'plus Create collection down' }).click();
+    await page.getByRole('menuitem', { name: 'General collection' }).locator('span').click();
+    await page.getByLabel('block-item-Input-collections-Collection display name').getByRole('textbox').fill(uid());
+    await page.locator('.ant-drawer-content-wrapper .ant-table-container .ant-table-selection-column').first().hover();
+    await page.locator('.ant-drawer-content-wrapper .ant-table-container .ant-table-selection-column').first().click();
+    await page.getByRole('row', { name: 'created By' }).locator('.ant-checkbox-input').check();
+    //断言提交的data是否符合预期
+    const [request] = await Promise.all([
+      page.waitForRequest((request) => request.url().includes('api/collections:create')),
+      page.getByLabel('action-Action-Submit').click(),
+    ]);
+    const postData = request.postDataJSON();
+    //提交的数据符合预期
+    expect(postData).toMatchObject({
+      autoGenId: false,
+      createdAt: false,
+      createdBy: true,
+      updatedAt: false,
+      updatedBy: false,
+      fields: expect.arrayContaining([
+        expect.objectContaining({
+          name: 'createdBy',
+          type: 'belongsTo',
+          // 其他属性
+        }),
+      ]),
+    });
+  });
+  test('updatedBy preset field', async ({ page }) => {
+    await page.goto('/admin/settings/data-source-manager/list');
+    await page.getByRole('button', { name: 'Configure' }).first().click();
+    await page.getByRole('button', { name: 'plus Create collection down' }).click();
+    await page.getByRole('menuitem', { name: 'General collection' }).locator('span').click();
+    await page.getByLabel('block-item-Input-collections-Collection display name').getByRole('textbox').fill(uid());
+    await page.locator('.ant-drawer-content-wrapper .ant-table-container .ant-table-selection-column').first().hover();
+    await page.locator('.ant-drawer-content-wrapper .ant-table-container .ant-table-selection-column').first().click();
+    await page.getByRole('row', { name: 'Last updated by' }).locator('.ant-checkbox-input').check();
+    //添加关系字段,断言提交的data是否符合预期
+    const [request] = await Promise.all([
+      page.waitForRequest((request) => request.url().includes('api/collections:create')),
+      page.getByLabel('action-Action-Submit').click(),
+    ]);
+    const postData = request.postDataJSON();
+    //提交的数据符合预期
+    expect(postData).toMatchObject({
+      autoGenId: false,
+      createdAt: false,
+      createdBy: false,
+      updatedAt: false,
+      updatedBy: true,
+      fields: expect.arrayContaining([
+        expect.objectContaining({
+          name: 'updatedBy',
+          type: 'belongsTo',
+          // 其他属性
+        }),
+      ]),
+    });
+  });
+  test('updatedAt preset field', async ({ page }) => {
+    await page.goto('/admin/settings/data-source-manager/list');
+    await page.getByRole('button', { name: 'Configure' }).first().click();
+    await page.getByRole('button', { name: 'plus Create collection down' }).click();
+    await page.getByRole('menuitem', { name: 'General collection' }).locator('span').click();
+    await page.getByLabel('block-item-Input-collections-Collection display name').getByRole('textbox').fill(uid());
+    await page.locator('.ant-drawer-content-wrapper .ant-table-container .ant-table-selection-column').first().hover();
+    await page.locator('.ant-drawer-content-wrapper .ant-table-container .ant-table-selection-column').first().click();
+    await page.getByRole('row', { name: 'Last updated at' }).locator('.ant-checkbox-input').check();
+    //添加关系字段,断言提交的data是否符合预期
+    const [request] = await Promise.all([
+      page.waitForRequest((request) => request.url().includes('api/collections:create')),
+      page.getByLabel('action-Action-Submit').click(),
+    ]);
+    const postData = request.postDataJSON();
+    //提交的数据符合预期
+    expect(postData).toMatchObject({
+      autoGenId: false,
+      createdAt: false,
+      createdBy: false,
+      updatedAt: true,
+      updatedBy: false,
+      fields: expect.arrayContaining([
+        expect.objectContaining({
+          name: 'updatedAt',
+          type: 'date',
+          // 其他属性
+        }),
+      ]),
+    });
+  });
+  test('unselect preset fields', async ({ page }) => {
+    await page.goto('/admin/settings/data-source-manager/list');
+    await page.getByRole('button', { name: 'Configure' }).first().click();
+    await page.getByRole('button', { name: 'plus Create collection down' }).click();
+    await page.getByRole('menuitem', { name: 'General collection' }).locator('span').click();
+    await page.getByLabel('block-item-Input-collections-Collection display name').getByRole('textbox').fill(uid());
+    await page.locator('.ant-drawer-content-wrapper .ant-table-container .ant-table-selection-column').first().hover();
+    await page.locator('.ant-drawer-content-wrapper .ant-table-container .ant-table-selection-column').first().click();
+    //断言提交的data是否符合预期
+    const [request] = await Promise.all([
+      page.waitForRequest((request) => request.url().includes('api/collections:create')),
+      page.getByLabel('action-Action-Submit').click(),
+    ]);
+    const postData = request.postDataJSON();
+    //提交的数据符合预期
+    expect(postData).toMatchObject({
+      autoGenId: false,
+      createdAt: false,
+      createdBy: false,
+      updatedAt: false,
+      updatedBy: false,
+      fields: [],
+    });
+  });
 });
 
 test.describe('configure fields', () => {
