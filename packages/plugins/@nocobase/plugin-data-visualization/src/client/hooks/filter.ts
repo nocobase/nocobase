868a487b2d2a24475a29aa72dbad1e27851ffeec
YANG QIA <2013xile@gmail.com>
2024-03-29 14:13:59 +0800

    fix(data-vi): bug when filtering chart with assocations (#3867)
    * fix(data-vi): bug when filtering chart with assocations
    
    * fix: remote select

--- packages/plugins/@nocobase/plugin-data-visualization/src/client/hooks/filter.ts
+++ packages/plugins/@nocobase/plugin-data-visualization/src/client/hooks/filter.ts
@@ -305,12 +305,15 @@ export const useChartFilter = () => {
       .filter(([_, props]) => props)
       .forEach(([name, props]) => {
         const { operator } = props || {};
-        const { dataSource, fieldName } = parseFilterFieldName(name);
+        const { dataSource, fieldName: _fieldName } = parseFilterFieldName(name);
+        let fieldName = _fieldName;
         const ds = dm.getDataSource(dataSource);
         const cm = ds.collectionManager;
         const field = cm.getCollectionField(fieldName);
         if (field?.target) {
-          name = `${fieldName}.${field.targetKey || 'id'}`;
+          const tk = field.targetKey || 'id';
+          fieldName = `${fieldName}.${tk}`;
+          name = `${name}.${tk}`;
         }
         const [collection, ...fields] = fieldName.split('.');
         const value = getValuesByPath(values, name);
