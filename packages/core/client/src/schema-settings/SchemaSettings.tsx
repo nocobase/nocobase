eb2b69028cc805a62855538077a5b25001e950b3
chenos <chenlinxh@gmail.com>
2024-03-29 12:14:10 +0800

    fix(client): the reference template was deleted by mistake

--- packages/core/client/src/schema-settings/SchemaSettings.tsx
+++ packages/core/client/src/schema-settings/SchemaSettings.tsx
@@ -42,11 +42,11 @@ import {
   ActionContextProvider,
   AssociationOrCollectionProvider,
   CollectionFieldOptions_deprecated,
+  CollectionRecordProvider,
   DataSourceApplicationProvider,
   Designable,
   FormDialog,
   FormProvider,
-  CollectionRecordProvider,
   RemoteSchemaComponent,
   SchemaComponent,
   SchemaComponentContext,
@@ -56,6 +56,7 @@ import {
   useAPIClient,
   useBlockRequestContext,
   useCollectionManager_deprecated,
+  useCollectionRecord,
   useCollection_deprecated,
   useCompile,
   useDataBlockProps,
@@ -63,7 +64,6 @@ import {
   useFilterBlock,
   useGlobalTheme,
   useLinkageCollectionFilterOptions,
-  useCollectionRecord,
   useRecord,
   useSchemaSettingsItem,
   useSortFields,
@@ -82,9 +82,9 @@ import {
   useFormActiveFields,
 } from '../block-provider/hooks';
 import { SelectWithTitle, SelectWithTitleProps } from '../common/SelectWithTitle';
+import { useNiceDropdownMaxHeight } from '../common/useNiceDropdownHeight';
 import { useDataSourceManager } from '../data-source/data-source/DataSourceManagerProvider';
 import { useDataSourceKey } from '../data-source/data-source/DataSourceProvider';
-import { useNiceDropdownMaxHeight } from '../common/useNiceDropdownHeight';
 import {
   FilterBlockType,
   getSupportFieldsByAssociation,
@@ -572,7 +572,7 @@ export const SchemaSettingsRemove: FC<SchemaSettingsRemoveProps> = (props) => {
               field.required = false;
               fieldSchema['required'] = false;
             }
-            if (template && ctx?.dn) {
+            if (template && template.uid === fieldSchema['x-uid'] && ctx?.dn) {
               await ctx?.dn.remove(null, options);
             } else {
               await dn.remove(null, options);
