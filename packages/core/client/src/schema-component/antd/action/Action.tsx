5d19ef58dc47f79bce1492f448cf9c773dd0f078
jack zhang <1098626505@qq.com>
2024-03-30 20:31:11 +0800

    feat: table performance (#3791)
    * fix: table add useMemo and useCallback
    
    * fix: memo bug
    
    * fix: sub table bug
    
    * fix: form item performance
    
    * fix: settings center performance impove
    
    * fix: bug
    
    * fix: bug
    
    * fix: form first value change check performance
    
    * fix: revert first form change
    
    * fix: css move out component
    
    * fix: page change table should not render
    
    * fix: pre process merge bug
    
    * fix: assotion bug
    
    * fix: input and useDeppMemoized performance
    
    * fix: bug
    
    * fix: bug
    
    * fix: improve Action.tsx lazy show content
    
    * fix: remove Action performance imporve
    
    * fix: assocication read pretty not loading
    
    * fix: cssInJs imporve
    
    * fix: imporve kanban rerender
    
    * fix: remove useless CurrentAppInfoProvider in plugin
    
    * fix: divide the schema into several parts
    
    * fix: tabs.tsx and Page.tsx divide
    
    * fix: form-item imporve
    
    * fix: add OverrideSchemaComponentRefresher
    
    * fix: page and tabs bug
    
    * fix: workflow bug
    
    * fix: remove useDeepMemorized()
    
    * fix: e2e bug
    
    * fix: internal Tag and viewer
    
    * fix: collection field read pretty mode skip
    
    * fix: others performance
    
    * fix: revert collection field read pretty
    
    * fix: table column not render when value is null or undefined
    
    * fix: table and grid add view check
    
    * fix: kanban lazy render
    
    * fix: remove table useWhyDidYouUpdate
    
    * fix: table index skip loading
    
    * fix: card drag rerender loading
    
    * fix: e2e skip lazy render
    
    * fix: e2e bug
    
    * fix: action e2e bug
    
    * fix: grid and kanban card
    
    * fix: remove override refresher component
    
    * fix: unit test bug
    
    * fix: change schema component props name
    
    * fix: e2e and unit test bug
    
    * fix: e2e bug
    
    * fix: not lazy render when data length less 10 (T-3784)
    
    * chore: fix merge
    
    * chore: fix e2e
    
    * fix: drag bug (T-3807)
    
    * fix: repetitive refresh (T-3729)
    
    * fix: pre fix merge confict
    
    ---------
    
    Co-authored-by: Zeke Zhang <958414905@qq.com>

--- packages/core/client/src/schema-component/antd/action/Action.tsx
+++ packages/core/client/src/schema-component/antd/action/Action.tsx
@@ -3,7 +3,7 @@ import { isPortalInBody } from '@nocobase/utils/client';
 import { App, Button } from 'antd';
 import classnames from 'classnames';
 import { default as lodash } from 'lodash';
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { StablePopover, useActionContext } from '../..';
 import { useDesignable } from '../../';
@@ -46,6 +46,7 @@ export const Action: ComposedAction = observer(
       actionCallback,
       /** 如果为 true 则说明该按钮是树表格的 Add child 按钮 */
       addChild,
+      onMouseEnter,
       ...others
     } = useProps(props);
     const aclCtx = useACLActionParamsContext();
@@ -65,17 +66,28 @@ export const Action: ComposedAction = observer(
     const openSize = fieldSchema?.['x-component-props']?.['openSize'];
 
     const disabled = form.disabled || field.disabled || field.data?.disabled || propsDisabled;
-    const linkageRules = fieldSchema?.['x-linkage-rules'] || [];
+    const linkageRules = useMemo(() => fieldSchema?.['x-linkage-rules'] || [], [fieldSchema?.['x-linkage-rules']]);
     const { designable } = useDesignable();
     const tarComponent = useComponent(component) || component;
     const { modal } = App.useApp();
     const variables = useVariables();
     const localVariables = useLocalVariables({ currentForm: { values: record } as any });
     const { getAriaLabel } = useGetAriaLabelOfAction(title);
-    let actionTitle = title || compile(fieldSchema.title);
-    actionTitle = lodash.isString(actionTitle) ? t(actionTitle) : actionTitle;
+    const [btnHover, setBtnHover] = useState(popover);
 
     useEffect(() => {
+      if (popover) {
+        setBtnHover(true);
+      }
+    }, [popover]);
+
+    const actionTitle = useMemo(() => {
+      const res = title || compile(fieldSchema.title);
+      return lodash.isString(res) ? t(res) : res;
+    }, [title, fieldSchema.title, t]);
+
+    useEffect(() => {
+      if (!btnHover) return;
       field.stateOfLinkageRules = {};
       linkageRules
         .filter((k) => !k.disabled)
@@ -90,13 +102,14 @@ export const Action: ComposedAction = observer(
             });
           });
         });
-    }, [field, linkageRules, localVariables, record, variables]);
+    }, [btnHover, field, linkageRules, localVariables, record, variables]);
 
     const handleButtonClick = useCallback(
       (e: React.MouseEvent) => {
         if (isPortalInBody(e.target as Element)) {
           return;
         }
+        setBtnHover(true);
 
         e.preventDefault();
         e.stopPropagation();
@@ -128,6 +141,13 @@ export const Action: ComposedAction = observer(
       };
     }, [designable, field?.data?.hidden, style]);
 
+    const handleMouseEnter = useCallback(
+      (e) => {
+        setBtnHover(true);
+        onMouseEnter?.(e);
+      },
+      [onMouseEnter],
+    );
     const renderButton = () => {
       if (!designable && (field?.data?.hidden || !aclCtx)) {
         return null;
@@ -138,6 +158,7 @@ export const Action: ComposedAction = observer(
           role="button"
           aria-label={getAriaLabel()}
           {...others}
+          onMouseEnter={handleMouseEnter}
           loading={field?.data?.loading}
           icon={icon ? <Icon type={icon} /> : null}
           disabled={disabled}
@@ -152,9 +173,16 @@ export const Action: ComposedAction = observer(
         </SortableItem>
       );
     };
+
+    const buttonElement = renderButton();
+
+    // if (!btnHover) {
+    //   return buttonElement;
+    // }
+
     const result = (
       <ActionContextProvider
-        button={renderButton()}
+        button={buttonElement}
         visible={visible}
         setVisible={setVisible}
         formValueChanged={formValueChanged}
