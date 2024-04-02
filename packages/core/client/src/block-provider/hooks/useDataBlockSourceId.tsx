c057766ccb9d830b3502532602bd600df9e01e3b
Zeke Zhang <958414905@qq.com>
2024-04-02 10:00:07 +0800

    chore: update comment

--- packages/core/client/src/block-provider/hooks/useDataBlockSourceId.tsx
+++ packages/core/client/src/block-provider/hooks/useDataBlockSourceId.tsx
@@ -11,6 +11,9 @@ import {
  * 注意：这里有一个需要更改 schema 才能解决的问题，就是在获取 sourceId 的时候无法确定（在关系字段和当前表同表时）
  * 是需要从 recordData 还是 parentRecordData 中获取;解决方法是通过更改 schema，在不同类型的关系区块中
  * （`通过点击关系字段按钮打开的弹窗中创建的非关系字段区块`和`关系字段区块`）使用不同的 hook。
+ *
+ * 更新：上面所说的“需要更改 schema 才能解决的问题”已在这个任务中更改：https://nocobase.height.app/T-3848/description
+ *
  * @param param0
  * @returns
  */
