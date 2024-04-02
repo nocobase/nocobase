59c124bedb90fcf280bd6aa172c121c4938b1686
Zeke Zhang <958414905@qq.com>
2024-04-02 10:19:17 +0800

    fix: get the correct sourceId

--- packages/core/client/src/modules/blocks/data-blocks/details-single/hooks/useDetailsByAssociationRecordDecoratorProps.ts
+++ packages/core/client/src/modules/blocks/data-blocks/details-single/hooks/useDetailsByAssociationRecordDecoratorProps.ts
@@ -1,5 +1,7 @@
+import { useCollectionRecordData } from '../../../../../data-source/collection-record/CollectionRecordProvider';
 import { useParamsFromRecord } from '../../../../../block-provider/BlockProvider';
-import { useDataBlockSourceId } from '../../../../../block-provider/hooks/useDataBlockSourceId';
+import { useCollectionManager } from '../../../../../data-source/collection/CollectionManagerProvider';
+import { InheritanceCollectionMixin } from '../../../../../collection-manager/mixins/InheritanceCollectionMixin';
 
 /**
  * 应用在通过 Association records 选项创建的区块中（弹窗中的 Add block 菜单）
@@ -13,7 +15,7 @@ export const useDetailsByAssociationRecordDecoratorProps = (props) => {
   // association 的值是固定不变的，所以可以在条件中使用 hooks
   if (props.association) {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    sourceId = useDataBlockSourceId({ association: props.association });
+    sourceId = useSourceId(props.association);
   }
 
   return {
@@ -21,3 +23,17 @@ export const useDetailsByAssociationRecordDecoratorProps = (props) => {
     sourceId,
   };
 };
+
+function useSourceId(association: string) {
+  const recordData = useCollectionRecordData();
+  const cm = useCollectionManager();
+
+  if (!association) return;
+
+  const associationField = cm.getCollectionField(association);
+  const associationCollection = cm.getCollection<InheritanceCollectionMixin>(associationField.collectionName);
+
+  return recordData?.[
+    associationField.sourceKey || associationCollection.filterTargetKey || associationCollection.getPrimaryKey() || 'id'
+  ];
+}
--- packages/core/client/src/modules/blocks/data-blocks/details-single/hooks/useDetailsDecoratorProps.ts
+++ packages/core/client/src/modules/blocks/data-blocks/details-single/hooks/useDetailsDecoratorProps.ts
@@ -1,5 +1,7 @@
+import { useCollectionParentRecordData } from '../../../../../data-source/collection-record/CollectionRecordProvider';
 import { useParamsFromRecord } from '../../../../../block-provider/BlockProvider';
-import { useDataBlockSourceId } from '../../../../../block-provider/hooks/useDataBlockSourceId';
+import { useCollectionManager } from '../../../../../data-source/collection/CollectionManagerProvider';
+import { InheritanceCollectionMixin } from '../../../../../collection-manager/mixins/InheritanceCollectionMixin';
 
 /**
  * 应用在通过 Current record 选项创建的区块中（弹窗中的 Add block 菜单）
@@ -13,11 +15,27 @@ export function useDetailsDecoratorProps(props) {
   // association 的值是固定不变的，所以可以在条件中使用 hooks
   if (props.association) {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    sourceId = useDataBlockSourceId({ association: props.association });
+    sourceId = useSourceId(props.association);
   }
 
   return {
     params,
     sourceId,
   };
+}
+
+function useSourceId(association: string) {
+  const parentRecordData = useCollectionParentRecordData();
+  const cm = useCollectionManager();
+
+  // 通过 Current record 选项创建的区块，如果有 association，则说明该弹窗是通过直接点击一个关系字段打开的，
+  // 此时需要通过 parentRecordData 才能获取到正确的 sourceId
+  if (!association) return;
+
+  const associationField = cm.getCollectionField(association);
+  const associationCollection = cm.getCollection<InheritanceCollectionMixin>(associationField.collectionName);
+
+  return parentRecordData?.[
+    associationField.sourceKey || associationCollection.filterTargetKey || associationCollection.getPrimaryKey() || 'id'
+  ];
 }
