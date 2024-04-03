a7d852ccfb7f92f3fcb6a9603443db2657435109
Zeke Zhang <958414905@qq.com>
2024-04-03 19:07:53 +0800

    refactor: use parentRecord instead of sourceId

--- packages/core/client/src/block-provider/hooks/useDataBlockSourceId.tsx
+++ packages/core/client/src/block-provider/hooks/useDataBlockSourceId.tsx
@@ -5,14 +5,13 @@ import {
   useCollectionParentRecordData,
   useCollectionRecordData,
 } from '../..';
-import { useSourceKey } from '../../modules/blocks/useSourceKey';
 
 /**
  * @internal
  * @deprecated
- * 已弃用（该方法现在只是用来兼容旧版 Schema 的），请通过各个区块的 x-use-decorator-props 中获取 sourceId
+ * 已弃用（该方法现在只是用来兼容旧版 Schema 的），请通过各个区块的 x-use-decorator-props 中获取 parentRecord
  *
- * 注意：这里有一个需要更改 schema 才能解决的问题，就是在获取 sourceId 的时候无法确定（在关系字段和当前表同表时）
+ * 注意：这里有一个需要更改 schema 才能解决的问题，就是在获取 parentRecord 的时候无法确定（在关系字段和当前表同表时）
  * 是需要从 recordData 还是 parentRecordData 中获取;解决方法是通过更改 schema，在不同类型的关系区块中
  * （`通过点击关系字段按钮打开的弹窗中创建的非关系字段区块`和`关系字段区块`）使用不同的 hook。
  *
@@ -21,12 +20,11 @@ import { useSourceKey } from '../../modules/blocks/useSourceKey';
  * @param param0
  * @returns
  */
-export const useDataBlockSourceId = ({ association }: { association: string }) => {
+export const useDataBlockParentRecord = ({ association }: { association: string }) => {
   const recordData = useCollectionRecordData();
   const parentRecordData = useCollectionParentRecordData();
   const cm = useCollectionManager();
   const currentRecordCollection = useCollection<InheritanceCollectionMixin>();
-  const sourceKey = useSourceKey(association);
 
   if (!association) return;
 
@@ -36,8 +34,8 @@ export const useDataBlockSourceId = ({ association }: { association: string }) =
     currentRecordCollection.name === sourceCollection.name ||
     currentRecordCollection.getParentCollectionsName?.().includes(sourceCollection.name)
   ) {
-    return recordData?.[sourceKey];
+    return recordData;
   }
 
-  return parentRecordData?.[sourceKey];
+  return parentRecordData;
 };
--- packages/core/client/src/modules/blocks/data-blocks/details-multi/hooks/useDetailsWithPaginationDecoratorProps.ts
+++ packages/core/client/src/modules/blocks/data-blocks/details-multi/hooks/useDetailsWithPaginationDecoratorProps.ts
@@ -1,15 +1,15 @@
-import { useSourceIdCommon } from '../../../useSourceIdCommon';
+import { useParentRecordCommon } from '../../../useParentRecordCommon';
 
 export function useDetailsWithPaginationDecoratorProps(props) {
-  let sourceId;
+  let parentRecord;
 
   // association 的值是固定不变的，所以可以在条件中使用 hooks
   if (props.association) {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    sourceId = useSourceIdCommon(props.association);
+    parentRecord = useParentRecordCommon(props.association);
   }
 
   return {
-    sourceId,
+    parentRecord,
   };
 }
