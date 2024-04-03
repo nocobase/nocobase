f1c9468f6eae84432448718b6794c1560f9e96de
YANG QIA <2013xile@gmail.com>
2024-04-04 00:06:30 +0800

    feat(data-vi): allows to add charts for mobile client (#3922)
    * feat(data-vi): allows to add charts for mobile client
    
    * fix: remove foreign keys

--- packages/plugins/@nocobase/plugin-mobile-client/src/client/core/schema/initializers/BlockInitializers.ts
+++ packages/plugins/@nocobase/plugin-mobile-client/src/client/core/schema/initializers/BlockInitializers.ts
@@ -1,12 +1,8 @@
 import { CompatibleSchemaInitializer, gridRowColWrap } from '@nocobase/client';
 import { generateNTemplate } from '../../../locale';
 
-/**
- * @deprecated
- * use `mBlockInitializers` instead
- */
-export const mBlockInitializers_deprecated = new CompatibleSchemaInitializer({
-  name: 'MBlockInitializers',
+const mBlockInitializersOptions = {
+  name: 'mobilePage:addBlock',
   title: '{{t("Add block")}}',
   icon: 'PlusOutlined',
   wrap: gridRowColWrap,
@@ -47,6 +43,11 @@ export const mBlockInitializers_deprecated = new CompatibleSchemaInitializer({
           title: generateNTemplate('Map'),
           Component: 'MapBlockInitializer',
         },
+        {
+          name: 'charts',
+          title: '{{t("Charts")}}',
+          Component: 'ChartV2BlockInitializer',
+        },
       ],
     },
     {
@@ -72,76 +73,18 @@ export const mBlockInitializers_deprecated = new CompatibleSchemaInitializer({
       ],
     },
   ],
+};
+
+/**
+ * @deprecated
+ * use `mBlockInitializers` instead
+ */
+export const mBlockInitializers_deprecated = new CompatibleSchemaInitializer({
+  ...mBlockInitializersOptions,
+  name: 'MBlockInitializers',
 });
 
 export const mBlockInitializers = new CompatibleSchemaInitializer(
-  {
-    name: 'mobilePage:addBlock',
-    title: '{{t("Add block")}}',
-    icon: 'PlusOutlined',
-    wrap: gridRowColWrap,
-    items: [
-      {
-        name: 'dataBlocks',
-        type: 'itemGroup',
-        title: '{{t("Data blocks")}}',
-        children: [
-          {
-            name: 'gridCard',
-            type: 'item',
-            title: '{{t("Grid Card")}}',
-            Component: 'GridCardBlockInitializer',
-          },
-          {
-            name: 'table',
-            title: '{{t("Table")}}',
-            Component: 'TableBlockInitializer',
-          },
-          {
-            name: 'form',
-            title: '{{t("Form")}}',
-            Component: 'FormBlockInitializer',
-          },
-          {
-            name: 'details',
-            title: '{{t("Details")}}',
-            Component: 'DetailsBlockInitializer',
-          },
-          {
-            name: 'calendar',
-            title: '{{t("Calendar")}}',
-            Component: 'CalendarBlockInitializer',
-          },
-          {
-            name: 'mapBlock',
-            title: generateNTemplate('Map'),
-            Component: 'MapBlockInitializer',
-          },
-        ],
-      },
-      {
-        name: 'otherBlocks',
-        type: 'itemGroup',
-        title: '{{t("Other blocks")}}',
-        children: [
-          {
-            name: 'menu',
-            title: generateNTemplate('Menu'),
-            Component: 'MMenuBlockInitializer',
-          },
-          {
-            name: 'markdown',
-            title: '{{t("Markdown")}}',
-            Component: 'MarkdownBlockInitializer',
-          },
-          {
-            name: 'settings',
-            title: generateNTemplate('Settings'),
-            Component: 'MSettingsBlockInitializer',
-          },
-        ],
-      },
-    ],
-  },
+  mBlockInitializersOptions,
   mBlockInitializers_deprecated,
 );
