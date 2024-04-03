55c57b33664aace110be9b5328cf3641ace1fb6d
Zeke Zhang <958414905@qq.com>
2024-04-03 11:49:04 +0800

    refactor: refactor useParentRequest function to use fieldCollection for filterTargetKey

--- packages/core/client/src/data-source/data-block/DataBlockRequestProvider.tsx
+++ packages/core/client/src/data-source/data-block/DataBlockRequestProvider.tsx
@@ -58,10 +58,10 @@ function useParentRequest<T>(options: Omit<AllDataBlockProps, 'type'>) {
       const field = cm.getCollectionField(association);
       const fieldCollection = cm.getCollection(field.collectionName || arr[0]);
       const isM2O = field.interface === 'm2o';
-      const filterTargetKey = cm.getCollection(arr[0]).getOption('filterTargetKey');
+      const filterTargetKey = fieldCollection.getOption('filterTargetKey');
       const filterKey = isM2O
         ? filterTargetKey
-        : field.sourceKey || fieldCollection.filterTargetKey || fieldCollection.getPrimaryKey() || 'id';
+        : field.sourceKey || filterTargetKey || fieldCollection.getPrimaryKey() || 'id';
       // <collection>:get?filter[filterKey]=sourceId
       const url = `${arr[0]}:get?filter[${filterKey}]=${sourceId}`;
       const res = await api.request({ url, headers });
