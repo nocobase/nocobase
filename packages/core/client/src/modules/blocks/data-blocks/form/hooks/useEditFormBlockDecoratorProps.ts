2be065984c7daa5e67a5c7b19a4f12794e874569
chenos <chenlinxh@gmail.com>
2024-04-03 20:41:23 +0800

    fix: source id null (#3917)
    * fix: source id null
    
    * refactor: use parentRecord instead of sourceId
    
    ---------
    
    Co-authored-by: Zeke Zhang <958414905@qq.com>

--- packages/core/client/src/modules/blocks/data-blocks/form/hooks/useEditFormBlockDecoratorProps.ts
+++ packages/core/client/src/modules/blocks/data-blocks/form/hooks/useEditFormBlockDecoratorProps.ts
@@ -1,20 +1,20 @@
 import { useParamsFromRecord } from '../../../../../block-provider/BlockProvider';
-import { useDetailsSourceId } from '../../details-single/hooks/useDetailsDecoratorProps';
+import { useDetailsParentRecord } from '../../details-single/hooks/useDetailsDecoratorProps';
 
 export function useEditFormBlockDecoratorProps(props) {
   const params = useFormBlockParams();
-  let sourceId;
+  let parentRecord;
 
   // association 的值是固定不变的，所以这里可以使用 hooks
   if (props.association) {
     // 复用详情区块的 sourceId 获取逻辑
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    sourceId = useDetailsSourceId(props.association);
+    parentRecord = useDetailsParentRecord(props.association);
   }
 
   return {
     params,
-    sourceId,
+    parentRecord,
   };
 }
 
