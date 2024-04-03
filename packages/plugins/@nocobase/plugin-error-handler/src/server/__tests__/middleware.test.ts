27ec43224775bda40e2fdf94926322509ed29f10
Junyi <mytharcher@users.noreply.github.com>
2024-04-03 22:26:35 +0800

    test(plugin-error-handler): middleware (#3909)
    * test(plugin-error-handler-middleware): add test case
    
    * chore: test
    
    * chore: handle error throw via ctx.throw
    
    ---------
    
    Co-authored-by: Chareice <chareice@live.com>

---
+++ packages/plugins/@nocobase/plugin-error-handler/src/server/__tests__/middleware.test.ts
@@ -0,0 +1,64 @@
+import { Database } from '@nocobase/database';
+import { MockServer, createMockServer } from '@nocobase/test';
+import Plugin from '..';
+
+describe('middleware', () => {
+  let app: MockServer;
+  beforeEach(async () => {
+    app = await createMockServer({
+      acl: false,
+      plugins: ['error-handler'],
+    });
+  });
+
+  afterEach(async () => {
+    await app.destroy();
+  });
+
+  it('should handle not null error', async () => {
+    const collection = app.db.collection({
+      name: 'users',
+      fields: [
+        {
+          name: 'name',
+          type: 'string',
+        },
+      ],
+    });
+
+    await collection.sync();
+
+    class CustomError extends Error {
+      constructor(message) {
+        super(message);
+        this.name = 'CustomError';
+      }
+    }
+
+    (app.pm.get(Plugin) as Plugin).errorHandler.register(
+      (err) => err instanceof CustomError,
+      (err, ctx) => {
+        ctx.body = {
+          errors: [err.message],
+        };
+      },
+    );
+
+    app.use(
+      (ctx, next) => {
+        ctx.throw(400, new CustomError('custom error'));
+      },
+      { after: 'dataSource' },
+    );
+
+    const response = await app.agent().resource('users').create({
+      values: {},
+    });
+
+    expect(response.statusCode).toEqual(400);
+
+    expect(response.body).toEqual({
+      errors: ['custom error'],
+    });
+  });
+});
