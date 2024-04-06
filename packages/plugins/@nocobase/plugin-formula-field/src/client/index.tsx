d8e969b9aebfff2de1e956e3328e1fcdd8ef3841
katherinehhh <shunai.tang@hand-china.com>
2024-04-06 08:15:22 +0800

    fix: formula field support format (#3928)

--- packages/plugins/@nocobase/plugin-formula-field/src/client/index.tsx
+++ packages/plugins/@nocobase/plugin-formula-field/src/client/index.tsx
@@ -1,11 +1,13 @@
 import { Plugin } from '@nocobase/client';
 import { FormulaFieldProvider } from './FormulaFieldProvider';
 import { FormulaFieldInterface } from './interfaces/formula';
+import { FormulaComponentFieldSettings } from './FormulaComponentFieldSettings';
 
 export class FormulaFieldPlugin extends Plugin {
   async load() {
     this.app.use(FormulaFieldProvider);
     this.app.dataSourceManager.addFieldInterfaces([FormulaFieldInterface]);
+    this.app.schemaSettingsManager.add(FormulaComponentFieldSettings);
   }
 }
 
