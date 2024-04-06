20bec9faaa06993a4025678f6a33014f41431912
mytharcher <mytharcher@gmail.com>
2024-04-06 01:53:34 +0000

    fix(plugin-formula-field): fix component context

--- packages/plugins/@nocobase/plugin-formula-field/src/client/index.tsx
+++ packages/plugins/@nocobase/plugin-formula-field/src/client/index.tsx
@@ -1,10 +1,16 @@
 import { Plugin } from '@nocobase/client';
-import { FormulaFieldProvider } from './FormulaFieldProvider';
+import { Formula } from './components';
+import { renderExpressionDescription } from './scopes';
 import { FormulaFieldInterface } from './interfaces/formula';
 
 export class FormulaFieldPlugin extends Plugin {
   async load() {
-    this.app.use(FormulaFieldProvider);
+    this.app.addComponents({
+      Formula,
+    });
+    this.app.addScopes({
+      renderExpressionDescription,
+    });
     this.app.dataSourceManager.addFieldInterfaces([FormulaFieldInterface]);
   }
 }
