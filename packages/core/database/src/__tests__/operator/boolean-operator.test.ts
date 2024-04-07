1d2d4379daf9021b7a4d55c285cbb6881e70bfd1
Chareice <chareice@live.com>
2024-04-07 15:27:41 +0800

    chore: operator test coverage

---
+++ packages/core/database/src/__tests__/operator/boolean-operator.test.ts
@@ -0,0 +1,68 @@
+import Database, { Collection, mockDatabase } from '@nocobase/database';
+
+describe('boolean operator', () => {
+  let db: Database;
+
+  let User: Collection;
+
+  afterEach(async () => {
+    await db.close();
+  });
+
+  beforeEach(async () => {
+    db = mockDatabase({});
+
+    await db.clean({ drop: true });
+
+    User = db.collection({
+      name: 'users',
+      fields: [{ type: 'boolean', name: 'activated' }],
+    });
+
+    await db.sync({
+      force: true,
+    });
+  });
+
+  it('should query $isFalsy', async () => {
+    await db.getRepository('users').create({
+      values: [
+        {
+          activated: false,
+        },
+        {
+          activated: true,
+        },
+      ],
+    });
+
+    const res = await db.getRepository('users').find({
+      filter: {
+        'activated.$isFalsy': true,
+      },
+    });
+
+    expect(res.length).toBe(1);
+  });
+
+  it('should query $isTruly', async () => {
+    await db.getRepository('users').create({
+      values: [
+        {
+          activated: false,
+        },
+        {
+          activated: true,
+        },
+      ],
+    });
+
+    const res = await db.getRepository('users').find({
+      filter: {
+        'activated.$isTruly': true,
+      },
+    });
+
+    expect(res.length).toBe(1);
+  });
+});
