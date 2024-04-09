9297993c4338ab05235172a3684f323a32d60154
Chareice <chareice@live.com>
2024-04-09 19:21:23 +0800

    chore(error-handler): display message cause the error

--- packages/plugins/@nocobase/plugin-error-handler/src/server/__tests__/render-error.test.ts
+++ packages/plugins/@nocobase/plugin-error-handler/src/server/__tests__/render-error.test.ts
@@ -1,5 +1,6 @@
 import { Database } from '@nocobase/database';
-import { MockServer, createMockServer } from '@nocobase/test';
+import { createMockServer, MockServer } from '@nocobase/test';
+
 describe('create with exception', () => {
   let app: MockServer;
   beforeEach(async () => {
@@ -13,6 +14,27 @@ describe('create with exception', () => {
     await app.destroy();
   });
 
+  it('should render error with cause property', async () => {
+    app.use(
+      (ctx, next) => {
+        ctx.throw(
+          400,
+          new Error('wrapped error', {
+            cause: new Error('original error'),
+          }),
+        );
+      },
+      { after: 'dataSource' },
+    );
+
+    const response = await app.agent().resource('users').create({
+      values: {},
+    });
+
+    expect(response.statusCode).toEqual(400);
+
+    expect(response.body.errors[0].message).contains('original error');
+  });
   it('should handle not null error', async () => {
     const collection = app.collection({
       name: 'users',
