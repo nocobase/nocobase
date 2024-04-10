d46fb1156444a0efd2aec1b2a13be0bd59916d07
katherinehhh <katherine_15995@163.com>
2024-04-10 10:39:30 +0800

    refactor: table block do not enable trees by default

--- packages/core/client/src/block-provider/TableBlockProvider.tsx
+++ packages/core/client/src/block-provider/TableBlockProvider.tsx
@@ -50,7 +50,7 @@ const InternalTableBlockProvider = (props: Props) => {
   const allIncludesChildren = useMemo(() => {
     const { treeTable } = fieldSchema?.['x-decorator-props'] || {};
     const data = service?.data?.data;
-    if (treeTable !== false) {
+    if (treeTable) {
       const keys = getIdsWithChildren(data);
       return keys;
     }
@@ -120,7 +120,7 @@ export const TableBlockProvider = withDynamicSchemaProps((props) => {
   const params = useTableBlockParamsCompat(props);
 
   let childrenColumnName = 'children';
-  if (collection?.tree && treeTable !== false) {
+  if (collection?.tree && treeTable) {
     if (resourceName?.includes('.')) {
       const f = getCollectionField(resourceName);
       if (f?.treeChildren) {
