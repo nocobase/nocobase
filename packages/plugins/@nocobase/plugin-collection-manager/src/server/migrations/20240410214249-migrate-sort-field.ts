34e545c8f0afdd343741df9a58e846c2480f0a8a
Chareice <chareice@live.com>
2024-04-14 01:08:58 +0800

    chore: association sortable field

--- packages/plugins/@nocobase/plugin-collection-manager/src/server/migrations/20240410214249-migrate-sort-field.ts
+++ packages/plugins/@nocobase/plugin-collection-manager/src/server/migrations/20240410214249-migrate-sort-field.ts
@@ -4,7 +4,7 @@ export default class extends Migration {
   on = 'afterLoad'; // 'beforeLoad' or 'afterLoad'
   appVersion = '<0.21.0-alpha.7';
 
-  async up() {
+  private async syncCollectionsSortField() {
     const collections = await this.db.getRepository('collections').find();
 
     for (const collection of collections) {
@@ -36,5 +36,55 @@ export default class extends Migration {
         });
       }
     }
+  }
+
+  private async syncAssociationSortField() {
+    const sortableAssociations = await this.db.getRepository('fields').find({
+      filter: {
+        'options.sortable': true,
+      },
+    });
+
+    for (const field of sortableAssociations) {
+      const collectionName = field.get('target');
+
+      const collection = await this.db.getRepository('collections').findOne({
+        filter: {
+          name: collectionName,
+        },
+      });
+
+      if (!collection) {
+        continue;
+      }
+
+      const sortFieldName = field.get('sortBy') ? field.get('sortBy') : field.get('name') + '_sort';
+
+      const fields = await collection.getFields();
+      const sortField = fields.find((item) => item.get('type') === 'sort' && item.get('name') === sortFieldName);
+
+      if (!sortField) {
+        await this.db.getRepository('fields').create({
+          values: {
+            name: sortFieldName,
+            type: 'sort',
+            interface: 'sort',
+            uiSchema: {
+              title: sortFieldName,
+              type: 'number',
+              'x-component': 'InputNumber',
+              'x-component-props': { stringMode: true, step: '1' },
+              'x-validator': 'integer',
+            },
+            collectionName: collection.get('name'),
+          },
+        });
+      }
+    }
+  }
+
+  async up() {
+    await this.syncCollectionsSortField();
+    await this.syncAssociationSortField();
   }
 }
