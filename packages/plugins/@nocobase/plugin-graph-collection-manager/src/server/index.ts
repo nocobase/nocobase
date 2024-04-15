47568af7af7fb375b91327484baa38499cbf03de
chenos <chenlinxh@gmail.com>
2024-04-15 19:56:32 +0800

    fix: graph

--- packages/plugins/@nocobase/plugin-graph-collection-manager/src/server/index.ts
+++ packages/plugins/@nocobase/plugin-graph-collection-manager/src/server/index.ts
@@ -1,10 +1,11 @@
 import { Plugin } from '@nocobase/server';
-import path from 'path';
 
 export class PluginGraphCollectionManagerServer extends Plugin {
   async load() {
-    await this.importCollections(path.resolve(__dirname, 'collections'));
-    this.app.acl.allow('graphPositions', '*');
+    this.app.acl.registerSnippet({
+      name: 'pm.data-source-manager.graph-collection-manager',
+      actions: ['graphPositions:*'],
+    });
   }
 }
 
