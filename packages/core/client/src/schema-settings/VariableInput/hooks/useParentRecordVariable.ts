fe7c2ab0954bc0dd7b3b9ab980a4c937eb5c8d66
Zeke Zhang <958414905@qq.com>
2024-04-15 15:53:22 +0800

    fix(variable): fix parent record variable reporting errors in data scope

--- packages/core/client/src/schema-settings/VariableInput/hooks/useParentRecordVariable.ts
+++ packages/core/client/src/schema-settings/VariableInput/hooks/useParentRecordVariable.ts
@@ -1,8 +1,9 @@
 import { Schema } from '@formily/json-schema';
 import { useTranslation } from 'react-i18next';
 import { CollectionFieldOptions_deprecated } from '../../../collection-manager';
-import { useParentCollection } from '../../../data-source/collection/AssociationProvider';
+import { useCollection } from '../../../data-source';
 import { useCollectionRecord } from '../../../data-source/collection-record/CollectionRecordProvider';
+import { useParentCollection } from '../../../data-source/collection/AssociationProvider';
 import { useFlag } from '../../../flag-provider/hooks/useFlag';
 import { useBaseVariable } from './useBaseVariable';
 
@@ -48,6 +49,7 @@ export const useCurrentParentRecordVariable = (props: Props = {}) => {
   const { t } = useTranslation();
   const record = useCollectionRecord();
   const { name: parentCollectionName } = useParentCollection() || {};
+  const collection = useCollection();
   const { isInSubForm, isInSubTable } = useFlag() || {};
 
   const currentParentRecordSettings = useBaseVariable({
@@ -55,15 +57,18 @@ export const useCurrentParentRecordVariable = (props: Props = {}) => {
     uiSchema: props.schema,
     name: '$nParentRecord',
     title: t('Parent record'),
-    collectionName: parentCollectionName,
+    collectionName: parentCollectionName || collection?.name,
     noDisabled: props.noDisabled,
     targetFieldSchema: props.targetFieldSchema,
   });
 
   return {
     currentParentRecordSettings,
-    currentParentRecordCtx: record?.parentRecord?.data,
+    // 当该变量使用在区块数据范围的时候，由于某些区块（如 Table）是在 DataBlockProvider 之前解析 filter 的，
+    // 导致此时 record.parentRecord 的值还是空的，此时正确的值应该是 record，所以在后面加了 record?.data 来防止这种情况
+    currentParentRecordCtx: record?.parentRecord?.data || record?.data,
     shouldDisplayCurrentParentRecord: !!record?.parentRecord?.data && !isInSubForm && !isInSubTable,
-    collectionName: parentCollectionName,
+    // 在后面加上 collection?.name 的原因如上面的变量一样
+    collectionName: parentCollectionName || collection?.name,
   };
 };
