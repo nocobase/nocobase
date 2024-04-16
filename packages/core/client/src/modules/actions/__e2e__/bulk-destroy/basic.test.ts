e829624a2260271a7c5249dab69ad6dd6aa5dfbc
Zeke Zhang <958414905@qq.com>
2024-04-16 09:38:25 +0800

    test: add e2e for bulk delete

---
+++ packages/core/client/src/modules/actions/__e2e__/bulk-destroy/basic.test.ts
@@ -0,0 +1,24 @@
+import { expect, test } from '@nocobase/test/e2e';
+import { oneTableWithGeneral } from './templates';
+
+test.describe('bulk-destroy', () => {
+  test('in table', async ({ page, mockPage, mockRecords }) => {
+    const nocoPage = await mockPage(oneTableWithGeneral).waitForInit();
+    await mockRecords('general', 3);
+    await nocoPage.goto();
+
+    await expect(page.getByLabel('block-item-CardItem-general-').getByText('No data')).not.toBeVisible();
+
+    // 1. 创建一个批量删除按钮
+    await page.getByLabel('schema-initializer-ActionBar-').click();
+    await page.getByRole('menuitem', { name: 'Delete' }).click();
+
+    // 2. 选中所有行
+    await page.getByLabel('Select all').check();
+
+    // 3. 点击批量删除按钮，Table 显示无数据
+    await page.getByLabel('action-Action-Delete-destroy-').click();
+    await page.getByRole('button', { name: 'OK', exact: true }).click();
+    await expect(page.getByLabel('block-item-CardItem-general-').getByText('No data')).toBeVisible();
+  });
+});
