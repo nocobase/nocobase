13c3b9af3b6bd1d357317b8f6dc11a49de866c17
katherinehhh <katherine_15995@163.com>
2024-04-16 20:25:58 +0800

    refactor: sub-table support setDefaultSortingRules

--- packages/core/client/src/modules/fields/component/SubTable/subTablePopoverComponentFieldSettings.tsx
+++ packages/core/client/src/modules/fields/component/SubTable/subTablePopoverComponentFieldSettings.tsx
@@ -1,6 +1,7 @@
 import { Field } from '@formily/core';
-import { useField, useFieldSchema } from '@formily/react';
+import { useField, useFieldSchema, ISchema } from '@formily/react';
 import { useTranslation } from 'react-i18next';
+import { ArrayItems } from '@formily/antd-v5';
 import { SchemaSettings } from '../../../../application/schema-settings/SchemaSettings';
 import { useFieldComponentName } from '../../../../common/useFieldComponentName';
 import {
@@ -10,6 +11,8 @@ import {
   useIsFieldReadPretty,
 } from '../../../../schema-component';
 import { isSubMode } from '../../../../schema-component/antd/association-field/util';
+import { useCollection_deprecated, useSortFields } from '../../../../collection-manager';
+import { useTableBlockContext } from '../../../../block-provider/TableBlockProvider';
 
 const fieldComponent: any = {
   name: 'fieldComponent',
@@ -85,7 +88,128 @@ const allowSelectExistingRecord = {
     };
   },
 };
+
+const setDefaultSortingRules = {
+  name: 'SetDefaultSortingRules',
+  type: 'modal',
+  useComponentProps() {
+    const { name } = useCollection_deprecated();
+    const field = useField();
+    const fieldSchema = useFieldSchema();
+    const sortFields = useSortFields(name);
+    const { service } = useTableBlockContext();
+    const { t } = useTranslation();
+    const { dn } = useDesignable();
+    const defaultSort = field.componentProps.sortArr || [];
+    const sort = defaultSort?.map((item: string) => {
+      return item?.startsWith('-')
+        ? {
+            field: item.substring(1),
+            direction: 'desc',
+          }
+        : {
+            field: item,
+            direction: 'asc',
+          };
+    });
+
+    return {
+      title: t('Set default sorting rules'),
+      components: { ArrayItems },
+      schema: {
+        type: 'object',
+        title: t('Set default sorting rules'),
+        properties: {
+          sort: {
+            type: 'array',
+            default: sort,
+            'x-component': 'ArrayItems',
+            'x-decorator': 'FormItem',
+            items: {
+              type: 'object',
+              properties: {
+                space: {
+                  type: 'void',
+                  'x-component': 'Space',
+                  properties: {
+                    sort: {
+                      type: 'void',
+                      'x-decorator': 'FormItem',
+                      'x-component': 'ArrayItems.SortHandle',
+                    },
+                    field: {
+                      type: 'string',
+                      enum: sortFields,
+                      required: true,
+                      'x-decorator': 'FormItem',
+                      'x-component': 'Select',
+                      'x-component-props': {
+                        style: {
+                          width: 260,
+                        },
+                      },
+                    },
+                    direction: {
+                      type: 'string',
+                      'x-decorator': 'FormItem',
+                      'x-component': 'Radio.Group',
+                      'x-component-props': {
+                        optionType: 'button',
+                      },
+                      enum: [
+                        {
+                          label: t('ASC'),
+                          value: 'asc',
+                        },
+                        {
+                          label: t('DESC'),
+                          value: 'desc',
+                        },
+                      ],
+                    },
+                    remove: {
+                      type: 'void',
+                      'x-decorator': 'FormItem',
+                      'x-component': 'ArrayItems.Remove',
+                    },
+                  },
+                },
+              },
+            },
+            properties: {
+              add: {
+                type: 'void',
+                title: t('Add sort field'),
+                'x-component': 'ArrayItems.Addition',
+              },
+            },
+          },
+        },
+      } as ISchema,
+      onSubmit: ({ sort }) => {
+        const sortArr = sort.map((item) => {
+          return item.direction === 'desc' ? `-${item.field}` : item.field;
+        });
+        field.componentProps.sortArr = sortArr;
+        console.log(fieldSchema);
+        fieldSchema['x-component-props'].sortArr = sortArr;
+        dn.emit('patch', {
+          schema: {
+            ['x-uid']: fieldSchema['x-uid'],
+            'x-component-props': fieldSchema['x-component-props'],
+          },
+        });
+        // console.log(service,fieldSchema)
+        // service.run({ ...service.params?.[0], sort: sortArr });
+      },
+    };
+  },
+  useVisible() {
+    const readPretty = useIsFieldReadPretty();
+    return readPretty;
+  },
+};
 export const subTablePopoverComponentFieldSettings = new SchemaSettings({
   name: 'fieldSettings:component:SubTable',
-  items: [fieldComponent, allowSelectExistingRecord],
+  items: [fieldComponent, allowSelectExistingRecord, setDefaultSortingRules],
 });
