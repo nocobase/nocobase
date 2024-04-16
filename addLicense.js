358935135d47919d64ce29bbb60128f36fd17640
dream2023 <1098626505@qq.com>
2024-04-16 09:20:52 +0800

    fix: upgrade

--- addLicense.js
+++ addLicense.js
@@ -18,16 +18,8 @@ const openSourceLicense = `
  * Copyright (c) 2020-2024 NocoBase Co., Ltd.
  * Authors: NocoBase Team.
  *
- * This program is offered under a commercial and under the AGPL license.
- * For more information, see <https://www.nocobase.com/agreement>
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/agpl-3.0.html>.
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 `.trim();
 
