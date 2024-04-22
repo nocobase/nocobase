13401a7f10ee93d447473d2e00b2a0d809ebddf8
dream2023 <1098626505@qq.com>
2024-04-22 17:25:07 +0800

    fix: build bug

--- packages/core/test/src/web/index.tsx
+++ packages/core/test/src/web/index.tsx
@@ -4,7 +4,6 @@ import { AxiosInstance, AxiosRequestConfig } from 'axios';
 import { set, get, pick } from 'lodash';
 import { useFieldSchema, observer } from '@formily/react';
 
-// @ts-ignore
 import {
   AntdSchemaComponentPlugin,
   Application,
@@ -15,6 +14,7 @@ import {
   SchemaComponent,
   SchemaSettings,
   SchemaSettingsPlugin,
+  // @ts-ignore
 } from '@nocobase/client';
 
 import dataSourceMainCollections from './dataSourceMainCollections.json';
