1f3ab68daae811693b20be66c9d18234f3809663
chenos <chenlinxh@gmail.com>
2024-04-23 10:24:16 +0800

    feat: rename plugin

---
+++ packages/presets/nocobase/src/server/migrations/20240423101556-rename.ts
@@ -0,0 +1,22 @@
+import { Migration } from '@nocobase/server';
+
+export default class extends Migration {
+  on = 'beforeLoad'; // 'beforeLoad' or 'afterLoad'
+  appVersion = '<0.21.0-alpha.15';
+
+  async up() {
+    const names = {
+      '@nocobase/plugin-collection-manager': '@nocobase/plugin-data-source-main',
+    };
+    for (const original of Object.keys(names)) {
+      await this.pm.repository.update({
+        filter: {
+          packageName: original,
+        },
+        values: {
+          packageName: names[original],
+        },
+      });
+    }
+  }
+}
