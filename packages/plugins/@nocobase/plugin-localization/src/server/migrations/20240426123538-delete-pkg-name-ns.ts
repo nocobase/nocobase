c5491ebcaef04b9267aa86a30e3616c628291fc4
xilesun <2013xile@gmail.com>
2024-04-26 14:32:26 +0800

    fix(localization): remove duplicate records

--- packages/plugins/@nocobase/plugin-localization/src/server/migrations/20240426123538-delete-pkg-name-ns.ts
+++ packages/plugins/@nocobase/plugin-localization/src/server/migrations/20240426123538-delete-pkg-name-ns.ts
@@ -1,10 +1,33 @@
-import { Migration } from '@nocobase/server';
+import { Migration, OFFICIAL_PLUGIN_PREFIX } from '@nocobase/server';
 
 export default class extends Migration {
   on = 'afterLoad'; // 'beforeLoad' or 'afterLoad'
-  appVersion = '<0.21.0-alpha.16';
+  appVersion = '<1.0.0-alpha.1';
 
   async up() {
-    // coding
+    const resources = await this.app.localeManager.getCacheResources('en-US');
+    const modules = Object.keys(resources);
+    console.log(resources, modules);
+    const toBeDeleted = [];
+    modules.forEach((module) => {
+      if (!module.startsWith(OFFICIAL_PLUGIN_PREFIX)) {
+        return;
+      }
+      const name = module.replace(OFFICIAL_PLUGIN_PREFIX, '');
+      if (!modules.includes(name)) {
+        return;
+      }
+      toBeDeleted.push(module);
+    });
+    if (!toBeDeleted.length) {
+      return;
+    }
+    await this.db.getRepository('localizationTexts').destroy({
+      filter: {
+        module: {
+          $in: toBeDeleted.map((module) => `resources.${module}`),
+        },
+      },
+    });
   }
 }
