88c161364be93ef1dc08f7f71bdf0b7669b06f76
Sun668 <947692259@qq.com>
2024-04-27 17:07:16 +0800

    fix: plugin-field-markdown-vdtor dep preload

--- packages/plugins/@nocobase/plugin-field-markdown-vditor/src/client/index.tsx
+++ packages/plugins/@nocobase/plugin-field-markdown-vditor/src/client/index.tsx
@@ -2,7 +2,7 @@ import { Plugin } from '@nocobase/client';
 import { MarkdownVditor } from './components';
 import { MarkdownVditorFieldInterface } from './interfaces/markdown-vditor';
 import 'vditor/dist/index.css';
-import katex from 'katex';
+import { cdn } from './components/const';
 export class PluginFieldMarkdownVditorClient extends Plugin {
   async afterAdd() {}
 
@@ -10,12 +10,28 @@ export class PluginFieldMarkdownVditorClient extends Plugin {
 
   async load() {
     this.app.addComponents({ MarkdownVditor });
-    this.initKatexDependency();
+    this.initVditorDependency();
     this.app.dataSourceManager.addFieldInterfaces([MarkdownVditorFieldInterface]);
   }
 
-  initKatexDependency() {
-    window['katex'] = katex;
+  initVditorDependency() {
+    const vditorDependence = {
+      katex: `${cdn}/dist/js/katex/katex.min.js?v=0.16.9`,
+      ABCJS: `${cdn}/dist/js/abcjs/abcjs_basic.min`,
+      plantumlEncoder: `${cdn}/dist/js/plantuml/plantuml-encoder.min`,
+      echarts: `${cdn}/dist/js/echarts/echarts.min`,
+      flowchart: `${cdn}/dist/js/flowchart.js/flowchart.min`,
+      Viz: `${cdn}/dist/js/graphviz/viz`,
+      mermaid: `${cdn}/dist/js/mermaid/mermaid.min`,
+    };
+    this.app.requirejs.require.config({
+      paths: vditorDependence,
+    });
+    Object.keys(vditorDependence).forEach((key) => {
+      this.app.requirejs.require([key], (m) => {
+        window[key] = m;
+      });
+    });
   }
 }
 
