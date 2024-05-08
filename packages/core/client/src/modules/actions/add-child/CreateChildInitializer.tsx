e54ebb69ca6e944a7547b750b7c32afa92c8633e
katherinehhh <katherine_15995@163.com>
2024-05-08 15:40:29 +0800

    style: add child style improve

--- packages/core/client/src/modules/actions/add-child/CreateChildInitializer.tsx
+++ packages/core/client/src/modules/actions/add-child/CreateChildInitializer.tsx
@@ -22,7 +22,7 @@ export const CreateChildInitializer = (props) => {
       openMode: 'drawer',
       type: 'link',
       addChild: true,
-      style: { padding: '0px', marginTop: '-5px' },
+      style: { padding: '0px 16px', marginTop: '-5px' },
       component: 'CreateRecordAction',
     },
     properties: {
