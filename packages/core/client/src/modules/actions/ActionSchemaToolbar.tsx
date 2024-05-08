dba9b664b4d9e377ea94663254da6f2d26f7fa7f
katherinehhh <katherine_15995@163.com>
2024-05-08 09:44:43 +0800

    fix: actionSchemaToolbar support x-toolbar-props

--- packages/core/client/src/modules/actions/ActionSchemaToolbar.tsx
+++ packages/core/client/src/modules/actions/ActionSchemaToolbar.tsx
@@ -7,9 +7,20 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
+import { useFieldSchema } from '@formily/react';
 import React from 'react';
 import { SchemaToolbar } from '../../schema-settings/GeneralSchemaDesigner';
 
 export const ActionSchemaToolbar = (props) => {
-  return <SchemaToolbar initializer={false} showBorder={false} showBackground {...props} />;
+  const fieldSchema = useFieldSchema();
+  console.log(props);
+  return (
+    <SchemaToolbar
+      initializer={false}
+      showBorder={false}
+      showBackground
+      {...props}
+      {...fieldSchema['x-toolbar-props']}
+    />
+  );
 };
