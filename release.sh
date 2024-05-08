fe977238b128e6e70f81d3fc943fdc38a5aa0d69
Chareice <chareice@live.com>
2024-05-08 22:21:38 +0800

    chore: release ci

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ cd ./packages/pro-plugins
 git add .
 git commit -m "chore(versions): 😊 publish v$(jq -r '.version' ../../lerna.json)"
 git tag v$(jq -r '.version' ../../lerna.json)
-git push --atomic origin main v$(jq -r '.version' ../../lerna.json)
+#git push --atomic origin main v$(jq -r '.version' ../../lerna.json)
 cd ../../
 git add .
 git commit -m "chore(versions): 😊 publish v$(jq -r '.version' lerna.json)"
@@ -11,4 +11,4 @@ git tag v$(jq -r '.version' lerna.json)
 yarn changelog --breaking-pattern "BREAKING CHANGE:"
 git add .
 git commit -m "chore: update changelog"
-git push --atomic origin main v$(jq -r '.version' lerna.json)
+#git push --atomic origin main v$(jq -r '.version' lerna.json)
