9835accaf267cf67de3f1ed8be73ab0f86f6fbf0
Zeke Zhang <958414905@qq.com>
2024-05-10 12:37:41 +0800

    chore: fix bot

--- .github/workflows/merge.config.ts
+++ .github/workflows/merge.config.ts
@@ -1,7 +1,5 @@
 export default {
   // Look for test files in the "tests" directory, relative to this configuration file.
   testDir: 'packages',
-  // Match all test files in the e2e and __e2e__ directories.
-  testMatch: /(.*\/e2e\/|.*\/__e2e__\/).+\.test\.[tj]sx*$/,
-  reporter: [['markdown'], ['html', { outputFolder: `./e2e-report`, open: 'never' }]]
+  reporter: [['markdown'], ['html', { outputFolder: `../../e2e-report`, open: 'never' }]]
 };
