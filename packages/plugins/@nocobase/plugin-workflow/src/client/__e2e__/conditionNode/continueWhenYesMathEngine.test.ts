e6e5559c6dc9cfa88e5bfd8b6c51b1d3430d0c31
hongboji <j414562100@qq.com>
2024-05-10 11:27:33 +0800

    test: optimal calculationNode e2e

--- packages/plugins/@nocobase/plugin-workflow/src/client/__e2e__/conditionNode/continueWhenYesMathEngine.test.ts
+++ packages/plugins/@nocobase/plugin-workflow/src/client/__e2e__/conditionNode/continueWhenYesMathEngine.test.ts
@@ -81,7 +81,7 @@ test('Collection event add data trigger, Math engine, determine trigger node int
   await page.getByRole('menuitemcheckbox', { name: 'Trigger variables' }).click();
   await page.getByRole('menuitemcheckbox', { name: 'Trigger data' }).click();
   await page.getByRole('menuitemcheckbox', { name: triggerNodeFieldDisplayName }).click();
-  const conditionalRightConstant = faker.number.int();
+  const conditionalRightConstant = faker.number.int({ max: 1000 });
   await page.waitForTimeout(500);
   await page.keyboard.type(`==${conditionalRightConstant}`, { delay: 50 });
   await expect(conditionNode.conditionExpressionEditBox).toHaveText(
@@ -165,7 +165,7 @@ test('Collection event Add Data Trigger, Math engine, determines that the trigge
   await page.getByRole('menuitemcheckbox', { name: 'Trigger variables' }).click();
   await page.getByRole('menuitemcheckbox', { name: 'Trigger data' }).click();
   await page.getByRole('menuitemcheckbox', { name: triggerNodeFieldDisplayName }).click();
-  const conditionalRightConstant = faker.number.int();
+  const conditionalRightConstant = faker.number.int({ max: 1000 });
   await page.waitForTimeout(500);
   await page.keyboard.type(`==${conditionalRightConstant}`, { delay: 50 });
   await expect(conditionNode.conditionExpressionEditBox).toHaveText(
@@ -248,7 +248,7 @@ test('Collection event Add Data Trigger, Math engine, determines that the trigge
   await page.getByRole('menuitemcheckbox', { name: 'Trigger variables' }).click();
   await page.getByRole('menuitemcheckbox', { name: 'Trigger data' }).click();
   await page.getByRole('menuitemcheckbox', { name: triggerNodeFieldDisplayName }).click();
-  const conditionalRightConstant = faker.number.int();
+  const conditionalRightConstant = faker.number.int({ max: 1000 });
   await page.waitForTimeout(500);
   await page.keyboard.type(`!=${conditionalRightConstant}`, { delay: 50 });
   await expect(conditionNode.conditionExpressionEditBox).toHaveText(
@@ -331,7 +331,7 @@ test('Collection event add data trigger, Math engine, determines that the trigge
   await page.getByRole('menuitemcheckbox', { name: 'Trigger variables' }).click();
   await page.getByRole('menuitemcheckbox', { name: 'Trigger data' }).click();
   await page.getByRole('menuitemcheckbox', { name: triggerNodeFieldDisplayName }).click();
-  const conditionalRightConstant = faker.number.int();
+  const conditionalRightConstant = faker.number.int({ max: 1000 });
   await page.waitForTimeout(500);
   await page.keyboard.type(`!=${conditionalRightConstant}`, { delay: 50 });
   await expect(conditionNode.conditionExpressionEditBox).toHaveText(
