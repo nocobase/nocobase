c1d6eb6b820934e7a553e88377c60e67c55fdd37
Chareice <chareice@live.com>
2024-05-11 19:50:27 +0800

    fix: destroy app

--- packages/core/server/src/commands/destroy.ts
+++ packages/core/server/src/commands/destroy.ts
@@ -12,12 +12,9 @@
 import Application from '../application';
 
 export default (app: Application) => {
-  app
-    .command('destroy')
-    .preload()
-    .action(async (...cliArgs) => {
-      await app.destroy({
-        cliArgs,
-      });
+  app.command('destroy').action(async (...cliArgs) => {
+    await app.destroy({
+      cliArgs,
     });
+  });
 };
