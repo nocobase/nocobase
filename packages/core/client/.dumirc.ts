3afc0e90020926fdc8c6833f3d84472a961974fd
jack zhang <1098626505@qq.com>
2024-05-28 16:18:30 +0800

    fix: bug

--- packages/core/client/.dumirc.ts
+++ packages/core/client/.dumirc.ts
@@ -20,6 +20,25 @@ export default defineConfig({
   sitemap: process.env.NODE_ENV === 'production' ? {
     hostname: lang === 'zh-CN' ? 'https://client.docs-cn.nocobase.com' : 'https://client.docs.nocobase.com'
   } : false,
+  metas: [
+    { name: 'keywords', content: 'nocobase,nocobase doc,low-code,no-code,self-hosted,open source,open-source,no-code development,low-code development,workflow management software,business process management,collaboration software,enterprise process management,enterprise management system,no-code system,no-code platform,free no-code development platform' },
+    { name: 'description', content: "NocoBase is a lightweight, extremely scalable open source no-code and low-code development platform. Instead of investing years of time and millions of dollars in research and development, deploy NocoBase in a few minutes and you'll have a private, controllable, and extremely scalable no- code development platform!" },
+  ],
+  headScripts: [
+    `document.addEventListener('DOMContentLoaded', function () {
+      document.body.style.visibility = 'hidden';
+    });
+
+    const checkLoading = setInterval(() => {
+      const loading = document.querySelector('.dumi-default-loading-skeleton');
+      const headerMenu = document.querySelector('header .ant-menu');
+      const antdIsLoaded = headerMenu && window.getComputedStyle(headerMenu).listStyle === 'outside none none';
+      if (loading || antdIsLoaded) {
+        document.body.style.visibility = 'initial';
+        clearInterval(checkLoading);
+      }
+    }, 50);`
+  ],
   fastRefresh: false, // 热更新会导致 Context 丢失，不开启
   // ssr: {},
   // exportStatic: {
--- packages/core/client/docs/en-US/index.md
+++ packages/core/client/docs/en-US/index.md
@@ -1,3 +1,3 @@
-# Index
-
 <Navigate replace to="/core/application/application"></Navigate>
+
+<embed src="./core/application/application.md"></embed>
