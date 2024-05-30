80dc3115de5c443d4e90040c5c1a97c32197c875
Zeke Zhang <958414905@qq.com>
2024-05-30 10:41:57 +0800

    refactor: extract

--- packages/plugins/@nocobase/plugin-map/src/client/fields/fieldSettingsComponentMap.ts
+++ packages/plugins/@nocobase/plugin-map/src/client/fields/fieldSettingsComponentMap.ts
@@ -7,53 +7,10 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
-import { ISchema, useField, useFieldSchema } from '@formily/react';
-import { SchemaSettings, SchemaSettingsModalItem, useDesignable } from '@nocobase/client';
-import _ from 'lodash';
-import { useMapTranslation } from '../locale';
+import { SchemaSettings } from '@nocobase/client';
+import { defaultZoomLevel } from '../block/MapBlock.Settings';
 
 export const fieldSettingsComponentMap = new SchemaSettings({
   name: 'fieldSettings:component:Map',
-  items: [
-    {
-      name: 'defaultZoomLevel',
-      Component: SchemaSettingsModalItem,
-      useComponentProps() {
-        const { t } = useMapTranslation();
-        const fieldSchema = useFieldSchema();
-        const field = useField();
-        const { dn } = useDesignable();
-        const defaultZoom = fieldSchema?.['x-component-props']?.['zoom'] || 13;
-        return {
-          title: t('The default zoom level of the map'),
-          schema: {
-            type: 'object',
-            title: t('Set default zoom level'),
-            properties: {
-              zoom: {
-                title: t('Zoom'),
-                default: defaultZoom,
-                'x-component': 'InputNumber',
-                'x-decorator': 'FormItem',
-                'x-component-props': {
-                  precision: 0,
-                },
-              },
-            },
-          } as ISchema,
-          onSubmit: ({ zoom }) => {
-            _.set(fieldSchema, 'x-component-props.zoom', zoom);
-            Object.assign(field.componentProps, fieldSchema['x-component-props']);
-            dn.emit('patch', {
-              schema: {
-                'x-uid': fieldSchema['x-uid'],
-                'x-component-props': field.componentProps,
-              },
-            });
-            dn.refresh();
-          },
-        };
-      },
-    },
-  ],
+  items: [defaultZoomLevel],
 });
