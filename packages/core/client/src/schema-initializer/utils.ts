a19e741ed33c278fbee3bb9c4399128dd4f9dbfa
Zeke Zhang <958414905@qq.com>
2024-05-30 17:45:35 +0800

    fix(client): fix the issue where adding block templates in the popup does not display (#4531)
    * chore: avoid crashing
    
    * fix(client): fix the issue where adding block templates in the popup does not display

--- packages/core/client/src/schema-initializer/utils.ts
+++ packages/core/client/src/schema-initializer/utils.ts
@@ -42,7 +42,7 @@ export const gridRowColWrap = (schema: ISchema) => {
         type: 'void',
         'x-component': 'Grid.Col',
         properties: {
-          [schema.name || uid()]: schema,
+          [schema?.name || uid()]: schema,
         },
       },
     },
@@ -968,13 +968,14 @@ export const useCollectionDataSourceItems = ({
         // 目的是使点击无效
         onClick() {},
         componentProps: {
+          ...dataBlockInitializerProps,
           icon: null,
           title: otherText || t('Other records'),
           name: 'otherRecords',
           showAssociationFields: false,
           onlyCurrentDataSource: false,
           hideChildrenIfSingleCollection: false,
-          onCreateBlockSchema: dataBlockInitializerProps.onCreateBlockSchema,
+          fromOthersInPopup: true,
           componentType: componentTypeMap[componentName] || componentName,
           filter({ collection, associationField }) {
             if (filterOtherRecordsCollection) {
