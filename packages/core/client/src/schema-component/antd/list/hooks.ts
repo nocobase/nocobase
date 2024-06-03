99b4534f8f8d60357ad9f14306f2b50586606a0b
katherinehhh <katherine_15995@163.com>
2024-06-03 09:58:17 +0800

    fix: bug

--- packages/core/client/src/schema-component/antd/list/hooks.ts
+++ packages/core/client/src/schema-component/antd/list/hooks.ts
@@ -11,6 +11,8 @@ import { SpaceProps, theme } from 'antd';
 import { useFieldSchema } from '@formily/react';
 import { useDesignable } from '../../../';
 import { useListBlockContext } from './List.Decorator';
+import { useDataBlockHeight } from '../../hooks/useBlockSize';
+import { useDataBlock } from '../../../';
 
 const spaceProps: SpaceProps = {
   size: ['large', 'small'],
@@ -23,13 +25,13 @@ export const useListActionBarProps = () => {
   };
 };
 
-import { useDataBlockHeight } from '../../hooks/useBlockSize';
-
 export const useListBlockHeight = () => {
   const height = useDataBlockHeight();
   const schema = useFieldSchema();
   const { token } = theme.useToken();
   const { designable } = useDesignable();
+  const { heightProps } = useDataBlock() || {};
+  const { title } = heightProps || {};
   const {
     service: { data },
   } = useListBlockContext() || {};
@@ -39,8 +41,9 @@ export const useListBlockHeight = () => {
   if (!height) {
     return;
   }
+  const blockTitleHeaderHeight = title ? token.fontSizeLG * token.lineHeightLG + token.padding * 2 - 1 : 0;
   const hasListActions = Object.keys(schema.parent.properties.actionBar?.properties || {}).length > 0;
   const actionBarHeight = hasListActions || designable ? token.controlHeight + 2 * token.marginLG : token.marginLG;
   const paginationHeight = hasPagination ? token.controlHeight + token.paddingLG + token.marginLG : token.marginLG;
-  return height - actionBarHeight - paginationHeight;
+  return height - actionBarHeight - paginationHeight - blockTitleHeaderHeight;
 };
