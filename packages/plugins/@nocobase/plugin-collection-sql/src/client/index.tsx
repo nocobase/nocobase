a5fc22c6b03167b31590b31e13c1dca5a96160d2
jack zhang <1098626505@qq.com>
2024-06-11 22:53:04 +0800

    feat: tree block (#4566)
    * feat: tree block
    
    * fix: remove showSettings
    
    * fix: bug
    
    * fix: bug
    
    * fix: bug
    
    * fix: bug
    
    * fix: table tree expand
    
    * fix: bug
    
    * fix: connection bug T-4501
    
    * fix: template settings bug

--- packages/plugins/@nocobase/plugin-collection-sql/src/client/index.tsx
+++ packages/plugins/@nocobase/plugin-collection-sql/src/client/index.tsx
@@ -1,3 +1,12 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
 import { Plugin } from '@nocobase/client';
 
 export class PluginCollectionSqlClient extends Plugin {
@@ -9,7 +18,6 @@ export class PluginCollectionSqlClient extends Plugin {
 
   // You can get and modify the app instance here
   async load() {
-    console.log(this.app);
     // this.app.addComponents({})
     // this.app.addScopes({})
     // this.app.addProvider()
