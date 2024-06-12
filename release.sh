0fb3c57c7c0e31b728ac801db4add0a422845551
chenos <chenlinxh@gmail.com>
2024-06-12 11:14:59 +0800

    chore: test ci

--- release.sh
+++ release.sh
@@ -13,17 +13,16 @@ fi
 
 yarn lerna version $new_version --preid alpha --force-publish=* --no-git-tag-version -y
 
-yarn version:alpha -y
-cd ./packages/pro-plugins
-git add .
-git commit -m "chore(versions): 😊 publish v$(jq -r '.version' ../../lerna.json)"
-git tag v$(jq -r '.version' ../../lerna.json)
-#git push --atomic origin main v$(jq -r '.version' ../../lerna.json)
-cd ../../
-git add .
-git commit -m "chore(versions): 😊 publish v$(jq -r '.version' lerna.json)"
-git tag v$(jq -r '.version' lerna.json)
-yarn changelog --breaking-pattern "BREAKING CHANGE:"
-git add .
-git commit -m "chore: update changelog"
-# git push --atomic origin main v$(jq -r '.version' lerna.json)
+# cd ./packages/pro-plugins
+# git add .
+# git commit -m "chore(versions): 😊 publish v$(jq -r '.version' ../../lerna.json)"
+# git tag v$(jq -r '.version' ../../lerna.json)
+# #git push --atomic origin main v$(jq -r '.version' ../../lerna.json)
+# cd ../../
+# git add .
+# git commit -m "chore(versions): 😊 publish v$(jq -r '.version' lerna.json)"
+# git tag v$(jq -r '.version' lerna.json)
+# yarn changelog --breaking-pattern "BREAKING CHANGE:"
+# git add .
+# git commit -m "chore: update changelog"
+# # git push --atomic origin main v$(jq -r '.version' lerna.json)
