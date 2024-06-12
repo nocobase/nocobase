e6848858d49bd1e6487aee7e13292748304d443c
Zeke Zhang <958414905@qq.com>
2024-06-12 11:07:04 +0800

    fix(filter-form): fix auto-trigger filter action without default value (#4627)
    * test: add e2e test
    
    * test: add unit tests
    
    * fix(reset-button): prevent data clear when default values exist
    
    * fix: resolve known issues and add tests

--- packages/core/client/src/modules/blocks/filter-blocks/form/__e2e__/autoFilterWhenSettingDefaultValue.test.ts
+++ packages/core/client/src/modules/blocks/filter-blocks/form/__e2e__/autoFilterWhenSettingDefaultValue.test.ts
@@ -8,7 +8,7 @@
  */
 
 import { expect, test } from '@nocobase/test/e2e';
-import { oneFilterFormAndTable } from './templates';
+import { oneFilterFormAndTable, oneFilterFormAndTableWithManualLoadingData } from './templates';
 
 test.describe('filter form', () => {
   test('When the filter form field is set with a default value, it should trigger a filtering action on the first page load', async ({
@@ -29,7 +29,65 @@ test.describe('filter form', () => {
       page.getByLabel('block-item-CardItem-users-table').getByRole('button', { name: 'Super Admin' }),
     ).not.toBeVisible();
 
-    // 2. 点击重置按钮后，会显示出全部数据
+    // 2. 点击重置按钮后，数据不变
+    await page.getByLabel('action-Action-Reset-users-').click({
+      position: {
+        x: 10,
+        y: 10,
+      },
+    });
+    await expect(
+      page.getByLabel('block-item-CardItem-users-table').getByRole('button', { name: 'test name' }),
+    ).toBeVisible();
+    await expect(
+      page.getByLabel('block-item-CardItem-users-table').getByRole('button', { name: 'Super Admin' }),
+    ).not.toBeVisible();
+
+    // 3. 清空 nickname 的值后，点击筛选按钮，应该只显示出所有数据
+    await page.getByLabel('block-item-CollectionField-').getByRole('textbox').clear();
+    await page.getByLabel('action-Action-Filter-submit-').click({
+      position: {
+        x: 10,
+        y: 10,
+      },
+    });
+    await expect(
+      page.getByLabel('block-item-CardItem-users-table').getByRole('button', { name: 'test name' }),
+    ).toBeVisible();
+    await expect(
+      page.getByLabel('block-item-CardItem-users-table').getByRole('button', { name: 'Super Admin' }),
+    ).toBeVisible();
+
+    // 4. 此时点击 Reset 按钮，应该只显示一条数据，因为会把 nickname 的值重置为 {{$user.nickname}}
+    await page.getByLabel('action-Action-Reset-users-').click({
+      position: {
+        x: 10,
+        y: 10,
+      },
+    });
+    await expect(
+      page.getByLabel('block-item-CardItem-users-table').getByRole('button', { name: 'test name' }),
+    ).toBeVisible();
+    await expect(
+      page.getByLabel('block-item-CardItem-users-table').getByRole('button', { name: 'Super Admin' }),
+    ).not.toBeVisible();
+  });
+
+  test('with dataLoadingMode is manual', async ({ page, mockPage, mockRecord }) => {
+    // nickname 字段的默认值是 {{$user.nickname}}
+    const nocoPage = await mockPage(oneFilterFormAndTableWithManualLoadingData).waitForInit();
+    await mockRecord('users', { nickname: 'test name' });
+    await nocoPage.goto();
+
+    // 1. 首次加载，应该已经触发过一次筛选的动作
+    await expect(
+      page.getByLabel('block-item-CardItem-users-table').getByRole('button', { name: 'Super Admin' }),
+    ).toBeVisible();
+    await expect(
+      page.getByLabel('block-item-CardItem-users-table').getByRole('button', { name: 'test name' }),
+    ).not.toBeVisible();
+
+    // 2. 点击重置按钮后，数据不变
     await page.getByLabel('action-Action-Reset-users-').click({
       position: {
         x: 10,
@@ -41,10 +99,20 @@ test.describe('filter form', () => {
     ).toBeVisible();
     await expect(
       page.getByLabel('block-item-CardItem-users-table').getByRole('button', { name: 'test name' }),
-    ).toBeVisible();
+    ).not.toBeVisible();
 
-    // 3. 再次点击筛选按钮，应该只显示出符合条件的数据
+    // 3. 清空 nickname 输入框，然后点击 Filter 按钮，应该显示空数据
+    await page.getByLabel('block-item-CollectionField-').getByRole('textbox').clear();
     await page.getByLabel('action-Action-Filter-submit-').click({
+      position: {
+        x: 10,
+        y: 10,
+      },
+    });
+    await expect(page.getByLabel('block-item-CardItem-users-table').getByText('No data')).toBeVisible();
+
+    // 4. 此时点击 Reset 按钮，应该只显示一条数据，因为会把 nickname 的值重置为 {{$user.nickname}}
+    await page.getByLabel('action-Action-Reset-users-').click({
       position: {
         x: 10,
         y: 10,
@@ -52,9 +120,9 @@ test.describe('filter form', () => {
     });
     await expect(
       page.getByLabel('block-item-CardItem-users-table').getByRole('button', { name: 'Super Admin' }),
-    ).not.toBeVisible();
+    ).toBeVisible();
     await expect(
       page.getByLabel('block-item-CardItem-users-table').getByRole('button', { name: 'test name' }),
-    ).toBeVisible();
+    ).not.toBeVisible();
   });
 });
