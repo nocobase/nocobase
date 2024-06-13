152ff4cbcfae51b77868f981cb6f3cb7374833f0
katherinehhh <katherine_15995@163.com>
2024-06-13 12:10:06 +0800

    fix: client test

--- packages/core/client/src/schema-component/antd/collection-select/__tests__/collection-select.test.tsx
+++ packages/core/client/src/schema-component/antd/collection-select/__tests__/collection-select.test.tsx
@@ -50,7 +50,7 @@ describe('CollectionSelect', () => {
         >
           <div
             aria-label="block-item-demo title"
-            class="nb-sortable-designer nb-block-item nb-form-item acss-gw2dva css-vua9sf"
+            class="nb-sortable-designer nb-block-item nb-form-item acss-gw2dva"
             role="button"
           >
             <div
@@ -187,7 +187,7 @@ describe('CollectionSelect', () => {
         >
           <div
             aria-label="block-item-demo title"
-            class="nb-sortable-designer nb-block-item nb-form-item acss-gw2dva css-vua9sf"
+            class="nb-sortable-designer nb-block-item nb-form-item acss-gw2dva"
             role="button"
           >
             <div
