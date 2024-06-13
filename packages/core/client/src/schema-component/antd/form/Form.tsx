0ab993b73a8db521dda26076b469872c0c3921bc
katherinehhh <katherine_15995@163.com>
2024-06-13 21:57:42 +0800

    refactor: support api data source

--- packages/core/client/src/schema-component/antd/form/Form.tsx
+++ packages/core/client/src/schema-component/antd/form/Form.tsx
@@ -108,6 +108,7 @@ export const Form: React.FC<FormProps> & { Designer?: any } = observer(
           form.setValues(data?.data);
           form.setInitialValues(data?.data);
         },
+        form,
       },
       props,
     );
