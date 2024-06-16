815ca55c7283076cf4f9cf307ee295f7ff0fc8d8
Chareice <chareice@live.com>
2024-06-16 17:07:48 +0800

    chore: console.log

--- packages/core/database/src/repository.ts
+++ packages/core/database/src/repository.ts
@@ -697,7 +697,6 @@ export class Repository<TModelAttributes extends {} = any, TCreationAttributes e
           ...options,
           transaction,
         });
-        console.log(`${this.collection.name}.afterSaveWithAssociations`);
         await this.database.emitAsync(`${this.collection.name}.afterSaveWithAssociations`, instance, {
           ...options,
           transaction,
