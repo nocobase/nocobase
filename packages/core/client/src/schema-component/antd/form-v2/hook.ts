4671a17628d107bc49b3d194340ca30e7484dabe
Katherine <shunai.tang@hand-china.com>
2024-06-20 21:21:55 +0800

    fix: subform/subtable height equaling form height when form height is set (#4717)
    * fix: subform height equaling form height when form height is set
    
    * fix: bug
    
    * fix: bug
    
    * fix: bug
    
    * fix: bug
    
    * fix: bug
    
    * fix: bug
    
    * fix: bug
    
    * fix: bug

--- packages/core/client/src/schema-component/antd/form-v2/hook.ts
+++ packages/core/client/src/schema-component/antd/form-v2/hook.ts
@@ -11,7 +11,7 @@ import { theme } from 'antd';
 import { useFieldSchema } from '@formily/react';
 import { useDataBlockHeight } from '../../hooks/useBlockSize';
 import { useDesignable } from '../../';
-import { useDataBlockRequest } from '../../../data-source';
+import { useCollection, useDataBlockRequest } from '../../../data-source';
 import { useFormDataTemplates } from './Templates';
 import { useBlockHeightProps } from '../../../block-provider/hooks/useBlockHeightProps';
 
@@ -31,12 +31,34 @@ export const useFormBlockHeight = () => {
   });
 
   const hasFormActions = Object.keys(actionSchema?.properties || {}).length > 0;
-  const actionBarHeight = hasFormActions || designable ? token.controlHeight + 2 * token.marginLG : 2 * token.marginLG;
+  const isFormBlock = schema?.parent?.['x-decorator']?.includes?.('FormBlockProvider');
+  const actionBarPadding = () => {
+    if (isFormBlock) {
+      return designable ? 2 : 1;
+    }
+    return 2;
+  };
+
+  const actionBarHeight =
+    hasFormActions || designable ? token.controlHeight + actionBarPadding() * token.marginLG : 1 * token.marginLG;
   const blockTitleHeaderHeight = title ? token.fontSizeLG * token.lineHeightLG + token.padding * 2 - 1 : 0;
   const { data } = useDataBlockRequest() || {};
   const { count, pageSize } = (data as any)?.meta || ({} as any);
   const hasPagination = count > pageSize;
-  const paginationHeight = hasPagination ? token.controlHeightSM + 1 * token.paddingLG : 0;
+  const paginationHeight = hasPagination ? token.controlHeightSM + (designable ? 1 : 0) * token.paddingLG : 0;
   const dataTemplateHeight = display && enabled ? token.controlHeight + 2 * token.padding + token.margin : 0;
-  return height - actionBarHeight - token.paddingLG - blockTitleHeaderHeight - paginationHeight - dataTemplateHeight;
+  const blockBottomPadding = () => {
+    if (!isFormBlock && !hasPagination) {
+      return designable ? 1 : 0;
+    }
+    return 1;
+  };
+  return (
+    height -
+    actionBarHeight -
+    blockBottomPadding() * token.paddingLG -
+    blockTitleHeaderHeight -
+    paginationHeight -
+    dataTemplateHeight
+  );
 };
