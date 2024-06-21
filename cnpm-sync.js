9827192518079fdf7f10f380d17040e5329a44e6
chenos <chenlinxh@gmail.com>
2024-06-21 13:03:01 +0800

    chore: cnpm sync

--- cnpm-sync.js
+++ cnpm-sync.js
@@ -18,8 +18,26 @@ const files = glob.sync(path.resolve(__dirname, './node_modules/@nocobase/**/pac
       if (latest !== lerna.version) {
         console.log(json.name, latest);
         console.log(`https://www.npmmirror.com/package/${json.name}`);
-        const response = await axios.put(`https://registry-direct.npmmirror.com/-/package/${json.name}/syncs`);
-        console.log(response.data);
+
+        const res = await fetch(`https://registry-direct.npmmirror.com/-/package/${json.name}/syncs`, {
+          headers: {
+            accept: '*/*',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            priority: 'u=1, i',
+            'sec-ch-ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+            Referer: 'https://www.npmmirror.com/',
+            'Referrer-Policy': 'strict-origin-when-cross-origin',
+          },
+          body: null,
+          method: 'PUT',
+        });
+        // const response = await axios.put(`https://registry-direct.npmmirror.com/-/package/${json.name}/syncs`);
+        console.log(await res.json());
         await new Promise((resolve) => {
           setTimeout(() => {
             resolve(null);
