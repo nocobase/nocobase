6acfda957899ded2af40e7359897bd88df99ba48
Zeke Zhang <958414905@qq.com>
2024-06-25 11:25:08 +0800

    feat(variable): add a new variable named 'Parent popup record'

--- packages/core/client/src/modules/variable/variablesProvider/CurrentPopupRecordProvider.tsx
+++ packages/core/client/src/modules/variable/variablesProvider/CurrentPopupRecordProvider.tsx
@@ -13,6 +13,7 @@ import { useCollectionRecordData } from '../../../data-source/collection-record/
 import { Collection } from '../../../data-source/collection/Collection';
 import { useCollection } from '../../../data-source/collection/CollectionProvider';
 import { DeclareVariable } from '../DeclareVariable';
+import { useVariable } from '../useVariable';
 
 export const VariablePopupRecordProvider: FC<{
   recordData?: Record<string, any>;
@@ -21,15 +22,23 @@ export const VariablePopupRecordProvider: FC<{
   const { t } = useTranslation();
   const recordData = useCollectionRecordData();
   const collection = useCollection();
+  const parent = useVariable('$nPopupRecord');
 
   return (
     <DeclareVariable
-      name="$nPopupRecord"
-      title={t('Current popup record')}
-      value={props.recordData || recordData}
-      collection={props.collection || collection}
+      name="$nParentPopupRecord"
+      title={t('Parent popup record')}
+      value={parent.value}
+      collection={parent.collection}
     >
-      {props.children}
+      <DeclareVariable
+        name="$nPopupRecord"
+        title={t('Current popup record')}
+        value={props.recordData || recordData}
+        collection={props.collection || collection}
+      >
+        {props.children}
+      </DeclareVariable>
     </DeclareVariable>
   );
 };
