92e595924bc4f28f742f65e8f8c8f39b31349e74
katherinehhh <katherine_15995@163.com>
2024-06-26 15:21:02 +0800

    fix: useFormHeight

--- packages/core/client/src/schema-component/antd/form-v2/hook.ts
+++ packages/core/client/src/schema-component/antd/form-v2/hook.ts
@@ -11,7 +11,7 @@ import { theme } from 'antd';
 import { useFieldSchema } from '@formily/react';
 import { useDataBlockHeight } from '../../hooks/useBlockSize';
 import { useDesignable } from '../../';
-import { useCollection, useDataBlockRequest } from '../../../data-source';
+import { useDataBlockRequest } from '../../../data-source';
 import { useFormDataTemplates } from './Templates';
 import { useBlockHeightProps } from '../../../block-provider/hooks/useBlockHeightProps';
 
@@ -34,22 +34,24 @@ export const useFormBlockHeight = () => {
   const isFormBlock = schema?.parent?.['x-decorator']?.includes?.('FormBlockProvider');
   const actionBarPadding = () => {
     if (isFormBlock) {
-      return designable ? 2 : 1;
+      return designable ? 2 : 2;
     }
     return 2;
   };
 
   const actionBarHeight =
-    hasFormActions || designable ? token.controlHeight + actionBarPadding() * token.marginLG : 1 * token.marginLG;
+    hasFormActions || designable
+      ? token.controlHeight + actionBarPadding() * token.marginLG
+      : (isFormBlock ? 2 : 1) * token.marginLG;
   const blockTitleHeaderHeight = title ? token.fontSizeLG * token.lineHeightLG + token.padding * 2 - 1 : 0;
   const { data } = useDataBlockRequest() || {};
   const { count, pageSize } = (data as any)?.meta || ({} as any);
   const hasPagination = count > pageSize;
-  const paginationHeight = hasPagination ? token.controlHeightSM + (designable ? 1 : 0) * token.paddingLG : 0;
+  const paginationHeight = hasPagination ? token.controlHeightSM + (designable ? 1 : 1) * token.paddingLG : 0;
   const dataTemplateHeight = display && enabled ? token.controlHeight + 2 * token.padding + token.margin : 0;
   const blockBottomPadding = () => {
     if (!isFormBlock && !hasPagination) {
-      return designable ? 1 : 0;
+      return designable ? 1 : 1;
     }
     return 1;
   };
