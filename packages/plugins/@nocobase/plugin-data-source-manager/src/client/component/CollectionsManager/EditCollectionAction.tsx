b516b920daec6e256259281d09b9d9223ee663e2
chenos <chenlinxh@gmail.com>
2024-06-28 09:00:16 +0800

    feat(data-source-manager): filterTargetKey configuration optimization (#4766)
    * feat(data-source-manager): filterTargetKey configuration optimization
    
    * fix: bugs
    
    * fix: improve code
    
    * fix: error
    
    * fix: error

--- packages/plugins/@nocobase/plugin-data-source-manager/src/client/component/CollectionsManager/EditCollectionAction.tsx
+++ packages/plugins/@nocobase/plugin-data-source-manager/src/client/component/CollectionsManager/EditCollectionAction.tsx
@@ -10,7 +10,6 @@
 import { ArrayTable } from '@formily/antd-v5';
 import { ISchema, useForm } from '@formily/react';
 import { uid } from '@formily/shared';
-import { tval } from '@nocobase/utils/client';
 import {
   ActionContextProvider,
   IField,
@@ -18,15 +17,16 @@ import {
   SchemaComponent,
   useAPIClient,
   useActionContext,
+  useCancelAction,
   useCollectionManager_deprecated,
   useCompile,
+  useDataSourceManager,
   useRecord,
   useRequest,
   useResourceActionContext,
   useResourceContext,
-  useCancelAction,
-  useDataSourceManager,
 } from '@nocobase/client';
+import { tval } from '@nocobase/utils/client';
 import cloneDeep from 'lodash/cloneDeep';
 import omit from 'lodash/omit';
 import React, { useEffect, useState } from 'react';
@@ -69,10 +69,10 @@ const getSchema = (schema: IField, record: any, compile, getContainer): ISchema 
         properties: {
           ...omit(properties, 'category', 'inherits', 'moreOptions'),
           filterTargetKey: {
-            title: `{{ t("Filter target key",{ ns: "${NAMESPACE}" }) }}`,
+            title: `{{ t("Record unique key") }}`,
             type: 'single',
             description: tval(
-              'Filter data based on the specific field, with the requirement that the field value must be unique.',
+              'If a collection lacks a primary key, you must configure a unique record key to locate row records within a block, failure to configure this will prevent the creation of data blocks for the collection.',
               { ns: NAMESPACE },
             ),
             'x-decorator': 'FormItem',
