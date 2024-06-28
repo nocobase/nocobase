a86315de61dda067928b2888bcdf0a63e2c3803a
chenos <chenlinxh@gmail.com>
2024-06-28 09:00:16 +0800

    feat(data-source-manager): filterTargetKey configuration optimization (#4766)
    * feat(data-source-manager): filterTargetKey configuration optimization
    
    * fix: bugs
    
    * fix: improve code
    
    * fix: error
    
    * fix: error

--- packages/plugins/@nocobase/plugin-data-source-manager/src/client/locale/index.ts
+++ packages/plugins/@nocobase/plugin-data-source-manager/src/client/locale/index.ts
@@ -8,9 +8,14 @@
  */
 
 import { i18n } from '@nocobase/client';
+import { useTranslation } from 'react-i18next';
 
 export const NAMESPACE = 'data-source-manager';
 
 export function lang(key: string, options = {}) {
   return i18n.t(key, { ...options, ns: NAMESPACE });
+}
+
+export function useDSMTranslation() {
+  return useTranslation([NAMESPACE, 'client'], { nsMode: 'fallback' });
 }
