1e89383ed5e85d6a3b62db37ff347a0b6627e93e
Chareice <chareice@live.com>
2024-06-29 14:53:25 +0800

    chore: datasource logger

--- packages/core/data-source-manager/src/data-source-manager.ts
+++ packages/core/data-source-manager/src/data-source-manager.ts
@@ -10,8 +10,13 @@
 import { ToposortOptions } from '@nocobase/utils';
 import { DataSource } from './data-source';
 import { DataSourceFactory } from './data-source-factory';
+import { createConsoleLogger, createLogger, Logger, LoggerOptions } from '@nocobase/logger';
 
 type DataSourceHook = (dataSource: DataSource) => void;
+
+type DataSourceManagerOptions = {
+  logger?: LoggerOptions | Logger;
+};
 
 export class DataSourceManager {
   dataSources: Map<string, DataSource>;
@@ -23,7 +28,7 @@ export class DataSourceManager {
   private onceHooks: Array<DataSourceHook> = [];
   private beforeAddHooks: Array<DataSourceHook> = [];
 
-  constructor(public options = {}) {
+  constructor(public options: DataSourceManagerOptions = {}) {
     this.dataSources = new Map();
     this.middlewares = [];
   }
@@ -33,6 +38,20 @@ export class DataSourceManager {
   }
 
   async add(dataSource: DataSource, options: any = {}) {
+    let logger;
+
+    if (this.options.logger) {
+      if (typeof this.options.logger['log'] === 'function') {
+        logger = this.options.logger as Logger;
+      } else {
+        logger = createLogger(this.options.logger);
+      }
+    } else {
+      logger = createConsoleLogger();
+    }
+
+    dataSource.setLogger(logger);
+
     for (const hook of this.beforeAddHooks) {
       hook(dataSource);
     }
