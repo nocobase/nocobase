67f9d157a2b070ebfc29fe856f7bb8bd2e11f7ff
xilesun <2013xile@gmail.com>
2024-07-02 18:23:19 +0800

    fix: fix T-4670

--- packages/core/client/src/collection-manager/interfaces/json.tsx
+++ packages/core/client/src/collection-manager/interfaces/json.tsx
@@ -68,7 +68,7 @@ export class JsonFieldInterface extends CollectionFieldInterface {
       'x-disabled': `{{ disabledJSONB }}`,
     },
   };
-  filterable = {
-    operators: operators.string,
-  };
+  // filterable = {
+  //   operators: operators.string,
+  // };
 }
