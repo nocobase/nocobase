bba397f9edccd7f4175be3966901d2f1f3227dab
mytharcher <mytharcher@gmail.com>
2024-07-07 04:17:41 +0000

    refactor(server): refactor api

--- packages/core/server/src/sync-manager.ts
+++ packages/core/server/src/sync-manager.ts
@@ -12,49 +12,61 @@ import EventEmitter from 'node:events';
 import Application from './application';
 import { isEqual, uniqWith } from 'lodash';
 
-export abstract class SyncAdapter {
+export abstract class SyncAdapter extends EventEmitter {
   abstract get ready(): boolean;
-  abstract onSync(callback: Function): void;
-  abstract publish(data: Record<string, any>): void;
+  public abstract publish(data: Record<string, any>): void;
 }
 
-export type SyncMessageData = Record<string, any>;
+export type SyncMessageData = Record<string, string>;
 
 export type SyncEventCallback = (message: SyncMessageData) => void;
 
 export type SyncMessage = {
   namespace: string;
   nodeId: string;
+  appName: string;
 } & SyncMessageData;
 
+/**
+ * @experimental
+ */
 export class SyncManager {
   private nodeId: string;
   private app: Application;
   private eventEmitter = new EventEmitter();
   private adapter = null;
-  private buffer: SyncMessageData[] = [];
+  private incomingBuffer: SyncMessageData[] = [];
+  private outgoingBuffer: [string, SyncMessageData][] = [];
   private flushTimer: NodeJS.Timeout = null;
 
-  onSync = (messages: SyncMessage) => {
+  private onSync = (messages: SyncMessage[]) => {
+    this.app.logger.info('sync messages received into buffer:', messages);
     if (this.flushTimer) {
       clearTimeout(this.flushTimer);
       this.flushTimer = null;
     }
-    const uniqueMessages = uniqWith(
-      this.buffer.concat(
+    this.incomingBuffer = uniqWith(
+      this.incomingBuffer.concat(
         messages
-          .filter((item) => item.message.nodeId !== this.nodeId && item.message.appName === this.app.name)
-          .map(({ message: { nodeId, appName, ...message } }) => message),
+          .filter((item) => item.nodeId !== this.nodeId && item.appName === this.app.name)
+          .map(({ nodeId, appName, ...message }) => message),
       ),
       isEqual,
     );
-    this.buffer = uniqueMessages;
 
     this.flushTimer = setTimeout(() => {
-      this.buffer.forEach(({ namespace, ...message }) => {
+      this.incomingBuffer.forEach(({ namespace, ...message }) => {
+        this.app.logger.info(`emit sync event in namespace ${namespace}`);
         this.eventEmitter.emit(namespace, message);
       });
     }, 1000);
+  };
+
+  private onReady = () => {
+    while (this.outgoingBuffer.length) {
+      const [namespace, data] = this.outgoingBuffer.shift();
+      this.publish(namespace, data);
+    }
   };
 
   constructor(app: Application) {
@@ -62,15 +74,16 @@ export class SyncManager {
     this.nodeId = `${process.env.NODE_ID || randomUUID()}-${process.pid}`;
   }
 
-  public async use(adapter: SyncAdapter) {
+  public init(adapter: SyncAdapter) {
     if (this.adapter) {
-      throw new Error('Adapter already exists');
+      throw new Error('sync adapter is already exists');
     }
     if (!adapter) {
       return;
     }
     this.adapter = adapter;
-    this.adapter.onSync(this.onSync);
+    this.adapter.on('message', this.onSync);
+    this.adapter.on('ready', this.onReady);
   }
 
   public subscribe(namespace: string, callback: SyncEventCallback) {
@@ -88,6 +101,14 @@ export class SyncManager {
     if (!this.adapter) {
       return;
     }
-    this.adapter.publish({ ...data, nodeId: this.nodeId, appName: this.app.name, namespace });
+    if (!this.adapter.ready) {
+      this.outgoingBuffer.push([namespace, data]);
+      this.app.logger.warn(`sync adapter is not ready for now, message will be send when it is ready`);
+      return;
+    }
+    this.app.logger.info(`publishing sync message from #${this.nodeId} (${this.app.name}) in namespace ${namespace}:`, {
+      data,
+    });
+    return this.adapter.publish({ ...data, nodeId: this.nodeId, appName: this.app.name, namespace });
   }
 }
