65d73daadc93ab4d965c237c7845dd287a22113c
katherinehhh <katherine_15995@163.com>
2024-07-19 15:29:09 +0800

    test: fix test

--- packages/core/client/src/schema-component/antd/input/__tests__/Input.test.tsx
+++ packages/core/client/src/schema-component/antd/input/__tests__/Input.test.tsx
@@ -142,7 +142,7 @@ describe('Input.JSON', () => {
     await userEvent.type(textarea, '{{name:"nocobase"}');
     // mock blur event
     await userEvent.click(document.body);
-    expect(screen.getByText(/Expected property name /)).toBeInTheDocument();
+    expect(screen.getByText(/Unexpected token n/)).toBeInTheDocument();
   });
 
   it('should not display error when the value is valid JSON5', async () => {
