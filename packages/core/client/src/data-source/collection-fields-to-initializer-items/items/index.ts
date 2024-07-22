1be5b2f5787d1f55829c444ff76c72a3c05dfe6f
jack zhang <1098626505@qq.com>
2024-07-22 11:25:12 +0800

    refactor: collection fields to initializer items (#4900)
    * refactor: add collection-fields-initializer-items
    
    * fix: bug
    
    * fix: bug
    
    * fix: rename and docs
    
    * fix: change dir
    
    * docs: imporve doc
    
    * Update packages/core/client/docs/en-US/core/data-source/collection-fields-to-initializer-items.md
    
    Co-authored-by: Zeke Zhang <958414905@qq.com>
    
    * Update packages/core/client/docs/en-US/core/data-source/collection-fields-to-initializer-items.md
    
    Co-authored-by: Zeke Zhang <958414905@qq.com>
    
    ---------
    
    Co-authored-by: Zeke Zhang <958414905@qq.com>

---
+++ packages/core/client/src/data-source/collection-fields-to-initializer-items/items/index.ts
@@ -0,0 +1,3 @@
+export { SelfFields } from './SelfFields';
+export { ParentCollectionFields } from './ParentCollectionFields';
+export { AssociationCollectionFields } from './AssociationCollectionFields';
