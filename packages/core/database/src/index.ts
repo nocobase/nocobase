5e3b886051945c63399cc068b3f6499123f3f03b
jimmy201602 <zhengge2012@gmail.com>
2024-07-25 15:39:05 +0800

    feat(collection-tree): merge the next branch code to fix confilct

--- packages/core/database/src/index.ts
+++ packages/core/database/src/index.ts
@@ -53,4 +53,5 @@ export * from './view/view-inference';
 export * from './helpers';
 export { default as sqlParser, SQLParserTypes } from './sql-parser';
 export * from './interfaces';
+export { default as fieldTypeMap } from './view/field-type-map';
 export * from './repositories/tree-repository/adjacency-list-repository';
