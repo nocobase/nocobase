c6f41bb81444d38dafea38a8ba0741f87ab59153
Chareice <chareice@live.com>
2024-07-26 23:35:48 +0800

    fix: test

--- packages/plugins/@nocobase/plugin-data-source-main/src/server/hooks/beforeCreateCheckFieldInMySQL.ts
+++ packages/plugins/@nocobase/plugin-data-source-main/src/server/hooks/beforeCreateCheckFieldInMySQL.ts
@@ -16,16 +16,27 @@ export function beforeCreateCheckFieldInMySQL(db) {
     const fieldOptions = model.get();
     if (fieldOptions.autoIncrement) {
       const collection = db.getCollection(fieldOptions.collectionName);
+
       if (!collection) {
         return;
       }
-      const rawAttributes = collection.model.rawAttributes;
-      console.log('rawAttributes', rawAttributes);
 
-      for (const fieldName in Object.keys(rawAttributes)) {
-        const field = rawAttributes[fieldName];
+      const rawAttributes = collection.model.rawAttributes;
+
+      const fields = Object.keys(rawAttributes);
+
+      for (const key of fields) {
+        if (key === fieldOptions.name) {
+          continue;
+        }
+
+        const field = rawAttributes[key];
         if (field.autoIncrement) {
-          throw new Error('Only one autoIncrement field is allowed in a table');
+          throw new Error(
+            `Can not add field ${
+              fieldOptions.name
+            }, autoIncrement field ${key} is already in a table ${collection.getTableNameWithSchemaAsString()}`,
+          );
         }
       }
     }
