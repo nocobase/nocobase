6201da87d179cc7be2e6486f2bf7a0cfdd041dd0
jimmy201602 <zhengge2012@gmail.com>
2024-07-29 11:05:39 +0800

    feat(collection-tree): remove unnecessary plugin for test case code

--- packages/plugins/@nocobase/plugin-data-source-main/src/server/__tests__/index.ts
+++ packages/plugins/@nocobase/plugin-data-source-main/src/server/__tests__/index.ts
@@ -13,7 +13,7 @@ export async function createApp(options: any = {}) {
   const app = await createMockServer({
     acl: false,
     ...options,
-    plugins: ['error-handler', 'data-source-main', 'ui-schema-storage', 'collection-tree'],
+    plugins: ['error-handler', 'data-source-main', 'ui-schema-storage'],
   });
   return app;
 }
