2edd3e1f1f2a075185467af57a8915c94595f703
jack zhang <1098626505@qq.com>
2024-07-30 09:59:40 +0800

    fix: improve i18n `tStr()`

--- packages/core/cli/templates/plugin/src/client/locale.ts
+++ packages/core/cli/templates/plugin/src/client/locale.ts
@@ -4,9 +4,9 @@ import { useApp } from '@nocobase/client';
 
 export function useT() {
   const app = useApp();
-  return (str: string) => app.i18n.t(str, { ns: pkg.name });
+  return (str: string) => app.i18n.t(str, { ns: [pkg.name, 'client'] });
 }
 
 export function tStr(key: string) {
-  return `{{t('${key}', { ns: '${pkg.name}', nsMode: 'fallback' })}}`;
+  return `{{t(${JSON.stringify(key)}, { ns: ['${pkg.name}', 'client'], nsMode: 'fallback' })}}`;
 }
