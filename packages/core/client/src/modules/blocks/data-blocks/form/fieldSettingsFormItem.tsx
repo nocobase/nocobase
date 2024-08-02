cd415e8f127dbb3dbc77d25dacb4e3c7dc2e7d6a
Katherine <katherine_15995@163.com>
2024-08-02 10:14:07 +0800

    fix: required constraint becomes invalid after setting validation rules on form fields (#4977)
    * fix: required constraint becomes invalid after setting validation rules on form fields
    
    * fix: bug

--- packages/core/client/src/modules/blocks/data-blocks/form/fieldSettingsFormItem.tsx
+++ packages/core/client/src/modules/blocks/data-blocks/form/fieldSettingsFormItem.tsx
@@ -319,7 +319,7 @@ export const fieldSettingsFormItem = new SchemaSettings({
               const { getField } = useCollection_deprecated();
               const collectionField =
                 getField(fieldSchema['name']) || getCollectionJoinField(fieldSchema['x-collection-field']);
-
+              const customPredicate = (value) => value !== null && value !== undefined && !Number.isNaN(value);
               return {
                 title: t('Set validation rules'),
                 components: { ArrayCollapse, FormLayout },
@@ -408,7 +408,7 @@ export const fieldSettingsFormItem = new SchemaSettings({
                 onSubmit(v) {
                   const rules = [];
                   for (const rule of v.rules) {
-                    rules.push(_.pickBy(rule, _.identity));
+                    rules.push(_.pickBy(rule, customPredicate));
                   }
                   const schema = {
                     ['x-uid']: fieldSchema['x-uid'],
@@ -426,6 +426,7 @@ export const fieldSettingsFormItem = new SchemaSettings({
                   }
                   const concatValidator = _.concat([], collectionField?.uiSchema?.['x-validator'] || [], rules);
                   field.validator = concatValidator;
+                  field.required = fieldSchema.required as any;
                   fieldSchema['x-validator'] = rules;
                   schema['x-validator'] = rules;
                   dn.emit('patch', {
