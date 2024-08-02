27daaa2528cf9139437af745302d82e282558e5b
jimmy201602 <zhengge2012@gmail.com>
2024-08-02 14:45:31 +0800

    feat(collection-tree): add exist tree collection path migration function

---
+++ packages/plugins/@nocobase/plugin-collection-tree/src/server/migrations/20240802141435-collection-tree.ts
@@ -0,0 +1,81 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import { Migration } from '@nocobase/server';
+import { Model } from '@nocobase/database';
+
+export default class extends Migration {
+  on = 'afterLoad'; // 'beforeLoad' or 'afterLoad'
+  appVersion = '<=1.3.0-alpha';
+
+  async up() {
+    const treeCollections = await this.app.db.getRepository('collections').find({
+      appends: ['fields'],
+      filter: {
+        'options.tree': 'adjacencyList',
+      },
+    });
+
+    for (const treeCollection of treeCollections) {
+      const name = `main_${treeCollection.name}_path`;
+      this.db.collection({
+        name,
+        autoGenId: false,
+        timestamps: false,
+        fields: [
+          { type: 'integer', name: 'nodePk' },
+          { type: 'jsonb', name: 'path' },
+          { type: 'integer', name: 'rootPK' },
+        ],
+      });
+      const treeExistsInDb = await this.app.db.getCollection(name).existsInDb();
+      if (!treeExistsInDb) {
+        await this.db.getCollection(name).sync();
+        this.db.collection({
+          name: treeCollection.name,
+          autoGenId: false,
+          timestamps: false,
+          fields: [
+            { type: 'integer', name: 'id' },
+            { type: 'integer', name: 'parentId' },
+          ],
+        });
+        const existDatas = await this.app.db.getRepository(treeCollection.name).find({});
+        for (const data of existDatas) {
+          let path = `/${data.dataValues?.id}`;
+          path = await this.getTreePath(data, path, treeCollection.name);
+          await this.app.db.getRepository(name).create({
+            values: {
+              nodePk: data.dataValues?.id,
+              path: path,
+              rootPK: path.split('/')[1],
+            },
+          });
+        }
+      }
+    }
+  }
+
+  async getTreePath(model: Model, path: string, collectionName: string) {
+    if (model.dataValues?.parentId !== null) {
+      const parent = await this.app.db.getRepository(collectionName).findOne({
+        filter: {
+          id: model.dataValues?.parentId,
+        },
+      });
+      if (parent) {
+        path = `/${parent.dataValues?.id}${path}`;
+        if (parent.dataValues?.parentId !== null) {
+          path = await this.getTreePath(parent, path, collectionName);
+        }
+      }
+    }
+    return path;
+  }
+}
