6b486d426aff8f0a6fabb65f4f6478ede14709d8
jack zhang <1098626505@qq.com>
2024-08-04 10:37:32 +0800

    fix: move encryption plugin to pro

--- packages/plugins/@nocobase/plugin-field-encryption/README.md
+++
@@ -1,1 +0,0 @@
-# @nocobase/plugin-field-encryption
--- packages/presets/nocobase/src/server/index.ts
+++ packages/presets/nocobase/src/server/index.ts
@@ -53,7 +53,6 @@ export class PresetNocoBase extends Plugin {
     'action-duplicate',
     'action-print',
     'collection-sql',
-    'field-encryption',
   ];
 
   localPlugins = [
