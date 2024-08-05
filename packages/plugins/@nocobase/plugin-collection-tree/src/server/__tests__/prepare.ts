58722a81e7259499d386fceeef4d83b84aef412f
jimmy201602 <zhengge2012@gmail.com>
2024-08-05 18:07:01 +0800

    feat(collection-tree): remove unused library

--- packages/plugins/@nocobase/plugin-collection-tree/src/server/__tests__/prepare.ts
+++ packages/plugins/@nocobase/plugin-collection-tree/src/server/__tests__/prepare.ts
@@ -13,16 +13,7 @@ export async function prepareApp(): Promise<MockServer> {
   const app = await createMockServer({
     registerActions: true,
     acl: true,
-    plugins: [
-      'acl',
-      'error-handler',
-      'users',
-      'ui-schema-storage',
-      'data-source-main',
-      'auth',
-      'data-source-manager',
-      'collection-tree',
-    ],
+    plugins: ['acl', 'error-handler', 'users', 'ui-schema-storage', 'data-source-main', 'data-source-manager'],
   });
 
   return app;
