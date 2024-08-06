b068ee83579b4c2a9ca16b7b527bf9d61718f5e4
jack zhang <1098626505@qq.com>
2024-08-06 16:08:11 +0800

    fix: bug

--- packages/core/database/src/fields/encryption-field/utils.ts
+++ packages/core/database/src/fields/encryption-field/utils.ts
@@ -109,17 +109,17 @@ export function checkValueAndIv(type: 'Decrypt' | 'Encrypt', value: string, iv: 
     throw new EncryptionError(msg + 'The value must be a string, but got ' + typeof value);
   }
 
-  if (typeof value !== 'string') {
-    throw new EncryptionError(msg + 'The `iv` must be a string, but got ' + typeof value);
-  }
-
-  if (iv.length !== 16) {
-    throw new EncryptionError(msg + 'The `iv` must be a 16-character string');
-  }
-
   if (type === 'Decrypt') {
     if (value.length % 2 !== 0) {
       throw new EncryptionError(msg + `The encrypted value is invalid, not a hex string. The value is "${value}"`);
     }
+  }
+
+  if (typeof iv !== 'string') {
+    throw new EncryptionError(msg + 'The `iv` must be a string, but got ' + typeof iv);
+  }
+
+  if (iv.length !== 16) {
+    throw new EncryptionError(msg + 'The `iv` must be a 16-character string');
   }
 }
