84593b8ff19785d3000ca75cff24311f3e5963c6
SuperGoo <gu.zhichao@nint.com>
2024-08-08 12:16:39 +0800

    fix: quote table name when get mysql table define

--- packages/core/database/src/query-interface/mysql-query-interface.ts
+++ packages/core/database/src/query-interface/mysql-query-interface.ts
@@ -91,7 +91,11 @@ export default class MysqlQueryInterface extends QueryInterface {
   async showTableDefinition(tableInfo: TableInfo): Promise<any> {
     const { tableName } = tableInfo;
 
-    const sql = `SHOW CREATE TABLE ${tableName}`;
+    //nint quote table name for it may be special word like group
+    let newTableName = tableName;
+    if (!tableName.startsWith('`')) newTableName = this.db.utils.quoteTable(tableName);
+    const sql = `SHOW CREATE TABLE ${newTableName}`;
+    //const sql = `SHOW CREATE TABLE ${tableName}`;
 
     const results = await this.db.sequelize.query(sql, { type: 'SELECT' });
 
