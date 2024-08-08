7e1d604355048140a259fd49bdaae5538588d991
jimmy201602 <zhengge2012@gmail.com>
2024-08-09 07:28:39 +0800

    feat(collection-tree): fix collection tree migration bug

--- packages/plugins/@nocobase/plugin-collection-tree/src/server/migrations/20240802141435-collection-tree.ts
+++ packages/plugins/@nocobase/plugin-collection-tree/src/server/migrations/20240802141435-collection-tree.ts
@@ -8,7 +8,7 @@
  */
 
 import { Migration } from '@nocobase/server';
-import { FindOneOptions, Model } from '@nocobase/database';
+import { FindOneOptions, Model, SyncOptions } from '@nocobase/database';
 import { Transaction } from 'sequelize';
 
 export default class extends Migration {
@@ -16,65 +16,65 @@ export default class extends Migration {
   appVersion = '<=1.3.0-alpha';
 
   async up() {
-    const transaction = await this.db.sequelize.transaction();
-    const treeCollections = await this.app.db.getRepository('collections').find({
-      appends: ['fields'],
-      filter: {
-        'options.tree': 'adjacencyList',
-      },
-      transaction,
-    });
-
-    for (const treeCollection of treeCollections) {
-      const name = `main_${treeCollection.name}_path`;
-      this.app.db.collection({
-        name,
-        autoGenId: false,
-        timestamps: false,
-        fields: [
-          { type: 'integer', name: 'nodePk' },
-          { type: 'string', name: 'path', length: 1024 },
-          { type: 'integer', name: 'rootPk' },
-        ],
-        indexes: [
-          {
-            fields: ['path'],
-          },
-        ],
+    await this.db.sequelize.transaction(async (transaction) => {
+      const treeCollections = await this.app.db.getRepository('collections').find({
+        appends: ['fields'],
+        filter: {
+          'options.tree': 'adjacencyList',
+        },
+        transaction,
       });
-      const treeExistsInDb = await this.app.db.getCollection(name).existsInDb();
-      if (!treeExistsInDb) {
-        await this.app.db.getCollection(name).sync();
+
+      for (const treeCollection of treeCollections) {
+        const name = `main_${treeCollection.name}_path`;
         this.app.db.collection({
-          name: treeCollection.name,
+          name,
           autoGenId: false,
           timestamps: false,
           fields: [
-            { type: 'integer', name: 'id' },
-            { type: 'integer', name: 'parentId' },
+            { type: 'integer', name: 'nodePk' },
+            { type: 'string', name: 'path', length: 1024 },
+            { type: 'integer', name: 'rootPk' },
+          ],
+          indexes: [
+            {
+              fields: ['path'],
+            },
           ],
         });
-        const chunkSize = 1000;
-        await this.app.db.getRepository(treeCollection.name).chunk({
-          chunkSize: chunkSize,
-          callback: async (rows, options) => {
-            const pathData = [];
-            for (const data of rows) {
-              let path = `/${data.get('id')}`;
-              path = await this.getTreePath(data, path, treeCollection.name, transaction);
-              pathData.push({
-                nodePk: data.get('id'),
-                path: path,
-                rootPk: path.split('/')[1],
-              });
-            }
-            await this.app.db.getModel(name).bulkCreate(pathData);
-          },
-          transaction,
-        });
+        const treeExistsInDb = await this.app.db.getCollection(name).existsInDb({ transaction });
+        if (!treeExistsInDb) {
+          await this.app.db.getCollection(name).sync({ transaction } as SyncOptions);
+          this.app.db.collection({
+            name: treeCollection.name,
+            autoGenId: false,
+            timestamps: false,
+            fields: [
+              { type: 'integer', name: 'id' },
+              { type: 'integer', name: 'parentId' },
+            ],
+          });
+          const chunkSize = 1000;
+          await this.app.db.getRepository(treeCollection.name).chunk({
+            chunkSize: chunkSize,
+            callback: async (rows, options) => {
+              const pathData = [];
+              for (const data of rows) {
+                let path = `/${data.get('id')}`;
+                path = await this.getTreePath(data, path, treeCollection.name, transaction);
+                pathData.push({
+                  nodePk: data.get('id'),
+                  path: path,
+                  rootPk: path.split('/')[1],
+                });
+              }
+              await this.app.db.getModel(name).bulkCreate(pathData, { transaction });
+            },
+            transaction,
+          });
+        }
       }
-    }
-    await transaction.commit();
+    });
   }
 
   async getTreePath(model: Model, path: string, collectionName: string, transaction: Transaction) {
