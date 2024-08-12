c2aee45c2a74d9949f44a203ff59ea1d81450dcd
陈智 <350447433@qq.com>
2024-08-12 22:27:00 +0800

    fix: adjust log

--- packages/plugins/@nocobase/plugin-user-data-sync/src/server/user-data-sync-service.ts
+++ packages/plugins/@nocobase/plugin-user-data-sync/src/server/user-data-sync-service.ts
@@ -28,7 +28,7 @@ export class UserDataSyncService {
     const source = await this.sourceManager.getByName(sourceName, ctx);
     const task = await source.newTask();
     await source.beginTask(task.id);
-    ctx.log.info(`begin sync task of source: ${sourceName} sourceType: ${source.instance.sourceType}`);
+    ctx.log.info('begin sync task of source', { source: sourceName, sourceType: source.instance.sourceType });
     this.runSync(source, task, ctx);
   }
 
@@ -45,27 +45,41 @@ export class UserDataSyncService {
   async retry(sourceId: number, taskId: number, ctx: Context) {
     const source = await this.sourceManager.getById(sourceId, ctx);
     const task = await source.retryTask(taskId);
-    ctx.log.info(`retry sync task of source: ${source.instance.name} sourceType: ${source.instance.sourceType}`);
+    ctx.log.info('retry sync task of source', {
+      source: source.instance.name,
+      sourceType: source.instance.name,
+      task: task.id,
+    });
     this.runSync(source, task, ctx);
   }
 
   async runSync(source: SyncSource, task: any, ctx: Context) {
     const currentTimeMillis = new Date().getTime();
     try {
-      ctx.log.info(`begin pull data of source: ${source.instance.name} sourceType: ${source.instance.sourceType}`);
+      ctx.log.info('begin pull data of source', {
+        source: source.instance.name,
+        sourceType: source.instance.sourceType,
+      });
       const data = await source.pull();
       // 输出拉取的数据
-      this.logger.info(
-        `pull data of source: ${source.instance.name} sourceType: ${source.instance.sourceType} batch:${
-          task.batch
-        } data: ${JSON.stringify(data)}`,
-      );
-      ctx.log.info(`end pull data of source: ${source.instance.name} sourceType: ${source.instance.sourceType}`);
-      ctx.log.info(`begin update data of source: ${source.instance.name} sourceType: ${source.instance.sourceType}`);
+      this.logger.info('pull data of source', {
+        source: source.instance.name,
+        sourceType: source.instance.sourceType,
+        batch: task.batch,
+        data: data,
+      });
+      ctx.log.info('end pull data of source', { source: source.instance.name, sourceType: source.instance.sourceType });
+      ctx.log.info('begin update data of source', {
+        source: source.instance.name,
+        sourceType: source.instance.sourceType,
+      });
       for (const item of data) {
         await this.resourceManager.updateOrCreate(item);
       }
-      ctx.log.info(`end update data of source: ${source.instance.name} sourceType: ${source.instance.sourceType}`);
+      ctx.log.info('end update data of source', {
+        source: source.instance.name,
+        sourceType: source.instance.sourceType,
+      });
       const costTime = new Date().getTime() - currentTimeMillis;
       await source.endTask({ taskId: task.id, success: true, cost: costTime });
     } catch (err) {
