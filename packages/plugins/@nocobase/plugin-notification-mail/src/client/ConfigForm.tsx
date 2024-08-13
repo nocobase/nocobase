042b50720b6b495d20bacfee7f8ea2be684de526
Sheldon Guo <sheldon_66@163.com>
2024-08-13 09:31:33 +0800

    refactor: update mail server configuration form schema and UI

--- packages/plugins/@nocobase/plugin-notification-mail/src/client/ConfigForm.tsx
+++ packages/plugins/@nocobase/plugin-notification-mail/src/client/ConfigForm.tsx
@@ -18,39 +18,108 @@ export const ChannelConfigForm = () => {
       schema={{
         type: 'object',
         properties: {
-          host: {
-            'x-decorator': 'FormItem',
-            type: 'boolean',
-            title: '{{t("Host")}}',
-            'x-component': 'Input',
-            required: true,
+          server: {
+            type: 'void',
+            'x-component': 'Grid',
+            properties: {
+              row: {
+                type: 'void',
+                'x-component': 'Grid.Row',
+                properties: {
+                  col1: {
+                    type: 'void',
+                    'x-component': 'Grid.Col',
+                    'x-component-props': {
+                      width: 50,
+                    },
+                    properties: {
+                      host: {
+                        'x-decorator': 'FormItem',
+                        type: 'string',
+                        title: '{{t("Host")}}',
+                        'x-component': 'Input',
+                        required: true,
+                      },
+                    },
+                  },
+                  col2: {
+                    type: 'void',
+                    'x-component': 'Grid.Col',
+                    'x-component-props': {
+                      width: 16,
+                    },
+                    properties: {
+                      port: {
+                        'x-decorator': 'FormItem',
+                        type: 'number',
+                        title: '{{t("Port")}}',
+                        'x-component': 'InputNumber',
+                        'x-component-props': {
+                          min: 1,
+                          max: 65535,
+                          step: 1,
+                        },
+                        default: 465,
+                        required: true,
+                      },
+                    },
+                  },
+                  col3: {
+                    type: 'void',
+                    'x-component': 'Grid.Col',
+                    'x-component-props': {
+                      width: 16,
+                    },
+                    properties: {
+                      secure: {
+                        'x-decorator': 'FormItem',
+                        type: 'boolean',
+                        title: '{{t("Secure")}}',
+                        'x-component': 'Checkbox',
+                      },
+                    },
+                  },
+                },
+              },
+            },
           },
-          port: {
-            'x-decorator': 'FormItem',
-            type: 'boolean',
-            title: '{{t("Port")}}',
-            'x-component': 'Input',
-            required: true,
-          },
-          secure: {
-            'x-decorator': 'FormItem',
-            type: 'boolean',
-            title: '{{t("Secure")}}',
-            'x-component': 'Checkbox',
-          },
-          account: {
-            'x-decorator': 'FormItem',
-            type: 'boolean',
-            title: '{{t("Account")}}',
-            'x-component': 'Input',
-            required: true,
-          },
-          password: {
-            'x-decorator': 'FormItem',
-            type: 'boolean',
-            title: '{{t("Password")}}',
-            'x-component': 'Input',
-            required: true,
+          auth: {
+            type: 'void',
+            'x-component': 'Grid',
+            properties: {
+              row: {
+                type: 'void',
+                'x-component': 'Grid.Row',
+                properties: {
+                  col1: {
+                    type: 'void',
+                    'x-component': 'Grid.Col',
+                    properties: {
+                      account: {
+                        'x-decorator': 'FormItem',
+                        type: 'boolean',
+                        title: '{{t("Account")}}',
+                        'x-component': 'Input',
+                        required: true,
+                      },
+                    },
+                  },
+                  col2: {
+                    type: 'void',
+                    'x-component': 'Grid.Col',
+                    properties: {
+                      password: {
+                        'x-decorator': 'FormItem',
+                        type: 'boolean',
+                        title: '{{t("Password")}}',
+                        'x-component': 'Input',
+                        required: true,
+                      },
+                    },
+                  },
+                },
+              },
+            },
           },
         },
       }}
