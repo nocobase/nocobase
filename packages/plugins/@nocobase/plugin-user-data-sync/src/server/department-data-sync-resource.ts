ecb6c9ff33019850e1d1a26f07910bb9b0d240a3
xilesun <2013xile@gmail.com>
2024-08-19 11:48:37 +0800

    chore: department

---
+++ packages/plugins/@nocobase/plugin-user-data-sync/src/server/department-data-sync-resource.ts
@@ -0,0 +1,178 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import { Model } from '@nocobase/database';
+import {
+  FormatDepartment,
+  FormatUser,
+  OriginRecord,
+  SyncAccept,
+  UserDataResource,
+} from '@nocobase/plugin-user-data-sync';
+
+export class DepartmentDataSyncResource extends UserDataResource {
+  name = 'departments';
+  accepts: SyncAccept[] = [
+    {
+      dataType: 'user',
+      associateResource: 'users',
+    },
+    'department',
+  ];
+
+  get userRepo() {
+    return this.db.getRepository('users');
+  }
+
+  get deptRepo() {
+    return this.db.getRepository('departments');
+  }
+
+  async update(record: OriginRecord, resourcePk: number): Promise<void> {
+    const { dataType, metaData, sourceName } = record;
+    if (dataType === 'user') {
+      const sourceUser = metaData;
+      const user = await this.userRepo.findOne({
+        filterByTk: resourcePk,
+      });
+      await this.updateUserDepartments(user, sourceUser.departments, sourceName);
+    } else if (dataType === 'department') {
+      const sourceDepartment = metaData;
+      const department = await this.deptRepo.findOne({
+        filterByTk: resourcePk,
+      });
+      await this.updateDepartment(department, sourceDepartment, sourceName);
+    } else {
+      this.logger.warn(`update department: unsupported data type: ${dataType}`);
+    }
+  }
+
+  async create(record: OriginRecord, uniqueKey: string): Promise<string> {
+    const { dataType, metaData, sourceName } = record;
+    if (dataType === 'user') {
+      return;
+    } else if (dataType === 'department') {
+      const sourceDepartment = metaData;
+      return await this.createDepartment(sourceDepartment, sourceName);
+    } else {
+      this.logger.warn(`create department: unsupported data type: ${dataType}`);
+    }
+    return;
+  }
+
+  async updateUserDepartments(user: FormatUser, sourceDepartmentIds: (number | string)[], sourceName: string) {
+    if (!this.deptRepo) {
+      return;
+    }
+    if (!sourceDepartmentIds || !sourceDepartmentIds.length) {
+      const userDepartments = await user.getDepartments();
+      if (userDepartments.length) {
+        await user.removeDepartments(userDepartments);
+      }
+    } else {
+      // 查询部门同步记录
+      const syncDepartmentRecords = await this.syncRecordRepo.find({
+        filter: {
+          sourceName,
+          dataType: 'department',
+          sourceUk: { $in: sourceDepartmentIds },
+          'resources.resource': this.name,
+        },
+        appends: ['resources'],
+      });
+      const departmentIds = syncDepartmentRecords
+        .filter((record) => record.resources?.length)
+        .map((record) => record.resources[0].resourcePk);
+      const departments = await this.deptRepo.find({ filter: { id: { $in: departmentIds } } });
+      const userDepartments = await user.getDepartments();
+      // 需要删除的部门
+      const toRemoveDepartments = userDepartments.filter((department) => {
+        return !departments.find((sourceDepartment) => sourceDepartment.id === department.id);
+      });
+      if (toRemoveDepartments.length) {
+        await user.removeDepartments(toRemoveDepartments);
+      }
+      // 需要添加的部门
+      const toAddDepartments = departments.filter((department) => {
+        if (userDepartments.length === 0) {
+          return true;
+        }
+        return !userDepartments.find((userDepartment) => userDepartment.id === department.id);
+      });
+      if (toAddDepartments.length) {
+        await user.addDepartments(toAddDepartments);
+      }
+    }
+  }
+
+  async updateDepartment(department: Model, sourceDepartment: FormatDepartment, sourceName: string) {
+    if (sourceDepartment.isDeleted) {
+      // 删除部门
+      await department.destroy();
+      return;
+    }
+    let dataChanged = false;
+    if (sourceDepartment.title !== undefined && department.title !== sourceDepartment.title) {
+      department.title = sourceDepartment.title;
+      dataChanged = true;
+    }
+    if (dataChanged) {
+      await department.save();
+    }
+    await this.updateParentDepartment(department, sourceDepartment.parentId, sourceName);
+  }
+
+  async createDepartment(sourceDepartment: FormatDepartment, sourceName: string): Promise<string> {
+    const department = await this.deptRepo.create({
+      values: {
+        title: sourceDepartment.title,
+      },
+    });
+    await this.updateParentDepartment(department, sourceDepartment.parentId, sourceName);
+    return department.id;
+  }
+
+  async updateParentDepartment(department: Model, parentId: string, sourceName: string) {
+    if (!parentId) {
+      const parentDepartment = await department.getParent();
+      if (parentDepartment) {
+        await department.setParent(null);
+      }
+    } else {
+      const syncDepartmentRecord = await this.syncRecordRepo.findOne({
+        filter: {
+          sourceName,
+          dataType: 'department',
+          sourceUk: parentId,
+          'resources.resource': this.name,
+        },
+        appends: ['resources'],
+      });
+      if (syncDepartmentRecord && syncDepartmentRecord.resources?.length) {
+        const parentDepartment = await this.deptRepo.findOne({
+          filterByTk: syncDepartmentRecord.resources[0].resourcePk,
+        });
+        if (!parentDepartment) {
+          await department.setParent(null);
+          return;
+        }
+        const parent = await department.getParent();
+        if (parent) {
+          if (parentDepartment.id !== parent.id) {
+            await department.setParent(parentDepartment);
+          }
+        } else {
+          await department.setParent(parentDepartment);
+        }
+      } else {
+        await department.setParent(null);
+      }
+    }
+  }
+}
