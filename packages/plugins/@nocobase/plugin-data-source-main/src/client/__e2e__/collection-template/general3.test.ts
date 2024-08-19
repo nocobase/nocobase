c82c98ce259cb7a7b5737878ff9ceb9f9129a5f1
katherinehhh <katherine_15995@163.com>
2024-08-19 21:04:39 +0800

    fix: test

--- packages/plugins/@nocobase/plugin-data-source-main/src/client/__e2e__/collection-template/general3.test.ts
+++ packages/plugins/@nocobase/plugin-data-source-main/src/client/__e2e__/collection-template/general3.test.ts
@@ -75,7 +75,10 @@ test.describe('configure fields', () => {
     await addField('Attachment');
 
     // 添加 date & time 字段
-    await addField('Datetime');
+    await addField('Datetime(with time zone)');
+    await addField('Datetime(without time zone)');
+    await addField('Date');
+    await addField('Unix Timestamp');
     await addField('Time');
 
     // 添加 relation 字段
