ccd83a82a9442ab958b177c4cea48d0be4b4cfd2
Chareice <chareice@live.com>
2024-08-22 14:13:33 +0800

    chore: simple paginate option

--- packages/core/database/src/collection.ts
+++ packages/core/database/src/collection.ts
@@ -115,6 +115,8 @@ export interface CollectionOptions extends Omit<ModelOptions, 'name' | 'hooks'> 
   tree?: string;
   template?: string;
 
+  simplePaginate?: boolean;
+
   /**
    * where is the collection from
    *
