2c757c43c5946c106f6f699269026ecd20e47f1d
Zhi Chen <350447433@qq.com>
2024-08-27 05:25:30 +0800

    feat: user data sync plugin (#4986)
    * feat: user data sync plugin
    
    * fix: nickname bug
    
    * feat: adjust sync
    
    * fix: delete package-lock.json
    
    * feat: adjust push
    
    * feat: adjust user-data-sync plugin
    
    * fix: delete submodule
    
    * fix: sync bug
    
    * fix: adjust log
    
    * fix: adjust log
    
    * chore: add to build-in
    
    * chore: update version
    
    * chore: add api doc
    
    * chore: add plugin description
    
    * chore: api doc
    
    * chore: change keywords
    
    * feat: add no source type prompt
    
    * chore: text to json
    
    * feat: optimize resource manager
    
    * fix: test & typo
    
    * test: add tests for resource manager & fix bug
    
    * fix: user sync
    
    * chore: remove department resource
    
    * fix: test
    
    * fix: build
    
    * fix: push data
    
    * fix: push api add default sourceName
    
    * test: add api test
    
    * fix: uniqueKey support any
    
    * chore: single user belongs to multi department situation
    
    * chore: department
    
    * fix: fix sync bugs
    
    * fix: sync departments
    
    * chore: remove department logic
    
    * chore: remove department
    
    * fix: fix logger type error
    
    * fix: fix sync bug
    
    * fix: logger & role
    
    * fix: fix sync bugs
    
    * fix: fix sync bug
    
    * fix: fix sync bugs
    
    * test: update test cases
    
    * chore: update
    
    * chore: update
    
    * fix: test
    
    * fix: test
    
    * fix: test
    
    * fix: bugs
    
    * fix: version
    
    ---------
    
    Co-authored-by: chenzhi <chenzhi@zhuopaikeji.com>
    Co-authored-by: xilesun <2013xile@gmail.com>

---
+++ packages/plugins/@nocobase/plugin-users/src/server/user-data-sync-resource.ts
@@ -0,0 +1,102 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import { Model } from '@nocobase/database';
+import {
+  FormatUser,
+  OriginRecord,
+  PrimaryKey,
+  RecordResourceChanged,
+  SyncAccept,
+  UserDataResource,
+} from '@nocobase/plugin-user-data-sync';
+
+export class UserDataSyncResource extends UserDataResource {
+  name = 'users';
+  accepts: SyncAccept[] = ['user'];
+
+  get userRepo() {
+    return this.db.getRepository('users');
+  }
+
+  async updateUser(user: Model, sourceUser: FormatUser) {
+    if (sourceUser.isDeleted) {
+      // 删除用户
+      const roles = await user.getRoles();
+      // 是否有Root角色
+      for (const role of roles) {
+        if (role.name === 'root') {
+          return;
+        }
+      }
+      await user.destroy();
+      return;
+    }
+    let dataChanged = false;
+    if (sourceUser.username !== undefined && user.username !== sourceUser.username) {
+      user.username = sourceUser.username;
+      dataChanged = true;
+    }
+    if (sourceUser.phone !== undefined && user.phone !== sourceUser.phone) {
+      user.phone = sourceUser.phone;
+      dataChanged = true;
+    }
+    if (sourceUser.email !== undefined && user.email !== sourceUser.email) {
+      user.email = sourceUser.email;
+      dataChanged = true;
+    }
+    if (sourceUser.nickname !== undefined && user.nickname !== sourceUser.nickname) {
+      user.nickname = sourceUser.nickname;
+      dataChanged = true;
+    }
+    if (dataChanged) {
+      await user.save();
+    }
+  }
+
+  async update(record: OriginRecord, resourcePks: PrimaryKey[], matchKey: string): Promise<RecordResourceChanged[]> {
+    const { metaData: sourceUser } = record;
+    const resourcePk = resourcePks[0];
+    const user = await this.userRepo.findOne({
+      filterByTk: resourcePk,
+    });
+    if (!user) {
+      // 用户不存在, 重新创建用户
+      const result = await this.create(record, matchKey);
+      return [...result, { resourcesPk: resourcePk, isDeleted: true }];
+    }
+    await this.updateUser(user, sourceUser);
+    return [];
+  }
+
+  async create(record: OriginRecord, matchKey: string): Promise<RecordResourceChanged[]> {
+    const { metaData: sourceUser } = record;
+    const filter = {};
+    let user: any;
+    if (['phone', 'email', 'username'].includes(matchKey)) {
+      filter[matchKey] = sourceUser[matchKey];
+      user = await this.userRepo.findOne({
+        filter,
+      });
+    }
+    if (user) {
+      await this.updateUser(user, sourceUser);
+    } else {
+      user = await this.userRepo.create({
+        values: {
+          nickname: sourceUser.nickname,
+          phone: sourceUser.phone,
+          email: sourceUser.email,
+          username: sourceUser.username,
+        },
+      });
+    }
+    return [{ resourcesPk: user.id, isDeleted: false }];
+  }
+}
