4e4e432eb41b78b03b24a91b14e818633f4e5fd4
Zeke Zhang <958414905@qq.com>
2024-08-28 14:50:49 +0800

    fix(mobile): resovle redirect issue (#5145)
    * fix(mobile): resovle redirect issue
    
    * test: add e2e tests

---
+++ packages/plugins/@nocobase/plugin-mobile/src/client/__e2e__/redirect.test.ts
@@ -0,0 +1,36 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import { devices, expect, test } from '@nocobase/test/e2e';
+
+test.use({
+  ...devices['Galaxy S9+'],
+});
+
+test.describe('redirect to other page from mobile', () => {
+  test('redirect to signin page', async ({ page }) => {
+    const baseURL = process.env.APP_BASE_URL || `http://localhost:${process.env.APP_PORT || 20000}`;
+
+    await page.goto('/m/signin');
+    await page.waitForURL(`${baseURL}/signin`);
+    expect(page.url()).toBe(`${baseURL}/signin`);
+
+    // do not redirect to mobile page
+    await page.waitForTimeout(5000);
+    expect(page.url()).toBe(`${baseURL}/signin`);
+  });
+
+  test('redirect to admin page', async ({ page }) => {
+    const baseURL = process.env.APP_BASE_URL || `http://localhost:${process.env.APP_PORT || 20000}`;
+
+    await page.goto('/m/admin/settings/@nocobase/plugin-api-keys');
+    await page.waitForURL(`${baseURL}/admin/settings/@nocobase/plugin-api-keys`);
+    expect(page.url()).toBe(`${baseURL}/admin/settings/@nocobase/plugin-api-keys`);
+  });
+});
