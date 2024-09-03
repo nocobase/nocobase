5797cefe4cbcb0e1790d450c53a5c8fe6a628e65
WangMin001 <2924492198@wm>
2024-09-03 19:28:32 +0800

    fix: 登录

---
+++ packages/plugins/@LeWanYun/plugin-login/src/server/locale/fr-FR.ts
@@ -0,0 +1,19 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+export default {
+  'The email is incorrect, please re-enter': 'L\'email est incorrect, veuillez le saisir à nouveau',
+  'Please fill in your email address': 'Veuillez remplir votre adresse e-mail',
+  'The password is incorrect, please re-enter': 'Le mot de passe est incorrect, veuillez le saisir à nouveau',
+  'Not a valid cellphone number, please re-enter': 'Numéro de téléphone portable non valide, veuillez le saisir à nouveau',
+  'The phone number has been registered, please login directly':
+    'Le numéro de téléphone a été enregistré, veuillez vous connecter directement',
+  'The phone number is not registered, please register first':
+    'Le numéro de téléphone n\'est pas enregistré, veuillez vous inscrire d\'abord',
+};
