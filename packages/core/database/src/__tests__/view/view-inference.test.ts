f1ff0685b961bb64973bff956e235d6f77dfc7a0
Chareice <chareice@live.com>
2024-09-05 18:00:18 +0800

    chore: test

--- packages/core/database/src/__tests__/view/view-inference.test.ts
+++ packages/core/database/src/__tests__/view/view-inference.test.ts
@@ -127,7 +127,7 @@ describe('view inference', function () {
     });
 
     const createdAt = UserCollection.model.rawAttributes['createdAt'].field;
-    expect(inferredFields[createdAt]['type']).toBe('datetime');
+    expect(inferredFields[createdAt]['type']).toBe('datetimeTz');
 
     if (db.options.dialect == 'sqlite') {
       expect(inferredFields['name']).toMatchObject({
