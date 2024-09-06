44275b5cd755f945cc1f5ee0f26249df646ee7b7
Zeke Zhang <958414905@qq.com>
2024-09-06 16:20:07 +0800

    refactor(plugin-acl): extensible support for role permissions configuration UI (#5216)

---
+++ packages/plugins/@nocobase/plugin-acl/src/client/ACLSettingsUI.tsx
@@ -0,0 +1,79 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import { TabsProps } from 'antd/es/tabs/index';
+import React from 'react';
+import { GeneralPermissions } from './permissions/GeneralPermissions';
+import { MenuItemsProvider } from './permissions/MenuItemsProvider';
+import { MenuPermissions } from './permissions/MenuPermissions';
+import { Role } from './RolesManagerProvider';
+
+interface PermissionsTabsProps {
+  /**
+   * the key of the currently active tab panel
+   */
+  activeKey: string;
+  /**
+   * the currently selected role
+   */
+  role: Role;
+  /**
+   * translation function
+   */
+  t: (key: string) => string;
+  /**
+   * used to constrain the size of the container in the Tab
+   */
+  TabLayout: React.FC;
+}
+
+type Tab = TabsProps['items'][0];
+
+type TabCallback = (props: PermissionsTabsProps) => Tab;
+
+/**
+ * the extension API for ACL settings page
+ */
+export class ACLSettingsUI {
+  private permissionsTabs: (Tab | TabCallback)[] = [
+    ({ t, TabLayout }) => ({
+      key: 'general',
+      label: t('System'),
+      children: (
+        <TabLayout>
+          <GeneralPermissions />
+        </TabLayout>
+      ),
+    }),
+    ({ activeKey, t, TabLayout }) => ({
+      key: 'menu',
+      label: t('Menu'),
+      children: (
+        <TabLayout>
+          <MenuItemsProvider>
+            <MenuPermissions active={activeKey === 'menu'} />
+          </MenuItemsProvider>
+        </TabLayout>
+      ),
+    }),
+  ];
+
+  addPermissionsTab(tab: Tab | TabCallback): void {
+    this.permissionsTabs.push(tab);
+  }
+
+  getPermissionsTabs(props: PermissionsTabsProps): Tab[] {
+    return this.permissionsTabs.map((tab) => {
+      if (typeof tab === 'function') {
+        return tab(props);
+      }
+      return tab;
+    });
+  }
+}
