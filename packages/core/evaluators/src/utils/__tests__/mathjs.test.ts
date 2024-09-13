9519eb779be981f52df4b75e1cddf835302915fe
mytharcher <mytharcher@gmail.com>
2024-09-13 20:32:22 +0800

    fix(evaluators): fix mathjs matrix output type

---
+++ packages/core/evaluators/src/utils/__tests__/mathjs.test.ts
@@ -0,0 +1,16 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import mathjs from '../mathjs';
+
+describe('evaluators > mathjs', () => {
+  it('matrix type should be to array', () => {
+    expect(mathjs('range(1, 3, 1)')).toEqual([1, 2, 3]);
+  });
+});
