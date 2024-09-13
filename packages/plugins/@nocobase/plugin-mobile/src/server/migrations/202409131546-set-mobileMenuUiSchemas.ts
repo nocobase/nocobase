bcf07ac7199c1a8b5722dacb2d8e2a644e1811d8
Zeke Zhang <958414905@qq.com>
2024-09-13 16:20:24 +0800

    chore: add migration

---
+++ packages/plugins/@nocobase/plugin-mobile/src/server/migrations/202409131546-set-mobileMenuUiSchemas.ts
@@ -0,0 +1,31 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+/* istanbul ignore file -- @preserve */
+
+import { Migration } from '@nocobase/server';
+
+export default class extends Migration {
+  appVersion = '<=1.3.22-beta';
+
+  async up() {
+    const mobileRoutes = await this.db.getRepository('mobileRoutes').find({
+      sort: 'sort',
+    });
+    const roles = await this.db.getRepository('roles').find();
+
+    for (const role of roles) {
+      await this.db.getRepository<any>('roles.mobileMenuUiSchemas', role.get('name')).add({
+        values: mobileRoutes.map((item) => item.get('id')),
+      });
+    }
+  }
+
+  async down() {}
+}
