942ff4be89886d6169bbe45e6895462375e51334
katherinehhh <katherine_15995@163.com>
2024-09-22 20:08:39 +0800

    fix: test

--- packages/core/client/src/schema-component/antd/date-picker/util.ts
+++ packages/core/client/src/schema-component/antd/date-picker/util.ts
@@ -10,8 +10,6 @@
 import { getDefaultFormat, str2moment, toGmt, toLocal, getPickerFormat } from '@nocobase/utils/client';
 import type { Dayjs } from 'dayjs';
 import dayjs from 'dayjs';
-import { useContext } from 'react';
-import { BlockContext } from '../../../block-provider';
 
 const toStringByPicker = (value, picker = 'date', timezone: 'gmt' | 'local') => {
   if (!dayjs.isDayjs(value)) return value;
@@ -98,12 +96,11 @@ const handleChangeOnForm = (value, dateOnly, utc, picker, showTime, gmt) => {
 
 export const mapDatePicker = function () {
   return (props: any) => {
-    const { dateOnly, showTime, picker, utc, gmt } = props;
+    const { dateOnly, showTime, picker, utc, gmt, underFilter } = props;
     const format = getDefaultFormat(props);
     const onChange = props.onChange;
-    const { name: blockType } = useContext(BlockContext);
     //是否是筛选的场景下
-    const isUnderFilter = blockType && blockType !== 'form';
+    const isUnderFilter = underFilter;
     return {
       ...props,
       format: format,
--- packages/core/client/src/schema-initializer/utils.ts
+++ packages/core/client/src/schema-initializer/utils.ts
@@ -464,6 +464,7 @@ export const useFilterFormItemInitializerFields = (options?: any) => {
         'x-component-props': {
           component: interfaceConfig?.filterable?.operators?.[0]?.schema?.['x-component'],
           utc: false,
+          underFilter: true,
         },
       };
       if (isAssocField(field)) {
@@ -478,7 +479,7 @@ export const useFilterFormItemInitializerFields = (options?: any) => {
           'x-decorator': 'FormItem',
           'x-use-decorator-props': 'useFormItemProps',
           'x-collection-field': `${name}.${field.name}`,
-          'x-component-props': field.uiSchema?.['x-component-props'],
+          'x-component-props': { ...field.uiSchema?.['x-component-props'], utc: false, underFilter: true },
         };
       }
       const resultItem = {
@@ -572,7 +573,7 @@ const associationFieldToMenu = (
       interface: field.interface,
     },
     'x-component': 'CollectionField',
-    'x-component-props': { utc: false },
+    'x-component-props': { utc: false, underFilter: true },
     'x-read-pretty': false,
     'x-decorator': 'FormItem',
     'x-collection-field': `${collectionName}.${schemaName}`,
@@ -688,7 +689,7 @@ export const useFilterInheritsFormItemInitializerFields = (options?) => {
             'x-component': 'CollectionField',
             'x-decorator': 'FormItem',
             'x-collection-field': `${name}.${field.name}`,
-            'x-component-props': { utc: false },
+            'x-component-props': { utc: false, underFilter: true },
             'x-read-pretty': field?.uiSchema?.['x-read-pretty'],
           };
           return {
