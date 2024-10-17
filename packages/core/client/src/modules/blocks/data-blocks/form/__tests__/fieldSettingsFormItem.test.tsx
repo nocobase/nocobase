5835a95b92022a3af04bee1b73c069e26fa64b47
katherinehhh <katherine_15995@163.com>
2024-10-17 10:47:11 +0800

    fix: test

--- packages/core/client/src/modules/blocks/data-blocks/form/__tests__/fieldSettingsFormItem.test.tsx
+++ packages/core/client/src/modules/blocks/data-blocks/form/__tests__/fieldSettingsFormItem.test.tsx
@@ -315,267 +315,267 @@ describe('FieldSettingsFormItem', () => {
     });
   });
 
-  // describe('menu items', () => {
-  //   test('Edit field title', async () => {
-  //     await renderSettings(commonFieldOptions());
-  //     await checkFieldTitle('Nickname');
-  //   });
+  describe('menu items', () => {
+    test('Edit field title', async () => {
+      await renderSettings(commonFieldOptions());
+      await checkFieldTitle('Nickname');
+    });
 
-  //   test('Display title', async () => {
-  //     await renderSettings(commonFieldOptions());
-  //     await checkSettings([
-  //       {
-  //         type: 'switch',
-  //         title: 'Display title',
-  //         beforeClick() {
-  //           expect(document.querySelector('.ant-formily-item-label')).toHaveStyle({ display: 'flex' });
-  //         },
-  //         async afterFirstClick() {
-  //           expect(document.querySelector('.ant-formily-item-label')).toHaveStyle({ display: 'none' });
-  //         },
-  //         afterSecondClick() {
-  //           expect(document.querySelector('.ant-formily-item-label')).toHaveStyle({ display: 'flex' });
-  //         },
-  //       },
-  //     ]);
-  //   });
+    test('Display title', async () => {
+      await renderSettings(commonFieldOptions());
+      await checkSettings([
+        {
+          type: 'switch',
+          title: 'Display title',
+          beforeClick() {
+            expect(document.querySelector('.ant-formily-item-label')).toHaveStyle({ display: 'flex' });
+          },
+          async afterFirstClick() {
+            expect(document.querySelector('.ant-formily-item-label')).toHaveStyle({ display: 'none' });
+          },
+          afterSecondClick() {
+            expect(document.querySelector('.ant-formily-item-label')).toHaveStyle({ display: 'flex' });
+          },
+        },
+      ]);
+    });
 
-  //   test('Edit description', async () => {
-  //     const newValue = 'new test';
-  //     await renderSettings(commonFieldOptions());
-  //     await checkSettings([
-  //       {
-  //         type: 'modal',
-  //         title: 'Edit description',
-  //         modalChecker: {
-  //           modalTitle: 'Edit description',
-  //           formItems: [
-  //             {
-  //               type: 'textarea',
-  //               newValue,
-  //             },
-  //           ],
-  //           afterSubmit() {
-  //             expect(screen.queryByText(newValue)).toBeInTheDocument();
-  //           },
-  //         },
-  //       },
-  //     ]);
-  //   });
+    test('Edit description', async () => {
+      const newValue = 'new test';
+      await renderSettings(commonFieldOptions());
+      await checkSettings([
+        {
+          type: 'modal',
+          title: 'Edit description',
+          modalChecker: {
+            modalTitle: 'Edit description',
+            formItems: [
+              {
+                type: 'textarea',
+                newValue,
+              },
+            ],
+            afterSubmit() {
+              expect(screen.queryByText(newValue)).toBeInTheDocument();
+            },
+          },
+        },
+      ]);
+    });
 
-  //   test('Edit tooltip', async () => {
-  //     const newValue = 'new test';
-  //     await renderSettings(commonFieldOptions());
-  //     await checkSettings([
-  //       {
-  //         type: 'modal',
-  //         title: 'Edit tooltip',
-  //         modalChecker: {
-  //           modalTitle: 'Edit tooltip',
-  //           formItems: [
-  //             {
-  //               type: 'textarea',
-  //               newValue,
-  //             },
-  //           ],
-  //           async afterSubmit() {
-  //             expect(document.querySelector('.anticon-question-circle')).toBeInTheDocument();
-  //             await userEvent.hover(document.querySelector('.anticon-question-circle'));
-  //             await waitFor(() => {
-  //               expect(screen.queryByText(newValue)).toBeInTheDocument();
-  //             });
-  //           },
-  //         },
-  //       },
-  //     ]);
-  //   });
+    test('Edit tooltip', async () => {
+      const newValue = 'new test';
+      await renderSettings(commonFieldOptions());
+      await checkSettings([
+        {
+          type: 'modal',
+          title: 'Edit tooltip',
+          modalChecker: {
+            modalTitle: 'Edit tooltip',
+            formItems: [
+              {
+                type: 'textarea',
+                newValue,
+              },
+            ],
+            async afterSubmit() {
+              expect(document.querySelector('.anticon-question-circle')).toBeInTheDocument();
+              await userEvent.hover(document.querySelector('.anticon-question-circle'));
+              await waitFor(() => {
+                expect(screen.queryByText(newValue)).toBeInTheDocument();
+              });
+            },
+          },
+        },
+      ]);
+    });
 
-  //   test('Required', async () => {
-  //     await renderSettings(commonFieldOptions());
-  //     await checkSettings([
-  //       {
-  //         type: 'switch',
-  //         title: 'Required',
-  //         afterFirstClick() {
-  //           expect(document.querySelector('.ant-formily-item-asterisk')).toBeInTheDocument();
-  //         },
-  //         afterSecondClick() {
-  //           expect(document.querySelector('.ant-formily-item-asterisk')).not.toBeInTheDocument();
-  //         },
-  //       },
-  //     ]);
-  //   });
+    test('Required', async () => {
+      await renderSettings(commonFieldOptions());
+      await checkSettings([
+        {
+          type: 'switch',
+          title: 'Required',
+          afterFirstClick() {
+            expect(document.querySelector('.ant-formily-item-asterisk')).toBeInTheDocument();
+          },
+          afterSecondClick() {
+            expect(document.querySelector('.ant-formily-item-asterisk')).not.toBeInTheDocument();
+          },
+        },
+      ]);
+    });
 
-  //   test.skip('Set default value', async () => {
-  //     await renderSettings(commonFieldOptions());
-  //     const newValue = 'new test';
+    test.skip('Set default value', async () => {
+      await renderSettings(commonFieldOptions());
+      const newValue = 'new test';
 
-  //     await checkSettings([
-  //       {
-  //         type: 'modal',
-  //         title: 'Set default value',
-  //         modalChecker: {
-  //           modalTitle: 'Set default value',
-  //           formItems: [
-  //             {
-  //               type: 'collectionField',
-  //               field: 'users.nickname',
-  //               newValue,
-  //             },
-  //           ],
-  //           afterSubmit() {
-  //             expect(screen.queryByRole('textbox')).toBeInTheDocument();
-  //             expect(screen.queryByRole('textbox')).toHaveValue(newValue);
-  //           },
-  //         },
-  //       },
-  //     ]);
-  //   });
+      await checkSettings([
+        {
+          type: 'modal',
+          title: 'Set default value',
+          modalChecker: {
+            modalTitle: 'Set default value',
+            formItems: [
+              {
+                type: 'collectionField',
+                field: 'users.nickname',
+                newValue,
+              },
+            ],
+            afterSubmit() {
+              expect(screen.queryByRole('textbox')).toBeInTheDocument();
+              expect(screen.queryByRole('textbox')).toHaveValue(newValue);
+            },
+          },
+        },
+      ]);
+    });
 
-  //   test.skip('Pattern', async () => {
-  //     await renderSettings(associationFieldOptions());
+    test.skip('Pattern', async () => {
+      await renderSettings(associationFieldOptions());
 
-  //     await checkSettings([
-  //       {
-  //         type: 'select',
-  //         title: 'Pattern',
-  //         oldValue: 'Editable',
-  //         options: [
-  //           {
-  //             label: 'Readonly',
-  //             checker() {
-  //               expect(document.querySelector('.nb-form-item input')).toHaveAttribute('disabled');
-  //             },
-  //           },
-  //           {
-  //             label: 'Easy-reading',
-  //             checker() {
-  //               expect(document.querySelector('.nb-form-item input')).not.toBeInTheDocument();
-  //             },
-  //           },
-  //           {
-  //             label: 'Editable',
-  //             checker() {
-  //               expect(document.querySelector('.nb-form-item input')).toBeInTheDocument();
-  //             },
-  //           },
-  //         ],
-  //       },
-  //     ]);
-  //   });
+      await checkSettings([
+        {
+          type: 'select',
+          title: 'Pattern',
+          oldValue: 'Editable',
+          options: [
+            {
+              label: 'Readonly',
+              checker() {
+                expect(document.querySelector('.nb-form-item input')).toHaveAttribute('disabled');
+              },
+            },
+            {
+              label: 'Easy-reading',
+              checker() {
+                expect(document.querySelector('.nb-form-item input')).not.toBeInTheDocument();
+              },
+            },
+            {
+              label: 'Editable',
+              checker() {
+                expect(document.querySelector('.nb-form-item input')).toBeInTheDocument();
+              },
+            },
+          ],
+        },
+      ]);
+    });
 
-  //   test.skip('EditValidationRules', async () => {
-  //     await renderSingleSettings(commonFieldOptions(true));
-  //     await checkSettings([
-  //       {
-  //         type: 'modal',
-  //         title: 'Set validation rules',
-  //         modalChecker: {
-  //           modalTitle: 'Set validation rules',
-  //           contentText: 'Add validation rule',
-  //           async beforeCheck() {
-  //             await userEvent.click(screen.getByText('Add validation rule'));
-  //             await waitFor(() => {
-  //               expect(screen.queryByText('Min length')).toBeInTheDocument();
-  //             });
-  //           },
-  //           formItems: [
-  //             {
-  //               type: 'number',
-  //               label: 'Min length',
-  //               newValue: 2,
-  //             },
-  //           ],
-  //           async afterSubmit() {
-  //             await userEvent.type(document.querySelector('.nb-form-item input'), '1');
-  //             await waitFor(() => {
-  //               expect(screen.queryByText('The length or number of entries must be at least 2')).toBeInTheDocument();
-  //             });
+    test.skip('EditValidationRules', async () => {
+      await renderSingleSettings(commonFieldOptions(true));
+      await checkSettings([
+        {
+          type: 'modal',
+          title: 'Set validation rules',
+          modalChecker: {
+            modalTitle: 'Set validation rules',
+            contentText: 'Add validation rule',
+            async beforeCheck() {
+              await userEvent.click(screen.getByText('Add validation rule'));
+              await waitFor(() => {
+                expect(screen.queryByText('Min length')).toBeInTheDocument();
+              });
+            },
+            formItems: [
+              {
+                type: 'number',
+                label: 'Min length',
+                newValue: 2,
+              },
+            ],
+            async afterSubmit() {
+              await userEvent.type(document.querySelector('.nb-form-item input'), '1');
+              await waitFor(() => {
+                expect(screen.queryByText('The length or number of entries must be at least 2')).toBeInTheDocument();
+              });
 
-  //             await userEvent.type(document.querySelector('.nb-form-item input'), '12');
-  //             await waitFor(() => {
-  //               expect(
-  //                 screen.queryByText('The length or number of entries must be at least 2'),
-  //               ).not.toBeInTheDocument();
-  //             });
-  //           },
-  //         },
-  //       },
-  //     ]);
-  //   });
+              await userEvent.type(document.querySelector('.nb-form-item input'), '12');
+              await waitFor(() => {
+                expect(
+                  screen.queryByText('The length or number of entries must be at least 2'),
+                ).not.toBeInTheDocument();
+              });
+            },
+          },
+        },
+      ]);
+    });
 
-  //   test('Field component', async () => {
-  //     await renderSingleSettings(associationFieldOptions(true));
+    test('Field component', async () => {
+      await renderSingleSettings(associationFieldOptions(true));
 
-  //     await checkSettings([
-  //       {
-  //         type: 'select',
-  //         title: 'Field component',
-  //         oldValue: 'Select',
-  //         beforeSelect() {
-  //           expect(screen.queryByTestId('select-object-multiple')).toBeInTheDocument();
-  //         },
-  //         options: [
-  //           {
-  //             label: 'Record picker',
-  //             async checker() {
-  //               expect(screen.queryByText('mode: Picker')).toBeInTheDocument();
-  //             },
-  //           },
-  //           {
-  //             label: 'Sub-table',
-  //             checker() {
-  //               expect(screen.queryByText('mode: SubTable')).toBeInTheDocument();
-  //             },
-  //           },
-  //           {
-  //             label: 'Sub-form',
-  //             checker() {
-  //               expect(screen.queryByText('mode: Nester')).toBeInTheDocument();
-  //             },
-  //           },
-  //           {
-  //             label: 'Sub-form(Popover)',
-  //             checker() {
-  //               expect(screen.queryByText('mode: PopoverNester')).toBeInTheDocument();
-  //             },
-  //           },
-  //         ],
-  //       },
-  //     ]);
-  //   });
+      await checkSettings([
+        {
+          type: 'select',
+          title: 'Field component',
+          oldValue: 'Select',
+          beforeSelect() {
+            expect(screen.queryByTestId('select-object-multiple')).toBeInTheDocument();
+          },
+          options: [
+            {
+              label: 'Record picker',
+              async checker() {
+                expect(screen.queryByText('mode: Picker')).toBeInTheDocument();
+              },
+            },
+            {
+              label: 'Sub-table',
+              checker() {
+                expect(screen.queryByText('mode: SubTable')).toBeInTheDocument();
+              },
+            },
+            {
+              label: 'Sub-form',
+              checker() {
+                expect(screen.queryByText('mode: Nester')).toBeInTheDocument();
+              },
+            },
+            {
+              label: 'Sub-form(Popover)',
+              checker() {
+                expect(screen.queryByText('mode: PopoverNester')).toBeInTheDocument();
+              },
+            },
+          ],
+        },
+      ]);
+    });
 
-  //   test('Title field', async () => {
-  //     await renderSettings(associationFieldOptions());
+    test('Title field', async () => {
+      await renderSettings(associationFieldOptions());
 
-  //     await checkSettings([
-  //       {
-  //         type: 'select',
-  //         title: 'Title field',
-  //         // oldValue: 'Role name',
-  //         async beforeSelect() {
-  //           expect(screen.queryByTestId('select-object-multiple')).toBeInTheDocument();
-  //           await userEvent.click(document.querySelector('.ant-select-selector'));
+      await checkSettings([
+        {
+          type: 'select',
+          title: 'Title field',
+          // oldValue: 'Role name',
+          async beforeSelect() {
+            expect(screen.queryByTestId('select-object-multiple')).toBeInTheDocument();
+            await userEvent.click(document.querySelector('.ant-select-selector'));
 
-  //           await waitFor(() => {
-  //             expect(screen.queryByText('Admin')).toBeInTheDocument();
-  //           });
-  //         },
-  //         options: [
-  //           {
-  //             label: 'Role UID',
-  //             async checker() {
-  //               expect(screen.queryByTestId('select-object-multiple')).toBeInTheDocument();
-  //               await userEvent.click(document.querySelector('.ant-select-selector'));
+            await waitFor(() => {
+              expect(screen.queryByText('Admin')).toBeInTheDocument();
+            });
+          },
+          options: [
+            {
+              label: 'Role UID',
+              async checker() {
+                expect(screen.queryByTestId('select-object-multiple')).toBeInTheDocument();
+                await userEvent.click(document.querySelector('.ant-select-selector'));
 
-  //               await waitFor(() => {
-  //                 expect(screen.queryByText('admin')).toBeInTheDocument();
-  //               });
-  //             },
-  //           },
-  //         ],
-  //       },
-  //     ]);
-  //   });
-  // });
+                await waitFor(() => {
+                  expect(screen.queryByText('admin')).toBeInTheDocument();
+                });
+              },
+            },
+          ],
+        },
+      ]);
+    });
+  });
 });
