09b86bf18b8d7b3098534907c2756daa9cf27ce4
mahuan <1007409254@qq.com>
2024-10-22 23:02:52 +0800

    refactor: standardize localization file name

--- packages/plugins/@nocobase/plugin-theme-editor/src/client/antd-token-previewer/locale/context.tsx
+++ packages/plugins/@nocobase/plugin-theme-editor/src/client/antd-token-previewer/locale/context.tsx
@@ -7,10 +7,18 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
-import React from 'react';
-import type { Locale } from './interface';
-import zhCN from './zh-CN';
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
 
-export const LocaleContext = React.createContext<Locale>(zhCN);
+import React from 'react';
+import zhCN from './zh-CN.json';
+
+export const LocaleContext = React.createContext(zhCN);
 
 export const useLocale = () => React.useContext(LocaleContext);
