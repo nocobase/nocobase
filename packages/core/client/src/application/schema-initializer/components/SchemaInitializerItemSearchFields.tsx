d8c38079fb7b98ff67236ff9cc24a1b5fe97fd41
katherinehhh <katherine_15995@163.com>
2024-10-23 20:08:39 +0800

    fix: bug

--- packages/core/client/src/application/schema-initializer/components/SchemaInitializerItemSearchFields.tsx
+++ packages/core/client/src/application/schema-initializer/components/SchemaInitializerItemSearchFields.tsx
@@ -21,9 +21,18 @@ export const SearchFields = ({ value: outValue, onChange }) => {
     setValue(outValue);
   }, [outValue]);
 
+  // TODO: antd 的 Input 的 autoFocus 有 BUG，会不生效，等待官方修复后再简化：https://github.com/ant-design/ant-design/issues/41239
   useEffect(() => {
+    // 1. 组件在第一次渲染时自动 focus，提高用户体验
     inputRef.current.input.focus();
-    const observer = new IntersectionObserver((entries) => {});
+
+    // 2. 当组件已经渲染，并再次显示时，自动 focus
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        inputRef.current.input.focus();
+      }
+    });
+
     observer.observe(inputRef.current.input);
     return () => {
       observer.disconnect();
@@ -62,6 +71,7 @@ export function useMenuSearch({
   showType?: boolean;
   hideSearch?: boolean;
 }) {
+  console.log(999);
   const items = children?.concat?.() || [];
   const [searchValue, setSearchValue] = useState(null);
   const currentItems = useMemo(() => {
