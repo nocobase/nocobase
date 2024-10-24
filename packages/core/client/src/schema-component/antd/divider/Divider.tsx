74f618fc8ea413f33eee11da105f5e9bc77920f2
katherinehhh <katherine_15995@163.com>
2024-10-24 10:55:47 +0800

    refactor: orientationMargin

--- packages/core/client/src/schema-component/antd/divider/Divider.tsx
+++ packages/core/client/src/schema-component/antd/divider/Divider.tsx
@@ -7,17 +7,14 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
-import { css } from '@emotion/css';
 import { connect, mapProps } from '@formily/react';
 import { Divider as AntdDivider } from 'antd';
-import cls from 'classnames';
 import React from 'react';
 
 export const Divider = connect(
   (props) => {
     const { color, borderColor } = props;
-    console.log(borderColor);
-    return <AntdDivider {...props} type="horizontal" style={{ color, borderColor }} />;
+    return <AntdDivider {...props} type="horizontal" style={{ color, borderColor }} orientationMargin="0" />;
   },
   mapProps((props) => {
     return {
