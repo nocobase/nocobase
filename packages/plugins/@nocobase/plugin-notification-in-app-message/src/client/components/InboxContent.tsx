bab326edeebe363f85306f235041bce74a011552
Sheldon Guo <sheldon_66@163.com>
2024-10-25 19:03:29 +0800

    refactor(notification-in-app): optimize InboxContent component

--- packages/plugins/@nocobase/plugin-notification-in-app-message/src/client/components/InboxContent.tsx
+++ packages/plugins/@nocobase/plugin-notification-in-app-message/src/client/components/InboxContent.tsx
@@ -7,28 +7,12 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
-import React, { useCallback } from 'react';
+import React from 'react';
 import { observer } from '@formily/reactive-react';
 
-import {
-  Layout,
-  List,
-  Card,
-  Descriptions,
-  Badge,
-  Button,
-  Flex,
-  Spin,
-  Tag,
-  Tabs,
-  ConfigProvider,
-  Typography,
-  Tooltip,
-  theme,
-} from 'antd';
+import { Layout, List, Badge, Button, Flex, Tabs, ConfigProvider, theme } from 'antd';
 import { css } from '@emotion/css';
 import { dayjs } from '@nocobase/utils/client';
-import { useNavigate } from 'react-router-dom';
 import { useLocalTranslation } from '../../locale';
 
 import {
@@ -36,17 +20,13 @@ import {
   selectedChannelNameObs,
   channelListObs,
   isFetchingChannelsObs,
-  channelMapObs,
   showChannelLoadingMoreObs,
-  fetchMessages,
-  isFecthingMessageObs,
   selectedMessageListObs,
-  showMsgLoadingMoreObs,
-  updateMessage,
-  inboxVisible,
   channelStatusFilterObs,
   ChannelStatus,
 } from '../observables';
+
+import { MessageList } from './MessageList';
 
 const InnerInboxContent = () => {
   const { token } = theme.useToken();
@@ -66,20 +46,6 @@ const InnerInboxContent = () => {
     fetchChannels({ filter, limit: 30 });
   };
 
-  const onLoadMessagesMore = useCallback(() => {
-    const filter: Record<string, any> = {};
-    const lastMessage = messages[messages.length - 1];
-    if (lastMessage) {
-      filter.receiveTimestamp = {
-        $lt: lastMessage.receiveTimestamp,
-      };
-    }
-    if (selectedChannelName) {
-      filter.channelName = selectedChannelName;
-    }
-    fetchMessages({ filter, limit: 30 });
-  }, [messages, selectedChannelName]);
-
   const loadChannelsMore = showChannelLoadingMoreObs.value ? (
     <div
       style={{
@@ -94,110 +60,6 @@ const InnerInboxContent = () => {
       </Button>
     </div>
   ) : null;
-
-  const MessageList = observer(() => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const navigate = useNavigate();
-    const isFetchingMessages = isFecthingMessageObs.value;
-    const msgStatusDict = {
-      read: t('Read'),
-      unread: t('Unread'),
-    };
-    if (!selectedChannelName) return null;
-    const onItemClicked = (message) => {
-      updateMessage({
-        filterByTk: message.id,
-        values: {
-          status: 'read',
-        },
-      });
-      if (message.options?.url) {
-        inboxVisible.value = false;
-        const url = message.options.url;
-        if (url.startsWith('/')) navigate(url);
-        else {
-          window.location.href = url;
-        }
-      }
-    };
-    return (
-      <ConfigProvider
-        theme={{
-          components: { Badge: { dotSize: 8 } },
-        }}
-      >
-        <Typography.Title level={4} style={{ marginBottom: token.marginLG }}>
-          {channelMapObs.value[selectedChannelName].title}
-        </Typography.Title>
-
-        {messages.length === 0 && isFecthingMessageObs.value ? (
-          <Spin style={{ width: '100%', marginTop: token.marginXXL }} />
-        ) : (
-          messages.map((message, index) => (
-            <>
-              <Card
-                size={'small'}
-                bordered={false}
-                style={{ marginBottom: token.marginMD }}
-                title={
-                  <Tooltip title={message.title} mouseEnterDelay={0.5}>
-                    <div
-                      onClick={() => {
-                        onItemClicked(message);
-                      }}
-                      style={{
-                        fontWeight: message.status === 'unread' ? 'bold' : 'normal',
-                        cursor: 'pointer',
-                        width: '100%',
-                      }}
-                    >
-                      {message.title}
-                    </div>
-                  </Tooltip>
-                }
-                extra={
-                  message.options?.url ? (
-                    <Button
-                      type="link"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        onItemClicked(message);
-                      }}
-                    >
-                      {t('View')}
-                    </Button>
-                  ) : null
-                }
-                key={message.id}
-              >
-                <Descriptions key={index} column={1}>
-                  <Descriptions.Item label={t('Content')}>
-                    {' '}
-                    <Tooltip title={message.content.length > 100 ? message.content : ''} mouseEnterDelay={0.5}>
-                      {message.content.slice(0, 100) + (message.content.length > 100 ? '...' : '')}{' '}
-                    </Tooltip>
-                  </Descriptions.Item>
-                  <Descriptions.Item label={t('Datetime')}>
-                    {dayjs(message.receiveTimestamp).fromNow()}
-                  </Descriptions.Item>
-                  <Descriptions.Item label={t('Status')}>
-                    <Tag color={message.status === 'unread' ? 'red' : 'green'}>{msgStatusDict[message.status]}</Tag>
-                  </Descriptions.Item>
-                </Descriptions>
-              </Card>
-            </>
-          ))
-        )}
-        {showMsgLoadingMoreObs.value && (
-          <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-            <Button onClick={onLoadMessagesMore} loading={isFetchingMessages}>
-              {t('Loading more')}
-            </Button>
-          </div>
-        )}
-      </ConfigProvider>
-    );
-  });
 
   const FilterTab = () => {
     interface TabItem {
