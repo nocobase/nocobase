62514c866652b76ea9d95fb17b393604ad7ee37f
katherinehhh <katherine_15995@163.com>
2024-10-28 23:05:51 +0800

    fix: bug

--- packages/core/client/src/application/schema-initializer/components/SchemaInitializerItemSearchFields.tsx
+++ packages/core/client/src/application/schema-initializer/components/SchemaInitializerItemSearchFields.tsx
@@ -7,6 +7,7 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
+import { uid } from '@formily/shared';
 import { Divider, Empty, Input, MenuProps } from 'antd';
 import React, { useEffect, useMemo, useState, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -77,7 +78,7 @@ export const SearchFields = ({ value: outValue, onChange }) => {
   );
 };
 
-export function useMenuSearch({
+export const useMenuSearch = ({
   children,
   showType,
   hideSearch,
@@ -85,101 +86,82 @@ export function useMenuSearch({
   children: any[];
   showType?: boolean;
   hideSearch?: boolean;
-}) {
+}) => {
   const items = children?.concat?.() || [];
   const [searchValue, setSearchValue] = useState(null);
-  const currentItems = useMemo(() => {
-    return items || [];
-  }, [items]);
 
-  // 根据搜索的值进行处理
-  const searchedItems = useMemo(() => {
+  // 处理搜索逻辑
+  const limitedSearchedItems = useMemo(() => {
     if (!searchValue || searchValue === '') {
       return items;
     }
     const lowerSearchValue = searchValue.toLocaleLowerCase();
-    return currentItems.filter(
+    return items.filter(
       (item) =>
         (item.label || item.title) &&
         String(item.label || item.title)
           .toLocaleLowerCase()
           .includes(lowerSearchValue),
     );
-  }, [searchValue, currentItems]);
+  }, [searchValue, items]);
 
-  // 根据 count 进行懒加载处理
-  const limitedSearchedItems = useMemo(() => {
-    return searchedItems;
-  }, [searchedItems]);
-
-  // 最终的返回结果
+  // 最终结果项
   const resultItems = useMemo<MenuProps['items']>(() => {
     const res = [];
     if (!hideSearch && (items.length > 10 || searchValue)) {
-      // 开头：搜索框
-      res.push(
-        Object.assign(
-          {
-            key: 'search',
-            Component: () => (
-              <SearchFields
-                value={searchValue}
-                onChange={(val: string) => {
-                  setSearchValue(val);
-                }}
-              />
-            ),
-            onClick({ domEvent }) {
-              domEvent.stopPropagation();
-            },
-          },
-          // isMenuType 为了 `useSchemaInitializerMenuItems()` 里面处理判断标识的
-          showType ? { isMenuType: true } : {},
+      res.push({
+        key: `search-${uid()}`,
+        Component: () => (
+          <SearchFields
+            value={searchValue}
+            onChange={(val: string) => {
+              setSearchValue(val);
+            }}
+          />
         ),
-      );
+        onClick({ domEvent }) {
+          domEvent.stopPropagation();
+        },
+        ...(showType ? { isMenuType: true } : {}),
+      });
     }
-    // 中间：搜索的数据
+
     if (limitedSearchedItems.length > 0) {
-      // 有搜索结果
       res.push(...limitedSearchedItems);
     } else {
-      // 搜索结果为空
-      res.push(
-        Object.assign(
-          {
-            key: 'empty',
-            style: {
-              height: 150,
-            },
-            Component: () => (
-              <div onClick={(e) => e.stopPropagation()}>
-                <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
-              </div>
-            ),
-          },
-          showType ? { isMenuType: true } : {},
+      res.push({
+        key: 'empty',
+        style: {
+          height: 150,
+        },
+        Component: () => (
+          <div onClick={(e) => e.stopPropagation()}>
+            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+          </div>
         ),
-      );
+        ...(showType ? { isMenuType: true } : {}),
+      });
     }
 
     return res;
   }, [hideSearch, limitedSearchedItems, searchValue, showType]);
 
-  // 处理嵌套的子菜单
-  const result = useMemo(() => {
-    return resultItems.map((item: any) => {
-      if (['subMenu', 'itemGroup'].includes(item.type)) {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const child = useMenuSearch({
-          children: item.children,
-          showType,
-          hideSearch,
-        });
-        return { ...item, children: child };
-      }
-      return item;
-    });
-  }, [resultItems, showType, hideSearch]);
+  const result = processedResult(resultItems, showType, hideSearch);
 
   return children ? result : undefined;
-}
+};
+
+// 处理嵌套子菜单
+const processedResult = (resultItems, showType, hideSearch) => {
+  return resultItems.map((item: any) => {
+    if (['subMenu', 'itemGroup'].includes(item.type)) {
+      const childItems = useMenuSearch({
+        children: item.children,
+        showType,
+        hideSearch,
+      });
+      return { ...item, children: childItems };
+    }
+    return item;
+  });
+};
