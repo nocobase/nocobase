84b7f38d826f91ce9d0738d89d57c07973bdfd3f
katherinehhh <katherine_15995@163.com>
2024-10-29 17:01:29 +0800

    fix: test

--- packages/plugins/@nocobase/plugin-data-source-main/src/client/__e2e__/fields/checkbox/schemaSettings.test.ts
+++ packages/plugins/@nocobase/plugin-data-source-main/src/client/__e2e__/fields/checkbox/schemaSettings.test.ts
@@ -159,7 +159,7 @@ test.describe('form item & edit form', () => {
       gotoPage: async () => {
         record = await (async (mockPage, mockRecord) => {
           const nocoPage = await mockPage(oneTableBlockWithAddNewAndViewAndEditAndChoicesFields).waitForInit();
-          const record = await mockRecord('general');
+          const record = await mockRecord('general', { checkbox: false });
           await nocoPage.goto();
 
           return record;
