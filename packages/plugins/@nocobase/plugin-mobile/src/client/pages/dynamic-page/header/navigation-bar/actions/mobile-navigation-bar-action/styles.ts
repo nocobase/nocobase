627f22cb240ba2e29770e01308aff4c7df3a2add
Zeke Zhang <958414905@qq.com>
2024-10-30 16:07:11 +0800

    perf(MobileNavigationBarAction): improve style performance

--- packages/plugins/@nocobase/plugin-mobile/src/client/pages/dynamic-page/header/navigation-bar/actions/mobile-navigation-bar-action/styles.ts
+++ packages/plugins/@nocobase/plugin-mobile/src/client/pages/dynamic-page/header/navigation-bar/actions/mobile-navigation-bar-action/styles.ts
@@ -7,43 +7,48 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
-import { createStyles } from 'antd-style';
+import { genStyleHook } from '@nocobase/client';
 
-export const useStyles = createStyles(() => ({
-  navigationBarAction: {
-    maxWidth: '10em',
-    '.adm-space': {
-      maxWidth: '100%',
-      overflow: 'hidden',
-    },
-  },
-  navigationBarActionIcon: {
-    width: 24,
-    height: 24,
-    lineHeight: '24px',
-    fontSize: 24,
-    padding: 0,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+export const useStyles = genStyleHook('nb-mobile-navigation-bar-action', (token) => {
+  const { componentCls } = token;
+  return {
+    [componentCls]: {
+      '.nb-navigation-bar-action': {
+        maxWidth: '10em',
+        '.adm-space': {
+          maxWidth: '100%',
+          overflow: 'hidden',
+        },
+      },
+      '.nb-navigation-bar-action-icon': {
+        width: 24,
+        height: 24,
+        lineHeight: '24px',
+        fontSize: 24,
+        padding: 0,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
 
-    '.schema-toolbar': {
-      inset: '-15px -8px',
+        '.schema-toolbar': {
+          inset: '-15px -8px',
+        },
+      },
+      '.nb-navigation-bar-action-title': {
+        fontSize: 17,
+        padding: 0,
+        '.schema-toolbar': {
+          inset: '-15px -8px',
+        },
+      },
+      '.nb-navigation-bar-action-icon-and-title': {
+        height: '32px !important',
+        fontSize: '17px !important',
+        padding: '0 6px !important',
+        '.schema-toolbar': {
+          inset: '-15px',
+        },
+      },
     },
-  },
-  navigationBarActionTitle: {
-    fontSize: 17,
-    padding: 0,
-    '.schema-toolbar': {
-      inset: '-15px -8px',
-    },
-  },
-  navigationBarActionIconAndTitle: {
-    height: '32px !important',
-    fontSize: '17px !important',
-    padding: '0 6px !important',
-    '.schema-toolbar': {
-      inset: '-15px',
-    },
-  },
-}));
+  };
+});
