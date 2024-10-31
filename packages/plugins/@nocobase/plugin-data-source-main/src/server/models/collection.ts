e3034a8541082f8031d158d99f016f81de47fbef
Chareice <chareice@live.com>
2024-10-31 22:40:31 +0800

    fix: test

--- packages/plugins/@nocobase/plugin-data-source-main/src/server/models/collection.ts
+++ packages/plugins/@nocobase/plugin-data-source-main/src/server/models/collection.ts
@@ -63,7 +63,7 @@ export class CollectionModel extends MagicAttributeModel {
       delete collectionOptions.schema;
     }
 
-    if (this.db.inDialect('postgres') && !collectionOptions.schema) {
+    if (this.db.inDialect('postgres') && !collectionOptions.schema && collectionOptions.from !== 'db2cm') {
       collectionOptions.schema = process.env.COLLECTION_MANAGER_SCHEMA || this.db.options.schema || 'public';
     }
 
