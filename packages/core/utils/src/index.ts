45976f37b2d4373c74eae492db0177987dc06aac
Junyi <mytharcher@users.noreply.github.com>
2024-11-06 09:59:27 +0800

    feat(plugin-notification-manager): support handlebars in notification message (#5559)
    * feat(plugin-notification-manager): support handlebars in message
    
    * fix(plugin-notification-manager): fix parsing null template
    
    * fix(plugin-notification-manager): fix compile

--- packages/core/utils/src/index.ts
+++ packages/core/utils/src/index.ts
@@ -17,6 +17,7 @@ export * from './date';
 export * from './dayjs';
 export * from './forEach';
 export * from './fs-exists';
+export * from './handlebars';
 export * from './isValidFilter';
 export * from './json-templates';
 export * from './koa-multer';
