f1702117e84d7431d9f02fce8c6736f8749ac3e9
katherinehhh <katherine_15995@163.com>
2024-11-07 15:36:38 +0800

    fix: bug

--- packages/plugins/@nocobase/plugin-mobile/src/client/pages/dynamic-page/MobilePage.tsx
+++ packages/plugins/@nocobase/plugin-mobile/src/client/pages/dynamic-page/MobilePage.tsx
@@ -7,33 +7,24 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
-import { RemoteSchemaComponent } from '@nocobase/client';
+import { RemoteSchemaComponent, AssociatedFields } from '@nocobase/client';
 import React, { useCallback } from 'react';
 import { Outlet, useParams } from 'react-router-dom';
-import {
-  Button as MobileButton,
-  Input as MobileInput,
-  Checkbox as MobileCheckbox,
-  Radio as MobileRadio,
-  Form as MobileForm,
-  List as MobileList,
-  Dialog as MobileDialog,
-} from 'antd-mobile';
+import { Button as MobileButton, Form as MobileForm, List as MobileList, Dialog as MobileDialog } from 'antd-mobile';
 import { MobilePicker } from './components/MobilePicker';
 import { MobileDateTimePicker } from './components/MobileDatePicker';
 
 const mobileComponents = {
   Button: MobileButton,
-  // Input: MobileInput,
   Select: MobilePicker,
   DatePicker: MobileDateTimePicker,
   UnixTimestamp: MobileDateTimePicker,
-  // Checkbox: MobileCheckbox,
-  // Radio: MobileRadio,
-  Form: MobileForm,
-  List: MobileList,
   Modal: MobileDialog,
-  // 添加其他组件映射
+  AssociatedFields: (
+    <div contentEditable="false">
+      <AssociatedFields />
+    </div>
+  ),
 };
 
 export const MobilePage = () => {
