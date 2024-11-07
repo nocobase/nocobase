e70d2d1c9e7f49ae1dd27f5b40845dc29e4de429
YANG QIA <2013xile@gmail.com>
2024-11-07 20:41:03 +0800

    chore(auth): optimize error message (#5612)

--- packages/plugins/@nocobase/plugin-error-handler/src/server/server.ts
+++ packages/plugins/@nocobase/plugin-error-handler/src/server/server.ts
@@ -56,7 +56,7 @@ export class PluginErrorHandlerServer extends Plugin {
             return {
               message: t(err.type, {
                 ns: this.i18nNs,
-                field: t(title, { ns: 'lm-collections' }),
+                field: t(title, { ns: ['lm-collections', 'client'] }),
               }),
             };
           }),
