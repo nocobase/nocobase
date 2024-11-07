56089dbdf92a10167c25ad98ca96e0b4c953a6de
xilesun <2013xile@gmail.com>
2024-11-07 21:56:11 +0800

    fix(error-handler): test

--- packages/plugins/@nocobase/plugin-error-handler/src/server/__tests__/render-error.test.ts
+++ packages/plugins/@nocobase/plugin-error-handler/src/server/__tests__/render-error.test.ts
@@ -112,7 +112,7 @@ describe('create with exception', () => {
 
     expect(response.statusCode).toEqual(400);
 
-    expect(response.body['errors'][0]['message']).toBe('name must be unique');
+    expect(response.body['errors'][0]['message']).toBe('name already exists');
   });
 
   it('should render error with field title', async () => {
