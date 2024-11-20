d00708c17650d0be8637c5a0649d279c34764a4b
Chareice <chareice@live.com>
2024-11-20 14:07:07 +0800

    chore: base exporter

---
+++ packages/plugins/@nocobase/plugin-action-export/src/server/services/base-exporter.ts
@@ -0,0 +1,149 @@
+import {
+  FindOptions,
+  ICollection,
+  ICollectionManager,
+  IField,
+  IModel,
+  IRelationField,
+} from '@nocobase/data-source-manager';
+import EventEmitter from 'events';
+import { deepGet } from '../utils/deep-get';
+
+export type ExportOptions = {
+  collectionManager: ICollectionManager;
+  collection: ICollection;
+  repository?: any;
+  fields: Array<Array<string>>;
+  findOptions?: FindOptions;
+  chunkSize?: number;
+};
+
+abstract class BaseExporter<T extends ExportOptions> extends EventEmitter {
+  limit = process.env['EXPORT_LIMIT'] ? parseInt(process.env['EXPORT_LIMIT']) : 2000;
+
+  protected constructor(protected options: T) {
+    super();
+  }
+
+  abstract init(ctx?): Promise<void>;
+  abstract finalize(): Promise<any>;
+  abstract addRows(rows: Array<any>, ctx?): Promise<void>;
+
+  async run(ctx?): Promise<any> {
+    await this.init(ctx);
+
+    const { collection, chunkSize, repository } = this.options;
+
+    const total = await (repository || collection.repository).count(this.getFindOptions());
+    let current = 0;
+
+    await (repository || collection.repository).chunk({
+      ...this.getFindOptions(),
+      chunkSize: chunkSize || 200,
+      callback: async (rows, options) => {
+        await this.addRows(rows, ctx);
+
+        current += rows.length;
+
+        this.emit('progress', {
+          total,
+          current,
+        });
+      },
+    });
+
+    return this.finalize();
+  }
+
+  protected getAppendOptionsFromFields() {
+    return this.options.fields
+      .map((field) => {
+        const fieldInstance = this.options.collection.getField(field[0]);
+        if (!fieldInstance) {
+          throw new Error(`Field "${field[0]}" not found: , please check the fields configuration.`);
+        }
+
+        if (fieldInstance.isRelationField()) {
+          return field.join('.');
+        }
+
+        return null;
+      })
+      .filter(Boolean);
+  }
+
+  protected getFindOptions() {
+    const { findOptions = {} } = this.options;
+
+    findOptions.limit = this.limit;
+
+    const appendOptions = this.getAppendOptionsFromFields();
+
+    if (appendOptions.length) {
+      return {
+        ...findOptions,
+        appends: appendOptions,
+      };
+    }
+
+    return findOptions;
+  }
+
+  protected findFieldByDataIndex(dataIndex: Array<string>): IField {
+    const { collection } = this.options;
+    const currentField = collection.getField(dataIndex[0]);
+
+    if (dataIndex.length > 1) {
+      let targetCollection: ICollection;
+
+      for (let i = 0; i < dataIndex.length; i++) {
+        const isLast = i === dataIndex.length - 1;
+
+        if (isLast) {
+          return targetCollection.getField(dataIndex[i]);
+        }
+
+        targetCollection = (currentField as IRelationField).targetCollection();
+      }
+    }
+
+    return currentField;
+  }
+
+  protected renderRawValue(value) {
+    if (typeof value === 'object' && value !== null) {
+      return JSON.stringify(value);
+    }
+
+    return value;
+  }
+
+  protected getFieldRenderer(field?: IField, ctx?): (value) => any {
+    const InterfaceClass = this.options.collectionManager.getFieldInterface(field?.options?.interface);
+    if (!InterfaceClass) {
+      return this.renderRawValue;
+    }
+    const fieldInterface = new InterfaceClass(field?.options);
+    return (value) => fieldInterface.toString(value, ctx);
+  }
+
+  protected formatValue(rowData: IModel, dataIndex: Array<string>, ctx?) {
+    rowData = rowData.toJSON();
+    const value = rowData[dataIndex[0]];
+    const field = this.findFieldByDataIndex(dataIndex);
+    const render = this.getFieldRenderer(field, ctx);
+
+    if (dataIndex.length > 1) {
+      const deepValue = deepGet(rowData, dataIndex);
+
+      if (Array.isArray(deepValue)) {
+        return deepValue.map(render).join(',');
+      }
+
+      return render(deepValue);
+    }
+    return render(value);
+  }
+}
+
+export default BaseExporter;
--- packages/plugins/@nocobase/plugin-action-export/src/server/xlsx-exporter.ts
+++ packages/plugins/@nocobase/plugin-action-export/src/server/xlsx-exporter.ts
@@ -1,25 +1,6 @@
-/**
- * This file is part of the NocoBase (R) project.
- * Copyright (c) 2020-2024 NocoBase Co., Ltd.
- * Authors: NocoBase Team.
- *
- * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
- * For more information, please refer to: https://www.nocobase.com/agreement.
- */
-
-import {
-  FindOptions,
-  ICollection,
-  ICollectionManager,
-  IField,
-  IModel,
-  IRelationField,
-} from '@nocobase/data-source-manager';
-
 import XLSX from 'xlsx';
-import { deepGet } from './utils/deep-get';
+import BaseExporter, { ExportOptions } from './services/base-exporter';
 import { NumberField } from '@nocobase/database';
-import EventEmitter from 'events';
 
 type ExportColumn = {
   dataIndex: Array<string>;
@@ -27,87 +8,56 @@ type ExportColumn = {
   defaultTitle: string;
 };
 
-type ExportOptions = {
-  collectionManager: ICollectionManager;
-  collection: ICollection;
-  repository?: any;
+type XlsxExportOptions = Omit<ExportOptions, 'fields'> & {
   columns: Array<ExportColumn>;
-  findOptions?: FindOptions;
-  chunkSize?: number;
 };
 
-class XlsxExporter extends EventEmitter {
-  /**
-   * You can adjust the maximum number of exported rows based on business needs and system
-   * available resources. However, please note that you need to fully understand the risks
-   * after the modification. Increasing the maximum number of rows that can be exported may
-   * increase system resource usage, leading to increased processing delays for other
-   * requests, or even server processes being recycled by the operating system.
-   *
-   * 您可以根据业务需求和系统可用资源等参数，调整最大导出数量的限制。但请注意，您需要充分了解修改之后的风险，
-   * 增加最大可导出的行数可能会导致系统资源占用率升高，导致其他请求处理延迟增加、无法处理、甚至
-   * 服务端进程被操作系统回收等问题。
-   */
-  limit = process.env['EXPORT_LIMIT'] ? parseInt(process.env['EXPORT_LIMIT']) : 2000;
+class XlsxExporter extends BaseExporter<XlsxExportOptions & { fields: Array<Array<string>> }> {
+  private workbook: XLSX.WorkBook;
+  private worksheet: XLSX.WorkSheet;
+  private startRowNumber: number;
 
-  constructor(private options: ExportOptions) {
-    super();
+  constructor(options: XlsxExportOptions) {
+    const fields = options.columns.map((col) => col.dataIndex);
+    super({ ...options, fields });
   }
 
-  async run(ctx?): Promise<XLSX.WorkBook> {
-    const { collection, columns, chunkSize, repository } = this.options;
-
-    const workbook = XLSX.utils.book_new();
-    const worksheet = XLSX.utils.sheet_new();
+  async init(ctx?): Promise<void> {
+    this.workbook = XLSX.utils.book_new();
+    this.worksheet = XLSX.utils.sheet_new();
 
     // write headers
-    XLSX.utils.sheet_add_aoa(worksheet, [this.renderHeaders()], {
+    XLSX.utils.sheet_add_aoa(this.worksheet, [this.renderHeaders(this.options.columns)], {
       origin: 'A1',
     });
 
-    let startRowNumber = 2;
+    this.startRowNumber = 2;
+  }
 
-    const total = await (repository || collection.repository).count(this.getFindOptions());
-    let current = 0;
-
-    await (repository || collection.repository).chunk({
-      ...this.getFindOptions(),
-      chunkSize: chunkSize || 200,
-      callback: async (rows, options) => {
-        const chunkData = rows.map((r) => {
-          return columns.map((col) => {
-            return this.renderCellValue(r, col, ctx);
-          });
-        });
-
-        XLSX.utils.sheet_add_aoa(worksheet, chunkData, {
-          origin: `A${startRowNumber}`,
-        });
-
-        startRowNumber += rows.length;
-
-        await new Promise((resolve) => {
-          setTimeout(resolve, 50);
-        });
-
-        current += rows.length;
-
-        this.emit('progress', {
-          total,
-          current,
-        });
-      },
+  async addRows(rows: Array<any>, ctx?): Promise<void> {
+    const chunkData = rows.map((r) => {
+      return this.options.columns.map((col) => {
+        return this.formatValue(r, col.dataIndex, ctx);
+      });
     });
 
-    for (const col of columns) {
-      const field = this.findFieldByDataIndex(col.dataIndex);
-      if (field instanceof NumberField) {
+    XLSX.utils.sheet_add_aoa(this.worksheet, chunkData, {
+      origin: `A${this.startRowNumber}`,
+    });
+
+    this.startRowNumber += rows.length;
+  }
+
+  async finalize(): Promise<XLSX.WorkBook> {
+    for (const col of this.options.columns) {
+      const fieldInstance = this.findFieldByDataIndex(col.dataIndex);
+      if (fieldInstance instanceof NumberField) {
         // set column cell type to number
-        const colIndex = columns.indexOf(col);
-        const cellRange = XLSX.utils.decode_range(worksheet['!ref']);
+        const colIndex = this.options.columns.indexOf(col);
+        const cellRange = XLSX.utils.decode_range(this.worksheet['!ref']);
 
         for (let r = 1; r <= cellRange.e.r; r++) {
-          const cell = worksheet[XLSX.utils.encode_cell({ c: colIndex, r })];
+          const cell = this.worksheet[XLSX.utils.encode_cell({ c: colIndex, r })];
           // if cell and cell.v is a number, set cell.t to 'n'
           if (cell && isNumeric(cell.v)) {
             cell.t = 'n';
@@ -116,114 +66,15 @@ class XlsxExporter extends EventEmitter {
       }
     }
 
-    XLSX.utils.book_append_sheet(workbook, worksheet, 'Data');
-    return workbook;
+    XLSX.utils.book_append_sheet(this.workbook, this.worksheet, 'Data');
+    return this.workbook;
   }
 
-  private getAppendOptionsFromColumns() {
-    return this.options.columns
-      .map((col) => {
-        if (col.dataIndex.length > 1) {
-          return col.dataIndex.join('.');
-        }
-
-        const field = this.options.collection.getField(col.dataIndex[0]);
-        if (!field) {
-          throw new Error(`Field "${col.dataIndex[0]}" not found: , please check the columns configuration.`);
-        }
-
-        if (field.isRelationField()) {
-          return col.dataIndex[0];
-        }
-
-        return null;
-      })
-      .filter(Boolean);
-  }
-
-  private getFindOptions() {
-    const { findOptions = {} } = this.options;
-
-    findOptions.limit = this.limit;
-
-    const appendOptions = this.getAppendOptionsFromColumns();
-
-    if (appendOptions.length) {
-      return {
-        ...findOptions,
-        appends: appendOptions,
-      };
-    }
-
-    return findOptions;
-  }
-
-  private findFieldByDataIndex(dataIndex: Array<string>): IField {
-    const { collection } = this.options;
-    const currentField = collection.getField(dataIndex[0]);
-
-    if (dataIndex.length > 1) {
-      let targetCollection: ICollection;
-
-      for (let i = 0; i < dataIndex.length; i++) {
-        const isLast = i === dataIndex.length - 1;
-
-        if (isLast) {
-          return targetCollection.getField(dataIndex[i]);
-        }
-
-        targetCollection = (currentField as IRelationField).targetCollection();
-      }
-    }
-
-    return currentField;
-  }
-
-  private renderHeaders() {
-    return this.options.columns.map((col) => {
-      const field = this.findFieldByDataIndex(col.dataIndex);
-      if (col.title) {
-        return col.title;
-      }
-
-      return field?.options.title || col.defaultTitle;
+  private renderHeaders(columns: Array<ExportColumn>) {
+    return columns.map((col) => {
+      const fieldInstance = this.findFieldByDataIndex(col.dataIndex);
+      return col.title || fieldInstance?.options.title || col.defaultTitle;
     });
-  }
-
-  private renderRawValue(value) {
-    if (typeof value === 'object' && value !== null) {
-      return JSON.stringify(value);
-    }
-
-    return value;
-  }
-
-  private getFieldRenderer(field?: IField, ctx?): (value) => any {
-    const InterfaceClass = this.options.collectionManager.getFieldInterface(field?.options?.interface);
-    if (!InterfaceClass) {
-      return this.renderRawValue;
-    }
-    const fieldInternface = new InterfaceClass(field?.options);
-    return (value) => fieldInternface.toString(value, ctx);
-  }
-
-  private renderCellValue(rowData: IModel, column: ExportColumn, ctx?) {
-    const { dataIndex } = column;
-    rowData = rowData.toJSON();
-    const value = rowData[dataIndex[0]];
-    const field = this.findFieldByDataIndex(dataIndex);
-    const render = this.getFieldRenderer(field, ctx);
-
-    if (dataIndex.length > 1) {
-      const deepValue = deepGet(rowData, dataIndex);
-
-      if (Array.isArray(deepValue)) {
-        return deepValue.map(render).join(',');
-      }
-
-      return render(deepValue);
-    }
-    return render(value);
   }
 }
 
