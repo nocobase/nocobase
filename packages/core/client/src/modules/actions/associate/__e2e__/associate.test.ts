cad7f1c2e731fba6d7ed7f3519bac2b18d6b576d
katherinehhh <katherine_15995@163.com>
2024-11-22 16:35:44 +0800

    test: e2e test

--- packages/core/client/src/modules/actions/associate/__e2e__/associate.test.ts
+++ packages/core/client/src/modules/actions/associate/__e2e__/associate.test.ts
@@ -21,13 +21,25 @@ test('basic', async ({ page, mockPage, mockRecord }) => {
   await page.getByText('Disassociate').click();
   await expect(page.getByLabel('action-Action-Disassociate-')).toBeVisible();
 
-  await page.getByLabel('block-item-CardItem-cc-table').hover();
-  await page.getByLabel('schema-initializer-ActionBar-table:configureActions-cc').hover();
-  await expect(page.getByRole('menuitem', { name: 'Associate' })).toBeVisible();
   await page
     .getByTestId('drawer-Action.Container-aa-Edit record')
     .getByRole('button', { name: 'Actions', exact: true })
     .hover();
   await page.getByLabel('designer-schema-initializer-TableV2.Column-fieldSettings:TableColumn-cc').hover();
   await expect(page.getByRole('tooltip').getByText('Disassociate')).toBeVisible();
+
+  await page.getByLabel('block-item-CardItem-cc-table').hover();
+  await page.getByLabel('schema-initializer-ActionBar-table:configureActions-cc').hover();
+  await page.getByRole('menuitem', { name: 'Associate' }).click();
+  //点击 associate 出现弹窗
+  await page.getByLabel('action-Action-Associate-').click();
+  await page.getByTestId('drawer-Action.Container-cc-Select record').click();
+  await page
+    .getByTestId('drawer-Action.Container-cc-Select record')
+    .getByLabel('schema-initializer-Grid-popup')
+    .hover();
+  await page.getByRole('menuitem', { name: 'form Table' }).click();
+  await expect(
+    page.getByTestId('drawer-Action.Container-cc-Select record').getByLabel('block-item-CardItem-cc-'),
+  ).toBeVisible();
 });
