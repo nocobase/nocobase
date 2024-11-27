486e002977d503e2065b0168302726fd0e41ec6a
xilesun <2013xile@gmail.com>
2024-11-27 12:46:43 +0800

    chore: optimize

--- packages/plugins/@nocobase/plugin-auth/src/server/plugin.ts
+++ packages/plugins/@nocobase/plugin-auth/src/server/plugin.ts
@@ -52,9 +52,19 @@ export class PluginAuthServer extends Plugin {
       title: tval('Password', { ns: namespace }),
       getPublicOptions: (options) => {
         const usersCollection = this.db.getCollection('users');
-        let signupForm = options?.public?.signupForm;
-        if (!(signupForm?.length && signupForm.some((item: any) => item.show && item.required))) {
-          signupForm = [{ field: 'username', show: true, required: true }];
+        let signupForm = options?.public?.signupForm || [];
+        signupForm = signupForm.filter((item: { show: boolean }) => item.show);
+        if (
+          !(
+            signupForm.length &&
+            signupForm.some(
+              (item: { field: string; show: boolean; required: boolean }) =>
+                ['username', 'email'].includes(item.field) && item.show && item.required,
+            )
+          )
+        ) {
+          // At least one of the username or email fields is required
+          signupForm.unshift({ field: 'username', show: true, required: true });
         }
         signupForm = signupForm
           .filter((field: { show: boolean }) => field.show)
@@ -76,10 +86,10 @@ export class PluginAuthServer extends Plugin {
     });
     // Register actions
     Object.entries(authActions).forEach(
-      ([action, handler]) => this.app.resourcer.getResource('auth')?.addAction(action, handler),
+      ([action, handler]) => this.app.resourceManager.getResource('auth')?.addAction(action, handler),
     );
     Object.entries(authenticatorsActions).forEach(([action, handler]) =>
-      this.app.resourcer.registerAction(`authenticators:${action}`, handler),
+      this.app.resourceManager.registerActionHandler(`authenticators:${action}`, handler),
     );
     // Set up ACL
     ['check', 'signIn', 'signUp'].forEach((action) => this.app.acl.allow('auth', action));
