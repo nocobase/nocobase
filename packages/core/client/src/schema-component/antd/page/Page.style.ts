f06391a5d870986a8e4a53b91ed6d2efe7e86eb0
Zeke Zhang <958414905@qq.com>
2024-11-28 19:06:14 +0800

    chore(page): should remember page scroll position

--- packages/core/client/src/schema-component/antd/page/Page.style.ts
+++ packages/core/client/src/schema-component/antd/page/Page.style.ts
@@ -18,9 +18,11 @@ export const useStyles = genStyleHook('nb-page', (token) => {
       top: 0,
       left: 0,
       right: 0,
+      bottom: 0,
       zIndex: 20,
       display: 'flex',
       flexDirection: 'column',
+      overflowY: 'auto',
 
       '&:hover': { '> .general-schema-designer': { display: 'block' } },
       '.ant-page-header': { zIndex: 1, position: 'relative' },
