ff80daca59329475af402ab4b5774d134f3e008d
katherinehhh <katherine_15995@163.com>
2024-12-02 22:00:40 +0800

    fix: record picker not displaying selected items when selecting across pages

--- packages/core/client/src/schema-component/antd/association-field/InternalPicker.tsx
+++ packages/core/client/src/schema-component/antd/association-field/InternalPicker.tsx
@@ -51,6 +51,9 @@ export const useTableSelectorProps = () => {
     rowKey,
     rowSelection: {
       type: multiple ? 'checkbox' : 'radio',
+      selectedRowKeys: rcSelectRows
+        ?.filter((item) => options.every((row) => row[rowKey] !== item[rowKey]))
+        .map((item) => item[rowKey]),
     },
     onRowSelectionChange(selectedRowKeys, selectedRows) {
       if (multiple) {
