eab571eaaeb48d85d35bd3eafc84809d534c1b3b
Zeke Zhang <958414905@qq.com>
2024-12-03 17:29:42 +0800

    fix(variable): resolve field visibility issues in multi-data source variables

--- packages/core/client/src/schema-settings/VariableInput/hooks/useFormVariable.ts
+++ packages/core/client/src/schema-settings/VariableInput/hooks/useFormVariable.ts
@@ -12,7 +12,7 @@ import { Schema } from '@formily/json-schema';
 import { useTranslation } from 'react-i18next';
 import { useFormBlockContext } from '../../../block-provider/FormBlockProvider';
 import { CollectionFieldOptions_deprecated } from '../../../collection-manager';
-import { useDataBlockRequestData } from '../../../data-source';
+import { useDataBlockRequestData, useDataSource } from '../../../data-source';
 import { useFlag } from '../../../flag-provider/hooks/useFlag';
 import { useBaseVariable } from './useBaseVariable';
 
@@ -107,6 +107,7 @@ export const useCurrentFormVariable = ({
   const { currentFormCtx, shouldDisplayCurrentForm } = useCurrentFormContext({ form: _form });
   const { t } = useTranslation();
   const { collectionName } = useFormBlockContext();
+  const dataSource = useDataSource();
   const currentFormSettings = useBaseVariable({
     collectionField,
     uiSchema: schema,
@@ -116,6 +117,7 @@ export const useCurrentFormVariable = ({
     title: t('Current form'),
     collectionName: collectionName,
     noDisabled,
+    dataSource: dataSource.key,
     returnFields: (fields, option) => {
       // fix https://nocobase.height.app/T-2277
       return fields;
