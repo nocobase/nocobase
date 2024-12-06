ba1446c77f0863cf20d19ad7cb0e0a21ab2c2d33
ChengLei Shao <chareice@live.com>
2024-12-06 14:57:58 +0800

    chore: empty filter check (#5806)
    * chore: empty filter check
    
    * chore: test
    
    * fix: test
    
    * fix: test
    
    ---------
    
    Co-authored-by: xilesun <2013xile@gmail.com>

--- packages/core/server/src/middlewares/validate-filter-params.ts
+++ packages/core/server/src/middlewares/validate-filter-params.ts
@@ -13,9 +13,40 @@ export default async function validateFilterParams(ctx, next) {
   const { params } = ctx.action;
   const guardedActions = ['update', 'destroy'];
 
+  const { actionName } = params;
+
+  if (skipValidate(params)) {
+    return await next();
+  }
+
+  if (emptyFilter(params) && emptyFilterByTk(params)) {
+    throw new Error(`to do ${actionName} action, filter or filterByTk is required`);
+  }
+
   if (params.filter && !isValidFilter(params.filter) && guardedActions.includes(params.actionName)) {
     throw new Error(`Invalid filter: ${JSON.stringify(params.filter)}`);
   }
 
   await next();
+}
+
+function emptyFilter(params) {
+  return !isValidFilter(params.filter);
+}
+
+function emptyFilterByTk(params) {
+  return !params.filterByTk;
+}
+
+function skipValidate(actionParams) {
+  const { actionName } = actionParams;
+  if (actionName === 'update') {
+    return actionParams.forceUpdate;
+  }
+
+  if (actionName === 'destroy') {
+    return actionParams.truncate;
+  }
+
+  return true;
 }
