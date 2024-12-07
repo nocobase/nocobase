122734d355c9d2ba5d93ab92c5aa0fdc5c498fb3
Zeke Zhang <958414905@qq.com>
2024-12-07 22:42:03 +0800

    fix(Details): fix refresh issue

--- packages/core/client/src/schema-component/antd/association-field/InternalTag.tsx
+++ packages/core/client/src/schema-component/antd/association-field/InternalTag.tsx
@@ -7,7 +7,7 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
-import { useFieldSchema } from '@formily/react';
+import { observer, useFieldSchema } from '@formily/react';
 import { toArr } from '@formily/shared';
 import React, { Fragment, useRef } from 'react';
 import { useDesignable } from '../../';
@@ -33,72 +33,77 @@ const toValue = (value, placeholder) => {
   return value;
 };
 
-const ButtonTabList: React.FC<ButtonListProps> = (props) => {
-  const fieldSchema = useFieldSchema();
-  const { enableLink, tagColorField } = fieldSchema['x-component-props'];
-  const fieldNames = useFieldNames({ fieldNames: props.fieldNames });
-  const insertViewer = useInsertSchema('Viewer');
-  const { options: collectionField } = useAssociationFieldContext();
-  const compile = useCompile();
-  const { designable } = useDesignable();
-  const labelUiSchema = useLabelUiSchema(collectionField, fieldNames?.label || 'label');
-  const { snapshot } = useActionContext();
-  const ellipsisWithTooltipRef = useRef<IEllipsisWithTooltipRef>();
-  const { getCollection } = useCollectionManager_deprecated();
-  const targetCollection = getCollection(collectionField?.target);
-  const isTreeCollection = targetCollection?.template === 'tree';
-  const { openPopup } = usePopupUtils();
-  const recordData = useCollectionRecordData();
+const ButtonTabList: React.FC<ButtonListProps> = observer(
+  (props) => {
+    const fieldSchema = useFieldSchema();
+    const { enableLink, tagColorField } = fieldSchema['x-component-props'];
+    const fieldNames = useFieldNames({ fieldNames: props.fieldNames });
+    const insertViewer = useInsertSchema('Viewer');
+    const { options: collectionField } = useAssociationFieldContext();
+    const compile = useCompile();
+    const { designable } = useDesignable();
+    const labelUiSchema = useLabelUiSchema(collectionField, fieldNames?.label || 'label');
+    const { snapshot } = useActionContext();
+    const ellipsisWithTooltipRef = useRef<IEllipsisWithTooltipRef>();
+    const { getCollection } = useCollectionManager_deprecated();
+    const targetCollection = getCollection(collectionField?.target);
+    const isTreeCollection = targetCollection?.template === 'tree';
+    const { openPopup } = usePopupUtils();
+    const recordData = useCollectionRecordData();
 
-  const renderRecords = () =>
-    toArr(props.value).map((record, index, arr) => {
-      const value = record?.[fieldNames?.label || 'label'];
-      const label = isTreeCollection
-        ? transformNestedData(record)
-            .map((o) => o?.[fieldNames?.label || 'label'])
-            .join(' / ')
-        : isObject(value)
-          ? JSON.stringify(value)
-          : value;
-      const val = toValue(compile(label), 'N/A');
-      const text = getTabFormatValue(compile(labelUiSchema), val, record[tagColorField]);
-      return (
-        <Fragment key={`${record?.[fieldNames.value]}_${index}`}>
-          <span>
-            {snapshot ? (
-              text
-            ) : enableLink !== false ? (
-              <a
-                onMouseEnter={() => {
-                  props.setBtnHover(true);
-                }}
-                onClick={(e) => {
-                  props.setBtnHover(true);
-                  e.stopPropagation();
-                  e.preventDefault();
-                  if (designable) {
-                    insertViewer(schema.Viewer);
-                  }
-                  openPopup({
-                    recordData: record,
-                    parentRecordData: recordData,
-                  });
-                  ellipsisWithTooltipRef?.current?.setPopoverVisible(false);
-                }}
-              >
-                {text}
-              </a>
-            ) : (
-              text
-            )}
-          </span>
-          {index < arr.length - 1 ? <span style={{ marginRight: 4, color: '#aaa' }}>,</span> : null}
-        </Fragment>
-      );
-    });
+    const renderRecords = () =>
+      toArr(props.value).map((record, index, arr) => {
+        const value = record?.[fieldNames?.label || 'label'];
+        const label = isTreeCollection
+          ? transformNestedData(record)
+              .map((o) => o?.[fieldNames?.label || 'label'])
+              .join(' / ')
+          : isObject(value)
+            ? JSON.stringify(value)
+            : value;
+        const val = toValue(compile(label), 'N/A');
+        const text = getTabFormatValue(compile(labelUiSchema), val, record[tagColorField]);
+        return (
+          <Fragment key={`${record?.[fieldNames.value]}_${index}`}>
+            <span>
+              {snapshot ? (
+                text
+              ) : enableLink !== false ? (
+                <a
+                  onMouseEnter={() => {
+                    props.setBtnHover(true);
+                  }}
+                  onClick={(e) => {
+                    props.setBtnHover(true);
+                    e.stopPropagation();
+                    e.preventDefault();
+                    if (designable) {
+                      insertViewer(schema.Viewer);
+                    }
+                    openPopup({
+                      recordData: record,
+                      parentRecordData: recordData,
+                    });
+                    ellipsisWithTooltipRef?.current?.setPopoverVisible(false);
+                  }}
+                >
+                  {text}
+                </a>
+              ) : (
+                text
+              )}
+            </span>
+            {index < arr.length - 1 ? <span style={{ marginRight: 4, color: '#aaa' }}>,</span> : null}
+          </Fragment>
+        );
+      });
 
-  return <>{renderRecords()}</>;
-};
+    return <>{renderRecords()}</>;
+  },
+  {
+    displayName: 'ButtonTabList',
+  },
+);
 
 export const ReadPrettyInternalTag: React.FC = (props: any) => {
   return <ReadPrettyInternalViewer {...props} ButtonList={ButtonTabList} />;
