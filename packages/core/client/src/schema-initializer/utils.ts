f451bea2e42ef8e1a721917969dda664ce26230f
katherinehhh <katherine_15995@163.com>
2024-12-07 22:20:14 +0800

    fix: third party date source missing fields

--- packages/core/client/src/schema-initializer/utils.ts
+++ packages/core/client/src/schema-initializer/utils.ts
@@ -121,7 +121,7 @@ export function useTableColumnInitializerFields() {
   const isReadPretty = isSubTable ? form.readPretty : true;
 
   return currentFields
-    .filter((v) => v.collectionName === name)
+    .filter((v) => !v.collectionName || v.collectionName === name)
     .filter((field) => field?.interface && field?.interface !== 'subTable' && !field?.treeChildren)
     .map((field) => {
       const interfaceConfig = getInterface(field.interface);
@@ -380,7 +380,7 @@ export const useFormItemInitializerFields = (options?: any) => {
   const action = fieldSchema?.['x-action'];
 
   return currentFields
-    .filter((v) => v.collectionName === name)
+    .filter((v) => !v.collectionName || v.collectionName === name)
     ?.filter((field) => field?.interface && !field?.treeChildren)
     ?.map((field) => {
       const interfaceConfig = getInterface(field.interface);
@@ -448,7 +448,7 @@ export const useFilterFormItemInitializerFields = (options?: any) => {
   const action = fieldSchema?.['x-action'];
 
   return currentFields
-    .filter((v) => v.collectionName === name)
+    .filter((v) => !v.collectionName || v.collectionName === name)
     ?.filter((field) => field?.interface && getInterface(field.interface)?.filterable)
     ?.map((field) => {
       const interfaceConfig = getInterface(field.interface);
