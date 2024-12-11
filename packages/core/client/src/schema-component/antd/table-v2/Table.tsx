5a62d90c4a1bcdd55a282dae221dd7ccb9217bfe
katherinehhh <katherine_15995@163.com>
2024-12-11 20:34:12 +0800

    fix: scrollbar issue

--- packages/core/client/src/schema-component/antd/table-v2/Table.tsx
+++ packages/core/client/src/schema-component/antd/table-v2/Table.tsx
@@ -619,6 +619,9 @@ const InternalNocoBaseTable = React.memo(
                   .ant-table-small .ant-table-cell {
                     padding: 8px ${token.paddingXS}px;
                   }
+                  .ant-table-cell-fix-right {
+                    padding: 8px 16px !important;
+                  }
                 }
               }
             }
