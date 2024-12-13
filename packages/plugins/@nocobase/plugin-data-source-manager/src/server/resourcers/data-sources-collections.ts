741433aa9f529c65aa9c9224f23faf32b1e6ecb2
Chareice <chareice@live.com>
2024-12-13 21:54:14 +0800

    fix: filter external data source collections

--- packages/plugins/@nocobase/plugin-data-source-manager/src/server/resourcers/data-sources-collections.ts
+++ packages/plugins/@nocobase/plugin-data-source-manager/src/server/resourcers/data-sources-collections.ts
@@ -8,6 +8,7 @@
  */
 
 import lodash from 'lodash';
+import { filterMatch } from '@nocobase/database';
 
 export default {
   name: 'dataSources.collections',
@@ -46,15 +47,9 @@ export default {
 
       const { paginate, filter } = ctx.action.params;
 
-      const filterTitle = lodash.get(filter, '$and.0.title.$includes')?.toLowerCase();
-      const filterName = lodash.get(filter, '$and.0.name.$includes')?.toLowerCase();
-
       const collections = lodash.sortBy(
         dataSource.collectionManager.getCollections().filter((collection) => {
-          return (
-            (!filterTitle || lodash.get(collection, 'options.title')?.toLowerCase().includes(filterTitle)) &&
-            (!filterName || collection.options.name.toLowerCase().includes(filterName))
-          );
+          return filterMatch(collection.options, filter);
         }),
         'name',
       );
