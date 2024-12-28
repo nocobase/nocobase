fe26c131e00dcbd9cbe6ea573f3e1058b8cde5a6
Zeke Zhang <958414905@qq.com>
2024-12-28 23:24:43 +0800

    feat: support to set Hidden for Tab

--- packages/core/client/src/schema-component/antd/page/PageTab.Settings.tsx
+++ packages/core/client/src/schema-component/antd/page/PageTab.Settings.tsx
@@ -7,8 +7,11 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
+import { ExclamationCircleFilled } from '@ant-design/icons';
 import { ISchema } from '@formily/json-schema';
-import { App } from 'antd';
+import { App, Modal } from 'antd';
+import _ from 'lodash';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { SchemaSettings } from '../../../application/schema-settings/SchemaSettings';
 import { useSchemaToolbar } from '../../../application/schema-toolbar';
@@ -72,6 +75,45 @@ export const pageTabSettings = new SchemaSettings({
               },
               false,
             );
+          },
+        };
+      },
+    },
+    {
+      name: 'hidden',
+      type: 'switch',
+      useComponentProps() {
+        const { t } = useTranslation();
+        const { schema } = useSchemaToolbar<{ schema: ISchema }>();
+        const { updateRoute } = useNocoBaseRoutes();
+        const { dn } = useDesignable();
+
+        return {
+          title: t('Hidden'),
+          checked: schema['x-component-props']?.hidden,
+          onChange: (v) => {
+            Modal.confirm({
+              title: '确定要隐藏该菜单吗？',
+              icon: <ExclamationCircleFilled />,
+              content: '隐藏后，该菜单将不再显示在菜单栏中。如需再次显示，需要去路由管理页面设置。',
+              async onOk() {
+                _.set(schema, 'x-component-props.hidden', !!v);
+
+                // 更新菜单对应的路由
+                if (schema['__route__']?.id) {
+                  await updateRoute(schema['__route__'].id, {
+                    hideInMenu: !!v,
+                  });
+                }
+
+                dn.emit('patch', {
+                  schema: {
+                    'x-uid': schema['x-uid'],
+                    'x-component-props': schema['x-component-props'],
+                  },
+                });
+              },
+            });
           },
         };
       },
