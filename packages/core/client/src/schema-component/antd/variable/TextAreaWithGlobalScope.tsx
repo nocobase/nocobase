b49760cf6841b4cb08eecc9d6e875d9d9c721fe0
katherinehhh <katherine_15995@163.com>
2025-01-02 15:59:09 +0800

    refactor: code improve

--- packages/core/client/src/schema-component/antd/variable/TextAreaWithGlobalScope.tsx
+++ packages/core/client/src/schema-component/antd/variable/TextAreaWithGlobalScope.tsx
@@ -32,21 +32,32 @@ const isVariable = (value) => {
   const regex = /{{.*?}}/;
   return regex.test(value);
 };
+interface TextAreaWithGlobalScopeProps {
+  supportsLineBreak?: boolean;
+  password?: boolean;
+  number?: boolean;
+  boolean?: boolean;
+  value?: any;
+  scope?: string | object;
+  [key: string]: any;
+}
 
-export const TextAreaWithGlobalScope = connect((props) => {
+export const TextAreaWithGlobalScope = connect((props: TextAreaWithGlobalScopeProps) => {
   const { supportsLineBreak, password, number, boolean, ...others } = props;
   const scope = useEnvironmentVariableOptions(props.scope);
+  const fieldNames = { value: 'name', label: 'title' };
+
   if (supportsLineBreak) {
-    return <RawTextArea {...others} scope={scope} fieldNames={{ value: 'name', label: 'title' }} rows={3} />;
+    return <RawTextArea {...others} scope={scope} fieldNames={fieldNames} rows={3} />;
   }
   if (number) {
-    return <Variable.Input {...props} scope={scope} fieldNames={{ value: 'name', label: 'title' }} />;
+    return <Variable.Input {...props} scope={scope} fieldNames={fieldNames} />;
   }
   if (password && props.value && !isVariable(props.value)) {
     return <Password {...others} autoFocus />;
   }
   if (boolean) {
-    return <Variable.Input {...props} scope={scope} fieldNames={{ value: 'name', label: 'title' }} />;
+    return <Variable.Input {...props} scope={scope} fieldNames={fieldNames} />;
   }
-  return <TextArea {...others} scope={scope} fieldNames={{ value: 'name', label: 'title' }} />;
+  return <TextArea {...others} scope={scope} fieldNames={fieldNames} />;
 }, mapReadPretty(Input.ReadPretty));
