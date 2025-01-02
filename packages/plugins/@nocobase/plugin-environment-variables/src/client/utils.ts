24f962f78843aba1c619019dcddbe50f00f4599c
katherinehhh <katherine_15995@163.com>
2025-01-02 11:51:37 +0800

    refactor: locale improve

--- packages/plugins/@nocobase/plugin-environment-variables/src/client/utils.ts
+++ packages/plugins/@nocobase/plugin-environment-variables/src/client/utils.ts
@@ -9,16 +9,18 @@
 
 import { useContext } from 'react';
 import { EnvAndSecretsContext } from './EnvironmentVariablesAndSecretsProvider';
+import { useT } from './locale';
 
 export const useGetEnvironmentVariables = () => {
+  const t = useT();
   const { variablesRequest } = useContext(EnvAndSecretsContext);
   const { data: variables, loading: variablesLoading } = variablesRequest || {};
   if (!variablesLoading && variables?.data?.length) {
     return {
       name: '$env',
-      title: 'Environment variables',
+      title: t('Environment variables'),
       value: '$env',
-      label: 'Environment variables',
+      label: t('Environment variables'),
       children: variables?.data
         .map((v) => {
           return { title: v.name, name: v.name, value: v.name, label: v.name };
