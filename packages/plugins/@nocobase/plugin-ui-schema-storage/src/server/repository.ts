2cf63eb43c9953505aa80ab7a36a0b6b2fd67c8e
gchust <gchust@qq.com>
2025-01-04 23:26:10 +0800

    chore: revert incorrect commit

--- packages/plugins/@nocobase/plugin-ui-schema-storage/src/server/repository.ts
+++ packages/plugins/@nocobase/plugin-ui-schema-storage/src/server/repository.ts
@@ -304,8 +304,7 @@ export class UiSchemaRepository extends Repository {
     await this.clearXUidPathCache(rootUid, transaction);
     if (!newSchema['properties']) {
       const s = await this.model.findByPk(rootUid, { transaction });
-      const sc = { ...s.toJSON(), ...newSchema };
-      s.set('schema', sc);
+      s.set('schema', { ...s.toJSON(), ...newSchema });
       // console.log(s.toJSON());
       await s.save({ transaction, hooks: false });
       if (newSchema['x-server-hooks']) {
