1738b11f5721bd56875a794ea96d65307713367c
chenos <chenlinxh@gmail.com>
2025-01-16 11:51:18 +0800

    fix: type MigrationRule

--- packages/core/database/src/collection.ts
+++ packages/core/database/src/collection.ts
@@ -88,7 +88,7 @@ export type DumpRules =
   | ({ skipped: true } & BaseDumpRules)
   | ({ group: BuiltInGroup | string } & BaseDumpRules);
 
-export type MigrationRule = 'overwrite' | 'skip' | 'upsert' | 'schema-only' | 'insert-ignore';
+export type MigrationRule = 'overwrite' | 'skip' | 'upsert' | 'schema-only' | 'insert-ignore' | (string & {}) | null;
 
 export interface CollectionOptions extends Omit<ModelOptions, 'name' | 'hooks'> {
   name: string;
