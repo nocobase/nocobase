5019059321ec29425edeb0cca3b0b07e6edd68c1
Zeke Zhang <958414905@qq.com>
2025-01-16 16:41:57 +0800

    fix: fix group issue

--- packages/plugins/@nocobase/plugin-client/src/client/routesTableSchema.tsx
+++ packages/plugins/@nocobase/plugin-client/src/client/routesTableSchema.tsx
@@ -611,9 +611,9 @@ export const createRoutesTableSchema = (collectionName: string, basename: string
                   const recordData = useCollectionRecordData();
                   return {
                     disabled:
-                      !recordData.enableTabs ||
                       (recordData.type !== NocoBaseDesktopRouteType.group &&
-                        recordData.type !== NocoBaseDesktopRouteType.page),
+                        recordData.type !== NocoBaseDesktopRouteType.page) ||
+                      (!recordData.enableTabs && recordData.type === NocoBaseDesktopRouteType.page),
                     openMode: 'drawer',
                   };
                 },
