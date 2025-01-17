2945532630b1215bd6d2bb975a36d23ea26c14ee
Katherine <katherine_15995@163.com>
2025-01-17 20:39:39 +0800

    feat: support setting description property in block (#6015)
    * feat: support setting description property in block
    
    * fix: bug
    
    * fix: bug
    
    * fix: test
    
    * chore: set block height
    
    * fix: bug
    
    * fix: bug
    
    * fix: test

--- packages/plugins/@nocobase/plugin-workflow-manual/src/client/__e2e__/updateRecordForm.test.ts
+++ packages/plugins/@nocobase/plugin-workflow-manual/src/client/__e2e__/updateRecordForm.test.ts
@@ -150,7 +150,7 @@ test.describe('field data update', () => {
     await page.getByLabel(`designer-schema-settings-CardItem-UpdateFormDesigner-${manualNodeCollectionName}`).hover();
     await page.getByRole('menuitem', { name: 'Edit block title' }).click();
     const blockTitle = 'Create record' + dayjs().format('YYYYMMDDHHmmss.SSS').toString();
-    await page.getByLabel('Edit block title').getByRole('textbox').fill(blockTitle);
+    await page.getByLabel('block-title').fill(blockTitle);
     await page.getByRole('button', { name: 'OK', exact: true }).click();
     await page
       .locator(`button[aria-label^="schema-initializer-Grid-form:configureFields-${manualNodeCollectionName}"]`)
@@ -329,7 +329,7 @@ test.describe('field data update', () => {
     await page.getByLabel(`designer-schema-settings-CardItem-UpdateFormDesigner-${manualNodeCollectionName}`).hover();
     await page.getByRole('menuitem', { name: 'Edit block title' }).click();
     const blockTitle = 'Create record' + dayjs().format('YYYYMMDDHHmmss.SSS').toString();
-    await page.getByLabel('Edit block title').getByRole('textbox').fill(blockTitle);
+    await page.getByLabel('block-title').fill(blockTitle);
     await page.getByRole('button', { name: 'OK', exact: true }).click();
     await page
       .locator(`button[aria-label^="schema-initializer-Grid-form:configureFields-${manualNodeCollectionName}"]`)
@@ -508,7 +508,7 @@ test.describe('field data update', () => {
     await page.getByLabel(`designer-schema-settings-CardItem-UpdateFormDesigner-${manualNodeCollectionName}`).hover();
     await page.getByRole('menuitem', { name: 'Edit block title' }).click();
     const blockTitle = 'Create record' + dayjs().format('YYYYMMDDHHmmss.SSS').toString();
-    await page.getByLabel('Edit block title').getByRole('textbox').fill(blockTitle);
+    await page.getByLabel('block-title').fill(blockTitle);
     await page.getByRole('button', { name: 'OK', exact: true }).click();
     await page
       .locator(`button[aria-label^="schema-initializer-Grid-form:configureFields-${manualNodeCollectionName}"]`)
@@ -688,7 +688,7 @@ test.describe('field data update', () => {
     await page.getByLabel(`designer-schema-settings-CardItem-UpdateFormDesigner-${manualNodeCollectionName}`).hover();
     await page.getByRole('menuitem', { name: 'Edit block title' }).click();
     const blockTitle = 'Create record' + dayjs().format('YYYYMMDDHHmmss.SSS').toString();
-    await page.getByLabel('Edit block title').getByRole('textbox').fill(blockTitle);
+    await page.getByLabel('block-title').fill(blockTitle);
     await page.getByRole('button', { name: 'OK', exact: true }).click();
     await page
       .locator(`button[aria-label^="schema-initializer-Grid-form:configureFields-${manualNodeCollectionName}"]`)
