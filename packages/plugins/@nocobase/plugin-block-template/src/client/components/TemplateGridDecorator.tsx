40fdb37a28a2c558b9cb0c96d5ba7b14be3c1e5a
gchust <gchust@qq.com>
2025-01-20 11:35:38 +0800

    fix: add blocks  button shown in block settings

--- packages/plugins/@nocobase/plugin-block-template/src/client/components/TemplateGridDecorator.tsx
+++ packages/plugins/@nocobase/plugin-block-template/src/client/components/TemplateGridDecorator.tsx
@@ -1,9 +1,19 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
 import React, { useCallback, useContext, useEffect, useRef } from 'react';
 import { observer, useFieldSchema, useField, useFormEffects } from '@formily/react';
 import { onFieldReact } from '@formily/core';
 import { useUpdate } from 'ahooks';
 import { SchemaComponentOnChangeContext, useAPIClient } from '@nocobase/client';
 import { useBlockTemplateInfo } from './BlockTemplateInfoContext';
+import { Schema } from '@nocobase/utils';
 
 export const TemplateGridDecorator = observer((props: any) => {
   const fieldSchema = useFieldSchema();
@@ -21,6 +31,32 @@ export const TemplateGridDecorator = observer((props: any) => {
     },
   };
 
+  // used to hide add blocks button in toolbar, for the moment, only support one layer
+  const updateInitializer = useCallback(
+    (display: string) => {
+      let updatedLayer = 0;
+      const updater = (s: Schema) => {
+        if (s['x-toolbar'] || s['x-settings']) {
+          s['x-toolbar-props'] = {
+            ...s['x-toolbar-props'],
+            initializer: display === 'block' ? undefined : false,
+          };
+          updatedLayer++;
+        }
+        if (updatedLayer > 0) {
+          return;
+        }
+        if (s.properties) {
+          Object.keys(s.properties).forEach((key) => {
+            updater(s.properties[key]);
+          });
+        }
+      };
+      updater(fieldSchema);
+    },
+    [fieldSchema],
+  );
+
   const updateInitializerDisplay = useCallback(
     (configured: boolean) => {
       const initializerDisplay = !configured ? 'block' : 'none';
@@ -29,6 +65,8 @@ export const TemplateGridDecorator = observer((props: any) => {
         field.decoratorProps.style = {
           display: initializerDisplay,
         };
+        updateInitializer(initializerDisplay);
+        field.form.clearFormGraph(); // refresh form graph
         update();
       }
     },
