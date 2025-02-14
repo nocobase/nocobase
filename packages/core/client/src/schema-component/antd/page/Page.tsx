ca46f34206db3b09eb44d8d0743271575d497617
Zeke Zhang <958414905@qq.com>
2025-02-14 08:42:21 +0800

    fix(navigation): add search parameters to tab navigation

--- packages/core/client/src/schema-component/antd/page/Page.tsx
+++ packages/core/client/src/schema-component/antd/page/Page.tsx
@@ -18,7 +18,7 @@ import React, { FC, memo, useCallback, useContext, useEffect, useMemo, useRef, u
 import { ErrorBoundary } from 'react-error-boundary';
 import { useTranslation } from 'react-i18next';
 import { NavigateFunction, Outlet, useOutletContext } from 'react-router-dom';
-import { FormDialog } from '..';
+import { FormDialog, withSearchParams } from '..';
 import { antTableCell } from '../../../acl/style';
 import { useRequest } from '../../../api-client';
 import {
@@ -417,9 +417,9 @@ export function navigateToTab({
   }
 
   if (isTabPage(pathname)) {
-    navigate(`${pathname.replace(/\/tabs\/[^/]+$/, `/tabs/${activeKey}`)}`, { replace: true });
+    navigate(withSearchParams(`${pathname.replace(/\/tabs\/[^/]+$/, `/tabs/${activeKey}`)}`), { replace: true });
   } else {
-    navigate(`${pathname}/tabs/${activeKey}`, { replace: true });
+    navigate(withSearchParams(`${pathname}/tabs/${activeKey}`), { replace: true });
   }
 }
 
