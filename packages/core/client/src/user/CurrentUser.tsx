a39a4690ea2becdfe15a5225e5d34542ed1e08d7
katherinehhh <katherine_15995@163.com>
2025-02-17 11:56:35 +0800

    fix: bug

--- packages/core/client/src/user/CurrentUser.tsx
+++ packages/core/client/src/user/CurrentUser.tsx
@@ -8,6 +8,11 @@
  */
 
 import { createContext } from 'react';
+import { SelectWithTitle } from '../common';
+
+export const SettingsMenuProvider = (props) => {
+  return SelectWithTitle;
+};
 
 export const DropdownVisibleContext = createContext(null);
 DropdownVisibleContext.displayName = 'DropdownVisibleContext';
