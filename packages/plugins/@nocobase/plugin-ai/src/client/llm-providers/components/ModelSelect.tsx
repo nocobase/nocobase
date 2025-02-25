9be08e030ec03f04f29c4c08ca4a3e57b4e9d20e
xilesun <2013xile@gmail.com>
2025-02-25 11:41:05 +0800

    feat: ai

---
+++ packages/plugins/@nocobase/plugin-ai/src/client/llm-providers/components/ModelSelect.tsx
@@ -0,0 +1,54 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import React from 'react';
+import { useAPIClient, useRequest } from '@nocobase/client';
+import { useForm, useField } from '@formily/react';
+import { Select, Spin } from 'antd';
+import { Field } from '@formily/core';
+
+export const ModelSelect: React.FC = () => {
+  const field = useField<Field>();
+  const form = useForm();
+  const api = useAPIClient();
+  const { data: models, loading } = useRequest<
+    {
+      label: string;
+      value: string;
+    }[]
+  >(
+    () =>
+      api
+        .resource('ai')
+        .listModels({
+          llmService: form.values?.llmService,
+        })
+        .then(
+          (res) =>
+            res?.data?.data?.map(({ id }) => ({
+              label: id,
+              value: id,
+            })),
+        ),
+    {
+      ready: !!form.values?.llmService,
+      refreshDeps: [form.values?.llmService],
+    },
+  );
+
+  return (
+    <Select
+      showSearch={true}
+      options={models}
+      notFoundContent={loading ? <Spin size="small" /> : null}
+      value={field.value}
+      onChange={(val) => (field.value = val)}
+    />
+  );
+};
---
+++ packages/plugins/@nocobase/plugin-ai/src/server/manager/ai-manager.ts
@@ -0,0 +1,28 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import { LLMProvider } from '../llm-providers/provider';
+
+export type LLMProviderOptions = {
+  title: string;
+  provider: new (opts: { serviceOptions?: any; chatOptions?: any }) => LLMProvider;
+};
+
+export class AIManager {
+  llmProviders = new Map<string, LLMProviderOptions>();
+
+  registerLLMProvider(name: string, options: LLMProviderOptions) {
+    this.llmProviders.set(name, options);
+  }
+
+  listLLMProviders() {
+    const providers = this.llmProviders.entries();
+    return Array.from(providers).map(([name, { title }]) => ({ name, title }));
+  }
+}
