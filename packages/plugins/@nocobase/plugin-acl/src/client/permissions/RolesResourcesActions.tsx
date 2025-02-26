d9adbd19cb14ed3d49b81e043d615bb4808f63bf
gchust <gchust@qq.com>
2025-02-26 11:28:34 +0800

    chore: keep code format

--- packages/plugins/@nocobase/plugin-acl/src/client/permissions/RolesResourcesActions.tsx
+++ packages/plugins/@nocobase/plugin-acl/src/client/permissions/RolesResourcesActions.tsx
@@ -106,50 +106,48 @@ export const RolesResourcesActions = connect((props) => {
               className={styles}
               size={'small'}
               pagination={false}
-              columns={
-                [
-                  {
-                    dataIndex: 'displayName',
-                    title: t('Action display name'),
-                    render: (value) => compile(value),
-                  },
-                  {
-                    dataIndex: 'onNewRecord',
-                    title: t('Action type'),
-                    render: (onNewRecord) =>
-                      onNewRecord ? (
-                        <Tag color={'green'}>{t('Action on new records')}</Tag>
-                      ) : (
-                        <Tag color={'geekblue'}>{t('Action on existing records')}</Tag>
-                      ),
-                  },
-                  {
-                    dataIndex: 'enabled',
-                    title: t('Allow'),
-                    render: (enabled, action) => (
-                      <Checkbox
-                        checked={enabled}
-                        onChange={() => {
-                          toggleAction(action.name);
+              columns={[
+                {
+                  dataIndex: 'displayName',
+                  title: t('Action display name'),
+                  render: (value) => compile(value),
+                },
+                {
+                  dataIndex: 'onNewRecord',
+                  title: t('Action type'),
+                  render: (onNewRecord) =>
+                    onNewRecord ? (
+                      <Tag color={'green'}>{t('Action on new records')}</Tag>
+                    ) : (
+                      <Tag color={'geekblue'}>{t('Action on existing records')}</Tag>
+                    ),
+                },
+                {
+                  dataIndex: 'enabled',
+                  title: t('Allow'),
+                  render: (enabled, action) => (
+                    <Checkbox
+                      checked={enabled}
+                      onChange={() => {
+                        toggleAction(action.name);
+                      }}
+                    />
+                  ),
+                },
+                {
+                  dataIndex: 'scope',
+                  title: t('Data scope'),
+                  render: (value, action) =>
+                    !action.onNewRecord && (
+                      <ScopeSelect
+                        value={value}
+                        onChange={(scope) => {
+                          setScope(action.name, scope);
                         }}
                       />
                     ),
-                  },
-                  {
-                    dataIndex: 'scope',
-                    title: t('Data scope'),
-                    render: (value, action) =>
-                      !action.onNewRecord && (
-                        <ScopeSelect
-                          value={value}
-                          onChange={(scope) => {
-                            setScope(action.name, scope);
-                          }}
-                        />
-                      ),
-                  },
-                ] as TableProps['columns']
-              }
+                },
+              ] as TableProps['columns']}
               dataSource={availableActions?.map((item) => {
                 let enabled = false;
                 let scope = null;
@@ -172,62 +170,60 @@ export const RolesResourcesActions = connect((props) => {
               className={styles}
               pagination={false}
               dataSource={fieldPermissions}
-              columns={
-                [
-                  {
-                    dataIndex: ['uiSchema', 'title'],
-                    title: t('Field display name'),
-                    render: (value) => compile(value),
-                  },
-                  ...availableActionsWithFields.map((action) => {
-                    const checked = allChecked?.[action.name];
-                    return {
-                      dataIndex: action.name,
-                      title: (
-                        <>
-                          <Checkbox
-                            checked={checked}
-                            onChange={() => {
-                              const item = actionMap[action.name] || {
-                                name: action.name,
-                              };
-                              if (checked) {
-                                item.fields = [];
-                              } else {
-                                item.fields = collectionFields?.map?.((item) => item.name);
-                              }
-                              actionMap[action.name] = item;
-                              onChange(Object.values(actionMap));
-                            }}
-                          />{' '}
-                          {compile(action.displayName)}
-                        </>
-                      ),
-                      render: (checked, field) => (
+              columns={[
+                {
+                  dataIndex: ['uiSchema', 'title'],
+                  title: t('Field display name'),
+                  render: (value) => compile(value),
+                },
+                ...availableActionsWithFields.map((action) => {
+                  const checked = allChecked?.[action.name];
+                  return {
+                    dataIndex: action.name,
+                    title: (
+                      <>
                         <Checkbox
                           checked={checked}
-                          aria-label={`${action.name}_checkbox`}
                           onChange={() => {
                             const item = actionMap[action.name] || {
                               name: action.name,
                             };
-                            const fields: string[] = item.fields || [];
                             if (checked) {
-                              const index = fields.indexOf(field.name);
-                              fields.splice(index, 1);
+                              item.fields = [];
                             } else {
-                              fields.push(field.name);
+                              item.fields = collectionFields?.map?.((item) => item.name);
                             }
-                            item.fields = fields;
                             actionMap[action.name] = item;
                             onChange(Object.values(actionMap));
                           }}
-                        />
-                      ),
-                    };
-                  }),
-                ] as TableProps['columns']
-              }
+                        />{' '}
+                        {compile(action.displayName)}
+                      </>
+                    ),
+                    render: (checked, field) => (
+                      <Checkbox
+                        checked={checked}
+                        aria-label={`${action.name}_checkbox`}
+                        onChange={() => {
+                          const item = actionMap[action.name] || {
+                            name: action.name,
+                          };
+                          const fields: string[] = item.fields || [];
+                          if (checked) {
+                            const index = fields.indexOf(field.name);
+                            fields.splice(index, 1);
+                          } else {
+                            fields.push(field.name);
+                          }
+                          item.fields = fields;
+                          actionMap[action.name] = item;
+                          onChange(Object.values(actionMap));
+                        }}
+                      />
+                    ),
+                  };
+                }),
+              ] as TableProps['columns']}
             />
           </FormItem>
         </FormLayout>
