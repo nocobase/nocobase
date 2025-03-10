af0b3b26e4eadb31b5a25f4aaff340a558005d77
katherinehhh <katherine_15995@163.com>
2025-03-10 10:48:14 +0800

    fix: bug

--- packages/core/client/src/data-source/collection-field/CollectionField.tsx
+++ packages/core/client/src/data-source/collection-field/CollectionField.tsx
@@ -83,6 +83,8 @@ const CollectionFieldInternalField_deprecated: React.FC = (props: Props) => {
     setRequired(field, fieldSchema, uiSchema);
     // @ts-ignore
     field.dataSource = uiSchema.enum;
+    field.data = field.data || {};
+    field.data.dataSource = uiSchema.enum;
     const originalProps = compile(uiSchema['x-component-props']) || {};
     field.componentProps = merge(originalProps, field.componentProps || {}, dynamicProps || {});
   }, [uiSchemaOrigin]);
