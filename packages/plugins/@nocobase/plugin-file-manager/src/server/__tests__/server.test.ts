5144d47eb9c08ca8c38581ab8c650c250644eb24
Junyi <mytharcher@users.noreply.github.com>
2025-03-17 08:54:26 +0800

    refactor(plugin-file-manager): refactor file url generating (#6472)
    * refactor(plugin-file-manager): change file url generation to a better way
    
    * fix(plugin-file-manager): use url field first
    
    * fix(client): remove unused function call
    
    * fix(plugin-file-manager): fix test cases
    
    * fix: local getURL
    
    * fix: md5
    
    * fix: getFileURL
    
    * fix(plugin-file-manager): fix FileModel class
    
    * fix: use DeleteObjectCommand
    
    * fix(plugin-file-manager): fix package version and require
    
    * test(plugin-file-manager): fix test cases
    
    * fix: storageInstance
    
    * fix: test case error
    
    * fix: url-join
    
    * fix: autoGenId false
    
    * fix: url
    
    * fix: file.url
    
    * fix: file preview
    
    * fix: test error
    
    * fix: isURL
    
    * fix: only
    
    ---------
    
    Co-authored-by: chenos <chenlinxh@gmail.com>

--- packages/plugins/@nocobase/plugin-file-manager/src/server/__tests__/server.test.ts
+++ packages/plugins/@nocobase/plugin-file-manager/src/server/__tests__/server.test.ts
@@ -12,7 +12,7 @@ import path from 'path';
 import { getApp } from '.';
 import PluginFileManagerServer from '../server';
 
-import { STORAGE_TYPE_LOCAL, FILE_FIELD_NAME } from '../../constants';
+import { FILE_FIELD_NAME, STORAGE_TYPE_LOCAL } from '../../constants';
 
 const { LOCAL_STORAGE_BASE_URL, LOCAL_STORAGE_DEST = 'storage/uploads', APP_PORT = '13000' } = process.env;
 const DEFAULT_LOCAL_BASE_URL = LOCAL_STORAGE_BASE_URL || `/storage/uploads`;
@@ -131,23 +131,48 @@ describe('file manager > server', () => {
     describe('getFileURL', () => {
       it('local attachment without env', async () => {
         const { body } = await agent.resource('attachments').create({
-          [FILE_FIELD_NAME]: path.resolve(__dirname, './files/text.txt'),
+          [FILE_FIELD_NAME]: path.resolve(__dirname, './files/[]中文报告! 1%~50.4% (123) {$#}.txt'),
         });
 
         const url = await plugin.getFileURL(body.data);
         expect(url).toBe(`${process.env.APP_PUBLIC_PATH?.replace(/\/$/g, '') || ''}${body.data.url}`);
       });
 
-      it('local attachment with env', async () => {
+      it('local (default with base url) attachment with env', async () => {
         const originalPath = process.env.APP_PUBLIC_PATH;
-        process.env.APP_PUBLIC_PATH = 'http://localhost';
+        process.env.APP_PUBLIC_PATH = '/app';
 
         const { body } = await agent.resource('attachments').create({
           [FILE_FIELD_NAME]: path.resolve(__dirname, './files/text.txt'),
         });
 
         const url = await plugin.getFileURL(body.data);
-        expect(url).toBe(`http://localhost${body.data.url}`);
+        expect(url).toBe(`${process.env.APP_PUBLIC_PATH}/storage/uploads/${body.data.filename}`);
+
+        process.env.APP_PUBLIC_PATH = originalPath;
+      });
+
+      it('local (default without base url) attachment with env', async () => {
+        const storage = await StorageRepo.create({
+          values: {
+            name: 'local2',
+            type: STORAGE_TYPE_LOCAL,
+            rules: {
+              size: 1024,
+            },
+            default: true,
+          },
+        });
+
+        const originalPath = process.env.APP_PUBLIC_PATH;
+        process.env.APP_PUBLIC_PATH = '/nocobase/';
+
+        const { body } = await agent.resource('attachments').create({
+          [FILE_FIELD_NAME]: path.resolve(__dirname, './files/text.txt'),
+        });
+
+        const url = await plugin.getFileURL(body.data);
+        expect(url).toBe(`/nocobase/${body.data.filename}`);
 
         process.env.APP_PUBLIC_PATH = originalPath;
       });
