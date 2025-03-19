1d95f3f10909a9abff17d0bc2dbdebc2651e2f43
katherinehhh <katherine_15995@163.com>
2025-03-19 14:29:23 +0800

    fix: bug

--- packages/plugins/@nocobase/plugin-block-workbench/src/client/WorkbenchAction.tsx
+++ packages/plugins/@nocobase/plugin-block-workbench/src/client/WorkbenchAction.tsx
@@ -14,7 +14,6 @@ import { createStyles } from 'antd-style';
 import React, { useContext } from 'react';
 import { WorkbenchBlockContext } from './WorkbenchBlock';
 import { WorkbenchLayout } from './workbenchBlockSettings';
-import { Grid, List } from 'antd-mobile';
 
 const useStyles = createStyles(({ token, css }) => ({
   // 支持 css object 的写法
