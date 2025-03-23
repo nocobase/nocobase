03f356d199fd6a9a95d8c0161ddf7d118436ec7a
chenos <chenlinxh@gmail.com>
2025-03-23 09:36:41 +0800

    refactor: async tasks

--- packages/plugins/@nocobase/plugin-async-task-manager/src/server/resourcers/async-tasks.ts
+++ packages/plugins/@nocobase/plugin-async-task-manager/src/server/resourcers/async-tasks.ts
@@ -1,14 +1,53 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
 import fs from 'fs';
+import _ from 'lodash';
 import { basename } from 'path';
+import { AsyncTasksManager } from '../../../server';
 export default {
   name: 'asyncTasks',
   actions: {
+    async list(ctx, next) {
+      const userId = ctx.auth.user.id;
+      const asyncTaskManager = ctx.app.container.get('AsyncTaskManager');
+      const tasks = await asyncTaskManager.getTasksByTag('userId', userId);
+      ctx.body = _.orderBy(tasks, 'createdAt', 'desc');
+      await next();
+    },
     async get(ctx, next) {
       const { filterByTk } = ctx.action.params;
       const taskManager = ctx.app.container.get('AsyncTaskManager');
       const taskStatus = await taskManager.getTaskStatus(filterByTk);
 
       ctx.body = taskStatus;
+      await next();
+    },
+    async cancel(ctx, next) {
+      const { filterByTk } = ctx.action.params;
+      const userId = ctx.auth.user.id;
+      const asyncTaskManager = ctx.app.container.get('AsyncTaskManager');
+
+      const task = asyncTaskManager.getTask(filterByTk);
+
+      if (!task) {
+        ctx.body = 'ok';
+        await next();
+        return;
+      }
+
+      if (task.tags['userId'] != userId) {
+        ctx.throw(403);
+      }
+
+      const cancelled = await asyncTaskManager.cancelTask(filterByTk);
+      ctx.body = cancelled;
       await next();
     },
     async fetchFile(ctx, next) {
