d2025ee76fc12e321bdd9e3ced8ddd04240cbd03
Katherine <katherine_15995@163.com>
2025-04-02 14:54:09 +0800

    fix: field label display issue to prevent truncation by colon (#6599)
    * fix: form field label truncation due to colon
    
    * fix: test
    
    * fix: bug

--- packages/plugins/@nocobase/plugin-workflow-test/src/e2e/e2ePageObjectModel.ts
+++ packages/plugins/@nocobase/plugin-workflow-test/src/e2e/e2ePageObjectModel.ts
@@ -115,7 +115,9 @@ export class ApprovalTriggerNode {
       .getByLabel('block-item-DataSourceCollectionCascader-workflows-Collection')
       .locator('.ant-select-selection-search-input');
     this.dataBlocksInitiationRadio = page.getByRole('radio', { name: 'Initiate in data blocks only' });
-    this.dataBlocksAndGlobalApprovalBlocksInitiationRadio = page.getByRole('radio', { name: 'Initiate in both data blocks' });
+    this.dataBlocksAndGlobalApprovalBlocksInitiationRadio = page.getByRole('radio', {
+      name: 'Initiate in both data blocks',
+    });
     this.allowedToBeWithdrawnCheckbox = page.getByLabel('Allowed to be withdrawn');
     this.goToconfigureButton = page.getByRole('button', { name: 'Go to configure' });
     this.addBlockButton = page.getByLabel(`schema-initializer-Grid-ApprovalApplyAddBlockButton-${collectionName}`);
