b3792895c2798e58161493c88005689e2e9c908d
katherinehhh <katherine_15995@163.com>
2025-04-10 15:09:02 +0800

    feat: markdown support linkage rule

--- packages/core/client/src/schema-component/antd/card-item/CardItem.tsx
+++ packages/core/client/src/schema-component/antd/card-item/CardItem.tsx
@@ -7,14 +7,16 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
+import React, { FC } from 'react';
 import { useFieldSchema } from '@formily/react';
 import { CardProps } from 'antd';
-import React, { FC } from 'react';
 import { useSchemaTemplate } from '../../../schema-templates';
 import { BlockItem } from '../block-item';
 import { BlockItemCard } from '../block-item/BlockItemCard';
 import { BlockItemError } from '../block-item/BlockItemError';
 import useStyles from './style';
+import { useCollection } from '../../../data-source';
+import { BlockLinkageRuleProvider } from '../../../modules/blocks/BlockLinkageRuleProvider';
 
 export interface CardItemProps extends CardProps {
   name?: string;
@@ -23,19 +25,25 @@ export interface CardItemProps extends CardProps {
   height?: number;
 }
 
-export const CardItem: FC<CardItemProps> = (props) => {
-  const { children, name, heightMode, ...restProps } = props;
+export const CardItem: FC<CardItemProps> = ({ children, name, heightMode, ...restProps }) => {
   const template = useSchemaTemplate();
   const fieldSchema = useFieldSchema();
   const templateKey = fieldSchema?.['x-template-key'];
   const { wrapSSR, componentCls, hashId } = useStyles();
+  const collection = useCollection();
 
+  // 如果有 templateKey 则不渲染
   if (templateKey && !template) return null;
+
+  const cardContent = (
+    <BlockItem name={name} className={`${componentCls} ${hashId} noco-card-item`}>
+      <BlockItemCard {...restProps}>{children}</BlockItemCard>
+    </BlockItem>
+  );
+
   return wrapSSR(
     <BlockItemError>
-      <BlockItem name={name} className={`${componentCls} ${hashId} noco-card-item`}>
-        <BlockItemCard {...restProps}>{props.children}</BlockItemCard>
-      </BlockItem>
+      {collection ? cardContent : <BlockLinkageRuleProvider>{cardContent}</BlockLinkageRuleProvider>}
     </BlockItemError>,
   );
 };
