5534cafdc2ed926ee4faac19bd04eb321867993d
gchust <gchust@qq.com>
2025-04-15 14:22:40 +0800

    fix: build output is not correct if plugin using some old amd library (#6665)
    * fix: build output is not correct if plugin using some old amd library
    
    * fix: amd library build error

--- packages/core/build/src/buildPlugin.ts
+++ packages/core/build/src/buildPlugin.ts
@@ -347,6 +347,7 @@ export async function buildPluginClient(cwd: string, userConfig: UserConfig, sou
         umdNamedDefine: true,
       },
     },
+    amd: {},
     resolve: {
       tsConfig: path.join(process.cwd(), 'tsconfig.json'),
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.less', '.css'],
