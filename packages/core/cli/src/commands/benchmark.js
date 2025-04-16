61d97bb70924be1f05a0fd1ef8035bf1dbabd582
mytharcher <mytharcher@gmail.com>
2025-04-16 10:55:07 +0800

    fix(cli): change to fast-glob

--- packages/core/cli/src/commands/benchmark.js
+++ packages/core/cli/src/commands/benchmark.js
@@ -7,7 +7,7 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
-const glob = require('glob');
+const glob = require('fast-glob');
 const { Command } = require('commander');
 const { run } = require('../util');
 
