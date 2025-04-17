bb6dc1141aa0d7eb47fadd85260a5f1d46af76fc
mytharcher <mytharcher@gmail.com>
2025-04-17 21:02:35 +0800

    fix(plugin-workflow-manual): fix test case

--- packages/plugins/@nocobase/plugin-workflow-manual/src/server/__tests__/tasks.test.ts
+++ packages/plugins/@nocobase/plugin-workflow-manual/src/server/__tests__/tasks.test.ts
@@ -11,6 +11,7 @@ import Database from '@nocobase/database';
 import { EXECUTION_STATUS, JOB_STATUS } from '@nocobase/plugin-workflow';
 import { getApp, sleep } from '@nocobase/plugin-workflow-test';
 import { MockServer } from '@nocobase/test';
+import { TASK_STATUS } from '../../common/constants';
 
 // NOTE: skipped because time is not stable on github ci, but should work in local
 describe('workflow > instructions > manual > task center', () => {
@@ -64,9 +65,12 @@ describe('workflow > instructions > manual > task center', () => {
           assignees: [users[0].id],
           forms: {
             f1: {
-              type: 'create',
+              type: 'update',
               actions: [{ status: JOB_STATUS.RESOLVED, key: 'resolve' }],
               collection: 'posts',
+              filter: {
+                id: '{{$context.data.id}}',
+              },
             },
           },
         },
@@ -82,30 +86,65 @@ describe('workflow > instructions > manual > task center', () => {
       });
       expect(pendingJobs.length).toBe(1);
 
-      const res0 = await userAgents[0].resource('workflowManualTasks').listMine();
-      expect(res0.status).toBe(200);
-      expect(res0.body.data.length).toBe(1);
+      const res1 = await userAgents[0].resource('workflowManualTasks').listMine();
+      expect(res1.status).toBe(200);
+      expect(res1.body.data.length).toBe(1);
 
-      const res1 = await userAgents[0].resource('workflowManualTasks').submit({
+      const res2 = await userAgents[0].resource('workflowManualTasks').listMine({
+        filter: {
+          status: TASK_STATUS.PENDING,
+        },
+      });
+      expect(res2.status).toBe(200);
+      expect(res2.body.data.length).toBe(1);
+
+      const res3 = await userAgents[0].resource('workflowManualTasks').listMine({
+        filter: {
+          status: TASK_STATUS.RESOLVED,
+        },
+      });
+      expect(res3.status).toBe(200);
+      expect(res3.body.data.length).toBe(0);
+
+      const res4 = await userAgents[0].resource('workflowManualTasks').submit({
         filterByTk: pendingJobs[0].get('id'),
         values: {
           result: { f1: { title: 't2' }, _: 'resolve' },
         },
       });
-      expect(res1.status).toBe(202);
+      expect(res4.status).toBe(202);
 
       await sleep(500);
 
-      const [e1] = await workflow.getExecutions();
+      const [e1] = await workflow.getExecutions({ order: [['createdAt', 'ASC']] });
       expect(e1.status).toBe(EXECUTION_STATUS.RESOLVED);
       const [j1] = await e1.getJobs();
       expect(j1.status).toBe(JOB_STATUS.RESOLVED);
       expect(j1.result).toMatchObject({ f1: { title: 't2' } });
 
       const posts = await PostRepo.find({ order: [['createdAt', 'ASC']] });
-      expect(posts.length).toBe(2);
-      expect(posts[0]).toMatchObject({ title: 't1' });
-      expect(posts[1]).toMatchObject({ title: 't2' });
+      expect(posts.length).toBe(1);
+      expect(posts[0]).toMatchObject({ title: 't2' });
+
+      const res5 = await userAgents[0].resource('workflowManualTasks').listMine({
+        filter: {
+          status: TASK_STATUS.PENDING,
+        },
+      });
+      expect(res5.status).toBe(200);
+      expect(res5.body.data.length).toBe(0);
+
+      const res6 = await userAgents[0].resource('workflowManualTasks').listMine({
+        filter: {
+          status: TASK_STATUS.RESOLVED,
+        },
+      });
+      expect(res6.status).toBe(200);
+      expect(res6.body.data.length).toBe(1);
+
+      const res7 = await userAgents[0].resource('workflowManualTasks').listMine();
+      expect(res7.status).toBe(200);
+      expect(res7.body.data.length).toBe(1);
     });
   });
 });
