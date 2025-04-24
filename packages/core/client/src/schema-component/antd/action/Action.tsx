4fb2f50843fc390b9b2037a525e3e394e1ed728d
katherinehhh <katherine_15995@163.com>
2025-04-24 14:28:38 +0800

    refactor: show button title with tooltip on action icon hover

--- packages/core/client/src/schema-component/antd/action/Action.tsx
+++ packages/core/client/src/schema-component/antd/action/Action.tsx
@@ -10,7 +10,7 @@
 import { Field } from '@formily/core';
 import { observer, Schema, useField, useFieldSchema, useForm } from '@formily/react';
 import { isPortalInBody } from '@nocobase/utils/client';
-import { App, Button } from 'antd';
+import { App, Button, Tooltip } from 'antd';
 import classnames from 'classnames';
 import debounce from 'lodash/debounce';
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
@@ -618,6 +618,18 @@ const RenderButtonInner = observer(
     const actionTitle = typeof rawTitle === 'string' ? t(rawTitle, { ns: NAMESPACE_UI_SCHEMA }) : rawTitle;
     const { opacity, ...restButtonStyle } = buttonStyle;
     const linkStyle = isLink && opacity ? { opacity } : undefined;
+    const WrapperComponent = React.forwardRef(
+      ({ component: Component = tarComponent || Button, icon, onlyIcon, children, ...restProps }: any, ref) => {
+        return (
+          <Component ref={ref} {...restProps}>
+            <Tooltip title={restProps.title}>
+              <span style={{ marginRight: 3 }}>{icon && typeof icon === 'string' ? <Icon type={icon} /> : icon}</span>
+            </Tooltip>
+            {onlyIcon ? children[1] : children}
+          </Component>
+        );
+      },
+    );
     return (
       <SortableItem
         role="button"
@@ -630,7 +642,7 @@ const RenderButtonInner = observer(
         disabled={disabled}
         style={isLink ? restButtonStyle : buttonStyle}
         onClick={process.env.__E2E__ ? handleButtonClick : debouncedClick} // E2E 中的点击操作都是很快的，如果加上 debounce 会导致 E2E 测试失败
-        component={tarComponent || Button}
+        component={WrapperComponent}
         className={classnames(componentCls, hashId, className, 'nb-action')}
         type={type === 'danger' ? undefined : type}
         title={actionTitle}
