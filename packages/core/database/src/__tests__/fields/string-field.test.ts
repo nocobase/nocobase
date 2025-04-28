823d67ac13ae9ffbecf0e75bc79aface680eae53
Junyi <mytharcher@users.noreply.github.com>
2025-04-28 21:17:05 +0800

    fix(database): fix trim option for string and text field (#6797)

--- packages/core/database/src/__tests__/fields/string-field.test.ts
+++ packages/core/database/src/__tests__/fields/string-field.test.ts
@@ -119,4 +119,32 @@ describe('string field', () => {
       name: 'n1',
     });
   });
+
+  it('trim when value is null should be null', async () => {
+    const collection = db.collection({
+      name: 'tests',
+      fields: [{ type: 'string', name: 'name', trim: true }],
+    });
+    await db.sync();
+    const model = await collection.model.create({
+      name: null,
+    });
+    expect(model.toJSON()).toMatchObject({
+      name: null,
+    });
+  });
+
+  it('when value is number should be convert to string', async () => {
+    const collection = db.collection({
+      name: 'tests',
+      fields: [{ type: 'string', name: 'name', trim: true }],
+    });
+    await db.sync();
+    const model = await collection.model.create({
+      name: 123,
+    });
+    expect(model.toJSON()).toMatchObject({
+      name: '123',
+    });
+  });
 });
