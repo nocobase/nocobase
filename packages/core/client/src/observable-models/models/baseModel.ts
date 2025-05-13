f9b7a04c7367811ed13870494e5a93172255aadf
gchust <gchust@qq.com>
2025-05-13 09:12:36 +0800

    chore: udpate model api

--- packages/core/client/src/observable-models/models/baseModel.ts
+++ packages/core/client/src/observable-models/models/baseModel.ts
@@ -11,16 +11,24 @@ export class BaseModel {
   public uid: string;
   public props: IModelComponentProps;
   public hidden: boolean;
+  public filterParams: Record<string, Record<string, any>>;
+  public eventParams: Record<string, Record<string, any>>;
 
   constructor(uid: string, initialProps: IModelComponentProps = {}) {
     this.uid = uid;
     this.props = initialProps;
     this.hidden = false;
+    this.filterParams = {};
+    this.eventParams = {};
 
     define(this, {
       props: observable,
       hidden: observable,
+      filterParams: observable,
+      eventParams: observable,
       setProps: action,
+      setFilterParams: action,
+      setEventParams: action,
     });
   }
 
@@ -38,5 +46,75 @@ export class BaseModel {
 
   getProps(): ReadonlyModelProps {
     return this.props;
+  }
+
+  // 为特定flowKey和stepKey设置过滤器参数
+  setFilterParams(flowKey: string, stepKey: string, params: any): void;
+  // 为特定flowKey设置所有步骤的参数
+  setFilterParams(flowKey: string, stepsParams: Record<string, any>): void;
+  // 批量设置多个flow的参数
+  setFilterParams(params: Record<string, Record<string, any>>): void;
+  
+  setFilterParams(
+    flowKeyOrParams: string | Record<string, Record<string, any>>, 
+    stepKeyOrParams?: string | Record<string, any>, 
+    params?: any
+  ): void {
+    // 处理三个参数的情况: setFilterParams(flowKey, stepKey, params)
+    if (typeof flowKeyOrParams === 'string' && typeof stepKeyOrParams === 'string' && params !== undefined) {
+      if (!this.filterParams[flowKeyOrParams]) {
+        this.filterParams[flowKeyOrParams] = {};
+      }
+      this.filterParams[flowKeyOrParams][stepKeyOrParams] = params;
+    } 
+    // 处理两个参数的情况: setFilterParams(flowKey, stepsParams)
+    else if (typeof flowKeyOrParams === 'string' && typeof stepKeyOrParams === 'object') {
+      if (!this.filterParams[flowKeyOrParams]) {
+        this.filterParams[flowKeyOrParams] = {};
+      }
+      this.filterParams[flowKeyOrParams] = { 
+        ...this.filterParams[flowKeyOrParams], 
+        ...stepKeyOrParams 
+      };
+    } 
+    // 处理一个参数的情况: setFilterParams(params)
+    else if (typeof flowKeyOrParams === 'object') {
+      this.filterParams = { ...this.filterParams, ...flowKeyOrParams };
+    }
+  }
+
+  // 为特定flowKey和stepKey设置事件参数
+  setEventParams(flowKey: string, stepKey: string, params: any): void;
+  // 为特定flowKey设置所有步骤的参数
+  setEventParams(flowKey: string, stepsParams: Record<string, any>): void;
+  // 批量设置多个flow的参数
+  setEventParams(params: Record<string, Record<string, any>>): void;
+  
+  setEventParams(
+    flowKeyOrParams: string | Record<string, Record<string, any>>, 
+    stepKeyOrParams?: string | Record<string, any>, 
+    params?: any
+  ): void {
+    // 处理三个参数的情况: setEventParams(flowKey, stepKey, params)
+    if (typeof flowKeyOrParams === 'string' && typeof stepKeyOrParams === 'string' && params !== undefined) {
+      if (!this.eventParams[flowKeyOrParams]) {
+        this.eventParams[flowKeyOrParams] = {};
+      }
+      this.eventParams[flowKeyOrParams][stepKeyOrParams] = params;
+    } 
+    // 处理两个参数的情况: setEventParams(flowKey, stepsParams)
+    else if (typeof flowKeyOrParams === 'string' && typeof stepKeyOrParams === 'object') {
+      if (!this.eventParams[flowKeyOrParams]) {
+        this.eventParams[flowKeyOrParams] = {};
+      }
+      this.eventParams[flowKeyOrParams] = { 
+        ...this.eventParams[flowKeyOrParams], 
+        ...stepKeyOrParams 
+      };
+    } 
+    // 处理一个参数的情况: setEventParams(params)
+    else if (typeof flowKeyOrParams === 'object') {
+      this.eventParams = { ...this.eventParams, ...flowKeyOrParams };
+    }
   }
 } 
