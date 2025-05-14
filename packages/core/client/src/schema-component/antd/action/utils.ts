e05abf58b2d85fdef0a6440d0322f61db8154255
katherinehhh <katherine_15995@163.com>
2025-05-14 21:34:49 +0800

    fix: bug

--- packages/core/client/src/schema-component/antd/action/utils.ts
+++ packages/core/client/src/schema-component/antd/action/utils.ts
@@ -88,6 +88,7 @@ export const linkageAction = async (
     variables,
     localVariables,
     conditionType,
+    variableNameOfLeftCondition,
   }: {
     operator;
     field;
@@ -95,6 +96,7 @@ export const linkageAction = async (
     variables: VariablesContextType;
     localVariables: VariableOption[];
     conditionType: 'advanced' | 'basic';
+    variableNameOfLeftCondition?: string;
   },
   jsonLogic: any,
 ) => {
@@ -103,7 +105,12 @@ export const linkageAction = async (
 
   switch (operator) {
     case ActionType.Visible:
-      if (await conditionAnalyses({ ruleGroup: condition, variables, localVariables, conditionType }, jsonLogic)) {
+      if (
+        await conditionAnalyses(
+          { ruleGroup: condition, variables, localVariables, conditionType, variableNameOfLeftCondition },
+          jsonLogic,
+        )
+      ) {
         displayResult.push(operator);
         field.data = field.data || {};
         field.data.hidden = false;
@@ -115,7 +122,12 @@ export const linkageAction = async (
       field.display = last(displayResult);
       break;
     case ActionType.Hidden:
-      if (await conditionAnalyses({ ruleGroup: condition, variables, localVariables, conditionType }, jsonLogic)) {
+      if (
+        await conditionAnalyses(
+          { ruleGroup: condition, variables, localVariables, conditionType, variableNameOfLeftCondition },
+          jsonLogic,
+        )
+      ) {
         field.data = field.data || {};
         field.data.hidden = true;
       } else {
@@ -135,7 +147,12 @@ export const linkageAction = async (
       field.componentProps['disabled'] = last(disableResult);
       break;
     case ActionType.Active:
-      if (await conditionAnalyses({ ruleGroup: condition, variables, localVariables, conditionType }, jsonLogic)) {
+      if (
+        await conditionAnalyses(
+          { ruleGroup: condition, variables, localVariables, conditionType, variableNameOfLeftCondition },
+          jsonLogic,
+        )
+      ) {
         disableResult.push(false);
       } else {
         disableResult.push(!!field.componentProps?.['disabled']);
