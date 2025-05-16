a095c96e80aabd191601a578ba57ae1a305fa63a
Sheldon Guo <sheldon_66@163.com>
2025-05-16 08:23:58 +0800

    refactor(router): change router property to public and improve basename handling in useScanner

--- packages/core/client/src/application/RouterManager.tsx
+++ packages/core/client/src/application/RouterManager.tsx
@@ -49,7 +49,7 @@ export class RouterManager {
   protected routes: Record<string, RouteType> = {};
   protected options: RouterOptions;
   public app: Application;
-  private router;
+  public router;
   get basename() {
     return this.router.basename;
   }
