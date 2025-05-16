f2d0472b5b5c4f8e50993d522681e2fb8e673c35
xilesun <2013xile@gmail.com>
2025-05-16 16:29:59 +0800

    fix: build

--- packages/plugins/@nocobase/plugin-ai/src/server/llm-providers/tongyi.ts
+++ packages/plugins/@nocobase/plugin-ai/src/server/llm-providers/tongyi.ts
@@ -7,41 +7,41 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
-import { LLMProvider } from './provider';
-import { ChatAlibabaTongyi } from '@langchain/community/chat_models/alibaba_tongyi';
-
-export class TongyiProvider extends LLMProvider {
-  declare chatModel: ChatAlibabaTongyi;
-
-  get baseURL() {
-    return 'https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation';
-  }
-
-  createModel() {
-    const { apiKey, baseURL } = this.serviceOptions || {};
-    const { model } = this.modelOptions || {};
-
-    const tongyi = new ChatAlibabaTongyi({
-      alibabaApiKey: apiKey,
-      ...this.modelOptions,
-      model,
-    });
-    if (baseURL) {
-      tongyi.apiUrl = baseURL;
-    }
-    return tongyi;
-  }
-
-  async listModels(): Promise<{
-    models?: { id: string }[];
-    code?: number;
-    errMsg?: string;
-  }> {
-    return { code: 500, errMsg: 'Not supported' };
-  }
-}
-
-export const tongyiProviderOptions = {
-  title: '{{t("Tongyi", {ns: "ai"})}}',
-  provider: TongyiProvider,
-};
+// import { LLMProvider } from './provider';
+// import { ChatAlibabaTongyi } from '@langchain/community/chat_models/alibaba_tongyi';
+//
+// export class TongyiProvider extends LLMProvider {
+//   declare chatModel: ChatAlibabaTongyi;
+//
+//   get baseURL() {
+//     return 'https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation';
+//   }
+//
+//   createModel() {
+//     const { apiKey, baseURL } = this.serviceOptions || {};
+//     const { model } = this.modelOptions || {};
+//
+//     const tongyi = new ChatAlibabaTongyi({
+//       alibabaApiKey: apiKey,
+//       ...this.modelOptions,
+//       model,
+//     });
+//     if (baseURL) {
+//       tongyi.apiUrl = baseURL;
+//     }
+//     return tongyi;
+//   }
+//
+//   async listModels(): Promise<{
+//     models?: { id: string }[];
+//     code?: number;
+//     errMsg?: string;
+//   }> {
+//     return { code: 500, errMsg: 'Not supported' };
+//   }
+// }
+//
+// export const tongyiProviderOptions = {
+//   title: '{{t("Tongyi", {ns: "ai"})}}',
+//   provider: TongyiProvider,
+// };
