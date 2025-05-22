82f6e070f78161e94bc994e07dfabb19732212c0
kerwin612 <kerwin612@qq.com>
2025-05-22 15:17:10 +0800

    feat(plugin-action-block-fullscreen): initial commit

---
+++ packages/plugins/@nocobase/plugin-action-block-fullscreen/src/server/index.ts
@@ -0,0 +1,19 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import { Plugin } from '@nocobase/server';
+
+// 简化版服务器插件 - 此插件主要功能在客户端
+export class PluginActionBlockFullscreenServer extends Plugin {
+  async load() {
+    // 无服务器端功能
+  }
+}
+
+export default PluginActionBlockFullscreenServer;
