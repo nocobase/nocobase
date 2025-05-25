da951d8836d66e9bc4013819c6d8f9700bce3bce
sheldon66 <sheldon_66@163.com>
2025-05-25 12:09:06 +0800

    feat(blocks): add list data block exports

--- packages/core/client/src/index.ts
+++ packages/core/client/src/index.ts
@@ -68,6 +68,7 @@ export { getVariableComponentWithScope, useURLAndHTMLSchema } from './modules/ac
 export * from './modules/blocks/BlockSchemaToolbar';
 export * from './modules/blocks/data-blocks/form';
 export * from './modules/blocks/data-blocks/table';
+export * from './modules/blocks/data-blocks/list';
 export * from './modules/blocks/data-blocks/table-selector';
 export * from './modules/blocks/index';
 export * from './modules/blocks/useParentRecordCommon';
