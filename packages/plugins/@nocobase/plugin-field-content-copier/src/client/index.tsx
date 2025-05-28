84298b1ee3631bf72b9ec1346563ce9d5462e042
Zeke Zhang <958414905@qq.com>
2025-05-28 15:16:59 +0800

    feat(plugin-field-content-copier): enhance InputCopyButton and integrate with addonAfter

--- packages/plugins/@nocobase/plugin-field-content-copier/src/client/index.tsx
+++ packages/plugins/@nocobase/plugin-field-content-copier/src/client/index.tsx
@@ -7,141 +7,32 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
+import { observer, useField, useFieldSchema } from '@formily/react';
+import { Plugin, useColumnSchema, useDesignable, useToken } from '@nocobase/client';
+import { Typography } from 'antd';
 import _ from 'lodash';
-import React, { useMemo, useState } from 'react';
-import { Button, message } from 'antd';
-import { connect, mapProps, mapReadPretty, useField, useFieldSchema } from '@formily/react';
-import { Input, Plugin, useColumnSchema, useDesignable } from '@nocobase/client';
+import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
-import { CopyOutlined } from '@ant-design/icons';
-import { LoadingOutlined } from '@ant-design/icons';
 
 // Define namespace for i18n
 const NAMESPACE = 'field-content-copier';
 
-// Create a copy button component that can be used in both input and read-pretty modes
-const CopyButton = ({ value }) => {
-  const { t } = useTranslation(NAMESPACE);
+const InputCopyButton: FC = observer(
+  () => {
+    const field = useField<{ value: string }>();
+    const { token } = useToken();
 
-  const handleCopy = () => {
-    if (value) {
-      navigator.clipboard
-        .writeText(String(value))
-        .then(() => {
-          message.success(t('Copied to clipboard'));
-        })
-        .catch((err) => {
-          console.error('Could not copy text: ', err);
-          message.error(t('Failed to copy'));
-        });
-    } else {
-      message.info(t('No content to copy'));
-    }
-  };
-
-  return (
-    <Button
-      size="small"
-      onClick={handleCopy}
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-      icon={<CopyOutlined />}
-      title={t('Copy to clipboard')}
-    />
-  );
-};
-
-// Create the read-pretty component to display content with a copy button
-const InputWithCopierReadPretty = (props) => {
-  const { value, enableCopier, ...otherProps } = props;
-  const prefixCls = 'nb-description-input';
-  const [enter, setEnter] = useState(false);
-  const compile = (value) => value; // This is a simplification, NocoBase has its own compile function
-
-  // Format the content similar to NocoBase's ReadPretty component
-  const content = useMemo(() => (value && typeof value === 'object' ? JSON.stringify(value) : compile(value)), [value]);
-
-  // Create the copy button if the feature is enabled
-  const copyButton = enableCopier && enter ? <CopyButton value={value} /> : null;
-
-  return (
-    <div
-      className={`${prefixCls} ${props.className || ''}`}
-      style={{ overflowWrap: 'break-word', whiteSpace: 'normal', ...props.style }}
-      onMouseEnter={() => {
-        setEnter(true);
-      }}
-      onMouseLeave={() => {
-        setEnter(false);
-      }}
-    >
-      {props.addonBefore}
-      {props.prefix}
-      <div style={{ position: 'relative' }}>
-        {/* Use the original content rendering but with our copy button */}
-        <span>{content}</span>
-        {copyButton && <span style={{ right: 0, position: 'absolute' }}>{copyButton}</span>}
-      </div>
-      {props.suffix}
-      {props.addonAfter}
-    </div>
-  );
-};
-
-// Connect the input component with the copy functionality
-const InputWithCopier = connect(
-  (props) => {
-    const { value, enableCopier, ...restProps } = props;
-
-    // Preserve all existing props
-    const inputProps = {
-      ...restProps,
-      value,
-    };
-
-    // Add the copy button without overriding existing addonAfter
-    if (enableCopier) {
-      const originalAddonAfter = inputProps.addonAfter;
-      inputProps.addonAfter = (
-        <>
-          {originalAddonAfter}
-          <CopyButton value={value} />
-        </>
-      );
-    }
-
-    return <Input {...inputProps} />;
+    return <Typography.Text copyable={{ text: field.value }} style={{ marginLeft: token.marginXXS }} />;
   },
-  mapProps((props, field) => {
-    return {
-      ...props,
-      suffix: <span>{field?.['loading'] || field?.['validating'] ? <LoadingOutlined /> : props.suffix}</span>,
-    };
-  }),
-  mapReadPretty(InputWithCopierReadPretty),
+  {
+    displayName: 'InputCopyButton',
+  },
 );
 
 class PluginFieldContentCopier extends Plugin {
   async load() {
-    // Get the original Input component to extend it
-    const OriginalInput = this.app.components.Input;
-
-    // Create a HOC that wraps the original Input component with our copy functionality
-    const EnhancedInput = (props) => {
-      // If copier is enabled, enhance the component with our functionality
-      if (props.enableCopier) {
-        return <InputWithCopier {...props} />;
-      }
-      // Otherwise, use the original component
-      return <OriginalInput {...props} />;
-    };
-
-    // Register our enhanced Input component
-    this.app.addComponents({
-      Input: Object.assign(EnhancedInput, OriginalInput),
+    this.app.addScopes({
+      InputCopyButton: <InputCopyButton />,
     });
 
     // Add the schema settings to enable/disable the copy functionality
@@ -158,18 +49,21 @@ class PluginFieldContentCopier extends Plugin {
 
         return {
           title: t('Enable content copier'),
-          checked: !!schema['x-component-props']?.enableCopier,
+          checked: !!schema['x-component-props']?.addonAfter,
           onChange: async (checked) => {
-            formField.componentProps.enableCopier = checked;
-
-            _.set(schema, 'x-component-props.enableCopier', checked);
+            if (checked) {
+              formField.componentProps.addonAfter = '{{InputCopyButton}}';
+              _.set(schema, 'x-component-props.addonAfter', '{{InputCopyButton}}');
+            } else {
+              formField.componentProps.addonAfter = undefined;
+              _.unset(schema, 'x-component-props.addonAfter');
+            }
 
             await dn.emit('patch', {
               schema: {
                 'x-uid': schema['x-uid'],
                 'x-component-props': {
                   ...schema['x-component-props'],
-                  enableCopier: checked,
                 },
               },
             });
@@ -177,8 +71,6 @@ class PluginFieldContentCopier extends Plugin {
         };
       },
     });
-
-    // Plugin loaded successfully
   }
 }
 
