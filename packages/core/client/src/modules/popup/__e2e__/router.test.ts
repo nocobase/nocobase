e5f497dbda5c60c156235b0a488298794ebff0be
Zeke Zhang <958414905@qq.com>
2025-06-03 22:00:50 +0800

    fix: resolve UI refresh issue (#6977)
    * test(e2e): update visibility checks for blocks in schema initializer tests
    
    * fix: remove deprecated useCollectionManager from DetailsBlockProvider
    
    * feat: add refreshComponent to Grid for improved schema updates
    
    * fix: update text in popup router tests for consistency
    
    * test(e2e): update text in e2e tests for consistency and visibility
    
    * test(e2e): update toolbar visibility handling for E2E testing
    
    * test(e2e): skip modal content persistence test after configuration

--- packages/core/client/src/modules/popup/__e2e__/router.test.ts
+++ packages/core/client/src/modules/popup/__e2e__/router.test.ts
@@ -36,7 +36,7 @@ test.describe('popup router', () => {
     await page.locator('.ant-drawer-mask').click();
 
     // expect to be back to the first page
-    await page.getByText('Users单层子页面Configure').hover();
+    await page.getByText('单层子页面').hover();
     await expect(
       page.getByRole('button', { name: 'designer-schema-settings-CardItem-blockSettings:table-users' }),
     ).toBeVisible();
@@ -60,7 +60,7 @@ test.describe('popup router', () => {
     await page.locator('.ant-drawer-mask').click();
 
     // expect to be back to the first page
-    await page.getByText('Users单层子页面Configure').hover();
+    await page.getByText('单层子页面').hover();
     await expect(
       page.getByRole('button', { name: 'designer-schema-settings-CardItem-blockSettings:table-users' }),
     ).toBeVisible();
