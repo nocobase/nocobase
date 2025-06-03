079dae946d428b6db00274a9c5569685cedbba5d
xilesun <2013xile@gmail.com>
2025-06-03 23:29:09 +0800

    chore: update

--- packages/core/client/src/data-source/data-block/DataBlockRequestProvider.tsx
+++ packages/core/client/src/data-source/data-block/DataBlockRequestProvider.tsx
@@ -90,6 +90,7 @@ function useRecordRequest<T>(options: Omit<AllDataBlockProps, 'type'>) {
     manual: dataLoadingMode === 'manual',
     ready: !!action,
     refreshDeps: [action, JSONParams, JSONRecord, resource, association, parentRecord, sourceId, templateFinished],
+    defaultParams: [params],
   });
 
   return request;
