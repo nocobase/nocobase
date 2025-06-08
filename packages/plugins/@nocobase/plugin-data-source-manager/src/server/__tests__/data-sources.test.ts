ac230e3926c887ead1174b225188a3d694995e8c
aaaaaajie <m18210870360@163.com>
2025-06-08 14:26:08 +0800

    fix: remove database dependency for possibleTypes, enforce API-driven configuration

--- packages/plugins/@nocobase/plugin-data-source-manager/src/server/__tests__/data-sources.test.ts
+++ packages/plugins/@nocobase/plugin-data-source-manager/src/server/__tests__/data-sources.test.ts
@@ -693,5 +693,46 @@ describe('data source', async () => {
       const collection2 = dataSource2.collectionManager.getCollection('comments');
       expect(collection2.getField('post')).toBeFalsy();
     });
+
+    it(`should not return possibleTypes field when creating field`, async () => {
+      const createResp = await app
+        .agent()
+        .resource('dataSourcesCollections.fields', 'mockInstance1.comments')
+        .create({
+          values: {
+            type: 'string',
+            name: 'title',
+            possibleTypes: ['123', '456'],
+          },
+        });
+
+      expect(createResp.status).toBe(200);
+      const data = createResp.body.data;
+      expect(data.possibleTypes).not.exist;
+
+      const fieldModel = await app.db.getRepository('dataSourcesFields').findOne({
+        filter: {
+          dataSourceKey: 'mockInstance1',
+        },
+      });
+      expect(fieldModel.get('options').possibleTypes).not.exist;
+    });
+
+    it(`should not return possibleTypes field when update field`, async () => {
+      const fieldUpdateResp = await app
+        .agent()
+        .resource('dataSourcesCollections.fields', 'mockInstance1.posts')
+        .update({
+          filterByTk: 'title',
+          values: {
+            title: '标题 Field',
+            possibleTypes: ['123', '456'],
+          },
+        });
+
+      expect(fieldUpdateResp.status).toBe(200);
+      const data = fieldUpdateResp.body.data;
+      expect(data.possibleTypes).not.exist;
+    });
   });
 });
