89e9b5f82fdb549981713d61811592592c7b86df
Zeke Zhang <958414905@qq.com>
2025-06-09 16:30:47 +0800

    fix: add comment to clarify popup container issue for mobile adaptation

--- packages/plugins/@nocobase/plugin-mobile/src/client/adaptor-of-desktop/FilterAction.tsx
+++ packages/plugins/@nocobase/plugin-mobile/src/client/adaptor-of-desktop/FilterAction.tsx
@@ -112,6 +112,7 @@ export const useToAdaptFilterActionToMobile = () => {
 const POPUP_ANIMATION_DELAY = 300;
 
 /**
+ * 为了解决在桌面端配置页面弹窗超出移动端页面范围的问题。
  * 之所以不直接在 mobile-container 中设置 transform，是因为会影响到子页面区块的拖拽功能。
  * @param visible 控制弹窗是否可见
  * @param animationDelay 动画延迟时间，默认300ms
