2fa26828d2696ab883615cae02b7035f5ef6cecd
Zeke Zhang <958414905@qq.com>
2025-06-12 23:50:30 +0800

    feat: add quick copy plugin (#6954)
    * Revert "Revert "feat: new plugin `plugin-input-copy-button` (#6894)""
    
    This reverts commit 49511e2dd6656c72b79f9bd05f61ed7673b1d075.
    
    * Revert "Revert "fix: improve visibility toggle for input copy button on mouse events""
    
    This reverts commit 9137ad651506718626f22ee48bd8c839baf6bf91.
    
    * fix: update version to 1.6.34 for input copy button plugin
    
    * fix: adjust margin style for InputCopyButton based on field readPretty state
    
    * fix: remove unused keywords from package.json for input copy button plugin
    
    * fix: add input copy button plugin to dependencies in package.json
    
    * fix: update version of input copy button plugin to 1.6.36
    
    * chore: update version of @nocobase/plugin-input-copy-button to 1.7.10
    
    * refactor: rename to plugin-text-copy
    
    * fix: update translation key from 'Show copy button' to 'Display copy button'
    
    * fix: add optional chaining to event listener for safety
    
    * test(e2e): update visibility check for 'Data sources' menu item in plugin settings permissions test

---
+++ packages/plugins/@nocobase/plugin-text-copy/src/server/index.ts
@@ -0,0 +1,18 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import { Plugin } from '@nocobase/server';
+
+export class PluginTextCopyServer extends Plugin {
+  async load() {
+    // Server-side initialization if needed
+  }
+}
+
+export default PluginTextCopyServer;
