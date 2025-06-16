7332455a78d06489d86ec9771cfbbf1c6e0e9f7f
Zeke Zhang <958414905@qq.com>
2025-06-16 18:43:38 +0800

    feat: implement openDrawer function to handle drawer view rendering

--- packages/core/client/src/flow/views/openView.tsx
+++ packages/core/client/src/flow/views/openView.tsx
@@ -7,17 +7,36 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
-const openDrawer = () => {};
+import { FlowContext } from '@nocobase/flow-engine';
+import { Drawer } from 'antd';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const openDrawer = (params: { ctx: FlowContext }) => {
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+
+  const root = createRoot(container);
+
+  const unmount = () => {
+    setTimeout(() => {
+      root.unmount();
+      document.body.removeChild(container);
+    }, 300);
+  };
+
+  root.render(<Drawer open={true} onClose={unmount}></Drawer>);
+};
 
 const openModal = () => {};
 
 const openSubPage = () => {};
 
-export const openView = (params: { type: 'drawer' | 'modal' | 'subPage' }) => {
-  const { type } = params;
+export const openView = (params: { type: 'drawer' | 'modal' | 'subPage'; ctx: FlowContext }) => {
+  const { type, ...others } = params;
   switch (type) {
     case 'drawer':
-      return openDrawer();
+      return openDrawer(others);
     case 'modal':
       return openModal();
     case 'subPage':
