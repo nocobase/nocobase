438c1ea15b2e872682393b7a8724627c22ce7d04
katherinehhh <katherine_15995@163.com>
2025-06-18 14:03:32 +0800

    refactor: dateOnly

--- packages/core/client/src/flow/models/form/fields/DateTimeFieldModel/DateTimeWithTzFieldModel.tsx
+++ packages/core/client/src/flow/models/form/fields/DateTimeFieldModel/DateTimeWithTzFieldModel.tsx
@@ -6,8 +6,19 @@
  * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
+import dayjs from 'dayjs';
 import { DateTimeFieldModel } from './DateTimeFieldModel';
 
 export class DateTimeWithTzFieldModel extends DateTimeFieldModel {
   static supportedFieldInterfaces = ['createdAt', 'datetime', 'updatedAt'];
+
+  setComponentProps(componentProps) {
+    super.setComponentProps({
+      ...componentProps,
+      onChange: (value) => {
+        const iso = value ? dayjs(value).toISOString() : undefined;
+        this.field.setValue(iso);
+      },
+    });
+  }
 }
