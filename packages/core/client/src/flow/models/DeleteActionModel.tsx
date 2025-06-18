fffaa759f23a2bccfb6f1abeeeaa1b7a32e9cb1d
Zeke Zhang <958414905@qq.com>
2025-06-18 16:19:51 +0800

    feat: add secondary confirmation step to DeleteActionModel flow

--- packages/core/client/src/flow/models/DeleteActionModel.tsx
+++ packages/core/client/src/flow/models/DeleteActionModel.tsx
@@ -18,11 +18,47 @@ export class DeleteActionModel extends ActionModel {
 }
 
 DeleteActionModel.registerFlow({
-  key: 'deleteRecord',
+  key: 'handleClick',
+  title: '点击事件',
   on: {
     eventName: 'click',
   },
   steps: {
+    secondaryConfirmation: {
+      title: 'Secondary confirmation',
+      uiSchema: {
+        enable: {
+          type: 'boolean',
+          title: 'Enable secondary confirmation',
+          default: true,
+          'x-decorator': 'FormItem',
+          'x-component': 'Checkbox',
+        },
+        title: {
+          type: 'string',
+          title: 'Title',
+          default: 'Delete record',
+          'x-decorator': 'FormItem',
+          'x-component': 'Input.TextArea',
+        },
+        content: {
+          type: 'string',
+          title: 'Content',
+          default: 'Are you sure you want to delete it?',
+          'x-decorator': 'FormItem',
+          'x-component': 'Input.TextArea',
+        },
+      },
+      handler(ctx, params) {
+        if (params.enable) {
+          ctx.globals.modal.confirm({
+            title: params.title,
+            content: params.content,
+            onOk: async () => {},
+          });
+        }
+      },
+    },
     delete: {
       async handler(ctx, params) {
         if (!ctx.extra.currentResource || !ctx.extra.currentRecord) {
