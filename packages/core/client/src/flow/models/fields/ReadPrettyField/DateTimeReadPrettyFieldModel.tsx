85c61bf9e58f25e716dc37408e864e68ed27aed2
katherinehhh <katherine_15995@163.com>
2025-06-26 15:21:57 +0800

    refactor: code improve

--- packages/core/client/src/flow/models/fields/ReadPrettyField/DateTimeReadPrettyFieldModel.tsx
+++ packages/core/client/src/flow/models/fields/ReadPrettyField/DateTimeReadPrettyFieldModel.tsx
@@ -1,3 +1,12 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
 import React from 'react';
 import dayjs from 'dayjs';
 import { reactive } from '@nocobase/flow-engine';
@@ -58,6 +67,12 @@ DateTimeReadPrettyFieldModel.registerFlow({
           timeFormat: timeFormat,
           showTime,
         };
+      },
+      handler(ctx: any, params) {
+        ctx.model.flowEngine.flowSettings.registerScopes({
+          collectionField: ctx.model.collectionField,
+        });
+        ctx.model.setProps?.({ ...params });
       },
     },
   },
