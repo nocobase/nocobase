bad0bded903628f6c0b498ee711998c613fd5c17
Zeke Zhang <958414905@qq.com>
2025-07-06 13:31:42 +0800

    feat: add isInTableCell prop to FlagProviderProps and use it in Table and Upload components

--- packages/core/client/src/schema-component/antd/table-v2/Table.tsx
+++ packages/core/client/src/schema-component/antd/table-v2/Table.tsx
@@ -36,6 +36,7 @@ import { useTranslation } from 'react-i18next';
 import { DndContext, isBulkEditAction, useDesignable, usePopupSettings, useTableSize } from '../..';
 import {
   BlockRequestLoadingContext,
+  FlagProvider,
   RecordIndexProvider,
   RecordProvider,
   useAssociationNames,
@@ -59,6 +60,8 @@ import {
 } from '../../../formily/NocoBaseRecursionField';
 import { withDynamicSchemaProps } from '../../../hoc/withDynamicSchemaProps';
 import { withSkeletonComponent } from '../../../hoc/withSkeletonComponent';
+import { withTooltipComponent } from '../../../hoc/withTooltipComponent';
+import { NAMESPACE_UI_SCHEMA } from '../../../i18n/constant';
 import { LinkageRuleDataKeyMap } from '../../../schema-settings/LinkageRules/type';
 import { GetStyleRules } from '../../../schema-settings/LinkageRules/useActionValues';
 import { HighPerformanceSpin } from '../../common/high-performance-spin/HighPerformanceSpin';
@@ -66,8 +69,6 @@ import { useToken } from '../__builtins__';
 import { useAssociationFieldContext } from '../association-field/hooks';
 import { TableSkeleton } from './TableSkeleton';
 import { extractIndex, isCollectionFieldComponent, isColumnComponent } from './utils';
-import { withTooltipComponent } from '../../../hoc/withTooltipComponent';
-import { NAMESPACE_UI_SCHEMA } from '../../../i18n/constant';
 
 type BodyRowComponentProps = {
   rowIndex?: number;
@@ -633,7 +634,7 @@ const InternalBodyCellComponent = React.memo<BodyCellComponentProps>((props) => 
   const mergedStyle = useMemo(() => ({ ...props.style, ...dynamicStyle }), [props.style, dynamicStyle]);
 
   return (
-    <>
+    <FlagProvider isInTableCell>
       {/* To improve rendering performance, do not render GetStyleRules component when no style rules are set */}
       {!_.isEmpty(styleRules) && (
         <GetStyleRules record={record} schema={schema} onStyleChange={isReadPrettyMode ? setDynamicStyle : _.noop} />
@@ -641,7 +642,7 @@ const InternalBodyCellComponent = React.memo<BodyCellComponentProps>((props) => 
       <td {...others} className={classNames(props.className, cellClass)} style={mergedStyle}>
         {props.children}
       </td>
-    </>
+    </FlagProvider>
   );
 });
 
@@ -654,9 +655,11 @@ const BodyCellComponent = React.memo<BodyCellComponentProps>((props) => {
 
   if (props.columnHidden) {
     return (
-      <td style={designable ? columnOpacityStyle : columnHiddenStyle}>
-        {designable ? props.children : <span style={displayNone}>{props.children}</span>}
-      </td>
+      <FlagProvider isInTableCell>
+        <td style={designable ? columnOpacityStyle : columnHiddenStyle}>
+          {designable ? props.children : <span style={displayNone}>{props.children}</span>}
+        </td>
+      </FlagProvider>
     );
   }
 
