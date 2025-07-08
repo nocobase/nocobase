4df3c4e8af9cc382849d8b598f9b07fe46d24bc1
katherinehhh <katherine_15995@163.com>
2025-07-08 16:00:42 +0800

    refactor: code improve

--- packages/core/client/src/flow/models/fields/EditableField/DateTimeFieldModel/DateOnlyEditableFieldModel.tsx
+++ packages/core/client/src/flow/models/fields/EditableField/DateTimeFieldModel/DateOnlyEditableFieldModel.tsx
@@ -6,7 +6,49 @@
  * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
+import { DatePicker } from 'antd';
+import React from 'react';
+import dayjs from 'dayjs';
+import { connect, mapProps, mapReadPretty } from '@formily/react';
+import { PreviewText } from '@formily/antd-v5';
 import { DateTimeFieldModel } from './DateTimeFieldModel';
+
+const DatePickerCom = connect(
+  DatePicker,
+  mapProps((props: any, field: any) => {
+    const { value, format = 'YYYY-MM-DD', picker = 'date', showTime, ...rest } = props;
+    const parsedValue = value && dayjs(value).isValid() ? dayjs(value) : null;
+
+    return {
+      ...rest,
+      value: parsedValue,
+      format,
+      picker,
+      showTime,
+      onChange: (val: any) => {
+        const outputFormat = 'YYYY-MM-DD';
+        if (!val) {
+          field.setValue(undefined);
+          return;
+        }
+        if (picker === 'date') {
+          field.setValue(val.format(outputFormat));
+        } else {
+          field.setValue(val.startOf(picker).format(outputFormat));
+        }
+      },
+    };
+  }),
+  mapReadPretty(({ value, format = 'YYYY-MM-DD', ...rest }) => {
+    if (!value) {
+      return;
+    }
+    const display = value && dayjs(value).format(format);
+    return <PreviewText.DatePicker {...rest} value={display} />;
+  }),
+);
+
+export default DatePickerCom;
 
 export class DateOnlyEditableFieldModel extends DateTimeFieldModel {
   static supportedFieldInterfaces = ['date'];
@@ -17,5 +59,8 @@ export class DateOnlyEditableFieldModel extends DateTimeFieldModel {
       showTime: false,
       utc: false,
     });
+  }
+  get component() {
+    return [DatePickerCom, {}];
   }
 }
