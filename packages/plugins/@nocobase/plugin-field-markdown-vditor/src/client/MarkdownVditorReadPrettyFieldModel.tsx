0a96bf85b6f168879beacf058817bd3322ddd424
katherinehhh <katherine_15995@163.com>
2025-07-09 17:18:24 +0800

    feat: markdownVaditor fieldModel

---
+++ packages/plugins/@nocobase/plugin-field-markdown-vditor/src/client/MarkdownVditorReadPrettyFieldModel.tsx
@@ -0,0 +1,33 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import React from 'react';
+import { reactive } from '@nocobase/flow-engine';
+import { ReadPrettyFieldModel, ApplicationContext, useApp } from '@nocobase/client';
+import { Display } from './components/Display';
+
+export class MarkdownVditorReadPrettyFieldModel extends ReadPrettyFieldModel {
+  static readonly supportedFieldInterfaces = ['vditor'];
+
+  @reactive
+  public render() {
+    const value = this.getValue();
+    const app = this.ctx.globals.app;
+    console.log(this.app);
+    console.log(this.ctx.globals.app);
+    if (!value) {
+      return <div></div>;
+    }
+    return (
+      <ApplicationContext.Provider value={app}>
+        <Display value={value} />
+      </ApplicationContext.Provider>
+    );
+  }
+}
