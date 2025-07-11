d09e6c109a92888f04bf8cfd407577f621207e2f
Katherine <katherine_15995@163.com>
2025-07-11 18:36:31 +0800

    2.0 map field (#7192)
    * feat: map field model
    
    * fix: bug
    
    * fix: bug
    
    * refactor: map field
    
    * fix: bug
    
    * fix: bug

---
+++ packages/plugins/@nocobase/plugin-map/src/client/fields/fieldModels/MapEditableFieldModel.tsx
@@ -0,0 +1,63 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+import { escapeT } from '@nocobase/flow-engine';
+
+import { css } from '@nocobase/client';
+import { EditableFieldModel } from '@nocobase/client';
+import { connect, mapReadPretty } from '@formily/react';
+import { MapComponent } from '../../components/MapComponent';
+import React from 'react';
+import { PointReadPretty } from './MapReadPrettyFieldModel';
+
+const className = css`
+  height: 100%;
+  border: 1px solid transparent;
+  .ant-formily-item-error & {
+    border: 1px solid #ff4d4f;
+  }
+`;
+
+const InternalMap = connect((props) => {
+  return (
+    <div className={className}>
+      <MapComponent {...props} />
+    </div>
+  );
+}, mapReadPretty(PointReadPretty));
+
+export class MapEditableFieldModel extends EditableFieldModel {
+  getMapFieldType() {
+    return null;
+  }
+  get component() {
+    return [
+      InternalMap,
+      {
+        type: this.getMapFieldType(),
+      },
+    ];
+  }
+}
+
+MapEditableFieldModel.registerFlow({
+  key: 'mapFieldSetting',
+  title: escapeT('Map field setting'),
+  auto: true,
+  sort: 500,
+  steps: {
+    zoom: {
+      use: 'setDefaultZoomLevel',
+      handler(ctx, params) {
+        ctx.model.setComponentProps({
+          zoom: params.zoom,
+        });
+      },
+    },
+  },
+});
