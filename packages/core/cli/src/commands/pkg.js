f7c08d40420da0ffc974580d9337037e750d30cd
Jiann Lu <lomo_hao@163.com>
2025-07-21 18:02:34 +0800

    fix: change tips

--- packages/core/cli/src/commands/pkg.js
+++ packages/core/cli/src/commands/pkg.js
@@ -95,7 +95,7 @@ class Package {
     }
     file = path.resolve(process.cwd(), 'node_modules', this.packageName, 'package.json');
     if (await fs.exists(file)) {
-      console.log(chalk.yellowBright(`Skipped node_modules: ${this.packageName} is in node_modules`));
+      console.log(chalk.yellowBright(`Skipped: ${this.packageName} is in node_modules`));
       return true;
     }
     return false;
