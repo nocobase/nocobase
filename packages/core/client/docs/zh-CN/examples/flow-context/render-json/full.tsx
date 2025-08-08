cdee9834aea7e729154a795ddb0e4b3babb9fbc1
chenos <chenlinxh@gmail.com>
2025-08-08 20:50:20 +0800

    fix: demo

---
+++ packages/core/client/docs/zh-CN/examples/flow-context/render-json/full.tsx
@@ -0,0 +1,81 @@
+import { Application, Plugin } from '@nocobase/client';
+import { FlowModel, FlowModelRenderer, resolveDefaultParams } from '@nocobase/flow-engine';
+import React from 'react';
+
+class HelloBlockModel extends FlowModel {
+  render() {
+    return <div ref={this.context.ref}></div>;
+  }
+  protected onMount() {
+    if (this.context.ref.current) {
+      console.log('HelloBlockModel mounted with ref:', this.context.ref.current);
+      this.rerender();
+    }
+  }
+}
+
+HelloBlockModel.registerFlow({
+  key: 'ref-example',
+  title: 'Ref Example',
+  steps: {
+    refReady: {
+      handler: async (ctx) => {
+        ctx.defineProperty('current', {
+          get: async () => {
+            return {
+              key1: 'val1',
+              key2: null,
+              key3: 3,
+              key4: { k: 'v' },
+              key5: [1, 2, 3],
+              key6: undefined,
+            };
+          },
+        });
+        ctx.onRefReady(ctx.ref, async (el) => {
+          const json = await ctx.renderJson({
+            key1: '{{ctx.current.key1}}',
+            key2: '{{ctx.current.key2}}',
+            key3: '{{ctx.current.key3}}',
+            key4: '{{ctx.current.key4}}',
+            key5: '{{ctx.current.key5}}',
+            key6: '{{ctx.current.key6}}',
+            key7: {
+              key1: '{{ctx.current.key1}}',
+              key2: '{{ctx.current.key2}}',
+            },
+            key8: ['{{ctx.current.key1}}', '{{ctx.current.key3}}'],
+            key9: '{{ctx.current.key1}} - "{{ctx.current.key3}}" - {{ctx.current.key3}} - val9',
+          });
+          el.innerHTML = `<pre>${JSON.stringify(json, null, 2)}</pre>`;
+        });
+      },
+    },
+  },
+});
+
+class PluginHelloModel extends Plugin {
+  async load() {
+    this.flowEngine.flowSettings.forceEnable();
+    this.flowEngine.registerModels({ HelloBlockModel });
+    this.flowEngine.context.defineProperty('foo', {
+      get: async () => 123,
+    });
+    const model = this.flowEngine.createModel({
+      uid: 'my-model',
+      use: 'HelloBlockModel',
+    });
+    this.router.add('root', {
+      path: '/',
+      element: <FlowModelRenderer model={model} />,
+    });
+  }
+}
+
+// 创建应用实例，注册插件
+const app = new Application({
+  router: { type: 'memory', initialEntries: ['/'] },
+  plugins: [PluginHelloModel],
+});
+
+export default app.getRootComponent();
--- packages/core/client/docs/zh-CN/examples/flow-context/render-json/index.md
+++ packages/core/client/docs/zh-CN/examples/flow-context/render-json/index.md
@@ -2,6 +2,10 @@
 
 变量解析
 
-- 支持异步
+## 完整的示例
+
+<code src="./full.tsx"></code>
+
+## 支持异步
 
 <code src="./index.tsx"></code>
