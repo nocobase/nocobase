969986fb917fe86eeb1f9dec858ebb787ab9e315
Zeke Zhang <958414905@qq.com>
2025-08-08 11:14:50 +0800

    refactor(List): extract ListItem component for better readability and maintainability

--- packages/core/client/src/schema-component/antd/list/List.tsx
+++ packages/core/client/src/schema-component/antd/list/List.tsx
@@ -12,7 +12,7 @@ import { FormLayout } from '@formily/antd-v5';
 import { ArrayField } from '@formily/core';
 import { Schema, useField, useFieldSchema } from '@formily/react';
 import { List as AntdList, PaginationProps, theme } from 'antd';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { getCardItemSchema } from '../../../block-provider';
 import { NocoBaseRecursionField } from '../../../formily/NocoBaseRecursionField';
 import { withDynamicSchemaProps } from '../../../hoc/withDynamicSchemaProps';
@@ -163,18 +163,10 @@ const InternalList = withSkeletonComponent(
               >
                 {field.value?.length
                   ? field.value.map((item, index) => {
-                      return (
-                        <NocoBaseRecursionField
-                          basePath={field.address}
-                          key={index}
-                          name={index}
-                          onlyRenderProperties
-                          schema={
-                            isMobileLayout ? transformMultiColumnToSingleColumn(getSchema(index)) : getSchema(index)
-                          }
-                        ></NocoBaseRecursionField>
-                      );
-                    })
+                    return (
+                      <ListItem key={index} field={field} index={index} schema={getSchema(index)} />
+                    );
+                  })
                   : null}
               </AntdList>
             </FormLayout>
@@ -188,6 +180,23 @@ const InternalList = withSkeletonComponent(
     displayName: 'InternalList',
   },
 );
+
+function ListItem(props: { field: any; index: number; schema: any }) {
+  const { isMobileLayout } = useMobileLayout();
+  const schema = useMemo(
+    () => (isMobileLayout ? transformMultiColumnToSingleColumn(props.schema) : props.schema),
+    [isMobileLayout, props.schema],
+  );
+
+  return (
+    <NocoBaseRecursionField
+      basePath={props.field.address}
+      name={props.index}
+      onlyRenderProperties
+      schema={schema}
+    />
+  );
+}
 
 export const List = withDynamicSchemaProps(InternalList) as typeof InternalList & {
   Item: typeof ListItem;
