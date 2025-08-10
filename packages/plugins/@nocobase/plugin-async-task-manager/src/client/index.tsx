05f9784e10f334d95cda64430e588ab25acc1db4
Junyi <mytharcher@users.noreply.github.com>
2025-08-10 17:51:32 +0800

    feat(plugin-async-task-manager): use event queue for distributed deployment (#7241)
    * feat(plugin-async-task-manager): use event queue for distributed deployment
    
    * test(plugin-async-task-manager): fix test cases
    
    * feat(server): boardcast ws messages
    
    * refactor(plugin-workflow): change ws api
    
    * fix(plugin-action-export): use storage temp path
    
    * fix(plugin-async-task-manager): fix subscribing
    
    * fix(plugin-async-task-manager): broadcast delete event to ui
    
    * fix(server): add logs for pubsub
    
    * fix(plugin-async-task-manager): fix ws message data
    
    * fix(plugin-async-task-manager): control concurrency in queue option
    
    * fix(plugin-async-task-manager): fix worker env and flow
    
    * fix(server): fix test cases
    
    * fix(plugin-async-task-manager): fix test cases
    
    * fix(plugin-async-task-manager): fix test case
    
    * fix(plugin-async-task-manager): fix life cycle event
    
    * fix(plugin-async-task-manager): fix duplicated initilization
    
    * fix(plugin-async-task-manager): fix duplicated initilization

--- packages/plugins/@nocobase/plugin-async-task-manager/src/client/index.tsx
+++ packages/plugins/@nocobase/plugin-async-task-manager/src/client/index.tsx
@@ -1,13 +1,27 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import { Registry } from '@nocobase/utils/client';
 import { Plugin } from '@nocobase/client';
 import { AsyncTaskManagerProvider } from './AsyncTaskManagerProvider';
-import { TaskResultRendererManager } from './TaskResultRendererManager';
 
-export class PluginAsyncExportClient extends Plugin {
-  taskResultRendererManager: TaskResultRendererManager = new TaskResultRendererManager();
+type TaskOrigin = {
+  Result?: React.ComponentType<any>;
+  ResultButton?: React.ComponentType<any>;
+};
+
+export class PluginAsyncTaskManagerClient extends Plugin {
+  taskOrigins: Registry<TaskOrigin> = new Registry();
 
   async load() {
     this.app.use(AsyncTaskManagerProvider);
   }
 }
 
-export default PluginAsyncExportClient;
+export default PluginAsyncTaskManagerClient;
