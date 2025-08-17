762972a53bb93fb99a7a5d09dedbfd9eb40d9c51
aaaaaajie <m18210870360@163.com>
2025-08-18 00:32:44 +0800

    fix: issue

--- packages/core/database/src/repository.ts
+++ packages/core/database/src/repository.ts
@@ -654,7 +654,6 @@ export class Repository<TModelAttributes extends {} = any, TCreationAttributes e
    */
   @transaction()
   async create(options: CreateOptions) {
-    this.validate({ values: options.values, context: options.context, operation: 'create' });
     if (Array.isArray(options.values)) {
       return this.createMany({
         ...options,
@@ -670,7 +669,7 @@ export class Repository<TModelAttributes extends {} = any, TCreationAttributes e
     });
 
     const values = (this.model as typeof Model).callSetters(guard.sanitize(options.values || {}), options);
-
+    this.validate({ values: values as any, context: options.context, operation: 'create' });
     const instance = await this.model.create<any>(values, {
       ...options,
       transaction,
@@ -730,7 +729,6 @@ export class Repository<TModelAttributes extends {} = any, TCreationAttributes e
   @mustHaveFilter()
   @injectTargetCollection
   async update(options: UpdateOptions & { forceUpdate?: boolean }) {
-    this.validate({ values: options.values, context: options.context, operation: 'update' });
     if (Array.isArray(options.values)) {
       return this.updateMany({
         ...options,
@@ -742,7 +740,7 @@ export class Repository<TModelAttributes extends {} = any, TCreationAttributes e
     const guard = UpdateGuard.fromOptions(this.model, { ...options, underscored: this.collection.options.underscored });
 
     const values = (this.model as typeof Model).callSetters(guard.sanitize(options.values || {}), options);
-
+    this.validate({ values: values as any, context: options.context, operation: 'update' });
     // NOTE:
     // 1. better to be moved to separated API like bulkUpdate/updateMany
     // 2. strictly `false` comparing for compatibility of legacy api invoking
