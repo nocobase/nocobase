8ef53505c862662ec61f490e892e4d74c9f820dc
katherinehhh <katherine_15995@163.com>
2025-09-02 21:33:40 +0800

    refactor: sub-form

--- packages/core/client/src/flow/models/fields/AssociationFieldModel/SubFormFieldModel.tsx
+++ packages/core/client/src/flow/models/fields/AssociationFieldModel/SubFormFieldModel.tsx
@@ -9,202 +9,105 @@
 
 import { escapeT, FlowModel, MultiRecordResource, useFlowModel, FlowModelRenderer } from '@nocobase/flow-engine';
 import { tval } from '@nocobase/utils/client';
-import { Card } from 'antd';
+import { Card, Form, Button } from 'antd';
 import React from 'react';
 import { AssociationFieldModel } from './AssociationFieldModel';
 import { FormComponent } from '../../data-blocks/form/FormModel';
 import { FormItem } from '../../data-blocks/form/FormItem/FormItem';
+import { each } from '@formily/shared';
+import { action } from '@formily/reactive';
 
-const ToOneSubForm = (props) => {
-  const { model } = props;
+const ArrayNester = ({ name, gridModel }: { name: string; gridModel: FlowModel }) => {
   return (
-    <Card size="small" style={{ marginBottom: 16 }}>
-      <FormItem noStyle shouldUpdate>
-        {({ getFieldValue }) => (
-          <>
-            <FlowModelRenderer model={model.subModels.grid} showFlowSettings={false} />
-          </>
-        )}
-      </FormItem>
+    <Form.List name={name}>
+      {(fields, { add, remove }) => (
+        <>
+          {fields.map(({ key, name, ...restField }) => {
+            // 给每个子项生成一个 fork model
+            const fork = gridModel.createFork({}, `${name}`);
+            return (
+              <Card key={key} style={{ marginBottom: 12 }}>
+                {/* 渲染 fork model */}
+                <FlowModelRenderer model={fork} showFlowSettings={false} />
+                <Button type="link" danger onClick={() => remove(name)}>
+                  删除
+                </Button>
+              </Card>
+            );
+          })}
+          <Button type="dashed" onClick={() => add({})} block>
+            添加一项
+          </Button>
+        </>
+      )}
+    </Form.List>
+  );
+};
+
+const ObjectNester = (props) => {
+  const model: any = useFlowModel();
+  console.log(model);
+  return (
+    <Card>
+      <FlowModelRenderer model={model.subModels.grid} showFlowSettings={false} />
     </Card>
   );
 };
 
-const ToManySubForm = () => {
-  return <div />;
-};
-
-export const SubForm = (props) => {
-  if (props.multiple) {
-    return <ToManySubForm {...props} />;
-  } else {
-    return <ToOneSubForm {...props} />;
-  }
-};
-
-export class SubFormFieldModel extends AssociationFieldModel {
-  static supportedFieldInterfaces = ['m2m', 'm2o', 'o2o', 'o2m', 'oho', 'obo', 'updatedBy', 'createdBy', 'mbm'];
-
-  get component() {
-    return [SubForm, { model: this }];
+class FormAssociationFieldModel extends AssociationFieldModel {
+  onInit(options) {
+    super.onInit(options);
+    this.context.defineProperty('currentCollection', {
+      value: this.collectionField.targetCollection,
+    });
   }
 }
 
-SubFormFieldModel.define({
+export class ObjectFormAssociationFieldModel extends FormAssociationFieldModel {
+  static supportedFieldInterfaces = ['m2o', 'o2o', 'oho', 'obo', 'updatedBy', 'createdBy'];
+  onInit(options) {
+    super.onInit(options);
+  }
+  get component() {
+    return [
+      ObjectNester,
+      {
+        type: this.collectionField.type,
+      },
+    ];
+  }
+}
+
+ObjectFormAssociationFieldModel.define({
   createModelOptions: {
-    use: 'SubFormFieldModel',
+    use: 'ObjectFormAssociationFieldModel',
     subModels: {
       grid: {
         use: 'FormFieldGridModel',
       },
     },
   },
-  sort: 350,
 });
 
-/**
- * This file is part of the NocoBase (R) project.
- * Copyright (c) 2020-2024 NocoBase Co., Ltd.
- * Authors: NocoBase Team.
- *
- * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
- * For more information, please refer to: https://www.nocobase.com/agreement.
- */
-import { connect, mapProps, mapReadPretty } from '@formily/react';
-import { observer, useField } from '@formily/react';
-import { tval } from '@nocobase/utils/client';
-import { escapeT, FlowModel, FlowModelRenderer, useFlowModel } from '@nocobase/flow-engine';
-import { each } from '@formily/shared';
-import { action } from '@formily/reactive';
-import { FormLayout } from '@formily/antd-v5';
-import { Card } from 'antd';
-import React from 'react';
-import { ArrayField as ArrayFieldType } from '@formily/core';
-import { EditableAssociationFieldModel } from './EditableAssociationFieldModel';
+export class ArrayFormAssociationFieldModel extends FormAssociationFieldModel {
+  static supportedFieldInterfaces = ['m2m', 'o2m', 'mbm'];
+  get component() {
+    return [
+      ArrayNester,
+      {
+        type: this.collectionField.type,
+      },
+    ];
+  }
+}
 
-const ArrayNester = observer((props) => {
-  const model = useFlowModel();
-  const { fieldPath } = model;
-  const arrayField = useField<ArrayFieldType>();
-  const value = [...(arrayField.value || [{}])];
-
-  return (
-    <>
-      <FormLayout layout="vertical">
-        {value.map((_, index) => {
-          const gridModel = model.subModels.grid as FlowModel;
-          const fork = gridModel.createFork({}, `${index}`);
-          fork.context.defineProperty('basePath', {
-            get: () => {
-              const basePath = model?.context.basePath;
-              const finalPath = basePath ? `${basePath}.${fieldPath}.${index}` : `${fieldPath}.${index}`;
-              return finalPath;
-            },
-            cache: false,
-          });
-          return (
-            <Card key={index} style={{ marginBottom: 12 }}>
-              <FlowModelRenderer model={fork} showFlowSettings={false} key={index} />
-            </Card>
-          );
-        })}
-      </FormLayout>
-      {arrayField.editable && (
-        <a
-          onClick={() => {
-            action(() => {
-              if (!Array.isArray(arrayField.value)) {
-                arrayField.value = [{}];
-              }
-              const index = arrayField.value.length;
-              arrayField.value.splice(index, 0, {});
-              each(arrayField.form.fields, (targetField, key) => {
-                if (!targetField) {
-                  delete arrayField.form.fields[key];
-                }
-              });
-              arrayField.onInput(arrayField.value);
-            });
-          }}
-        >
-          {escapeT('Add new')}
-        </a>
-      )}
-    </>
-  );
+ArrayFormAssociationFieldModel.define({
+  createModelOptions: {
+    use: 'ArrayFormAssociationFieldModel',
+    subModels: {
+      grid: {
+        use: 'FormFieldGridModel',
+      },
+    },
+  },
 });
-
-//  const ObjectNester = (props) => {
-//    const model: any = useFlowModel();
-//    return (
-//      <Card>
-//        <FormLayout layout={'vertical'}>
-//          <FlowModelRenderer model={model.subModels.grid} showFlowSettings={false} />
-//        </FormLayout>
-//      </Card>
-//    );
-//  };
-
-//  class FormEditableAssociationFieldModel extends EditableAssociationFieldModel {
-//    onInit(options) {
-//      super.onInit(options);
-//      this.context.defineProperty('currentCollection', {
-//        value: this.collectionField.targetCollection,
-//      });
-//    }
-//  }
-
-//  export class ObjectFormEditableAssociationFieldModel extends FormEditableAssociationFieldModel {
-//    static supportedFieldInterfaces = ['m2o', 'o2o', 'oho', 'obo', 'updatedBy', 'createdBy'];
-//    onInit(options) {
-//      super.onInit(options);
-//      this.context.defineProperty('basePath', {
-//        get: () => {
-//          const basePath = this.parent?.context.basePath;
-//          return basePath ? `${basePath}.${this.fieldPath}` : this.fieldPath;
-//        },
-//      });
-//    }
-//    get component() {
-//      return [
-//        ObjectNester,
-//        {
-//          type: this.collectionField.type,
-//        },
-//      ];
-//    }
-//  }
-
-//  ObjectFormEditableAssociationFieldModel.define({
-//    defaultOptions: {
-//      use: 'ObjectFormEditableAssociationFieldModel',
-//      subModels: {
-//        grid: {
-//          use: 'FormFieldGridModel',
-//        },
-//      },
-//    },
-//  });
-
-//  export class ArrayFormEditableAssociationFieldModel extends FormEditableAssociationFieldModel {
-//    static supportedFieldInterfaces = ['m2m', 'o2m', 'mbm'];
-//    get component() {
-//      return [
-//        ArrayNester,
-//        {
-//          type: this.collectionField.type,
-//        },
-//      ];
-//    }
-//  }
-
-//  ArrayFormEditableAssociationFieldModel.define({
-//    defaultOptions: {
-//      use: 'ArrayFormEditableAssociationFieldModel',
-//      subModels: {
-//        grid: {
-//          use: 'FormFieldGridModel',
-//        },
-//      },
-//    },
-//  });
