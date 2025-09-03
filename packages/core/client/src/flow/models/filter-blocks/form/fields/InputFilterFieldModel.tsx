5dcab86ea2266b8d77baf969251574af40da33ed
Zeke Zhang <958414905@qq.com>
2025-09-04 01:07:50 +0800

    refactor(InputFilterFieldModel): update Input import source

--- packages/core/client/src/flow/models/filter-blocks/form/fields/InputFilterFieldModel.tsx
+++ packages/core/client/src/flow/models/filter-blocks/form/fields/InputFilterFieldModel.tsx
@@ -7,7 +7,7 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
-import { Input } from '@formily/antd-v5';
+import { Input } from 'antd';
 import { FilterFormFieldModel } from './FilterFormFieldModel';
 
 export class InputFilterFieldModel extends FilterFormFieldModel {
