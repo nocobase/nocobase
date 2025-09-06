d6bb350a3d4f1e3eadcff67601aafc3b08223eaa
Drol <cgyrock@gmail.com>
2025-09-06 22:19:34 +0800

    feat: can send empty message with work context to ai employee

--- packages/plugins/@nocobase/plugin-ai/src/client/ai-employees/chatbox/Sender.tsx
+++ packages/plugins/@nocobase/plugin-ai/src/client/ai-employees/chatbox/Sender.tsx
@@ -9,40 +9,71 @@
 
 import React, { useEffect, useRef, useState } from 'react';
 import { Sender as AntSender } from '@ant-design/x';
-import { GetRef } from 'antd';
+import { GetRef, Button } from 'antd';
 import { useT } from '../../locale';
 import { SenderFooter } from './SenderFooter';
 import { SenderHeader } from './SenderHeader';
-import { AttachmentsHeader } from './AttachmentsHeader';
 import { useChatConversationsStore } from './stores/chat-conversations';
 import { useChatMessagesStore } from './stores/chat-messages';
 import { useChatMessageActions } from './hooks/useChatMessageActions';
 import { useChatBoxStore } from './stores/chat-box';
 import { useChatBoxActions } from './hooks/useChatBoxActions';
+import _ from 'lodash';
 
-export const Sender: React.FC = () => {
-  const t = useT();
-  const senderRef = useRef<GetRef<typeof AntSender> | null>(null);
-
-  const senderValue = useChatBoxStore.use.senderValue();
-  const setSenderValue = useChatBoxStore.use.setSenderValue();
-  const senderPlaceholder = useChatBoxStore.use.senderPlaceholder();
+const useSendMessage = () => {
   const currentEmployee = useChatBoxStore.use.currentEmployee();
-  const setSenderRef = useChatBoxStore.use.setSenderRef();
   const isEditingMessage = useChatBoxStore.use.isEditingMessage();
   const editingMessageId = useChatBoxStore.use.editingMessageId();
 
   const currentConversation = useChatConversationsStore.use.currentConversation();
 
-  const responseLoading = useChatMessagesStore.use.responseLoading();
   const attachments = useChatMessagesStore.use.attachments();
   const contextItems = useChatMessagesStore.use.contextItems();
   const systemMessage = useChatMessagesStore.use.systemMessage();
   const skillSettings = useChatMessagesStore.use.skillSettings();
 
-  const { cancelRequest, finishEditingMessage } = useChatMessageActions();
+  const { finishEditingMessage } = useChatMessageActions();
 
   const { send } = useChatBoxActions();
+  const handleSubmit = (content: string) => {
+    send({
+      sessionId: currentConversation,
+      aiEmployee: currentEmployee,
+      systemMessage,
+      messages: [
+        {
+          type: 'text',
+          content,
+        },
+      ],
+      attachments: attachments.filter((x) => x.status === 'done'),
+      workContext: contextItems,
+      editingMessageId: isEditingMessage ? editingMessageId : undefined,
+      skillSettings,
+    });
+
+    if (isEditingMessage) {
+      finishEditingMessage();
+    }
+  };
+
+  return [handleSubmit];
+};
+
+export const Sender: React.FC = () => {
+  const t = useT();
+  const [handleSubmit] = useSendMessage();
+  const senderRef = useRef<GetRef<typeof AntSender> | null>(null);
+  const senderButtonRef = useRef<GetRef<typeof Button> | null>(null);
+
+  const senderValue = useChatBoxStore.use.senderValue();
+  const setSenderValue = useChatBoxStore.use.setSenderValue();
+  const currentEmployee = useChatBoxStore.use.currentEmployee();
+  const setSenderRef = useChatBoxStore.use.setSenderRef();
+
+  const responseLoading = useChatMessagesStore.use.responseLoading();
+
+  const { cancelRequest } = useChatMessageActions();
 
   const [value, setValue] = useState(senderValue);
 
@@ -60,6 +91,18 @@ export const Sender: React.FC = () => {
     setValue(senderValue);
   }, [senderValue]);
 
+  const contextItems = useChatMessagesStore.use.contextItems();
+  useEffect(() => {
+    senderRef.current.nativeElement.onkeydown = (e) => {
+      if (e.key === 'Enter' && !e.shiftKey) {
+        e.preventDefault();
+        if (_.isEmpty(senderValue) && contextItems.length) {
+          senderButtonRef.current.click();
+        }
+      }
+    };
+  }, [senderValue, contextItems]);
+
   return (
     <AntSender
       // components={{
@@ -70,31 +113,28 @@ export const Sender: React.FC = () => {
       onChange={(value) => {
         setValue(value);
       }}
-      onSubmit={(content) => {
-        send({
-          sessionId: currentConversation,
-          aiEmployee: currentEmployee,
-          systemMessage,
-          messages: [
-            {
-              type: 'text',
-              content,
-            },
-          ],
-          attachments: attachments.filter((x) => x.status === 'done'),
-          workContext: contextItems,
-          editingMessageId: isEditingMessage ? editingMessageId : undefined,
-          skillSettings,
-        });
-
-        if (isEditingMessage) {
-          finishEditingMessage();
-        }
-      }}
+      onSubmit={handleSubmit}
       onCancel={cancelRequest}
       header={<SenderHeader />}
       loading={responseLoading}
-      footer={({ components }) => <SenderFooter components={components} />}
+      footer={({ components }) => {
+        const { SendButton } = components;
+        if (SendButton) {
+          const DecoratedSendButton = (props) => {
+            const senderValue = useChatBoxStore.use.senderValue();
+            const contextItems = useChatMessagesStore.use.contextItems();
+            const [handleSubmit] = useSendMessage();
+            const handleEmptySubmit = () => {
+              if (_.isEmpty(senderValue) && contextItems.length) {
+                handleSubmit('');
+              }
+            };
+            return <SendButton ref={senderButtonRef} {...props} onClick={handleEmptySubmit} />;
+          };
+          components.SendButton = DecoratedSendButton;
+        }
+        return <SenderFooter components={components} />;
+      }}
       disabled={!currentEmployee}
       // placeholder={!currentEmployee ? t('Please choose an AI employee') : senderPlaceholder}
       actions={false}
