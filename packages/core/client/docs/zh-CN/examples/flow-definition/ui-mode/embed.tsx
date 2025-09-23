d9dc55f997a720a7d20d6a82d276152c8ce05a24
chenos <chenlinxh@gmail.com>
2025-09-23 21:39:52 +0800

    fix: embed demo

--- packages/core/client/docs/zh-CN/examples/flow-definition/ui-mode/embed.tsx
+++ packages/core/client/docs/zh-CN/examples/flow-definition/ui-mode/embed.tsx
@@ -1,6 +1,7 @@
+import { useForm } from '@formily/react';
 import { Application, css, Plugin } from '@nocobase/client';
-import { defineFlow, FlowModel, FlowModelRenderer } from '@nocobase/flow-engine';
-import { Button } from 'antd';
+import { defineFlow, FlowModel, FlowModelContext, FlowModelRenderer, useFlowContext } from '@nocobase/flow-engine';
+import { Button, Space } from 'antd';
 import React from 'react';
 
 // 基础 UI Mode 示例：展示字符串形式的 uiMode
@@ -8,37 +9,50 @@ class BasicModel extends FlowModel {
   render() {
     return <Button {...this.props} />;
   }
+
+  setSteParamsAndPreview(params) {
+    this.setStepParams('flow1', {
+      step1: params,
+    });
+    this.rerender();
+  }
+}
+
+function PreviewButton() {
+  const ctx = useFlowContext();
+  const form = useForm();
+  return (
+    <Button
+      onClick={async () => {
+        await form.submit();
+        // form.values;
+        ctx.model.setSteParamsAndPreview({
+          title: 'abc',
+        });
+      }}
+    >
+      Preview
+    </Button>
+  );
 }
 
 const basicUIMode = defineFlow({
-  key: 'basicUIMode',
+  key: 'flow1',
   title: '基础 UI 模式示例',
   steps: {
-    dialogStep: {
+    step1: {
       title: '对话框配置1',
-      uiMode: 'embed', // 字符串形式
-      uiSchema: {
-        title: {
-          type: 'string',
-          title: '标题',
-          'x-decorator': 'FormItem',
-          'x-component': 'Input',
+      uiMode: {
+        type: 'embed',
+        props: {
+          footer: (originNode) => (
+            <Space>
+              <PreviewButton />
+              {originNode}
+            </Space>
+          ),
         },
-      },
-      onFormValuesChange: (ctx, params, previousParams) => {
-        ctx.setStepParams('dialogStep', params);
-        ctx.model.applyFlow('basicUIMode');
-      },
-      afterParamsSave: async (ctx, params) => {},
-      defaultParams: {
-        title: '默认标题',
-      },
-      handler(ctx, params) {
-        ctx.model.setProps({ children: params.title });
-      },
-    },
-    dialogStep2: {
-      title: '对话框配置2',
+      }, // 字符串形式
       uiSchema: {
         title: {
           type: 'string',
@@ -67,6 +81,13 @@ class BasicUIPlugin extends Plugin {
     const model = this.flowEngine.createModel({
       uid: 'basic-ui-model',
       use: 'BasicModel',
+      stepParams: {
+        flow1: {
+          step1: {
+            title: '默认标题1',
+          },
+        },
+      },
     });
 
     this.router.add('root', {
