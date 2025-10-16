de5a83a580e9b320bdc2505d52b1487ffb2f6d63
mytharcher <mytharcher@gmail.com>
2025-10-16 21:19:40 +0800

    test(database): add test cases

--- packages/core/database/src/__tests__/operator/in.test.ts
+++ packages/core/database/src/__tests__/operator/in.test.ts
@@ -28,6 +28,43 @@ describe('operator > $in', () => {
     await db.close();
   });
 
+  it('in with empty array should return 0', async () => {
+    await db.getRepository('tests').create({});
+
+    const result = await db.getRepository('tests').count({
+      filter: {
+        'name.$in': [],
+      },
+    });
+    expect(result).toEqual(0);
+  });
+
+  it('in with array not contains value should return 0', async () => {
+    await db.getRepository('tests').create({
+      values: { name: 'test' },
+    });
+
+    const result = await db.getRepository('tests').count({
+      filter: {
+        'name.$in': ['test2', 'test1'],
+      },
+    });
+    expect(result).toEqual(0);
+  });
+
+  it('in with array contains value should return 1', async () => {
+    await db.getRepository('tests').create({
+      values: { name: 'test' },
+    });
+
+    const result = await db.getRepository('tests').count({
+      filter: {
+        'name.$in': ['test', 'test1'],
+      },
+    });
+    expect(result).toEqual(1);
+  });
+
   it('in with null should not throw error', async () => {
     await db.getRepository('tests').create({});
 
