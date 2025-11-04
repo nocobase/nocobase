a0e277de1f3112913ec62aa28eebf145905c2712
Katherine <katherine_15995@163.com>
2025-11-04 15:58:28 +0800

    fix: issue with clearing non-timezone date in edit form (#7759)

--- packages/core/client/src/flow/models/fields/DateTimeFieldModel/DateTimeNoTzFieldModel.tsx
+++ packages/core/client/src/flow/models/fields/DateTimeFieldModel/DateTimeNoTzFieldModel.tsx
@@ -23,6 +23,9 @@ export const DateTimeNoTzPicker = (props) => {
       picker={picker}
       showTime={showTime}
       onChange={(val: any) => {
+        if (!val) {
+          return onChange(val);
+        }
         const outputFormat = showTime ? 'YYYY-MM-DD HH:mm:ss' : 'YYYY-MM-DD';
         if (showTime) {
           onChange(dayjs(val, format).format(outputFormat));
