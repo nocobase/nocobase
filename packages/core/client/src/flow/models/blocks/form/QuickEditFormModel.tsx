65a4fd48feda3a702997f3729cd7a53d6ac1286a
katherinehhh <katherine_15995@163.com>
2025-11-12 11:48:56 +0800

    fix: quick edit form

--- packages/core/client/src/flow/models/blocks/form/QuickEditFormModel.tsx
+++ packages/core/client/src/flow/models/blocks/form/QuickEditFormModel.tsx
@@ -114,6 +114,11 @@ export class QuickEditFormModel extends FlowModel {
       resolveOnServer: true,
       meta: recordMeta,
     });
+    this.context.defineProperty('collection', {
+      get: () => {
+        return this.collection;
+      },
+    });
   }
 
   addAppends(fieldPath: string, refresh = false) {
@@ -167,11 +172,14 @@ export class QuickEditFormModel extends FlowModel {
               const formValues = {
                 ...values,
               };
+              console.log(formValues);
               const originalValues = {
                 [this.fieldPath]: this.resource.getData()?.[this.fieldPath],
               };
               try {
-                this.resource.save(formValues, { refresh: false }).catch((error) => {});
+                this.resource.save(formValues, { refresh: false }).catch((error) => {
+                  console.log(error, 999);
+                });
                 this.__onSubmitSuccess?.(formValues);
                 this.viewContainer.close();
                 this.context.message.success(this.context.t('Saved successfully'));
