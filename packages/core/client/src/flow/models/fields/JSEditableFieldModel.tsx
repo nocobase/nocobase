31cce72bc0f52cc16e769c2f0ea6e460f5c1b8d2
gchust <gchust@qq.com>
2025-11-13 19:45:24 +0800

    fix: default js field code configuration

--- packages/core/client/src/flow/models/fields/JSEditableFieldModel.tsx
+++ packages/core/client/src/flow/models/fields/JSEditableFieldModel.tsx
@@ -27,27 +27,23 @@ const DEFAULT_CODE = `
 function JsEditableField() {
   const React = ctx.React;
   const { Input } = ctx.antd;
-  const [value, setValue] = React.useState(ctx.getValue?.() ?? '');
+  // const [value, setValue] = React.useState(ctx.getValue?.() ?? '');
 
-  React.useEffect(() => {
-    const handler = (ev) => setValue(ev?.detail ?? '');
-    ctx.element?.addEventListener('js-field:value-change', handler);
-    return () => ctx.element?.removeEventListener('js-field:value-change', handler);
-  }, []);
+  // React.useEffect(() => {
+  //   const handler = (ev) => setValue(ev?.detail ?? '');
+  //   ctx.element?.addEventListener('js-field:value-change', handler);
+  //   return () => ctx.element?.removeEventListener('js-field:value-change', handler);
+  // }, []);
 
-  const onChange = (e) => {
-    const v = e?.target?.value ?? '';
-    setValue(v);
-    ctx.setValue?.(v);
-  };
+  // const onChange = (e) => {
+  //   const v = e?.target?.value ?? '';
+  //   setValue(v);
+  //   ctx.setValue?.(v);
+  // };
 
   return (
     <Input
-      value={value}
-      onChange={onChange}
-      disabled={!!ctx.disabled}
-      readOnly={!!ctx.readOnly}
-      style={{ width: '100%' }}
+      {...ctx.model.props}
     />
   );
 }
