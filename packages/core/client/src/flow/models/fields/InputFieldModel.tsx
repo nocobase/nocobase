067e592f7f272d0dcdc0df0282f4facfd3486ef0
katherinehhh <katherine_15995@163.com>
2025-11-17 14:28:30 +0800

    refactor: scan input

--- packages/core/client/src/flow/models/fields/InputFieldModel.tsx
+++ packages/core/client/src/flow/models/fields/InputFieldModel.tsx
@@ -10,10 +10,10 @@
 import { EditableItemModel, FilterableItemModel, tExpr } from '@nocobase/flow-engine';
 import { FieldModel } from '../base';
 import React, { useState, useRef } from 'react';
-import { Input, Button, Modal } from 'antd';
-import { Html5Qrcode } from 'html5-qrcode';
+import { Input, Modal } from 'antd';
+import { Html5Qrcode, Html5QrcodeSupportedFormats } from 'html5-qrcode';
 import { ScanOutlined } from '@ant-design/icons';
-const ScanInput = (props) => {
+const ScanInput = ({ disableManualInput, ...props }) => {
   const [value, setValue] = useState('');
   const [visible, setVisible] = useState(false);
   const scannerRef = useRef<any>(null);
@@ -24,7 +24,17 @@ const ScanInput = (props) => {
   };
 
   const startScan = () => {
-    const html5QrCode = new Html5Qrcode('qr-scanner');
+    const html5QrCode = new Html5Qrcode('qr-scanner', {
+      formatsToSupport: [
+        Html5QrcodeSupportedFormats.CODE_128,
+        Html5QrcodeSupportedFormats.EAN_13,
+        Html5QrcodeSupportedFormats.EAN_8,
+        Html5QrcodeSupportedFormats.UPC_A,
+        Html5QrcodeSupportedFormats.ITF,
+        Html5QrcodeSupportedFormats.CODE_39,
+      ],
+      verbose: false,
+    });
     scannerRef.current = html5QrCode;
 
     html5QrCode.start(
@@ -39,9 +49,7 @@ const ScanInput = (props) => {
         stopScan();
         setVisible(false);
       },
-      (err) => {
-        console.log(err);
-      },
+      (err) => {},
     );
   };
 
@@ -58,14 +66,14 @@ const ScanInput = (props) => {
     stopScan();
     setVisible(false);
   };
-
   return (
-    <div style={{ width: 300 }}>
+    <div>
       <Input
+        {...props}
         value={value}
         onChange={(e) => setValue(e.target.value)}
         addonAfter={<ScanOutlined onClick={openScanner} />}
-        {...props}
+        readOnly={disableManualInput}
       />
 
       <Modal open={visible} onCancel={closeModal} footer={null} destroyOnClose>
@@ -77,12 +85,60 @@ const ScanInput = (props) => {
 
 export class InputFieldModel extends FieldModel {
   render() {
-    return <ScanInput {...this.props} />;
+    return this.props.enableScan ? (
+      <ScanInput disableManualInput={this.props.disableManualInput} {...this.props} />
+    ) : (
+      <Input {...this.props} />
+    );
   }
 }
 
 InputFieldModel.define({
   label: tExpr('Input'),
+});
+InputFieldModel.registerFlow({
+  key: 'inputFieldSettings',
+  title: tExpr('Input Settings'),
+  steps: {
+    enAbleScan: {
+      title: tExpr('Enable Scan'),
+      uiSchema: (ctx) => {
+        return {
+          enableScan: {
+            type: 'string',
+            'x-decorator': 'FormItem',
+            'x-component': 'Switch',
+          },
+          disableManualInput: {
+            type: 'string',
+            'x-decorator': 'FormItem',
+            'x-component': 'Switch',
+            title: "{{t('Disable manual input')}}",
+            'x-reactions': [
+              (field) => {
+                const { enableScan } = field.form.values;
+                field.hidden = !enableScan;
+              },
+            ],
+          },
+        };
+      },
+      defaultParams: (ctx) => {
+        const { enableScan, disableManualInput } = ctx.model.props;
+        return {
+          enableScan: enableScan,
+          disableManualInput,
+        };
+      },
+      handler(ctx, params) {
+        const { enableScan, disableManualInput } = params;
+        ctx.model.setProps({
+          enableScan: enableScan,
+          disableManualInput,
+        });
+      },
+    },
+  },
 });
 EditableItemModel.bindModelToInterface('InputFieldModel', ['input', 'email', 'phone', 'uuid', 'url', 'nanoid'], {
   isDefault: true,
