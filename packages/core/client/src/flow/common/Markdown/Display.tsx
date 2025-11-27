35289b9a070b52154901bd7ca6c9b2cd8578f663
Katherine <katherine_15995@163.com>
2025-11-27 22:03:20 +0800

    fix: display issue when the markdown field is ellipsized in HTML mode (#7994)

--- packages/core/client/src/flow/common/Markdown/Display.tsx
+++ packages/core/client/src/flow/common/Markdown/Display.tsx
@@ -19,7 +19,6 @@ function convertToText(markdownText: string) {
   // 使用 DOMParser 来解析 HTML 字符串
   const parser = new DOMParser();
   const doc = parser.parseFromString(markdownText, 'text/html'); // 解析为 HTML 文档
-
   // 提取所有图片标签，并替换为其 src 属性
   const imgTags = doc.querySelectorAll('img');
   imgTags.forEach((img) => {
@@ -195,7 +194,7 @@ export const Display = (props) => {
                 }
               `}
             >
-              <DisplayInner value={value} />
+              <DisplayInner value={value} loadImages={true} />
             </div>
           )}
         </div>
