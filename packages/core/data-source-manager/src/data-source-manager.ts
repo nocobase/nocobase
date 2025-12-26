37414e0ac850a15f816fd1aa4f0fb86a3aaf9ee3
Drol <cgyrock@gmail.com>
2025-12-26 11:21:43 +0800

    fix(plugin-workflow): after change data source password collection trigger invalid (#8207)
    * refactor(data-source): reuse database instance in data source refresh action
    
    * refactor(data-source): resolve options naming conflicts
    
    * fix(work-flow): after change data source password colletcion trigger invalid
    
    * test(data-source-manager): add testcase for database reuse after data source reload
    
    * test(data-source-manager): modify testcase
    
    * fix(data-source-manager): breaking build packages
    
    * fix(data-source-manager): fix breaking testcase
    
    * test(data-source-manager): move testcase to pro-plugins

--- packages/core/data-source-manager/src/data-source-manager.ts
+++ packages/core/data-source-manager/src/data-source-manager.ts
@@ -67,12 +67,16 @@ export class DataSourceManager {
       hook(dataSource);
     }
 
-    await dataSource.load(options);
     const oldDataSource = this.dataSources.get(dataSource.name);
-
     if (oldDataSource) {
-      await oldDataSource.close();
+      if (options.reuseDB === true) {
+        await oldDataSource.cleanCache();
+      } else {
+        await oldDataSource.close();
+      }
     }
+
+    await dataSource.load(options);
     this.dataSources.set(dataSource.name, dataSource);
 
     for (const hook of this.onceHooks) {
