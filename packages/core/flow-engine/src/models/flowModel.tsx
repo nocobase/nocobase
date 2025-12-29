c3bccb493bdf319c37c5cf4f63aa4d1fe8e69f83
Zeke Zhang <958414905@qq.com>
2025-12-29 16:33:44 +0800

    fix(flow): refactor debounce function in FlowModel

--- packages/core/flow-engine/src/models/flowModel.tsx
+++ packages/core/flow-engine/src/models/flowModel.tsx
@@ -771,7 +771,12 @@ export class FlowModel<Structure extends DefaultStructure = DefaultStructure> {
   }
 
   private _dispatchEventWithDebounce = _.debounce(
-    async (eventName: string, inputArgs?: Record<string, any>, options?: DispatchEventOptions) => {
+    async function (
+      this: FlowModel,
+      eventName: string,
+      inputArgs?: Record<string, any>,
+      options?: DispatchEventOptions,
+    ) {
       return this._dispatchEvent(eventName, inputArgs, options);
     },
     500,
