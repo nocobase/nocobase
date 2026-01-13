8c998b2dc8df5ea8a5226a8f2d7a0ad62ec21a7f
Drol <cgyrock@gmail.com>
2026-01-13 14:12:00 +0800

    fix(core/database): options parser missing BelongsToArrayAssociation handle (#8406)
    * fix(core/database): options parser missing BelongsToArrayAssociation handle
    
    * test(core/database): add testcase of issue that m2m(array) in second-level appends occure error

--- packages/core/database/src/options-parser.ts
+++ packages/core/database/src/options-parser.ts
@@ -14,6 +14,7 @@ import { Database } from './database';
 import FilterParser from './filter-parser';
 import { Appends, Except, FindOptions } from './repository';
 import qs from 'qs';
+import { BelongsToArrayAssociation } from './belongs-to-array/belongs-to-array-repository';
 
 const debug = require('debug')('noco-database');
 
@@ -347,7 +348,7 @@ export class OptionsParser {
      * @param queryParams
      * @param append
      */
-    const setInclude = (model: ModelStatic<any>, queryParams: any, append: string) => {
+    const setInclude = (model: ModelStatic<any>, queryParams: any, append: string, parentAs?: string) => {
       const appendWithOptions = this.parseAppendWithOptions(append);
 
       append = appendWithOptions.name;
@@ -416,10 +417,21 @@ export class OptionsParser {
       // if association not exist, create it
       if (existIncludeIndex == -1) {
         // association not exists
-        queryParams['include'].push({
+        const association = associations[appendAssociation];
+        if (!association) {
+          throw new Error(`association ${appendAssociation} in ${model.name} not found`);
+        }
+        let includeOptions = {
           association: appendAssociation,
           options: appendWithOptions.options || {},
-        });
+        };
+        if (association.associationType === 'BelongsToArray') {
+          includeOptions = {
+            ...includeOptions,
+            ...(association as any as BelongsToArrayAssociation).generateInclude(parentAs),
+          };
+        }
+        queryParams['include'].push(includeOptions);
 
         existIncludeIndex = queryParams['include'].length - 1;
       }
@@ -469,11 +481,8 @@ export class OptionsParser {
           nextAppend += appendWithOptions.raw;
         }
 
-        setInclude(
-          model.associations[queryParams['include'][existIncludeIndex].association].target,
-          queryParams['include'][existIncludeIndex],
-          nextAppend,
-        );
+        const association = model.associations[queryParams['include'][existIncludeIndex].association];
+        setInclude(association.target, queryParams['include'][existIncludeIndex], nextAppend, association.as);
       }
     };
 
