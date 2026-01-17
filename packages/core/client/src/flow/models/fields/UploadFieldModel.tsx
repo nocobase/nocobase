7d730f1fa1033a3258808a1d8fdf88700f8552a8
gchust <gchust@qq.com>
2026-01-17 21:53:33 +0800

    fix: upload popup configuration has not bee saved

--- packages/core/client/src/flow/models/fields/UploadFieldModel.tsx
+++ packages/core/client/src/flow/models/fields/UploadFieldModel.tsx
@@ -558,13 +558,15 @@ UploadFieldModel.registerFlow({
         };
         const openMode = ctx.isMobileLayout ? 'embed' : ctx.inputArgs.mode || params.mode || 'drawer';
         const size = ctx.inputArgs.size || params.size || 'medium';
+        const parentIdForPicker = ctx.model.props?.sourceFieldModelUid || ctx.model.uid;
+
         ctx.viewer.open({
           type: openMode,
           width: sizeToWidthMap[openMode][size],
           inheritContext: false,
           target: ctx.layoutContentElement,
           inputArgs: {
-            parentId: ctx.model.uid,
+            parentId: parentIdForPicker,
             scene: 'select',
             dataSourceKey: ctx.collection.dataSourceKey,
             collectionName: ctx.collectionField?.target,
