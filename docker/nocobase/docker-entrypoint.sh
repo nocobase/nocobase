d4368d79c18b4fa7da0f270d1b735fc922c0520c
chenos <chenlinxh@gmail.com>
2026-01-19 15:24:42 +0800

    chore: update docker-entrypoint.sh

--- docker/nocobase/docker-entrypoint.sh
+++ docker/nocobase/docker-entrypoint.sh
@@ -28,6 +28,7 @@ if [ ! -f "/app/nocobase/package.json" ]; then
 fi
 
 cd /app/nocobase && yarn nocobase postinstall
+cd /app/nocobase && yarn nocobase db:auth
 cd /app/nocobase && yarn nocobase create-nginx-conf
 cd /app/nocobase && yarn nocobase generate-instance-id
 rm -rf /etc/nginx/sites-enabled/nocobase.conf
@@ -39,6 +40,7 @@ echo 'nginx started';
 # run scripts in storage/scripts
 if [ -d "/app/nocobase/storage/scripts" ]; then
   for f in /app/nocobase/storage/scripts/*.sh; do
+    [ -e "$f" ] || continue
     echo "Running $f"
     sh "$f"
   done
