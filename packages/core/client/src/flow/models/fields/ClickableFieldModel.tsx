8653688bbca3d9801680f37a4af5ecf088923d84
Zeke Zhang <958414905@qq.com>
2026-01-29 17:51:08 +0800

    Revert "fix(flow): keep click-to-open tag clickable when empty"
    This reverts commit 58619a7d8a77acc625d7a453503e1363f0cfc722.

--- packages/core/client/src/flow/models/fields/ClickableFieldModel.tsx
+++ packages/core/client/src/flow/models/fields/ClickableFieldModel.tsx
@@ -154,10 +154,12 @@ export class ClickableFieldModel extends FieldModel {
     };
 
     if (isTag) {
-      return !value && !clickToOpen ? null : (
-        <Tag {...restProps} style={commonStyle} onClick={handleClick}>
-          {display}
-        </Tag>
+      return (
+        value && (
+          <Tag {...restProps} style={commonStyle} onClick={handleClick}>
+            {display}
+          </Tag>
+        )
       );
     }
     if (clickToOpen) {
