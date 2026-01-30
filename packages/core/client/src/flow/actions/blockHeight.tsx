32028657010531f2e2f785665be18a9854c63367
katherinehhh <katherine_15995@163.com>
2026-01-30 17:12:00 +0800

    feat: block height

---
+++ packages/core/client/src/flow/actions/blockHeight.tsx
@@ -0,0 +1,83 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import { defineAction, MultiRecordResource, pruneFilter, tExpr, useFlowSettingsContext } from '@nocobase/flow-engine';
+import { isEmptyFilter, transformFilter } from '@nocobase/utils/client';
+import _ from 'lodash';
+import React from 'react';
+import { FilterGroup, VariableFilterItem } from '../components/filter';
+import { FieldModel } from '../models/base/FieldModel';
+
+export const HeightMode = {
+  DEFAULT: 'defaultHeight',
+  SPECIFY_VALUE: 'specifyValue',
+  FULL_HEIGHT: 'fullHeight',
+};
+
+export const blockHeight = defineAction({
+  name: 'blockHeight',
+  title: tExpr('Block Height'),
+  uiMode: {
+    type: 'dialog',
+    props: {
+      width: 800,
+    },
+  },
+  uiSchema: (ctx) => {
+    const { t } = ctx;
+    return {
+      heightMode: {
+        type: 'string',
+        enum: [
+          { label: t('Default'), value: HeightMode.DEFAULT },
+          { label: t('Specify height'), value: HeightMode.SPECIFY_VALUE },
+          { label: t('Full height'), value: HeightMode.FULL_HEIGHT },
+        ],
+        required: true,
+        'x-decorator': 'FormItem',
+        'x-component': 'Radio.Group',
+      },
+      height: {
+        title: t('Height'),
+        type: 'string',
+        required: true,
+        'x-decorator': 'FormItem',
+        'x-component': 'InputNumber',
+        'x-component-props': {
+          addonAfter: 'px',
+        },
+        'x-validator': [
+          {
+            minimum: 40,
+          },
+        ],
+        'x-reactions': {
+          dependencies: ['heightMode'],
+          fulfill: {
+            state: {
+              hidden: '{{ $deps[0]==="fullHeight"||$deps[0]==="defaultHeight"}}',
+              value: '{{$deps[0]!=="specifyValue"?null:$self.value}}',
+            },
+          },
+        },
+      },
+    };
+  },
+  defaultParams(ctx) {
+    return {
+      heightMode: HeightMode.DEFAULT,
+    };
+  },
+  async handler(ctx, params) {
+    ctx.model.setProps({
+      heightMode: params.heightMode,
+      height: params.height,
+    });
+  },
+});
