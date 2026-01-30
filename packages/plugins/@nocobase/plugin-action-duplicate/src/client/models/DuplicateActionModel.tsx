6cfc38c7998b1495a730e4f72c0a557ac89317fd
katherinehhh <katherine_15995@163.com>
2026-01-30 11:02:23 +0800

    refactor: locale improve

--- packages/plugins/@nocobase/plugin-action-duplicate/src/client/models/DuplicateActionModel.tsx
+++ packages/plugins/@nocobase/plugin-action-duplicate/src/client/models/DuplicateActionModel.tsx
@@ -15,6 +15,7 @@ import { Tree as AntdTree } from 'antd';
 import { cloneDeep } from 'lodash';
 import React, { useEffect, useState } from 'react';
 import { getCollectionState, getSyncFromForm } from './utils';
+import { NAMESPACE } from '../locale';
 
 const Tree = connect(
   AntdTree,
@@ -455,7 +456,9 @@ DuplicateActionModel.registerFlow({
       async handler(ctx, params) {
         const { duplicateFields = [] } = ctx.model.props;
         if (!duplicateFields?.length) {
-          ctx.message.error(ctx.t('Please configure the duplicate fields'));
+          ctx.message.error(
+            ctx.t('Please configure the duplicate fields in duplicate mode configuration', { ns: NAMESPACE }),
+          );
           return;
         }
         if (ctx.model.duplicateLoading) {
@@ -555,7 +558,9 @@ DuplicateActionModel.registerFlow({
       async handler(ctx, params) {
         const { duplicateFields = [] } = ctx.model.props;
         if (!duplicateFields?.length) {
-          ctx.message.error(ctx.t('Please configure the duplicate fields'));
+          ctx.message.error(
+            ctx.t('Please configure the duplicate fields in duplicate mode configuration', { ns: NAMESPACE }),
+          );
           return;
         }
         const sizeToWidthMap: Record<string, any> = {
