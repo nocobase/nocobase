14e9c78951d7a8ce0ffea6c4dd2f1504ea8aca8e
xilesun <2013xile@gmail.com>
2026-02-09 23:20:17 +0800

    fix: editing message

--- packages/plugins/@nocobase/plugin-ai/src/client/ai-employees/chatbox/SenderHeader.tsx
+++ packages/plugins/@nocobase/plugin-ai/src/client/ai-employees/chatbox/SenderHeader.tsx
@@ -10,15 +10,20 @@
 import React from 'react';
 import { AttachmentsHeader } from './AttachmentsHeader';
 import { ContextItemsHeader } from './ContextItemsHeader';
+import { EditMessageHeader } from './EditMessageHeader';
 import { useChatBoxStore } from './stores/chat-box';
 import { useChatMessagesStore } from './stores/chat-messages';
 
 export const SenderHeader: React.FC = () => {
   const currentEmployee = useChatBoxStore.use.currentEmployee();
+  const isEditingMessage = useChatBoxStore.use.isEditingMessage();
   const contextItems = useChatMessagesStore.use.contextItems();
   const attachments = useChatMessagesStore.use.attachments();
 
-  if (!currentEmployee || (!contextItems?.length && !attachments?.length)) {
+  const hasContextItems = !!contextItems?.length;
+  const hasAttachments = !!attachments?.length;
+
+  if (!isEditingMessage && (!currentEmployee || (!hasContextItems && !hasAttachments))) {
     return null;
   }
 
@@ -28,8 +33,13 @@ export const SenderHeader: React.FC = () => {
         padding: '8px 8px 0 8px',
       }}
     >
-      <ContextItemsHeader />
-      <AttachmentsHeader />
+      {isEditingMessage ? (
+        <div>
+          <EditMessageHeader />
+        </div>
+      ) : null}
+      {currentEmployee ? <ContextItemsHeader /> : null}
+      {currentEmployee ? <AttachmentsHeader /> : null}
     </div>
   );
 };
