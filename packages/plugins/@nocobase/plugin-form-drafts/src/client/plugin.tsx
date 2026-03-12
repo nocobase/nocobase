c42082488faf4e47cfdf781fccce127d22da6a49
gchust <gchust@qq.com>
2026-03-12 15:19:56 +0800

    fix: form draft not working correctly

--- packages/plugins/@nocobase/plugin-form-drafts/src/client/plugin.tsx
+++ packages/plugins/@nocobase/plugin-form-drafts/src/client/plugin.tsx
@@ -1,3 +1,12 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
 import { FormBlockModel, Plugin } from '@nocobase/client';
 import { FlowModelContext } from '@nocobase/flow-engine';
 import { Alert, Button } from 'antd';
@@ -66,8 +75,7 @@ FormBlockModel.registerFlow({
           await ctx.draftRepository.create();
           return;
         }
-        const model = ctx.blockModel as FormBlockModel;
-        model.form.setFieldsValue(draft.values || {});
+        await ctx.setFormValues(draft.values);
         ctx.showDraftAlert(
           'This is an unsubmitted draft. You can continue editing and submit it, or click “Delete draft” to start over.',
         );
