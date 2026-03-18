cecd97abc8808a8a3399bd8876eaa8e8218a1238
xilesun <2013xile@gmail.com>
2026-03-18 10:30:38 +0800

    fix: dep

--- packages/plugins/@nocobase/plugin-idp-oauth/build.config.ts
+++ packages/plugins/@nocobase/plugin-idp-oauth/build.config.ts
@@ -38,7 +38,7 @@ async function copyRuntimePackage(packageName: string, copied: Set<string>, log:
 export default defineConfig({
   afterBuild: async (log) => {
     const copied = new Set<string>();
-    const packagesToCopy = ['oidc-provider', 'jose', 'quick-lru'];
+    const packagesToCopy = ['oidc-provider', 'jose', 'quick-lru', 'eta'];
     for (const packageName of packagesToCopy) {
       await copyRuntimePackage(packageName, copied, log);
     }
