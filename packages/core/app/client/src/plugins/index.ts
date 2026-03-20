29945b44f3427b767c4522b11f537ab8d7d1317b
mytharcher-r2 <mytharcher+r2@gmail.com>
2026-03-20 21:33:29 +0800

    fix: ignore symbol-link in update artifict step of build ci

--- packages/core/app/client/src/plugins/index.ts
+++ packages/core/app/client/src/plugins/index.ts
@@ -1,1 +1,12 @@
-export default function devDynamicImport() { return Promise.resolve(null); }
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+export default function devDynamicImport(packageName: string): Promise<any> {
+  return Promise.resolve(null);
+}
