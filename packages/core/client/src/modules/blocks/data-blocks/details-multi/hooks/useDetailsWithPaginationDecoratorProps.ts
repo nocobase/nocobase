bcd167be19a4f276f7ab07f521ed10263fd1819a
safsafa-dev <safa.oues123@gmail.com>
2026-04-12 12:50:49 +0200

    fix: details block with pagination uses get action for non-DB datasources (REST API)

--- packages/core/client/src/modules/blocks/data-blocks/details-multi/hooks/useDetailsWithPaginationDecoratorProps.ts
+++ packages/core/client/src/modules/blocks/data-blocks/details-multi/hooks/useDetailsWithPaginationDecoratorProps.ts
@@ -6,19 +6,33 @@
  * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
-
 import { useParentRecordCommon } from '../../../useParentRecordCommon';
 import { useDetailsWithPaginationBlockParams } from './useDetailsWithPaginationBlockParams';
+import { useDataSource } from '../../../../../data-source/data-source/DataSourceProvider';
+import { useFilterByTk } from '../../../../../block-provider/BlockProvider';
 
 export function useDetailsWithPaginationDecoratorProps(props) {
   let parentRecord;
-
   const { params, parseVariableLoading } = useDetailsWithPaginationBlockParams(props);
+  const dataSource = useDataSource();
+  const filterByTk = useFilterByTk(props);
+  const isDBInstance = dataSource?.getOption('isDBInstance') ?? true;
 
   // association 的值是固定不变的，所以可以在条件中使用 hooks
   if (props.association) {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     parentRecord = useParentRecordCommon(props.association);
+  }
+
+  // For non-DB datasources (e.g. REST API), use 'get' action with filterByTk
+  // because their list and get endpoints return different data structures
+  if (!isDBInstance && filterByTk) {
+    return {
+      parentRecord,
+      action: 'get',
+      params: { filterByTk },
+      parseVariableLoading,
+    };
   }
 
   return {
