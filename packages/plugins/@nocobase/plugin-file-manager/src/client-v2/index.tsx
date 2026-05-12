8aa9f633c5c2b5e20f8348600f47bf6cebe437a9
katherinehhh <katherine_15995@163.com>
2026-05-12 15:58:26 +0800

    fix: build error

--- packages/plugins/@nocobase/plugin-file-manager/src/client-v2/index.tsx
+++ packages/plugins/@nocobase/plugin-file-manager/src/client-v2/index.tsx
@@ -35,3 +35,5 @@ export {
 } from './components';
 export type { DefaultFieldProps } from './components/DefaultField';
 export type { PathFieldProps } from './components/PathField';
+
+export { CardUpload, UploadFieldModel } from './models/UploadFieldModel';
