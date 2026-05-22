ba828d6eda5f5ee0ef1b44f9e9ccf6fd83de3486
Drol <cgyrock@gmail.com>
2026-05-22 17:35:55 +0800

    chore: add collection tags

--- packages/plugins/@nocobase/plugin-backups/src/server/collections/backup-settings.ts
+++ packages/plugins/@nocobase/plugin-backups/src/server/collections/backup-settings.ts
@@ -1,8 +1,18 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
 import { defineCollection } from '@nocobase/database';
 import { SETTINGS } from '../utils';
 
 export default defineCollection({
   name: `${SETTINGS}`,
+  tags: 'business',
   dumpRules: 'required',
   migrationRules: ['overwrite', 'skip'],
   fields: [
