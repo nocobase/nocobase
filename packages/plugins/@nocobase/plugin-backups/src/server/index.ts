fc65d89a9b5f8539b5615eec242867c2cf50b217
Drol <cgyrock@gmail.com>
2026-05-22 17:30:00 +0800

    refactor: expose plugin-backups server api

--- packages/plugins/@nocobase/plugin-backups/src/server/index.ts
+++ packages/plugins/@nocobase/plugin-backups/src/server/index.ts
@@ -1,1 +1,14 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+export { BackupManager } from './managers/backup';
+export type { BackupSettings, BackupTaskResult } from './managers/backup';
+export { RestoreManager } from './managers/restore';
+export { BACKUP_EXTENSION } from './utils';
 export { default } from './plugin';
