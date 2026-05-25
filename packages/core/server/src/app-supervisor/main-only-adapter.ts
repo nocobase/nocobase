580a916d0c36178e6fb4c1b1d8f5412392b6fcec
xilesun <2013xile@gmail.com>
2026-05-25 10:21:56 +0800

    fix(server): align main-only app event dispatch type

--- packages/core/server/src/app-supervisor/main-only-adapter.ts
+++ packages/core/server/src/app-supervisor/main-only-adapter.ts
@@ -115,12 +115,12 @@ export class MainOnlyAdapter implements AppDiscoveryAdapter, AppProcessAdapter {
     await this.apps[appName].runCommand('upgrade');
   }
 
-  async dispatchAppEvent(appName: string, event: string, payload?: any) {
+  async dispatchAppEvent(appName: string, event: string, payload?: any, _context?: { requestId: string }) {
     const app = await this.getApp(appName, { withOutBootStrap: true });
     if (!app) {
       return;
     }
-    return app.emitAsync(event, payload);
+    await app.emitAsync(event, payload);
   }
 
   async removeAllApps() {
