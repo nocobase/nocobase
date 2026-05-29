1f56a1478b02eb5eff152ecbfdaf4fc1b288bc1e
Jiann <lomo_hao@163.com>
2026-05-29 16:59:54 +0800

    fix(plugin-license): support license settings in client v2 (#9555)
    * fix(plugin-license): support license settings in client v2
    
    * fix(plugin-license): align client-v2 license setting layout
    
    * fix(client-v2): support commercial wrapper plugin entry

---
+++ packages/plugins/@nocobase/plugin-license/src/client-v2/locale.ts
@@ -0,0 +1,22 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import { tExpr as flowTExpr, useFlowEngine } from '@nocobase/flow-engine';
+
+const PACKAGE_NAME = '@nocobase/plugin-license';
+
+export function useT() {
+  const engine = useFlowEngine();
+  return (key: string, options?: Record<string, any>) =>
+    engine.context.t(key, { ns: [PACKAGE_NAME, 'client'], ...options });
+}
+
+export function tExpr(key: string) {
+  return flowTExpr(key, { ns: [PACKAGE_NAME, 'client'] });
+}
