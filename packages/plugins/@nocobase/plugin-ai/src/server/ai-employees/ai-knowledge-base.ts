89d2d605f71ffa5d44ceff45312d4ac88b1e29d6
Drol <cgyrock@gmail.com>
2026-06-10 15:54:35 +0800

    refactor(plugin-ai): reuse knowledge base feature search

--- packages/plugins/@nocobase/plugin-ai/src/server/ai-employees/ai-knowledge-base.ts
+++ packages/plugins/@nocobase/plugin-ai/src/server/ai-employees/ai-knowledge-base.ts
@@ -11,7 +11,6 @@ import { ChatPromptTemplate } from '@langchain/core/prompts';
 import { AIEmployee } from '../../collections/ai-employees';
 import { EEFeatures } from '../manager/ai-feature-manager';
 import type PluginAIServer from '../plugin';
-import { KnowledgeBaseGroup, DocumentSegmentedWithScore } from '../types';
 import _ from 'lodash';
 
 export type KnowledgeBaseRetrieveOptions = { username?: string; employee?: AIEmployee; query: string };
@@ -20,95 +19,27 @@ export class KnowledgeBaseManager {
   constructor(private readonly plugin: PluginAIServer) {}
 
   async retrievePrompt({ username, employee, query }: KnowledgeBaseRetrieveOptions): Promise<string> {
-    employee = employee ?? (await this.getEmployee(username));
+    employee = employee ?? (username ? await this.getEmployee(username) : null);
     if (!employee) {
       return 'Specified knowledge base not existed';
     }
+    const { knowledgeBaseKeys, topK, score } = employee.knowledgeBase ?? {};
+    if (!knowledgeBaseKeys || _.isEmpty(knowledgeBaseKeys)) {
+      return 'Specified knowledge base not existed';
+    }
 
-    const promptTemplate = ChatPromptTemplate.fromTemplate(employee.knowledgeBasePrompt);
-    const docs = await this.retrieve({
-      employee,
-      query,
-    });
+    const promptTemplate = ChatPromptTemplate.fromTemplate(employee.knowledgeBasePrompt ?? '{knowledgeBaseData}');
+    const docs = await this.plugin.features.knowledgeBase.search({ knowledgeBaseKeys, query, topK, score });
     if (!docs?.length) {
       return 'No document match in knowledge base';
     }
 
     const knowledgeBaseData = docs.map((x) => x.content).join('\n');
     return _.isEmpty(knowledgeBaseData)
-      ? undefined
+      ? 'No document match in knowledge base'
       : await promptTemplate.format({
           knowledgeBaseData,
         });
-  }
-
-  async retrieve({ username, employee, query }: KnowledgeBaseRetrieveOptions): Promise<DocumentSegmentedWithScore[]> {
-    const vectorStoreProvider = this.plugin.features.vectorStoreProvider;
-    let queryResult: DocumentSegmentedWithScore[] = [];
-    if (!query || _.isEmpty(query)) {
-      return queryResult;
-    }
-    employee = employee ?? (await this.getEmployee(username));
-    if (!employee) {
-      return queryResult;
-    }
-    const { topK, score } = this.getAIEmployeeKnowledgeBaseConfig(employee);
-    const knowledgeBaseGroup = await this.getKnowledgeBaseGroup(employee);
-    for (const entry of knowledgeBaseGroup) {
-      const { vectorStoreConfig, knowledgeBaseType, knowledgeBaseList } = entry;
-      if (!knowledgeBaseList || _.isEmpty(knowledgeBaseList)) {
-        continue;
-      }
-
-      if (knowledgeBaseType === 'LOCAL') {
-        const vectorStoreService = await vectorStoreProvider.createVectorStoreService(
-          vectorStoreConfig.vectorStoreProvider,
-          [
-            {
-              key: 'vectorStoreConfigKey',
-              value: vectorStoreConfig.vectorStoreConfigKey ?? '',
-            },
-          ],
-        );
-        const knowledgeBaseOuterIds = knowledgeBaseList.map((x) => x.knowledgeBaseOuterId);
-        const result = await vectorStoreService.search(query, {
-          topK,
-          score,
-          filter: {
-            knowledgeBaseOuterId: { in: knowledgeBaseOuterIds },
-          },
-        });
-        queryResult = [...queryResult, ...result];
-      } else if (knowledgeBaseType === 'READONLY') {
-        const vectorStoreService = await vectorStoreProvider.createVectorStoreService(
-          vectorStoreConfig.vectorStoreProvider,
-          [
-            {
-              key: 'vectorStoreConfigKey',
-              value: vectorStoreConfig.vectorStoreConfigKey ?? '',
-            },
-          ],
-        );
-        const result = await vectorStoreService.search(query, {
-          topK,
-          score,
-        });
-        queryResult = [...queryResult, ...result];
-      } else if (knowledgeBaseType === 'EXTERNAL') {
-        for (const knowledgeBase of knowledgeBaseList) {
-          const vectorStoreService = await vectorStoreProvider.createVectorStoreService(
-            vectorStoreConfig.vectorStoreProvider,
-            this.plugin.app.environment.renderJsonTemplate(knowledgeBase.vectorStoreProps ?? []),
-          );
-          const result = await vectorStoreService.search(query, {
-            topK,
-            score,
-          });
-          queryResult = [...queryResult, ...result];
-        }
-      }
-    }
-    return queryResult;
   }
 
   async isEnabledKnowledgeBase(username: string): Promise<boolean>;
@@ -119,22 +50,6 @@ export class KnowledgeBaseManager {
       typeof usernameOrEmployee === 'string' ? await this.getEmployee(usernameOrEmployee) : usernameOrEmployee;
     const knowledgeBaseEnabled = employee?.enableKnowledgeBase;
     return featureEnabled && knowledgeBaseEnabled;
-  }
-
-  private getAIEmployeeKnowledgeBaseConfig(employee: AIEmployee) {
-    const { topK, score } = employee?.knowledgeBase ?? {};
-    return {
-      topK,
-      score,
-    };
-  }
-
-  private async getKnowledgeBaseGroup(employee: AIEmployee): Promise<KnowledgeBaseGroup[]> {
-    const { knowledgeBaseKeys } = employee?.knowledgeBase ?? {};
-    if (!knowledgeBaseKeys || _.isEmpty(knowledgeBaseKeys)) {
-      return [];
-    }
-    return await this.plugin.features.knowledgeBase.getKnowledgeBaseGroup(knowledgeBaseKeys);
   }
 
   private async getEmployee(username: string) {
