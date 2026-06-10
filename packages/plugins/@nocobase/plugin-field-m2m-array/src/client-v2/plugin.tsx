7d9f926e08f4b84094f64b3c3e9cd960b645027b
katherinehhh <katherine_15995@163.com>
2026-06-10 15:19:09 +0800

    feat(plugin-field-m2m-array): add client v2 entry

---
+++ packages/plugins/@nocobase/plugin-field-m2m-array/src/client-v2/plugin.tsx
@@ -0,0 +1,19 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import { Application, Plugin } from '@nocobase/client-v2';
+import { MBMFieldInterface } from './interface';
+
+export class PluginM2MArrayClient extends Plugin<unknown, Application> {
+  async load() {
+    this.app.addFieldInterfaces([MBMFieldInterface]);
+  }
+}
+
+export default PluginM2MArrayClient;
