25c43374bf1cb5e2282fa033d1fa8d87c0cba928
PiEgg <marksz@teamsz.xyz>
2026-06-15 17:19:55 +0800

    feat(plugin-workflow): migrate multi-conditions node to v2

--- packages/plugins/@nocobase/plugin-workflow/src/client-v2/nodes/multi-conditions.tsx
+++ packages/plugins/@nocobase/plugin-workflow/src/client-v2/nodes/multi-conditions.tsx
@@ -11,6 +11,7 @@ import React from 'react';
 import { ClusterOutlined } from '@ant-design/icons';
 import { Instruction } from '../canvas/Instruction';
 import { NAMESPACE } from '../locale';
+import { MULTI_CONDITION_BRANCH_INDEX, createEmptyMultiCondition } from './components/multiConditionsShared';
 
 const t = (key: string) => `{{t("${key}", { ns: "${NAMESPACE}" })}}`;
 
@@ -18,5 +19,26 @@ export default class extends Instruction {
   type = 'multi-conditions';
   title = t('Multi conditions');
   group = 'control';
+  description = `{{t('From left to right, attempt each branch sequentially based on the configured conditions. Only branches that meet the conditions will be executed. Otherwise, the next branch will be attempted. If none of the branches meet the conditions, it can either exit the process or continue to the next node based on configuration.', { ns: "${NAMESPACE}" })}}`;
   icon = (<ClusterOutlined />);
+  branching = [
+    {
+      label: t('First condition'),
+      value: MULTI_CONDITION_BRANCH_INDEX.DEFAULT,
+    },
+    {
+      label: t('Otherwise'),
+      value: MULTI_CONDITION_BRANCH_INDEX.OTHERWISE,
+    },
+  ];
+  FieldsetLoader = () => import('./components/multi-conditions').then((m) => ({ default: m.MultiConditionsFieldset }));
+  ComponentLoader = () =>
+    import('./components/multi-conditions').then((m) => ({ default: m.MultiConditionsCanvasComponent }));
+
+  createDefaultConfig() {
+    return {
+      conditions: [createEmptyMultiCondition()],
+      continueOnNoMatch: false,
+    };
+  }
 }
