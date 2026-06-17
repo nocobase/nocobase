42b56b8bf403f1fc837f565dce0b172f3cd8d322
PiEgg <marksz@teamsz.xyz>
2026-06-17 17:09:55 +0800

    refactor(plugin-workflow): migrate output node to v2 and align result display (#9813)

--- packages/plugins/@nocobase/plugin-workflow/src/client-v2/nodes/output.tsx
+++ packages/plugins/@nocobase/plugin-workflow/src/client-v2/nodes/output.tsx
@@ -11,6 +11,7 @@ import React from 'react';
 import { ProfileOutlined } from '@ant-design/icons';
 import { Instruction } from '../canvas/Instruction';
 import { NAMESPACE } from '../locale';
+import { BaseTypeSets } from '../canvas/collectionFieldOptions';
 
 const t = (key: string) => `{{t("${key}", { ns: "${NAMESPACE}" })}}`;
 
@@ -18,5 +19,22 @@ export default class extends Instruction {
   type = 'output';
   title = t('Output');
   group = 'control';
+  description = t(
+    'Set output data of this workflow. When this one is executed as a subflow, the output could be used as variables in downstream nodes of super workflow. You can also use this node in an AI employee workflow, to define what to output. If this node is added multiple times, the value of the last executed node prevails.',
+  );
   icon = (<ProfileOutlined />);
+  FieldsetLoader = () => import('./components/output').then((m) => ({ default: m.OutputFieldset }));
+
+  useVariables({ key, title }, { types }) {
+    if (
+      types &&
+      !types.some((type) => type in BaseTypeSets || Object.values(BaseTypeSets).some((set) => set.has(type)))
+    ) {
+      return null;
+    }
+    return {
+      value: key,
+      label: title,
+    };
+  }
 }
