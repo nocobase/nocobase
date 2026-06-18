74e82df9c4cb22e9d80f211fa267fc54bb1af434
Drol <cgyrock@gmail.com>
2026-06-19 01:48:14 +0800

    refactor(plugin-ai): reuse v2 ai employee flow models in v1

--- packages/plugins/@nocobase/plugin-ai/src/client/ai-employees/flow/models/AIEmployeeShortcutListModel.tsx
+++ packages/plugins/@nocobase/plugin-ai/src/client/ai-employees/flow/models/AIEmployeeShortcutListModel.tsx
@@ -7,48 +7,4 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
-import React from 'react';
-import { DndProvider, DragHandler, Droppable, FlowModel, FlowModelRenderer } from '@nocobase/flow-engine';
-import { AIEmployeeShortcutModel } from './AIEmployeeShortcutModel';
-
-type AIEmployeeShortcutListModelStructure = {
-  subModels: {
-    shortcuts: AIEmployeeShortcutModel[];
-  };
-};
-
-export class AIEmployeeShortcutListModel extends FlowModel<AIEmployeeShortcutListModelStructure> {
-  isNewModel = false;
-
-  render() {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          gap: '8px',
-        }}
-      >
-        <DndProvider>
-          {this.mapSubModels('shortcuts', (shortcut) => {
-            return (
-              <Droppable model={shortcut} key={shortcut.uid}>
-                <FlowModelRenderer
-                  key={shortcut.uid}
-                  model={shortcut}
-                  showFlowSettings={!shortcut.props?.builtIn}
-                  extraToolbarItems={[
-                    {
-                      key: 'drag-handler',
-                      component: DragHandler,
-                      sort: 1,
-                    },
-                  ]}
-                />
-              </Droppable>
-            );
-          })}
-        </DndProvider>
-      </div>
-    );
-  }
-}
+export { AIEmployeeShortcutListModel } from '../../../../client-v2/models/ai-employees/AIEmployeeShortcutListModel';
