902348ba985ea3eecb46fb72e39885a5442fd261
gchust <gchust@qq.com>
2026-06-24 15:01:18 +0000

    feat(plugin-vsc-file): scaffold plugin

---
+++ packages/plugins/@nocobase/plugin-vsc-file/src/client-v2/plugin.tsx
@@ -0,0 +1,16 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
+ * For more information, please refer to: https://www.nocobase.com/agreement.
+ */
+
+import { Plugin } from '@nocobase/client-v2';
+
+export class PluginVscFileClientV2 extends Plugin {
+  async load() {}
+}
+
+export default PluginVscFileClientV2;
