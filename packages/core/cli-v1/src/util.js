9389ee5581565fbea75d1f9020b285c4220027fe
gchust <gchust@qq.com>
2026-07-27 00:11:05 +0000

    fix: bug

--- packages/core/cli-v1/src/util.js
+++ packages/core/cli-v1/src/util.js
@@ -354,6 +354,7 @@ exports.genTsConfigPaths = function genTsConfigPaths() {
       paths[`${packageJsonName}/compiler`] = [`${relativePath}/src/compiler`];
       paths[`${packageJsonName}/compiler/build-identity`] = [`${relativePath}/src/compiler/build-identity`];
       paths[`${packageJsonName}/compiler/portable`] = [`${relativePath}/src/compiler/portable`];
+      paths[`${packageJsonName}/server`] = [`${relativePath}/src/server`];
       paths[`${packageJsonName}/settings`] = [`${relativePath}/src/settings`];
     }
   });
